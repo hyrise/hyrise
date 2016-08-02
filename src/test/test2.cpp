79f2ad74cde1cecfb9697a33338aa33be6818292
martin.boissier <martin.boissier@hpi.uni-potsdam.de>
2016-08-02 18:58:00 +0200

    Changed test2.cpp to print storage manager information.

--- src/test/test2.cpp
+++ src/test/test2.cpp
@@ -2,21 +2,21 @@
 #include "storage/storage_manager.hpp"
 
 int main() {
-	std::shared_ptr<opossum::table> t(new opossum::table(2));
+	std::shared_ptr<opossum::table> t(new opossum::table(100));
 	// auto t = std::make_unique(opossum::table(2));
 
-	t->add_column("a", opossum::column_type::int_type);
-	t->add_column("langer spaltenname", opossum::column_type::float_type);
-	t->add_column("c", opossum::column_type::string_type);
-	t->add_column("d", opossum::column_type::double_type);
+	t->add_column("a", "int");
+	t->add_column("langer spaltenname", "float");
+	t->add_column("c", "string");
+	t->add_column("d", "double");
 
 	t->append({123, 456.7, "test", 51});
 	t->append({12345, 456.7, "test", 516.2});
 	t->append({12345, 456.7, "testabc", 62});
 
-	t->print();
-
 	opossum::storage_manager s;
 
 	s.add_table("tab", std::move(t));
+
+	s.print();
 }
