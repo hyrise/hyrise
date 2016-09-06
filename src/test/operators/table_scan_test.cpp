73fe73359c51239578152eb7c382d7ab0c96ecfb
Nils Thamm <nils.thamm@student.hpi.de>
2016-09-06 18:00:27 +0200

    prevent referencing to reference columns during table scan

--- src/test/operators/table_scan_test.cpp
+++ src/test/operators/table_scan_test.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <memory>
 #include <utility>
 
@@ -20,9 +21,9 @@ class operators_table_scan : public ::testing::Test {
     test_table->add_column("a", "int");
     test_table->add_column("b", "float");
 
-    test_table->append({123, 456.7});
-    test_table->append({1234, 457.7});
-    test_table->append({12345, 458.7});
+    test_table->append({123, 456.7f});
+    test_table->append({1234, 457.7f});
+    test_table->append({12345, 458.7f});
 
     opossum::StorageManager::get().add_table("table_a", std::move(test_table));
 
@@ -34,15 +35,17 @@ class operators_table_scan : public ::testing::Test {
   std::shared_ptr<opossum::GetTable> gt;
 };
 
-TEST_F(operators_table_scan, single_scan_test) {
+TEST_F(operators_table_scan, double_scan_test) {
   auto scan_1 = std::make_shared<opossum::TableScan>(gt, "a", ">=", 1234);
   scan_1->execute();
-  // auto scan_2 = std::make_shared<opossum::TableScan>(scan_1, "b", "<", 457.9);
-  // scan_2->execute();
-  EXPECT_NE(type_cast<int>((*(scan_1->get_output()->get_chunk(0).get_column(0)))[0]), 123);
-  EXPECT_NE(type_cast<int>((*(scan_1->get_output()->get_chunk(0).get_column(0)))[1]), 123);
 
-  EXPECT_EQ(scan_1->get_output()->row_count(), (u_int)2);
+  auto scan_2 = std::make_shared<opossum::TableScan>(scan_1, "b", "<", 457.9);
+  scan_2->execute();
+
+  EXPECT_EQ(type_cast<int>((*(scan_1->get_output()->get_chunk(0).get_column(0)))[0]), 1234);
+  EXPECT_EQ(type_cast<float>((*(scan_1->get_output()->get_chunk(0).get_column(1)))[1]), 458.7f);
+
+  EXPECT_EQ(scan_2->get_output()->row_count(), (u_int)1);
 }
 
 class operators_table_scan_impl : public ::testing::Test {
@@ -52,9 +55,9 @@ class operators_table_scan_impl : public ::testing::Test {
     test_table->add_column("a", "int");
     test_table->add_column("b", "float");
 
-    test_table->append({123, 456.7});
-    test_table->append({1234, 457.7});
-    test_table->append({12345, 458.7});
+    test_table->append({123, 456.7f});
+    test_table->append({1234, 457.7f});
+    test_table->append({12345, 458.7f});
 
     opossum::StorageManager::get().add_table("table_a", std::move(test_table));
 
@@ -71,10 +74,10 @@ TEST_F(operators_table_scan_impl, single_scan_returns_correct_row_count) {
       make_unique_by_column_type<AbstractOperatorImpl, TableScanImpl>("int", gt, "a", ">=", 1234));
   scan->execute();
 
-  EXPECT_NE(type_cast<int>((*(scan->get_output()->get_chunk(0).get_column(0)))[0]), 123);
-  EXPECT_NE(type_cast<int>((*(scan->get_output()->get_chunk(0).get_column(0)))[1]), 123);
+  EXPECT_EQ(type_cast<int>((*(scan->get_output()->get_chunk(0).get_column(0)))[0]), 1234);
+  EXPECT_EQ(type_cast<int>((*(scan->get_output()->get_chunk(0).get_column(0)))[1]), 12345);
 
-  EXPECT_EQ(scan->get_output()->row_count(), (u_int)2);
+  EXPECT_EQ(scan->get_output()->row_count(), (uint32_t)2);
 }
 
 TEST_F(operators_table_scan_impl, unknown_operator_throws_exception) {
