4572522a98c30a866b82c9887426f1c027b1b4f6
Nils Thamm <nils.thamm@student.hpi.de>
2016-09-20 15:47:28 +0200

    minor refactoring

--- src/lib/operators/projection.cpp
+++ src/lib/operators/projection.cpp
@@ -4,7 +4,7 @@
 
 namespace opossum {
 Projection::Projection(const std::shared_ptr<AbstractOperator> in, const std::vector<std::string> &columns)
-    : _table(in->get_output()), _column_filter(columns), _output(new Table) {}
+    : AbstractOperator(in), _column_filter(columns), _output(new Table) {}
 
 const std::string Projection::get_name() const { return "Projection"; }
 
@@ -14,9 +14,9 @@ uint8_t Projection::get_num_out_tables() const { return 1; }
 
 void Projection::execute() {
   for (auto column_name : _column_filter) {
-    auto column_id = _table->get_column_id_by_name(column_name);
-    auto ref_col = std::make_shared<ReferenceColumn>(_table, column_id, nullptr);
-    _output->add_column(column_name, _table->get_column_type(column_id), false);
+    auto column_id = _input_left->get_column_id_by_name(column_name);
+    auto ref_col = std::make_shared<ReferenceColumn>(_input_left, column_id, nullptr);
+    _output->add_column(column_name, _input_left->get_column_type(column_id), false);
     _output->get_chunk(0).add_column(ref_col);
   }
 }
