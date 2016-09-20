46d3b140cc6fa07d7432e4e65196f0efbe8bf5e2
Nils Thamm <nils.thamm@student.hpi.de>
2016-09-20 17:31:10 +0200

    added constructors and cpp file

---
+++ src/lib/operators/sort.cpp
@@ -0,0 +1,18 @@
+#include "sort.hpp"
+
+namespace opossum {
+Sort::Sort(const std::shared_ptr<AbstractOperator> in, const std::string &sort_column_name, const bool ascending)
+    : AbstractOperator(in),
+      _impl(make_unique_by_column_type<AbstractOperatorImpl, SortImpl>(
+          _input_left->get_column_type(_input_left->get_column_id_by_name(sort_column_name)), in, sort_column_name,
+          ascending)) {}
+      const std::string Sort::get_name() const { return "Sort"; }
+
+uint8_t Sort::get_num_in_tables() const { return 1; }
+
+uint8_t Sort::get_num_out_tables() const { return 1; }
+
+void Sort::execute() { _impl->execute(); }
+
+std::shared_ptr<Table> Sort::get_output() const { return _impl->get_output(); }
+}
--- src/lib/operators/sort.hpp
+++ src/lib/operators/sort.hpp
@@ -5,6 +5,10 @@
 #include "abstract_operator.hpp"
 
 namespace opossum {
+
+template <typename T>
+class SortImpl;
+
 class Sort : public AbstractOperator {
  public:
   Sort(const std::shared_ptr<AbstractOperator> in, const std::string &sort_column_name, const bool ascending = true);
@@ -15,5 +19,28 @@ class Sort : public AbstractOperator {
   virtual const std::string get_name() const;
   virtual uint8_t get_num_in_tables() const;
   virtual uint8_t get_num_out_tables() const;
+
+  const std::unique_ptr<AbstractOperatorImpl> _impl;
+};
+
+template <typename T>
+class SortImpl : public AbstractOperatorImpl {
+ public:
+  SortImpl(const std::shared_ptr<AbstractOperator> in, const std::string &sort_column_name, const bool ascending = true)
+      : _sort_column_id(in->get_output()->get_column_id_by_name(sort_column_name)),
+        _ascending(ascending),
+        _output(new Table),
+        _pos_list(new PosList) {}
+
+  virtual void execute() {
+
+  }
+
+  virtual std::shared_ptr<Table> get_output() const;
+
+  const size_t _sort_column_id;
+  const bool _ascending;
+  std::shared_ptr<Table> _output;
+  std::shared_ptr<PosList> _pos_list;
 };
 }
