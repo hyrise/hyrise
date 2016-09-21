bfe5ce32d1eee4829486a576e40929f179227cdf
Nils Thamm <nils.thamm@student.hpi.de>
2016-09-21 15:37:18 +0200

    added sorting for value and reference columns

--- src/lib/operators/sort.cpp
+++ src/lib/operators/sort.cpp
@@ -6,7 +6,7 @@ Sort::Sort(const std::shared_ptr<AbstractOperator> in, const std::string &sort_c
       _impl(make_unique_by_column_type<AbstractOperatorImpl, SortImpl>(
           _input_left->get_column_type(_input_left->get_column_id_by_name(sort_column_name)), in, sort_column_name,
           ascending)) {}
-      const std::string Sort::get_name() const { return "Sort"; }
+const std::string Sort::get_name() const { return "Sort"; }
 
 uint8_t Sort::get_num_in_tables() const { return 1; }
 
--- src/lib/operators/sort.hpp
+++ src/lib/operators/sort.hpp
@@ -1,8 +1,11 @@
 #pragma once
 
+#include <algorithm>
+#include <functional>
 #include <memory>
 
 #include "abstract_operator.hpp"
+#include "storage/reference_column.hpp"
 
 namespace opossum {
 
@@ -27,20 +30,81 @@ template <typename T>
 class SortImpl : public AbstractOperatorImpl {
  public:
   SortImpl(const std::shared_ptr<AbstractOperator> in, const std::string &sort_column_name, const bool ascending = true)
-      : _sort_column_id(in->get_output()->get_column_id_by_name(sort_column_name)),
+      : _in_table(in->get_output()),
+        _sort_column_id(_in_table->get_column_id_by_name(sort_column_name)),
         _ascending(ascending),
         _output(new Table),
-        _pos_list(new PosList) {}
-
-  virtual void execute() {
-
+        _pos_list(new PosList),
+        _row_id_value_vector(new std::vector<std::pair<RowID, T>>()) {
+    for (size_t column_id = 0; column_id < _in_table->col_count(); ++column_id) {
+      std::shared_ptr<ReferenceColumn> ref;
+      if (auto ref_col = std::dynamic_pointer_cast<ReferenceColumn>(_in_table->get_chunk(0).get_column(column_id))) {
+        ref = std::make_shared<ReferenceColumn>(ref_col->get_referenced_table(), column_id, _pos_list);
+      } else {
+        ref = std::make_shared<ReferenceColumn>(_in_table, column_id, _pos_list);
+      }
+      _output->add_column(_in_table->get_column_name(column_id), _in_table->get_column_type(column_id), false);
+      _output->get_chunk(0).add_column(ref);
+      // TODO(Anyone): do we want to distinguish between chunk tables and "reference tables"?
+    }
   }
 
-  virtual std::shared_ptr<Table> get_output() const;
+  virtual void execute() {
+    for (size_t chunk = 0; chunk < _in_table->chunk_count(); chunk++) {
+      if (auto value_column =
+              std::dynamic_pointer_cast<ValueColumn<T>>(_in_table->get_chunk(chunk).get_column(_sort_column_id))) {
+        auto &values = value_column->get_values();
+        for (size_t offset = 0; offset < values.size(); offset++) {
+          _row_id_value_vector->emplace_back(get_row_id_from_chunk_id_and_chunk_offset(chunk, offset), values[offset]);
+        }
+      } else if (auto referenced_column = std::dynamic_pointer_cast<ReferenceColumn>(
+                     _in_table->get_chunk(chunk).get_column(_sort_column_id))) {
+        auto val_table = referenced_column->get_referenced_table();
+        std::vector<std::vector<T>> values = {};
+        for (size_t chunk = 0; chunk < val_table->chunk_count(); chunk++) {
+          if (auto val_col =
+                  std::dynamic_pointer_cast<ValueColumn<T>>(val_table->get_chunk(chunk).get_column(_sort_column_id))) {
+            values.emplace_back(val_col->get_values());
+          } else {
+            throw std::logic_error("Referenced table must only contain value columns");
+          }
+        }
+        auto in_pos_list = referenced_column->get_pos_list();
 
+        for (size_t pos = 0; pos < in_pos_list->size(); pos++) {
+          auto row_id = (*in_pos_list)[pos];
+          auto chunk_id = get_chunk_id_from_row_id(row_id);
+          auto chunk_offset = get_chunk_offset_from_row_id(row_id);
+          _row_id_value_vector->emplace_back(row_id, values[chunk_id][chunk_offset]);
+        }
+      } else {
+        throw std::logic_error("Column must either be a value or reference column");
+      }
+    }
+    if (_ascending) {
+      sort_with_operator<std::less<>>();
+    } else {
+      sort_with_operator<std::greater<>>();
+    }
+    for (size_t row = 0; row < _row_id_value_vector->size(); row++) {
+      _pos_list->emplace_back(_row_id_value_vector->at(row).first);
+    }
+  }
+
+  template <typename Comp>
+  void sort_with_operator() {
+    Comp comp;
+    std::sort(_row_id_value_vector->begin(), _row_id_value_vector->end(),
+              [comp](std::pair<RowID, T> a, std::pair<RowID, T> b) { return comp(a.second, b.second); });
+  }
+
+  virtual std::shared_ptr<Table> get_output() const { return _output; }
+
+  const std::shared_ptr<Table> _in_table;
   const size_t _sort_column_id;
   const bool _ascending;
   std::shared_ptr<Table> _output;
   std::shared_ptr<PosList> _pos_list;
+  std::shared_ptr<std::vector<std::pair<RowID, T>>> _row_id_value_vector;
 };
 }
