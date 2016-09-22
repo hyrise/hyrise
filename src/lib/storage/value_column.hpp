0309c0a7ba1c2d24c44248fb615cea5295286351
Markus Dreseler <markus@dreseler.de>
2016-09-22 10:24:23 +0200

    Adds comments to some hpp files

--- src/lib/storage/value_column.hpp
+++ src/lib/storage/value_column.hpp
@@ -10,14 +10,21 @@ namespace opossum {
 template <typename T>
 class ValueColumn : public BaseColumn {
  public:
+  // default constructor
   ValueColumn() {}
 
+  // TODO(anyone) remove copy constructor
+
+  // return the value at a certain position
   virtual AllTypeVariant operator[](const size_t i) const { return _values[i]; }
 
+  // add a value to the end
   virtual void append(const AllTypeVariant& val) { _values.push_back(type_cast<T>(val)); }
 
+  // returns all values
   const std::vector<T>& get_values() const { return _values; }
 
+  // return the number of entries
   virtual size_t size() const { return _values.size(); }
 
  protected:
