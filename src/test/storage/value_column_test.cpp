e0e53982143495a490db3d732259c532b8857e55
Stefan Klauck <klauck2@gmail.com>
2016-10-07 20:56:15 +0200

    Splitted up StorageValueColumnTest into multiple testcases

--- src/test/storage/value_column_test.cpp
+++ src/test/storage/value_column_test.cpp
@@ -1,21 +1,24 @@
 #include <string>
+#include <vector>
 
 #include "gtest/gtest.h"
 
 #include "../../lib/storage/value_column.hpp"
 
-TEST(StorageValueColumnTest, CreateColumn) {
-  // create column of different types
+class StorageValueColumnTest : public ::testing::Test {
+ protected:
   opossum::ValueColumn<int> vc_int;
-  EXPECT_EQ(vc_int.size(), 0u);
-
   opossum::ValueColumn<std::string> vc_str;
-  EXPECT_EQ(vc_str.size(), 0u);
-
   opossum::ValueColumn<double> vc_double;
-  EXPECT_EQ(vc_double.size(), 0u);
+};
 
-  // add value of same type
+TEST_F(StorageValueColumnTest, GetSize) {
+  EXPECT_EQ(vc_int.size(), 0u);
+  EXPECT_EQ(vc_str.size(), 0u);
+  EXPECT_EQ(vc_double.size(), 0u);
+}
+
+TEST_F(StorageValueColumnTest, AddValueOfSameType) {
   vc_int.append(3);
   EXPECT_EQ(vc_int.size(), 1u);
 
@@ -24,17 +27,29 @@ TEST(StorageValueColumnTest, CreateColumn) {
 
   vc_double.append(3.14);
   EXPECT_EQ(vc_double.size(), 1u);
+}
 
-  // add value of different type
+TEST_F(StorageValueColumnTest, AddValueOfDifferentType) {
   vc_int.append(3.14);
-  EXPECT_EQ(vc_int.size(), 2u);
+  EXPECT_EQ(vc_int.size(), 1u);
   EXPECT_THROW(vc_int.append("Hi"), std::exception);
 
   vc_str.append(3);
   vc_str.append(4.44);
-  EXPECT_EQ(vc_str.size(), 3u);
+  EXPECT_EQ(vc_str.size(), 2u);
 
   vc_double.append(4);
-  EXPECT_EQ(vc_double.size(), 2u);
+  EXPECT_EQ(vc_double.size(), 1u);
   EXPECT_THROW(vc_double.append("Hi"), std::exception);
+}
+
+TEST_F(StorageValueColumnTest, RetrieveValue) {
+  vc_int.append(3);
+  EXPECT_EQ(vc_int.get_values()[0], 3);
+
+  vc_str.append("Hello");
+  EXPECT_EQ(vc_str.get_values()[0], "Hello");
+
+  vc_double.append(3.14);
+  EXPECT_EQ(vc_double.get_values()[0], 3.14);
 }
