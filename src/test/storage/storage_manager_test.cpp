46cc4099ae1c1bd096eabd2648a41991eb5f850c
Stefan Klauck <klauck2@gmail.com>
2016-10-07 17:49:21 +0200

    Added tests for Opossum StorageManager

---
+++ src/test/storage/storage_manager_test.cpp
@@ -0,0 +1,33 @@
+#include <memory>
+
+#include "gtest/gtest.h"
+
+#include "../../lib/storage/storage_manager.hpp"
+#include "../../lib/storage/table.hpp"
+
+class StorageManagerTest : public ::testing::Test {
+ protected:
+  virtual void SetUp() {
+    auto &sm = opossum::StorageManager::get();
+    auto t1 = std::make_shared<opossum::Table>();
+    auto t2 = std::make_shared<opossum::Table>(4);
+
+    sm.add_table("first_table", t1);
+    sm.add_table("second_table", t2);
+  }
+};
+
+TEST_F(StorageManagerTest, GetTable) {
+  auto &sm = opossum::StorageManager::get();
+  auto t3 = sm.get_table("first_table");
+  auto t4 = sm.get_table("second_table");
+  EXPECT_THROW(sm.get_table("third_table"), std::exception);
+}
+
+TEST_F(StorageManagerTest, DropTable) {
+  auto &sm = opossum::StorageManager::get();
+  sm.drop_table("first_table");
+  EXPECT_THROW(sm.get_table("first_table"), std::exception);
+  // TODO(MB): Should the StorageManager catch exceptions for get_table executed with unknown name?
+  // EXPECT_THROW(sm.drop_table("first_table"), std::exception);
+}
