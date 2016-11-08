4f33b7c5c13a32732143e8924f4d9fd9fb28946a
Marvin Keller <marvin.keller@valsight.com>
2016-11-08 14:25:37 +0100

    added failing test for testing

--- src/test/storage/table_test.cpp
+++ src/test/storage/table_test.cpp
@@ -17,7 +17,7 @@ TEST_F(StorageTableTest, ChunkCount) {
   t.append({4, "Hello,"});
   t.append({6, "world"});
   t.append({3, "!"});
-  EXPECT_EQ(t.chunk_count(), 2u);
+  EXPECT_EQ(t.chunk_count(), 7u);
 }
 
 TEST_F(StorageTableTest, GetChunk) {
