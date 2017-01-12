c619efbba7674b62dc69380a3ad98967591fa469
Max Jendruk <code@jendruk.com>
2017-01-12 20:44:26 +0100

    Update AbstractModifyingOperator’s interface

--- src/lib/operators/abstract_modifying_operator.hpp
+++ src/lib/operators/abstract_modifying_operator.hpp
@@ -3,27 +3,31 @@
 #include <cstdint>
 #include <memory>
 
-#include "../storage/table.hpp"
+#include "storage/table.hpp"
 
 namespace opossum {
 
 class AbstractModifyingOperator {
  public:
-  AbstractModifyingOperator(std::shared_ptr<Table> table) : _table(table) {}
+  AbstractModifyingOperator(const std::shared_ptr<const AbstractOperator> & op) : _operator{op}, _succeeded{true} {}
 
-  virtual void execute(const uint32_t tid) = 0;
+  virtual void execute(const TransactionContext & context) = 0;
   virtual void commit(const uint32_t cid) = 0;
   virtual void abort() = 0;
 
-  virtual std::shared_ptr<const Table> get_output() const { return nullptr; };
+  bool succeeded() const { return _succeeded; };
+
+  std::shared_ptr<const Table> get_output() const override { return nullptr; };
 
   virtual const std::string name() const = 0;
   virtual uint8_t num_in_tables() const = 0;
-  virtual uint8_t num_out_tables() const { return 0; };
+
+  uint8_t num_out_tables() const override { return 0; };
 
  protected:
-  std::shared_ptr<Table> _table;
+  std::shared_ptr<AbstractOperator> _operator;
   std::vector<RowID> _modified_rows;
+  bool _succeeded; // false if transaction needs to be aborted
 };
 
 }  // namespace opossum
