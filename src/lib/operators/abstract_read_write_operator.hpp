1bf8bd8d5509a1d2a9f020b09e3b5688d48608e8
Max Jendruk <code@jendruk.com>
2017-01-26 16:11:18 +0100

    Rename succeeded flag to execute_failed

--- src/lib/operators/abstract_read_write_operator.hpp
+++ src/lib/operators/abstract_read_write_operator.hpp
@@ -13,18 +13,18 @@ class AbstractReadWriteOperator : public AbstractOperator {
  public:
   explicit AbstractReadWriteOperator(const std::shared_ptr<const AbstractOperator> left,
                                      const std::shared_ptr<const AbstractOperator> right = nullptr)
-      : AbstractOperator(left, right), _succeeded{true} {}
+      : AbstractOperator(left, right), _execute_failed{false} {}
 
   std::shared_ptr<const Table> on_execute(const TransactionContext* context) override = 0;
   virtual void commit(const uint32_t cid) = 0;
   virtual void abort() = 0;
 
-  bool succeeded() const { return _succeeded; }
+  bool execute_failed() const { return _execute_failed; }
 
   uint8_t num_out_tables() const override { return 0; };
 
  protected:
-  bool _succeeded;
+  bool _execute_failed;
 };
 
 }  // namespace opossum
