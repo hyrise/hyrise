8ad0aefd13ede278088a0b90c13c2c25d6d52874
Sven Ihde <sven.ihde@student.hpi.de>
2017-01-29 13:20:38 +0100

    Add basic layout for SortMergeJoin as well as move execution from basic class to implementation

--- src/lib/operators/sort_merge_join.cpp
+++ src/lib/operators/sort_merge_join.cpp
@@ -2,50 +2,82 @@
 
 #include <memory>
 #include <string>
+#include <utility>
 
 namespace opossum {
 // TODO(Fabian): Comment everything!!
 
-SortMergeJoin::SortMergeJoin(std::shared_ptr<AbstractOperator> left, std::shared_ptr<AbstractOperator> right,
-                             std::string left_column_name, std::string right_column_name, std::string op, JoinMode mode)
+SortMergeJoin::SortMergeJoin(const std::shared_ptr<AbstractOperator> left,
+                             const std::shared_ptr<AbstractOperator> right,
+                             optional<std::pair<const std::string&, const std::string&>> column_names,
+                             const std::string& op, const JoinMode mode)
+    : AbstractOperator(left, right), _op{op}, _mode{mode} {
+  // Check optional column names
+  // Per definition either two names are specified or none
+  if (column_names) {
+    _left_column_name = column_names->first;
+    _right_column_name = column_names->second;
 
-    : AbstractOperator(left, right),
-      _left_column_name{left_column_name},
-      _right_column_name{right_column_name},
-      _op{op},
-      _mode{mode} {
-  if (left == nullptr) {
-    std::string message = "SortMergeJoin::SortMergeJoin: left input operator is null";
-    std::cout << message << std::endl;
-    throw std::exception(std::runtime_error(message));
+    if (left == nullptr) {
+      std::string message = "SortMergeJoin::SortMergeJoin: left input operator is null";
+      std::cout << message << std::endl;
+      throw std::exception(std::runtime_error(message));
+    }
+
+    if (right == nullptr) {
+      std::string message = "SortMergeJoin::SortMergeJoin: right input operator is null";
+      std::cout << message << std::endl;
+      throw std::exception(std::runtime_error(message));
+    }
+    // Check column_type
+    auto left_column_id = _input_left->column_id_by_name(_left_column_name);
+    auto right_column_id = _input_right->column_id_by_name(_right_column_name);
+    auto left_column_type = _input_left->column_type(left_column_id);
+    auto right_column_type = _input_right->column_type(right_column_id);
+
+    if (left_column_type != right_column_type) {
+      std::string message = "SortMergeJoin::execute: column type \"" + left_column_type + "\" of left column \"" +
+                            _left_column_name + "\" does not match colum type \"" + right_column_type +
+                            "\" of right column \"" + _right_column_name + "\"!";
+      std::cout << message << std::endl;
+      throw std::exception(std::runtime_error(message));
+    }
+    // Create implementation to compute join result
+    if (_mode != JoinMode::Cross) {
+      _impl = make_unique_by_column_type<AbstractOperatorImpl, SortMergeJoinImpl>(left_column_type, *this);
+    } else {
+      _product = std::make_shared<Product>(left, right, "left", "right");
+    }
+  } else {
+    // No names specified --> this is only valid if we want to cross-join
+    if (_mode != JoinMode::Cross) {
+      std::string message = "NestedLoopJoin::NestedLoopJoin: No columns specified for join operator";
+      std::cout << message << std::endl;
+      throw std::exception(std::runtime_error(message));
+    } else {
+      _product = std::make_shared<Product>(left, right, "left", "right");
+    }
   }
 
-  if (right == nullptr) {
-    std::string message = "SortMergeJoin::SortMergeJoin: right input operator is null";
-    std::cout << message << std::endl;
-    throw std::exception(std::runtime_error(message));
-  }
-
-  _pos_list_left = std::make_shared<PosList>();
-  _pos_list_right = std::make_shared<PosList>();
+  /*_pos_list_left = std::make_shared<PosList>();
+  _pos_list_right = std::make_shared<PosList>();*/
 }
 
 void SortMergeJoin::execute() {
-  auto left_column_id = _input_left->column_id_by_name(_left_column_name);
-  auto right_column_id = _input_right->column_id_by_name(_right_column_name);
-  auto left_column_type = _input_left->column_type(left_column_id);
-  auto right_column_type = _input_right->column_type(right_column_id);
-
-  if (left_column_type != right_column_type) {
-    std::string message = "SortMergeJoin::execute: column type \"" + left_column_type + "\" of left column \"" +
-                          _left_column_name + "\" does not match colum type \"" + right_column_type +
-                          "\" of right column \"" + _right_column_name + "\"!";
-    std::cout << message << std::endl;
-    throw std::exception(std::runtime_error(message));
+  if (_mode != JoinMode::Cross) {
+    _impl->execute();
+  } else {
+    _product->execute();
   }
 }
 
-std::shared_ptr<const Table> SortMergeJoin::get_output() const { return _output; }
+std::shared_ptr<const Table> SortMergeJoin::get_output() const {
+  if (_mode != JoinMode::Cross) {
+    return _impl->get_output();
+  } else {
+    return _product->get_output();
+  }
+}
 
 const std::string SortMergeJoin::name() const { return "SortMergeJoin"; }
 
@@ -53,41 +85,73 @@ uint8_t SortMergeJoin::num_in_tables() const { return 2u; }
 
 uint8_t SortMergeJoin::num_out_tables() const { return 1u; }
 
+/**
+** Start of implementation
+**/
+
 template <typename T>
 SortMergeJoin::SortMergeJoinImpl<T>::SortMergeJoinImpl(SortMergeJoin& nested_loop_join)
     : _nested_loop_join{nested_loop_join} {
-  /*
-// TODO(student) : ignore op for cross join?
-if (_nested_loop_join._op == "=") {
-_compare = [](const T& value_left, const T& value_right) -> bool { return value_left == value_right; };
-} else if (_nested_loop_join._op == "<") {
-_compare = [](const T& value_left, const T& value_right) -> bool { return value_left < value_right; };
-} else if (_nested_loop_join._op == ">") {
-_compare = [](const T& value_left, const T& value_right) -> bool { return value_left < value_right; };
-} else if (_nested_loop_join._op == ">=") {
-_compare = [](const T& value_left, const T& value_right) -> bool { return value_left >= value_right; };
-} else if (_nested_loop_join._op == "<=") {
-_compare = [](const T& value_left, const T& value_right) -> bool { return value_left <= value_right; };
-} else if (_nested_loop_join._op == "!=") {
-_compare = [](const T& value_left, const T& value_right) -> bool { return value_left != value_right; };
-} else {
-std::string message = "SortMergeJoinImpl::SortMergeJoinImpl: Unknown operator " + _nested_loop_join._op;
-std::cout << message << std::endl;
-throw std::exception(std::runtime_error(message));
-}
+  if (_nested_loop_join._op == "=") {
+    _compare = [](const T& value_left, const T& value_right) -> bool { return value_left == value_right; };
+  } else {
+    std::string message = "SortMergeJoinImpl::SortMergeJoinImpl: Unknown operator " + _nested_loop_join._op;
+    std::cout << message << std::endl;
+    throw std::exception(std::runtime_error(message));
+  }
+  /* right now only equi-joins supported
+  if (_nested_loop_join._op == "=") {
+  _compare = [](const T& value_left, const T& value_right) -> bool { return value_left == value_right; };
+  } else if (_nested_loop_join._op == "<") {
+  _compare = [](const T& value_left, const T& value_right) -> bool { return value_left < value_right; };
+  } else if (_nested_loop_join._op == ">") {
+  _compare = [](const T& value_left, const T& value_right) -> bool { return value_left < value_right; };
+  } else if (_nested_loop_join._op == ">=") {
+  _compare = [](const T& value_left, const T& value_right) -> bool { return value_left >= value_right; };
+  } else if (_nested_loop_join._op == "<=") {
+  _compare = [](const T& value_left, const T& value_right) -> bool { return value_left <= value_right; };
+  } else if (_nested_loop_join._op == "!=") {
+  _compare = [](const T& value_left, const T& value_right) -> bool { return value_left != value_right; };
+  } else {
+  std::string message = "SortMergeJoinImpl::SortMergeJoinImpl: Unknown operator " + _nested_loop_join._op;
+  std::cout << message << std::endl;
+  throw std::exception(std::runtime_error(message));
+  }*/
 }
 
 template <typename T>
-void SortMergeJoin::SortMergeJoinImpl<T>::execute() {}
+typename SortMergeJoin::SortMergeJoinImpl<T>::SortedTable SortMergeJoin::SortMergeJoinImpl<T>::sort_left_table() {
+  auto sorted_left_table = SortMergeJoin::SortMergeJoinImpl<T>::SortedTable();
+  return sorted_left_table;
+}
 
+template <typename T>
+typename SortMergeJoin::SortMergeJoinImpl<T>::SortedTable SortMergeJoin::SortMergeJoinImpl<T>::sort_right_table() {
+  auto sorted_right_table = SortMergeJoin::SortMergeJoinImpl<T>::SortedTable();
+  return sorted_right_table;
+}
+
+template <typename T>
+void perform_join(typename SortMergeJoin::SortMergeJoinImpl<T>::SortedTable& left,
+                  typename SortMergeJoin::SortMergeJoinImpl<T>::SortedTable& right) {
+  // do join
+}
+
+template <typename T>
+void SortMergeJoin::SortMergeJoinImpl<T>::execute() {
+  auto sorted_left_table = sort_left_table();
+  auto sorted_right_table = sort_right_table();
+  perform_join(sorted_left_table, sorted_right_table);
+}
+
+/*
 template <typename T>
 std::shared_ptr<Table> SortMergeJoin::SortMergeJoinImpl<T>::get_output() const {
-std::string message = "SortMergeJoinImpl::get_output() not implemented";
-std::cout << message << std::endl;
-throw std::exception(std::runtime_error(message));
-return nullptr;
-*/
-}
+  std::string message = "SortMergeJoinImpl::get_output() not implemented";
+  std::cout << message << std::endl;
+  throw std::exception(std::runtime_error(message));
+  return nullptr;
+}*/
 
 template <typename T>
 void SortMergeJoin::SortMergeJoinImpl<T>::join_value_value(ValueColumn<T>& left, ValueColumn<T>& right,
--- src/lib/operators/sort_merge_join.hpp
+++ src/lib/operators/sort_merge_join.hpp
@@ -1,9 +1,13 @@
 #pragma once
 
+#include <map>
 #include <memory>
 #include <string>
+#include <utility>
+#include <vector>
 
 #include "abstract_operator.hpp"
+#include "product.hpp"
 #include "storage/column_visitable.hpp"
 #include "storage/dictionary_column.hpp"
 #include "storage/reference_column.hpp"
@@ -14,8 +18,9 @@ namespace opossum {
 
 class SortMergeJoin : public AbstractOperator {
  public:
-  SortMergeJoin(std::shared_ptr<AbstractOperator> left, std::shared_ptr<AbstractOperator> right,
-                std::string left_coumn_name, std::string right_column_name, std::string op, JoinMode mode);
+  SortMergeJoin(const std::shared_ptr<AbstractOperator> left, const std::shared_ptr<AbstractOperator> right,
+                optional<std::pair<const std::string&, const std::string&>> column_names, const std::string& op,
+                const JoinMode mode);
 
   void execute() override;
   std::shared_ptr<const Table> get_output() const override;
@@ -24,6 +29,10 @@ class SortMergeJoin : public AbstractOperator {
   uint8_t num_out_tables() const override;
 
  private:
+  std::shared_ptr<Table> sort_left_table(ColumnVisitable& impl);
+  std::shared_ptr<Table> sort_right_table(ColumnVisitable& impl);
+  void sort_merge_join(std::shared_ptr<Table> table_left, std::shared_ptr<Table> table_right);
+
   struct JoinContext : ColumnVisitableContext {
     JoinContext(std::shared_ptr<BaseColumn> column_left, std::shared_ptr<BaseColumn> column_right,
                 ChunkID left_chunk_id, ChunkID right_chunk_id, JoinMode mode)
@@ -49,6 +58,39 @@ class SortMergeJoin : public AbstractOperator {
     void execute() override;
     std::shared_ptr<Table> get_output() const override;
 
+    // struct used for materialized sorted Chunk
+    class SortedChunk {
+     public:
+      SortedChunk() {}
+      std::vector<T> _values;
+      std::shared_ptr<PosList> _original_positions;
+      std::map<T, int> _chunk_index;
+      std::map<T, int> _histogram;
+      std::map<T, int> _prefix;
+    };
+
+    // struct used for materialized sorted Table
+    class SortedTable {
+     public:
+      SortedTable() {}
+      std::vector<SortedChunk> _chunks;
+      std::map<T, int> _histogram;
+      std::map<T, int> _prefix;
+    };
+
+    // Context for sorting tables
+    struct SortContext : ColumnVisitableContext {
+      SortContext() {}
+
+      std::map<T, int> _histogram;
+      std::map<T, int> _prefix;
+    };
+
+    // Sort functions
+    SortedTable sort_left_table();
+    SortedTable sort_right_table();
+    void perform_join(SortedTable& left, SortedTable& right);
+
     // ColumnVisitable implementation
     virtual void handle_value_column(BaseColumn& column, std::shared_ptr<ColumnVisitableContext> context);
     virtual void handle_dictionary_column(BaseColumn& column, std::shared_ptr<ColumnVisitableContext> context);
@@ -70,8 +112,12 @@ class SortMergeJoin : public AbstractOperator {
    private:
     SortMergeJoin& _nested_loop_join;
     std::function<bool(const T&, const T&)> _compare;
+    SortedTable _left_table;
+    SortedTable _right_table;
   };
 
+  std::unique_ptr<AbstractOperatorImpl> _impl;
+  std::shared_ptr<Product> _product;
   std::string _left_column_name;
   std::string _right_column_name;
   std::string _op;
