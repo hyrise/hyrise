f1bf91a2a8b8cca1d286fa910d02fa97ae74d073
Max Jendruk <code@jendruk.com>
2017-03-12 20:36:34 +0100

    Move TypeColumnProcessor template from insert.hpp to insert.cpp (only used internally)

--- src/lib/operators/insert.cpp
+++ src/lib/operators/insert.cpp
@@ -1,7 +1,6 @@
 #include "insert.hpp"
 
 #include <algorithm>
-#include <limits>
 #include <memory>
 #include <string>
 #include <vector>
@@ -10,6 +9,45 @@
 #include "storage/storage_manager.hpp"
 
 namespace opossum {
+
+// We need these classes to perform the dynamic cast into a templated ValueColumn
+class AbstractTypedColumnProcessor {
+ public:
+  virtual void resize_vector(std::shared_ptr<BaseColumn> column, size_t new_size) = 0;
+  virtual void copy_data(std::shared_ptr<BaseColumn> source, size_t source_start_index,
+                         std::shared_ptr<BaseColumn> target, size_t target_start_index, size_t length) = 0;
+};
+
+template <typename T>
+class TypedColumnProcessor : public AbstractTypedColumnProcessor {
+ public:
+  void resize_vector(std::shared_ptr<BaseColumn> column, size_t new_size) override {
+    auto casted_col = std::dynamic_pointer_cast<ValueColumn<T>>(column);
+    auto& vect = casted_col->values();
+
+    vect.resize(new_size);
+  }
+
+  // this copies
+  void copy_data(std::shared_ptr<BaseColumn> source, size_t source_start_index, std::shared_ptr<BaseColumn> target,
+                 size_t target_start_index, size_t length) override {
+    auto casted_target = std::dynamic_pointer_cast<ValueColumn<T>>(target);
+    if (!casted_target) throw std::logic_error("Type mismatch");
+    auto& vect = casted_target->values();
+
+    if (auto casted_source = std::dynamic_pointer_cast<ValueColumn<T>>(source)) {
+      std::copy_n(casted_source->values().begin() + source_start_index, length, vect.begin() + target_start_index);
+      // } else if(auto casted_source = std::dynamic_pointer_cast<ReferenceColumn>(source)){
+      // since we have no guarantee that a referenceColumn references only a single other column,
+      // this would require us to find out the referenced column's type for each single row.
+      // instead, we just use the slow path below.
+    } else {
+      for (auto i = 0u; i < length; i++) {
+        vect[target_start_index + i] = type_cast<T>((*source)[source_start_index + i]);
+      }
+    }
+  }
+};
 
 Insert::Insert(const std::string& table_name, const std::shared_ptr<AbstractOperator>& values_to_insert)
     : AbstractReadWriteOperator(values_to_insert), _table_name(table_name) {}
--- src/lib/operators/insert.hpp
+++ src/lib/operators/insert.hpp
@@ -1,9 +1,7 @@
 #pragma once
 
-#include <algorithm>
 #include <memory>
 #include <string>
-#include <vector>
 
 #include "abstract_read_write_operator.hpp"
 
@@ -39,42 +37,4 @@ class Insert : public AbstractReadWriteOperator {
   std::shared_ptr<Table> _table;
 };
 
-// We need these classes to perform the dynamic cast into a templated ValueColumn
-class AbstractTypedColumnProcessor {
- public:
-  virtual void resize_vector(std::shared_ptr<BaseColumn> column, size_t new_size) = 0;
-  virtual void copy_data(std::shared_ptr<BaseColumn> source, size_t source_start_index,
-                         std::shared_ptr<BaseColumn> target, size_t target_start_index, size_t length) = 0;
-};
-
-template <typename T>
-class TypedColumnProcessor : public AbstractTypedColumnProcessor {
- public:
-  void resize_vector(std::shared_ptr<BaseColumn> column, size_t new_size) override {
-    auto casted_col = std::dynamic_pointer_cast<ValueColumn<T>>(column);
-    auto& vect = casted_col->values();
-
-    vect.resize(new_size);
-  }
-
-  // this copies
-  void copy_data(std::shared_ptr<BaseColumn> source, size_t source_start_index, std::shared_ptr<BaseColumn> target,
-                 size_t target_start_index, size_t length) override {
-    auto casted_target = std::dynamic_pointer_cast<ValueColumn<T>>(target);
-    if (!casted_target) throw std::logic_error("Type mismatch");
-    auto& vect = casted_target->values();
-
-    if (auto casted_source = std::dynamic_pointer_cast<ValueColumn<T>>(source)) {
-      std::copy_n(casted_source->values().begin() + source_start_index, length, vect.begin() + target_start_index);
-      // } else if(auto casted_source = std::dynamic_pointer_cast<ReferenceColumn>(source)){
-      // since we have no guarantee that a referenceColumn references only a single other column,
-      // this would require us to find out the referenced column's type for each single row.
-      // instead, we just use the slow path below.
-    } else {
-      for (auto i = 0u; i < length; i++) {
-        vect[target_start_index + i] = type_cast<T>((*source)[source_start_index + i]);
-      }
-    }
-  }
-};
 }  // namespace opossum
