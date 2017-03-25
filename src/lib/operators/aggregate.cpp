cfc51dcfff056e2f6e4345e1b22288ae3db86ddb
Markus Dreseler <markus.dreseler@hpi.de>
2017-03-25 13:43:05 +0100

    Merged !68  Aggregate Operator

---
+++ src/lib/operators/aggregate.cpp
@@ -0,0 +1,223 @@
+#include "aggregate.hpp"
+
+#include <algorithm>
+#include <map>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+namespace opossum {
+
+// disable string columns for aggregate functions
+template <>
+AggregateBuilder<std::string>::AggregateBuilder(const AggregateFunction) {
+  throw std::runtime_error("Cannot use string columns in aggregates");
+}
+
+Aggregate::Aggregate(const std::shared_ptr<AbstractOperator> in,
+                     const std::vector<std::pair<std::string, AggregateFunction>> aggregates,
+                     const std::vector<std::string> groupby_columns)
+    : AbstractReadOnlyOperator(in), _aggregates(aggregates), _groupby_columns(groupby_columns) {}
+
+const std::string Aggregate::name() const { return "Aggregate"; }
+
+uint8_t Aggregate::num_in_tables() const { return 1; }
+
+uint8_t Aggregate::num_out_tables() const { return 1; }
+
+std::shared_ptr<const Table> Aggregate::on_execute() {
+  auto input_table = input_table_left();
+
+  // find group by column IDs
+  std::vector<ColumnID> groupby_column_ids;
+  std::transform(_groupby_columns.begin(), _groupby_columns.end(), std::back_inserter(groupby_column_ids),
+                 [&](std::string name) { return input_table->column_id_by_name(name); });
+
+  // find aggregated column IDs
+  std::vector<ColumnID> aggregate_column_ids;
+  std::transform(
+      _aggregates.begin(), _aggregates.end(), std::back_inserter(aggregate_column_ids),
+      [&](std::pair<std::string, AggregateFunction> pair) { return input_table->column_id_by_name(pair.first); });
+
+  /*
+  PARTITIONING PHASE
+  First we partition the input chunks by the given group key(s).
+  This is done by creating a vector that contains the AggregateKey for each row.
+  It is gradually built by visitors, one for each group column.
+  */
+  auto keys_per_chunk = std::vector<std::shared_ptr<std::vector<AggregateKey>>>(input_table->chunk_count());
+
+  std::vector<std::shared_ptr<AbstractTask>> jobs;
+  jobs.reserve(input_table->chunk_count());
+
+  for (ChunkID chunk_id = 0; chunk_id < input_table->chunk_count(); ++chunk_id) {
+    jobs.emplace_back(std::make_shared<JobTask>([&, chunk_id, groupby_column_ids]() {
+      const Chunk &chunk_in = input_table->get_chunk(chunk_id);
+
+      auto hash_keys = std::make_shared<std::vector<AggregateKey>>(chunk_in.size());
+
+      // Partition by group columns
+      for (auto column_id : groupby_column_ids) {
+        auto base_column = chunk_in.get_column(column_id);
+        auto column_type = input_table->column_type(column_id);
+
+        auto builder = make_shared_by_column_type<ColumnVisitable, PartitionBuilder>(column_type);
+        auto ctx = std::make_shared<AggregateContext>(input_table, chunk_id, column_id, hash_keys);
+        base_column->visit(*builder, ctx);
+      }
+
+      keys_per_chunk[chunk_id] = hash_keys;
+    }));
+    jobs.back()->schedule();
+  }
+
+  CurrentScheduler::wait_for_tasks(jobs);
+
+  /*
+  AGGREGATION PHASE
+  */
+  auto results_per_column = std::vector<std::shared_ptr<std::map<AggregateKey, AggregateResult>>>(_aggregates.size());
+
+  // pre-insert empty maps for each aggregate column
+  for (ColumnID column_index = 0; column_index < results_per_column.size(); ++column_index) {
+    results_per_column[column_index] = std::make_shared<std::map<AggregateKey, AggregateResult>>();
+  }
+
+  /*
+  define the functions that are later used to combine the aggregation results
+  from the different chunks.
+  */
+  std::vector<std::function<double(double, double)>> combine_functions;
+  for (auto &kv : _aggregates) {
+    switch (kv.second) {
+      case Min:
+        combine_functions.emplace_back([](double new_value, double current_aggregate) {
+          if (std::isnan(current_aggregate)) {
+            return new_value;
+          }
+          return (new_value < current_aggregate) ? new_value : current_aggregate;
+        });
+        break;
+
+      case Max:
+        combine_functions.emplace_back([](double new_value, double current_aggregate) {
+          if (std::isnan(current_aggregate)) {
+            return new_value;
+          }
+          return (new_value > current_aggregate) ? new_value : current_aggregate;
+        });
+        break;
+
+      case Sum:
+      case Avg:
+        combine_functions.emplace_back([](double new_value, double current_aggregate) {
+          if (std::isnan(current_aggregate)) {
+            return new_value;
+          }
+          return current_aggregate + new_value;
+        });
+        break;
+
+      default:
+        throw std::runtime_error("Aggregate: invalid aggregate function");
+    }
+  }
+
+  for (ChunkID chunk_id = 0; chunk_id < input_table->chunk_count(); ++chunk_id) {
+    const Chunk &chunk_in = input_table->get_chunk(chunk_id);
+
+    auto hash_keys = keys_per_chunk[chunk_id];
+
+    ColumnID column_index = 0;
+    for (auto column_id : aggregate_column_ids) {
+      auto base_column = chunk_in.get_column(column_id);
+      auto column_type = input_table->column_type(column_id);
+
+      auto results = std::make_shared<std::map<AggregateKey, AggregateResult>>();
+
+      /*
+      Invoke the AggregateBuilder for each aggregate column
+      */
+      auto builder =
+          make_shared_by_column_type<ColumnVisitable, AggregateBuilder>(column_type, _aggregates[column_index].second);
+      auto ctx = std::make_shared<AggregateContext>(input_table, chunk_id, column_id, hash_keys, results);
+      base_column->visit(*builder, ctx);
+
+      auto &column_results = static_cast<std::map<AggregateKey, AggregateResult> &>(*results_per_column[column_index]);
+
+      /*
+      Combine the results from this chunk with previous results for this column
+      */
+      for (auto &kv : *results) {
+        auto hash = kv.first;
+        auto result_value = kv.second.first;
+        auto result_counter = kv.second.second;
+
+        column_results[hash].first = combine_functions[column_index](result_value, column_results[hash].first);
+        column_results[hash].second += result_counter;
+      }
+
+      column_index++;
+    }
+  }
+
+  // Write the output
+  auto output = std::make_shared<Table>();
+  Chunk out_chunk;
+  std::vector<std::shared_ptr<BaseColumn>> group_columns;
+
+  if (_groupby_columns.size()) {
+    // add group by columns
+    for (ColumnID column_index = 0; column_index < _groupby_columns.size(); ++column_index) {
+      output->add_column(_groupby_columns[column_index], input_table->column_type(groupby_column_ids[column_index]),
+                         false);
+
+      group_columns.emplace_back(make_shared_by_column_type<BaseColumn, ValueColumn>(
+          input_table->column_type(groupby_column_ids[column_index])));
+
+      out_chunk.add_column(group_columns.back());
+    }
+  }
+
+  /*
+  Write the aggregated columns to the output
+  */
+  ColumnID column_index = 0;
+  for (auto aggregate : _aggregates) {
+    auto &column_name = aggregate.first;
+    auto &func = aggregate.second;
+
+    // generate the name, e.g. MAX(column_a)
+    std::vector<std::string> names{"MIN", "MAX", "SUM", "AVG"};
+    output->add_column(names[func] + "(" + column_name + ")", "double", false);
+
+    auto col = std::make_shared<ValueColumn<double>>();
+    auto &values = col->values();
+
+    for (auto &kv : *results_per_column[column_index]) {
+      if (column_index == 0) {
+        // in first iteration, also add the group key values
+        for (size_t group_column_index = 0; group_column_index < kv.first.size(); ++group_column_index) {
+          group_columns[group_column_index]->append(kv.first[group_column_index]);
+        }
+      }
+
+      if (func == Avg) {
+        // finally calculate the average from the sum
+        values.push_back(kv.second.first / kv.second.second);
+      } else {
+        values.push_back(kv.second.first);
+      }
+    }
+    out_chunk.add_column(col);
+
+    column_index++;
+  }
+
+  output->add_chunk(std::move(out_chunk));
+
+  return output;
+}
+
+}  // namespace opossum
---
+++ src/test/operators/aggregate_test.cpp
@@ -0,0 +1,271 @@
+#include <iostream>
+#include <map>
+#include <memory>
+#include <set>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "../base_test.hpp"
+#include "gtest/gtest.h"
+
+#include "../../lib/operators/abstract_read_only_operator.hpp"
+#include "../../lib/operators/aggregate.hpp"
+#include "../../lib/operators/get_table.hpp"
+#include "../../lib/operators/table_scan.hpp"
+#include "../../lib/storage/storage_manager.hpp"
+#include "../../lib/storage/table.hpp"
+#include "../../lib/types.hpp"
+
+namespace opossum {
+
+class OperatorsAggregateTest : public BaseTest {
+ protected:
+  void SetUp() override {
+    auto test_table = load_table("src/test/tables/aggregateoperator/groupby_int_1gb_1agg/input.tbl", 2);
+    StorageManager::get().add_table("table_a", std::move(test_table));
+    _gt_1_1 = std::make_shared<GetTable>("table_a");
+    _gt_1_1->execute();
+
+    test_table = load_table("src/test/tables/aggregateoperator/groupby_int_1gb_2agg/input.tbl", 2);
+    StorageManager::get().add_table("table_b", std::move(test_table));
+    _gt_1_2 = std::make_shared<GetTable>("table_b");
+    _gt_1_2->execute();
+
+    test_table = load_table("src/test/tables/aggregateoperator/groupby_int_2gb_1agg/input.tbl", 2);
+    StorageManager::get().add_table("table_c", std::move(test_table));
+    _gt_2_1 = std::make_shared<GetTable>("table_c");
+    _gt_2_1->execute();
+
+    test_table = load_table("src/test/tables/aggregateoperator/groupby_int_2gb_2agg/input.tbl", 2);
+    StorageManager::get().add_table("table_d", std::move(test_table));
+    _gt_2_2 = std::make_shared<GetTable>("table_d");
+    _gt_2_2->execute();
+
+    test_table = load_table("src/test/tables/aggregateoperator/groupby_string_1gb_1agg/input.tbl", 2);
+    StorageManager::get().add_table("table_a_string", std::move(test_table));
+    _gt_1_1_string = std::make_shared<GetTable>("table_a_string");
+    _gt_1_1_string->execute();
+
+    test_table = load_table("src/test/tables/aggregateoperator/groupby_int_1gb_1agg/input.tbl", 2);
+    test_table->compress_chunk(0);
+    test_table->compress_chunk(1);
+    StorageManager::get().add_table("table_a_dict", std::move(test_table));
+    _gt_1_1_dict = std::make_shared<GetTable>("table_a_dict");
+    _gt_1_1_dict->execute();
+  }
+
+  void test_output(const std::shared_ptr<AbstractOperator> in,
+                   const std::vector<std::pair<std::string, AggregateFunction>> aggregates,
+                   const std::vector<std::string> groupby_columns, const std::string &file_name, size_t chunk_size) {
+    // load expected results from file
+    std::shared_ptr<Table> expected_result = load_table(file_name, chunk_size);
+    EXPECT_NE(expected_result, nullptr) << "Could not load expected result table";
+
+    // collect possible columns to scan before aggregate
+    std::set<std::string> ref_columns;
+    ref_columns.insert("");
+
+    for (auto const &agg : aggregates) {
+      ref_columns.insert(agg.first);
+    }
+
+    for (auto const &groupby : groupby_columns) {
+      ref_columns.insert(groupby);
+    }
+
+    for (auto &ref : ref_columns) {
+      // make one Aggregate w/o ReferenceColumn
+      auto input = in;
+
+      if (ref != "") {
+        // also try a TableScan on every involved column
+        input = std::make_shared<TableScan>(in, ref, ">=", 0);
+        input->execute();
+      }
+
+      // build and execute Aggregate
+      auto aggregate = std::make_shared<Aggregate>(input, aggregates, groupby_columns);
+      EXPECT_NE(aggregate, nullptr) << "Could not build Aggregate";
+      aggregate->execute();
+      EXPECT_TABLE_EQ(aggregate->get_output(), expected_result);
+    }
+  }
+
+  std::shared_ptr<GetTable> _gt_1_1, _gt_1_2, _gt_2_1, _gt_2_2, _gt_1_1_string, _gt_1_1_dict;
+};
+
+TEST_F(OperatorsAggregateTest, NumInputTables) {
+  auto aggregate = std::make_shared<Aggregate>(
+      _gt_1_1, std::vector<std::pair<std::string, AggregateFunction>>{std::make_pair(std::string("b"), Max)},
+      std::vector<std::string>{std::string("a")});
+  aggregate->execute();
+
+  EXPECT_EQ(aggregate->num_in_tables(), 1);
+}
+
+TEST_F(OperatorsAggregateTest, NumOutputTables) {
+  auto aggregate = std::make_shared<Aggregate>(
+      _gt_1_1, std::vector<std::pair<std::string, AggregateFunction>>{std::make_pair(std::string("b"), Max)},
+      std::vector<std::string>{std::string("a")});
+
+  EXPECT_EQ(aggregate->num_out_tables(), 1);
+}
+
+TEST_F(OperatorsAggregateTest, OperatorName) {
+  auto aggregate = std::make_shared<Aggregate>(
+      _gt_1_1, std::vector<std::pair<std::string, AggregateFunction>>{std::make_pair(std::string("b"), Max)},
+      std::vector<std::string>{std::string("a")});
+
+  EXPECT_EQ(aggregate->name(), "Aggregate");
+}
+
+TEST_F(OperatorsAggregateTest, CannotAggregateStringColumns) {
+  auto aggregate = std::make_shared<Aggregate>(
+      _gt_1_1_string, std::vector<std::pair<std::string, AggregateFunction>>{std::make_pair(std::string("a"), Min)},
+      std::vector<std::string>{std::string("a")});
+
+  EXPECT_THROW(aggregate->execute(), std::runtime_error);
+}
+
+TEST_F(OperatorsAggregateTest, SingleAggregateMax) {
+  this->test_output(_gt_1_1, {std::make_pair(std::string("b"), Max)}, {std::string("a")},
+                    "src/test/tables/aggregateoperator/groupby_int_1gb_1agg/max.tbl", 1);
+}
+
+TEST_F(OperatorsAggregateTest, SingleAggregateMin) {
+  this->test_output(_gt_1_1, {std::make_pair(std::string("b"), Min)}, {std::string("a")},
+                    "src/test/tables/aggregateoperator/groupby_int_1gb_1agg/min.tbl", 1);
+}
+
+TEST_F(OperatorsAggregateTest, SingleAggregateSum) {
+  this->test_output(_gt_1_1, {std::make_pair(std::string("b"), Sum)}, {std::string("a")},
+                    "src/test/tables/aggregateoperator/groupby_int_1gb_1agg/sum.tbl", 1);
+}
+
+TEST_F(OperatorsAggregateTest, SingleAggregateAvg) {
+  this->test_output(_gt_1_1, {std::make_pair(std::string("b"), Avg)}, {std::string("a")},
+                    "src/test/tables/aggregateoperator/groupby_int_1gb_1agg/avg.tbl", 1);
+}
+
+TEST_F(OperatorsAggregateTest, StringSingleAggregateMax) {
+  this->test_output(_gt_1_1_string, {std::make_pair(std::string("b"), Max)}, {std::string("a")},
+                    "src/test/tables/aggregateoperator/groupby_string_1gb_1agg/max.tbl", 1);
+}
+
+TEST_F(OperatorsAggregateTest, StringSingleAggregateMin) {
+  this->test_output(_gt_1_1_string, {std::make_pair(std::string("b"), Min)}, {std::string("a")},
+                    "src/test/tables/aggregateoperator/groupby_string_1gb_1agg/min.tbl", 1);
+}
+
+TEST_F(OperatorsAggregateTest, StringSingleAggregateSum) {
+  this->test_output(_gt_1_1_string, {std::make_pair(std::string("b"), Sum)}, {std::string("a")},
+                    "src/test/tables/aggregateoperator/groupby_string_1gb_1agg/sum.tbl", 1);
+}
+
+TEST_F(OperatorsAggregateTest, StringSingleAggregateAvg) {
+  this->test_output(_gt_1_1_string, {std::make_pair(std::string("b"), Avg)}, {std::string("a")},
+                    "src/test/tables/aggregateoperator/groupby_string_1gb_1agg/avg.tbl", 1);
+}
+
+TEST_F(OperatorsAggregateTest, DictionarySingleAggregateMax) {
+  this->test_output(_gt_1_1_dict, {std::make_pair(std::string("b"), Max)}, {std::string("a")},
+                    "src/test/tables/aggregateoperator/groupby_int_1gb_1agg/max.tbl", 1);
+}
+
+TEST_F(OperatorsAggregateTest, DictionarySingleAggregateMin) {
+  this->test_output(_gt_1_1_dict, {std::make_pair(std::string("b"), Min)}, {std::string("a")},
+                    "src/test/tables/aggregateoperator/groupby_int_1gb_1agg/min.tbl", 1);
+}
+
+TEST_F(OperatorsAggregateTest, DictionarySingleAggregateSum) {
+  this->test_output(_gt_1_1_dict, {std::make_pair(std::string("b"), Sum)}, {std::string("a")},
+                    "src/test/tables/aggregateoperator/groupby_int_1gb_1agg/sum.tbl", 1);
+}
+
+TEST_F(OperatorsAggregateTest, DictionarySingleAggregateAvg) {
+  this->test_output(_gt_1_1_dict, {std::make_pair(std::string("b"), Avg)}, {std::string("a")},
+                    "src/test/tables/aggregateoperator/groupby_int_1gb_1agg/avg.tbl", 1);
+}
+
+TEST_F(OperatorsAggregateTest, TwoAggregateAvgMax) {
+  this->test_output(_gt_1_2, {std::make_pair(std::string("b"), Max), std::make_pair(std::string("c"), Avg)},
+                    {std::string("a")}, "src/test/tables/aggregateoperator/groupby_int_1gb_2agg/max_avg.tbl", 1);
+}
+
+TEST_F(OperatorsAggregateTest, TwoAggregateMinAvg) {
+  this->test_output(_gt_1_2, {std::make_pair(std::string("b"), Min), std::make_pair(std::string("c"), Avg)},
+                    {std::string("a")}, "src/test/tables/aggregateoperator/groupby_int_1gb_2agg/min_avg.tbl", 1);
+}
+
+TEST_F(OperatorsAggregateTest, TwoAggregateMinMax) {
+  this->test_output(_gt_1_2, {std::make_pair(std::string("b"), Min), std::make_pair(std::string("c"), Max)},
+                    {std::string("a")}, "src/test/tables/aggregateoperator/groupby_int_1gb_2agg/min_max.tbl", 1);
+}
+
+TEST_F(OperatorsAggregateTest, TwoAggregateAvgAvg) {
+  this->test_output(_gt_1_2, {std::make_pair(std::string("b"), Avg), std::make_pair(std::string("c"), Avg)},
+                    {std::string("a")}, "src/test/tables/aggregateoperator/groupby_int_1gb_2agg/avg_avg.tbl", 1);
+}
+
+TEST_F(OperatorsAggregateTest, TwoAggregateSumAvg) {
+  this->test_output(_gt_1_2, {std::make_pair(std::string("b"), Sum), std::make_pair(std::string("c"), Avg)},
+                    {std::string("a")}, "src/test/tables/aggregateoperator/groupby_int_1gb_2agg/sum_avg.tbl", 1);
+}
+
+TEST_F(OperatorsAggregateTest, TwoAggregateSumSum) {
+  this->test_output(_gt_1_2, {std::make_pair(std::string("b"), Sum), std::make_pair(std::string("c"), Sum)},
+                    {std::string("a")}, "src/test/tables/aggregateoperator/groupby_int_1gb_2agg/sum_sum.tbl", 1);
+}
+
+TEST_F(OperatorsAggregateTest, TwoGroupbyMax) {
+  this->test_output(_gt_2_1, {std::make_pair(std::string("c"), Max)}, {std::string("a"), std::string("b")},
+                    "src/test/tables/aggregateoperator/groupby_int_2gb_1agg/max.tbl", 1);
+}
+
+TEST_F(OperatorsAggregateTest, TwoGroupbyMin) {
+  this->test_output(_gt_2_1, {std::make_pair(std::string("c"), Min)}, {std::string("a"), std::string("b")},
+                    "src/test/tables/aggregateoperator/groupby_int_2gb_1agg/min.tbl", 1);
+}
+
+TEST_F(OperatorsAggregateTest, TwoGroupbySum) {
+  this->test_output(_gt_2_1, {std::make_pair(std::string("c"), Sum)}, {std::string("a"), std::string("b")},
+                    "src/test/tables/aggregateoperator/groupby_int_2gb_1agg/sum.tbl", 1);
+}
+
+TEST_F(OperatorsAggregateTest, TwoGroupbyAvg) {
+  this->test_output(_gt_2_1, {std::make_pair(std::string("c"), Avg)}, {std::string("a"), std::string("b")},
+                    "src/test/tables/aggregateoperator/groupby_int_2gb_1agg/avg.tbl", 1);
+}
+
+TEST_F(OperatorsAggregateTest, TwoGroupbyAndTwoAggregateMaxAvg) {
+  this->test_output(_gt_2_2, {std::make_pair(std::string("c"), Max), std::make_pair(std::string("d"), Avg)},
+                    {std::string("a"), std::string("b")},
+                    "src/test/tables/aggregateoperator/groupby_int_2gb_2agg/max_avg.tbl", 1);
+}
+
+TEST_F(OperatorsAggregateTest, TwoGroupbyAndTwoAggregateMinAvg) {
+  this->test_output(_gt_2_2, {std::make_pair(std::string("c"), Min), std::make_pair(std::string("d"), Avg)},
+                    {std::string("a"), std::string("b")},
+                    "src/test/tables/aggregateoperator/groupby_int_2gb_2agg/min_avg.tbl", 1);
+}
+
+TEST_F(OperatorsAggregateTest, TwoGroupbyAndTwoAggregateMinMax) {
+  this->test_output(_gt_2_2, {std::make_pair(std::string("c"), Min), std::make_pair(std::string("d"), Max)},
+                    {std::string("a"), std::string("b")},
+                    "src/test/tables/aggregateoperator/groupby_int_2gb_2agg/min_max.tbl", 1);
+}
+
+TEST_F(OperatorsAggregateTest, TwoGroupbyAndTwoAggregateSumAvg) {
+  this->test_output(_gt_2_2, {std::make_pair(std::string("c"), Sum), std::make_pair(std::string("d"), Avg)},
+                    {std::string("a"), std::string("b")},
+                    "src/test/tables/aggregateoperator/groupby_int_2gb_2agg/sum_avg.tbl", 1);
+}
+
+TEST_F(OperatorsAggregateTest, TwoGroupbyAndTwoAggregateSumSum) {
+  this->test_output(_gt_2_2, {std::make_pair(std::string("c"), Sum), std::make_pair(std::string("d"), Sum)},
+                    {std::string("a"), std::string("b")},
+                    "src/test/tables/aggregateoperator/groupby_int_2gb_2agg/sum_sum.tbl", 1);
+}
+
+}  // namespace opossum
