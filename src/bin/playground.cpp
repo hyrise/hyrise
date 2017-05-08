5a6b68515ae8d9a524cec2aa657f3f51bff5a320
Moritz Eyssen <Moritz.Eyssen@student.hpi.de>
2017-05-08 17:49:57 +0200

    started on tpcc table->csv

--- src/bin/playground.cpp
+++ src/bin/playground.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
 #include <memory>
 
-#include "storage/column_visitable.hpp"
-#include "storage/dictionary_column.hpp"
-#include "storage/value_column.hpp"
+#include "operators/export_csv.hpp"
+#include "operators/table_wrapper.hpp"
+#include "../benchmark/tpcc/tpcc_table_generator.hpp"
 
-int main() {}
+using namespace opossum;
+
+void save_as_csv(const std::shared_ptr<Table> & table, const std::string & path)
+{
+  auto tableWrapper = std::make_shared<TableWrapper>(table);
+  tableWrapper->execute();
+
+  auto exportCsv = std::make_shared<ExportCsv>(tableWrapper, ".", path);
+  exportCsv->execute();
+}
+
+int main() {
+  TPCCTableGenerator generator;
+
+  save_as_csv(generator.generate_items_table(), "tpcc-items.csv");
+
+
+
+}
