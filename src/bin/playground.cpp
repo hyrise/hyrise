782eb4aa5534be8f2855181a9ead319d16422475
Nils Thamm <nils.thamm@student.hpi.de>
2017-05-11 22:28:44 +0200

    Proposal for new assert structure

--- src/bin/playground.cpp
+++ src/bin/playground.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
 #include <memory>
+#include <string>
 
-#include "storage/column_visitable.hpp"
-#include "storage/dictionary_column.hpp"
-#include "storage/value_column.hpp"
+template <typename Lambda>
+inline void Assert(bool expr, Lambda msg) {
+  if (expr) {
+    return;
+  }
+  throw std::logic_error(msg());
+}
+template <typename Lambda>
+inline void Fail(Lambda msg) {
+  throw std::logic_error(msg());
+}
 
-int main() {}
+int test_function() {
+  std::cout << "Message evaluated\n";
+  return 42;
+}
+
+template <typename Lambda>
+inline void DebugAssert(bool expr, Lambda msg) {
+  if (!IS_DEBUG)
+    ;
+  else
+    Assert(expr, msg);
+}
+
+template <typename Lambda>
+inline void DebugFail(Lambda msg) {
+  if (!IS_DEBUG)
+    ;
+  else
+    Fail(msg);
+}
+int main() {
+  DebugAssert(false, []() { return std::string("Failed with code ") + std::to_string(test_function()); });
+}
