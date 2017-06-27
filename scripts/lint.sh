e3dbf2902ead625aabe401e54e6d2d2db1916459
Timo Djürken <timo.djuerken@gmail.com>
2017-06-27 13:17:57 +0200

    lint verbosity back to 0

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python2.7 cpplint.py --verbose=3 --extensions=hpp,cpp --counting=detailed --filter=-legal/copyright,-whitespace/newline,-runtime/references,-build/c++11 --linelength=120 src/**/*.{hpp,cpp}
+python2.7 cpplint.py --verbose=0 --extensions=hpp,cpp --counting=detailed --filter=-legal/copyright,-whitespace/newline,-runtime/references,-build/c++11 --linelength=120 src/**/*.{hpp,cpp}
