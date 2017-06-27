2743f4b45b65ef1cf429236421bffcad50fb9179
Timo Djürken <TwoLaid@users.noreply.github.com>
2017-06-27 14:06:29 +0200

    Make lint script faster (#94)
    * Make lint script 4X faster
    
    * lint verbosity back to 0

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-find src -iname "*.cpp" -o -iname "*.hpp" | xargs -I{} python2.7 cpplint.py --verbose=0 --extensions=hpp,cpp --counting=detailed --filter=-legal/copyright,-whitespace/newline,-runtime/references,-build/c++11 --linelength=120 {}
+python2.7 cpplint.py --verbose=0 --extensions=hpp,cpp --counting=detailed --filter=-legal/copyright,-whitespace/newline,-runtime/references,-build/c++11 --linelength=120 src/**/*.{hpp,cpp}
