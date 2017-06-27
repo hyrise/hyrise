9afd3ee419bbecc9ad755ce19cae7c89f018f129
Tim Zimmermann <tim.zimmermann@student.hpi.uni-potsdam.de>
2017-06-27 14:12:20 +0200

    Create tasks from result operator (#88)
    * #77: Create task tree from result operator
    
    * Add includes
    
    * Write to vector by refernece; rename to mutable inputs
    
    * Add comment for recursive function
    
    * minor change to comments

--- src/lib/scheduler/operator_task.hpp
+++ src/lib/scheduler/operator_task.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include <vector>
 
 #include "operators/abstract_operator.hpp"
 
@@ -15,10 +16,22 @@ class OperatorTask : public AbstractTask {
  public:
   explicit OperatorTask(std::shared_ptr<AbstractOperator> op);
 
+  /**
+   * Create tasks recursively from result operator and set task dependencies automatically.
+   */
+  static const std::vector<std::shared_ptr<OperatorTask>> make_tasks_from_operator(
+      std::shared_ptr<AbstractOperator> op);
+
   const std::shared_ptr<AbstractOperator>& get_operator() const;
 
  protected:
   void on_execute() override;
+
+  /**
+   * Create tasks recursively. Called by `make_tasks_from_operator`.
+   */
+  static void _add_tasks_from_operator(std::shared_ptr<AbstractOperator> op,
+                                       std::vector<std::shared_ptr<OperatorTask>>& tasks);
 
  private:
   std::shared_ptr<AbstractOperator> _op;
