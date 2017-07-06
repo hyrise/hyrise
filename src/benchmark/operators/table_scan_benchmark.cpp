c49b47ef65a805f647abdc73199256225333a457
Tim Zimmermann <tim.zimmermann@student.hpi.uni-potsdam.de>
2017-07-06 21:03:06 +0200

    Fix format

--- src/benchmark/operators/table_scan_benchmark.cpp
+++ src/benchmark/operators/table_scan_benchmark.cpp
@@ -22,10 +22,12 @@ BENCHMARK_F(BenchmarkBasicFixture, BM_TableScanConstant)(benchmark::State& state
 
 BENCHMARK_F(BenchmarkBasicFixture, BM_TableScanVariable)(benchmark::State& state) {
   clear_cache();
-  auto warm_up = std::make_shared<TableScan>(_table_wrapper_a, ColumnName("a"), ScanType::OpGreaterThanEquals, ColumnName("b"));
+  auto warm_up =
+      std::make_shared<TableScan>(_table_wrapper_a, ColumnName("a"), ScanType::OpGreaterThanEquals, ColumnName("b"));
   warm_up->execute();
   while (state.KeepRunning()) {
-    auto table_scan = std::make_shared<TableScan>(_table_wrapper_a, ColumnName("a"), ScanType::OpGreaterThanEquals, ColumnName("b"));
+    auto table_scan =
+        std::make_shared<TableScan>(_table_wrapper_a, ColumnName("a"), ScanType::OpGreaterThanEquals, ColumnName("b"));
     table_scan->execute();
   }
 }
