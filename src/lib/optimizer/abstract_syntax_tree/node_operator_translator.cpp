1f0e956d5e949b19a6291d3d72792d4fba710545
Tim Zimmermann <tim.zimmermann@student.hpi.uni-potsdam.de>
2017-07-06 13:11:39 +0200

    Add support for order by in node-operator translator

--- src/lib/optimizer/abstract_syntax_tree/node_operator_translator.cpp
+++ src/lib/optimizer/abstract_syntax_tree/node_operator_translator.cpp
@@ -4,7 +4,9 @@
 
 #include "operators/get_table.hpp"
 #include "operators/projection.hpp"
+#include "operators/sort.hpp"
 #include "operators/table_scan.hpp"
+#include "optimizer/abstract_syntax_tree/sort_node.hpp"
 #include "optimizer/abstract_syntax_tree/table_node.hpp"
 #include "optimizer/abstract_syntax_tree/table_scan_node.hpp"
 
@@ -23,6 +25,8 @@ NodeOperatorTranslator::NodeOperatorTranslator() {
       std::bind(&NodeOperatorTranslator::translate_table_scan_node, this, std::placeholders::_1);
   _operator_factory[NodeType::Projection] =
       std::bind(&NodeOperatorTranslator::translate_projection_node, this, std::placeholders::_1);
+  _operator_factory[NodeType::Sort] =
+          std::bind(&NodeOperatorTranslator::translate_order_by_node, this, std::placeholders::_1);
 }
 
 const std::shared_ptr<AbstractOperator> NodeOperatorTranslator::translate_node(
@@ -63,6 +67,14 @@ const std::shared_ptr<AbstractOperator> NodeOperatorTranslator::translate_projec
     std::shared_ptr<AbstractNode> node) const {
   auto input_operator = translate_node(node->left());
   return std::make_shared<Projection>(input_operator, node->output_columns());
+}
+
+const std::shared_ptr<AbstractOperator> NodeOperatorTranslator::translate_order_by_node(
+        std::shared_ptr<AbstractNode> node) const {
+  auto input_operator = translate_node(node->left());
+
+  auto sort_node = std::dynamic_pointer_cast<SortNode>(node);
+  return std::make_shared<Sort>(input_operator, sort_node->column_name(), sort_node->asc());
 }
 
 }  // namespace opossum
