eef4ba184b6ae35e848251d1e8cbc0831704112a
jstriebel <jstriebel@users.noreply.github.com>
2017-07-06 10:56:44 +0000

    statistics: revert map as shared_ptr

--- src/lib/optimizer/table_statistics.cpp
+++ src/lib/optimizer/table_statistics.cpp
@@ -15,8 +15,7 @@ namespace opossum {
 
 TableStatistics::TableStatistics(const std::string &name, const std::weak_ptr<Table> table)
     : _name(name),
-      _table(table),
-      _column_statistics(std::make_shared<std::map<std::string, std::shared_ptr<AbstractColumnStatistics>>>()) {
+      _table(table) {
   _row_count = _table.lock()->row_count();
 }
 
@@ -24,20 +23,20 @@ TableStatistics::TableStatistics(const TableStatistics &table_statistics)
     : _name(table_statistics._name),
       _table(table_statistics._table),
       _row_count(table_statistics._row_count),
-      _column_statistics(table_statistics._column_statistics) {}
+      _column_statistics(std::map<std::string, std::shared_ptr<AbstractColumnStatistics>>(table_statistics._column_statistics)) {}
 
 double TableStatistics::row_count() { return _row_count; }
 
 std::shared_ptr<AbstractColumnStatistics> TableStatistics::get_column_statistics(const std::string &column_name) {
   auto table = _table.lock();
-  auto column_stat = _column_statistics->find(column_name);
-  if (column_stat == _column_statistics->end()) {
+  auto column_stat = _column_statistics.find(column_name);
+  if (column_stat == _column_statistics.end()) {
     auto column_type = table->column_type(table->column_id_by_name(column_name));
     auto column_statistics =
         make_shared_by_column_type<AbstractColumnStatistics, ColumnStatistics>(column_type, _table, column_name);
-    _column_statistics->emplace(column_name, column_statistics);
+    _column_statistics[column_name] = column_statistics;
   }
-  return _column_statistics->at(column_name);
+  return _column_statistics[column_name];
 }
 
 std::shared_ptr<TableStatistics> TableStatistics::predicate_statistics(const std::string &column_name,
@@ -60,7 +59,8 @@ std::shared_ptr<TableStatistics> TableStatistics::predicate_statistics(const std
     return clone;
   }
 
-  auto column_statistics = get_column_statistics(column_name);
+  auto clone = std::make_shared<TableStatistics>(*this);
+  auto column_statistics = clone->get_column_statistics(column_name);
   double selectivity;
   std::shared_ptr<AbstractColumnStatistics> column_statistic;
   if (value.type() == typeid(ColumnName)) {
@@ -72,9 +72,8 @@ std::shared_ptr<TableStatistics> TableStatistics::predicate_statistics(const std
     auto casted_value1 = boost::get<AllTypeVariant>(value);
     std::tie(selectivity, column_statistic) = column_statistics->predicate_selectivity(op, casted_value1, value2);
   }
-  auto clone = std::make_shared<TableStatistics>(*this);
   if (column_statistic) {
-    clone->_column_statistics->emplace(column_name, column_statistic);
+    clone->_column_statistics[column_name] = column_statistic;
   }
   clone->_row_count *= selectivity;
 
