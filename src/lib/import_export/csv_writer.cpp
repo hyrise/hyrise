72542fe2d0000ff2579dfbd04a5f2e433c4c5a20
Leander Neiß <leander.neiss@student.hpi.uni-potsdam.de>
2017-07-06 13:40:32 +0200

    Use CsvConfig everywhere (#112)
    * Use CsvConfig instead of static values everywhere. #83
    
    * Add optional CsvConfig to CsvWriter constructor. #83

--- src/lib/import_export/csv_writer.cpp
+++ src/lib/import_export/csv_writer.cpp
@@ -4,12 +4,12 @@
 #include <string>
 #include <vector>
 
-#include "csv.hpp"
 #include "types.hpp"
 
 namespace opossum {
 
-CsvWriter::CsvWriter(const std::string& file) {
+CsvWriter::CsvWriter(const std::string& file, const CsvConfig& config)
+: _config(config) {
   _stream.exceptions(std::ifstream::failbit | std::ifstream::badbit);
   _stream.open(file);
 }
@@ -20,8 +20,8 @@ CsvWriter::CsvWriter(const std::string& file) {
 std::string CsvWriter::escape(const std::string& string) {
   std::string result(string);
   size_t next_pos = 0;
-  while (std::string::npos != (next_pos = result.find(csv_quote, next_pos))) {
-    result.insert(next_pos, 1, csv_escape);
+  while (std::string::npos != (next_pos = result.find(_config.quote, next_pos))) {
+    result.insert(next_pos, 1, _config.escape);
     // Has to jump 2 positions ahead because a new character had been inserted.
     next_pos += 2;
   }
@@ -36,7 +36,7 @@ void CsvWriter::write_line(const std::vector<AllTypeVariant>& values) {
 }
 
 void CsvWriter::end_line() {
-  _stream << csv_delimiter;
+  _stream << _config.delimiter;
   _current_col_count = 0;
 }
 
--- src/lib/import_export/csv_writer.hpp
+++ src/lib/import_export/csv_writer.hpp
@@ -17,12 +17,12 @@ class CsvWriter {
    * Creates a new CsvWriter with the given file as output file.
    * @param file The file to output the csv to.
    */
-  explicit CsvWriter(const std::string& file);
+  explicit CsvWriter(const std::string& file, const CsvConfig& config = {});
 
   template <typename T>
   void write(const T& value) {
     if (_current_col_count > 0) {
-      _stream << csv_separator;
+      _stream << _config.separator;
     }
     _write_value(value);
     ++this->_current_col_count;
@@ -47,6 +47,7 @@ class CsvWriter {
 
   std::ofstream _stream;
   ColumnID _current_col_count{0};
+  CsvConfig _config;
 };
 
 template <typename T>
@@ -64,9 +65,9 @@ inline void CsvWriter::_write_value<std::string>(const std::string& value) {
    * this behaviour to either general quoting or checking for "illegal"
    * characters.
    */
-  _stream << csv_quote;
+  _stream << _config.quote;
   _stream << escape(value);
-  _stream << csv_quote;
+  _stream << _config.quote;
 }
 
 template <>
