27e851979f89ef54aaa5fb03fc5aff8d8eb78f03
Markus Dreseler <markus.dreseler@hpi.de>
2017-07-07 14:54:09 +0200

    Moar const

--- src/lib/constant_mappings.hpp
+++ src/lib/constant_mappings.hpp
@@ -3,7 +3,7 @@
 
 namespace opossum {
 
-  const std::unordered_map<std::string, proto::ScanType> op_string_to_scan_type = {
+  const std::unordered_map<std::string, const proto::ScanType> op_string_to_scan_type = {
     {"=", opossum::proto::ScanType::OpEquals},
     {"!=", opossum::proto::ScanType::OpNotEquals},
 
