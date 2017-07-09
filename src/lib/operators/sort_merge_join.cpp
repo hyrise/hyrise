90f85943d84858f39403d553d6c967955b9564d6
Arne Mayer <arne.mayer@student.hpi.de>
2017-07-09 03:51:08 +0200

    started moving the implementation class to the cpp file

--- src/lib/operators/sort_merge_join.cpp
+++ src/lib/operators/sort_merge_join.cpp
@@ -72,9 +72,110 @@ uint8_t SortMergeJoin::num_out_tables() const { return 1u; }
 **/
 
 template <typename T>
-SortMergeJoin::SortMergeJoinImpl<T>::SortMergeJoinImpl(SortMergeJoin& sort_merge_join, std::string left_column_name,
-  std::string right_column_name, uint32_t partition_count) : _sort_merge_join{sort_merge_join},
-  _left_column_name{left_column_name}, _right_column_name{right_column_name}, _partition_count{partition_count} {}
+class SortMergeJoin::SortMergeJoinImpl : public AbstractJoinOperatorImpl, public ColumnVisitable {
+ public:
+  SortMergeJoinImpl<T>(SortMergeJoin& sort_merge_join, std::string left_column_name, std::string right_column_name,
+    uint32_t partition_count) :  : _sort_merge_join{sort_merge_join}, _left_column_name{left_column_name},
+    _right_column_name{right_column_name}, _partition_count{partition_count} {}
+
+  virtual ~SortMergeJoinImpl() = default;
+
+ protected:
+  SortMergeJoin& _sort_merge_join;
+  std::shared_ptr<SortedTable> _sorted_left_table;
+  std::shared_ptr<SortedTable> _sorted_right_table;
+
+  std::string _left_column_name;
+  std::string _right_column_name;
+
+  // the partition count should be a power of two, i.e. 1, 2, 4, 8, 16, ...
+  uint32_t _partition_count;
+
+  std::shared_ptr<PosList> _pos_list_left;
+  std::shared_ptr<PosList> _pos_list_right;
+
+  // AbstractJoinOperatorImpl implementation
+  std::shared_ptr<const Table> on_execute() override;
+
+  // struct used for materialized sorted Chunk
+  struct SortedChunk {
+    SortedChunk() {}
+
+    std::vector<std::pair<T, RowID>> values;
+
+    // Used to count the number of entries for each partition from this chunk
+    std::map<uint32_t, uint32_t> partition_histogram;
+    std::map<uint32_t, uint32_t> prefix;
+
+    std::map<T, uint32_t> value_histogram;
+    std::map<T, uint32_t> prefix_v;
+  };
+
+  // struct used for a materialized sorted Table
+  struct SortedTable {
+    SortedTable() {}
+
+    std::vector<SortedChunk> partitions;
+
+    // used to count the number of entries for each partition from the whole table
+    std::map<uint32_t, uint32_t> partition_histogram;
+    std::map<T, uint32_t> value_histogram;
+  };
+
+  struct SortContext : ColumnVisitableContext {
+    SortContext(ChunkID id, std::vector<std::pair<T, RowID>>& output) : chunk_id(id), sort_output(output) {}
+
+    ChunkID chunk_id;
+    std::vector<std::pair<T, RowID>>& sort_output;
+  };
+
+  // Radix calculation functions
+  template <typename T2>
+  typename std::enable_if<std::is_arithmetic<T2>::value, uint32_t>::type get_radix(T2 value, uint32_t radix_bits) {
+    return static_cast<uint32_t>(value) & radix_bits;
+  }
+  template <typename T2>
+  typename std::enable_if<!std::is_arithmetic<T2>::value, uint32_t>::type get_radix(T2 value, uint32_t radix_bits) {
+    auto result = reinterpret_cast<const uint32_t*>(value.c_str());
+    return *result & radix_bits;
+  }
+
+  // Sort functions
+  std::shared_ptr<SortedTable> sort_table(std::shared_ptr<const Table> input, const std::string& column_name);
+  std::shared_ptr<std::vector<SortedChunk>> sort_input_chunks(std::shared_ptr<const Table> input, const std::string& column_name);
+
+  // ColumnVisitable implementations to sort concrete input chunks
+  void handle_value_column(BaseColumn& column, std::shared_ptr<ColumnVisitableContext> context) override;
+  void handle_dictionary_column(BaseColumn& column, std::shared_ptr<ColumnVisitableContext> context) override;
+  void handle_reference_column(ReferenceColumn& column, std::shared_ptr<ColumnVisitableContext> context) override;
+
+  // Partitioning in case of Non-Equi-Join
+  void value_based_partitioning();
+  void value_based_table_partitioning(std::shared_ptr<SortedTable> sort_table, std::vector<T>& p_values);
+
+  // Looks for matches and possibly calls helper function to add match to _sort_merge_join._output
+  void partition_join(uint32_t partition_number, std::vector<PosList>& pos_lists_left,
+                      std::vector<PosList>& pos_lists_right);
+
+  void addSmallerValues(uint32_t partition_number, std::shared_ptr<SortedTable>& table_smaller_values,
+                        std::vector<PosList>& pos_list_smaller, std::vector<PosList>& pos_list_greater,
+                        uint32_t max_index_smaller_values, RowID greaterId);
+
+  void addGreaterValues(uint32_t partition_number,
+                        std::shared_ptr<SortMergeJoinImpl::SortedTable>& table_greater_values,
+                        std::vector<PosList>& pos_list_smaller, std::vector<PosList>& pos_list_greater,
+                        uint32_t max_index_greater_values, RowID smallerId);
+
+  void perform_join();
+
+  // builds output based on pos_list_left/-_right
+  std::shared_ptr<Table> build_output();
+  void add_output_columns(std::shared_ptr<Table> output_table, std::shared_ptr<const Table> input_table,
+                            const std::string& prefix, std::shared_ptr<const PosList> pos_list);
+  std::shared_ptr<PosList> dereference_pos_list(std::shared_ptr<const Table> input_table, ColumnID column_id,
+                                                std::shared_ptr<const PosList> pos_list);
+};
+
 
 /**
 ** Executes the sort-merge-join algorithm
@@ -107,13 +208,9 @@ template <typename T>
 std::shared_ptr<std::vector<typename SortMergeJoin::SortMergeJoinImpl<T>::SortedChunk>>
   SortMergeJoin::SortMergeJoinImpl<T>::sort_input_chunks(std::shared_ptr<const Table> input, const std::string& column_name) {
 
-    // Resize the output vectors to the input sizes
     auto sorted_chunks = std::make_shared<std::vector<SortedChunk>>(input->chunk_count());
-    for (ChunkID chunk_id{0}; chunk_id < input->chunk_count(); chunk_id++) {
-      sorted_chunks->at(chunk_id).values.resize(input->get_chunk(chunk_id).size());
-    }
 
-    // can be extended to find that value dynamically later on (depending on hardware etc.)
+    // Can be extended to find that value dynamically later on (depending on hardware etc.)
     const uint32_t partitionSizeThreshold = 10000;
     std::vector<std::shared_ptr<AbstractTask>> jobs;
 
@@ -124,7 +221,7 @@ std::shared_ptr<std::vector<typename SortMergeJoin::SortMergeJoinImpl<T>::Sorted
       size += input->chunk_size();
       chunk_ids.push_back(chunk_id);
       if (size > partitionSizeThreshold || chunk_id == input->chunk_count() - 1) {
-        // Spawn a job responsible for sorting multiple chunks of the input table
+        // Create a job responsible for sorting multiple chunks of the input table
         jobs.push_back(std::make_shared<JobTask>([this, sorted_chunks, chunk_ids, input, column_name] {
           for (auto chunk_id : chunk_ids) {
             auto column = input->get_chunk(chunk_id).get_column(input->column_id_by_name(column_name));
@@ -227,7 +324,7 @@ void SortMergeJoin::SortMergeJoinImpl<T>::handle_value_column(BaseColumn& column
   auto& value_column = dynamic_cast<ValueColumn<T>&>(column);
   auto sort_context = std::static_pointer_cast<SortContext>(context);
   auto& output = sort_context->sort_output;
-  DebugAssert(column.size() == output.size(), "An input chunk has a different size than a sorted chunk!");
+  output.resize(column.size());
 
   // Copy over every entry
   for (ChunkOffset chunk_offset{0}; chunk_offset < value_column.values().size(); chunk_offset++) {
@@ -246,7 +343,7 @@ void SortMergeJoin::SortMergeJoinImpl<T>::handle_dictionary_column(BaseColumn& c
   auto& dictionary_column = dynamic_cast<DictionaryColumn<T>&>(column);
   auto sort_context = std::static_pointer_cast<SortContext>(context);
   auto& output = sort_context->sort_output;
-  DebugAssert(column.size() == output.size(), "An input chunk has a different size than a sorted chunk!");
+  output.resize(column.size());
 
   auto value_ids = dictionary_column.attribute_vector();
   auto dict = dictionary_column.dictionary();
@@ -279,8 +376,7 @@ void SortMergeJoin::SortMergeJoinImpl<T>::handle_reference_column(ReferenceColum
   auto sort_context = std::static_pointer_cast<SortContext>(context);
   auto pos_list = ref_column.pos_list();
   auto& output = sort_context->sort_output;
-  DebugAssert(ref_column.size() == output.size(), "An input chunk has a different size than a sorted chunk!");
-
+  output.resize(ref_column.size());
 
   // Retrieve the columns from the referenced table so they only have to be casted once
   auto v_columns = std::vector<std::shared_ptr<ValueColumn<T>>>(referenced_table->chunk_count());
