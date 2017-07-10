0e6f470346f25f48a6b54fd0aeb812ed06624c23
Moritz Eyssen <moritz.eyssen@student.hpi.de>
2017-07-10 18:02:48 +0200

    Current state

--- src/lib/optimizer/abstract_syntax_tree/node_operator_translator.cpp
+++ src/lib/optimizer/abstract_syntax_tree/node_operator_translator.cpp
@@ -2,10 +2,12 @@
 
 #include <memory>
 
+#include "operators/aggregate.hpp"
 #include "operators/get_table.hpp"
 #include "operators/projection.hpp"
 #include "operators/sort.hpp"
 #include "operators/table_scan.hpp"
+#include "optimizer/abstract_syntax_tree/aggregate_node.hpp"
 #include "optimizer/abstract_syntax_tree/sort_node.hpp"
 #include "optimizer/abstract_syntax_tree/table_node.hpp"
 #include "optimizer/abstract_syntax_tree/predicate_node.hpp"
@@ -27,26 +29,26 @@ NodeOperatorTranslator::NodeOperatorTranslator() {
       std::bind(&NodeOperatorTranslator::translate_projection_node, this, std::placeholders::_1);
   _operator_factory[AstNodeType::Sort] =
       std::bind(&NodeOperatorTranslator::translate_order_by_node, this, std::placeholders::_1);
+  _operator_factory[AstNodeType::Aggregate] =
+      std::bind(&NodeOperatorTranslator::translate_aggregate_node, this, std::placeholders::_1);
 }
 
-const std::shared_ptr<AbstractOperator> NodeOperatorTranslator::translate_node(
+std::shared_ptr<AbstractOperator> NodeOperatorTranslator::translate_node(
     std::shared_ptr<AbstractAstNode> node) const {
   auto it = _operator_factory.find(node->type());
 
-  if (it == _operator_factory.end()) {
-    throw std::runtime_error("No factory for AstNodeType.");
-  }
+  Assert(it != _operator_factory.end(), "No factory for AstNodeType.");
 
   return it->second(node);
 }
 
-const std::shared_ptr<AbstractOperator> NodeOperatorTranslator::translate_table_node(
+std::shared_ptr<AbstractOperator> NodeOperatorTranslator::translate_table_node(
     std::shared_ptr<AbstractAstNode> node) const {
   auto table_node = std::dynamic_pointer_cast<TableNode>(node);
   return std::make_shared<GetTable>(table_node->table_name());
 }
 
-const std::shared_ptr<AbstractOperator> NodeOperatorTranslator::translate_table_scan_node(
+std::shared_ptr<AbstractOperator> NodeOperatorTranslator::translate_table_scan_node(
     std::shared_ptr<AbstractAstNode> node) const {
   auto input_operator = translate_node(node->left());
   auto predicate_node = std::dynamic_pointer_cast<PredicateNode>(node);
@@ -54,18 +56,80 @@ const std::shared_ptr<AbstractOperator> NodeOperatorTranslator::translate_table_
                                      predicate_node->value(), predicate_node->value2());
 }
 
-const std::shared_ptr<AbstractOperator> NodeOperatorTranslator::translate_projection_node(
+std::shared_ptr<AbstractOperator> NodeOperatorTranslator::translate_projection_node(
     std::shared_ptr<AbstractAstNode> node) const {
   auto input_operator = translate_node(node->left());
   return std::make_shared<Projection>(input_operator, node->output_columns());
 }
 
-const std::shared_ptr<AbstractOperator> NodeOperatorTranslator::translate_order_by_node(
+std::shared_ptr<AbstractOperator> NodeOperatorTranslator::translate_order_by_node(
     std::shared_ptr<AbstractAstNode> node) const {
   auto input_operator = translate_node(node->left());
 
   auto sort_node = std::dynamic_pointer_cast<SortNode>(node);
   return std::make_shared<Sort>(input_operator, sort_node->column_name(), sort_node->asc());
+}
+
+std::shared_ptr<AbstractOperator> NodeOperatorTranslator::translate_aggregate_node(
+  std::shared_ptr<AbstractAstNode> node) const {
+  auto input_operator = translate_node(node->left());
+
+  const auto aggregate_node = std::dynamic_pointer_cast<AggregateNode>(node);
+  const auto & aggregates = aggregate_node->aggregates();
+
+  std::shared_ptr<AbstractOperator> out_operator;
+
+  /**
+   * Handle arithmetic expressions in aggregate functions via Projection. Support only one level
+   * of arithmetics, i.e. SUM(a*b) is fine SUM(a*b+c) is not
+   */
+
+  /**
+   * Build Aggregate
+   */
+  std::vector<std::pair<std::string, AggregateFunction>> aggregate_definitions;
+  aggregate_definitions.reserve(aggregates.size());
+  for (size_t aggregate_idx = 0; aggregate_idx < aggregates.size(); aggregate_idx++) {
+    const auto & aggregate = aggregates[aggregate_idx];
+
+    Assert(aggregate.expr->expression_type() == ExpressionType::ExpressionFunctionReference, ""
+      "Only functions are supported in Aggregates");
+    const auto aggregate_function = aggregate.expr->aggregate_function();
+
+    aggregate_definitions.emplace_back(expr_aliases[aggregate_idx], aggregate_function);
+  }
+  out_operator = std::make_shared<Aggregate>(out_operator, aggregate_definitions)
+
+  /**
+   * Build Projection from Aggregate functions to alias names
+   *    e.g. for `SUM(a*b) as foo` this will project the column "SUM(a*b)" to "foo"
+   */
+  auto alias_projection_needed = std::any_of(aggregates.begin(), aggregates.end(),
+                                             [] (const auto & aggregate) {
+    return static_cast<bool>(aggregate.alias);
+  });
+
+  // If there are no aliases just skip this step, e.g. for SELECT COUNT(), SUM(a) [...]
+  // this step is not necessary.
+  if (alias_projection_needed) {
+    std::vector<std::string> columns;
+    columns.reserve(aggregates.size());
+
+    for (const auto & aggregate : aggregates) {
+      std::string out_alias;
+      if (aggregate.alias) {
+        out_alias = *aggregate.alias;
+      } else {
+        // If there is no alias for a column, just reproject to the same name, e.g.,
+        // in SELECT COUNT() as c, SUM(a) [...] this will project "SUM(a)" to "SUM(a)"
+        out_alias = aggregate.expr->to_alias_name();
+      }
+
+      columns.emplace_back(out_alias);
+    }
+
+    out_operator = std::make_shared<Projection>(out_operator, columns);
+  }
 }
 
 }  // namespace opossum
--- src/lib/optimizer/abstract_syntax_tree/node_operator_translator.hpp
+++ src/lib/optimizer/abstract_syntax_tree/node_operator_translator.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <atomic>
 #include <functional>
 #include <memory>
 #include <unordered_map>
@@ -19,21 +20,25 @@ class NodeOperatorTranslator {
   NodeOperatorTranslator &operator=(const NodeOperatorTranslator &) = delete;
   NodeOperatorTranslator(NodeOperatorTranslator &&) = delete;
 
-  const std::shared_ptr<AbstractOperator> translate_node(std::shared_ptr<AbstractAstNode> node) const;
+  std::shared_ptr<AbstractOperator> translate_node(std::shared_ptr<AbstractAstNode> node) const;
 
  protected:
   NodeOperatorTranslator();
   NodeOperatorTranslator &operator=(NodeOperatorTranslator &&) = default;
 
  private:
-  const std::shared_ptr<AbstractOperator> translate_table_node(std::shared_ptr<AbstractAstNode> node) const;
-  const std::shared_ptr<AbstractOperator> translate_table_scan_node(std::shared_ptr<AbstractAstNode> node) const;
-  const std::shared_ptr<AbstractOperator> translate_projection_node(std::shared_ptr<AbstractAstNode> node) const;
-  const std::shared_ptr<AbstractOperator> translate_order_by_node(std::shared_ptr<AbstractAstNode> node) const;
+  std::shared_ptr<AbstractOperator> translate_table_node(std::shared_ptr<AbstractAstNode> node) const;
+  std::shared_ptr<AbstractOperator> translate_table_scan_node(std::shared_ptr<AbstractAstNode> node) const;
+  std::shared_ptr<AbstractOperator> translate_projection_node(std::shared_ptr<AbstractAstNode> node) const;
+  std::shared_ptr<AbstractOperator> translate_order_by_node(std::shared_ptr<AbstractAstNode> node) const;
+  std::shared_ptr<AbstractOperator> translate_aggregate_node(std::shared_ptr<AbstractAstNode> node) const;
 
  private:
   std::unordered_map<AstNodeType, std::function<std::shared_ptr<AbstractOperator>(std::shared_ptr<AbstractAstNode>)>>
       _operator_factory;
+  // Used to generate unique_aliases
+  // TODO(mp) could be turned into translation-local non-atomic
+  std::atomic_uint64_t _alias_generator;
 };
 
 }  // namespace opossum
