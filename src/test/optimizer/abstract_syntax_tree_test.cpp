c964fd0cb6869f1d7c4a828fb42315cf7c6ce607
mrzzzrm <mrzesn@t-online.de>
2017-07-10 11:53:59 +0200

    De-abbreviate node names in test

--- src/test/optimizer/abstract_syntax_tree_test.cpp
+++ src/test/optimizer/abstract_syntax_tree_test.cpp
@@ -18,61 +18,61 @@ class AbstractSyntaxTreeTest : public BaseTest {
 };
 
 TEST_F(AbstractSyntaxTreeTest, ParentTest) {
-  const auto t_n = std::make_shared<TableNode>("a");
+  const auto table_node = std::make_shared<TableNode>("a");
 
-  ASSERT_EQ(t_n->left(), nullptr);
-  ASSERT_EQ(t_n->right(), nullptr);
-  ASSERT_EQ(t_n->parent(), nullptr);
+  ASSERT_EQ(table_node->left(), nullptr);
+  ASSERT_EQ(table_node->right(), nullptr);
+  ASSERT_EQ(table_node->parent(), nullptr);
 
-  const auto pred_n = std::make_shared<PredicateNode>("c1", ScanType::OpEquals, "a");
-  pred_n->set_left(t_n);
+  const auto predicate_node = std::make_shared<PredicateNode>("c1", ScanType::OpEquals, "a");
+  predicate_node->set_left(table_node);
 
-  ASSERT_EQ(t_n->parent(), pred_n);
-  ASSERT_EQ(pred_n->left(), t_n);
-  ASSERT_EQ(pred_n->right(), nullptr);
-  ASSERT_EQ(pred_n->parent(), nullptr);
+  ASSERT_EQ(table_node->parent(), predicate_node);
+  ASSERT_EQ(predicate_node->left(), table_node);
+  ASSERT_EQ(predicate_node->right(), nullptr);
+  ASSERT_EQ(predicate_node->parent(), nullptr);
 
   std::vector<std::string> column_names = {"c1", "c2"};
-  const auto p_n = std::make_shared<ProjectionNode>(column_names);
-  p_n->set_left(pred_n);
+  const auto projection_node = std::make_shared<ProjectionNode>(column_names);
+  projection_node->set_left(predicate_node);
 
-  ASSERT_EQ(pred_n->parent(), p_n);
-  ASSERT_EQ(p_n->left(), pred_n);
-  ASSERT_EQ(p_n->right(), nullptr);
-  ASSERT_EQ(p_n->parent(), nullptr);
+  ASSERT_EQ(predicate_node->parent(), projection_node);
+  ASSERT_EQ(projection_node->left(), predicate_node);
+  ASSERT_EQ(projection_node->right(), nullptr);
+  ASSERT_EQ(projection_node->parent(), nullptr);
 }
 
 TEST_F(AbstractSyntaxTreeTest, ChainSameNodesTest) {
-  const auto t_n = std::make_shared<TableNode>("a");
+  const auto table_node = std::make_shared<TableNode>("a");
 
-  ASSERT_EQ(t_n->left(), nullptr);
-  ASSERT_EQ(t_n->right(), nullptr);
-  ASSERT_EQ(t_n->parent(), nullptr);
+  ASSERT_EQ(table_node->left(), nullptr);
+  ASSERT_EQ(table_node->right(), nullptr);
+  ASSERT_EQ(table_node->parent(), nullptr);
 
-  const auto pred_n = std::make_shared<PredicateNode>("c1", ScanType::OpEquals, "a");
-  pred_n->set_left(t_n);
+  const auto predicate_node = std::make_shared<PredicateNode>("c1", ScanType::OpEquals, "a");
+  predicate_node->set_left(table_node);
 
-  ASSERT_EQ(t_n->parent(), pred_n);
-  ASSERT_EQ(pred_n->left(), t_n);
-  ASSERT_EQ(pred_n->right(), nullptr);
-  ASSERT_EQ(pred_n->parent(), nullptr);
+  ASSERT_EQ(table_node->parent(), predicate_node);
+  ASSERT_EQ(predicate_node->left(), table_node);
+  ASSERT_EQ(predicate_node->right(), nullptr);
+  ASSERT_EQ(predicate_node->parent(), nullptr);
 
-  const auto pred_n_2 = std::make_shared<PredicateNode>("c2", ScanType::OpEquals, "b");
-  pred_n_2->set_left(pred_n);
+  const auto predicate_node_2 = std::make_shared<PredicateNode>("c2", ScanType::OpEquals, "b");
+  predicate_node_2->set_left(predicate_node);
 
-  ASSERT_EQ(pred_n->parent(), pred_n_2);
-  ASSERT_EQ(pred_n_2->left(), pred_n);
-  ASSERT_EQ(pred_n_2->right(), nullptr);
-  ASSERT_EQ(pred_n_2->parent(), nullptr);
+  ASSERT_EQ(predicate_node->parent(), predicate_node_2);
+  ASSERT_EQ(predicate_node_2->left(), predicate_node);
+  ASSERT_EQ(predicate_node_2->right(), nullptr);
+  ASSERT_EQ(predicate_node_2->parent(), nullptr);
 
   std::vector<std::string> column_names = {"c1", "c2"};
-  const auto p_n = std::make_shared<ProjectionNode>(column_names);
-  p_n->set_left(pred_n_2);
+  const auto projection_node = std::make_shared<ProjectionNode>(column_names);
+  projection_node->set_left(predicate_node_2);
 
-  ASSERT_EQ(pred_n_2->parent(), p_n);
-  ASSERT_EQ(p_n->left(), pred_n_2);
-  ASSERT_EQ(p_n->right(), nullptr);
-  ASSERT_EQ(p_n->parent(), nullptr);
+  ASSERT_EQ(predicate_node_2->parent(), projection_node);
+  ASSERT_EQ(projection_node->left(), predicate_node_2);
+  ASSERT_EQ(projection_node->right(), nullptr);
+  ASSERT_EQ(projection_node->parent(), nullptr);
 }
 
 }  // namespace opossum
