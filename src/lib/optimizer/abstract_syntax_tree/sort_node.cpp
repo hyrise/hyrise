7b83c779a07ced92076e36779a4f6dcac4020a62
Tim Zimmermann <tim.zimmermann@student.hpi.uni-potsdam.de>
2017-07-10 18:05:54 +0200

    Format

--- src/lib/optimizer/abstract_syntax_tree/sort_node.cpp
+++ src/lib/optimizer/abstract_syntax_tree/sort_node.cpp
@@ -7,7 +7,9 @@ namespace opossum {
 SortNode::SortNode(const std::string column_name, const bool asc)
     : AbstractAstNode(AstNodeType::Sort), _column_name(column_name), _ascending(asc) {}
 
-std::string SortNode::description() const { return "Sort: " + _column_name + " (" + (_ascending ? "asc" : "desc") + ")"; }
+std::string SortNode::description() const {
+    return "Sort: " + _column_name + " (" + (_ascending ? "asc" : "desc") + ")";
+}
 
 std::string SortNode::column_name() const { return _column_name; }
 
