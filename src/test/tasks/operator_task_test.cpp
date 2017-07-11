dbe673b8b4ab1dac8171815a866d29e6f4994645
Tim Zimmermann <tim.zimmermann@student.hpi.uni-potsdam.de>
2017-07-11 14:59:51 +0200

    Re-factor enum join mode (#126)

--- src/test/tasks/operator_task_test.cpp
+++ src/test/tasks/operator_task_test.cpp
@@ -54,7 +54,7 @@ TEST_F(OperatorTaskTest, DoubleDependencyTasksFromOperatorTest) {
   auto gt_a = std::make_shared<GetTable>("table_a");
   auto gt_b = std::make_shared<GetTable>("table_b");
   auto join = std::make_shared<JoinHash>(gt_a, gt_b, std::pair<std::string, std::string>("a", "a"), ScanType::OpEquals,
-                                         Inner, std::string("left."), std::string("right."));
+                                         JoinMode::Inner, std::string("left."), std::string("right."));
 
   auto tasks = OperatorTask::make_tasks_from_operator(join);
   for (auto &task : tasks) {
