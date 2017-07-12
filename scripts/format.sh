5d2970227e2a4179f94c2d5476642fc1508cb216
Tim Zimmermann <tim.zimmermann@student.hpi.uni-potsdam.de>
2017-07-12 23:41:22 +0200

    Only format changed files

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-find src -iname "*.cpp" -o -iname "*.hpp" | xargs -I{} sh -c "clang-format -i -style=file '{}'"
+format_cmd="clang-format -i -style=file '{}'"
+
+if [ -z "${1}" ]; then
+    # Run on all changed as well as untracked cpp/hpp files, as compared to the current master. Skip deleted files.
+    { git diff --diff-filter=d --name-only master & git ls-files --others --exclude-standard; } | grep -E "\.[ch]pp$" | xargs -I{} sh -c "${format_cmd}"
+else
+    find src -iname "*.cpp" -o -iname "*.hpp" | xargs -I{} sh -c "${format_cmd}"
+fi
