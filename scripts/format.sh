9ea462fb675fb1b3ab5f37ca0f6252c4592c8c3a
Tim Zimmermann <tim.zimmermann@student.hpi.uni-potsdam.de>
2017-07-13 17:30:53 +0200

    Use argument to decide which files to format

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,10 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 format_cmd="clang-format -i -style=file '{}'"
 
-if [ -z "${1}" ]; then
+if [ "${1}" = "all" ]; then
+    find src -iname "*.cpp" -o -iname "*.hpp" | xargs -I{} sh -c "${format_cmd}"
+elif [ "$1" = "modified" ]; then
+    # Run on all changed as well as untracked cpp/hpp files, as compared to the current HEAD. Skip deleted files.
+    { git diff --diff-filter=d --name-only & git ls-files --others --exclude-standard; } | grep -E "\.[ch]pp$" | xargs -I{} sh -c "${format_cmd}"
+else
     # Run on all changed as well as untracked cpp/hpp files, as compared to the current master. Skip deleted files.
     { git diff --diff-filter=d --name-only master & git ls-files --others --exclude-standard; } | grep -E "\.[ch]pp$" | xargs -I{} sh -c "${format_cmd}"
-else
-    find src -iname "*.cpp" -o -iname "*.hpp" | xargs -I{} sh -c "${format_cmd}"
 fi
