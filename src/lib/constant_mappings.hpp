67fa1ba7bd22a72f03fba1e6b35f93fc78011f3f
Moritz Eyssen <mrzesn@t-online.de>
2017-07-14 14:35:40 +0200

    AST Nodes: Projection, Predicate, Table, Sort (#123)
    * Import AST Code from mp/basic-optimizer
    
    * Incorporate Review changes
    
    * De-abbreviate node names in test
    
    * Lint&Format
    
    * Incorporate Review requests
    
    * Update constant_mappings for ScanType to and from string
    
    * Disable GCC warnings in proto generated files via wrapper header
    
    * SortNode::_asc -> _ascending
    
    * Format
    
    * Address review
    
    * Rename TableNode to PhysicalTableNode
    
    * Format
    
    * Includes
    
    * Address second review

--- src/lib/constant_mappings.hpp
+++ src/lib/constant_mappings.hpp
@@ -1,20 +1,30 @@
 #include <string>
 #include <unordered_map>
 
+#include "network/opossum.pb.wrapper.hpp"
+#include "types.hpp"
+
 namespace opossum {
 
-  const std::unordered_map<std::string, const proto::ScanType> op_string_to_scan_type = {
-    {"=", opossum::proto::ScanType::OpEquals},
-    {"!=", opossum::proto::ScanType::OpNotEquals},
+const std::unordered_map<std::string, proto::ScanType> string_to_proto_scan_type = {
+    {"=", opossum::proto::ScanType::OpEquals},        {"!=", opossum::proto::ScanType::OpNotEquals},
+    {"<", opossum::proto::ScanType::OpLessThan},      {"<=", opossum::proto::ScanType::OpLessThanEquals},
+    {">", opossum::proto::ScanType::OpGreaterThan},   {">=", opossum::proto::ScanType::OpGreaterThanEquals},
+    {"BETWEEN", opossum::proto::ScanType::OpBetween}, {"LIKE", opossum::proto::ScanType::OpLike},
+};
 
-    {"<", opossum::proto::ScanType::OpLessThan},
-    {"<=", opossum::proto::ScanType::OpLessThanEquals},
+const std::unordered_map<std::string, ScanType> string_to_scan_type = {
+    {"=", opossum::ScanType::OpEquals},        {"!=", opossum::ScanType::OpNotEquals},
+    {"<", opossum::ScanType::OpLessThan},      {"<=", opossum::ScanType::OpLessThanEquals},
+    {">", opossum::ScanType::OpGreaterThan},   {">=", opossum::ScanType::OpGreaterThanEquals},
+    {"BETWEEN", opossum::ScanType::OpBetween}, {"LIKE", opossum::ScanType::OpLike},
+};
 
-    {">", opossum::proto::ScanType::OpGreaterThan},
-    {">=", opossum::proto::ScanType::OpGreaterThanEquals},
-
-    {"BETWEEN", opossum::proto::ScanType::OpBetween},
-    {"LIKE", opossum::proto::ScanType::OpLike},
-  };
+const std::unordered_map<ScanType, std::string> scan_type_to_string = {
+    {opossum::ScanType::OpEquals, "="},        {opossum::ScanType::OpNotEquals, "!="},
+    {opossum::ScanType::OpLessThan, "<"},      {opossum::ScanType::OpLessThanEquals, "<="},
+    {opossum::ScanType::OpGreaterThan, ">"},   {opossum::ScanType::OpGreaterThanEquals, ">="},
+    {opossum::ScanType::OpBetween, "BETWEEN"}, {opossum::ScanType::OpLike, "LIKE"},
+};
 
 }  // namespace opossum
