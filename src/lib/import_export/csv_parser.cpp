098eb955298de1ace8b57e0393b41feb96160f05
Leander Neiss <leander.neiss@student.hpi.de>
2017-07-15 00:19:41 +0200

    Add chunk parsing skeleton to CsvParser.

--- src/lib/import_export/csv_parser.cpp
+++ src/lib/import_export/csv_parser.cpp
@@ -3,10 +3,11 @@
 #include <fstream>
 #include <memory>
 #include <string>
+#include <list>
 
 #include "utils/assert.hpp"
 #include "import_export/csv_converter.hpp"
-// #include "scheduler/job_task.hpp"
+#include "scheduler/job_task.hpp"
 
 // #include "resolve_type.hpp"
 // #include "types.hpp"
@@ -14,22 +15,73 @@
 
 namespace opossum {
 
-CsvParser::CsvParser(size_t buffer_size, const CsvConfig& csv_config)
+CsvParser::CsvParser(size_t buffer_size, const CsvConfig & csv_config)
     : _buffer_size(buffer_size), _csv_config(csv_config) {}
 
 std::shared_ptr<Table> CsvParser::parse(const std::string& filename) {
+  const auto& delimiter = _csv_config.delimiter;
+
   const auto table = process_meta_file(filename + _csv_config.meta_file_extension);
+
+  std::ifstream csvfile { filename };
+  std::string content { std::istreambuf_iterator<char>(csvfile), {} };
+
+  // return empty table if input file is empty
+  if (!csvfile) return table;
+
+  // make sure content ends with '\n' for better row processing later
+  if (content.back() != delimiter) content.push_back(delimiter);
+
+  // Safe chunks in list to avoid memory relocations
+  std::list<Chunk> chunks;
+  std::vector<std::shared_ptr<JobTask>> tasks;
+  size_t pos;
+  std::vector<size_t> row_ends;
+  do {
+    row_ends.clear();
+    pos = find_Nth(content, delimiter, table->chunk_size(), row_ends);
+
+    // create chunk and fill with columns
+    chunks.emplace_back(true);
+    auto& chunk = chunks.back();
+
+    // create and start parsing task
+    tasks.emplace_back(std::make_shared<JobTask>([this, &chunk, &table, content, row_ends]() {
+      parse_chunk(chunk, *table, content, row_ends);
+    }));
+    tasks.back()->schedule();
+
+    content.erase(0, pos + 1);
+  } while (pos != std::string::npos);
+
+  for (auto& task : tasks) {
+    task->join();
+  }
+
+  for (auto& chunk : chunks) {
+    table->add_chunk(std::move(chunk));
+  }
+
   return table;
 }
 
-std::shared_ptr<Table> CsvParser::process_meta_file(const std::string& filename) {
+void CsvParser::parse_chunk(Chunk & chunk, const Table & table, const std::string & csvcontent, const std::vector<size_t> & row_ends) {
+  size_t start = 0;
+  for (const auto end : row_ends) {
+    auto row = csvcontent.substr(start, end-start);
+    // printf("%s\n", row.c_str());
+    start = end + 1;
+  }
+}
+
+std::shared_ptr<Table> CsvParser::process_meta_file(const std::string & filename) {
   const char delimiter = '\n';
   const char seperator = ',';
 
   std::ifstream metafile { filename };
-  std::string content { std::istreambuf_iterator<char>(metafile), std::istreambuf_iterator<char>() };
+  std::string content { std::istreambuf_iterator<char>(metafile), {} };
 
-  // make sure content ends with '\n' for better row processing
+  // make sure content ends with '\n' for better row processing later
   if (content.back() != delimiter) content.push_back(delimiter);
 
   // skip header
@@ -68,6 +120,23 @@ std::shared_ptr<Table> CsvParser::process_meta_file(const std::string& filename)
   }
 
   return table;
+}
+
+size_t CsvParser::find_Nth(const std::string & str, const char & find, const unsigned int N, std::vector<size_t> & indices) {
+  DebugAssert(indices.empty(), "CsvParse::find_Nth should be passed an empty indices vector.");
+  if ( 0 == N ) { return std::string::npos; }
+
+  size_t pos, from = 0;
+  indices.reserve(N);
+
+  for (unsigned int i = 0; i < N; ++i) {
+    pos = str.find(find, from);
+    if ( std::string::npos == pos ) { break; }
+    indices.push_back(pos);
+    from = pos + 1; // from = pos + find.size();
+  }
+
+  return pos;
 }
 
 }  // namespace opossum
--- src/lib/import_export/csv_parser.hpp
+++ src/lib/import_export/csv_parser.hpp
@@ -17,19 +17,21 @@ class CsvParser {
    * @param buffer_size Specifies the amount of data from the input file in bytes that a single task should work on.
    * @param csv_config  Csv configuration (delimiter, separator, ..)
    */
-  explicit CsvParser(size_t buffer_size, const CsvConfig& csv_config = {});
+  explicit CsvParser(size_t buffer_size, const CsvConfig & csv_config = {});
 
   // cannot move-assign because of const members
-  CsvParser& operator=(CsvParser&&) = delete;
+  CsvParser& operator=(CsvParser &&) = delete;
 
   /*
    * @param filename Path to the input file.
    * @returns        The table that was created from the csv file.
    */
-  std::shared_ptr<Table> parse(const std::string& filename);
+  std::shared_ptr<Table> parse(const std::string & filename);
 
  protected:
-  std::shared_ptr<Table> process_meta_file(const std::string& filename);
+  void parse_chunk(Chunk & chunk, const Table & table, const std::string & csvcontent, const std::vector<size_t> & row_ends);
+  std::shared_ptr<Table> process_meta_file(const std::string & filename);
+  size_t find_Nth(const std::string & str, const char & find, const unsigned int N, std::vector<size_t> & indices);
 
   // Number of bytes that a task processes from the input file.
   const size_t _buffer_size;
