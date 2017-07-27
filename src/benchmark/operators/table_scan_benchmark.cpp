ecb21df9c733a656f26bb2c8bc14bc7b7ee8f339
Max Jendruk <code@jendruk.com>
2017-07-27 20:36:34 +0200

    Add second table scan benchmark with new implementation

--- src/benchmark/operators/table_scan_benchmark.cpp
+++ src/benchmark/operators/table_scan_benchmark.cpp
@@ -7,6 +7,7 @@
 #include "../base_fixture.cpp"
 #include "../table_generator.hpp"
 #include "operators/table_scan.hpp"
+#include "operators/new_table_scan.hpp"
 
 namespace opossum {
 
@@ -29,6 +30,19 @@ BENCHMARK_F(BenchmarkBasicFixture, BM_TableScanVariable)(benchmark::State& state
   while (state.KeepRunning()) {
     auto table_scan =
         std::make_shared<TableScan>(_table_wrapper_a, ColumnName("a"), ScanType::OpGreaterThanEquals, ColumnName("b"));
+    table_scan->execute();
+  }
+}
+
+BENCHMARK_F(BenchmarkBasicFixture, BM_NewTableScanVariable)(benchmark::State& state) {
+  clear_cache();
+
+  auto warm_up =
+      std::make_shared<NewTableScan>(_table_wrapper_a, ColumnName("a"), ScanType::OpGreaterThanEquals, ColumnName("b"));
+  warm_up->execute();
+  while (state.KeepRunning()) {
+    auto table_scan =
+        std::make_shared<NewTableScan>(_table_wrapper_a, ColumnName("a"), ScanType::OpGreaterThanEquals, ColumnName("b"));
     table_scan->execute();
   }
 }
