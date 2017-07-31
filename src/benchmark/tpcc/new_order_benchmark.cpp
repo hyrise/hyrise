80af0f6e588225ad417f3c20c9ceb164cdfb4e92
jstriebel <jstriebel@users.noreply.github.com>
2017-07-31 12:15:38 +0200

    minor fix

--- src/benchmark/tpcc/new_order_benchmark.cpp
+++ src/benchmark/tpcc/new_order_benchmark.cpp
@@ -31,17 +31,17 @@ class TPCCNewOrderBenchmark : public TPCCBenchmarkFixture {
     while (state.KeepRunning()) {
       tpcc::NewOrderParams params;
 
-      params.w_id = static_cast<int32_t>(_random_gen.number(0, _gen._warehouse_size - 1));
-      params.d_id = static_cast<int32_t>(_random_gen.number(0, tpcc::NUM_DISTRICTS_PER_WAREHOUSE - 1));
-      params.c_id = static_cast<int32_t>(_random_gen.number(0, tpcc::NUM_CUSTOMERS_PER_DISTRICT - 1));
+      params.w_id = static_cast<int32_t>(_random_gen.random_number(0, _gen._warehouse_size - 1));
+      params.d_id = static_cast<int32_t>(_random_gen.random_number(0, tpcc::NUM_DISTRICTS_PER_WAREHOUSE - 1));
+      params.c_id = static_cast<int32_t>(_random_gen.random_number(0, tpcc::NUM_CUSTOMERS_PER_DISTRICT - 1));
       params.o_entry_d = 0;  // TODO(anybody): Real data
-      params.order_lines.reserve(_random_gen.number(tpcc::MIN_ORDER_LINE_COUNT, tpcc::MAX_ORDER_LINE_COUNT));
+      params.order_lines.reserve(_random_gen.random_number(tpcc::MIN_ORDER_LINE_COUNT, tpcc::MAX_ORDER_LINE_COUNT));
 
       for (size_t ol = 0; ol < params.order_lines.size(); ol++) {
         auto &order_line = params.order_lines[ol];
-        order_line.w_id = static_cast<int32_t>(_random_gen.number(0, _gen._warehouse_size - 1));
-        order_line.i_id = static_cast<int32_t>(_random_gen.number(0, tpcc::NUM_ITEMS - 1));
-        order_line.qty = static_cast<int32_t>(_random_gen.number(1, tpcc::MAX_ORDER_LINE_QUANTITY));
+        order_line.w_id = static_cast<int32_t>(_random_gen.random_number(0, _gen._warehouse_size - 1));
+        order_line.i_id = static_cast<int32_t>(_random_gen.random_number(0, tpcc::NUM_ITEMS - 1));
+        order_line.qty = static_cast<int32_t>(_random_gen.random_number(1, tpcc::MAX_ORDER_LINE_QUANTITY));
       }
 
       impl.run_transaction(params);
