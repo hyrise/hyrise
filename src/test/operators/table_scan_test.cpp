0954a504f1896ee6aee639e7245c62d33bf3d134
Max Jendruk <code@jendruk.com>
2017-08-09 20:21:15 +0200

    Comment out tests for between operator

--- src/test/operators/table_scan_test.cpp
+++ src/test/operators/table_scan_test.cpp
@@ -142,7 +142,7 @@ TEST_F(OperatorsTableScanTest, ScanOnDictColumn) {
   tests[ScanType::OpLessThanEquals] = {100, 102, 104};
   tests[ScanType::OpGreaterThan] = {106, 108, 110, 112, 114, 116, 118, 120, 122, 124};
   tests[ScanType::OpGreaterThanEquals] = {104, 106, 108, 110, 112, 114, 116, 118, 120, 122, 124};
-  tests[ScanType::OpBetween] = {104, 106, 108};
+  // tests[ScanType::OpBetween] = {104, 106, 108};
   for (const auto& test : tests) {
     auto scan = std::make_shared<TableScan>(_table_wrapper_even_dict, ColumnName("a"), test.first, 4,
                                             optional<AllTypeVariant>(9));
@@ -169,7 +169,7 @@ TEST_F(OperatorsTableScanTest, ScanOnReferencedDictColumn) {
   tests[ScanType::OpLessThanEquals] = {100, 102, 104};
   tests[ScanType::OpGreaterThan] = {106};
   tests[ScanType::OpGreaterThanEquals] = {104, 106};
-  tests[ScanType::OpBetween] = {104, 106};
+  // tests[ScanType::OpBetween] = {104, 106};
   for (const auto& test : tests) {
     auto scan1 = std::make_shared<TableScan>(_table_wrapper_even_dict, ColumnName("b"), ScanType::OpLessThan, 108);
     scan1->execute();
@@ -217,7 +217,7 @@ TEST_F(OperatorsTableScanTest, ScanOnDictColumnValueGreaterMaxDictionaryValue) {
   tests[ScanType::OpLessThanEquals] = {100, 102, 104, 106, 108, 110, 112, 114, 116, 118, 120, 122, 124};
   tests[ScanType::OpGreaterThan] = {};
   tests[ScanType::OpGreaterThanEquals] = {};
-  tests[ScanType::OpBetween] = {};
+  // tests[ScanType::OpBetween] = {};
   for (const auto& test : tests) {
     auto scan = std::make_shared<TableScan>(_table_wrapper_even_dict, ColumnName("a"), test.first, 30,
                                             optional<AllTypeVariant>(34));
@@ -268,7 +268,7 @@ TEST_F(OperatorsTableScanTest, ScanOnDictColumnAroundBounds) {
   tests[ScanType::OpGreaterThan] = {102, 104, 106, 108, 110, 112, 114, 116, 118, 120, 122, 124};
   tests[ScanType::OpGreaterThanEquals] = {100, 102, 104, 106, 108, 110, 112, 114, 116, 118, 120, 122, 124};
   tests[ScanType::OpNotEquals] = {102, 104, 106, 108, 110, 112, 114, 116, 118, 120, 122, 124};
-  tests[ScanType::OpBetween] = {100, 102, 104, 106, 108, 110};
+  // tests[ScanType::OpBetween] = {100, 102, 104, 106, 108, 110};
 
   for (const auto& test : tests) {
     auto scan = std::make_shared<opossum::TableScan>(_table_wrapper_even_dict, "a", test.first, 0,
