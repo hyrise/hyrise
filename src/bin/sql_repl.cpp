b3a2837c46f54f068084c96f8e15b64165aa823d
Leander Neiss <leander.neiss@student.hpi.de>
2017-08-09 21:45:29 +0200

    Add SQL command parsing/executing, and builtin commands for SqlRepl.

--- src/bin/sql_repl.cpp
+++ src/bin/sql_repl.cpp
@@ -3,13 +3,15 @@
 #include <iostream>
 
 #include "SQLParser.h"
+// #include "operators/import_csv.hpp"
 
 namespace opossum {
 
 SqlRepl::SqlRepl(const std::string & prompt, std::istream & in, std::ostream & out)
     : _prompt(prompt)
     , _in(in.rdbuf())
-    , _out(out.rdbuf()) {}
+    , _out(out.rdbuf())
+    , _commands({"loadtable", "exit"}) {}
 
 void SqlRepl::repl() {
   for (;;)
@@ -26,16 +28,56 @@ std::string SqlRepl::_read() {
 }
 
 std::string SqlRepl::_eval(const std::string & input) {
-  hsql::SQLParserResult parse_result;
-
-  hsql::SQLParser::parseSQLString(input, &parse_result);
-  bool isValid = parse_result.isValid();
-
-  return isValid ? "Valid" : "Not Valid";
+  std::string input_no_spaces = SqlRepl::trim(input);
+  if (_commands.find(input_no_spaces.substr(0, input_no_spaces.find('('))) != _commands.end())
+  {
+    return _eval_command(input_no_spaces);
+  }
+  return _eval_sql(input);
 }
 
 void SqlRepl::_print(const std::string & result) {
   _out << result << "\n";
+}
+
+std::string SqlRepl::_eval_command(const std::string & cmd) {
+  return cmd;
+}
+
+std::string SqlRepl::_eval_sql(const std::string & sql) {
+  hsql::SQLParserResult parse_result;
+  hsql::SQLParser::parseSQLString(sql, &parse_result);
+
+  if (!parse_result.isValid())
+  {
+    return "Error: SQL query not valid.";
+  }
+
+  // Compile the parse result.
+  if (!_translator.translate_parse_result(parse_result))
+  {
+    return "Error while compiling: " + _translator.get_error_msg();
+  }
+
+  _plan = _translator.get_query_plan();
+
+  for (const auto& task : _plan.tasks()) {
+    task->get_operator()->execute();
+  }
+
+  auto result = _plan.tree_roots().back()->get_output();
+
+  return parse_result.isValid() ? "Valid" : "Not valid";
+}
+
+std::string SqlRepl::trim(const std::string & str) {
+  size_t first = str.find_first_not_of(' ');
+  if (std::string::npos == first)
+  {
+    return str;
+  }
+  size_t last = str.find_last_not_of(' ');
+  return str.substr(first, (last - first + 1));
 }
 
 }  // namespace opossum
@@ -44,5 +86,3 @@ int main(int argc, char** argv) {
   opossum::SqlRepl sql;
   sql.repl();
 }
-
-//
--- src/bin/sql_repl.hpp
+++ src/bin/sql_repl.hpp
@@ -1,6 +1,9 @@
 
 #include <string>
 #include <iostream>
+#include <set>
+
+#include "sql/sql_query_translator.hpp"
 
 namespace opossum {
 
@@ -15,10 +18,19 @@ class SqlRepl
   std::string _read();
   std::string _eval(const std::string & input);
   void _print(const std::string & result);
+  std::string _eval_command(const std::string & sql);
+  std::string _eval_sql(const std::string & sql);
+
+  static std::string trim(const std::string & str);
 
   std::string _prompt;
   std::istream _in;
   std::ostream _out;
+
+  const std::set<std::string> _commands;
+
+  SQLQueryTranslator _translator;
+  SQLQueryPlan _plan;
 };
 
 }  // namespace opossum
