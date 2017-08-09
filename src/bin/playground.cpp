4aa61931290ad5eb52e5a76d84b788053cb413c4
Max Jendruk <code@jendruk.com>
2017-08-09 20:34:39 +0200

    Remove code from playground.cpp

--- src/bin/playground.cpp
+++ src/bin/playground.cpp
@@ -1,26 +1,6 @@
 
 #include <iostream>
-#include <memory>
-#include <type_traits>
-#include <typeinfo>
-
-#include <boost/hana/type.hpp>
-
-#include "resolve_column_type.hpp"
-
-using namespace opossum;
-
-template <typename T>
-void method(T&& val) {
-  if
-    constexpr(decltype(val)::is_reference_column()) { std::cout << "Hallo" << std::endl; }
-}
 
 int main() {
-  auto column = ValueColumn<int>{};
-  column.append(42);
-
-  resolve_column_type("int", column, [](auto& column) { std::cout << typeid(column).name() << std::endl; });
-
-  method(column);
+  std::cout << "Hello world!" << std:endl;
 }
