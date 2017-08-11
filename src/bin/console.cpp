c709d86ff2c0ecc1672cda461d9c50dad32b8625
Leander Neiss <leander.neiss@student.hpi.de>
2017-08-11 15:00:31 +0200

    Enable custom commands for opossum Console.

--- src/bin/console.cpp
+++ src/bin/console.cpp
@@ -1,27 +1,58 @@
 #include "console.hpp"
 
 #include <iostream>
-#include "readline/readline.h"
+#include <readline/readline.h>
+#include <readline/history.h>
+
+namespace {
+
+  // Helper functions
+
+  std::string trim(const std::string & str) {
+    size_t first = str.find_first_not_of(' ');
+    if (std::string::npos == first)
+    {
+      return str;
+    }
+    size_t last = str.find_last_not_of(' ');
+    return str.substr(first, (last - first + 1));
+  }
+
+}
 
 namespace opossum {
 
+// Command functions declaration
+
+int exit(const std::string &);
+
+// Console implementation
+
 Console::Console(const std::string & prompt)
-    : _prompt(prompt) {}
+    : _prompt(prompt)
+    , _commands() {
+      register_command("exit", exit);
+      register_command("loadtpcc", load_tpcc);
+    }
 
-int Console::read_line() {
-  char* buffer; //Buffer of line entered by user
+int Console::read() {
+  char* buffer; // Buffer of line entered by user
 
-  buffer = readline(_prompt.c_str());
-  std::string input(buffer);
-  free(buffer);
-  buffer = NULL;
+  buffer = readline(_prompt.c_str()); // Prompt user for input
 
-  if ("exit" == input)
-  {
-    return -1;
+  if(strcmp(buffer, "") != 0) { // Only save non-empty commands to history
+    add_history(buffer);
   }
 
-  return 0;
+  std::string input(buffer);
+  free(buffer); // Free buffer, since readline() allocates new string every time
+  buffer = NULL;
+
+  return _eval(input);
+}
+
+void Console::register_command(const std::string & name, const CommandFunction & f) {
+  _commands[name] = f;
 }
 
 void Console::setPrompt(const std::string & prompt) {
@@ -32,6 +63,40 @@ std::string Console::prompt() const {
   return _prompt;
 }
 
+int Console::_eval(const std::string & input) {
+  std::string input_trimmed = trim(input);
+
+  RegisteredCommands::iterator it;
+  if ((it = _commands.find(input_trimmed.substr(0, input_trimmed.find('(')))) != std::end(_commands)) {
+    return _eval_command(it->second, input_trimmed);
+  }
+
+  return _eval_sql(input_trimmed);
+}
+
+int Console::_eval_command(const CommandFunction & f, const std::string & input) {
+  size_t first = input.find('(');
+  size_t last = input.find_last_of(')');
+
+  if (std::string::npos == first)
+  {
+    return static_cast<int>(f(""));
+  }
+
+  std::string arg = input.substr(first, last-first);
+  return static_cast<int>(f(arg));
+}
+
+int Console::_eval_sql(const std::string & input) {
+  return ReturnCode::Ok;
+}
+
+// Command functions implementation
+
+int exit(const std::string &) {
+  return opossum::Console::ReturnCode::Quit;
+}
+
 }  // namespace opossum
 
 int main(int argc, char** argv) {
@@ -40,7 +105,7 @@ int main(int argc, char** argv) {
   opossum::Console console("> ");
   int retCode;
 
-  while ((retCode = console.read_line()) != Return::Quit) {
+  while ((retCode = console.read()) != Return::Quit) {
     if (retCode == Return::Ok)
     {
       console.setPrompt("> ");
--- src/bin/console.hpp
+++ src/bin/console.hpp
@@ -1,11 +1,17 @@
+#pragma once
 
 #include <string>
+#include <functional>
+#include <unordered_map>
 
 namespace opossum {
 
 class Console
 {
  public:
+
+  using CommandFunction = std::function<int(const std::string &)>;
+  using RegisteredCommands = std::unordered_map<std::string, CommandFunction>;
 
   enum ReturnCode {
     Quit = -1,
@@ -15,13 +21,19 @@ class Console
 
   explicit Console(const std::string & prompt = "> ");
 
-  int read_line();
+  int read();
+  void register_command(const std::string & name, const CommandFunction & f);
 
   void setPrompt(const std::string & prompt);
   std::string prompt() const;
 
  protected:
+  int _eval(const std::string & input);
+  int _eval_command(const CommandFunction & f, const std::string & input);
+  int _eval_sql(const std::string & input);
+
   std::string _prompt;
+  RegisteredCommands _commands;
 };
 
 }  // namespace opossum
