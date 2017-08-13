42b7a07e3fb9666632becc3d25526121edcfcf27
Fabian Wiebe <fabian.wiebe@student.hpi.de>
2017-08-13 19:43:26 +0200

    Revert "Fix compile error on jenkins"
    This reverts commit 1d9ef416b508c938266f5180023d0399782ba870.

--- src/lib/optimizer/base_column_statistics.hpp
+++ src/lib/optimizer/base_column_statistics.hpp
@@ -74,8 +74,8 @@ struct ColumnSelectivityResult {
  * Return type of selectivity functions for operations on two columns.
  */
 struct TwoColumnSelectivityResult : public ColumnSelectivityResult {
-  TwoColumnSelectivityResult(float selectivity, std::shared_ptr<BaseColumnStatistics> column_stats,
-                             std::shared_ptr<BaseColumnStatistics> second_column_stats)
+  TwoColumnSelectivityResult(float selectivity, std::shared_ptr<BaseColumnStatistics> &&column_stats,
+                             std::shared_ptr<BaseColumnStatistics> &&second_column_stats)
       : ColumnSelectivityResult{selectivity, column_stats}, second_column_statistics(second_column_stats) {}
 
   std::shared_ptr<BaseColumnStatistics> second_column_statistics;
