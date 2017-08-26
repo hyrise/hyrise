2f0b238f21815c7d38d461f873f9f9aefca1c676
Max Jendruk <code@jendruk.com>
2017-08-26 12:28:00 +0200

    Refine “code size”

--- src/lib/storage/iterables/base_iterables.hpp
+++ src/lib/storage/iterables/base_iterables.hpp
@@ -22,7 +22,7 @@ namespace opossum {
  * This results in a large amount of code. In cases where one is certain that
  * no indices (i.e. a ChunkOffsetsList) have been passed, get_iterators_no_indices
  * can be used. This method won’t instantiate the lambda for indexed iterators, hence,
- * reduce code size.
+ * reduce the size of the compiled binary file.
  *
  *
  * Example Usage
