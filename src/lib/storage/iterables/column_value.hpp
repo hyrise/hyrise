98abf9f2bef5f3d4b5b26457c0aad71794c35033
Max Jendruk <code@jendruk.com>
2017-08-28 12:19:02 +0200

    Add comments

--- src/lib/storage/iterables/column_value.hpp
+++ src/lib/storage/iterables/column_value.hpp
@@ -1,3 +1,4 @@
+
 #pragma once
 
 #include "types.hpp"
@@ -6,23 +7,19 @@ namespace opossum {
 
 /**
  * @defgroup Values returned by iterators from iterables
+ *
+ * If the underlying column (or data structure) is indexed,
+ * i.e. referenced by a reference column, chunk_offset()
+ * returns the offset into the reference column. Otherwise
+ * it returns an offset into the column.
+ *
  * @{
  */
 
-template <typename T>
-class ColumnValue {
- public:
-  ColumnValue(const T& value, const ChunkOffset& chunk_offset) : _value{value}, _chunk_offset{chunk_offset} {}
-
-  const T& value() const { return _value; }
-  bool is_null() const { return false; }
-  ChunkOffset chunk_offset() const { return _chunk_offset; }
-
- private:
-  const T _value;
-  const ChunkOffset _chunk_offset;
-};
-
+/**
+ * This is the most generic column value class
+ * and is used in most iterators.
+ */
 template <typename T>
 class NullableColumnValue {
  public:
@@ -39,6 +36,30 @@ class NullableColumnValue {
   const ChunkOffset _chunk_offset;
 };
 
+/**
+ * Used when the underlying column
+ * (or data structure) cannot be null.
+ */
+template <typename T>
+class ColumnValue {
+ public:
+  ColumnValue(const T& value, const ChunkOffset& chunk_offset) : _value{value}, _chunk_offset{chunk_offset} {}
+
+  const T& value() const { return _value; }
+  bool is_null() const { return false; }
+  ChunkOffset chunk_offset() const { return _chunk_offset; }
+
+ private:
+  const T _value;
+  const ChunkOffset _chunk_offset;
+};
+
+/**
+ * Used for data structures that only
+ * store if the entry is null or not.
+ *
+ * @see NullValueVectorIterable
+ */
 class ColumnNullValue {
  public:
   ColumnNullValue(const bool null_value, const ChunkOffset& chunk_offset)
