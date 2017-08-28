1f6358fc96d131cdf0217e6d7f1d53755d7bb587
Moritz Eyssen <moritz.eyssen@student.hpi.de>
2017-08-28 14:23:43 +0200

    Docs for new AbstractASTNode members

--- src/lib/optimizer/abstract_syntax_tree/abstract_ast_node.hpp
+++ src/lib/optimizer/abstract_syntax_tree/abstract_ast_node.hpp
@@ -57,12 +57,38 @@ class AbstractASTNode : public std::enable_shared_from_this<AbstractASTNode> {
   virtual const std::vector<ColumnID> &output_column_ids() const;
   bool has_output_column(const std::string &column_name) const;
 
+  // @{
+  /**
+   * AbstractASTNode::find_column_id_for_column_identifier() looks for the @param column_identifier in the columns this
+   * node outputs. If it can find it, it will be returned, otherwise nullopt is returned.
+   * AbstractASTNode::get_column_id_for_column_identifier() is more strict and will fail, if the
+   * @param column_identifier cannot be found
+   * NOTE: These functions will possibly result in a full recursive traversal of the ancestors of this node.
+   */
   ColumnID get_column_id_for_column_identifier(const ColumnIdentifier &column_identifier) const;
   virtual optional<ColumnID> find_column_id_for_column_identifier(const ColumnIdentifier &column_identifier) const;
+  // @}
 
+  // @{
+  /**
+   * AbstractASTNode::find_column_id_for_column_identifier() looks for the @param expression in the columns this
+   * node outputs, checking by semantic and NOT by Expression address. If it can find it, it will be returned,
+   * otherwise nullopt is returned.
+   * AbstractASTNode::get_column_id_for_column_identifier() is more strict and will fail, if the
+   * @param expression cannot be found
+   * NOTE: These functions will possibly result in a full recursive traversal of the ancestors of this node.
+   */
   ColumnID get_column_id_for_expression(const std::shared_ptr<ExpressionNode> &expression) const;
   virtual optional<ColumnID> find_column_id_for_expression(const std::shared_ptr<ExpressionNode> &expression) const;
+  // @}
 
+  /**
+   * Checks whether this node or any of its ancestors retrieve the table @param table_name from the StorageManager or
+   * whether it or any of its ancestors assign an ALIAS with the name @param table_name.
+   * Used especially to figure out which of the children of a Join is referenced.
+   * @param table_name
+   * @return
+   */
   virtual bool manages_table(const std::string &table_name) const;
 
   void print(const uint32_t level = 0, std::ostream &out = std::cout) const;
