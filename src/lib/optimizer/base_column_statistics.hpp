4d0bc5dd8d6be012f1b8c35dbf871379e153f0b5
Fabian Wiebe <fabian.wiebe@student.hpi.de>
2017-09-05 21:28:53 +0200

    Adjust comments for base column statistics.

--- src/lib/optimizer/base_column_statistics.hpp
+++ src/lib/optimizer/base_column_statistics.hpp
@@ -66,17 +66,19 @@ class BaseColumnStatistics {
   virtual std::shared_ptr<BaseColumnStatistics> clone() const = 0;
 
   /**
-   * Adjust non-null value ratio of a column after a left/right/full outer join.
+   * Adjust null value ratio of a column after a left/right/full outer join.
    */
-  void set_non_null_value_ratio(const float non_null_value_ratio) { _non_null_value_ratio = non_null_value_ratio; }
-
   void set_null_value_ratio(const float null_value_ratio) { _non_null_value_ratio = 1.f - null_value_ratio; }
 
-  float non_null_value_ratio() const { return _non_null_value_ratio; }
-
+  /**
+   * Gets null value ratio of a column for calculation of null values for left/right/full outer join.
+   */
   float null_value_ratio() const { return 1.f - _non_null_value_ratio; }
 
  protected:
+  /**
+   * Column statistics uses the non-null value ratio for calculation of selectivity.
+   */
   float _non_null_value_ratio;
 
   /**
