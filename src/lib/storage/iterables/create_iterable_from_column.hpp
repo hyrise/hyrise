55f62b770a381a27884f71cd0270c98cd859f8e8
Max Jendruk <code@jendruk.com>
2017-09-05 13:44:54 +0200

    Add comment to create_iterable_from_column.hpp

--- src/lib/storage/iterables/create_iterable_from_column.hpp
+++ src/lib/storage/iterables/create_iterable_from_column.hpp
@@ -6,6 +6,15 @@
 
 namespace opossum {
 
+/**
+ * @defgroup Uniform interface to create an iterable from a column
+ *
+ * These methods cannot be part of the columns’ interfaces because
+ * reference column are not templated and thus don’t know their type.
+ *
+ * @{
+ */
+
 template <typename T>
 auto create_iterable_from_column(const ValueColumn<T>& column) {
   return ValueColumnIterable<T>{column};
@@ -20,5 +29,7 @@ template <typename T>
 auto create_iterable_from_column(const ReferenceColumn& column) {
   return ReferenceColumnIterable<T>{column};
 }
+
+/**@}*/
 
 }  // namespace opossum
