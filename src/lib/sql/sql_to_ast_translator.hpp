4c14df5283cc0802dc8c9c34c0cf46e515ae62b4
Timo Djürken <timo.djuerken@gmail.com>
2017-09-27 12:39:22 +0200

    Revert "Translator: HAVING w/o functions in SELECT list"
    This reverts commit 502b5e891991b9ee09b15820ec72a1e4239f701f.

--- src/lib/sql/sql_to_ast_translator.hpp
+++ src/lib/sql/sql_to_ast_translator.hpp
@@ -109,9 +109,6 @@ class SQLToASTTranslator final : public boost::noncopyable {
 
   std::shared_ptr<AbstractASTNode> _translate_show(const hsql::ShowStatement& show_statement);
 
-  std::vector<std::shared_ptr<Expression>> _retrieve_having_aggregates(
-      const hsql::Expr& expr, const std::shared_ptr<AbstractASTNode>& input_node);
-
  private:
   SQLToASTTranslator() = default;
 };
