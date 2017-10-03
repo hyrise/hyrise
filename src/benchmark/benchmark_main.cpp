d40126aa43fed0d45416fe07fe95333d4f401d8d
mrks <markus@dreseler.de>
2017-10-02 19:12:06 -0700

    Revert "Fix benchmarks crashing on exit (#295)" (#298)
    This reverts commit f8bdd56e0d550a36e817010f23ed99da55920df5.

--- src/benchmark/benchmark_main.cpp
+++ src/benchmark/benchmark_main.cpp
@@ -1,15 +1,2 @@
 #include <benchmark/benchmark.h>
-
-int main(int argc, char** argv) {
-  // mostly taken from the BENCHMARK_MAIN macro
-  ::benchmark::Initialize(&argc, argv);
-  if (::benchmark::ReportUnrecognizedArguments(argc, argv)) return 1;
-
-  ::benchmark::RunSpecifiedBenchmarks();
-
-  /*
-   * Cleanup benchmarks right away so we dont get into race conditions
-   * with the NUMA memory resources.
-   */
-  ::benchmark::ClearRegisteredBenchmarks();
-}
+BENCHMARK_MAIN();
