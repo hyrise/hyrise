94104cd3ca3c4fd169ab82eb013697d2152269c7
Moritz Eyssen <Moritz.Eyssen@student.hpi.de>
2017-10-09 13:34:36 +0200

    Make lint happy

--- src/lib/optimizer/abstract_syntax_tree/join_node.cpp
+++ src/lib/optimizer/abstract_syntax_tree/join_node.cpp
@@ -113,7 +113,8 @@ std::shared_ptr<TableStatistics> JoinNode::derive_statistics_from(
   if (_join_mode == JoinMode::Cross) {
     return left_child->get_statistics()->generate_cross_join_statistics(right_child->get_statistics());
   } else {
-    Assert(_join_column_ids, "Only cross joins and joins with join column ids supported for generating join statistics");
+    Assert(_join_column_ids,
+           "Only cross joins and joins with join column ids supported for generating join statistics");
     Assert(_scan_type, "Only cross joins and joins with scan type supported for generating join statistics");
     return left_child->get_statistics()->generate_predicated_join_statistics(
       right_child->get_statistics(), _join_mode, *_join_column_ids, *_scan_type);
