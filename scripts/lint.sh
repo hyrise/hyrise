39ac7ab95df48f79b3b840b4e0d53fc21071fb7e
Markus Dreseler <markus.dreseler@hpi.de>
2017-10-10 16:40:32 +0200

    Make ./scripts/lint.sh run in parallel

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,3 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-python2.7 cpplint.py --verbose=0 --extensions=hpp,cpp --counting=detailed --filter=-legal/copyright,-whitespace/newline,-runtime/references,-build/c++11 --linelength=120 $(find src -iname "*.cpp" -o -iname "*.hpp")
+find src -iname "*.cpp" -o -iname "*.hpp" -print0 | parallel --null --no-notice -j 50% python2.7 cpplint.py --verbose=0 --extensions=hpp,cpp --counting=detailed --filter=-legal/copyright,-whitespace/newline,-runtime/references,-build/c++11 --linelength=120 {} 2\>\&1 \| grep -v \'\^Done processing\' \| grep -v \'\^Total errors found: 0\' \; test \${PIPESTATUS[0]} -eq 0
+
+# Conceptual: find | parallel python cpplint \| grep -v \| test \${PIPESTATUS[0]} -eq 0
+#             ^      ^       |^                 ^          ^                           |
+#             |      |        +--------------------runs in parallel---------------------+
+#             |      |        |                 |          |
+#             |      |        |                 |          Get the return code of the first pipeline item (here: cpplint)
+#             |      |        |                 Removes the prints for files without errors
+#             |      |        Regular call of cpplint with options
+#             |      Runs the following in parallel, using 50% of the available cores
+#             Finds all .cpp and .hpp files, separated by \0
--- src/benchmark/base_fixture.hpp
+++ src/benchmark/base_fixture.hpp
@@ -19,6 +19,11 @@ class BenchmarkBasicFixture : public benchmark::Fixture {
  protected:
   void clear_cache();
 
+
+
+
+
+
  protected:
   std::shared_ptr<TableWrapper> _table_wrapper_a;
   std::shared_ptr<TableWrapper> _table_wrapper_b;
@@ -26,4 +31,4 @@ class BenchmarkBasicFixture : public benchmark::Fixture {
   ChunkID _chunk_size;
 };
 
-}  // namespace opossum
+}
