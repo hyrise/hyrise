5fb067cfef76822603f5f1701de1fafe9d53be28
Jan Kossmann <kossmann.jan@gmail.com>
2017-10-20 17:11:12 +0200

    Add tests for recreation

---
+++ src/test/operators/recreation_test.cpp
@@ -0,0 +1,73 @@
+#include "../base_test.hpp"
+#include "gtest/gtest.h"
+
+#include "operators/join_hash.hpp"
+#include "operators/join_nested_loop_a.hpp"
+#include "operators/join_nested_loop_b.hpp"
+#include "operators/join_sort_merge.hpp"
+#include "operators/table_scan.hpp"
+#include "operators/table_wrapper.hpp"
+#include "storage/table.hpp"
+#include "utils/load_table.hpp"
+#include "types.hpp"
+
+namespace opossum {
+
+class RecreationTest : public BaseTest {};
+
+template <typename T>
+class RecreationTestJoin : public RecreationTest {};
+
+// here we define all Join types
+using JoinTypes = ::testing::Types<JoinNestedLoopA, JoinNestedLoopB, JoinHash, JoinSortMerge>;
+TYPED_TEST_CASE(RecreationTestJoin, JoinTypes);
+
+
+TYPED_TEST(RecreationTestJoin, RecreationJoin) {
+  auto table_wrapper_a = std::make_shared<TableWrapper>(load_table("src/test/tables/int_float.tbl", 2));
+  auto table_wrapper_b = std::make_shared<TableWrapper>(load_table("src/test/tables/int_float2.tbl", 2));
+  table_wrapper_a->execute();
+  table_wrapper_b->execute();
+
+  std::shared_ptr<Table> expected_result = load_table("src/test/tables/joinoperators/int_left_join.tbl", 1);
+  EXPECT_NE(expected_result, nullptr) << "Could not load expected result table";
+
+  // build and execute join
+  auto join = std::make_shared<TypeParam>(table_wrapper_a, table_wrapper_b, JoinMode::Left, std::pair<ColumnID, ColumnID>(ColumnID{0}, ColumnID{0}), ScanType::OpEquals);
+  EXPECT_NE(join, nullptr) << "Could not build Join";
+  join->execute();
+  this->EXPECT_TABLE_EQ(join->get_output(), expected_result);
+
+  // recreate and execute recreated join
+  auto recreated_join = join->recreate();
+  EXPECT_NE(recreated_join, nullptr) << "Could not recreate Join";
+
+  // table wrappers need to be executed manually
+  recreated_join->mutable_input_left()->execute();
+  recreated_join->mutable_input_right()->execute();
+  recreated_join->execute();
+  this->EXPECT_TABLE_EQ(recreated_join->get_output(), expected_result);
+}
+
+TEST_F(RecreationTest, RecreationTableScan) {
+  auto table_wrapper_a = std::make_shared<TableWrapper>(load_table("src/test/tables/int_float.tbl", 2));
+  table_wrapper_a->execute();
+  std::shared_ptr<Table> expected_result = load_table("src/test/tables/int_float_filtered2.tbl", 1);
+
+  auto scan = std::make_shared<TableScan>(table_wrapper_a, ColumnID{0}, ScanType::OpGreaterThanEquals, 1234);
+  scan->execute();
+
+  EXPECT_TABLE_EQ(scan->get_output(), expected_result);
+
+
+  // recreate and execute recreated join
+  auto recreated_scan = scan->recreate();
+  EXPECT_NE(recreated_scan, nullptr) << "Could not recreate Scan";
+
+  // table wrappers need to be executed manually
+  recreated_scan->mutable_input_left()->execute();
+  recreated_scan->execute();
+  EXPECT_TABLE_EQ(recreated_scan->get_output(), expected_result);
+}
+
+}  // namespace opossum
