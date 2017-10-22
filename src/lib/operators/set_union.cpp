ce032c4979ebada596684bcbf4181e495f1edc94
Moritz Eyssen <Moritz.Eyssen@student.hpi.de>
2017-10-22 14:56:56 +0200

    Lots more benchmarky stuff

--- src/lib/operators/set_union.cpp
+++ src/lib/operators/set_union.cpp
@@ -1,6 +1,8 @@
 #include "set_union.hpp"
 
 #include <algorithm>
+#include <chrono>
+#include <numeric>
 #include <memory>
 #include <string>
 #include <utility>
@@ -29,15 +31,71 @@
 namespace {
 
 // See doc above
-using ColumnSegmentsRow = std::vector<opossum::RowID>;
+using MultiPosList = std::vector<opossum::PosList>;
 
-bool column_segments_row_cmp(const ColumnSegmentsRow& lhs, ColumnSegmentsRow& rhs) {
-  for (size_t segment_id = 0; segment_id < lhs.size(); ++segment_id) {
-    if (lhs[segment_id] < rhs[segment_id]) return true;
-    if (rhs[segment_id] < lhs[segment_id]) return false;
+enum class InputSide : uint8_t {
+  Left, Right
+};
+
+struct VirtualPosListEntry {
+  InputSide side;
+  size_t index;
+};
+
+using VirtualPosList = std::vector<VirtualPosListEntry>;
+
+struct MultiPosListContext {
+  MultiPosList &left;
+  MultiPosList &right;
+  bool operator()(const VirtualPosListEntry& lhs, const VirtualPosListEntry& rhs) const {
+    const auto & lhs_mpl = lhs.side == InputSide::Left ? left : right;
+    const auto & rhs_mpl = rhs.side == InputSide::Left ? left : right;
+
+    for (size_t segment_id = 0; segment_id < left.size(); ++segment_id) {
+      if (lhs_mpl[segment_id][lhs.index] < rhs_mpl[segment_id][rhs.index]) return true;
+      if (rhs_mpl[segment_id][rhs.index] < lhs_mpl[segment_id][lhs.index]) return false;
+    }
+    return false;
   }
-  return false;
-}
+};
+
+struct VirtualPosListContext {
+  MultiPosList &multi_pos_list;
+  bool operator()(const VirtualPosListEntry& lhs, const VirtualPosListEntry& rhs) const {
+    for (size_t segment_id = 0; segment_id < multi_pos_list.size(); ++segment_id) {
+      const auto& segment_pos_list = multi_pos_list[segment_id];
+      const auto left_row_id = segment_pos_list[lhs.index];
+      const auto right_row_id = segment_pos_list[rhs.index];
+
+      if (left_row_id < right_row_id) return true;
+      if (right_row_id < left_row_id) return false;
+    }
+    return false;
+  }
+};
+
+//void print_multi_pos_list(const MultiPosList& mpl) {
+//  std::cout << "MultiPosList " << mpl.size() << "x" << mpl[0].size() << std::endl;
+//  for (size_t y = 0; y < mpl[0].size(); y++) {
+//    for (size_t x = 0; x < mpl.size(); x++) {
+//      std::cout << mpl[x][y].chunk_id << ":" << mpl[x][y].chunk_offset;
+//      if (x + 1 < mpl.size()) std::cout << ",";
+//    }
+//    std::cout << std::endl;
+//  }
+//}
+//
+//void print_virtual_pos_list(const VirtualPosList& vpl, const MultiPosListContext& context) {
+//  for (size_t y = 0; y < vpl.size(); y++) {
+//    std::cout << vpl[y].index << " " << (vpl[y].side == InputSide::Left ?"l":"r") << "|";
+////
+////    for (size_t segment_id = 0; segment_id < context.left.size(); ++segment_id) {
+////      std::cout <<
+////    }
+//    std::cout << std::endl;
+//  }
+//}
+
 }  // namespace
 
 namespace opossum {
@@ -64,47 +122,88 @@ std::shared_ptr<const Table> SetUnion::_on_execute() {
     return early_result;
   }
 
+  auto input_build_begin = std::chrono::high_resolution_clock::now();
   /**
-   * For each input, create the ColumnSegmentsRows
+   * For each input, create a MultiPosList
    */
-  const auto add_column_segments_from_input_table = [&](const auto& input_table, auto& out_column_segments) {
+  const auto build_multi_pos_list = [&](const auto& input_table, auto& multi_pos_list) {
+    multi_pos_list.resize(_column_segment_begins.size());
+    for (auto & pos_list : multi_pos_list) {
+      pos_list.reserve(input_table->row_count());
+    }
+
     for (auto chunk_id = ChunkID{0}; chunk_id < input_table->chunk_count(); ++chunk_id) {
       const auto& chunk = input_table->get_chunk(ChunkID{chunk_id});
 
-      for (auto chunk_offset = ChunkOffset{0}; chunk_offset < chunk.size(); ++chunk_offset) {
-        ColumnSegmentsRow row;
+      for (size_t segment_id = 0; segment_id < _column_segment_begins.size(); ++segment_id) {
+        const auto column_id = _column_segment_begins[segment_id];
+        const auto column = chunk.get_column(column_id);
+        const auto ref_column = std::dynamic_pointer_cast<const ReferenceColumn>(column);
 
-        for (auto column_id : _column_segment_begins) {
-          const auto column = chunk.get_column(column_id);
-          const auto ref_column = std::dynamic_pointer_cast<const ReferenceColumn>(column);
-          row.emplace_back((*ref_column->pos_list())[chunk_offset]);
-        }
-
-        out_column_segments.emplace_back(row);
+        auto& out_pos_list = multi_pos_list[segment_id];
+        auto in_pos_list = ref_column->pos_list();
+        std::copy(in_pos_list->begin(), in_pos_list->end(), std::back_inserter(out_pos_list));
       }
     }
   };
 
-  std::vector<ColumnSegmentsRow> column_segments_left;
-  column_segments_left.reserve(_input_table_left()->row_count());
-  std::vector<ColumnSegmentsRow> column_segments_right;
-  column_segments_right.reserve(_input_table_right()->row_count());
+  MultiPosList multi_pos_list_left;
+  MultiPosList multi_pos_list_right;
 
-  add_column_segments_from_input_table(_input_table_left(), column_segments_left);
-  add_column_segments_from_input_table(_input_table_right(), column_segments_right);
+  build_multi_pos_list(_input_table_left(), multi_pos_list_left);
+  build_multi_pos_list(_input_table_right(), multi_pos_list_right);
+
+  auto input_build_begin2 = std::chrono::high_resolution_clock::now();
+//  std::cout << "MultiPosListLeft:" << std::endl;
+//  print_multi_pos_list(multi_pos_list_left);
+//  std::cout << "MultiPosListRight:" << std::endl;
+//  print_multi_pos_list(multi_pos_list_right);
+
+  /**
+   * Init the virtual pos lists
+   */
+  const auto init_virtual_pos_list = [] (const auto &table, InputSide side) {
+    VirtualPosList virtual_pos_list;
+    virtual_pos_list.reserve(table->row_count());
+
+    for (size_t row_idx = 0; row_idx < table->row_count(); ++row_idx) {
+      virtual_pos_list.emplace_back(VirtualPosListEntry{side, row_idx});
+    }
+
+    return virtual_pos_list;
+  };
+
+  auto virtual_pos_list_left = init_virtual_pos_list(_input_table_left(), InputSide::Left);
+  auto virtual_pos_list_right = init_virtual_pos_list(_input_table_right(), InputSide::Right);
 
   /**
    * This is where the magic happens:
-   * Compute the actual union by sorting the ColumnSegmentsRows and then merging them using std::set_union
+   * Compute the actual union by sorting the virtual pos lists and then merging them using std::set_union
    */
-  std::sort(column_segments_left.begin(), column_segments_left.end(), column_segments_row_cmp);
-  std::sort(column_segments_right.begin(), column_segments_right.end(), column_segments_row_cmp);
+  MultiPosListContext context{multi_pos_list_left, multi_pos_list_right};
 
-  std::vector<ColumnSegmentsRow> merged_rows;
-  merged_rows.reserve(_input_table_left()->row_count() + _input_table_right()->row_count());
+  auto sort_begin = std::chrono::high_resolution_clock::now();
+  std::sort(virtual_pos_list_left.begin(), virtual_pos_list_left.end(), VirtualPosListContext{multi_pos_list_left});
+  std::sort(virtual_pos_list_right.begin(), virtual_pos_list_right.end(), VirtualPosListContext{multi_pos_list_right});
 
-  std::set_union(column_segments_left.begin(), column_segments_left.end(), column_segments_right.begin(),
-                 column_segments_right.end(), std::back_inserter(merged_rows));
+  auto sort_end = std::chrono::high_resolution_clock::now();
+//  std::cout << "VirtualPosListLeft" << std::endl;
+//  print_virtual_pos_list(virtual_pos_list_left, context);
+//
+//  std::cout << "VirtualPosListRight" << std::endl;
+//  print_virtual_pos_list(virtual_pos_list_right, context);
+
+  VirtualPosList merged_rows;
+  // Min of both row counts is safe to reserve
+  merged_rows.reserve(std::min(_input_table_left()->row_count(), _input_table_right()->row_count()));
+
+  std::set_union(virtual_pos_list_left.begin(), virtual_pos_list_left.end(), virtual_pos_list_right.begin(),
+                 virtual_pos_list_right.end(), std::back_inserter(merged_rows), context);
+  auto union_end = std::chrono::high_resolution_clock::now();
+
+
+//  std::cout << "VirtualPosListMerged" << std::endl;
+//  print_virtual_pos_list(merged_rows, context);
 
   /**
    * Build result table
@@ -123,7 +222,15 @@ std::shared_ptr<const Table> SetUnion::_on_execute() {
       auto segment_pos_list = std::make_shared<PosList>();
       segment_pos_list->reserve(num_rows_per_chunk);
       for (auto chunk_offset = ChunkOffset{0}; chunk_offset < num_rows_per_chunk; ++chunk_offset) {
-        segment_pos_list->emplace_back(merged_rows[row_idx + chunk_offset][segment_id]);
+        auto& virtual_pos_list_entry = merged_rows[row_idx + chunk_offset];
+        RowID row_id;
+        if (virtual_pos_list_entry.side == InputSide::Left) {
+          row_id = multi_pos_list_left[segment_id][virtual_pos_list_entry.index];
+        } else {
+          row_id = multi_pos_list_right[segment_id][virtual_pos_list_entry.index];
+        }
+
+        segment_pos_list->emplace_back(row_id);
       }
 
       /**
@@ -142,6 +249,13 @@ std::shared_ptr<const Table> SetUnion::_on_execute() {
 
     out_table->emplace_chunk(std::move(out_chunk));
   }
+
+  auto generation_end = std::chrono::high_resolution_clock::now();
+  std::cout << "Input0: " << std::chrono::duration_cast<std::chrono::milliseconds>(input_build_begin2 - input_build_begin).count() << std::endl;
+  std::cout << "Input1: " << std::chrono::duration_cast<std::chrono::milliseconds>(sort_begin - input_build_begin2).count() << std::endl;
+  std::cout << "Sort: " << std::chrono::duration_cast<std::chrono::milliseconds>(sort_end - sort_begin).count() << std::endl;
+  std::cout << "Union: " << std::chrono::duration_cast<std::chrono::milliseconds>(union_end - sort_end).count() << std::endl;
+  std::cout << "Generation: " << std::chrono::duration_cast<std::chrono::milliseconds>(generation_end - union_end).count() << std::endl;
 
   return out_table;
 }
