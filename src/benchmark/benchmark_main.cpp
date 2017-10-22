7adedb35f1d144ca76384565c7265b839fcff081
Moritz Eyssen <Moritz.Eyssen@student.hpi.de>
2017-10-22 12:33:56 +0200

    set union benchmark running

--- src/benchmark/benchmark_main.cpp
+++ src/benchmark/benchmark_main.cpp
@@ -1,2 +1,15 @@
 #include <benchmark/benchmark.h>
-BENCHMARK_MAIN()
+
+int main(int argc, char** argv) {
+  // mostly taken from the BENCHMARK_MAIN macro
+  ::benchmark::Initialize(&argc, argv);
+  if (::benchmark::ReportUnrecognizedArguments(argc, argv)) return 1;
+
+  ::benchmark::RunSpecifiedBenchmarks();
+
+  /*
+   * Cleanup benchmarks right away so we dont get into race conditions
+   * with the NUMA memory resources.
+   */
+  ::benchmark::ClearRegisteredBenchmarks();
+}
