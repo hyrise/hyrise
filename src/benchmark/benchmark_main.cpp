f3fccd6603698634fafc41a18be3e5f2140b5709
Moritz Eyssen <Moritz.Eyssen@student.hpi.de>
2017-10-25 18:32:01 +0200

    tmp

--- src/benchmark/benchmark_main.cpp
+++ src/benchmark/benchmark_main.cpp
@@ -1,2 +1,19 @@
 #include <benchmark/benchmark.h>
-BENCHMARK_MAIN()
+
+/**
+ * We don't use BENCHMARK_MAIN() - and here is why:
+ *
+ * The BENCHMARK*() macros generate
+ */
+
+
+int main(int argc, char** argv) {
+  // BENCHMARK_MAIN begin
+  ::benchmark::Initialize(&argc, argv);
+  if (::benchmark::ReportUnrecognizedArguments(argc, argv)) return 1;
+  ::benchmark::RunSpecifiedBenchmarks();
+  // BENCHMARK_MAIN end
+
+  // Destroy all benchmark::Fixture objects
+  ::benchmark::ClearRegisteredBenchmarks();
+}
--- src/bin/playground.cpp
+++ src/bin/playground.cpp
@@ -1,7 +1,26 @@
-
 #include <iostream>
 
+#include "operators/print.hpp"
+#include "storage/chunk.hpp"
+#include "storage/table.hpp"
+#include "storage/storage_manager.hpp"
+#include "storage/value_column.hpp"
+#include "types.hpp"
+
 int main() {
-  std::cout << "Hello world!" << std::endl;
+  if (!opossum::StorageManager::get().has_table("test")) {
+    opossum::pmr_concurrent_vector<int32_t> values({1, 2, 3});
+
+    opossum::Chunk chunk(true);
+    chunk.add_column(std::make_shared<opossum::ValueColumn<int32_t>>(std::move(values)));
+
+    auto test_table = std::make_shared<opossum::Table>();
+    test_table->add_column_definition("a", "int");
+    test_table->emplace_chunk(std::move(chunk));
+
+    opossum::StorageManager::get().add_table("test", test_table);
+    opossum::Print::print(test_table);
+  }
+
   return 0;
 }
