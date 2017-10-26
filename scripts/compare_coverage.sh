796fb1bb6edaaf17734dfd517c048d74074d90b5
mrks <markus@dreseler.de>
2017-10-26 19:45:13 +0200

    Reduce scope of coverage (#431)
    * Removes benchmark(lib) folder from coverage
    
    * don't check third_party either
    
    * Remove protobuf exclusion
    
    * Does this fix formatting?
    
    * revert meta hack
    
    * Link to coverage report
    
    * formatting

--- scripts/compare_coverage.sh
+++ scripts/compare_coverage.sh
@@ -3,7 +3,6 @@
 master_branch=$(curl https://ares.epic.hpi.uni-potsdam.de/jenkins/job/Hyrise/job/hyrise/job/master/lastStableBuild/artifact/coverage_percent.txt)
 this_branch=$(cat coverage_percent.txt)
 
-echo -n 'Compared to master, coverage '
 if (( $(bc <<< "$master_branch < $this_branch") ))
 then
 echo -n increased by $(bc <<< "$this_branch - $master_branch")
@@ -14,4 +13,4 @@ else
 echo -n decreased by $(bc <<< "$master_branch - $this_branch")
 fi
 
-echo " and is now $this_branch%"
+echo " compared to master and is now $this_branch%"
