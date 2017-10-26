5725f3df42a2be4dee69b557117c5492259e5226
Jan Kossmann <kossmann.jan@gmail.com>
2017-10-26 13:02:09 +0200

    Fix

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -6,12 +6,14 @@ if [ -z "$1" ]
     exit 1
 fi
 
+./$1/hyriseCoverage && rm -fr coverage; mkdir coverage && gcovr -s -r . --exclude="(.*types*.|.*test*.|.*\.pb\.|third_party)" --html --html-details -o coverage/index.html > coverage_output.txt
+
 # without coverage badge generation
 if [ -z "$2" ]
   then
-    ./$1/hyriseCoverage && rm -fr coverage; mkdir coverage && gcovr -s -r . --exclude="(.*types*.|.*test*.|.*\.pb\.|third_party)" --html --html-details -o coverage/index.html
+    cat coverage_output.txt
 else
-    coverage_percent=$(./$1/hyriseCoverage && rm -fr coverage; mkdir coverage && gcovr -s -r . --exclude="(.*types*.|.*test*.|.*\.pb\.|third_party)" --html --html-details -o coverage/index.html | grep lines: | sed -e 's/lines: //; s/% .*$//')
+    coverage_percent=$(cat coverage_output.txt | grep lines: | sed -e 's/lines: //; s/% .*$//')
     echo $coverage_percent
     if (( $(bc <<< "$coverage_percent >= 90") ))
     then
@@ -25,6 +27,7 @@ else
     fi
 
     url="https://img.shields.io/badge/Coverage-$coverage_percent-$color.svg"
-    echo $url
     curl -g -o coverage_badge.svg $url
 fi
+
+rm coverage_output.txt
