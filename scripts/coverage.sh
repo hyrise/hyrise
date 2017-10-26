9a47e2d8390e13862a62e21dc8d18891d72703f5
Markus Dreseler <markus.dreseler@hpi.de>
2017-10-26 19:21:35 +0200

    revert meta hack

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -8,11 +8,6 @@ fi
 
 ./$1/hyriseCoverage && rm -fr coverage; mkdir coverage && gcovr -s -r . --exclude="(.*types*.|.*test*.|third_party/*|src/benchmark*)" --html --html-details -o coverage/index.html > coverage_output.txt
 
-# add meta so that the reverse proxy doesn't break formatting
-echo "<meta http-equiv=\"Content-Security-Policy\" content=\"default-src *; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval'\">" > coverate/index_fixed.html
-cat coverage/index.html >> coverage/index_fixed.html 
-mv coverage/index_fixed.html coverage/index.html
-
 # without coverage badge generation
 if [ -z "$2" ]
   then
