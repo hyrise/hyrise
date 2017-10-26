fb6c56498bc55735f7a2ff678a6d1aa2ae6bc6b0
Markus Dreseler <markus.dreseler@hpi.de>
2017-10-26 17:56:50 +0200

    Make coverage text shorter

--- scripts/compare_coverage.sh
+++ scripts/compare_coverage.sh
@@ -3,15 +3,15 @@
 master_branch=$(curl https://ares.epic.hpi.uni-potsdam.de/jenkins/job/Hyrise/job/hyrise/job/master/lastStableBuild/artifact/coverage_percent.txt)
 this_branch=$(cat coverage_percent.txt)
 
-echo -n 'Compared to master, coverage '
+echo -n 'Compared to master, '
 if (( $(bc <<< "$master_branch < $this_branch") ))
 then
-echo -n increased by $(bc <<< "$this_branch - $master_branch")
+echo -n went up by $(bc <<< "$this_branch - $master_branch")%
 elif (( $(bc <<< "$master_branch == $this_branch") ))
 then
 echo -n stayed the same
 else
-echo -n decreased by $(bc <<< "$master_branch - $this_branch")
+echo -n went down by $(bc <<< "$master_branch - $this_branch")%
 fi
 
 echo " and is now $this_branch%"
