4c0479e7f04329f60f57765b52e4956534ccde9d
Moritz Eyssen <mrzesn@t-online.de>
2017-10-26 17:27:42 +0200

    Switch back to explicitly tearing down benchmark fixtures (#416)
    * tmp
    
    * Switch back to explicitly tearing down benchmarks
    
    * revert some changes
    
    * extend comment

--- src/lib/utils/boost_default_memory_resource.cpp
+++ src/lib/utils/boost_default_memory_resource.cpp
@@ -17,16 +17,19 @@ class default_resource_impl : public memory_resource {
   virtual void do_deallocate(void* p, std::size_t bytes, std::size_t alignment) { std::free(p); }
 
   virtual bool do_is_equal(const memory_resource& other) const BOOST_NOEXCEPT { return &other == this; }
-} default_resource_instance;
+};
 
-memory_resource* new_delete_resource() BOOST_NOEXCEPT { return &default_resource_instance; }
-
-memory_resource* set_default_resource(memory_resource* r) BOOST_NOEXCEPT {
-  // Do nothing
+memory_resource* get_default_resource() BOOST_NOEXCEPT {
+  static default_resource_impl default_resource_instance;
   return &default_resource_instance;
 }
 
-memory_resource* get_default_resource() BOOST_NOEXCEPT { return &default_resource_instance; }
+memory_resource* new_delete_resource() BOOST_NOEXCEPT { return get_default_resource(); }
+
+memory_resource* set_default_resource(memory_resource* r) BOOST_NOEXCEPT {
+  // Do nothing
+  return get_default_resource();
+}
 
 }  // namespace pmr
 }  // namespace container
