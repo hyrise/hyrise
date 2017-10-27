895cd6c0d59a85b78beb17cbd63413a8dc4bb750
Markus Dreseler <markus.dreseler@hpi.de>
2017-10-27 14:53:41 +0200

    remove root

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -6,7 +6,7 @@ if [ -z "$1" ]
     exit 1
 fi
 
-./$1/hyriseCoverage && rm -fr coverage; mkdir coverage && gcovr --gcov-executable="gcov -s `pwd`" -s -p -r . --exclude='.*/(?:third_party|src/test|src/benchmark).*' --exclude-unreachable-branches --html --html-details -o coverage/index.html > coverage_output.txt
+./$1/hyriseCoverage && rm -fr coverage; mkdir coverage && gcovr --gcov-executable="gcov -s `pwd`" -s -p --exclude='.*/(?:third_party|src/test|src/benchmark).*' --exclude-unreachable-branches --html --html-details -o coverage/index.html > coverage_output.txt
 cat coverage_output.txt
 
 # without coverage badge generation
