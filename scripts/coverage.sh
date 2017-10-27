aac22d810d5b8e25d2c7692397e70416931d8829
Markus Dreseler <markus.dreseler@hpi.de>
2017-10-27 13:12:11 +0200

    agaaaaain

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -6,7 +6,7 @@ if [ -z "$1" ]
     exit 1
 fi
 
-./$1/hyriseCoverage && rm -fr coverage; mkdir coverage && gcovr $1 --verbose -s -r . --exclude="(.*types*.|.*test*.|third_party/*|src/benchmark*)" --html --html-details -o coverage/index.html > coverage_output.txt
+./$1/hyriseCoverage && rm -fr coverage; mkdir coverage && gcovr $1 --gcov-executable="gcov -x" --verbose -s -r . --exclude="(.*types*.|.*test*.|third_party/*|src/benchmark*)" --html --html-details -o coverage/index.html > coverage_output.txt
 cat coverage_output.txt
 
 # without coverage badge generation
