37ea61f971b34c81fb919f8864bf6f25b2c341c7
Markus Dreseler <markus.dreseler@hpi.de>
2017-10-27 15:07:07 +0200

    bla

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -9,9 +9,9 @@ fi
 ./$1/hyriseCoverage && rm -fr coverage; mkdir coverage && gcovr --gcov-executable="gcov -s `pwd`" -s -p --exclude='.*/(?:third_party|src/test|src/benchmark).*' --exclude-unreachable-branches --html --html-details -o coverage/index.html > coverage_output.txt
 cat coverage_output.txt
 
-# without coverage badge generation
 if [ ! -z "$2" ]
 then
+    # coverage badge generation
     coverage_percent=$(cat coverage_output.txt | grep lines: | sed -e 's/lines: //; s/% .*$//')
     echo $coverage_percent > coverage_percent.txt
     if (( $(bc <<< "$coverage_percent >= 90") ))
