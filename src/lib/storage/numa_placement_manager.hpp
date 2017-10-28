10aa580696a578fe1979657a62607e61de7254c3
Norman Rzepka <norman.rzepka@student.hpi.de>
2017-10-28 15:32:35 +0200

    adds comments to NUMAPlacementManager::Options

--- src/lib/storage/numa_placement_manager.hpp
+++ src/lib/storage/numa_placement_manager.hpp
@@ -16,14 +16,38 @@ namespace opossum {
 
 // The NUMAPlacementManager is a singleton that maintains the NUMA-aware allocators
 // and triggers the NUMA-aware chunk migration tasks
+// The NUMAPlacementManager is initialized in a paused state and needs to be 
+// `resumed` to start its operation.
 class NUMAPlacementManager {
  public:
   struct Options {
-    // TODO(normanrz): Comment where these numbers come from and what they mean
+    // Parameters of the NUMA placement and chunk migration algorithm.
+    // These parameters have been obtained from running experiments with dynamic
+    // workloads in the database. The workloads in the experiments changed after
+    // 2min 30s. These parameters should be adjusted for when workloads change 
+    // more or less frequently.
+
+    // The time interval at which a snaphsot of the current access time counters
+    // of all stored chunks are preserved. These access times are the basis to 
+    // detect load imbalances between the NUMA nodes. This procedure is run in a loop. 
     std::chrono::milliseconds counter_history_interval = std::chrono::milliseconds(100);
+
+    // The time interval at which the migration routine (MigrationPreparationTask) 
+    // is scheduled in a loop.
+    // Should be much higher than counter_history_interval
     std::chrono::milliseconds migration_interval = std::chrono::seconds(10);
+
+    // The time interval of what historical chunk access times are considered 
+    // for determining the load imbalance between the NUMA nodes.
+    // Should be set less than 100 * counter_history_interval and less than
+    // migration_interval
     std::chrono::milliseconds counter_history_range = std::chrono::seconds(7);
+
+    // Number of chunks that are migrated per loop iteration
     size_t migration_count = 3;
+
+    // The threshold for the load imbalance metric between the NUMA nodes.
+    // If the imbalance is less than this threshold, no chunks are migrated.
     double imbalance_threshold = 0.1;
   };
 
