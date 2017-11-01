c3200d0f5fdf984ca7966c405c77b0dbe0eab497
Leander Neiss <leander.neiss@student.hpi.de>
2017-11-01 06:57:38 +0100

    Add CsvMeta (replacing CsvConfig), containg all meta information for Csv.

---
+++ src/lib/import_export/csv_meta.cpp
@@ -0,0 +1,94 @@
+#include "csv_meta.hpp"
+
+#include <fstream>
+
+#include "utils/assert.hpp"
+
+namespace opossum {
+
+CsvMeta process_csv_meta_file(const std::string& filename) {
+  std::ifstream metafile{filename};
+  Assert(metafile.good(), "Meta file does not exist: " + filename);
+  nlohmann::json meta_json;
+  metafile >> meta_json;
+  CsvMeta meta = meta_json;
+  return meta;
+}
+
+void assign_if_exists(char &value, const nlohmann::json& json_object, const std::string &key) {
+  if (json_object.find(key) != json_object.end()) {
+    std::string character = json_object.at(key);
+    Assert(character.size() == 1, "CSV meta file config: Character specifications can only be a single character.");
+    value = character[0];
+  }
+}
+
+void assign_if_exists(bool &value, const nlohmann::json& json_object, const std::string &key) {
+  if (json_object.find(key) != json_object.end()) {
+    value = json_object.at(key);
+  }
+}
+
+void from_json(const nlohmann::json& json, CsvMeta& meta) {
+  meta.chunk_size = json.at("chunk_size");
+
+  // Apply only parts of the ParseConfig that are provided, use default values otherwise
+  ParseConfig config{};
+  if (json.find("config") != json.end()) {
+    Assert(json.at("columns").is_object(), "CSV meta file,\"Config\" field has to be a json object.");
+    nlohmann::json config_json = json.at("config");
+    assign_if_exists(config.delimiter, config_json, "delimiter");
+    assign_if_exists(config.separator, config_json, "separator");
+    assign_if_exists(config.quote, config_json, "quote");
+    assign_if_exists(config.escape, config_json, "escape");
+    assign_if_exists(config.delimiter_escape, config_json, "delimiter_escape");
+    assign_if_exists(config.reject_quoted_nonstrings, config_json, "reject_quoted_nonstrings");
+    assign_if_exists(config.rfc_mode, config_json, "rfc_mode");
+  }
+
+  if (json.find("columns") != json.end()) {
+    Assert(json.at("columns").is_array(), "CSV meta file,\"Columns\" field has to be a json array.");
+    std::vector<ColumnMeta> columns;
+    for (auto column : json.at("columns")) {
+      ColumnMeta column_meta;
+      column_meta.name = column.at("name");
+      column_meta.type = column.at("type");
+      assign_if_exists(column_meta.nullable, column, "nullable");
+      columns.push_back(column_meta);
+    }
+    meta.columns = columns;
+  }
+
+  meta.config = config;
+}
+
+void to_json(nlohmann::json& json, const CsvMeta& meta) {
+  nlohmann::json config = nlohmann::json {
+    {"delimiter", meta.config.delimiter},
+    {"separator", meta.config.separator},
+    {"quote", meta.config.quote},
+    {"escape", meta.config.escape},
+    {"delimiter_escape", meta.config.delimiter_escape},
+    {"reject_quoted_nonstrings", meta.config.reject_quoted_nonstrings},
+    {"rfc_mode", meta.config.rfc_mode}
+  };
+
+  nlohmann::json columns = "[]"_json;
+  for (auto column_meta : meta.columns) {
+    nlohmann::json column_meta_json = nlohmann::json {
+      {"name", column_meta.name},
+      {"type", column_meta.type},
+      {"nullable", column_meta.nullable}
+    };
+    columns.push_back(column_meta_json);
+  }
+
+  json = nlohmann::json {
+    {"chunk_size", meta.chunk_size},
+    {"meta_file_extension", meta.meta_file_extension},
+    {"config", config},
+    {"columns", columns}
+  };
+}
+
+}  // namespace opossum
---
+++ src/lib/import_export/csv_meta.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "json.hpp"
+
+namespace opossum {
+
+struct ColumnMeta {
+  std::string name;
+  std::string type;
+
+  bool nullable = false;
+};
+
+struct ParseConfig {
+  char delimiter = '\n';
+  char separator = ',';
+  char quote = '"';
+  char escape = '"';
+  char delimiter_escape = '\\';
+
+  // If this is set to true, "4.3" will not be accepted as a value for a float column
+  bool reject_quoted_nonstrings = true;
+
+
+  // Indicator whether the Csv follows RFC 4180. (see https://tools.ietf.org/html/rfc4180)
+  bool rfc_mode = true;
+
+  static constexpr const char* NULL_STRING = "null";
+};
+
+/*
+ * Predefined characters used for the csv file
+ */
+
+struct CsvMeta {
+  size_t chunk_size;
+  ParseConfig config;
+  std::vector<ColumnMeta> columns;
+
+  std::string meta_file_extension = ".json";
+};
+
+void from_json(const nlohmann::json& json, CsvMeta& meta);
+void to_json(nlohmann::json& json, const CsvMeta& meta);
+
+CsvMeta process_csv_meta_file(const std::string& filename);
+
+} // namespace opossum
