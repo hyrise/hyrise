318ebd75f730577939b6db910cd0e78ea86ab921
Moritz Eyssen <Moritz.Eyssen@student.hpi.de>
2017-11-04 22:26:21 +0100

    tmop

---
+++ src/lib/planviz/join_graph_visualizer.cpp
@@ -0,0 +1,14 @@
+#include "join_graph_visualizer.hpp"
+
+namespace opossum {
+
+JoinGraphVisualizer::JoinGraphVisualizer(const std::vector <std::shared_ptr<AbstractASTNode>> &ast_roots,
+                                         const std::string &output_prefix, const DotConfig &config) {
+
+}
+
+void JoinGraphVisualizer::visualize() {
+
+}
+
+}
--- src/lib/planviz/join_graph_visualizer.hpp
+++ src/lib/planviz/join_graph_visualizer.hpp
@@ -1,10 +1,26 @@
 #pragma once
 
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "dot_config.hpp"
+
 namespace opossum {
 
-class JoinGraphVisualizer {
+class AbstractASTNode;
 
-  public;:
+class JoinGraphVisualizer {
+ public:
+  JoinGraphVisualizer(const std::vector<std::shared_ptr<AbstractASTNode>>& ast_roots, const std::string& output_prefix,
+                      const DotConfig& config = {});
+
+  void visualize();
+
+ private:
+  const std::vector<std::shared_ptr<AbstractASTNode>> _ast_roots;
+  const std::string _output_prefix;
+  const DotConfig _config;
 
 };
 
