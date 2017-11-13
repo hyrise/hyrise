a8a056d7b240a34cdb5807884f553ee658801fb5
Moritz Eyssen <Moritz.Eyssen@student.hpi.de>
2017-11-13 20:12:47 +0100

    Checkout master playground

--- src/bin/playground.cpp
+++ src/bin/playground.cpp
@@ -1,26 +1,6 @@
 #include <iostream>
-#include <optimizer/abstract_syntax_tree/abstract_ast_node.hpp>
-#include <sql/sql_to_ast_translator.hpp>
-#include <storage/storage_manager.hpp>
-#include <utils/load_table.hpp>
-
-#include "SQLParser.h"
-#include "SQLParserResult.h"
 
 int main() {
-  opossum::StorageManager::get().add_table(
-      "groupby_int_2gb_2agg",
-      opossum::load_table("src/test/tables/aggregateoperator/groupby_int_2gb_2agg/input.tbl", 0));
-
-  auto query =
-      "SELECT a, b, MAX(c), AVG(d) FROM groupby_int_2gb_2agg GROUP BY a, b HAVING b > 457 OR b = 1234 OR b = 12345;";
-
-  hsql::SQLParserResult result;
-  hsql::SQLParser::parse(query, &result);
-
-  auto nodes = opossum::SQLToASTTranslator(false).translate_parse_result(result);
-
-  nodes[0]->print();
-
+  std::cout << "Hello world!" << std::endl;
   return 0;
 }
