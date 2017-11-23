9a68929e1481c9113f122e12ccd73ad7680dd788
Markus Dreseler <markus.dreseler@hpi.de>
2017-11-23 16:54:44 +0100

    Reset Playground

--- src/bin/playground.cpp
+++ src/bin/playground.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
-#include <limits>
-
-#include "types.hpp"
 
 int main() {
-  std::cout << std::numeric_limits<opossum::ChunkID>::min() << std::endl;
-  std::cout << std::numeric_limits<opossum::ChunkID>::max() << std::endl;
+  std::cout << "Hello world!" << std::endl;
   return 0;
 }
