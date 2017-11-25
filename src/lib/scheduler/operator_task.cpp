8c8d437138e8e20dca916b33b944e96614c4589c
Markus Dreseler <markus.dreseler@hpi.de>
2017-11-25 13:47:49 +0100

    early rollback

--- src/lib/scheduler/operator_task.cpp
+++ src/lib/scheduler/operator_task.cpp
@@ -64,6 +64,10 @@ void OperatorTask::_on_execute() {
       case TransactionPhase::Aborted:
       case TransactionPhase::RolledBack:
         // The transaction already failed. No need to execute this.
+        if (auto read_write_operator = std::dynamic_pointer_cast<AbstractReadWriteOperator>(_op)) {
+          // Essentially a noop, because no modifications are recorded yet. Better be on the safe side though.
+          read_write_operator->rollback_records();
+        }
         return;
         break;
 
