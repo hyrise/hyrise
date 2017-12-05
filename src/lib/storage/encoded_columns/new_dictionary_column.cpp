486ab6cf39b973d2fc1a7a3dae4ddf52a7934903
Max Jendruk <code@jendruk.com>
2017-12-05 16:18:47 +0100

    fix compiler error

--- src/lib/storage/encoded_columns/new_dictionary_column.cpp
+++ src/lib/storage/encoded_columns/new_dictionary_column.cpp
@@ -99,8 +99,8 @@ template <typename T>
 std::shared_ptr<BaseColumn> NewDictionaryColumn<T>::copy_using_allocator(
     const PolymorphicAllocator<size_t>& alloc) const {
   auto new_attribute_vector_ptr = _attribute_vector->copy_using_allocator(alloc);
-  auto new_dictionary = std::pmr_vector<T>{*_dictionary, alloc};
-  auto new_dictionary_ptr = std::allocate_shared(alloc, std::move(new_dictionary));
+  auto new_dictionary = pmr_vector<T>{*_dictionary, alloc};
+  auto new_dictionary_ptr = std::allocate_shared<pmr_vector<T>>(alloc, std::move(new_dictionary));
   return std::allocate_shared<NewDictionaryColumn<T>>(alloc, new_dictionary_ptr, new_attribute_vector_ptr, _null_value_id);
 }
 
