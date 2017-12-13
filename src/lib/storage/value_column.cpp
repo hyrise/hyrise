3ec206c7d7cbc3063b56f0f0602bbaf4cdb5e3cf
Max Jendruk <code@jendruk.com>
2017-12-13 18:18:48 +0100

    Fix ValueColumn<T>::data_size

--- src/lib/storage/value_column.cpp
+++ src/lib/storage/value_column.cpp
@@ -143,7 +143,7 @@ template <typename T>
 size_t ValueColumn<T>::data_size() const {
   auto data_size = _values.size() + sizeof(T);
 
-  if (is_nullable()) data_size += _null_values.size() * sizeof(bool);
+  if (is_nullable()) data_size += _null_values->size() * sizeof(bool);
 
   return data_size;
 }
