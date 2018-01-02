73b881af06c3d63f126eecb66f3d808874376a0e
Stephan Detje <stephan.detje@student.hpi.de>
2018-01-02 16:00:42 +0100

    add tests for startupPackage and writeString

--- src/test/server/postgres_wire_handler_test.cpp
+++ src/test/server/postgres_wire_handler_test.cpp
@@ -8,6 +8,7 @@ class PostgresWireHandlerTest : public BaseTest {
  protected:
   PostgresWireHandler postgres_wire_handler;
   InputPacket _input_packet;
+  OutputPacket _output_packet;
 };
 
 TEST_F(PostgresWireHandlerTest, HandleQueryPacketEmpty) {
@@ -15,7 +16,7 @@ TEST_F(PostgresWireHandlerTest, HandleQueryPacketEmpty) {
 
   std::string result = postgres_wire_handler.handle_query_packet(_input_packet, length);
 
-  ASSERT_EQ(result.length(), 0);
+  ASSERT_EQ(result.length(), length);
 }
 
 TEST_F(PostgresWireHandlerTest, HandleQueryPacket) {
@@ -25,7 +26,7 @@ TEST_F(PostgresWireHandlerTest, HandleQueryPacket) {
 
   std::string result = postgres_wire_handler.handle_query_packet(_input_packet, length);
 
-  ASSERT_GT(result.length(), 0);
+  ASSERT_GT(result.length(), 0ul);
   ASSERT_TRUE(result.find("Query") != std::string::npos);
 }
 
@@ -40,6 +41,28 @@ TEST_F(PostgresWireHandlerTest, HandleHeader) {
 
   ASSERT_EQ(command_header.message_type, NetworkMessageType::SimpleQueryCommand);
   // length is 100 - 4 because the 4 bytes used to store the length itself are subtracted
-  ASSERT_EQ(command_header.payload_length, 96);
+  ASSERT_EQ(command_header.payload_length, 96ul);
+}
+
+TEST_F(PostgresWireHandlerTest, HandleStartupPackage) {
+  ByteBuffer buffer = {};
+  uint32_t value = ntohl(100);
+  char* chars = reinterpret_cast<char*>(&value);
+  buffer.insert(buffer.end(), chars, chars + sizeof(uint32_t)); // length
+  buffer.insert(buffer.end(), chars, chars + sizeof(uint32_t)); // version
+  _input_packet.data = buffer;
+
+  uint32_t result = postgres_wire_handler.handle_startup_package(_input_packet);
+  ASSERT_EQ(result, 92ul); // 100 - 2 * sizeof(uint32_t)
+}
+
+TEST_F(PostgresWireHandlerTest, WriteString) {
+  std::string value("Response");
+
+  postgres_wire_handler.write_string(_output_packet, value, true);
+
+  ASSERT_EQ(_output_packet.data.size(), value.length() + 1);
+  //string should be terminated
+  ASSERT_EQ(_output_packet.data[value.length()], '\0');
 }
 }  // namespace opossum
