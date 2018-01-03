efbb0f6ecdc4d2b2647c816307a01097ce211d68
Jan Kossmann <kossmann.jan@gmail.com>
2018-01-03 11:57:08 +0100

    Add multi-threaded sending

--- src/bin/playground.cpp
+++ src/bin/playground.cpp
@@ -15,52 +15,58 @@
 #include "sql/random_cache.hpp"
 #include "sql/sql_query_cache.hpp"
 #include "sql/sql_query_plan.hpp"
+#include "utils/thread_pool.h"
 
 
 using namespace std::chrono_literals;
 
 int main() {
-    constexpr size_t EXECUTIONS = 100;
+    constexpr size_t EXECUTIONS = 1000;
     constexpr size_t CACHE_SIZE = 6;
+
+    ThreadPool thread_pool(2);
+
+    std::array<std::string, 15> queries;
+    queries[0] = "SELECT * FROM a;";
+    queries[1] = "SELECT * FROM b;";
+    queries[2] = "SELECT * FROM c;";
+    queries[3] = "SELECT * FROM d;";
+    queries[4] = "SELECT * FROM e;";
+    queries[5] = "SELECT * FROM f;";
+    queries[6] = "SELECT * FROM g;";
+    queries[7] = "SELECT * FROM h;";
+    queries[8] = "SELECT * FROM i;";
+    queries[9] = "SELECT * FROM j;";
+    queries[10] = "SELECT * FROM k;";
+    queries[11] = "SELECT * FROM l;";
+    queries[12] = "SELECT * FROM m;";
+    queries[13] = "SELECT * FROM n;";
+    queries[14] = "SELECT * FROM o;";
+    queries[15] = "SELECT * FROM p;";
+
+    std::map<std::string, std::shared_ptr<opossum::SQLQueryCache<std::string>>> caches;
+
+    caches.emplace("GDS", std::make_shared<opossum::SQLQueryCache<std::string>>(CACHE_SIZE));
+    caches["GDS"]->replace_cache_impl<opossum::GDSCache<std::string, std::string>>(CACHE_SIZE);
+
+    caches.emplace("GDFS", std::make_shared<opossum::SQLQueryCache<std::string>>(CACHE_SIZE));
+    caches["GDFS"]->replace_cache_impl<opossum::GDFSCache<std::string, std::string>>(CACHE_SIZE);
+
+    caches.emplace("LRU", std::make_shared<opossum::SQLQueryCache<std::string>>(CACHE_SIZE));
+    caches["LRU"]->replace_cache_impl<opossum::LRUCache<std::string, std::string>>(CACHE_SIZE);
+
+    caches.emplace("LRU_K", std::make_shared<opossum::SQLQueryCache<std::string>>(CACHE_SIZE));
+    caches["LRU_K"]->replace_cache_impl<opossum::LRUKCache<2, std::string, std::string>>(CACHE_SIZE);
+
+    caches.emplace("RANDOM", std::make_shared<opossum::SQLQueryCache<std::string>>(CACHE_SIZE));
+    caches["RANDOM"]->replace_cache_impl<opossum::RandomCache<std::string, std::string>>(CACHE_SIZE);
+
+
     uWS::Hub h;
 
-    h.onMessage([](uWS::WebSocket<uWS::SERVER> *ws, char *message, size_t length, uWS::OpCode opCode) {
+    h.onMessage([&caches, &queries, &thread_pool](uWS::WebSocket<uWS::SERVER> *ws, char *message, size_t length, uWS::OpCode opCode) {
         std::cout << "start" << std::endl;
 
-        std::array<std::string, 15> queries;
-        queries[0] = "SELECT * FROM a;";
-        queries[1] = "SELECT * FROM b;";
-        queries[2] = "SELECT * FROM c;";
-        queries[3] = "SELECT * FROM d;";
-        queries[4] = "SELECT * FROM e;";
-        queries[5] = "SELECT * FROM f;";
-        queries[6] = "SELECT * FROM g;";
-        queries[7] = "SELECT * FROM h;";
-        queries[8] = "SELECT * FROM i;";
-        queries[9] = "SELECT * FROM j;";
-        queries[10] = "SELECT * FROM k;";
-        queries[11] = "SELECT * FROM l;";
-        queries[12] = "SELECT * FROM m;";
-        queries[13] = "SELECT * FROM n;";
-        queries[14] = "SELECT * FROM o;";
-        queries[15] = "SELECT * FROM p;";
-
-        std::map<std::string, std::shared_ptr<opossum::SQLQueryCache<std::string>>> caches;
-
-        caches.emplace("GDS", std::make_shared<opossum::SQLQueryCache<std::string>>(CACHE_SIZE));
-        caches["GDS"]->replace_cache_impl<opossum::GDSCache<std::string, std::string>>(CACHE_SIZE);
-
-        caches.emplace("GDFS", std::make_shared<opossum::SQLQueryCache<std::string>>(CACHE_SIZE));
-        caches["GDFS"]->replace_cache_impl<opossum::GDFSCache<std::string, std::string>>(CACHE_SIZE);
-
-        caches.emplace("LRU", std::make_shared<opossum::SQLQueryCache<std::string>>(CACHE_SIZE));
-        caches["LRU"]->replace_cache_impl<opossum::LRUCache<std::string, std::string>>(CACHE_SIZE);
-
-        caches.emplace("LRU_K", std::make_shared<opossum::SQLQueryCache<std::string>>(CACHE_SIZE));
-        caches["LRU_K"]->replace_cache_impl<opossum::LRUKCache<2, std::string, std::string>>(CACHE_SIZE);
-
-        caches.emplace("RANDOM", std::make_shared<opossum::SQLQueryCache<std::string>>(CACHE_SIZE));
-        caches["RANDOM"]->replace_cache_impl<opossum::RandomCache<std::string, std::string>>(CACHE_SIZE);
 
         std::mt19937 rng;
         rng.seed(std::random_device()());
@@ -85,9 +91,11 @@ int main() {
                 }
                 results["cacheHits"][strategy] = cache_hit;
             }
-            auto r_dump = results.dump();
-            ws->send(r_dump.c_str());
-            std::this_thread::sleep_for(1s);
+            thread_pool.enqueue([](uWS::WebSocket<uWS::SERVER> *ws, nlohmann::json results) {
+                auto results_dump = results.dump();
+                ws->send(results_dump.c_str());
+            }, ws, results);
+            std::this_thread::sleep_for(0.2s);
         }
     });
 
