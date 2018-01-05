c6cd1c8dda51aec302b937f16ec39bab62d42991
Adrian Holfter <git@adrian.holfter.de>
2018-01-05 14:26:42 +0100

    add PlaygroundTableGenerator to generate custom test table, use that table in our playground

--- src/bin/playground_group01.cpp
+++ src/bin/playground_group01.cpp
@@ -9,6 +9,7 @@
 #include "tpcc/tpcc_table_generator.hpp"
 #include "tuning/index_tuner.hpp"
 #include "tuning/system_statistics.hpp"
+#include "operators/import_binary.hpp"
 
 using std::chrono::high_resolution_clock;
 
@@ -17,9 +18,12 @@ using std::chrono::high_resolution_clock;
 //                workload, e.g. the TPC-C benchmark
 // Idea behind the current queries: have three indexable columns, but one only used once, one twice, and one thrice.
 std::vector<std::string> test_queries{
-    "SELECT * FROM CUSTOMER WHERE C_DISCOUNT > 0.3",         "SELECT * FROM CUSTOMER WHERE C_DISCOUNT < 0.6",
-    "SELECT * FROM CUSTOMER WHERE C_LAST = 'BARBARBAR'",     "SELECT * FROM CUSTOMER WHERE C_LAST = 'OUGHTPRIBAR'",
-    "SELECT * FROM CUSTOMER WHERE C_LAST = 'OUGHTANTIPRES'", "SELECT * FROM CUSTOMER WHERE C_CREDIT = 'BC'"};
+    "SELECT * FROM CUSTOMER WHERE INTEREST > 0.3",
+    "SELECT * FROM CUSTOMER WHERE INTEREST < 0.6",
+    "SELECT * FROM CUSTOMER WHERE NAME = 'BARBARBAR'",
+    "SELECT * FROM CUSTOMER WHERE NAME = 'OUGHTPRIBAR'",
+    "SELECT * FROM CUSTOMER WHERE NAME = 'OUGHTANTIPRES'",
+    "SELECT * FROM CUSTOMER WHERE LEVEL = 3"};
 
 // Forward declarations
 std::shared_ptr<opossum::SQLPipeline> _create_and_cache_pipeline(const std::string& query,
@@ -31,15 +35,8 @@ int main() {
   auto statistics = std::make_shared<opossum::SystemStatistics>(cache);
   opossum::IndexTuner tuner(statistics);
 
-  // Generate TPCC tables
-  // -- This would generate all tables
-  //  auto tables = tpcc::TpccTableGenerator().generate_all_tables();
-  //  for (auto& pair : tables) {
-  //    opossum::StorageManager::get().add_table(pair.first, pair.second);
-  //  }
-  // -- This will generate only the customer table
-  auto customer_table = tpcc::TpccTableGenerator().generate_customer_table();
-  opossum::StorageManager::get().add_table("CUSTOMER", customer_table);
+  auto importer = std::make_shared<opossum::ImportBinary>("group01_CUSTOMER.bin", "CUSTOMER");
+  importer->execute();
 
   // Fire SQL query and cache it
   auto first_execution_time = _execute_sample_queries(cache);
@@ -64,15 +61,7 @@ int main() {
 // Creates a Pipeline based on the supplied query and puts its query plan in the supplied cache
 std::shared_ptr<opossum::SQLPipeline> _create_and_cache_pipeline(const std::string& query,
                                                                  opossum::SQLQueryCache<opossum::SQLQueryPlan>& cache) {
-  //  auto op = std::make_shared<opossum::SQLQueryOperator>("SELECT * FROM CUSTOMER WHERE C_DISCOUNT > 0.3 LIMIT 20");
-  //  auto task = std::make_shared<opossum::OperatorTask>(op);
-  //  task->execute();
   auto pipeline = std::make_shared<opossum::SQLPipeline>(query);
-
-  // const auto& lqps = pipeline->get_optimized_logical_plans();
-  // for (const auto& lqp : lqps) {
-  //   lqp->print();
-  // }
 
   cache.set(query, pipeline->get_query_plan());
   return pipeline;
@@ -84,20 +73,27 @@ int _execute_sample_queries(opossum::SQLQueryCache<opossum::SQLQueryPlan>& cache
   std::vector<std::shared_ptr<opossum::SQLPipeline>> pipelines;
 
   // Execute queries multiple times to get more stable timing results
-  for (auto counter = 0u; counter < 500; counter++) {
+  for (auto counter = 0u; counter < 5; counter++) {
     for (const auto& query : test_queries) {
       pipelines.push_back(_create_and_cache_pipeline(query, cache));
     }
   }
 
   // ToDo(group01): Discuss which method calls to measure. get timing result directly from pipeline(?).
-  high_resolution_clock::time_point start_time = high_resolution_clock::now();
+  high_resolution_clock::time_point start_time;
+  high_resolution_clock::time_point end_time;
+  high_resolution_clock::duration accumulated_time = high_resolution_clock::duration::zero();
 
-  for (const auto& pipeline : pipelines) {
+  for (auto& pipeline : pipelines) {
+    start_time = high_resolution_clock::now();
     pipeline->get_result_table();
+    end_time = high_resolution_clock::now();
+    accumulated_time += end_time - start_time;
+
+    // Delete pipeline -- otherwise we would keep all materialized tables in memory
+    pipeline.reset();
   }
 
-  high_resolution_clock::time_point end_time = high_resolution_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
+  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(accumulated_time).count();
   return static_cast<int>(duration);
 }
---
+++ src/bin/playground_table_generator.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <map>
+#include <utility>
+
+#include "storage/storage_manager.hpp"
+#include "storage/table.hpp"
+#include "operators/table_wrapper.hpp"
+#include "operators/export_binary.hpp"
+#include "storage/dictionary_compression.hpp"
+
+#include "tpcc/tpcc_table_generator.hpp"
+#include "tpcc/tpcc_random_generator.hpp"
+
+class PlaygroundTableGenerator : public benchmark_utilities::AbstractBenchmarkTableGenerator {
+public:
+ explicit PlaygroundTableGenerator(const opossum::ChunkOffset chunk_size = 10000, const size_t row_count = 100000)
+        : AbstractBenchmarkTableGenerator(chunk_size),
+          _row_count(row_count),
+          _random_gen(tpcc::TpccRandomGenerator()) {}
+
+    std::map<std::string, std::shared_ptr<opossum::Table>> generate_all_tables() override {
+
+        auto cardinalities = std::make_shared<std::vector<size_t>>();
+        cardinalities->emplace_back(_row_count);
+
+        auto customer_table = std::make_shared<opossum::Table>(_chunk_size);
+
+        add_column<int>(customer_table, "ID", cardinalities, [&](std::vector<size_t> indices) { return indices[0]; });
+        add_column<std::string>(customer_table, "NAME", cardinalities,
+                                [&](std::vector<size_t> indices) { return _random_gen.last_name(indices[0]); });
+        add_column<float>(customer_table, "BALANCE", cardinalities,
+                          [&](std::vector<size_t>) { return _random_gen.random_number(-_row_count, _row_count) / 100.f; });
+        add_column<float>(customer_table, "INTEREST", cardinalities,
+                          [&](std::vector<size_t>) { return _random_gen.random_number(0, 1000) / 1000.f; });
+        add_column<int>(customer_table, "LEVEL", cardinalities,
+                          [&](std::vector<size_t>) { return _random_gen.random_number(0, 5); });
+
+        opossum::DictionaryCompression::compress_table(*customer_table);
+
+        std::map<std::string, std::shared_ptr<opossum::Table>> tables;
+        tables.insert(std::make_pair("CUSTOMER", customer_table));
+        return tables;
+    }
+
+private:
+    size_t _row_count;
+    tpcc::TpccRandomGenerator _random_gen;
+};
+
+int main() {
+  std::cout << "Playground group 01 table generator" << std::endl;
+  std::cout << " > Generating tables" << std::endl;
+  auto generator = PlaygroundTableGenerator{10'000, 10'000'000};
+  auto tables = generator.generate_all_tables();
+
+  for (auto& pair : tables) {
+    opossum::StorageManager::get().add_table(pair.first, pair.second);
+  }
+
+  std::cout << " > Dumping as binary" << std::endl;
+  auto customer_table = opossum::StorageManager::get().get_table("CUSTOMER");
+  auto table_wrapper = std::make_shared<opossum::TableWrapper>(std::move(customer_table));
+  table_wrapper->execute();
+  auto ex = std::make_shared<opossum::ExportBinary>(table_wrapper, "group01_CUSTOMER.bin");
+  ex->execute();
+
+  std::cout << " > Done" << std::endl;
+
+  return 0;
+}
