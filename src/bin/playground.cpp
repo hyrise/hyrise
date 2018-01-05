bb852d288e18e470c88ae99678680cd93c328535
Jan Kossmann <kossmann.jan@gmail.com>
2018-01-05 14:59:59 +0100

    Add query execute on backend side

--- src/bin/playground.cpp
+++ src/bin/playground.cpp
@@ -31,42 +31,35 @@ using hsql::SQLParser;
 using hsql::SQLParserResult;
 
 void evaluate_query(uWS::WebSocket<uWS::SERVER> *ws, Query& query, std::map<std::string, std::shared_ptr<opossum::SQLQueryCache<std::string>>>& caches, size_t execution, size_t query_id) {
-    nlohmann::json results;
-
-    results["executionId"] = execution;
-    results["queryId"] = query_id;
-    results["cacheHits"] = {};
-    results["planningTime"] = {};
-    for (auto &[strategy, cache] : caches) {
-        std::optional<std::string> cached_plan = cache->try_get(query.sql_string);
-        if (cached_plan) {
-            results["cacheHits"][strategy] = true;
-            results["planningTime"][strategy] = 0.0f;
-        } else {
-            results["cacheHits"][strategy] = false;
-            results["planningTime"][strategy] = query.planning_time;
-            cache->set(query.sql_string, query.sql_string);
-        }
-    }
-    nlohmann::json package;
-    package["message"] = "query_execution";
-    package["data"] = results;
-
-
-    auto package_dump = package.dump();
-    ws->send(package_dump.c_str());
+    return;
 }
 
 int main() {
+    constexpr size_t CACHE_SIZE = 30;
+    size_t execution_id = 1;
 
-    // ThreadPool thread_pool(2);
+    std::map<std::string, std::shared_ptr<opossum::SQLQueryCache<std::string>>> caches;
+    caches.emplace("GDS", std::make_shared<opossum::SQLQueryCache<std::string>>(CACHE_SIZE));
+    caches["GDS"]->replace_cache_impl<opossum::GDSCache<std::string, std::string>>(CACHE_SIZE);
 
+    caches.emplace("GDFS", std::make_shared<opossum::SQLQueryCache<std::string>>(CACHE_SIZE));
+    caches["GDFS"]->replace_cache_impl<opossum::GDFSCache<std::string, std::string>>(CACHE_SIZE);
+
+    caches.emplace("LRU", std::make_shared<opossum::SQLQueryCache<std::string>>(CACHE_SIZE));
+    caches["LRU"]->replace_cache_impl<opossum::LRUCache<std::string, std::string>>(CACHE_SIZE);
+
+    caches.emplace("LRU_K", std::make_shared<opossum::SQLQueryCache<std::string>>(CACHE_SIZE));
+    caches["LRU_K"]->replace_cache_impl<opossum::LRUKCache<2, std::string, std::string>>(CACHE_SIZE);
+
+    caches.emplace("RANDOM", std::make_shared<opossum::SQLQueryCache<std::string>>(CACHE_SIZE));
+    caches["RANDOM"]->replace_cache_impl<opossum::RandomCache<std::string, std::string>>(CACHE_SIZE);
+
+    auto workloads = initialize_workloads();
 
     uWS::Hub h;
 
-    h.onConnection([](uWS::WebSocket<uWS::SERVER> *ws, uWS::HttpRequest req) {
+    h.onConnection([&workloads](uWS::WebSocket<uWS::SERVER> *ws, uWS::HttpRequest req) {
         std::cout << "Connected!" << std::endl;
-        auto workloads = initialize_workloads();
 
         nlohmann::json initial_data;
         initial_data["message"]  = "startup";
@@ -95,44 +88,48 @@ int main() {
 
     });
 
-    // h.onMessage([](uWS::WebSocket<uWS::SERVER> *ws, char *message, size_t length, uWS::OpCode opCode) {
-    //     std::cout << "start" << std::endl;
+    h.onMessage([&caches, &workloads, &execution_id](uWS::WebSocket<uWS::SERVER> *ws, char *message, size_t length, uWS::OpCode opCode) {
+        auto message_json = nlohmann::json::parse(message);
+        if (message_json["message"] == "execute_query") {
+            auto workload_id = message_json["data"]["workload"];
+            auto query_id = message_json["data"]["query"];
 
-    //     constexpr size_t EXECUTIONS = 1000;
-    //     constexpr size_t CACHE_SIZE = 6;
+            std::cout << "Execute query: " << query_id << " from workload: " << workload_id << std::endl;
 
-    //     // auto queries = load_queries();
+            auto& query = workloads[workload_id][query_id];
 
-    //     std::mt19937 rng;
-    //     rng.seed(std::random_device()());
-    //     std::uniform_int_distribution<std::mt19937::result_type> dist(0, queries.size() - 1);
+            nlohmann::json results;
 
-    //     std::map<std::string, std::shared_ptr<opossum::SQLQueryCache<std::string>>> caches;
+            results["executionId"] = execution_id++;
+            results["workload"] = workload_id;
+            results["query"] = query_id;
+            results["cacheHits"] = {};
+            results["planningTime"] = {};
+            for (auto &[strategy, cache] : caches) {
+                std::optional<std::string> cached_plan = cache->try_get(query.sql_string);
+                if (cached_plan) {
+                    results["cacheHits"][strategy] = true;
+                    results["planningTime"][strategy] = 0.0f;
+                } else {
+                    results["cacheHits"][strategy] = false;
+                    results["planningTime"][strategy] = query.planning_time;
+                    cache->set(query.sql_string, query.sql_string);
+                }
+            }
+            nlohmann::json package;
+            package["message"] = "query_execution";
+            package["data"] = results;
 
-    //     caches.emplace("GDS", std::make_shared<opossum::SQLQueryCache<std::string>>(CACHE_SIZE));
-    //     caches["GDS"]->replace_cache_impl<opossum::GDSCache<std::string, std::string>>(CACHE_SIZE);
 
-    //     caches.emplace("GDFS", std::make_shared<opossum::SQLQueryCache<std::string>>(CACHE_SIZE));
-    //     caches["GDFS"]->replace_cache_impl<opossum::GDFSCache<std::string, std::string>>(CACHE_SIZE);
+            auto package_dump = package.dump();
+            ws->send(package_dump.c_str());
+        }
 
-    //     caches.emplace("LRU", std::make_shared<opossum::SQLQueryCache<std::string>>(CACHE_SIZE));
-    //     caches["LRU"]->replace_cache_impl<opossum::LRUCache<std::string, std::string>>(CACHE_SIZE);
+        // std::mt19937 rng;
+        // rng.seed(std::random_device()());
+        // std::uniform_int_distribution<std::mt19937::result_type> dist(0, queries.size() - 1);
 
-    //     caches.emplace("LRU_K", std::make_shared<opossum::SQLQueryCache<std::string>>(CACHE_SIZE));
-    //     caches["LRU_K"]->replace_cache_impl<opossum::LRUKCache<2, std::string, std::string>>(CACHE_SIZE);
-
-    //     caches.emplace("RANDOM", std::make_shared<opossum::SQLQueryCache<std::string>>(CACHE_SIZE));
-    //     caches["RANDOM"]->replace_cache_impl<opossum::RandomCache<std::string, std::string>>(CACHE_SIZE);
-
-    //     for (size_t execution = 0; execution < EXECUTIONS; ++execution) {
-    //         size_t query_id = dist(rng);
-    //         auto& query = queries[query_id];
-
-    //         std::thread t(evaluate_query, ws, std::ref(query), std::ref(caches), execution, query_id);
-    //         std::this_thread::sleep_for(0.2s);
-    //         t.join();
-    //     }
-    // });
+    });
 
     if (h.listen(4000)) {
         h.run();
