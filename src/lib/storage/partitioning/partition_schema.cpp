d547b680b4b162ff8cd2a88eef90110f5b0e0ed0
Jonas Chromik <jonas.chromik@student.hpi.de>
2018-01-10 13:59:45 +0100

    Move function definitions from partition_schema.hpp to partition_schema.cpp

--- src/lib/storage/partitioning/partition_schema.cpp
+++ src/lib/storage/partitioning/partition_schema.cpp
@@ -2,6 +2,59 @@
 
 namespace opossum {
 
+void PartitionSchema::add_column(DataType data_type, bool nullable) {
+  for (std::shared_ptr<Partition> partition_ptr : _partitions) {
+    partition_ptr->add_column(data_type, nullable);
+  }
+}
+
+ChunkID PartitionSchema::chunk_count() const {
+  ChunkID num_of_chunks = ChunkID{0};
+  for (std::shared_ptr<Partition> partition_ptr : _partitions) {
+    num_of_chunks += partition_ptr->chunk_count();
+  }
+  return num_of_chunks;
+}
+
+uint64_t PartitionSchema::row_count() const {
+  uint64_t num_of_rows = 0;
+  for (std::shared_ptr<Partition> partition_ptr : _partitions) {
+    num_of_rows += partition_ptr->row_count();
+  }
+  return num_of_rows;
+}
+
+const std::shared_ptr<Partition> PartitionSchema::find_partition(PartitionID partition_to_find) {
+  for (std::shared_ptr<Partition> partition_ptr : _partitions) {
+    if (partition_ptr->get_partition_id() == partition_to_find) {
+      return partition_ptr;
+    }
+  }
+  throw "Cannot find specified partition!";
+}
+
+const PartitionID PartitionSchema::max_partition_id() {
+  if (_partitions.size() == 0) {
+    throw "The minimum number of partitions is 1!";
+  }
+  PartitionID max = PartitionID{0};
+  for (std::shared_ptr<Partition> partition_ptr : _partitions) {
+    if (partition_ptr->get_partition_id() > max) {
+      max = partition_ptr->get_partition_id();
+    }
+  }
+  return max;
+}
+
+virtual void PartitionSchema::create_new_chunk(const std::vector<DataType>& column_types,
+                                               const std::vector<bool>& column_nullables) {
+  throw "create_new_chunk can not be used on partitioned tables";
+}
+
+virtual void PartitionSchema::emplace_chunk(Chunk& chunk, uint16_t column_count) {
+  throw "emplace_chunk can not be used on partitioned tables";
+}
+
 Chunk& PartitionSchema::get_modifiable_chunk(ChunkID chunk_id, PartitionID partition_id) {
   DebugAssert(static_cast<PartitionID>(_partitions.size()) > partition_id, "No Partition with ID " + std::to_string(partition_id));
   return _partitions.at(partition_id)->get_modifiable_chunk(chunk_id);
--- src/lib/storage/partitioning/partition_schema.hpp
+++ src/lib/storage/partitioning/partition_schema.hpp
@@ -22,64 +22,26 @@ class PartitionSchema {
   virtual TableType get_type(uint16_t column_count) const = 0;
   virtual AllTypeVariant get_value(const ColumnID column_id, const size_t row_number) const = 0;
   
-  void add_column(DataType data_type, bool nullable) {
-    for (std::shared_ptr<Partition> partition_ptr : _partitions) {
-      partition_ptr->add_column(data_type, nullable);
-    }
-  }
+  void add_column(DataType data_type, bool nullable);
 
-  ChunkID chunk_count() const {
-    ChunkID num_of_chunks = ChunkID{0};
-    for (std::shared_ptr<Partition> partition_ptr : _partitions) {
-      num_of_chunks += partition_ptr->chunk_count();
-    }
-    return num_of_chunks;
-  }
+  ChunkID chunk_count() const;
 
-  uint64_t row_count() const {
-    uint64_t num_of_rows = 0;
-    for (std::shared_ptr<Partition> partition_ptr : _partitions) {
-      num_of_rows += partition_ptr->row_count();
-    }
-    return num_of_rows;
-  }
+  uint64_t row_count() const;
 
   // The following functions can be overriden,
   // when a partition schema is continous and hence can make sense of them.
   // For example, the NullPartitionSchema implements them.
   // This results in speed-up of some operators (e.g. insert).
-  virtual void create_new_chunk(const std::vector<DataType>& column_types, const std::vector<bool>& column_nullables) {
-    throw "create_new_chunk can not be used on partitioned tables";
-  }
-  virtual void emplace_chunk(Chunk& chunk, uint16_t column_count) {
-    throw "emplace_chunk can not be used on partitioned tables";
-  }
+  virtual void create_new_chunk(const std::vector<DataType>& column_types, const std::vector<bool>& column_nullables);
+  virtual void emplace_chunk(Chunk& chunk, uint16_t column_count);
   virtual Chunk& get_modifiable_chunk(ChunkID chunk_id, PartitionID partition_id);
   virtual const Chunk& get_chunk(ChunkID chunk_id, PartitionID partition_id) const;
   virtual ProxyChunk get_modifiable_chunk_with_access_counting(ChunkID chunk_id, PartitionID partition_id);
   virtual const ProxyChunk get_chunk_with_access_counting(ChunkID chunk_id, PartitionID partition_id) const;
 
-  const std::shared_ptr<Partition> find_partition(PartitionID partition_to_find) {
-    for (std::shared_ptr<Partition> partition_ptr : _partitions) {
-      if (partition_ptr->get_partition_id() == partition_to_find) {
-        return partition_ptr;
-      }
-    }
-    throw "Cannot find specified partition!";
-  }
+  const std::shared_ptr<Partition> find_partition(PartitionID partition_to_find);
 
-  const PartitionID max_partition_id() {
-    if (_partitions.size() == 0) {
-      throw "The minimum number of partitions is 1!";
-    }
-    PartitionID max = PartitionID{0};
-    for (std::shared_ptr<Partition> partition_ptr : _partitions) {
-      if (partition_ptr->get_partition_id() > max) {
-        max = partition_ptr->get_partition_id();
-      }
-    }
-    return max;
-  }
+  const PartitionID max_partition_id();
 
   // Indicates that the functions above are
   //   1. not meaningfully implemented if true is returned (default case)
