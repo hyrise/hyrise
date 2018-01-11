4e396a716cb5a788f4d4d6a833fee12e9ede66f9
Max Jendruk <code@jendruk.com>
2018-01-12 00:32:16 +0100

    Remove old comment and add final

--- src/lib/storage/zero_suppression/base_zero_suppression_vector.hpp
+++ src/lib/storage/zero_suppression/base_zero_suppression_vector.hpp
@@ -80,7 +80,7 @@ class ZeroSuppressionVector : public BaseZeroSuppressionVector {
     return _self()._on_create_base_decoder();
   }
 
-  std::shared_ptr<BaseZeroSuppressionVector> copy_using_allocator(const PolymorphicAllocator<size_t>& alloc) const {
+  std::shared_ptr<BaseZeroSuppressionVector> copy_using_allocator(const PolymorphicAllocator<size_t>& alloc) const final {
     return _self()._on_copy_using_allocator(alloc);
   }
 
