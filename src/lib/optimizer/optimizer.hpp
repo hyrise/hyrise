703e1f1ddbc5a594f3fd096af12753f6ef533e65
Moritz Eyssen <Moritz.Eyssen@student.hpi.de>
2018-01-17 17:10:32 +0100

    Template for Rule Batching

--- src/lib/optimizer/optimizer.hpp
+++ src/lib/optimizer/optimizer.hpp
@@ -3,6 +3,8 @@
 #include <memory>
 #include <vector>
 
+#include "optimizer/strategy/rule_batch.hpp"
+
 namespace opossum {
 
 class AbstractRule;
@@ -15,12 +17,16 @@ class Optimizer final {
  public:
   static const Optimizer& get();
 
-  Optimizer();
+  static Optimizer createDefaultOptimizer();
+
+  explicit Optimizer(const uint32_t max_num_iterations);
+
+  void add_rule_batch(RuleBatch rule_batch);
 
   std::shared_ptr<AbstractLQPNode> optimize(const std::shared_ptr<AbstractLQPNode>& input) const;
 
  private:
-  std::vector<std::shared_ptr<AbstractRule>> _rules;
+  std::vector<RuleBatch> _rule_batches;
 
   // Rather arbitrary right now, atm all rules should be done after one iteration
   uint32_t _max_num_iterations = 10;
