991010491481deb0d8a35f0aeaf68ad4deb03e1d
Lukas Wenzel <lukas.wenzel@student.hpi.de>
2018-01-18 17:30:28 +0100

    Implement time budget tracking in IndexTuner

--- src/lib/tuning/index_tuner.cpp
+++ src/lib/tuning/index_tuner.cpp
@@ -1,5 +1,8 @@
 #include "index_tuner.hpp"
 
+#include <chrono>
+#include <limits>
+
 #include "sql/sql_query_cache.hpp"
 #include "sql/sql_query_operator.hpp"
 #include "storage/index/adaptive_radix_tree/adaptive_radix_tree_index.hpp"
@@ -13,21 +16,52 @@ namespace opossum {
 IndexTuner::IndexTuner(std::shared_ptr<SystemStatistics> statistics)
     : _statistics{statistics},
       _evaluator{std::make_unique<IndexEvaluator>()},
-      _selector{std::make_unique<IndexSelector>()} {}
+      _selector{std::make_unique<IndexSelector>()},
+      _time_budget{std::numeric_limits<float>::infinity()},
+      _memory_budget{std::numeric_limits<float>::infinity()} {}
+
+void IndexTuner::set_time_budget(float budget) { _time_budget = budget; }
+
+void IndexTuner::disable_time_budget() { _time_budget = std::numeric_limits<float>::infinity(); }
+
+float IndexTuner::time_budget() { return _time_budget; }
+
+void IndexTuner::set_memory_budget(float budget) { _memory_budget = budget; }
+
+void IndexTuner::disable_memory_budget() { _memory_budget = std::numeric_limits<float>::infinity(); }
+
+float IndexTuner::memory_budget() { return _memory_budget; }
 
 void IndexTuner::execute() {
-  const auto& operations = _selector->select_indices(_evaluator->evaluate_indices(*_statistics), 0.0f);
+  const auto& operations = _selector->select_indices(_evaluator->evaluate_indices(*_statistics), _memory_budget);
 
   std::cout << "Recommended changes: \n";
-
   for (const auto& operation : operations) {
     const auto& column_name = StorageManager::get().get_table(operation.table_name)->column_name(operation.column_id);
     if (operation.create) {
       std::cout << "  Create index on table " << operation.table_name << ", column " << column_name << "\n";
-      _create_index(operation.table_name, operation.column_id);
     } else {
       std::cout << "  Delete index on table " << operation.table_name << ", column " << column_name << "\n";
+    }
+  }
+
+  _execute_operations(operations);
+}
+
+void IndexTuner::_execute_operations(const std::vector<IndexOperation>& operations) {
+  std::chrono::duration<float, std::chrono::seconds::period> timeout{_time_budget};
+  auto begin = std::chrono::high_resolution_clock::now();
+
+  for (const auto& operation : operations) {
+    if (operation.create) {
+      _create_index(operation.table_name, operation.column_id);
+    } else {
       _delete_index(operation.table_name, operation.column_id);
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    if (end - begin > timeout) {
+      std::cout << "  IndexTuning was interrupted because the time budget was insufficient\n";
+      break;
     }
   }
 }
--- src/lib/tuning/index_tuner.hpp
+++ src/lib/tuning/index_tuner.hpp
@@ -5,6 +5,7 @@
 
 #include "tuning/abstract_index_evaluator.hpp"
 #include "tuning/abstract_index_selector.hpp"
+#include "tuning/index_operation.hpp"
 #include "tuning/system_statistics.hpp"
 
 namespace opossum {
@@ -18,23 +19,55 @@ namespace opossum {
  * that is expected to improve system performance by using an AbstractIndexSelector.
  *
  * While executing the operation sequence it considers a time budget.
+ *
+ * Both the time and memory budget (the latter being enforced by AbstractIndexSelector)
+ * are initially infinite (i.e. disabled).
  */
 class IndexTuner {
  public:
   explicit IndexTuner(std::shared_ptr<SystemStatistics> statistics);
 
+  /**
+   * The time budget (in seconds) limits the time available for
+   * index creation and deletion operations.
+   *
+   * The time consumed by the AbstractIndexEvaluator and -Selector is
+   * not counted against this budget.
+   *
+   * A value of positive infinity disables time budget checking.
+   */
+  void set_time_budget(float budget);
+  void disable_time_budget();
+  float time_budget();
+
+  /**
+   * The memory budget (in MiB) limits the space that can be consumed
+   * by all indices maintained by the IndexTuner.
+   *
+   * A value of positive infinity disables memory budget checking.
+   */
+  void set_memory_budget(float budget);
+  void disable_memory_budget();
+  float memory_budget();
+
+  /**
+   * Initiates the tuning process
+   */
   void execute();
 
  protected:
-  // Creates an index on all chunks of the specified table/column
+  void _execute_operations(const std::vector<IndexOperation>& operations);
+
   void _create_index(const std::string& table_name, ColumnID column_id);
-  // Deletes an index on chunks of the specified table/column
   void _delete_index(const std::string& table_name, ColumnID column_id);
 
  protected:
   std::shared_ptr<SystemStatistics> _statistics;
   std::unique_ptr<AbstractIndexEvaluator> _evaluator;
   std::unique_ptr<AbstractIndexSelector> _selector;
+
+  float _time_budget;
+  float _memory_budget;
 };
 
 }  // namespace opossum
