583859c707989ee561c1fe996d5f881081c4a9e1
Lukas Wenzel <lukas.wenzel@student.hpi.de>
2018-01-19 18:26:15 +0100

    Make IndexSelector algo balance memory savings against lost desirability

--- src/lib/tuning/index_selector.cpp
+++ src/lib/tuning/index_selector.cpp
@@ -23,9 +23,7 @@ std::vector<IndexOperation> IndexSelector::select_indices(std::vector<IndexEvalu
   auto best_index = evaluations.end() - 1;
   auto worst_index = evaluations.begin();
 
-  // ToDo(group01): This is still not optimal as several less desirable indices
-  // might be sacrificed for a more desirable but larger index even though their
-  // cumulative desirability might be larger than that of the created index
+  // ToDo(group01): research if this problem can actually be solved by a greedy algorithm
 
   while (best_index >= worst_index) {
     if (worst_index->desirablility < 0.0f && -worst_index->desirablility > best_index->desirablility) {
@@ -36,23 +34,36 @@ std::vector<IndexOperation> IndexSelector::select_indices(std::vector<IndexEvalu
       }
       ++worst_index;
     } else {
-      // best index must be positive and more beneficial than removal of worst index
+      // best index is positive and more beneficial than removal of worst index
       if (best_index->exists) {
         --best_index;
       } else {
-        if (memory_consumption + best_index->memory_cost < memory_budget) {
-          // create best index as it fits into memory budget
+        // determine minimum desirability that must be sacrificed
+        // to obtain enough memory for the new index
+        float sacrificed_desirability = 0.0f;
+        float obtained_memory = 0.0f;
+        float required_memory = best_index->memory_cost + memory_consumption - memory_budget;
+        auto sacrifice_index = worst_index;
+        while (obtained_memory < required_memory && sacrifice_index != best_index) {
+          if (sacrifice_index->exists) {
+            sacrificed_desirability += sacrifice_index->desirablility;
+            obtained_memory += sacrifice_index->memory_cost;
+          }
+          ++sacrifice_index;
+        }
+        if (obtained_memory >= required_memory && sacrificed_desirability <= best_index->desirablility) {
+          // delete the previously selected indices, then create the better index
+          for (auto delete_index = worst_index; delete_index != sacrifice_index; ++delete_index) {
+            if (delete_index->exists) {
+              _operations.emplace_back(delete_index->table_name, delete_index->column_id, false);
+              memory_consumption -= delete_index->memory_cost;
+            }
+          }
+          worst_index = sacrifice_index;
           _operations.emplace_back(best_index->table_name, best_index->column_id, true);
           memory_consumption += best_index->memory_cost;
-          --best_index;
-        } else {
-          // worst index is deleted to make space for better (=best) index
-          if (worst_index->exists) {
-            _operations.emplace_back(worst_index->table_name, worst_index->column_id, false);
-            memory_consumption -= worst_index->memory_cost;
-          }
-          ++worst_index;
         }
+        --best_index;
       }
     }
   }
