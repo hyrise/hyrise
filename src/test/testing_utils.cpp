084cf1d5c59aed1ee6d0a25a2386f510a4f0f1eb
Jan Kossmann <kossmann.jan@gmail.com>
2018-01-19 11:11:41 +0100

    Create random uuids

--- src/test/testing_utils.cpp
+++ src/test/testing_utils.cpp
@@ -3,7 +3,7 @@
 namespace opossum {
 
 std::string unique_random_name() {
-    return to_string(boost::uuids::uuid());
+    return to_string(boost::uuids::random_generator()());
 }
 
 }  // namespace opossum
