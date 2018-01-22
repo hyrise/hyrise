5d9e7b8078f49ddb548607e9611d2d1413c3ec09
Jan Kossmann <kossmann.jan@gmail.com>
2018-01-22 13:37:06 +0100

    Move includes

--- src/test/gtest_main.cpp
+++ src/test/gtest_main.cpp
@@ -4,17 +4,17 @@
 #include <experimental/filesystem>
 #endif
 
-#include <gtest/gtest.h>
-#include "base_test.hpp"
-
-#include "utils/assert.hpp"
-#include "utils/performance_warning.hpp"
-
 #if __has_include(<filesystem>)
 namespace filesystem = std::filesystem;
 #else
 namespace filesystem = std::experimental::filesystem;
 #endif
+
+#include <gtest/gtest.h>
+#include "base_test.hpp"
+
+#include "utils/assert.hpp"
+#include "utils/performance_warning.hpp"
 
 void create_test_data_directory(std::optional<std::string>& prefix) {
   if (prefix)
