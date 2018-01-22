03f25d0567fa01e1ea9c4c017085dd05b71e5d58
Jan Kossmann <kossmann.jan@gmail.com>
2018-01-22 14:50:58 +0100

    Cleanup creation

--- src/test/gtest_main.cpp
+++ src/test/gtest_main.cpp
@@ -13,22 +13,19 @@ namespace filesystem = std::experimental::filesystem;
 #include "utils/performance_warning.hpp"
 
 void create_test_data_directory(std::optional<std::string>& prefix) {
-  if (prefix)
-    opossum::test_data_path = "./" + *prefix + "/test_data/";
-  else
-    opossum::test_data_path = "./test_data/";
+  opossum::Assert(!filesystem::exists(opossum::test_data_path),
+    "Cannot create directory for test data: " + opossum::test_data_path +" - it already exists.");
 
-  if (filesystem::exists(opossum::test_data_path)) {
-    filesystem::remove_all(opossum::test_data_path);
+  if (prefix) {
+    opossum::Assert(filesystem::exists("./" + *prefix),
+      "Cannot create directory for test data because " + *prefix + " does not exist");
   }
-  filesystem::create_directories(opossum::test_data_path);
+
+  filesystem::create_directory(opossum::test_data_path);
 }
 
-void remove_test_data_directory(std::optional<std::string>& prefix) {
-  if (prefix)
-    filesystem::remove_all("./" + *prefix);
-  else
-    filesystem::remove_all(opossum::test_data_path);
+void remove_test_data_directory() {
+  filesystem::remove_all(opossum::test_data_path);
 }
 
 int main(int argc, char** argv) {
@@ -39,16 +36,20 @@ int main(int argc, char** argv) {
   opossum::PerformanceWarningDisabler pwd;
   ::testing::InitGoogleTest(&argc, argv);
 
-  std::optional<std::string> test_data_prefix;
+
+  std::optional<std::string> prefix;
   if (argc > 1) {
     // If argv[1] is set after gtest extracted its commands, we interpret it as directory name prefix for test data
-    test_data_prefix = argv[1];
+    opossum::test_data_path = "./" + std::string(argv[1]) + "/.hyrise_test_data/";
+    prefix = argv[1];
+  } else {
+    opossum::test_data_path = "./.hyrise_test_data/";
   }
-  create_test_data_directory(test_data_prefix);
+  create_test_data_directory(prefix);
 
   int ret = RUN_ALL_TESTS();
 
-  remove_test_data_directory(test_data_prefix);
+  remove_test_data_directory();
 
   return ret;
 }
