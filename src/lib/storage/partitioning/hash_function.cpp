6585857a59d0db7f11bbe51bea61fa373f50a63f
Jonas Chromik <jonas.chromik@student.hpi.de>
2018-01-24 13:47:11 +0100

    Refactoring in HashFunction

--- src/lib/storage/partitioning/hash_function.cpp
+++ src/lib/storage/partitioning/hash_function.cpp
@@ -4,28 +4,18 @@
 
 namespace opossum {
 
+class hash_value_visitor : public boost::static_visitor<HashValue> {
+ public:
+  
+  HashValue operator()(NullValue) { return HashValue{0}; }
+
+  template <typename T>
+  HashValue operator()(T value) { return HashValue{std::hash<T>{}(value)}; }
+};
+
 const HashValue HashFunction::operator()(const AllTypeVariant value) const {
-  if (value.type() == typeid(int32_t)) {
-    return HashValue{std::hash<int32_t>{}(type_cast<int32_t>(value))};
-  }
-
-  if (value.type() == typeid(int64_t)) {
-    return HashValue{std::hash<int64_t>{}(type_cast<int64_t>(value))};
-  }
-
-  if (value.type() == typeid(float)) {
-    return HashValue{std::hash<float>{}(type_cast<float>(value))};
-  }
-
-  if (value.type() == typeid(double)) {
-    return HashValue{std::hash<double>{}(type_cast<double>(value))};
-  }
-
-  if (value.type() == typeid(std::string)) {
-    return HashValue{std::hash<std::string>{}(type_cast<std::string>(value))};
-  }
-
-  throw "Unknown type, can not hash";
+  hash_value_visitor visitor;
+  return boost::apply_visitor(visitor, value);
 }
 
 }  // namespace opossum
--- src/test/storage/partitioning/hash_function_test.cpp
+++ src/test/storage/partitioning/hash_function_test.cpp
@@ -3,44 +3,63 @@
 
 namespace opossum {
 
-class HashFunctionTest : public BaseTest {
+class StorageHashFunctionTest : public BaseTest {
  protected:
   void SetUp() override {}
 };
 
-TEST_F(HashFunctionTest, HashInt32) {
+/*
+We want to make sure
+ 1. that hashing the same value *always* produces the same result
+ 2. that hashing different values *probably* produces different results
+
+About 2 we can not be absolutely certains due to hash collisions. 
+There are no hash collisions for the used values, though.
+
+We tests this for all data types AllTypeVariant can hold.
+*/
+
+TEST_F(StorageHashFunctionTest, HashInt32) {
   HashFunction hf;
   EXPECT_EQ(hf(INT32_C(42)), hf(INT32_C(42)));
   EXPECT_EQ(hf(INT32_C(21)), hf(INT32_C(21)));
   EXPECT_NE(hf(INT32_C(42)), hf(INT32_C(21)));
 }
 
-TEST_F(HashFunctionTest, HashInt64) {
+TEST_F(StorageHashFunctionTest, HashInt64) {
   HashFunction hf;
   EXPECT_EQ(hf(INT64_C(42)), hf(INT64_C(42)));
   EXPECT_EQ(hf(INT64_C(21)), hf(INT64_C(21)));
   EXPECT_NE(hf(INT64_C(42)), hf(INT64_C(21)));
 }
 
-TEST_F(HashFunctionTest, HashFloat) {
+TEST_F(StorageHashFunctionTest, HashFloat) {
   HashFunction hf;
   EXPECT_EQ(hf(4.2f), hf(4.2f));
   EXPECT_EQ(hf(2.1f), hf(2.1f));
   EXPECT_NE(hf(4.2f), hf(2.1f));
 }
 
-TEST_F(HashFunctionTest, HashDouble) {
+TEST_F(StorageHashFunctionTest, HashDouble) {
   HashFunction hf;
   EXPECT_EQ(hf(4.2), hf(4.2));
   EXPECT_EQ(hf(2.1), hf(2.1));
   EXPECT_NE(hf(4.2), hf(2.1));
 }
 
-TEST_F(HashFunctionTest, HashString) {
+TEST_F(StorageHashFunctionTest, HashString) {
   HashFunction hf;
   EXPECT_EQ(hf("42"), hf("42"));
   EXPECT_EQ(hf("21"), hf("21"));
   EXPECT_NE(hf("42"), hf("21"));
+}
+
+TEST_F(StorageHashFunctionTest, HashNullValue) {
+  HashFunction hf;
+  NullValue nv;
+
+  // NullValues are hashed to HashValue 0
+  EXPECT_EQ(hf(nv), HashValue{0});
 }
 
 }  // namespace opossum
