a88ac4c0d1b0e3fd58befbf7df604434a195f9c2
Moritz Eyssen <Moritz.Eyssen@student.hpi.de>
2018-01-29 14:29:50 +0100

    include Mvcc members

--- src/lib/storage/chunk.cpp
+++ src/lib/storage/chunk.cpp
@@ -187,6 +187,7 @@ MemoryUsage Chunk::estimate_memory_usage(MemoryUsageEstimationMode estimation_mo
   // TODO(anybody) AccessCounter memory usage missing
 
   if (_mvcc_columns) {
+    bytes += sizeof(_mvcc_columns->tids) + sizeof(_mvcc_columns->begin_cids) + sizeof(_mvcc_columns->end_cids);
     bytes += _mvcc_columns->tids.size() * sizeof(decltype(_mvcc_columns->tids)::value_type);
     bytes += _mvcc_columns->begin_cids.size() * sizeof(decltype(_mvcc_columns->begin_cids)::value_type);
     bytes += _mvcc_columns->end_cids.size() * sizeof(decltype(_mvcc_columns->end_cids)::value_type);
