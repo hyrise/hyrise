3462d2c6c83debfdfbe94eba067dbc5d6039040d
Leander Neiss <leander.neiss@student.hpi.uni-potsdam.de>
2018-02-03 12:28:29 +0100

    Use LQPColumnReference instead of ColumnID in SQLTranslator for nested expressions in predicates.

--- src/lib/sql/sql_translator.cpp
+++ src/lib/sql/sql_translator.cpp
@@ -975,7 +975,7 @@ std::shared_ptr<AbstractLQPNode> SQLTranslator::_translate_predicate(
 
     DebugAssert(column_expressions.size() <= std::numeric_limits<uint16_t>::max(),
                 "Number of column expressions cannot exceed maximum value of ColumnID.");
-    value = ColumnID{static_cast<uint16_t>(column_expressions.size() - 1)};
+    value = LQPColumnReference(current_node, ColumnID{static_cast<uint16_t>(column_expressions.size() - 1)});
   } else {
     value = HSQLExprTranslator::to_all_parameter_variant(*value_ref_hsql_expr);
   }
