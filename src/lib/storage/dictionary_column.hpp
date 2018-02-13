7c98797055e53d65b47a6fcdb463664272a8eb56
Max Jendruk <code@jendruk.com>
2018-02-13 13:47:49 +0100

    Move up estimate_memory_usage()

--- src/lib/storage/dictionary_column.hpp
+++ src/lib/storage/dictionary_column.hpp
@@ -36,15 +36,13 @@ class DictionaryColumn : public BaseDictionaryColumn {
 
   std::shared_ptr<BaseColumn> copy_using_allocator(const PolymorphicAllocator<size_t>& alloc) const final;
 
+  size_t estimate_memory_usage() const final;
   /**@}*/
 
   /**
    * @defgroup BaseDictionaryColumn interface
    * @{
    */
-
-  size_t estimate_memory_usage() const final;
-
   ValueID lower_bound(const AllTypeVariant& value) const final;
   ValueID upper_bound(const AllTypeVariant& value) const final;
 
