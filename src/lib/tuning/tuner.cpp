67b22a120623066014c92f570f8a0794e63585ad
Lukas Wenzel <lukas.wenzel@student.hpi.de>
2018-02-14 22:09:23 +0100

    make Tuner schedulable

--- src/lib/tuning/tuner.cpp
+++ src/lib/tuning/tuner.cpp
@@ -1,7 +1,6 @@
 #include "tuner.hpp"
 
-#include <chrono>
-#include <limits>
+#include "scheduler/job_task.hpp"
 
 #include "utils/assert.hpp"
 #include "utils/logging.hpp"
@@ -9,9 +8,14 @@
 namespace opossum {
 
 Tuner::Tuner()
-    : _time_budget{std::numeric_limits<float>::infinity()}, _cost_budget{std::numeric_limits<float>::infinity()} {}
+    : _time_budget{NoBudget},
+      _evaluate_time_budget{NoBudget},
+      _select_time_budget{NoBudget},
+      _execute_time_budget{NoBudget},
+      _cost_budget{NoBudget} {}
 
 void Tuner::add_evaluator(std::unique_ptr<AbstractEvaluator>&& evaluator) {
+  Assert(evaluator, "Tried to add an invalid evaluator.");
   _evaluators.push_back(std::move(evaluator));
 }
 
@@ -23,68 +27,150 @@ void Tuner::remove_evaluator(std::size_t index) {
 
 const std::vector<std::unique_ptr<AbstractEvaluator>>& Tuner::evaluators() const { return _evaluators; }
 
-void Tuner::set_selector(std::unique_ptr<AbstractSelector>&& selector) { _selector = std::move(selector); }
+void Tuner::set_selector(std::unique_ptr<AbstractSelector>&& selector) {
+  Assert(selector, "Tried to add an invalid evaluator.");
+  _selector = std::move(selector);
+}
 
 const std::unique_ptr<AbstractSelector>& Tuner::selector() const { return _selector; }
 
-void Tuner::set_time_budget(float budget) { _time_budget = budget; }
+void Tuner::set_time_budget(float budget, float execute_budget, float evaluate_budget, float select_budget) {
+  _time_budget = Runtime{budget};
+  _evaluate_time_budget = Runtime{evaluate_budget};
+  _select_time_budget = Runtime{select_budget};
+  _execute_time_budget = Runtime{execute_budget};
+}
 
-void Tuner::disable_time_budget() { _time_budget = std::numeric_limits<float>::infinity(); }
+float Tuner::time_budget() const { return _time_budget.count(); }
 
-float Tuner::time_budget() const { return _time_budget; }
+float Tuner::evaluate_time_budget() const { return _evaluate_time_budget.count(); }
+
+float Tuner::select_time_budget() const { return _select_time_budget.count(); }
+
+float Tuner::execute_time_budget() const { return _execute_time_budget.count(); }
 
 void Tuner::set_cost_budget(float budget) { _cost_budget = budget; }
 
-void Tuner::disable_cost_budget() { _cost_budget = std::numeric_limits<float>::infinity(); }
-
 float Tuner::cost_budget() const { return _cost_budget; }
 
-void Tuner::execute() {
+void Tuner::schedule_tuning_process() {
   Assert(_evaluators.size() > 0, "Can not run Tuner without at least one AbstractEvaluator");
   Assert(_selector, "Can not run Tuner without an AbstractSelector");
+  Assert(!is_running(), "Can not schedule another tuning process while the previous process is still running");
 
-  std::vector<std::shared_ptr<TuningChoice>> choices;
-  LOG_INFO("Running evaluators...");
+  _evaluate_task = std::make_shared<JobTask>([this]() { this->_evaluate(); });
+  _select_task = std::make_shared<JobTask>([this]() { this->_select(); });
+  _execute_task = std::make_shared<JobTask>([this]() { this->_execute(); });
+
+  _evaluate_task->set_as_predecessor_of(_select_task);
+  _select_task->set_as_predecessor_of(_execute_task);
+
+  _remaining_time_budget = _time_budget;
+  _time_budget_exceeded = false;
+
+  _evaluate_task->schedule();
+  _select_task->schedule();
+  _execute_task->schedule();
+}
+
+bool Tuner::is_running() {
+  return (_evaluate_task && !_evaluate_task->is_done()) || (_select_task && !_select_task->is_done()) ||
+         (_execute_task && !_execute_task->is_done());
+}
+
+void Tuner::wait_for_completion() {
+  if (_evaluate_task) {
+    _evaluate_task->join();
+  }
+  if (_select_task) {
+    _select_task->join();
+  }
+  if (_execute_task) {
+    _execute_task->join();
+  }
+}
+
+void Tuner::_evaluate() {
+  LOG_INFO("Begin tuning evaluation phase...");
+
+  auto begin = RuntimeClock::now();
+
+  _choices.clear();
   for (const auto& evaluator : _evaluators) {
-    if (evaluator) {
-      evaluator->evaluate(choices);
-    } else {
-      LOG_WARN("Found null evaluator");
+    evaluator->evaluate(_choices);
+
+    auto runtime = RuntimeClock::now() - begin;
+    if (runtime > _evaluate_time_budget || runtime > _remaining_time_budget) {
+      LOG_INFO("Interrupt evaluation phase: Time budget exceeded.");
+      _time_budget_exceeded = true;
+      return;
+    }
+  }
+  _remaining_time_budget -= RuntimeClock::now() - begin;
+
+  _log_choices();
+  LOG_INFO("Tuning evaluation phase completed.");
+}
+
+void Tuner::_select() {
+  if (_time_budget_exceeded) {
+    LOG_INFO("Skip tuning selection phase because previous step exceeded the time budget.");
+    return;
+  }
+  LOG_INFO("Begin tuning selection phase...");
+
+  auto begin = RuntimeClock::now();
+
+  _operations = _selector->select(_choices, _cost_budget);
+
+  auto runtime = RuntimeClock::now() - begin;
+  if (runtime > _select_time_budget || runtime > _remaining_time_budget) {
+    LOG_INFO("Interrupt selection phase: Time budget exceeded.");
+    _time_budget_exceeded = true;
+    return;
+  }
+  _remaining_time_budget -= runtime;
+
+  _log_operations();
+  LOG_INFO("Tuning selection phase completed.");
+}
+
+void Tuner::_execute() {
+  if (_time_budget_exceeded) {
+    LOG_INFO("Skip tuning execution phase because previous step exceeded the time budget.");
+    return;
+  }
+  LOG_INFO("Begin tuning execution phase...");
+
+  auto begin = RuntimeClock::now();
+
+  for (auto& operation : _operations) {
+    operation->execute();
+
+    auto runtime = RuntimeClock::now() - begin;
+    if (runtime > _execute_time_budget || runtime > _remaining_time_budget) {
+      LOG_INFO("Interrupt execution phase: Time budget exceeded.");
+      _time_budget_exceeded = true;
+      return;
     }
   }
 
-  for (const auto& choice : choices) {
+  LOG_INFO("Tuning execution phase completed.");
+}
+
+void Tuner::_log_choices() {
+  LOG_DEBUG("TuningChoice set:");
+  for (const auto& choice : _choices) {
     LOG_DEBUG("-> " << *choice);
     (void)choice;  // Silence warning about unused variable in release builds
   }
-
-  LOG_INFO("Running Selector...");
-  std::vector<std::shared_ptr<TuningOperation>> operations = _selector->select(choices, _cost_budget);
-
-  LOG_DEBUG("Operation sequence:");
-  for (const auto& operation : operations) {
-    LOG_DEBUG("-> " << *operation);
-    (void)operation;  // Silence warning about unused variable in release builds
-  }
-
-  LOG_INFO("Executing operations...");
-  _execute_operations(operations);
-
-  LOG_INFO("Tuning completed.");
 }
 
-void Tuner::_execute_operations(std::vector<std::shared_ptr<TuningOperation>>& operations) {
-  std::chrono::duration<float, std::chrono::seconds::period> timeout{_time_budget};
-  auto begin = std::chrono::high_resolution_clock::now();
-
-  for (auto& operation : operations) {
-    LOG_INFO("Executing " << *operation);
-    operation->execute();
-    auto end = std::chrono::high_resolution_clock::now();
-    if (end - begin > timeout) {
-      LOG_INFO("Execution incomplete: Time budget exceeded.");
-      break;
-    }
+void Tuner::_log_operations() {
+  LOG_DEBUG("TuningOperation sequence:");
+  for (const auto& operation : _operations) {
+    LOG_DEBUG("-> " << *operation);
+    (void)operation;  // Silence warning about unused variable in release builds
   }
 }
 
