d8c90fa0b509f73a69dfef9b78ef0eb6ba9c60db
Robert Schmid <r.schmid@outlook.com>
2018-02-19 15:56:40 +0100

    Remove unused template parameter from operator definition

--- src/lib/server/then_operator.hpp
+++ src/lib/server/then_operator.hpp
@@ -37,18 +37,18 @@ struct make_operator {
   make_operator() {}
 };
 
-template <class T, char, class O>
+template <class T, class O>
 struct half_apply {
   T&& lhs;
 };
 
 template <class Lhs, class Op>
-half_apply<Lhs, '*', Op> operator>>(Lhs&& lhs, make_operator<Op>) {
+half_apply<Lhs, Op> operator>>(Lhs&& lhs, make_operator<Op>) {
   return {std::forward<Lhs>(lhs)};
 }
 
 template <class Lhs, class Op, class Rhs>
-auto operator>>(half_apply<Lhs, '*', Op>&& lhs, Rhs&& rhs)
+auto operator>>(half_apply<Lhs, Op>&& lhs, Rhs&& rhs)
     -> decltype(invoke(std::forward<Lhs>(lhs.lhs), Op{}, std::forward<Rhs>(rhs))) {
   return invoke(std::forward<Lhs>(lhs.lhs), Op{}, std::forward<Rhs>(rhs));
 }
@@ -67,7 +67,7 @@ template <class F, class R = std::result_of_t<std::decay_t<F>()>>
 auto invoke(boost::future<void>&& lhs, then_t, F&& f) -> typename std::enable_if<is_future<R>::value, R>::type {
   return lhs
       .then(boost::launch::sync,
-            [f = std::forward<F>(f)](boost::future<void> fut) mutable->R {
+            [f = std::forward<F>(f)] (boost::future<void> fut) mutable->R {
               fut.get();
               return std::move(f)();
             })
@@ -79,7 +79,7 @@ template <class T, class F, class R = std::result_of_t<std::decay_t<F>(T)>>
 auto invoke(boost::future<T>&& lhs, then_t, F&& f) -> typename std::enable_if<is_future<R>::value, R>::type {
   return lhs
       .then(boost::launch::sync,
-            [f = std::forward<F>(f)](boost::future<T> fut) mutable->R {
+            [f = std::forward<F>(f)] (boost::future<T> fut) mutable->R {
               return std::move(f)(std::forward<T>(fut.get()));
             })
       .unwrap();
@@ -89,7 +89,7 @@ auto invoke(boost::future<T>&& lhs, then_t, F&& f) -> typename std::enable_if<is
 template <class F, class R = std::result_of_t<std::decay_t<F>()>>
 auto invoke(boost::future<void>&& lhs, then_t, F&& f) ->
     typename std::enable_if<!is_future<R>::value, boost::future<R>>::type {
-  return lhs.then(boost::launch::sync, [f = std::forward<F>(f)](boost::future<void> fut) mutable->R {
+  return lhs.then(boost::launch::sync, [f = std::forward<F>(f)] (boost::future<void> fut) mutable->R {
     fut.get();
     return std::move(f)();
   });
@@ -99,9 +99,10 @@ auto invoke(boost::future<void>&& lhs, then_t, F&& f) ->
 template <class T, class F, class R = std::result_of_t<std::decay_t<F>(T)>>
 auto invoke(boost::future<T>&& lhs, then_t, F&& f) ->
     typename std::enable_if<!is_future<R>::value, boost::future<R>>::type {
-  return lhs.then(boost::launch::sync, [f = std::forward<F>(f)](boost::future<T> fut) mutable->R {
+  return lhs.then(boost::launch::sync, [f = std::forward<F>(f)] (boost::future<T> fut) mutable->R {
     return std::move(f)(std::forward<T>(fut.get()));
   });
 };
+
 }
 }
