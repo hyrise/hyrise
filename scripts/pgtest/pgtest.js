d139c04827c29f85d7afc46d47e682eaa594faf6
Lawrence <mail@lawben.com>
2018-02-21 15:02:19 +0100

    Remove old lines from pgtest

--- scripts/pgtest/pgtest.js
+++ scripts/pgtest/pgtest.js
@@ -1,22 +1,3 @@
-// const pg = require('pg');
-//
-// const config = {
-//     host: 'localhost',
-//     port: 5432
-// };
-//
-// // pool takes the object above as parameter
-// const pool = new pg.Pool(config);
-//
-//
-// const connectionString = 'postgres://localhost:5432/postgres';
-//
-// pool.connect(connectionString, (err, client, done) => {
-//
-//     client.query('INSERT INTO items(text, complete) values($1, $2)', ['foo', 0]);
-//
-// });
-
 const { Pool } = require('pg')
 
 // pools will use environment variables
