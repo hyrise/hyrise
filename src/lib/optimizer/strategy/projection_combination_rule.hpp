591afa14248e5dda3f96332bbdf4e580f7e46348
Leander Neiss <leander.neiss@student.hpi.uni-potsdam.de>
2018-02-25 12:54:09 +0100

    Expand comment of ProjectionCombinationRule.

--- src/lib/optimizer/strategy/projection_combination_rule.hpp
+++ src/lib/optimizer/strategy/projection_combination_rule.hpp
@@ -12,7 +12,13 @@ class AbstractLQPNode;
 class ProjectionNode;
 
 /**
- * This optimizer rule combines consecutive ProjectionNodes if possible.
+ * This optimizer rule combines consecutive ProjectionNodes if they all have exactly one child/parent
+ * (Note: The node furthest up in the tree, the "root" of a ProjectionNode sequence,
+ * can have multiple parents; the one furthest down in the sequence can have multiple children).
+ *
+ * The rule removes all but the root ProjectionNode of the sequence from the tree.
+ * If the root ProjectionNode contains any LQPColumnReferences to a column in one of the ProjectionNodes
+ * to be removed, then the LQPColumnReference is replaced by the actual column.
  */
 class ProjectionCombinationRule : public AbstractRule {
  public:
