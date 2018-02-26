3626143088bc82fb87f691efe118f04d41dd43e4
Leander Neiss <leander.neiss@student.hpi.uni-potsdam.de>
2018-02-26 02:14:17 +0100

    Add test to ProjectioCombinationRuleTest.

--- src/test/optimizer/strategy/projection_combination_rule_test.cpp
+++ src/test/optimizer/strategy/projection_combination_rule_test.cpp
@@ -44,4 +44,46 @@ TEST_F(ProjectionCombinationRuleTest, CombineProjectionsTest) {
   EXPECT_EQ(projection_node->column_expressions().size(), 2u);
 }
 
+TEST_F(ProjectionCombinationRuleTest, CombineProjectionsInJoinTest) {
+  const auto query =
+      R"(SELECT *
+        FROM (
+          SELECT * FROM table_a WHERE b = 10.0 / 2.5
+        ) AS foo
+        JOIN (
+          SELECT 20, a
+          FROM (
+            SELECT * FROM table_a WHERE b = 10.0 / 2.5
+          ) AS baz
+          WHERE a = 123
+        ) AS bar
+        ON foo.a = bar.a;)";
+  const auto result_node = compile_query(query);
+
+  const auto combined = StrategyBaseTest::apply_rule(_rule, result_node);
+
+  EXPECT_EQ(combined->type(), LQPNodeType::Projection);
+  EXPECT_EQ(combined->left_child()->type(), LQPNodeType::Join);
+
+  ASSERT_EQ(combined->left_child()->left_child()->type(), LQPNodeType::Projection);
+  const auto projection_node_1 = std::static_pointer_cast<ProjectionNode>(combined->left_child()->left_child());
+  ASSERT_EQ(projection_node_1->column_expressions().size(), 2u);
+  ASSERT_EQ(projection_node_1->column_expressions()[0]->type(), ExpressionType::Column);
+  ASSERT_EQ(projection_node_1->column_expressions()[1]->type(), ExpressionType::Column);
+
+  const auto original_node_1 = projection_node_1->left_child()->left_child()->left_child();
+  EXPECT_EQ(projection_node_1->column_expressions()[0]->column_reference(), LQPColumnReference(original_node_1, ColumnID{0}));
+  EXPECT_EQ(projection_node_1->column_expressions()[1]->column_reference(), LQPColumnReference(original_node_1, ColumnID{1}));
+
+  ASSERT_EQ(combined->left_child()->right_child()->left_child()->left_child()->type(), LQPNodeType::Projection);
+  const auto projection_node_2 = std::static_pointer_cast<ProjectionNode>(combined->left_child()->right_child()->left_child()->left_child());
+  ASSERT_EQ(projection_node_2->column_expressions().size(), 2u);
+  ASSERT_EQ(projection_node_2->column_expressions()[0]->type(), ExpressionType::Column);
+  ASSERT_EQ(projection_node_2->column_expressions()[1]->type(), ExpressionType::Column);
+
+  const auto original_node_2 = projection_node_2->left_child()->left_child()->left_child();
+  EXPECT_EQ(projection_node_2->column_expressions()[0]->column_reference(), LQPColumnReference(original_node_2, ColumnID{0}));
+  EXPECT_EQ(projection_node_2->column_expressions()[1]->column_reference(), LQPColumnReference(original_node_2, ColumnID{1}));
+}
+
 }  // namespace opossum
