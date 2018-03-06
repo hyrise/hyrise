f58f2c886bd507b5a645b507728bfb45dda02d30
Tim Zimmermann <tim.zimmermann@student.hpi.uni-potsdam.de>
2018-03-06 07:52:29 +0100

    Add TPCH 22 query (#762)
    * Add TPCH 22 query
    
    * Remove legacy variable

--- src/benchmarklib/tpch/tpch_queries.hpp
+++ src/benchmarklib/tpch/tpch_queries.hpp
@@ -7,8 +7,7 @@ namespace opossum {
 constexpr size_t NUM_TPCH_QUERIES = 22;
 constexpr size_t NUM_SUPPORTED_TPCH_QUERIES = 7;
 
-extern const char* tpch_queries[21];
-extern const char* tpch_query_templates[NUM_TPCH_QUERIES];
+extern const char* tpch_queries[NUM_TPCH_QUERIES];
 
 /**
  * Indicates whether the query with a specific index is considered to be supported by Hyrise
