c57263e996ba51328827e2a7622c5449165288cc
tim-friedrich <tim.friedrich@student.hpi.uni-potsdam.de>
2018-03-06 17:14:19 +0100

    some renaming and clean up

--- src/lib/optimizer/strategy/predicate_pushdown_rule.cpp
+++ src/lib/optimizer/strategy/predicate_pushdown_rule.cpp
@@ -11,63 +11,68 @@ namespace opossum {
 std::string PredicatePushdownRule::name() const { return "Predicate Pushdown Rule"; }
 
 bool PredicatePushdownRule::apply_to(const std::shared_ptr<AbstractLQPNode>& node) {
-  // find predicate node
-  if (node->type() == LQPNodeType::Predicate) {
-    const auto parents = node->parents();
-    if (parents.empty() || parents.size() > 1) {
-      return false;
-    }
-
-    const auto predicate_node = std::dynamic_pointer_cast<PredicateNode>(node);
-
-    // can predicate node demoted?
-    auto child = node->left_child();
-    while (child->type() == LQPNodeType::Predicate) {
-      child = child->left_child();
-    }
-
-    // can be demoted?
-    if (child->type() == LQPNodeType::Join) {
-      const auto join_node = std::dynamic_pointer_cast<JoinNode>(child);
-
-      const bool valid = join_node->join_mode() == JoinMode::Inner && _predicate_value_valid(predicate_node, join_node);
-
-      if (valid) {
-        node->remove_from_tree();
-
-        const auto predicate_column = predicate_node->column_reference();
-        if (_contained_in_left_subtree(join_node, predicate_column)) {
-          const auto prev_left_child = join_node->left_child();
-          join_node->set_left_child(node);
-          node->set_left_child(prev_left_child);
-        } else if (_contained_in_right_subtree(join_node, predicate_column)) {
-          const auto prev_right_child = join_node->right_child();
-          join_node->set_right_child(node);
-          node->set_left_child(prev_right_child);
-        }
-
-        return true;
-      }
-      // push always down if other node is a sort node
-    } else if (child->type() == LQPNodeType::Sort) {
-      const auto sort_node = std::dynamic_pointer_cast<SortNode>(child);
-
-      node->remove_from_tree();
-      const auto prev_left_child = sort_node->left_child();
-
-      sort_node->set_left_child(node);
-      node->set_left_child(prev_left_child);
-
-      return true;
-    }
+  if (node->type() != LQPNodeType::Predicate) {
+    return _apply_to_children(node);
   }
 
-  return _apply_to_children(node);
-  // how far can the predicate node demoted?
-  // actually demote predicate node one or more levels
+  const auto parents = node->parents();
+  // Only predicates with exactly one parent are currently supported.
+  if (parents.empty() || parents.size() > 1) {
+    return false;
+  }
+
+  // find predicate node
+  const auto predicate_node = std::dynamic_pointer_cast<PredicateNode>(node);
+
+  auto child = node->left_child();
+  while (child->type() == LQPNodeType::Predicate) {
+    child = child->left_child();
+  }
+
+  if (child->type() == LQPNodeType::Join) {
+    const auto join_node = std::dynamic_pointer_cast<JoinNode>(child);
+
+    const bool demotable =
+        join_node->join_mode() == JoinMode::Inner && _predicate_value_demotable(predicate_node, join_node);
+
+    if (!demotable) {
+      return _apply_to_children(node);
+    }
+
+    node->remove_from_tree();
+
+    const auto predicate_column = predicate_node->column_reference();
+
+    if (_contained_in_left_subtree(join_node, predicate_column)) {
+      const auto previous_left_child = join_node->left_child();
+      join_node->set_left_child(node);
+      node->set_left_child(previous_left_child);
+    } else if (_contained_in_right_subtree(join_node, predicate_column)) {
+      const auto previous_right_child = join_node->right_child();
+      join_node->set_right_child(node);
+      node->set_left_child(previous_right_child);
+    } else {
+      DebugAssert(false, "Predicate denotion failed. The join node must be contained in one subtree.")
+    }
+
+    return true;
+
+    // push always down if other node is a sort node
+  } else if (child->type() == LQPNodeType::Sort) {
+    const auto sort_node = std::dynamic_pointer_cast<SortNode>(child);
+
+    node->remove_from_tree();
+    const auto previous_left_child = sort_node->left_child();
+
+    sort_node->set_left_child(node);
+    node->set_left_child(previous_left_child);
+
+    return true;
+  }
+  return false;
 }
 
-bool PredicatePushdownRule::_predicate_value_valid(const std::shared_ptr<PredicateNode>& predicate_node,
+bool PredicatePushdownRule::_predicate_value_demotable(const std::shared_ptr<PredicateNode>& predicate_node,
                                                    const std::shared_ptr<AbstractLQPNode>& node) const {
   // The predicate must not be demoted if it combines columns of both join partners.
   // This can happen if the value to compare against is indeed another column ID
@@ -80,8 +85,9 @@ bool PredicatePushdownRule::_predicate_value_valid(const std::shared_ptr<Predica
     const bool all_left =
         _contained_in_left_subtree(node, predicate_node->column_reference()) && _contained_in_left_subtree(node, value);
 
-    const bool all_right = _contained_in_right_subtree(node, predicate_node->column_reference()) &&
-                           _contained_in_right_subtree(node, value);
+    const bool all_right =
+        _contained_in_right_subtree(node, predicate_node->column_reference()) &&
+        _contained_in_right_subtree(node, value);
 
     return all_left ^ all_right;
   }
@@ -91,7 +97,7 @@ bool PredicatePushdownRule::_predicate_value_valid(const std::shared_ptr<Predica
 
 bool PredicatePushdownRule::_contained_in_left_subtree(const std::shared_ptr<AbstractLQPNode>& node,
                                                        const LQPColumnReference& column) const {
-  Assert(node->left_child(), "must have a left child");
+  Assert(node->left_child(), "Node must have left child to compare column to");
   return _contained_in_node(node->left_child(), column);
 }
 
