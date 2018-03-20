2474cfc70375fff57e10f28617e1d7d59677976b
Lawrence <mail@lawben.com>
2018-03-20 02:15:38 +0100

    Fix SqliterRunner queries (#792)
    * fix sqliterunner queries
    
    * Rename int_int_int_100.tbl -> id_int_int_int_100.tbl; Rename id_int_int_int_100.tbl columns int_a/b/c -> a/b/c; Adjust sqliterunner tests.
    
    * Fix PhysicalQueryPlanTest.
    
    * Format.

--- src/test/operators/physical_query_plan_test.cpp
+++ src/test/operators/physical_query_plan_test.cpp
@@ -14,9 +14,10 @@ namespace opossum {
 class PhysicalQueryPlanTest : public BaseTest {};
 
 TEST_F(PhysicalQueryPlanTest, Print) {
-  StorageManager::get().add_table("int_int_int_100", load_table("src/test/tables/sqlite/int_int_int_100.tbl", 20));
+  StorageManager::get().add_table("id_int_int_int_100",
+                                  load_table("src/test/tables/sqlite/id_int_int_int_100.tbl", 20));
 
-  auto get_table = std::make_shared<GetTable>("int_int_int_100");
+  auto get_table = std::make_shared<GetTable>("id_int_int_int_100");
   auto table_scan_a = std::make_shared<TableScan>(get_table, ColumnID{0}, PredicateCondition::GreaterThan, 20);
   auto table_scan_b = std::make_shared<TableScan>(get_table, ColumnID{1}, PredicateCondition::GreaterThan, 30);
   auto union_positions = std::make_shared<UnionPositions>(table_scan_a, table_scan_b);
@@ -26,7 +27,7 @@ TEST_F(PhysicalQueryPlanTest, Print) {
 
   EXPECT_EQ(stream.str(), R"([0] UnionPositions
  \_[1] TableScan (Col #0 > 20)
- |  \_[2] GetTable (int_int_int_100)
+ |  \_[2] GetTable (id_int_int_int_100)
  \_[3] TableScan (Col #1 > 30)
     \_Recurring Node --> [2]
 )");
