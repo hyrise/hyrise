d38ae383737d812da4af9e77674d4c749d920e61
Leander Neiss <leander.neiss@student.hpi.uni-potsdam.de>
2018-03-28 06:56:23 +0200

    Fix benchmarks that still used max_chunk_size=0 instead of Chunk::MAX_SIZE.

--- src/benchmark/operators/product_benchmark.cpp
+++ src/benchmark/operators/product_benchmark.cpp
@@ -19,7 +19,7 @@ BENCHMARK_DEFINE_F(BenchmarkBasicFixture, BM_Product)(benchmark::State& state) {
   }
 }
 BENCHMARK_REGISTER_F(BenchmarkBasicFixture, BM_Product)
-    ->Args({0})
+    ->Args({static_cast<int>(Chunk::MAX_SIZE)})
     ->Args({10000});  // for this benchmark only tables with a chunk_size of 0 and 10 000 are used. A product operation
                       // on two tables with chunk_size of 100 000 takes about one hour
 
