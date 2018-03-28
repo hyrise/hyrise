dfd7e94ce17ec9d52b4f108b9a05e39e2f2ff6ab
Lawrence <mail@lawben.com>
2018-03-28 20:12:19 +0200

    Update tpch_queries.hpp

--- src/benchmarklib/tpch/tpch_queries.hpp
+++ src/benchmarklib/tpch/tpch_queries.hpp
@@ -6,7 +6,8 @@
 namespace opossum {
 
 /**
- * Contains all supported TPCH queries. Use ordered map to have  by query id.
+ * Contains all supported TPCH queries. Use ordered map to have queries sorted by query id. 
+ * This allows for guaranteed execution order when iterating over the queries.
  */
 extern const std::map<size_t, const char*> tpch_queries;
 
