c3f469e3a1c5a1fc084681eed44a904692b6b9e1
Florian Wagner <florian.bonn@gmail.com>
2018-03-30 16:54:38 +0200

    adding some debugging

--- src/benchmark/table_generator.cpp
+++ src/benchmark/table_generator.cpp
@@ -82,6 +82,8 @@ std::shared_ptr<Table> TableGenerator::generate_table(const ChunkID chunk_size,
 std::shared_ptr<Table> TableGenerator::generate_table(
     const std::vector<ColumnDataDistribution>& column_data_distributions, const size_t num_rows,
     const size_t chunk_size, std::optional<EncodingType> encoding_type, bool use_multiple_partitions) {
+
+  DebugAssert(chunk_size != 0, "cannot generate table with chunk size 0")
   const auto num_columns = column_data_distributions.size();
   const auto num_chunks = std::ceil(static_cast<double>(num_rows) / static_cast<double>(chunk_size));
 
