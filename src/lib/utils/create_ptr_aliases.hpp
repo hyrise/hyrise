13312d7c114b62c5fc275aa8b471916ae137667a
Moritz Eyssen <Moritz.Eyssen@student.hpi.de>
2018-04-05 19:23:52 +0200

    update marcos

--- src/lib/utils/create_ptr_aliases.hpp
+++ src/lib/utils/create_ptr_aliases.hpp
@@ -7,3 +7,10 @@
   using name##CSPtr = std::shared_ptr<const name>; \
   using name##WPtr = std::weak_ptr<name>; \
   using name##CWPtr = std::weak_ptr<const name>;
+
+#define CREATE_TEMPLATE_PTR_ALIASES(name) \
+  template<typename T> using name##SPtr = std::shared_ptr<name<T>>; \
+  template<typename T> using name##CSPtr = std::shared_ptr<const name<T>>; \
+  template<typename T> using name##WPtr = std::weak_ptr<name<T>>; \
+  template<typename T> using name##CWPtr = std::weak_ptr<const name<T>>;
+
