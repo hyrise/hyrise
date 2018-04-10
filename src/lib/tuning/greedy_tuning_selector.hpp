2f6358707177017df49dc3b6678dbd3cf2a27225
Arthur Silber <arthur@arthursilber.de>
2018-04-10 18:26:42 +0200

    Adds link to example in presentation slides

--- src/lib/tuning/greedy_tuning_selector.hpp
+++ src/lib/tuning/greedy_tuning_selector.hpp
@@ -12,6 +12,10 @@ namespace opossum {
  *    Then repeatedly performs the following operations:
  * 2. Tries to free up budget by rejecting choices with lowest desirabilities ("starting from the end of list")
  * 3. Tries to fill unused budget by accepting choices with highest desirabilities ("starting from the start of the list")
+ *
+ * Also see the example at https://docs.google.com/presentation/d/1iWL86UmbMkUooi-7zyGk-Bb_GfGVPPVLVx4tmKxzKyE/edit?usp=sharing
+ * (BYOD "Self Driving Database" final presentation, starting with slide 14)
+ *
  */
 class GreedyTuningSelector : public AbstractTuningSelector {
  public:
