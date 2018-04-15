5dbc495aa8af93a752ba67851129b35a0ee0c4e8
Moritz Eyssen <Moritz.Eyssen@student.hpi.de>
2018-04-15 23:46:47 +0200

    revert naming

--- src/lib/optimizer/chunk_statistics/chunk_statistics.hpp
+++ src/lib/optimizer/chunk_statistics/chunk_statistics.hpp
@@ -15,12 +15,9 @@ namespace opossum {
  */
 class ChunkStatistics final : public std::enable_shared_from_this<ChunkStatistics> {
  public:
-  explicit ChunkStatistics(std::vector<std::shared_ptr<ChunkColumnStatistics>> chunk_column_statistics)
-      : _chunk_column_statistics(chunk_column_statistics) {}
+  explicit ChunkStatistics(std::vector<std::shared_ptr<ChunkColumnStatistics>> statistics) : _statistics(statistics) {}
 
-  const std::vector<std::shared_ptr<ChunkColumnStatistics>>& chunk_column_statistics() const {
-    return _chunk_column_statistics;
-  }
+  const std::vector<std::shared_ptr<ChunkColumnStatistics>>& statistics() const { return _statistics; }
 
   /**
    * calls can_prune on the ChunkColumnStatistics corresponding to column_id
@@ -28,6 +25,6 @@ class ChunkStatistics final : public std::enable_shared_from_this<ChunkStatistic
   bool can_prune(const ColumnID column_id, const AllTypeVariant& value, const PredicateCondition predicate_type) const;
 
  protected:
-  std::vector<std::shared_ptr<ChunkColumnStatistics>> _chunk_column_statistics;
+  std::vector<std::shared_ptr<ChunkColumnStatistics>> _statistics;
 };
 }  // namespace opossum
