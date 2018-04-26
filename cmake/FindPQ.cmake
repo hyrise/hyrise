68effdb8e7148a3c5f5db19a35885e606571e930
mrks <markus@dreseler.de>
2018-04-26 08:04:15 +0200

    Try fix cmake libpq (#851)

--- cmake/FindPQ.cmake
+++ cmake/FindPQ.cmake
@@ -5,11 +5,11 @@
 #   PQ_LIBRARY     : library path of postgres' pq
 
 FIND_PATH(PQ_INCLUDE_DIR NAME libpq-fe.h HINTS
-    $ENV{HOME}/local/include /opt/local/include /usr/local/include /usr/include /usr/local/include/postgresql /usr/include/postgresql
+    $ENV{HOME}/local/include /opt/local/include /usr/local/include /usr/include /usr/local/include/postgresql /usr/include/postgresql /usr/local/opt/libpq/include
 )
 
 FIND_LIBRARY(PQ_LIBRARY NAME pq HINTS
-    "$ENV{LIB_DIR}/lib" "$ENV{LIB}/lib" "$ENV{HOME}/local/lib64" "$ENV{HOME}/local/lib" /usr/local/lib64 /usr/local/lib /opt/local/lib64 /opt/local/lib /usr/lib64 /usr/lib
+    "$ENV{LIB_DIR}/lib" "$ENV{LIB}/lib" "$ENV{HOME}/local/lib64" "$ENV{HOME}/local/lib" /usr/local/lib64 /usr/local/lib /opt/local/lib64 /opt/local/lib /usr/lib64 /usr/lib /usr/local/opt/libpq/lib
 )
 
 IF(PQ_INCLUDE_DIR AND PQ_LIBRARY)
