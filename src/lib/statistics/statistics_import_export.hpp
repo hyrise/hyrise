a173f6e19086a9da8d4161c48fe4bbbcb4926f58
Moritz Eyssen <Moritz.Eyssen@student.hpi.de>
2018-04-27 10:20:49 +0200

    2nd attempt

---
+++ src/lib/statistics/statistics_import_export.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <iostream>
+
+#include "json.hpp"
+
+#include "table_statistics.hpp"
+
+namespace opossum {
+
+TableStatistics import_table_statistics(const std::string& path);
+void export_table_statistics(const TableStatistics& table_statistics, const std::string& path);
+
+TableStatistics import_table_statistics(std::istream& stream);
+void export_table_statistics(const TableStatistics& table_statistics, std::ostream& stream);
+
+TableStatistics import_table_statistics(const nlohmann::json& json);
+std::shared_ptr<AbstractColumnStatistics> import_column_statistics(const nlohmann::json& json);
+
+nlohmann::json export_table_statistics(const TableStatistics& table_statistics);
+nlohmann::json export_column_statistics(const AbstractColumnStatistics& column_statistics);
+}  // namespace opossum
