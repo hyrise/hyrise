20b5b584611af11bee05509c1b157f174d108c0a
Markus Dreseler <markus.dreseler@hpi.de>
2018-04-30 11:42:40 +0200

    Fix NUMA test, improve isolation

--- src/lib/utils/numa_memory_resource.cpp
+++ src/lib/utils/numa_memory_resource.cpp
@@ -3,6 +3,8 @@
 #include <boost/math/common_factor_rt.hpp>
 #include <string>
 
+#include "utils/assert.hpp"
+
 #if HYRISE_NUMA_SUPPORT
 #define NUMA_MEMORY_RESOURCE_ARENA_SIZE 1LL << 30
 #else
@@ -21,15 +23,6 @@ void* NUMAMemoryResource::do_allocate(std::size_t bytes, std::size_t alignment) 
 
 void NUMAMemoryResource::do_deallocate(void* p, std::size_t bytes, std::size_t alignment) { _memory_source.free(p); }
 
-bool NUMAMemoryResource::do_is_equal(const memory_resource& other) const noexcept {
-  const auto other_numa_resource = dynamic_cast<const NUMAMemoryResource*>(&other);
-  if (other_numa_resource == NULL) {
-    return false;
-  } else {
-    return other_numa_resource->_memory_source == _memory_source;
-  }
-}
-
 int NUMAMemoryResource::get_node_id() const { return _memory_source.getPhysicalNode(); }
 
 #else
@@ -44,9 +37,23 @@ void NUMAMemoryResource::do_deallocate(void* p, std::size_t bytes, std::size_t a
   boost::container::pmr::get_default_resource()->deallocate(p, bytes, alignment);
 }
 
-bool NUMAMemoryResource::do_is_equal(const memory_resource& other) const noexcept { return true; }
-
 int NUMAMemoryResource::get_node_id() const { return UNDEFINED_NODE_ID; }
 #endif
+
+bool NUMAMemoryResource::do_is_equal(const memory_resource& other) const noexcept {
+  // "Two memory_resources compare equal if and only if memory allocated from one memory_resource can be deallocated
+  // from the other and vice versa."
+  //
+  // This function is problematic if we compare the behavior with and without HYRISE_NUMA_SUPPORT:
+  //
+  // auto resource_a = NUMAMemoryResource(numa_node, "foo");
+  // auto resource_b = NUMAMemoryResource(numa_node, "bar");
+  // resource_a.is_equal(resource_b); // false if with NUMA support, true if without
+  //
+  // This is not used right now and I am not sure in what context this might be needed. Let's prevent any surprises
+  // if someone decides to use this in the future:
+
+  Fail("NUMAMemoryResource::do_is_equal is not implemented - see comment");
+}
 
 }  // namespace opossum
