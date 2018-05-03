780c00cabb549f2f68201e60f7d6ec1c815163e7
Markus Dreseler <markus.dreseler@hpi.de>
2018-05-03 09:58:47 +0200

    format

--- src/benchmark/operators/table_scan_benchmark.cpp
+++ src/benchmark/operators/table_scan_benchmark.cpp
@@ -47,21 +47,21 @@ BENCHMARK_F(BenchmarkBasicFixture, BM_TableScan_Like)(benchmark::State& state) {
   lineitem_wrapper->execute();
 
   const auto column_names_and_patterns = std::vector<std::pair<std::string, std::string>>({
-                                                                                          {"l_comment", "%final%"},
-                                                                                          {"l_comment", "%final%requests%"},
-                                                                                          {"l_shipinstruct", "quickly%"},
-                                                                                          {"l_comment", "%foxes"},
-                                                                                          {"l_comment", "%quick_y__above%even%"},
+      {"l_comment", "%final%"},
+      {"l_comment", "%final%requests%"},
+      {"l_shipinstruct", "quickly%"},
+      {"l_comment", "%foxes"},
+      {"l_comment", "%quick_y__above%even%"},
   });
 
   while (state.KeepRunning()) {
     for (const auto& column_name_and_pattern : column_names_and_patterns) {
-      auto table_scan = std::make_shared<TableScan>(lineitem_wrapper, lineitem_table->column_id_by_name(column_name_and_pattern.first),
+      auto table_scan = std::make_shared<TableScan>(lineitem_wrapper,
+                                                    lineitem_table->column_id_by_name(column_name_and_pattern.first),
                                                     PredicateCondition::Like, column_name_and_pattern.second);
       table_scan->execute();
     }
   }
 }
-
 
 }  // namespace opossum
