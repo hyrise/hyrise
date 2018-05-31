d45299472fe5b76e461150b48f99d904f4b98d05
Lawrence <mail@lawben.com>
2018-05-31 15:38:26 +0200

    Fix git version file

--- src/lib/version.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// We need this file to store the current GIT hash and make it available to our binaries.
-
-// The SHA1 for the HEAD of the repo.
-#define GIT_HEAD_SHA1 "210f815bacc2cd2caad1bc3a01da6c2cea89b4e4"
-
-// Whether or not there were uncommitted changes present.
-#define GIT_IS_DIRTY true
