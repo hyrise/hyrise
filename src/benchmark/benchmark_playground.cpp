bf400112e7fbf62da1d57fffab7e8e1193637600
Adrian Holfter <git@adrian.holfter.de>
2018-06-19 17:35:27 +0200

    add hyriseBenchmarkPlayground

---
+++ src/benchmark/benchmark_playground.cpp
@@ -0,0 +1,70 @@
+#include <memory>
+
+#include "benchmark/benchmark.h"
+
+#include "benchmark_basic_fixture.hpp"
+#include "operators/sort.hpp"
+#include "operators/table_wrapper.hpp"
+
+namespace opossum {
+
+using ValueT = int32_t;
+
+class BenchmarkPlaygroundFixture : public BenchmarkBasicFixture {
+ public:
+  void SetUp(::benchmark::State& state) override {
+    BenchmarkBasicFixture::SetUp(state);
+
+    _vec.resize(1'000'000);
+    std::generate(_vec.begin(), _vec.end(), []() {
+      static ValueT v = 0;
+      v = ++v % 1'000;
+      return v;
+    });
+  }
+  void TearDown(::benchmark::State& state) override { BenchmarkBasicFixture::TearDown(state); }
+
+ protected:
+  std::vector<ValueT> _vec;
+};
+
+BENCHMARK_F(BenchmarkPlaygroundFixture, BM_Playground_Reference)(benchmark::State& state) {
+  clear_cache();
+
+  // warm up
+  for (const auto& element : _vec) {
+    benchmark::DoNotOptimize(element);
+  }
+
+  while (state.KeepRunning()) {
+    std::vector<size_t> result;
+    auto size = _vec.size();
+    for (size_t i = 0; i < size; ++i) {
+      if (_vec[i] == 999) {
+        result.push_back(i);
+      }
+    }
+  }
+}
+
+BENCHMARK_F(BenchmarkPlaygroundFixture, BM_Playground_NewApproach)(benchmark::State& state) {
+  clear_cache();
+
+  // warm up
+  for (const auto& element : _vec) {
+    benchmark::DoNotOptimize(element);
+  }
+
+  while (state.KeepRunning()) {
+    std::vector<size_t> result;
+    result.reserve(1000);
+    auto size = _vec.size();
+    for (size_t i = 0; i < size; ++i) {
+      if (_vec[i] == 999) {
+        result.push_back(i);
+      }
+    }
+  }
+}
+
+}  // namespace opossum
