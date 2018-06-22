1f99b2cea133ccd88f7fb996a4cdfb7131ae1bbf
Adrian Holfter <git@adrian.holfter.de>
2018-06-22 09:57:45 +0200

    PR review

--- src/benchmark/benchmark_playground.cpp
+++ src/benchmark/benchmark_playground.cpp
@@ -5,6 +5,26 @@
 
 namespace opossum {
 
+/**
+ * Welcome to the benchmark playground. Here, you can quickly compare two
+ * approaches in a minimal setup. Of course you can also use it to just benchmark
+ * one single thing.
+ *
+ * In this example, a minimal TableScan-like operation is used to evaluate the
+ * performance impact of pre-allocating the result vector (PosList in hyrise).
+ *
+ * A few tips:
+ * * The optimizer is not your friend. If you do a bunch of calculations and
+ *   don't actually use the result, it will optimize your code out and you will
+ *   benchmark only noise.
+ * * benchmark::ClobberMemory(); can be used to force calculations to be written
+ *   to memory.
+ * * benchmark::DoNotOptimize(<expression>); can be used to prevent <expression>
+ *   from being optimized out. However, this does not stop the optimizer from
+ *   optimizing the expression itself!
+ *
+ */
+
 using ValueT = int32_t;
 
 class BenchmarkPlaygroundFixture : public BenchmarkBasicFixture {
@@ -12,6 +32,10 @@ class BenchmarkPlaygroundFixture : public BenchmarkBasicFixture {
   void SetUp(::benchmark::State& state) override {
     BenchmarkBasicFixture::SetUp(state);
 
+    clear_cache();
+
+    // Fill the vector with 1M values in the pattern 0, 1, 2, 3, 0, 1, 2, 3, ...
+    // The "TableScan" will scan for one value (2), so it will select 25%.
     _vec.resize(1'000'000);
     std::generate(_vec.begin(), _vec.end(), []() {
       static ValueT v = 0;
@@ -25,12 +49,14 @@ class BenchmarkPlaygroundFixture : public BenchmarkBasicFixture {
   std::vector<ValueT> _vec;
 };
 
+/**
+ * Reference implementation, growing the vector on demand
+ */
 BENCHMARK_F(BenchmarkPlaygroundFixture, BM_Playground_Reference)(benchmark::State& state) {
-  clear_cache();
+  // Add some benchmark-specific setup here
 
   while (state.KeepRunning()) {
     std::vector<size_t> result;
-    benchmark::DoNotOptimize(result.data());
     auto size = _vec.size();
     for (size_t i = 0; i < size; ++i) {
       if (_vec[i] == 2) {
@@ -38,17 +64,20 @@ BENCHMARK_F(BenchmarkPlaygroundFixture, BM_Playground_Reference)(benchmark::Stat
         benchmark::ClobberMemory();  // Force that record to be written to memory
       }
     }
+    benchmark::DoNotOptimize(result.data());  // Do not optimize out the vector
   }
 }
 
+/**
+ * Alternative implementation, pre-allocating the vector
+ */
 BENCHMARK_F(BenchmarkPlaygroundFixture, BM_Playground_PreAllocate)(benchmark::State& state) {
-  clear_cache();
+  // Add some benchmark-specific setup here
 
   while (state.KeepRunning()) {
     std::vector<size_t> result;
     // pre-allocate result vector
     result.reserve(250'000);
-    benchmark::DoNotOptimize(result.data());
     auto size = _vec.size();
     for (size_t i = 0; i < size; ++i) {
       if (_vec[i] == 2) {
@@ -56,6 +85,7 @@ BENCHMARK_F(BenchmarkPlaygroundFixture, BM_Playground_PreAllocate)(benchmark::St
         benchmark::ClobberMemory();  // Force that record to be written to memory
       }
     }
+    benchmark::DoNotOptimize(result.data());  // Do not optimize out the vector
   }
 }
 
