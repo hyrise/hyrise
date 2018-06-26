54154f92985f535dfcbdeab8906830aecb9b0fd3
Dimitri Schmidt <dimitri.schmidt@student.hpi.de>
2018-06-26 17:26:26 +0200

    mock RecoverDatabaseTask

--- src/test/server/mock_task_runner.hpp
+++ src/test/server/mock_task_runner.hpp
@@ -11,6 +11,7 @@
 #include "tasks/server/execute_server_prepared_statement_task.hpp"
 #include "tasks/server/execute_server_query_task.hpp"
 #include "tasks/server/load_server_file_task.hpp"
+#include "tasks/server/recover_database_task.hpp"
 
 namespace opossum {
 
@@ -24,6 +25,7 @@ class MockTaskRunner {
                boost::future<std::shared_ptr<const Table>>(std::shared_ptr<ExecuteServerPreparedStatementTask>));
   MOCK_METHOD1(dispatch_server_task, boost::future<void>(std::shared_ptr<ExecuteServerQueryTask>));
   MOCK_METHOD1(dispatch_server_task, boost::future<void>(std::shared_ptr<LoadServerFileTask>));
+  MOCK_METHOD1(dispatch_server_task, boost::future<void>(std::shared_ptr<RecoverDatabaseTask>));
 };
 
 }  // namespace opossum
