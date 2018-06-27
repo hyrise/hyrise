387a5841ba0e531dd6f08e3bf99f994fe1d915b4
Toni Stachewicz <toni.stachewicz@student.hpi.de>
2018-06-27 15:56:22 +0200

    dont need lib

--- cmake/FindSYSTEMTAP.cmake
+++ cmake/FindSYSTEMTAP.cmake
@@ -1,18 +1,13 @@
 # Find the systemtap library.
 # Output variables:
 #  SYSTEMTAP_INCLUDE_DIR : e.g., /usr/include/.
-#  SYSTEMTAP_LIBRARY     : Library path of systemtap library
 #  SYSTEMTAP_FOUND       : True if found.
 FIND_PATH(SYSTEMTAP_INCLUDE_DIR NAME sys/sdt.h
     HINTS $ENV{HOME}/local/include /opt/local/include /usr/local/include /usr/include /usr/include/x86_64-linux-gnu/ )
 
-FIND_LIBRARY(SYSTEMTAP_LIBRARY NAME libstapsdt.so
-    HINTS $ENV{HOME}/local/lib64 $ENV{HOME}/local/lib /usr/local/lib64 /usr/local/lib /opt/local/lib64 /opt/local/lib /usr/lib64 /usr/lib
-    )
-
-IF (SYSTEMTAP_INCLUDE_DIR AND SYSTEMTAP_LIBRARY)
+IF (SYSTEMTAP_INCLUDE_DIR)
     SET(SYSTEMTAP_FOUND TRUE)
-    MESSAGE(STATUS "Found systemtap library: inc=${SYSTEMTAP_INCLUDE_DIR}, lib=${SYSTEMTAP_LIBRARY}")
+    MESSAGE(STATUS "Found systemtap library: inc=${SYSTEMTAP_INCLUDE_DIR}")
 ELSE ()
     SET(SYSTEMTAP_FOUND FALSE)
     MESSAGE(STATUS "WARNING: systemtap library not found.")
