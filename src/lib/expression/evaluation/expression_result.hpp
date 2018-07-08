eaa0c0f89ce528490a9856e06d3fddeab4469c37
Moritz Eyssen <Moritz.Eyssen@student.hpi.de>
2018-07-08 10:59:34 +0200

    docs

--- src/lib/expression/evaluation/expression_result.hpp
+++ src/lib/expression/evaluation/expression_result.hpp
@@ -9,9 +9,9 @@
 #include "storage/column_iterables/column_iterator_values.hpp"
 #include "storage/create_iterable_from_column.hpp"
 #include "utils/assert.hpp"
+#include "expression_result_views.hpp"
 
 namespace opossum {
-
 
 class BaseExpressionResult {
  public:
@@ -87,91 +87,6 @@ class ExpressionResult : public BaseExpressionResult {
 
   std::vector<T> values;
   std::vector<bool> nulls;
-};
-
-/**
- * View that looks at an ExpressionResult knowing that is a series and may contains nulls
- */
-template <typename T>
-class ExpressionResultNullableSeries {
- public:
-  using Type = T;
-
-  ExpressionResultNullableSeries(const std::vector<T>& values, const std::vector<bool>& nulls)
-      : _values(values), _nulls(nulls) {}
-
-  bool is_series() const { return true; }
-  bool is_literal() const { return false; }
-  bool is_nullable() const { return !_nulls.empty(); }
-
-  const T& value(const size_t idx) const {
-    DebugAssert(idx < _values.size(), "Index out of range");
-    return _values[idx];
-  }
-
-  size_t size() const { return _values.size(); }
-
-  bool null(const size_t idx) const {
-    DebugAssert(idx < _values.size(), "Index out of range");
-    return _nulls[idx];
-  }
-
- private:
-  const std::vector<T>& _values;
-  const std::vector<bool>& _nulls;
-};
-
-/**
- * View that looks at an ExpressionResult knowing that is a series, but may not return nulls, so null() always returns
- * false
- */
-template <typename T>
-class ExpressionResultNonNullSeries {
- public:
-  using Type = T;
-
-  explicit ExpressionResultNonNullSeries(const std::vector<T>& values) : _values(values) {}
-
-  bool is_series() const { return true; }
-  bool is_literal() const { return false; }
-  bool is_nullable() const { return false; }
-
-  size_t size() const { return _values.size(); }
-
-  const T& value(const size_t idx) const {
-    DebugAssert(idx < _values.size(), "Index out of range");
-    return _values[idx];
-  }
-
-  bool null(const size_t idx) const { return false; }
-
- private:
-  const std::vector<T>& _values;
-};
-
-/**
- * View that looks at an ExpressionResult knowing that is a literal, so always returns the first element in value() and
- * null(), no matter which index is requested
- */
-template <typename T>
-class ExpressionResultLiteral {
- public:
-  using Type = T;
-
-  ExpressionResultLiteral(const T& value, const bool null) : _value(value), _null(null) {}
-
-  bool is_series() const { return false; }
-  bool is_literal() const { return true; }
-  bool is_nullable() const { return _null; }
-
-  size_t size() const { return 1u; }
-
-  const T& value(const size_t = 0) const { return _value; }
-  bool null(const size_t = 0) const { return _null; }
-
- private:
-  T _value;
-  bool _null;
 };
 
 }  // namespace opossum
