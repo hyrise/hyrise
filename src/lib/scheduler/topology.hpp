339c8ac1f47701c631b20f9fe0d930352bf4920a
Leander Neiss <leander.neiss@student.hpi.uni-potsdam.de>
2018-07-14 19:30:25 +0200

    Add comments to topology.hpp.

--- src/lib/scheduler/topology.hpp
+++ src/lib/scheduler/topology.hpp
@@ -24,16 +24,45 @@ struct TopologyNode final {
 };
 
 /**
- * Encapsulates the Machine Architecture, i.e. how many Nodes/Cores there are and how to distribute
- * Workers/Queues among them.
+ * Topology is a singleton that encapsulates the Machine Architecture, i.e. how many Nodes/Cores there are.
+ * It is initialized with the actual system topology by default, but can be newly initialized with a custom topology
+ * if needed, e.g. for testing purposes.
+ *
+ * The 'init_*_topology()' methods replace the current topology information by the new one, and should be used carefully.
  */
 class Topology final {
  public:
   static Topology& current();
 
+  /**
+   * Initialize with the default system topology.
+   *
+   * Calls init_numa_topology() or init_non_numa_topology() if on a NUMA or non-NUMA system respectively.
+   */
   void init_default_topology();
+
+  /**
+   * Initialize with a NUMA topology.
+   * The topology has a number of cores equal to either max_num_cores or the number of physically availyble cores,
+   * whichever one is lower. The cores are distributed among the available NUMA nodes in a way that a node is filled up
+   * to it's maximum core count first, bevor a new node is added.
+   *
+   * Calls init_fake_numa_topology() if on a non-NUMA system.
+   */
   void init_numa_topology(uint32_t max_num_cores = 0);
+
+  /**
+   * Initialize with a non-NUMA topology.
+   * The topology has one node, and a number of cores equal to either max_num_cores or the number of physically
+   * availyble cores, whichever one is lower.
+   */
   void init_non_numa_topology(uint32_t max_num_cores = 0);
+
+  /**
+   * Initialize with a fake-NUMA topology.
+   * The topology has a number of cores equal to either max_num_cores or the number of physically availyble cores,
+   * whichever one is lower. Virtual NUMA nodes are created based on the workers_per_node parameter.
+   */
   void init_fake_numa_topology(uint32_t max_num_workers = 0, uint32_t workers_per_node = 1);
 
   const std::vector<TopologyNode>& nodes();
