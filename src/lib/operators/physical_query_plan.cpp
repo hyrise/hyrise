5174f7402757028021afd703153d42328dd72168
Moritz Eyssen <Moritz.Eyssen@student.hpi.de>
2018-07-15 21:50:03 +0200

    remove physical_query_plan_*

--- src/lib/operators/physical_query_plan.cpp
+++
@@ -1,9 +0,0 @@
-#include "physical_query_plan.hpp"
-
-namespace opossum {
-
-PhysicalQueryPlan::PhysicalQueryPlan(const std::shared_ptr<AbstractOperator>& root_op,
-                                     const std::unordered_map<ValuePlaceholderID, ParameterID>& value_placeholders)
-    : root_op(root_op), value_placeholders(value_placeholders) {}
-
-}  // namespace opossum
