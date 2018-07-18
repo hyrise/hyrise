03af9ead87963d53fa9f7ac1282758e2d7c2cf25
Fabian Wiebe <fabian.wiebe@student.hpi.de>
2018-07-18 18:13:50 +0200

    Fix issue fromformatting

--- src/test/operators/jit_operator/specialization/get_runtime_pointer_for_value_test.cpp
+++ src/test/operators/jit_operator/specialization/get_runtime_pointer_for_value_test.cpp
@@ -98,7 +98,7 @@ class GetRuntimePointerForValueTest : public BaseTest {
   llvm::Value* _value_5;
   llvm::Value* _value_6;
   llvm::Value* _value_7;
-}
+};
 
 TEST_F(GetRuntimePointerForValueTest, BitcodePointerInstructionsAreProperlySimulated) {
   bitcode_pointer_test();
