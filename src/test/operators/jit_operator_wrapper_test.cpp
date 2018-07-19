4e3e6e0bacec28467ae9d5fe0eb3faf36af249e1
Fabian Wiebe <fabian.wiebe@student.hpi.de>
2018-07-19 11:28:28 +0200

    Reenable test as also other test fail

--- src/test/operators/jit_operator_wrapper_test.cpp
+++ src/test/operators/jit_operator_wrapper_test.cpp
@@ -122,9 +122,7 @@ TEST_F(JitOperatorWrapperTest, CallsJitOperatorHooks) {
   jit_operator_wrapper.execute();
 }
 
-// ToDo(anyone) Fix inline methods (this test only fails on the test server, it works on Pella)
-// See issue #933 (https://github.com/hyrise/hyrise/issues/976)
-TEST_F(JitOperatorWrapperTest, DISABLED_JitOperatorsSpecializedWithMultipleInliningOfSameFunction) {
+TEST_F(JitOperatorWrapperTest, JitOperatorsSpecializedWithMultipleInliningOfSameFunction) {
   // During query specialization, the function calls of JitExpression::compute are inlined with two different objects:
   // First the compute function call with the object "expression" is inlined,
   // then the two function calls with the two time referenced object "column_expression" are inlined.
