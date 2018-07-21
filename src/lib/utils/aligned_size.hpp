3502618c7bec0a4749fcbc72c5be9caaadf439af
Markus Dreseler <markus.dreseler@hpi.de>
2018-07-21 17:54:14 +0200

    missing file

---
+++ src/lib/utils/aligned_size.hpp
@@ -0,0 +1,9 @@
+namespace opossum {
+
+// returns the actually used size by an object of type T
+template <typename T>
+constexpr size_t aligned_size() {
+  return sizeof(T) + alignof(T) - 1 - (sizeof(T) - 1) % alignof(T);
+};
+
+}  // namespace opossum
