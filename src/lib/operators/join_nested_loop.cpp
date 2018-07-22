2d417b45f8cc722af9a2c0590a79d7d3f7d972f1
Markus Dreseler <markus.dreseler@hpi.de>
2018-07-22 12:09:54 +0200

    bla

--- src/lib/operators/join_nested_loop.cpp
+++ src/lib/operators/join_nested_loop.cpp
@@ -137,7 +137,7 @@ void JoinNestedLoop::_join_two_untyped_columns(const std::shared_ptr<const BaseC
           iterable_right.with_iterators([&](auto right_it, auto right_end) {
             with_comparator(params.predicate_condition, [&](auto comparator) {
               _join_two_typed_columns(comparator, left_it, left_end, right_it, right_end, chunk_id_left,
-                                      chunk_id_right, const_cast<JoinNestedLoop::JoinParams>(params));
+                                      chunk_id_right, params);
             });
           });
         });
