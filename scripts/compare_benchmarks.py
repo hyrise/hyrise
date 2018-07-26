59db6b64763d564792931b20a945516eade7c708
Adrian Holfter <git@adrian.holfter.de>
2018-07-26 14:21:32 +0200

    replace BeautifulTable with TerminalTables

--- scripts/compare_benchmarks.py
+++ scripts/compare_benchmarks.py
@@ -2,7 +2,7 @@
 
 import json
 import sys
-from beautifultable import BeautifulTable
+from terminaltables import AsciiTable
 from termcolor import colored
 from scipy.stats import ttest_ind, combine_pvalues
 from array import array
@@ -14,7 +14,7 @@ def format_diff(diff):
 		return colored("{0:.0%}".format(diff), 'red')
 
 def format_p_value(p_value):
-	if p_value < 0.05:
+	if p_value < 0.001:
 		return colored("{0:.5}".format(p_value), 'green')
 	else:
 		return colored("{0:.5}".format(p_value), 'red')
@@ -29,15 +29,8 @@ with open(sys.argv[1]) as old_file:
 with open(sys.argv[2]) as new_file:
     new_data = json.load(new_file)
 
-table = BeautifulTable(default_alignment = BeautifulTable.ALIGN_LEFT, max_width=100)
-table.column_headers = ["Benchmark", "prev. iter/s", "new iter/s", "change", "p-value"]
-table.column_widths = [10, 10, 10, 20, 20]
-table.width_exceed_policy = BeautifulTable.WEP_ELLIPSIS
-table.row_separator_char = ''
-table.top_border_char = ''
-table.bottom_border_char = ''
-table.left_border_char =''
-table.right_border_char =''
+table_data = []
+table_data.append(["Benchmark", "prev. iter/s", "runs", "new iter/s", "runs", "change", "p-value (significant if <0.001)"])
 
 average_diff_sum = 0.0
 p_values = []
@@ -47,8 +40,6 @@ for old, new in zip(old_data['benchmarks'], new_data['benchmarks']):
 		print("Benchmark name mismatch")
 		exit()
 	diff = float(new['items_per_second']) / float(old['items_per_second']) - 1
-	old_rsd = "{0:.0%}".format(old['real_time_per_iteration_relative_standard_deviation'])
-	new_rsd = "{0:.0%}".format(new['real_time_per_iteration_relative_standard_deviation'])
 	average_diff_sum += diff
 	diff_formatted = format_diff(diff)
 
@@ -56,13 +47,14 @@ for old, new in zip(old_data['benchmarks'], new_data['benchmarks']):
 	p_value_formatted = format_p_value(p_value)
 	p_values.append(p_value)
 
-
-	table.append_row([old['name'], str(old['items_per_second']), str(new['items_per_second']), diff_formatted, p_value_formatted])
+	table_data.append([old['name'], str(old['items_per_second']), str(old['iterations']), str(new['items_per_second']), str(new['iterations']), diff_formatted, p_value_formatted])
 
 p_values_combined = combine_pvalues(p_values)[1]
 p_values_combined_formatted = format_p_value(p_values_combined)
-table.append_row(['average', '', '', format_diff(average_diff_sum / len(old_data['benchmarks'])), p_values_combined_formatted])
+table_data.append(['average', '', '', '', '', format_diff(average_diff_sum / len(old_data['benchmarks'])), p_values_combined_formatted])
+
+table = AsciiTable(table_data)
 
 print("")
-print(table)
+print(table.table)
 print("")
