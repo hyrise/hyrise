1978636167e8b80f0e50ef3239304bd549c3c3c5
Toni Stachewicz <toni.stachewicz@student.hpi.de>
2018-08-15 15:18:25 +0200

    performance tracking per operator

---
+++ scripts/track_time_by_operator.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import sys
+import subprocess
+
+command = "sudo stap staps/query_analyzer.stp -DMAXMAPENTRIES=10000 -DMAXACTION=100000000 -G debug=0 -c \"{}\" {}".format(' '.join(sys.argv[1:]), sys.argv[1])
+process = subprocess.Popen(command, shell=True)
+
+process.communicate()
