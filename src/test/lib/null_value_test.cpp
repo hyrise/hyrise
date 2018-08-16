c4f48a8ec23b136d26320395c1342edc92635312
martin.boissier <martin.boissier@hpi.de>
2018-08-16 10:17:08 +0200

    Add NullValue tests

---
+++ src/test/lib/null_value_test.cpp
@@ -0,0 +1,27 @@
+#include <memory>
+#include <string_view>
+
+#include "../base_test.hpp"
+#include "gtest/gtest.h"
+#include "null_value.hpp"
+#include "all_type_variant.hpp"
+
+namespace opossum {
+
+class NullValueTest : public BaseTest {
+};
+
+TEST_F(NullValueTest, Comparators) {
+  auto null_0 = AllTypeVariant{};
+  auto null_1 = AllTypeVariant{};
+
+  EXPECT_FALSE(null_0 == null_1);
+  EXPECT_FALSE(null_0 != null_1);
+  EXPECT_FALSE(null_0 <  null_1);
+  EXPECT_FALSE(null_0 <= null_1);
+  EXPECT_FALSE(null_0 >  null_1);
+  EXPECT_FALSE(null_0 >= null_1);
+
+  // EXPECT_TRUE(-null_0 == NullValue{});
+}
+}  // namespace opossum
