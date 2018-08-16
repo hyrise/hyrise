4a62ad025ce14c94131fe62a4861ee03d103e169
martin.boissier <martin.boissier@hpi.de>
2018-08-16 13:20:50 +0200

    Add test for AllTypeVariant NULLs

--- src/test/lib/null_value_test.cpp
+++ src/test/lib/null_value_test.cpp
@@ -11,7 +11,19 @@ namespace opossum {
 class NullValueTest : public BaseTest {
 };
 
-TEST_F(NullValueTest, Comparators) {
+TEST_F(NullValueTest, NullValueComparators) {
+  auto null_0 = NullValue{};
+  auto null_1 = NullValue{};
+
+  EXPECT_FALSE(null_0 == null_1);
+  EXPECT_FALSE(null_0 != null_1);
+  EXPECT_FALSE(null_0 <  null_1);
+  EXPECT_FALSE(null_0 <= null_1);
+  EXPECT_FALSE(null_0 >  null_1);
+  EXPECT_FALSE(null_0 >= null_1);
+}
+
+TEST_F(NullValueTest, AllTypeVariantNullComparators) {
   auto null_0 = AllTypeVariant{};
   auto null_1 = AllTypeVariant{};
 
@@ -21,7 +33,6 @@ TEST_F(NullValueTest, Comparators) {
   EXPECT_FALSE(null_0 <= null_1);
   EXPECT_FALSE(null_0 >  null_1);
   EXPECT_FALSE(null_0 >= null_1);
-
-  // EXPECT_TRUE(-null_0 == NullValue{});
 }
+
 }  // namespace opossum
