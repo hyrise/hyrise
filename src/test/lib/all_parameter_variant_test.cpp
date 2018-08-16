64a3dae2a747bc62073bdd7695e185e032680366
martin.boissier <martin.boissier@hpi.de>
2018-08-16 17:16:31 +0200

    Change test method

--- src/test/lib/all_parameter_variant_test.cpp
+++ src/test/lib/all_parameter_variant_test.cpp
@@ -3,8 +3,8 @@
 #include "../base_test.hpp"
 #include "gtest/gtest.h"
 
-#include "storage/storage_manager.hpp"
 #include "logical_query_plan/stored_table_node.hpp"
+#include "storage/storage_manager.hpp"
 
 #include "all_parameter_variant.hpp"
 #include "type_cast.hpp"
@@ -87,11 +87,11 @@ TEST_F(AllParameterVariantTest, GetCurrentValue) {
 TEST_F(AllParameterVariantTest, ToString) {
   {
     const AllParameterVariant parameter(ParameterID{17});
-    EXPECT_TRUE(to_string(parameter) == "Placeholder #17");
+    EXPECT_EQ(to_string(parameter), "Placeholder #17");
   }
   {
     const AllParameterVariant parameter(ColumnID{17});
-    EXPECT_TRUE(to_string(parameter) == "Col #17");
+    EXPECT_EQ(to_string(parameter), "Col #17");
   }
   {
     StorageManager::get().add_table("int_float", load_table("src/test/tables/int_float.tbl"));
@@ -100,14 +100,14 @@ TEST_F(AllParameterVariantTest, ToString) {
     const LQPColumnReference col_b = {int_float, ColumnID{1}};
 
     const AllParameterVariant parameter_col_a(col_a);
-    EXPECT_TRUE(to_string(parameter_col_a) == "a");
+    EXPECT_EQ(to_string(parameter_col_a), "a");
 
     const AllParameterVariant parameter_col_b(col_b);
-    EXPECT_TRUE(to_string(parameter_col_b) == "b");
+    EXPECT_EQ(to_string(parameter_col_b), "b");
   }
   {
     const AllParameterVariant parameter("string");
-    EXPECT_TRUE(to_string(parameter) == "string");
+    EXPECT_EQ(to_string(parameter), "string");
   }
 }
 
