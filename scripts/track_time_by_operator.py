3a908fb7acec88949182cf27433b52e397562023
Toni Stachewicz <toni.stachewicz@student.hpi.de>
2018-08-20 16:12:57 +0200

    updates script, fixes bug in query analyzer

--- scripts/track_time_by_operator.py
+++ scripts/track_time_by_operator.py
@@ -3,7 +3,7 @@
 import sys
 import subprocess
 
-command = "sudo stap staps/query_analyzer.stp -DMAXMAPENTRIES=10000 -DMAXACTION=100000000 -G debug=0 -c \"{}\" {}".format(' '.join(sys.argv[1:]), sys.argv[1])
+command = "sudo stap tracer_scripts/query_analyzer.stp -DMAXMAPENTRIES=10000 -DMAXACTION=100000000 -G debug=0 -c \"{}\" {}".format(' '.join(sys.argv[1:]), sys.argv[1])
 process = subprocess.Popen(command, shell=True)
 
 process.communicate()
