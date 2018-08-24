42b989cb654c3f79509cca92dca5c1969aa0f2f0
Toni Stachewicz <toni.stachewicz@student.hpi.de>
2018-08-24 17:47:12 +0200

    adjutable max map size for python script

--- scripts/track_time_by_operator.py
+++ scripts/track_time_by_operator.py
@@ -2,8 +2,16 @@
 
 import sys
 import subprocess
+import os
 
-command = "sudo stap tracer_scripts/query_analyzer.stp -DMAXMAPENTRIES=10000 -DMAXACTION=100000000 -G debug=0 -c \"{}\" {}".format(' '.join(sys.argv[1:]), sys.argv[1])
+MAXMAPENTRIES = os.environ.get("MAXMAPENTRIES")
+if not MAXMAPENTRIES:
+    print("MAXMAPENTRIES not set, using default of 10000")
+    print("run \"export MAXMAPENTRIES=<VALUE>\" to increase the maximum map size (maximum is 2^32)")
+    MAXMAPENTRIES = 10000
+
+
+command = "sudo stap tracer_scripts/query_analyzer.stp -DMAXMAPENTRIES={} -DMAXACTION=100000000 -G debug=0 -c \"{}\" {}".format(MAXMAPENTRIES, ' '.join(sys.argv[1:]), sys.argv[1])
 process = subprocess.Popen(command, shell=True)
 
 process.communicate()
