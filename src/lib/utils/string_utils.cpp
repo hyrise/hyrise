ec143d28b35b990032575cc7bf9b9ecc29994010
Jan Kossmann <jan.kossmann@hpi.de>
2018-08-27 16:20:15 -0300

    Rename string_functions to string_utils. Doc.

--- src/lib/utils/string_utils.cpp
+++ src/lib/utils/string_utils.cpp
@@ -1,10 +1,12 @@
 #include <boost/algorithm/string/split.hpp>
 #include <boost/algorithm/string/trim_all.hpp>
 
-#include "string_functions.hpp"
+#include "string_utils.hpp"
 
 namespace opossum {
 
+// Removes whitspaces from the front and back. Also reduces multiple whitespaces between words to a single one.
+// Splits the result by whitespace into single words. Intended for usage in the console.
 std::vector<std::string> trim_and_split(const std::string& input) {
   std::string converted = input;
 
