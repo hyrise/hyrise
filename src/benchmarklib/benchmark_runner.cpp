01b998ec30a3ff9cc423f90ea2716dedb4a79fd4
Leander Neiss <leander.neiss@student.hpi.de>
2018-09-03 22:14:49 +0200

    Re-add PermutedQuerySets mode.

--- src/benchmarklib/benchmark_runner.cpp
+++ src/benchmarklib/benchmark_runner.cpp
@@ -104,46 +104,51 @@ void BenchmarkRunner::_benchmark_permuted_query_sets() {
   std::random_device random_device;
   std::mt19937 random_generator(random_device());
 
-  // if (_config.parallel_execution) {
-  //   for (auto run = size_t{0}; run < _config.max_num_query_runs; ++run) {
-  //     std::shuffle(mutable_named_queries.begin(), mutable_named_queries.end(), random_generator);
+  const auto number_of_queries_per_set = mutable_named_queries.size();
+  auto currently_running_query_sets = std::atomic_uint{0};
+  auto finished_query_set_runs = std::atomic_uint{0};
+  auto finished_queries_total = std::atomic_uint{0};
+  auto result_mutex = std::mutex{};
 
-  //     for (const auto& named_query : mutable_named_queries) {
-  //       const auto query_benchmark_begin = std::chrono::steady_clock::now();
+  auto tasks = std::vector<std::shared_ptr<AbstractTask>>{};
+  auto state = BenchmarkState{_config.max_num_query_runs, _config.max_duration};
 
-  //       auto tasks = std::vector<std::shared_ptr<AbstractTask>>();
-  //       for (auto run = size_t{0}; run < _config.max_num_query_runs; ++run) {
-  //         auto query_tasks = _schedule_query_execution(named_query);
-  //         tasks.insert(tasks.end(), query_tasks.begin(), query_tasks.end());
-  //       }
-  //       CurrentScheduler::wait_for_tasks(tasks);
+  while (state.keep_running(finished_query_set_runs.load(std::memory_order_relaxed))) {
+    if (currently_running_query_sets.load(std::memory_order_relaxed) < _config.clients) {
+      currently_running_query_sets++;
+      std::shuffle(mutable_named_queries.begin(), mutable_named_queries.end(), random_generator);
 
-  //       const auto query_benchmark_end = std::chrono::steady_clock::now();
+      for (const auto& named_query : mutable_named_queries) {
+        const auto query_run_begin = std::chrono::steady_clock::now();
 
-  //       auto& query_benchmark_result = _query_results_by_query_name[named_query.first];
-  //       query_benchmark_result.duration += query_benchmark_end - query_benchmark_begin;
-  //       query_benchmark_result.num_iterations++;
-  //     }
-  //   }
-  // } else {
-    // BenchmarkState state{_config.max_num_query_runs, _config.max_duration};
-    // while (state.keep_running()) {
-    //   std::shuffle(mutable_named_queries.begin(), mutable_named_queries.end(), random_generator);
+        auto on_query_done = [query_run_begin, named_query, number_of_queries_per_set, &currently_running_query_sets, &finished_query_set_runs, &finished_queries_total, &result_mutex, this](){
+          const auto duration = std::chrono::steady_clock::now() - query_run_begin;
+          finished_queries_total++;
+          if (finished_queries_total.load(std::memory_order_relaxed) % number_of_queries_per_set == 0) {
+            currently_running_query_sets--;
+            finished_query_set_runs++;
+          }
+          {
+            auto lock = std::lock_guard<std::mutex>(result_mutex);
+            auto& query_benchmark_result = _query_results_by_query_name[named_query.first];
+            query_benchmark_result.duration += duration;
+            query_benchmark_result.iteration_durations.push_back(duration);
+            query_benchmark_result.num_iterations++;
+          }
+        };
 
-    //   for (const auto& named_query : mutable_named_queries) {
-    //     const auto query_benchmark_begin = std::chrono::steady_clock::now();
+        auto query_tasks = _schedule_query_execution(named_query, on_query_done);
+        tasks.insert(tasks.end(), query_tasks.begin(), query_tasks.end());
+      }
+    } else {
+      std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+  }
 
-    //     // Execute the query, we don't care about the results
-    //     _execute_query(named_query);
-
-    //     const auto query_benchmark_end = std::chrono::steady_clock::now();
-
-    //     auto& query_benchmark_result = _query_results_by_query_name[named_query.first];
-    //     query_benchmark_result.duration += query_benchmark_end - query_benchmark_begin;
-    //     query_benchmark_result.num_iterations++;
-    //   }
-    // }
-  // }
+  // Wait for the rest of the tasks that didn't make it in time - they will not count toward the results
+  // TODO: To be replaced with something like CurrentScheduler::abort(), that properly removes all remaining tasks from all queues, without having to wait for them
+  CurrentScheduler::wait_for_tasks(tasks);
+  DebugAssert(currently_running_query_sets.load(std::memory_order_relaxed) == 0, "All query set runs must be finished at this point");
 }
 
 void BenchmarkRunner::_benchmark_individual_queries() {
@@ -168,7 +173,6 @@ void BenchmarkRunner::_benchmark_individual_queries() {
           const auto query_run_end = std::chrono::steady_clock::now();
           currently_running_queries--;
           finished_query_runs++;
-
           {
             auto lock = std::lock_guard<std::mutex>(durations_mutex);
             iteration_durations.push_back(query_run_end - query_run_begin);
