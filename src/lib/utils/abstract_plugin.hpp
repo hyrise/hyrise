00cb9fb7a9b7d585861d17993027817ffe434350
Jan Kossmann <jan.kossmann@hpi.de>
2018-09-18 17:15:22 +0200

    Avoid confusion

--- src/lib/utils/abstract_plugin.hpp
+++ src/lib/utils/abstract_plugin.hpp
@@ -10,10 +10,10 @@ namespace opossum {
 
 // This is necessary to make the plugin instantiable, it leads to plain C linkage to avoid
 // ugly mangled names. Use EXPORT in the implementation file of your plugin.
-#define EXPORT(PluginName)                                            \
-  extern "C" AbstractPlugin* factory() {                              \
-    auto plugin = static_cast<AbstractPlugin*>(&(PluginName::get())); \
-    return plugin;                                                    \
+#define EXPORT(Plugin)                                            \
+  extern "C" AbstractPlugin* factory() {                          \
+    auto plugin = static_cast<AbstractPlugin*>(&(Plugin::get())); \
+    return plugin;                                                \
   }
 
 // AbstractPlugin is the abstract super class for all plugins. An example implementation can be found
