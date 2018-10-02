b790dec38a7a903c950d1497606c802a0b2d1959
Markus Dreseler <markus.dreseler@hpi.de>
2018-10-03 00:54:35 +0200

    cleanup

--- src/lib/optimizer/strategy/logical_expression_reducer_rule.cpp
+++ src/lib/optimizer/strategy/logical_expression_reducer_rule.cpp
@@ -5,7 +5,6 @@
 
 #include "expression/expression_functional.hpp"
 #include "expression/expression_utils.hpp"
-#include "expression/logical_expression.hpp"
 #include "logical_query_plan/abstract_lqp_node.hpp"
 #include "logical_query_plan/predicate_node.hpp"
 #include "logical_query_plan/projection_node.hpp"
@@ -15,60 +14,6 @@ namespace opossum {
 using namespace opossum::expression_functional;  // NOLINT
 
 std::string LogicalExpressionReducerRule::name() const { return "Logical Expression Reducer Rule"; }
-
-namespace {
-
-void collect_chained_logical_expressions(const std::shared_ptr<AbstractExpression>& expression,
-                                         LogicalOperator logical_operator, ExpressionUnorderedSet& result) {
-  if (expression->type != ExpressionType::Logical) {
-    // Not a logical expression, so for our purposes, we consider it "atomic"
-    result.emplace(expression);
-    return;
-  }
-
-  // Potentially continuing a chain of logical ANDs/ORs
-  const auto& logical_expression = std::static_pointer_cast<LogicalExpression>(expression);
-  if (logical_expression->logical_operator != logical_operator) {
-    // A logical expression, but not of the right type
-    result.emplace(expression);
-    return;
-  }
-
-  collect_chained_logical_expressions(logical_expression->left_operand(), logical_operator, result);
-  collect_chained_logical_expressions(logical_expression->right_operand(), logical_operator, result);
-}
-
-void remove_expressions_from_chain(std::shared_ptr<AbstractExpression>& chain, LogicalOperator logical_operator,
-                                   const ExpressionUnorderedSet& expressions_to_remove) {
-  if (chain->type != ExpressionType::Logical) {
-    // Not a logical expression, so for our purposes, we consider it "atomic"
-    return;
-  }
-
-  // Potentially continuing a chain of logical ANDs/ORs
-  const auto& logical_expression = std::static_pointer_cast<LogicalExpression>(chain);
-  if (logical_expression->logical_operator != logical_operator) {
-    // A logical expression, but not of the right type
-    return;
-  }
-
-  if (expressions_to_remove.count(logical_expression->left_operand())) {
-    chain = logical_expression->right_operand();
-    remove_expressions_from_chain(chain, logical_operator, expressions_to_remove);
-    return;
-  }
-
-  if (expressions_to_remove.count(logical_expression->right_operand())) {
-    chain = logical_expression->left_operand();
-    remove_expressions_from_chain(chain, logical_operator, expressions_to_remove);
-    return;
-  }
-
-  remove_expressions_from_chain(logical_expression->left_operand(), logical_operator, expressions_to_remove);
-  remove_expressions_from_chain(logical_expression->right_operand(), logical_operator, expressions_to_remove);
-}
-
-}  // namespace
 
 bool LogicalExpressionReducerRule::apply_to(const std::shared_ptr<AbstractLQPNode>& node) const {
   auto changed = false;
@@ -84,6 +29,9 @@ bool LogicalExpressionReducerRule::_apply_to_node(const std::shared_ptr<Abstract
                                                   MapType& previously_reduced_expressions) const {
   auto changed = false;
 
+  std::cout << "\tLooking at node " << node->description() << std::endl;
+
+  // We only deal with predicates and projections, as these are the only LQP node types that handle complex expressions
   if (node->type == LQPNodeType::Predicate) {
     const auto& predicate_node = std::static_pointer_cast<PredicateNode>(node);
     // It doesn't hurt to have a temporary vector here, because the top-level expression will not be changed anyway, only expressions further down are. This is verified below.
@@ -95,6 +43,7 @@ bool LogicalExpressionReducerRule::_apply_to_node(const std::shared_ptr<Abstract
     changed |= _apply_to_expressions(projection_node->expressions, previously_reduced_expressions);
   }
 
+  // We have to do _apply_to_inputs manually, because we want to pass a parameter
   if (node->left_input()) {
     changed |= _apply_to_node(node->left_input(), previously_reduced_expressions);
   }
@@ -112,6 +61,7 @@ bool LogicalExpressionReducerRule::_apply_to_expressions(std::vector<std::shared
   for (auto& expression : expressions) {
     std::cout << "\tLooking at expression " << expression->as_column_name() << std::endl;
     visit_expression(expression, [&](auto& subexpression) {
+      // Step 0: Check if we already reduced this expression previously, if yes, reuse it
       auto reduced_expression_it = previously_reduced_expressions.find(subexpression);
       if (reduced_expression_it != previously_reduced_expressions.cend()) {
         subexpression = reduced_expression_it->second;
@@ -125,8 +75,10 @@ bool LogicalExpressionReducerRule::_apply_to_expressions(std::vector<std::shared
 
       const auto& logical_expression = std::static_pointer_cast<LogicalExpression>(subexpression);
       if (logical_expression->logical_operator == LogicalOperator::Or) {
+        // Step 1: Collect the outer chain. For the comments, we assume that the outer condition is OR and the inner
+        // condition is AND: `(a AND b AND x) OR (c AND d AND x)`.
         ExpressionUnorderedSet or_expressions;
-        collect_chained_logical_expressions(subexpression, LogicalOperator::Or, or_expressions);
+        _collect_chained_logical_expressions(subexpression, LogicalOperator::Or, or_expressions);
 
         std::cout << "\t\tOr expressions: " << std::endl;
         for (const auto& subexpression : or_expressions) {
@@ -135,15 +87,15 @@ bool LogicalExpressionReducerRule::_apply_to_expressions(std::vector<std::shared
 
         ExpressionUnorderedSet common_and_expressions;
         {
-          // Step 1: See if there is anything to do
+          // Step 2: Fill common_and_expressions with the expressions found in the first outer expression. Then iterate over the others and intersect their expressions with common_and_expressions.
 
           auto or_expression_it = or_expressions.begin();
-          collect_chained_logical_expressions(*or_expression_it, LogicalOperator::And, common_and_expressions);
+          _collect_chained_logical_expressions(*or_expression_it, LogicalOperator::And, common_and_expressions);
           ++or_expression_it;
           while (or_expression_it != or_expressions.end()) {
             std::cout << "\t\tLooking in next OR: " << (*or_expression_it)->as_column_name() << std::endl;
             ExpressionUnorderedSet current_and_expressions;
-            collect_chained_logical_expressions(*or_expression_it, LogicalOperator::And, current_and_expressions);
+            _collect_chained_logical_expressions(*or_expression_it, LogicalOperator::And, current_and_expressions);
             for (auto and_expression_it = common_and_expressions.begin();
                  and_expression_it != common_and_expressions.end();) {
               std::cout << "\t\t\tTesting: " << (*and_expression_it)->as_column_name() << std::endl;
@@ -161,31 +113,35 @@ bool LogicalExpressionReducerRule::_apply_to_expressions(std::vector<std::shared
             std::cout << "\t\t\t- " << subexpression->as_column_name() << std::endl;
           }
 
+          // Step 3: If there are no common_and_expressions, we are done.
           if (common_and_expressions.empty()) {
             return ExpressionVisitation::DoNotVisitArguments;
           }
         }
 
+        changed = true;
+
         {
-          // Step 2: Rewrite the expression
+          // Step 4: Rewrite the expression
 
-          changed = true;
-
+          // Step 4.1: Rebuild the inner expressions, but without the common expressions
           auto or_expression_it = or_expressions.cbegin();
           auto new_chain = *or_expression_it;
-          remove_expressions_from_chain(new_chain, LogicalOperator::And, common_and_expressions);
+          _remove_expressions_from_chain(new_chain, LogicalOperator::And, common_and_expressions);
           for (; or_expression_it != or_expressions.cend(); ++or_expression_it) {
             auto or_expression = *or_expression_it;  // We want a copy here, because we can't modify the set
-            remove_expressions_from_chain(or_expression, LogicalOperator::And, common_and_expressions);
+            _remove_expressions_from_chain(or_expression, LogicalOperator::And, common_and_expressions);
             new_chain = or_(new_chain, or_expression);
           }
 
+          // Step 4.2: Add the commond expressions to the outside
           for (const auto& subexpression : common_and_expressions) {
             new_chain = and_(subexpression, new_chain);
           }
 
           std::cout << "New expression: " << new_chain->as_column_name() << std::endl;
 
+          // Step 4.3: Store the result and replace it in the node that is to be optimized
           previously_reduced_expressions.emplace(subexpression, new_chain);
           subexpression = new_chain;
         }
@@ -198,6 +154,65 @@ bool LogicalExpressionReducerRule::_apply_to_expressions(std::vector<std::shared
   }
 
   return changed;
+}
+
+// This is a helper method that puts all elements of the chain (a AND (b AND (c AND ...))) into result, `AND` being defined by logical_operator
+void LogicalExpressionReducerRule::_collect_chained_logical_expressions(
+    const std::shared_ptr<AbstractExpression>& expression, LogicalOperator logical_operator,
+    ExpressionUnorderedSet& result) const {
+  // TODO test these separately
+  if (expression->type != ExpressionType::Logical) {
+    // Not a logical expression, so for our purposes, we consider it "atomic"
+    result.emplace(expression);
+    return;
+  }
+
+  // Potentially continuing a chain of logical ANDs/ORs
+  const auto& logical_expression = std::static_pointer_cast<LogicalExpression>(expression);
+  if (logical_expression->logical_operator != logical_operator) {
+    // A logical expression, but not of the right type
+    result.emplace(expression);
+    return;
+  }
+
+  _collect_chained_logical_expressions(logical_expression->left_operand(), logical_operator, result);
+  _collect_chained_logical_expressions(logical_expression->right_operand(), logical_operator, result);
+}
+
+// This is a helper method that removes all elements in the chain (a AND (b AND (c AND ...))) that are contained in expressions_to_remove, `AND` being defined by logical_operator
+void LogicalExpressionReducerRule::_remove_expressions_from_chain(
+    std::shared_ptr<AbstractExpression>& chain, LogicalOperator logical_operator,
+    const ExpressionUnorderedSet& expressions_to_remove) const {
+  // TODO test these separately
+  if (chain->type != ExpressionType::Logical) {
+    // Not a logical expression, so for our purposes, we consider it "atomic"
+    return;
+  }
+
+  // Potentially continuing a chain of logical ANDs/ORs
+  const auto& logical_expression = std::static_pointer_cast<LogicalExpression>(chain);
+  if (logical_expression->logical_operator != logical_operator) {
+    // A logical expression, but not of the right type
+    return;
+  }
+
+  // See if the left side is to be removed, if yes, remove the current expression from the chain and continue only with the right expression
+  if (expressions_to_remove.count(logical_expression->left_operand())) {
+    chain = logical_expression->right_operand();
+    _remove_expressions_from_chain(chain, logical_operator, expressions_to_remove);
+    return;
+  }
+
+  // See if the right side is to be removed, if yes, remove the current expression from the chain and continue only with the left expression
+  if (expressions_to_remove.count(logical_expression->right_operand())) {
+    chain = logical_expression->left_operand();
+    _remove_expressions_from_chain(chain, logical_operator, expressions_to_remove);
+    return;
+  }
+
+  // Nothing removed, recurse into both left and right
+  _remove_expressions_from_chain(logical_expression->left_operand(), logical_operator, expressions_to_remove);
+  _remove_expressions_from_chain(logical_expression->right_operand(), logical_operator, expressions_to_remove);
 }
 
 }  // namespace opossum
--- src/lib/optimizer/strategy/logical_expression_reducer_rule.hpp
+++ src/lib/optimizer/strategy/logical_expression_reducer_rule.hpp
@@ -4,6 +4,7 @@
 
 #include "abstract_rule.hpp"
 #include "expression/abstract_expression.hpp"
+#include "expression/logical_expression.hpp"
 
 namespace opossum {
 
@@ -22,6 +23,11 @@ class LogicalExpressionReducerRule : public AbstractRule {
   bool _apply_to_node(const std::shared_ptr<AbstractLQPNode>& node, MapType& previously_reduced_expressions) const;
   bool _apply_to_expressions(std::vector<std::shared_ptr<AbstractExpression>>& expressions,
                              MapType& previously_reduced_expressions) const;
+
+  void _collect_chained_logical_expressions(const std::shared_ptr<AbstractExpression>& expression,
+                                            LogicalOperator logical_operator, ExpressionUnorderedSet& result) const;
+  void _remove_expressions_from_chain(std::shared_ptr<AbstractExpression>& chain, LogicalOperator logical_operator,
+                                      const ExpressionUnorderedSet& expressions_to_remove) const;
 };
 
 }  // namespace opossum
