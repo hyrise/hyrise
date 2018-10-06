a3a1d4b179c10334d72935a5ace67b9ad3027e94
Jan Koßmann <kossmann.jan@gmail.com>
2018-10-06 11:44:50 +0200

    Removes SSE2-specific code from SIMD-BP128 implementation (#1165)
    * Remove SSE2 code switch
    
    * Further alignments
    
    * Add comment
    
    * Lint

--- src/lib/storage/vector_compression/simd_bp128/simd_bp128_compressor.cpp
+++ src/lib/storage/vector_compression/simd_bp128/simd_bp128_compressor.cpp
@@ -61,7 +61,7 @@ void SimdBp128Compressor::_finish() {
 bool SimdBp128Compressor::_meta_block_complete() { return (Packing::meta_block_size - _meta_block_index) <= 0u; }
 
 void SimdBp128Compressor::_pack_meta_block() {
-  const auto bits_needed = _bits_needed_per_block();
+  alignas(16) const auto bits_needed = _bits_needed_per_block();
   _write_meta_info(bits_needed);
   _pack_blocks(Packing::blocks_in_meta_block, bits_needed);
 
@@ -72,7 +72,7 @@ void SimdBp128Compressor::_pack_incomplete_meta_block() {
   // Fill remaining elements with zero
   std::fill(_pending_meta_block.begin() + _meta_block_index, _pending_meta_block.end(), 0u);
 
-  const auto bits_needed = _bits_needed_per_block();
+  alignas(16) const auto bits_needed = _bits_needed_per_block();
   _write_meta_info(bits_needed);
 
   // Returns ceiling of integer division
