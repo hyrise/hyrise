a5486180f2f2d79a7b2513a158beba257c3f5b8f
mrks <markus@dreseler.de>
2018-10-15 16:43:01 +0200

    Fix ambiguous names in expression_functional.hpp (#1190)
    * Fix ambiguous names in expression_functional.hpp
    
    * bla
    
    * blabla
    
    * lint
    
    * Remove workaround

--- src/test/expression/expression_test.cpp
+++ src/test/expression/expression_test.cpp
@@ -65,8 +65,8 @@ TEST_F(ExpressionTest, Equals) {
   EXPECT_EQ(*is_null_(a), *is_null_(a));
   EXPECT_NE(*is_null_(a), *is_null_(b));
   EXPECT_EQ(*is_not_null_(a), *is_not_null_(a));
-  EXPECT_EQ(*parameter_(ParameterID{4}), *parameter_(ParameterID{4}));
-  EXPECT_NE(*parameter_(ParameterID{4}), *parameter_(ParameterID{5}));
+  EXPECT_EQ(*uncorrelated_parameter_(ParameterID{4}), *uncorrelated_parameter_(ParameterID{4}));
+  EXPECT_NE(*uncorrelated_parameter_(ParameterID{4}), *uncorrelated_parameter_(ParameterID{5}));
   EXPECT_EQ(*extract_(DatetimeComponent::Month, "1999-07-30"), *extract_(DatetimeComponent::Month, "1999-07-30"));
   EXPECT_NE(*extract_(DatetimeComponent::Day, "1999-07-30"), *extract_(DatetimeComponent::Month, "1999-07-30"));
   EXPECT_EQ(*unary_minus_(6), *unary_minus_(6));
@@ -110,15 +110,15 @@ TEST_F(ExpressionTest, RequiresCalculation) {
   EXPECT_TRUE(is_null_(null_())->requires_computation());
   EXPECT_TRUE(and_(1, 0)->requires_computation());
   EXPECT_TRUE(unary_minus_(5)->requires_computation());
-  EXPECT_FALSE(parameter_(ParameterID{5})->requires_computation());
-  EXPECT_FALSE(parameter_(ParameterID{5}, a)->requires_computation());
-  EXPECT_FALSE(column_(a)->requires_computation());
+  EXPECT_FALSE(uncorrelated_parameter_(ParameterID{5})->requires_computation());
+  EXPECT_FALSE(correlated_parameter_(ParameterID{5}, a)->requires_computation());
+  EXPECT_FALSE(lqp_column_(a)->requires_computation());
   EXPECT_FALSE(PQPColumnExpression::from_table(*table_int_float, "a")->requires_computation());
   EXPECT_FALSE(value_(5)->requires_computation());
   EXPECT_TRUE(cast_(5, DataType::Int)->requires_computation());
   EXPECT_TRUE(cast_(5.5, DataType::Int)->requires_computation());
 
-  const auto lqp_select_expression = select_(int_float_node);
+  const auto lqp_select_expression = lqp_select_(int_float_node);
 
   EXPECT_TRUE(lqp_select_expression->requires_computation());
   EXPECT_TRUE(exists_(lqp_select_expression)->requires_computation());
@@ -157,8 +157,8 @@ TEST_F(ExpressionTest, AsColumnName) {
   EXPECT_EQ(value_(3.25)->as_column_name(), "3.25");
   EXPECT_EQ(null_()->as_column_name(), "NULL");
   EXPECT_EQ(cast_("36", DataType::Float)->as_column_name(), "CAST('36' AS float)");
-  EXPECT_EQ(parameter_(ParameterID{0})->as_column_name(), "Parameter[id=0]");
-  EXPECT_EQ(parameter_(ParameterID{0}, a)->as_column_name(), "Parameter[name=a;id=0]");
+  EXPECT_EQ(uncorrelated_parameter_(ParameterID{0})->as_column_name(), "Parameter[id=0]");
+  EXPECT_EQ(correlated_parameter_(ParameterID{0}, a)->as_column_name(), "Parameter[name=a;id=0]");
 }
 
 TEST_F(ExpressionTest, AsColumnNameNested) {
@@ -240,8 +240,8 @@ TEST_F(ExpressionTest, IsNullable) {
   EXPECT_TRUE(case_(1, 1, null_())->is_nullable());
   EXPECT_TRUE(add_(greater_than_(2, null_()), 1)->is_nullable());
   EXPECT_TRUE(and_(greater_than_(2, null_()), 1)->is_nullable());
-  EXPECT_FALSE(column_(a)->is_nullable());
-  EXPECT_TRUE(column_(a_nullable)->is_nullable());
+  EXPECT_FALSE(lqp_column_(a)->is_nullable());
+  EXPECT_TRUE(lqp_column_(a_nullable)->is_nullable());
   EXPECT_FALSE(cast_(12, DataType::String)->is_nullable());
   EXPECT_TRUE(cast_(null_(), DataType::String)->is_nullable());
   EXPECT_TRUE(sum_(null_())->is_nullable());
