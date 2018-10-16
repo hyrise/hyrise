04ede2e830cf2b8d06d910b12a6548c198d24c34
Markus Dreseler <markus.dreseler@hpi.de>
2018-10-16 16:46:57 +0200

    dibngs

--- src/lib/storage/pos_list.hpp
+++ src/lib/storage/pos_list.hpp
@@ -51,13 +51,13 @@ struct PosList : private pmr_vector<RowID> {
 
   bool references_single_chunk() const { return _references_single_chunk; }
 
-  // std::vector<ChunkOffset> offsets() {
-  //   Assert(references_single_chunk(),
-  //          "Can only convert PosList to ChunkOffsetsList if it is guaranteed to refer to a single chunk");
-  //   std::vector<ChunkOffset> list(size());
-  //   for (auto i = 0ul; i < size(); ++i) list[i] = (*this)[i].chunk_offset;
-  //   return list;
-  // }
+  std::vector<ChunkOffset> convert_to_offsets() const {
+    Assert(references_single_chunk(),
+           "Can only convert PosList to ChunkOffsetsList if it is guaranteed to refer to a single chunk");
+    std::vector<ChunkOffset> list(size());
+    for (auto i = 0ul; i < size(); ++i) list[i] = (*this)[i].chunk_offset;
+    return list;
+  }
 
   using Vector::assign;
   using Vector::get_allocator;
--- src/lib/storage/segment_iterables/chunk_offset_mapping.cpp
+++ src/lib/storage/segment_iterables/chunk_offset_mapping.cpp
@@ -4,18 +4,24 @@ namespace opossum {
 
 ChunkOffsetsByChunkID split_pos_list_by_chunk_id(const PosList& pos_list, const size_t number_of_chunks) {
   auto chunk_offsets_by_chunk_id = ChunkOffsetsByChunkID{number_of_chunks};
-  for (auto& chunk_offsets : chunk_offsets_by_chunk_id) {
-    chunk_offsets.reserve(pos_list.size() / number_of_chunks);
-  }
 
-  for (auto chunk_offset = ChunkOffset{0u}; chunk_offset < pos_list.size(); ++chunk_offset) {
-    const auto row_id = pos_list[chunk_offset];
-    if (row_id.is_null()) continue;
+  if (pos_list.references_single_chunk() && !pos_list.empty()) {
+    const auto chunk_id = pos_list[0].chunk_id;
+    chunk_offsets_by_chunk_id[chunk_id] = pos_list.convert_to_offsets();
+  } else {
+    for (auto& chunk_offsets : chunk_offsets_by_chunk_id) {
+      chunk_offsets.reserve(pos_list.size() / number_of_chunks);
+    }
 
-    // Returns ChunkOffsetsList for row_id.chunk_id or creates new
-    auto& mapped_chunk_offsets = chunk_offsets_by_chunk_id[row_id.chunk_id];
+    for (auto chunk_offset = ChunkOffset{0u}; chunk_offset < pos_list.size(); ++chunk_offset) {
+      const auto row_id = pos_list[chunk_offset];
+      if (row_id.is_null()) continue;
 
-    mapped_chunk_offsets.emplace_back(chunk_offset, row_id.chunk_offset);
+      // Returns ChunkOffsetsList for row_id.chunk_id or creates new
+      auto& mapped_chunk_offsets = chunk_offsets_by_chunk_id[row_id.chunk_id];
+
+      mapped_chunk_offsets.emplace_back(chunk_offset, row_id.chunk_offset);
+    }
   }
 
   return chunk_offsets_by_chunk_id;
