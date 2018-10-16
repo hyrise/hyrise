2d7db6aa84428b714d3588b186bd13e09093056a
Markus Dreseler <markus.dreseler@hpi.de>
2018-10-16 20:39:10 +0200

    bla

--- src/lib/storage/run_length_segment/run_length_segment_iterable.hpp
+++ src/lib/storage/run_length_segment/run_length_segment_iterable.hpp
@@ -26,9 +26,9 @@ class RunLengthSegmentIterable : public PointAccessibleSegmentIterable<RunLength
   template <typename Functor>
   void _on_with_iterators(const PosList& position_filter, const Functor& functor) const {
     auto begin =
-        PointAccessIterator{*_segment.values(), *_segment.null_values(), *_segment.end_positions(), position_filter};
+        PointAccessIterator{*_segment.values(), *_segment.null_values(), *_segment.end_positions(), position_filter.cbegin()};
     auto end =
-        PointAccessIterator{*_segment.values(), *_segment.null_values(), *_segment.end_positions(), position_filter};
+        PointAccessIterator{*_segment.values(), *_segment.null_values(), *_segment.end_positions(), position_filter.cbegin()};
 
     functor(begin, end);
   }
