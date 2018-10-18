e2e1f6aed9632755648a863b99a89774628e21b0
Tim Zimmermann <tim.zimmermann@student.hpi.uni-potsdam.de>
2018-10-18 12:15:14 +0200

    Ignore deleted staged files

--- scripts/format.sh
+++ scripts/format.sh
@@ -20,7 +20,7 @@ elif [ "$1" = "modified" ]; then
     { git diff --diff-filter=d --name-only & git ls-files --others --exclude-standard; } | grep -E "^src.*\.[chi]pp$" | xargs -I{} sh -c "${format_cmd}"
 elif [ "$1" = "staged" ]; then
     # Run on all files that are staged to be committed.
-    git diff --cached --name-only | grep -E "^src.*\.[chi]pp$" | xargs -I{} sh -c "${format_cmd}"
+    git diff --diff-filter=d --cached --name-only | grep -E "^src.*\.[chi]pp$" | xargs -I{} sh -c "${format_cmd}"
 else
     # Run on all changed as well as untracked cpp/hpp files, as compared to the current master. Skip deleted files.
     { git diff --diff-filter=d --name-only master & git ls-files --others --exclude-standard; } | grep -E "^src.*\.[chi]pp$" | xargs -I{} sh -c "${format_cmd}"
