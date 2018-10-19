ddec531ac69882422f2aacfd61f2081bdf9f5bbb
martin.boissier <martin.boissier@hpi.de>
2018-10-19 17:55:34 +0200

    Invoke hash join with radix_bits as reference

--- src/lib/operators/join_hash.cpp
+++ src/lib/operators/join_hash.cpp
@@ -26,7 +26,7 @@ namespace opossum {
 JoinHash::JoinHash(const std::shared_ptr<const AbstractOperator>& left,
                    const std::shared_ptr<const AbstractOperator>& right, const JoinMode mode,
                    const ColumnIDPair& column_ids, const PredicateCondition predicate_condition,
-                   const std::optional<size_t> radix_bits)
+                   const std::optional<size_t>& radix_bits)
     : AbstractJoinOperator(OperatorType::JoinHash, left, right, mode, column_ids, predicate_condition),
       _radix_bits(radix_bits) {
   DebugAssert(predicate_condition == PredicateCondition::Equals, "Operator not supported by Hash Join.");
--- src/lib/operators/join_hash.hpp
+++ src/lib/operators/join_hash.hpp
@@ -25,7 +25,7 @@ class JoinHash : public AbstractJoinOperator {
  public:
   JoinHash(const std::shared_ptr<const AbstractOperator>& left, const std::shared_ptr<const AbstractOperator>& right,
            const JoinMode mode, const ColumnIDPair& column_ids, const PredicateCondition predicate_condition,
-           const std::optional<size_t> radix_bits = std::nullopt);
+           const std::optional<size_t>& radix_bits = std::nullopt);
 
   const std::string name() const override;
 
