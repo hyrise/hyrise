54751bc40bc85c7619d3c5b8c5a3942b4fbc3ac7
Markus Dreseler <markus.dreseler@hpi.de>
2018-10-22 13:54:18 +0200

    TADAAAA

--- src/benchmarklib/tpch/tpch_queries.cpp
+++ src/benchmarklib/tpch/tpch_queries.cpp
@@ -727,7 +727,7 @@ const char* const tpch_query_18 =
  */
 const char* const tpch_query_19 =
 
-  R"(SELECT SUM(l_extendedprice * (1 - l_discount) ) as revenue FROM lineitem, part WHERE (( p_partkey = l_partkey AND
+    R"(SELECT SUM(l_extendedprice * (1 - l_discount) ) as revenue FROM lineitem, part WHERE (( p_partkey = l_partkey AND
   p_brand = 'Brand#12' AND p_container in ( 'SM CASE', 'SM BOX', 'SM PACK', 'SM PKG') AND l_quantity >= 1 AND l_quantity
   <= 1 + 10 AND p_size between 1 AND 5 AND l_shipmode in ('AIR', 'AIR REG') AND l_shipinstruct = 'DELIVER IN PERSON') or
   (p_partkey = l_partkey AND p_brand = 'Brand#23' AND p_container in ('MED BAG', 'MED BOX', 'MED PKG', 'MED PACK') AND
