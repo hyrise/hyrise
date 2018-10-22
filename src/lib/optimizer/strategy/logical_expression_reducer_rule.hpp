82a7121e6efb315ad5388bdcec8a6ca3c5886554
mrks <markus.dreseler@hpi.de>
2018-10-22 16:58:58 +0200

    typo

--- src/lib/optimizer/strategy/logical_expression_reducer_rule.hpp
+++ src/lib/optimizer/strategy/logical_expression_reducer_rule.hpp
@@ -11,7 +11,7 @@ namespace opossum {
 class AbstractLQPNode;
 
 /**
- * This rule tries to simply logical expressions. Currently, it only looks at disjunctive chains, i.e., chains of
+ * This rule tries to simpifly logical expressions. Currently, it only looks at disjunctive chains, i.e., chains of
  * ORs: `(a AND b) OR (a AND c) OR (a AND d)`. In this example, `a` can be extracted: `a AND (b OR c OR d)`.
  * Furthermore, if the expression is used by a predicate, this rule extracts `a` into its own PredicateNode, which
  * can then be independently pushed down or used for detecting joins. A good example can be seen in TPC-H query 19.
