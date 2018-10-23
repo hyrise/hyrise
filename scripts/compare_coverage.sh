4b035dbf23ffebdcdb258c499a2c5efa6ab61c4a
Markus Dreseler <markus@dreseler.de>
2018-10-23 17:54:18 +0200

    test

--- scripts/compare_coverage.sh
+++ scripts/compare_coverage.sh
@@ -3,14 +3,16 @@
 master_branch=$(curl https://hyrise-ci.epic-hpi.de/job/Hyrise/job/hyrise/job/master/lastStableBuild/artifact/coverage_percent.txt)
 this_branch=$(cat coverage_percent.txt)
 
-if (( $(bc <<< "$master_branch < $this_branch") ))
-then
-echo -n increased by $(bc <<< "$this_branch - $master_branch")
-elif (( $(bc <<< "$master_branch == $this_branch") ))
-then
-echo -n stayed the same
-else
-echo -n decreased by $(bc <<< "$master_branch - $this_branch")
-fi
+echo "this: $this_branch master: $master_branch"
 
-echo " compared to master and is now $this_branch%"
+# if (( $(bc <<< "$master_branch < $this_branch") ))
+# then
+# echo -n increased by $(bc <<< "$this_branch - $master_branch")
+# elif (( $(bc <<< "$master_branch == $this_branch") ))
+# then
+# echo -n stayed the same
+# else
+# echo -n decreased by $(bc <<< "$master_branch - $this_branch")
+# fi
+
+# echo " compared to master and is now $this_branch%"
