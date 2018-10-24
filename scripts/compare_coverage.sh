3e7586004ef885ad2cd26566d0acf9cb468e578b
Markus Dreseler <markus.dreseler@hpi.de>
2018-10-24 11:05:16 +0200

    Oder so?

--- scripts/compare_coverage.sh
+++ scripts/compare_coverage.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-master_branch=$(curl http://localhost:8080/job/Hyrise/job/hyrise/job/master/lastStableBuild/artifact/coverage_percent.txt)
+if ping -q -c 1 -W 1 imresx01.eaalab.hpi.uni-potsdam.de >/dev/null; then
+	# Workaround as accessing the DMZ from the the IMR does not work
+	master_branch=$(curl http://imresx01.eaalab.hpi.uni-potsdam.de:8080/job/Hyrise/job/hyrise/job/master/lastStableBuild/artifact/coverage_percent.txt)
+else
+	master_branch=$(curl https://hyrise-ci.epic-hpi.de/job/Hyrise/job/hyrise/job/master/lastStableBuild/artifact/coverage_percent.txt)
+fi
 this_branch=$(cat coverage_percent.txt)
 
 if (( $(bc <<< "$master_branch < $this_branch") ))
