3a388ae1fa5fb27e6624688859860c4a6aee9a6e
Moritz Eyssen <Moritz.Eyssen@student.hpi.de>
2018-10-28 23:41:15 +0100

    comment

--- src/lib/optimizer/strategy/logical_reduction_rule.hpp
+++ src/lib/optimizer/strategy/logical_reduction_rule.hpp
@@ -11,14 +11,19 @@ namespace opossum {
 class AbstractLQPNode;
 
 /**
- * This rule tries to simpifly logical expressions. Currently, it only looks at disjunctive chains, i.e., chains of
- * ORs: `(a AND b) OR (a AND c) OR (a AND d)`. In this example, `a` can be extracted: `a AND (b OR c OR d)`.
- * Furthermore, if the expression is used by a predicate, this rule extracts `a` into its own PredicateNode, which
- * can then be independently pushed down or used for detecting joins. A good example can be seen in TPC-H query 19.
- * Without this rule, `p_partkey = l_partkey` would not be available as a join predicate and the predicates on
- * `l_shipmode` and `l_shipinstruct` could not be pulled below the join.
- * Conjunctive chains, i.e., `(a OR b) AND (a OR c) -> a OR (b AND c)` are currently ignored, as they don't allow
- * us to fully extract predicates.
+ * This rule tries to simplify logical expressions, so that they can be processed faster.
+ *
+ * Reverse Distributivity
+ *   Expressions of the form `(a AND b) OR (a AND c)` are rewritten as `a AND (b OR c)`.
+ *
+ * Also, this rule turns conjunctive chains (e.g. `a AND b AND c`) are turned into multiple consecutive PredicateNodes,
+ *   because other Optimizer rules can work better with simpler predicates.
+ *
+ * EXAMPLE: TPC-H query 19.
+ *   Without this rule, `p_partkey = l_partkey` would not be available as a join predicate and the predicates on
+ *   `l_shipmode` and `l_shipinstruct` could not be pulled below the join.
+ *   Conjunctive chains, i.e., `(a OR b) AND (a OR c) -> a OR (b AND c)` are currently ignored, as they don't allow
+ *   us to fully extract predicates.
  */
 class LogicalReductionRule : public AbstractRule {
  public:
