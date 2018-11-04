bdcadcc5c9a8a1d2de3f3cd16adb34a6a9c049b4
Moritz Eyssen <Moritz.Eyssen@student.hpi.de>
2018-11-04 10:42:25 +0100

    load_table_cached

--- src/test/base_test.hpp
+++ src/test/base_test.hpp
@@ -14,6 +14,7 @@
 #include "sql/sql_query_cache.hpp"
 #include "sql/sql_query_plan.hpp"
 #include "storage/dictionary_segment.hpp"
+#include "storage/materialize.hpp"
 #include "storage/numa_placement_manager.hpp"
 #include "storage/segment_encoding_utils.hpp"
 #include "storage/storage_manager.hpp"
@@ -112,6 +113,52 @@ class BaseTestWithParam
 
     return std::make_shared<TableScan>(in, predicate);
   }
+
+  static std::shared_ptr<Table> load_table_cached(const std::string& file_name, size_t chunk_size = Chunk::MAX_SIZE) {
+    auto cache_iter = table_cache.find({file_name, chunk_size});
+
+    if (cache_iter == table_cache.end()) {
+      cache_iter = table_cache.emplace(std::make_pair(file_name, chunk_size), load_table(file_name, chunk_size)).first;
+    }
+
+    const auto cached_table = cache_iter->second;
+
+    const auto result_table = std::make_shared<Table>(cached_table->column_definitions(), TableType::Data, cached_table->max_chunk_size());
+
+    for (const auto& cached_chunk : cached_table->chunks()) {
+      auto segments = Segments{cached_table->column_count()};
+
+      for (auto column_id = ColumnID{0}; column_id < cached_table->column_count(); ++column_id) {
+        const auto cached_segment = cached_chunk->get_segment(column_id);
+        const auto cached_value_segment = std::dynamic_pointer_cast<BaseValueSegment>(cached_segment);
+        Assert(cached_value_segment, "Expected ValueSegment in cache");
+
+        resolve_data_type(cached_table->column_data_type(column_id), [&](const auto data_type_t) {
+          using ColumnDataType = typename decltype(data_type_t)::type;
+
+          pmr_concurrent_vector<ColumnDataType> values;
+          values.reserve(cached_segment->size());
+          materialize_values<ColumnDataType>(*cached_segment, values);
+
+          if (cached_value_segment->is_nullable()) {
+            pmr_concurrent_vector<bool> null_values;
+            null_values.reserve(cached_segment->size());
+            materialize_nulls<ColumnDataType>(*cached_segment, null_values);
+
+            segments[column_id] = std::make_shared<ValueSegment>(std::move(values), std::move(null_values));
+          } else {
+
+            segments[column_id] = std::make_shared<ValueSegment>(std::move(values));
+          }
+        });
+
+      }
+    }
+
+    return result_table;
+  }
+
+  static std::map<std::pair<std::string, ChunkOffset>, std::shared_ptr<Table>> table_cache;
 };
 
 using BaseTest = BaseTestWithParam<void>;
