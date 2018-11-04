e1c0d22df29f5966f1fba4c8bf8dfa8f0613ac72
Moritz Eyssen <Moritz.Eyssen@student.hpi.de>
2018-11-04 10:21:05 +0100

    this compiles now

--- src/lib/utils/load_table.cpp
+++ src/lib/utils/load_table.cpp
@@ -15,13 +15,13 @@ namespace opossum {
 std::shared_ptr<Table> create_table_from_header(std::ifstream& infile, size_t chunk_size) {
   std::string line;
   std::getline(infile, line);
-  std::vector<std::string> column_names = split_string_by_delimiter<std::string>(line, '|');
+  std::vector<std::string> column_names = split_string_by_delimiter(line, '|');
   std::getline(infile, line);
-  std::vector<std::string> column_types = split_string_by_delimiter<std::string>(line, '|');
+  std::vector<std::string> column_types = split_string_by_delimiter(line, '|');
 
   auto column_nullable = std::vector<bool>{};
   for (auto& type : column_types) {
-    auto type_nullable = split_string_by_delimiter<std::string>(type, '_');
+    auto type_nullable = split_string_by_delimiter(type, '_');
     type = type_nullable[0];
 
     auto nullable = type_nullable.size() > 1 && type_nullable[1] == "null";
@@ -53,17 +53,18 @@ std::shared_ptr<Table> load_table(const std::string& file_name, size_t chunk_siz
 
   std::string line;
   while (std::getline(infile, line)) {
-    std::vector<AllTypeVariant> values = split_string_by_delimiter<AllTypeVariant>(line, '|');
+    const auto string_values = split_string_by_delimiter(line, '|');
+    auto variant_values = std::vector<AllTypeVariant>(string_values.size());
 
-    for (auto column_id = ColumnID{0}; column_id < values.size(); ++column_id) {
-      auto& value = values[column_id];
-
-      if (table->column_is_nullable(column_id) && (value == AllTypeVariant{"null"})) {
-        value = NULL_VALUE;
+    for (auto column_id = ColumnID{0}; column_id < string_values.size(); ++column_id) {
+      if (table->column_is_nullable(column_id) && string_values[column_id] == "null") {
+        variant_values[column_id] = NULL_VALUE;
+      } else {
+        variant_values[column_id] = AllTypeVariant{std::move(string_values[column_id])};
       }
     }
 
-    table->append(values);
+    table->append(variant_values);
 
     auto chunk = table->get_chunk(static_cast<ChunkID>(table->chunk_count() - 1));
     auto mvcc_data = chunk->get_scoped_mvcc_data_lock();
