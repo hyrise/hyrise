ce9eff3f5eb1f02ad8aa68a088a2b1696619be70
Moritz Eyssen <mrzesn@t-online.de>
2018-11-05 07:37:01 +0100

    _split() -> split_string_by_delimiter() (#1258)
    * _split() -> split_string_by_delimiter()
    
    * this compiles now
    
    * CI?
    
    * tidy

--- src/lib/utils/load_table.hpp
+++ src/lib/utils/load_table.hpp
@@ -11,19 +11,6 @@ namespace opossum {
 
 class Table;
 
-template <typename T>
-std::vector<T> _split(const std::string& str, char delimiter) {
-  std::vector<T> internal;
-  std::stringstream ss(str);
-  std::string tok;
-
-  while (std::getline(ss, tok, delimiter)) {
-    internal.push_back(tok);
-  }
-
-  return internal;
-}
-
 std::shared_ptr<Table> load_table(const std::string& file_name, size_t chunk_size = Chunk::MAX_SIZE);
 
 /**
