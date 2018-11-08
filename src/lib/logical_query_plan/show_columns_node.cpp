078a2526594d695693f27f039fef6df8ddbb3f27
martin.boissier <martin.boissier@hpi.de>
2018-11-08 11:40:30 +0100

    Change access to table names of some LQP nodes

--- src/lib/logical_query_plan/show_columns_node.cpp
+++ src/lib/logical_query_plan/show_columns_node.cpp
@@ -9,11 +9,9 @@ using namespace opossum::expression_functional;  // NOLINT
 namespace opossum {
 
 ShowColumnsNode::ShowColumnsNode(const std::string& table_name)
-    : AbstractLQPNode(LQPNodeType::ShowColumns), _table_name(table_name) {}
+    : AbstractLQPNode(LQPNodeType::ShowColumns), table_name(table_name) {}
 
-std::string ShowColumnsNode::description() const { return "[ShowColumns] Table: '" + _table_name + "'"; }
-
-const std::string& ShowColumnsNode::table_name() const { return _table_name; }
+std::string ShowColumnsNode::description() const { return "[ShowColumns] Table: '" + table_name + "'"; }
 
 const std::vector<std::shared_ptr<AbstractExpression>>& ShowColumnsNode::column_expressions() const {
   if (!_column_expressions) {
@@ -28,12 +26,12 @@ const std::vector<std::shared_ptr<AbstractExpression>>& ShowColumnsNode::column_
 }
 
 std::shared_ptr<AbstractLQPNode> ShowColumnsNode::_on_shallow_copy(LQPNodeMapping& node_mapping) const {
-  return ShowColumnsNode::make(_table_name);
+  return ShowColumnsNode::make(table_name);
 }
 
 bool ShowColumnsNode::_on_shallow_equals(const AbstractLQPNode& rhs, const LQPNodeMapping& node_mapping) const {
   const auto& show_columns_node_rhs = static_cast<const ShowColumnsNode&>(rhs);
-  return _table_name == show_columns_node_rhs._table_name;
+  return table_name == show_columns_node_rhs.table_name;
 }
 
 }  // namespace opossum
