a856c705de4e55f928ef02af5bc8547b57386d2b
Markus Dreseler <markus.dreseler@hpi.de>
2018-11-13 10:13:45 +0100

    Remove LQPColumnReference from AllParameterVariant

--- src/lib/all_parameter_variant.hpp
+++ src/lib/all_parameter_variant.hpp
@@ -21,7 +21,6 @@ namespace hana = boost::hana;
 static constexpr auto parameter_types =
     hana::make_tuple(hana::make_pair("AllTypeVariant", hana::type_c<AllTypeVariant>),
                      hana::make_pair("ColumnID", hana::type_c<ColumnID>),                      // NOLINT
-                     hana::make_pair("LQPColumnReference", hana::type_c<LQPColumnReference>),  // NOLINT
                      hana::make_pair("Parameter", hana::type_c<ParameterID>));                 // NOLINT
 
 // This holds only the possible data types.
@@ -39,11 +38,6 @@ inline bool is_variant(const AllParameterVariant& variant) { return (variant.typ
 
 // Function to check if AllParameterVariant is a column id
 inline bool is_column_id(const AllParameterVariant& variant) { return (variant.type() == typeid(ColumnID)); }
-
-// Function to check if AllParameterVariant is an LQP column reference
-inline bool is_lqp_column_reference(const AllParameterVariant& variant) {
-  return (variant.type() == typeid(LQPColumnReference));
-}
 
 // Function to check if AllParameterVariant is a ParameterID
 inline bool is_parameter_id(const AllParameterVariant& variant) { return (variant.type() == typeid(ParameterID)); }
