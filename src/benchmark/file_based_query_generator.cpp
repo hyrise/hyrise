bb1c055d9f2f5c7052367a53a9bcc5fd3c6bbdff
Moritz Eyssen <Moritz.Eyssen@student.hpi.de>
2018-11-15 20:05:52 +0100

    changes

--- src/benchmark/file_based_query_generator.cpp
+++ src/benchmark/file_based_query_generator.cpp
@@ -3,6 +3,8 @@
 #include <boost/algorithm/string.hpp>
 #include <fstream>
 
+#include "SQLParser.h"
+#include "sql/create_sql_parser_error_message.hpp"
 #include "utils/assert.hpp"
 #include "utils/filesystem.hpp"
 
@@ -31,27 +33,29 @@ FileBasedQueryGenerator::FileBasedQueryGenerator(const BenchmarkConfig& config, 
   std::iota(_selected_queries.begin(), _selected_queries.end(), QueryID{0});
 }
 
-void FileBasedQueryGenerator::_parse_query_file(const std::string& query_file) {
-  auto query_id = 0u;
+void FileBasedQueryGenerator::_parse_query_file(const std::string& query_path) {
+  std::ifstream file(query_path);
+  const auto filename = filesystem::path{query_path}.stem().string();
 
-  std::ifstream file(query_file);
-  const auto filename = filesystem::path{query_file}.stem().string();
+  std::string content{std::istreambuf_iterator<char>(file), {}};
 
-  std::string query;
-  while (std::getline(file, query)) {
-    if (query.empty() || query.substr(0, 2) == "--") {
-      continue;
-    }
+  hsql::SQLParserResult parse_result;
+  hsql::SQLParser::parse(content, &parse_result);
+  Assert(parse_result.isValid(), create_sql_parser_error_message(content, parse_result));
 
-    const auto query_name = filename + '.' + std::to_string(query_id);
+  size_t sql_string_offset{0u};
+  for (auto statement_idx = size_t{0}; statement_idx < parse_result.size(); ++statement_idx) {
+    const auto query_name = filename + '.' + std::to_string(statement_idx);
+    const auto statement_string_length = parse_result.getStatement(statement_idx)->stringLength;
+    const auto statement_string = boost::trim_copy(content.substr(sql_string_offset, statement_string_length));
+    sql_string_offset += statement_string_length;
     _query_names.emplace_back(query_name);
-    _queries.emplace_back(std::move(query));
-    query_id++;
+    _queries.emplace_back(std::move(statement_string));
   }
 
   // More convenient names if there is only one query per file
-  if (query_id == 1) {
-    auto& query_name = _query_names.back();
+  if (_queries.size() == 1) {
+    auto& query_name = _query_names[0];
     query_name.erase(query_name.end() - 2, query_name.end());  // -2 because .0 at end of name
   }
 }
