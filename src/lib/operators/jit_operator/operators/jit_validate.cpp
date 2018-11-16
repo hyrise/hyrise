f4a49a51bbc9b0462486ce66e6ae38ccced23e67
Fabian Wiebe <fabian.wiebe@student.hpi.de>
2018-11-16 13:06:37 +0100

    Make clang tidy happy:
    - Rename _is_row_visible to is_row_visible

--- src/lib/operators/jit_operator/operators/jit_validate.cpp
+++ src/lib/operators/jit_operator/operators/jit_validate.cpp
@@ -6,12 +6,14 @@
 namespace opossum {
 
 namespace {
-bool _is_row_visible(const CommitID our_tid, const TransactionID row_tid, const CommitID snapshot_commit_id,
-                     const ChunkOffset chunk_offset, const MvccData& mvcc_data) {
+
+bool is_row_visible(const CommitID our_tid, const TransactionID row_tid, const CommitID snapshot_commit_id,
+                    const ChunkOffset chunk_offset, const MvccData& mvcc_data) {
   const auto begin_cid = mvcc_data.begin_cids[chunk_offset];
   const auto end_cid = mvcc_data.end_cids[chunk_offset];
   return Validate::is_row_visible(our_tid, snapshot_commit_id, row_tid, begin_cid, end_cid);
 }
+
 }  // namespace
 
 JitValidate::JitValidate(const TableType input_table_type) : _input_table_type(input_table_type) {}
@@ -26,13 +28,13 @@ void JitValidate::_consume(JitRuntimeContext& context) const {
     const auto& referenced_chunk = context.referenced_table->get_chunk(row_id.chunk_id);
     const auto mvcc_data = referenced_chunk->get_scoped_mvcc_data_lock();
     const auto row_tid = _load_atomic_value(mvcc_data->tids[row_id.chunk_offset]);
-    if (_is_row_visible(context.transaction_id, row_tid, context.snapshot_commit_id, row_id.chunk_offset, *mvcc_data)) {
+    if (is_row_visible(context.transaction_id, row_tid, context.snapshot_commit_id, row_id.chunk_offset, *mvcc_data)) {
       _emit(context);
     }
   } else {
     const auto row_tid = context.row_tids[context.chunk_offset];
-    if (_is_row_visible(context.transaction_id, row_tid, context.snapshot_commit_id, context.chunk_offset,
-                        *context.mvcc_data)) {
+    if (is_row_visible(context.transaction_id, row_tid, context.snapshot_commit_id, context.chunk_offset,
+                       *context.mvcc_data)) {
       _emit(context);
     }
   }
