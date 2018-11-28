df62d19e1e636b8fa12208f977828b9fcb4a0a71
Leander Neiß <leander.neiss@student.hpi.de>
2018-11-29 00:14:12 +0100

    Adjust bencho.py.

--- bencho.py
+++ bencho.py
@@ -19,11 +19,14 @@ CWD = os.getcwd()
 OUTPUT_FILE = 'current_run.out'
 ERROR_FILE = 'current_run.err'
 
+NOTE = '_pcmnew' # '_something'
+
 timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 
 static_parameters = [
     '--verbose',
-    '--queries 1,2,3,4,5,6,7,8,9,10,11,12,13,14,16,17,18,19,20,21,22',
+    # '--queries 1,2,3,4,5,6,7,8,9,10,11,12,13,14,16,17,18,19,20,21,22',
+    '--queries 1,2,3,4,5,6,7,8,9,10,11,12,13,14,16,18,19,22', # Ohne 17,20,21
     # '--cores 224 168 112 56 28 14 7 2 1 0',
     '--cores 0 1 2 7 14 28 56 84 112 140 168 196 224',
     '--runs 10000',
@@ -73,7 +76,7 @@ for vp in variable_parameters:
     params = ''.join(' '.join(vp).split('--')).split(' ')
     chunksize_str = chunk_size_to_str[params[-1]]
     params_str = params[0] + params[1] + '_' + params[2] + params[3] + '_chunksize' + chunksize_str
-    resultname = 'tpch_pcmnew_' + params_str + '_' + timestamp
+    resultname = 'tpch' + NOTE + '_' + params_str + '_' + timestamp
     cmd = './scripts/benchmark_multithreaded.py -e build-release/hyriseBenchmarkTPCH ' + ' '.join(static_parameters) + ' ' + ' '.join(vp) + ' ' + '--result-name ' + resultname
     commands.append((cmd, resultname))
 
