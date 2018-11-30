7e53a94461931f1350014de21d9ca9800e2a9d4d
Moritz Eyssen <Moritz.Eyssen@student.hpi.de>
2018-11-30 10:24:41 +0100

    release stuff

--- src/lib/storage/segment_iteration.hpp
+++ src/lib/storage/segment_iteration.hpp
@@ -4,6 +4,8 @@
 #include "resolve_type.hpp"
 #include "storage/base_segment.hpp"
 #include "storage/segment_iterables/any_segment_iterable.hpp"
+#include "storage/segment_iterables.hpp"
+#include "storage/create_iterable_from_segment.hpp"
 
 namespace opossum {
 
@@ -14,7 +16,20 @@ void segment_with_iterators(const BaseSegment& base_segment, const std::shared_p
   const auto any_segment_iterable = create_any_segment_iterable<T>(base_segment);
   any_segment_iterable.with_iterators(position_filter, functor);
 #else
+  resolve_segment_type<T>(base_segment, [&](const auto& segment) {
+    const auto segment_iterable = create_iterable_from_segment<T>(segment);
+    if (position_filter) {
+      using SegmentType = typename std::decay_t<decltype(segment)>;
 
+      if constexpr (is_point_accessible_segment_iterable_v<SegmentType>) {
+        segment_iterable.with_iterators(position_filter, functor);
+      } else {
+        Fail("Cannot access non-PointAccessibleSegmentIterable with position_filter"); 
+      }
+    } else {
+      segment_iterable.with_iterators(functor);
+    }
+  });
 #endif
 }
 
@@ -41,12 +56,12 @@ void segment_with_iterators_and_data_type_resolve(const BaseSegment& base_segmen
 template <typename T, typename Functor>
 void segment_for_each(const BaseSegment& base_segment, const std::shared_ptr<const PosList>& position_filter,
                       const Functor& functor) {
-#if IS_DEBUG
-  const auto any_segment_iterable = create_any_segment_iterable<T>(base_segment);
-  any_segment_iterable.for_each(position_filter, functor);
-#else
-
-#endif
+  segment_with_iterators<T>(base_segment, position_filter, [&](auto it, const auto end) {
+    while (it != end) {
+      functor(*it);
+      ++it;
+    }
+  });
 }
 
 template <typename T, typename Functor>
