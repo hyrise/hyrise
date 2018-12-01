98497cc6643a4fa335c5a4ae0786a06319f0a76d
Alexander Riese <alexander.riese@student.hpi.de>
2018-12-01 17:19:27 +0100

    add aggregate benchmarks with different column quantities

--- src/benchmark/operators/aggregate_benchmark.cpp
+++ src/benchmark/operators/aggregate_benchmark.cpp
@@ -9,19 +9,38 @@
 
 namespace opossum {
 
-BENCHMARK_F(MicroBenchmarkBasicFixture, BM_Aggregate)(benchmark::State& state) {
-  _clear_cache();
-
+void bm_aggregate_impl(benchmark::State& state, std::shared_ptr<TableWrapper> table, std::vector<ColumnID>& groupby) {
   std::vector<AggregateColumnDefinition> aggregates = {{ColumnID{1} /* "b" */, AggregateFunction::Min}};
-
-  std::vector<ColumnID> groupby = {ColumnID{0} /* "a" */};
-
-  auto warm_up = std::make_shared<Aggregate>(_table_wrapper_a, aggregates, groupby);
+  auto warm_up = std::make_shared<Aggregate>(table, aggregates, groupby);
   warm_up->execute();
   while (state.KeepRunning()) {
-    auto aggregate = std::make_shared<Aggregate>(_table_wrapper_a, aggregates, groupby);
+    auto aggregate = std::make_shared<Aggregate>(table, aggregates, groupby);
     aggregate->execute();
   }
 }
 
+BENCHMARK_F(MicroBenchmarkBasicFixture, BM_Aggregate_1_Column)(benchmark::State& state) {
+  _clear_cache();
+  std::vector<ColumnID> groupby = {ColumnID{0}};
+  bm_aggregate_impl(state, _table_wrapper_a, groupby);
+}
+
+BENCHMARK_F(MicroBenchmarkBasicFixture, BM_Aggregate_2_Columns)(benchmark::State& state) {
+  _clear_cache();
+  std::vector<ColumnID> groupby = {ColumnID{0}, ColumnID{1}};
+  bm_aggregate_impl(state, _table_wrapper_a, groupby);
+}
+
+BENCHMARK_F(MicroBenchmarkBasicFixture, BM_Aggregate_4_Columns)(benchmark::State& state) {
+  _clear_cache();
+  std::vector<ColumnID> groupby = {ColumnID{0}, ColumnID{1}, ColumnID{2}, ColumnID{3}};
+  bm_aggregate_impl(state, _table_wrapper_a, groupby);
+}
+
+BENCHMARK_F(MicroBenchmarkBasicFixture, BM_Aggregate_8_Columns)(benchmark::State& state) {
+  _clear_cache();
+  std::vector<ColumnID> groupby = {ColumnID{0}, ColumnID{1}, ColumnID{2}, ColumnID{3},
+                                   ColumnID{4}, ColumnID{5}, ColumnID{6}, ColumnID{7}};
+  bm_aggregate_impl(state, _table_wrapper_a, groupby);
+}
 }  // namespace opossum
