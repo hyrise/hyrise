e6ca6c8aae25f34d6ea1c285b2809a458900f519
Moritz Eyssen <mrzesn@t-online.de>
2018-12-05 13:09:15 +0100

    Retire iterative Optimizer architecture (#1347)
    * No more batches
    
    * fullci

--- src/lib/optimizer/strategy/column_pruning_rule.hpp
+++ src/lib/optimizer/strategy/column_pruning_rule.hpp
@@ -22,11 +22,11 @@ class AbstractLQPNode;
 class ColumnPruningRule : public AbstractRule {
  public:
   std::string name() const override;
-  bool apply_to(const std::shared_ptr<AbstractLQPNode>& lqp) const override;
+  void apply_to(const std::shared_ptr<AbstractLQPNode>& lqp) const override;
 
  private:
   static ExpressionUnorderedSet _collect_actually_used_columns(const std::shared_ptr<AbstractLQPNode>& lqp);
-  static bool _prune_columns_from_leaves(const std::shared_ptr<AbstractLQPNode>& lqp,
+  static void _prune_columns_from_leaves(const std::shared_ptr<AbstractLQPNode>& lqp,
                                          const ExpressionUnorderedSet& referenced_columns);
   static void _prune_columns_in_projections(const std::shared_ptr<AbstractLQPNode>& lqp,
                                             const ExpressionUnorderedSet& referenced_columns);
