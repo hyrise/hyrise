52fd6dda8ad1c2dbb9cd70d31c7372992202b907
martin.boissier <martin.boissier@hpi.de>
2018-12-10 14:02:33 +0100

    Playground for remote testing.

--- src/bin/playground.cpp
+++ src/bin/playground.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <bitset>
 
+#include "statistics/chunk_statistics/counting_quotient_filter.hpp"
+
 #include "types.hpp"
 
 using namespace opossum;  // NOLINT
@@ -8,24 +10,32 @@ using namespace opossum;  // NOLINT
 template <typename T>
 void hashish(T value) {
 	size_t hash = std::hash<T>{}(value);
-	size_t new_hash = (std::hash<T>{}(value) * 11400714819323198485llu) >> 32;
-	std::cout << value << " >> " << std::bitset<64>(hash) << " -- " << std::bitset<64>(new_hash) << std::endl;
+	size_t fibo = (std::hash<T>{}(value) * 11400714819323198485llu);
+	size_t fibo2 = CountingQuotientFilter<T>::get_hash_bits(static_cast<T>(value), 64);
+	std::cout << value << " >>\n\t\t" << std::bitset<64>(hash) << "\n\t\t" << std::bitset<64>(fibo) << "\n\t\t" << std::bitset<64>(fibo2) << std::endl << std::endl;
 }
 
 int main() {
+  std::cout << "Floats" << std::endl;
   hashish(1.2f);
   hashish(12.0f);
   hashish(120.0f);
+  hashish(123.0f);
   hashish(1200000000.0f);
 
+  std::cout << "Doubles" << std::endl;
   hashish(1.2);
   hashish(12.0);
   hashish(120.0);
+  hashish(123.0);
   hashish(1200000000.0);
 
+  std::cout << "Integers" << std::endl;
   hashish(1);
   hashish(12);
   hashish(120);
+  hashish(123);
   hashish(1200000000);
+
   return 0;
 }
