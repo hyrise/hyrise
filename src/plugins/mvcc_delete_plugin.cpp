ad4b863bfa8cca4babba7f652f2fd2a3f3b1ef0e
David Justen <mail@davidjusten.de>
2018-12-16 16:48:08 +0100

    Implement experimental mvcc delete

--- src/plugins/mvcc_delete_plugin.cpp
+++ src/plugins/mvcc_delete_plugin.cpp
@@ -7,7 +7,24 @@ namespace opossum {
 const std::string MvccDeletePlugin::description() const { return "This is the Hyrise TestPlugin"; }
 
 void MvccDeletePlugin::start() {
-  //TODO: Implement MVCC Delete logic
+  for(const auto& table : sm.tables()) {
+    auto& chunks = table.second->chunks();
+
+    for (ChunkID chunk_id = ChunkID{0}; chunk_id < chunks.size(); chunk_id++) {
+      auto chunk = chunks[chunk_id];
+
+      if (chunk) {
+        std::cout << chunk->invalid_row_count() << " of " << chunk->size() << std::endl;
+        const double invalid_row_amount = static_cast<double>(chunk->invalid_row_count()) / chunk->size();
+        std::cout << invalid_row_amount << std::endl;
+        if (invalid_row_amount == 1) {
+          //TODO: Implement update if e.g. 0.9 < invalid_row_amaount < 1
+
+          table.second->delete_chunk(chunk_id);
+        }
+      }
+    }
+  }
 }
 
 void MvccDeletePlugin::stop() {
