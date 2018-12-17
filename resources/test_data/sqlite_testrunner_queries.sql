758939c29a617fe5ef454096bd5d646a899487d8
Moritz Eyssen <Moritz.Eyssen@student.hpi.de>
2018-12-17 21:32:21 +0100

    fixy

--- resources/test_data/sqlite_testrunner_queries.sql
+++ resources/test_data/sqlite_testrunner_queries.sql
@@ -28,9 +28,6 @@ SELECT * FROM mixed_null WHERE b*c IS NOT NULL;
 SELECT * FROM mixed_null WHERE b = 12;
 SELECT * FROM mixed_null WHERE NOT (b = 12);
 SELECT * FROM mixed_null WHERE NOT (b IN (12, 13, 14));
-SELECT * FROM mixed_null WHERE EXISTS(SELECT 0) OR b = 42;
-SELECT * FROM mixed_null WHERE EXISTS(SELECT 1);
-SELECT * FROM mixed_null WHERE NOT EXISTS(SELECT * FROM mixed WHERE b > 1000);
 
 -- Scans with predicates that do not reference columns
 SELECT * FROM mixed_null WHERE 4 > 3;
@@ -287,6 +284,9 @@ SELECT NOT EXISTS(SELECT * FROM id_int_int_int_100) AS some_exists;
 SELECT * FROM mixed AS outer_mixed WHERE EXISTS(SELECT * FROM mixed AS inner_mixed WHERE inner_mixed.id = outer_mixed.id * 10);
 SELECT * FROM mixed WHERE EXISTS (SELECT id_int_int_int_100.a FROM id_int_int_int_100 WHERE id_int_int_int_100.b = mixed.b);
 SELECT * FROM mixed WHERE NOT EXISTS (SELECT id_int_int_int_100.a FROM id_int_int_int_100 WHERE id_int_int_int_100.b = mixed.b);
+SELECT * FROM mixed_null WHERE EXISTS(SELECT 0) OR b = 42;
+SELECT * FROM mixed_null WHERE EXISTS(SELECT 1);
+SELECT * FROM mixed_null WHERE NOT EXISTS(SELECT * FROM mixed WHERE b > 1000);
 
 -- Cannot test the following expressions, because sqlite doesn't support them:
 --  * EXTRACT
