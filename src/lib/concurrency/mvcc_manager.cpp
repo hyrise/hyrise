c31cf06e1206c00da19a6d57617dbaaad287341b
Julian Menzler <julianmenzler@gmail.com>
2018-12-18 14:55:55 +0100

    Add assertion for edge case in deleteChunkLogically()

--- src/lib/concurrency/mvcc_manager.cpp
+++ src/lib/concurrency/mvcc_manager.cpp
@@ -13,12 +13,13 @@ void opossum::MvccManager::testDeleteChunkLogically(const std::string &tableName
 }
 
 void opossum::MvccManager::deleteChunkLogically(const std::string &tableName, const opossum::ChunkID chunkID) {
-
   auto& sm = StorageManager::get();
-  auto transaction_context = TransactionManager::get().new_transaction_context();
+  const auto table = sm.get_table(tableName);
+
+  // ToDo: Maybe handle this as an edge case: -> Re-Insert has to happen into a new chunk, not the latest
+  Assert(chunkID < (table->chunk_count() - 1), "MVCC Logical Delete should not be applied on the last/current table-chunk.")
 
   // Put relevant chunk into a temporary table
-  const auto table = sm.get_table(tableName);
   const auto table_extract = std::make_shared<Table>(table->column_definitions(), TableType::Data,
                                                           table->max_chunk_size(), table->has_mvcc());
   table_extract->append_chunk(table->get_chunk(chunkID));
@@ -27,8 +28,11 @@ void opossum::MvccManager::deleteChunkLogically(const std::string &tableName, co
 
   // Filter valid records
   auto validate_table = std::make_shared<Validate>(table_wrapper);
+  auto transaction_context = TransactionManager::get().new_transaction_context();
   validate_table->set_transaction_context(transaction_context);
   validate_table->execute();
+
+
 
   // Re-insert valid records into table (most recent chunk)
   auto insert_op = std::make_shared<Insert>(tableName, validate_table);
@@ -42,5 +46,7 @@ void opossum::MvccManager::deleteChunkLogically(const std::string &tableName, co
 }
 
 void opossum::MvccManager::deleteChunkPhysically(const std::string &tableName, const opossum::ChunkID chunkID) {
+  // Assert: Logical delete must have happened to this point.
+
   // TODO later
 }
