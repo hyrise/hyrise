1eb0568b14803009535157ab475a9ea8b84a7ffe
Moritz Eyssen <Moritz.Eyssen@student.hpi.de>
2018-12-29 20:26:39 +0100

    marge

--- src/benchmark/tpch_benchmark.cpp
+++ src/benchmark/tpch_benchmark.cpp
@@ -129,5 +129,6 @@ int main(int argc, char* argv[]) {
 
   // Run the benchmark
   opossum::BenchmarkRunner(*config, std::make_unique<opossum::TPCHQueryGenerator>(use_prepared_statements, query_ids),
-                           std::make_unique<TpchTableGenerator>(scale_factor, config), context).run();
+                           std::make_unique<TpchTableGenerator>(scale_factor, config), context)
+      .run();
 }
