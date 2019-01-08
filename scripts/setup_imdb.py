8e060c197a915f70c7baa64d19367d87f0bd2255
Moritz Eyssen <moritz.eyssen@student.hpi.de>
2019-01-08 14:18:52 +0100

    add script

---
+++ scripts/setup_imdb.py
@@ -0,0 +1,98 @@
+import hashlib
+import os
+import sys
+import urllib.request
+import zipfile
+
+
+def clean_up(including_table_dir=False):
+    if os.path.exists(FILE_NAME):
+        os.remove(FILE_NAME)
+
+    if including_table_dir and os.path.exists(table_dir):
+        for file in os.listdir(table_dir):
+            os.remove("./%s/%s" % (table_dir, file))
+        os.rmdir(table_dir)
+
+
+def is_setup():
+    for table_name in TABLE_NAMES:
+        if not os.path.exists(os.path.join(table_dir, table_name + ".csv")):
+            return False
+        if not os.path.exists(os.path.join(table_dir, table_name + ".csv.json")):
+            return False
+
+    return True
+
+
+# [cmd, table_dir]
+assert len(sys.argv) == 2
+table_dir = sys.argv[1]
+
+LOCATION = "https://www.dropbox.com/s/ckh4nyqpol70ri3/imdb.zip?dl=1"
+FILE_NAME = "imdb.zip"
+TABLE_NAMES = ["aka_name", "aka_title", "cast_info", "char_name", "company_name", "company_type", "comp_cast_type", "complete_cast", "info_type",
+                    "keyword", "kind_type", "link_type", "movie_companies", "movie_info", "movie_info_idx", "movie_keyword", "movie_link", "name",
+                    "person_info", "role_type", "title"]
+
+if is_setup():
+    print("IMDB setup already complete, no setup action required")
+    sys.exit(0)
+
+# We are going to calculate the md5 hash later, on-the-fly while downloading
+hash_md5 = hashlib.md5()
+
+url = urllib.request.urlopen(LOCATION)
+meta = url.info()
+file_size = int(meta['Content-Length'])
+
+file = open(FILE_NAME, 'wb')
+
+print("Retrieving the IMDB dataset.")
+print("Downloading: %s %.2f GB" % (FILE_NAME, file_size / 1000 / 1000 / 1000))
+
+already_retrieved = 0
+block_size = 8192
+try:
+    while True:
+        buffer = url.read(block_size)
+        if not buffer:
+            break
+
+        hash_md5.update(buffer)
+
+        already_retrieved += len(buffer)
+        file.write(buffer)
+        status = r"Retrieved %3.2f%% of the data" % (already_retrieved * 100. / file_size)
+        status = status + chr(8) * (len(status) + 1)
+        print(status, end='\r')
+except:
+    print("Aborting. Something went wrong during the download. Cleaning up.")
+    clean_up()
+    sys.exit(1)
+
+file.close()
+print()
+print("Validating integrity...")
+
+hash_dl = hash_md5.hexdigest()
+
+if hash_dl != "79e4c71f8ec0dae17d6aa9182fdab835":
+    print("Aborting. Data was modified during the download. Cleaning up.")
+    clean_up()
+    sys.exit(2)
+
+print("Downloaded file is valid.")
+print("Unzipping the file...")
+
+try:
+    zip = zipfile.ZipFile("imdb.zip", "r")
+    zip.extractall(table_dir)
+    zip.close()
+except:
+    print("Aborting. Something went wrong during unzipping. Cleaning up.")
+    clean_up(including_table_dir=True)
+    sys.exit(3)
+
+print("Success. Deleting the archive.")
+clean_up()
