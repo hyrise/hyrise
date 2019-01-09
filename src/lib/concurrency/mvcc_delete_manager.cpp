fefa22622b5f7f704019282362be156e660943ca
Julian Menzler <julianmenzler@gmail.com>
2019-01-09 09:16:50 +0100

    Adjust comments

--- src/lib/concurrency/mvcc_delete_manager.cpp
+++ src/lib/concurrency/mvcc_delete_manager.cpp
@@ -16,28 +16,28 @@ void opossum::MvccDeleteManager::_delete_logically(const std::string &tableName,
   auto& sm = StorageManager::get();
   const auto table = sm.get_table(tableName);
 
-  // ToDo: Maybe handle this as an edge case: -> Re-Insert has to happen into a new chunk, not the latest
-  Assert(chunkID < (table->chunk_count() - 1), "MVCC Logical Delete should not be applied on the last/current table-chunk.")
+  // ToDo: Maybe handle this as an edge case: -> Create a new chunk before Re-Insert
+  DebugAssert(chunkID < (table->chunk_count() - 1), "MVCC Logical Delete should not be applied on the last/current table-chunk.")
 
-  // Put relevant chunk into a temporary table
+  // Append chunk to temporary table
   const auto table_extract = std::make_shared<Table>(table->column_definitions(), TableType::Data,
                                                           table->max_chunk_size(), table->has_mvcc());
   table_extract->append_chunk(table->get_chunk(chunkID));
   auto table_wrapper = std::make_shared<TableWrapper>(table_extract);
   table_wrapper->execute();
 
-  // Filter valid records
-  auto validate_table = std::make_shared<Validate>(table_wrapper);
+  // Validate temporary table
   auto transaction_context = TransactionManager::get().new_transaction_context();
+  auto validate_table = std::make_shared<Validate>(table_wrapper);
   validate_table->set_transaction_context(transaction_context);
   validate_table->execute();
 
-  // Re-insert valid records into table (most recent chunk)
+  // Re-insert records of temporary table into original table
   auto insert_op = std::make_shared<Insert>(tableName, validate_table);
   insert_op->set_transaction_context(transaction_context);
   insert_op->execute();
 
-  // Remove records (old chunk) from table
+  // Remove records of temporary table from original table
   auto delete_op = std::make_shared<Delete>(tableName, validate_table);
   delete_op->set_transaction_context(transaction_context);
   delete_op->execute();
