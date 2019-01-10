5fac8471ae39f1256dcb6acc9e9cfb6e8591552d
Jan Kossmann <jan.kossmann@hpi.de>
2019-01-10 18:19:05 +0100

    Reflect movement of test data to resource folder

--- src/test/tasks/load_server_file_task_test.cpp
+++ src/test/tasks/load_server_file_task_test.cpp
@@ -9,15 +9,15 @@ namespace opossum {
 class LoadServerFileTaskTest : public BaseTest {};
 
 TEST_F(LoadServerFileTaskTest, LoadsDifferentFileTypes) {
-  auto tbl_task = std::make_shared<LoadServerFileTask>("src/test/tables/int_float.tbl", "int_float_tbl");
+  auto tbl_task = std::make_shared<LoadServerFileTask>("resources/test_data/tbl/int_float.tbl", "int_float_tbl");
   tbl_task->execute();
   EXPECT_TRUE(StorageManager::get().has_table("int_float_tbl"));
 
-  auto csv_task = std::make_shared<LoadServerFileTask>("src/test/tables/int_float.csv", "int_float_csv");
+  auto csv_task = std::make_shared<LoadServerFileTask>("resources/test_data/csv/int_float.csv", "int_float_csv");
   csv_task->execute();
   EXPECT_TRUE(StorageManager::get().has_table("int_float_csv"));
 
-  auto bin_task = std::make_shared<LoadServerFileTask>("src/test/tables/int_float.bin", "int_float_bin");
+  auto bin_task = std::make_shared<LoadServerFileTask>("resources/test_data/bin/int_float.bin", "int_float_bin");
   bin_task->execute();
   EXPECT_TRUE(StorageManager::get().has_table("int_float_bin"));
 
