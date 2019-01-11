a4ca467e90442a3b4eaba8f701820a462c31cbea
Markus Dreseler <markus@dreseler.de>
2019-01-11 11:12:48 +0100

    forgot one

--- third_party/tpch-dbgen/build.c
+++ third_party/tpch-dbgen/build.c
@@ -190,8 +190,8 @@ mk_order(DSS_HUGE index, order_t * o, long upd_num, float scale)
 
 
 	RANDOM(tmp_date, O_ODATE_MIN, O_ODATE_MAX, O_ODATE_SD);
-	// Modified from original. We know that dates are always yyyy-mm-dd.
-	memcpy(o->odate, asc_date[tmp_date - STARTDATE], 10);
+	// Modified from original. We know that dates are always yyyy-mm-dd + \0.
+	memcpy(o->odate, asc_date[tmp_date - STARTDATE], 11);
 
 	pick_str(&o_priority_set, O_PRIO_SD, o->opriority);
 	RANDOM(clk_num, 1, MAX((scale * O_CLRK_SCL), O_CLRK_SCL), O_CLRK_SD);
