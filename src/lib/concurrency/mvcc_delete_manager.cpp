9a46c3baab2b91b5440193f6bae779b5a753f090
Julian Menzler <julianmenzler@gmail.com>
2019-01-14 23:02:43 +0100

    Fix logical delete by adding and using a _get_referencing_table() method
    Previous implementations did not work since they conflicted with the implementation of the Delete resp. Update operator:
      - Delete operator requirement: "values_to_delete must (...) reference the table specified by table_name."
      - The GetTable operator in conjunction with set_excluded_chunk_ids() creates a new temporary table, which is not stored in the StorageManager. Following operators like Validate create tables referencing the temporary table, but not the original table.
    
    _get_referencing_table() creates a temporary table, just like GetTable with set_excluded_chunk_ids(). But in comparison it references to the original table, which makes the difference when using the Update resp. Delete operators.

--- src/lib/concurrency/mvcc_delete_manager.cpp
+++ src/lib/concurrency/mvcc_delete_manager.cpp
@@ -1,54 +1,51 @@
+#include "mvcc_delete_manager.hpp"
 #include <operators/get_table.hpp>
-#include <operators/validate.hpp>
 #include <operators/table_wrapper.hpp>
 #include <operators/update.hpp>
-#include "mvcc_delete_manager.hpp"
-#include "storage/storage_manager.hpp"
+#include <operators/validate.hpp>
+#include <storage/pos_list.hpp>
+#include <storage/reference_segment.hpp>
 #include "concurrency/transaction_manager.hpp"
-
+#include "storage/storage_manager.hpp"
 
 namespace opossum {
 
-void MvccDeleteManager::run_logical_delete(const std::string &table_name, ChunkID chunk_id) {
+void MvccDeleteManager::run_logical_delete(const std::string& table_name, const ChunkID chunk_id) {
   _delete_logically(table_name, chunk_id);
 }
 
-bool MvccDeleteManager::_delete_logically(const std::string &table_name, const ChunkID chunk_id) {
+bool MvccDeleteManager::_delete_logically(const std::string& table_name, const ChunkID chunk_id) {
   auto& sm = StorageManager::get();
   const auto table = sm.get_table(table_name);
   auto chunk = table->get_chunk(chunk_id);
 
   // ToDo: Maybe handle this as an edge case: -> Create a new chunk before Re-Insert
-  DebugAssert(chunk_id < (table->chunk_count() - 1), "MVCC Logical Delete should not be applied on the last/current mutable chunk.")
+  DebugAssert(chunk_id < (table->chunk_count() - 1),
+              "MVCC Logical Delete should not be applied on the last/current mutable chunk.")
 
-  auto get_table = std::make_shared<GetTable>(table_name);
-  std::vector<ChunkID> excluded_chunks;
-  excluded_chunks.reserve(table->chunk_count());
-  for(ChunkID current_chunk_id{0}; current_chunk_id < table->chunk_count(); ++current_chunk_id) {
-    if(current_chunk_id != chunk_id)
-    excluded_chunks.push_back(current_chunk_id);
-  }
-  get_table->set_excluded_chunk_ids(excluded_chunks);
-  get_table->execute();
+      // Create temporary referencing table that contains the given chunk only
+      auto table_filtered = _get_referencing_table(table_name, chunk_id);
+  auto table_wrapper = std::make_shared<TableWrapper>(table_filtered);
+  table_wrapper->execute();
 
   // Validate temporary table
   auto transaction_context = TransactionManager::get().new_transaction_context();
-  auto validate_table = std::make_shared<Validate>(get_table);
+  auto validate_table = std::make_shared<Validate>(table_wrapper);
   validate_table->set_transaction_context(transaction_context);
   validate_table->execute();
 
   // Use UPDATE operator to DELETE and RE-INSERT valid records in chunk
-    // Pass validate_table as input twice since data will not be changed.
+  // Pass validate_table as input twice since data will not be changed.
   auto update_table = std::make_shared<Update>(table_name, validate_table, validate_table);
   update_table->set_transaction_context(transaction_context);
   update_table->execute();
 
   // Check for success
-  if(update_table->execute_failed()) {
+  if (update_table->execute_failed()) {
     return false;
   }
 
-  // TODO: Check for success of commit, currently (2019-01-11) not possible.
+  // TODO(all): Check for success of commit, currently (2019-01-11) not possible.
   transaction_context->commit();
 
   // Mark chunk as logically deleted
@@ -56,11 +53,46 @@ bool MvccDeleteManager::_delete_logically(const std::string &table_name, const C
   return true;
 }
 
-void MvccDeleteManager::_delete_physically(const std::string &tableName, const opossum::ChunkID chunkID) {
+/**
+ * Creates a new referencing table with only one chunk from a given table
+ */
+std::shared_ptr<const Table> MvccDeleteManager::_get_referencing_table(const std::string& table_name,
+                                                                       const ChunkID chunk_id) {
+  auto& sm = StorageManager::get();
+  const auto table_in = sm.get_table(table_name);
+  const auto chunk_in = table_in->get_chunk(chunk_id);
+
+  // Create new table
+  auto table_out = std::make_shared<Table>(table_in->column_definitions(), TableType::References);
+
+  DebugAssert(!std::dynamic_pointer_cast<const ReferenceSegment>(chunk_in->get_segment(ColumnID{0})),
+              "Only Value- or DictionarySegments can be used.");
+
+  // Generate pos_list_out.
+  auto pos_list_out = std::make_shared<PosList>();
+  auto chunk_size = chunk_in->size();  // The compiler fails to optimize this in the for clause :(
+  for (auto i = 0u; i < chunk_size; i++) {
+    pos_list_out->emplace_back(RowID{chunk_id, i});
+  }
+
+  // Create actual ReferenceSegment objects.
+  Segments output_segments;
+  for (ColumnID column_id{0}; column_id < chunk_in->column_count(); ++column_id) {
+    auto ref_segment_out = std::make_shared<ReferenceSegment>(table_in, column_id, pos_list_out);
+    output_segments.push_back(ref_segment_out);
+  }
+
+  if (!pos_list_out->empty() > 0) {
+    table_out->append_chunk(output_segments);
+  }
+
+  return table_out;
+}  // namespace opossum
+
+void MvccDeleteManager::_delete_physically(const std::string& tableName, const ChunkID chunkID) {
   // Assert: Logical delete must have happened to this point.
 
-  // TODO later
+  // TODO(Julian) later
 }
-
 
 }  // namespace opossum
