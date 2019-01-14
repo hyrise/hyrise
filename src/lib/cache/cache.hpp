4e80a57e3e4e7eb1b7d2afd0de8fdc6ea07b5132
martin.boissier <martin.boissier@hpi.de>
2019-01-14 17:27:59 +0100

    Do not use auto as a return type.

--- src/lib/cache/cache.hpp
+++ src/lib/cache/cache.hpp
@@ -19,6 +19,8 @@ inline constexpr size_t DefaultCacheCapacity = 1024;
 template <typename Value, typename Key = std::string>
 class Cache : public Singleton<Cache<Value, Key>> {
  public:
+  using Iterator = typename AbstractCacheImpl<Key, Value>::ErasedIterator;
+
   explicit Cache(size_t capacity = DefaultCacheCapacity)
       : _impl(std::move(std::make_unique<GDFSCache<Key, Value>>(capacity))) {}
 
@@ -72,9 +74,9 @@ class Cache : public Singleton<Cache<Value, Key>> {
     _impl = std::make_unique<cache_t>(capacity);
   }
 
-  auto begin() { return _impl->begin(); }
+  Iterator begin() { return _impl->begin(); }
 
-  auto end() { return _impl->end(); }
+  Iterator end() { return _impl->end(); }
 
  protected:
   // Underlying cache eviction strategy.
