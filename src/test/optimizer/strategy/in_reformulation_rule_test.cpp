3ca4d25d01b290a3b32a668d652064ddecdccaee
Jonathan Janetzki <jonathan.janetzki@student.hpi.de>
2019-01-14 16:43:00 +0100

    #6 :white_check_mark: Test simple correlated query with equality predicate

--- src/test/optimizer/strategy/in_reformulation_rule_test.cpp
+++ src/test/optimizer/strategy/in_reformulation_rule_test.cpp
@@ -127,6 +127,38 @@ TEST_F(InReformulationRuleTest, SimpleCorrelatedInToInnerJoin) {
 }
 
 // We cannot use an anti join here, because it does not preserve the columns from the right sub-tree.
+TEST_F(InReformulationRuleTest, SimpleCorrelatedNotInWithEqualityPredicateToInnerJoin) {
+  // SELECT * FROM a WHERE a.a NOT IN (SELECT b.a FROM b WHERE b.b = a.b)
+  const auto parameter = correlated_parameter_(ParameterID{0}, node_table_a_col_b);
+
+  // clang-format off
+  const auto subselect_lqp =
+      ProjectionNode::make(expression_vector(node_table_b_col_a),
+                           PredicateNode::make(equals_(node_table_b_col_b, parameter),
+                                               node_table_b));
+
+  const auto subselect = lqp_select_(subselect_lqp, std::make_pair(ParameterID{0}, node_table_a_col_b));
+
+  const auto input_lqp =
+      PredicateNode::make(not_in_(node_table_a_col_a, subselect),
+                          node_table_a);
+
+  const auto expected_lqp =
+      AggregateNode::make(expression_vector(node_table_a_col_a, node_table_a_col_b), expression_vector(),
+                          ProjectionNode::make(expression_vector(node_table_a_col_a, node_table_a_col_b),
+                                               PredicateNode::make(equals_(node_table_b_col_b, node_table_a_col_b),
+                                                                   JoinNode::make(JoinMode::Inner,
+                                                                                  not_equals_(node_table_a_col_a,
+                                                                                              node_table_b_col_a),
+                                                                                  node_table_a,
+                                                                                  node_table_b))));
+  // clang-format on
+  const auto actual_lqp = StrategyBaseTest::apply_rule(_rule, input_lqp);
+
+  EXPECT_LQP_EQ(actual_lqp, expected_lqp);
+}
+
+// We cannot use an anti join here, because it does not preserve the columns from the right sub-tree.
 TEST_F(InReformulationRuleTest, SimpleCorrelatedNotInWithLessThanPredicateToInnerJoin) {
   // SELECT * FROM a WHERE a.a NOT IN (SELECT b.a FROM b WHERE b.b < a.b)
   const auto parameter = correlated_parameter_(ParameterID{0}, node_table_a_col_b);
