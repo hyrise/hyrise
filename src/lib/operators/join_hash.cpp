74a360329f42a959f6d1f7a1a315ef78ff0f5066
Marcel <marcel.weisgut@student.hpi.de>
2019-01-15 15:32:41 +0100

    added accessor approaches

--- src/lib/operators/join_hash.cpp
+++ src/lib/operators/join_hash.cpp
@@ -301,14 +301,17 @@ class JoinHash::JoinHashImpl : public AbstractJoinOperatorImpl {
     The workers for each radix partition P should be scheduled on the same node as the input data:
     leftP, rightP and hashtableP.
     */
-    const auto& join_pred_vector = _additional_join_predicates.has_value() ? _additional_join_predicates.value() : std::vector<JoinPredicate>{};
+    const auto& join_pred_vector =
+        _additional_join_predicates.has_value() ? _additional_join_predicates.value() : std::vector<JoinPredicate>{};
     if (_mode == JoinMode::Semi || _mode == JoinMode::Anti) {
       probe_semi_anti<RightType, HashedType>(radix_right, hashtables, right_pos_lists, _mode);
     } else {
       if (_mode == JoinMode::Left || _mode == JoinMode::Right) {
-        probe<RightType, HashedType, true>(radix_right, hashtables, left_pos_lists, right_pos_lists, _mode, *left_in_table, *right_in_table, join_pred_vector);
+        probe<RightType, HashedType, true>(radix_right, hashtables, left_pos_lists, right_pos_lists, _mode,
+                                           *left_in_table, *right_in_table, join_pred_vector);
       } else {
-        probe<RightType, HashedType, false>(radix_right, hashtables, left_pos_lists, right_pos_lists, _mode, *left_in_table, *right_in_table, join_pred_vector);
+        probe<RightType, HashedType, false>(radix_right, hashtables, left_pos_lists, right_pos_lists, _mode,
+                                            *left_in_table, *right_in_table, join_pred_vector);
       }
     }
 
