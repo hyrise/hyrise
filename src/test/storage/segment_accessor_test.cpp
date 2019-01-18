8f79ea9982e38230972140de835612030636c01d
Jan Kossmann <jan.kossmann@hpi.de>
2019-01-18 13:50:32 +0100

    Add tests for NULL for SegmentAccessor

--- src/test/storage/segment_accessor_test.cpp
+++ src/test/storage/segment_accessor_test.cpp
@@ -15,15 +15,17 @@ namespace opossum {
 class SegmentAccessorTest : public BaseTest {
  protected:
   void SetUp() override {
-    vc_int = make_shared_by_data_type<BaseValueSegment, ValueSegment>(DataType::Int);
+    vc_int = make_shared_by_data_type<BaseValueSegment, ValueSegment>(DataType::Int, true);
     vc_int->append(4);
     vc_int->append(6);
     vc_int->append(3);
+    vc_int->append(NULL_VALUE);
 
-    vc_str = make_shared_by_data_type<BaseValueSegment, ValueSegment>(DataType::String);
+    vc_str = make_shared_by_data_type<BaseValueSegment, ValueSegment>(DataType::String, true);
     vc_str->append("Hello,");
     vc_str->append("world");
     vc_str->append("!");
+    vc_str->append(NULL_VALUE);
 
     dc_int = encode_segment(EncodingType::Dictionary, DataType::Int, vc_int);
     dc_str = encode_segment(EncodingType::Dictionary, DataType::String, vc_str);
@@ -35,7 +37,14 @@ class SegmentAccessorTest : public BaseTest {
     tbl->append_chunk(chunk);
 
     pos_list = std::make_shared<PosList>(PosList{
-        {RowID{ChunkID{0}, ChunkOffset{1}}, RowID{ChunkID{0}, ChunkOffset{2}}, RowID{ChunkID{0}, ChunkOffset{0}}}});
+        {
+          RowID{ChunkID{0}, ChunkOffset{1}},
+          RowID{ChunkID{0}, ChunkOffset{2}},
+          RowID{ChunkID{0}, ChunkOffset{0}},
+          RowID{ChunkID{0}, ChunkOffset{3}},
+          RowID{NULL_ROW_ID},
+        }
+    });
 
     rc_int = std::make_shared<ReferenceSegment>(tbl, ColumnID{0}, pos_list);
     rc_str = std::make_shared<ReferenceSegment>(tbl, ColumnID{1}, pos_list);
@@ -59,6 +68,7 @@ TEST_F(SegmentAccessorTest, TestValueSegmentInt) {
   EXPECT_EQ(vc_int_base_accessor->access(ChunkOffset{0}), 4);
   EXPECT_EQ(vc_int_base_accessor->access(ChunkOffset{1}), 6);
   EXPECT_EQ(vc_int_base_accessor->access(ChunkOffset{2}), 3);
+  EXPECT_FALSE(vc_int_base_accessor->access(ChunkOffset{3}).has_value());
 }
 
 TEST_F(SegmentAccessorTest, TestValueSegmentString) {
@@ -67,6 +77,7 @@ TEST_F(SegmentAccessorTest, TestValueSegmentString) {
   EXPECT_EQ(vc_str_accessor->access(ChunkOffset{0}), "Hello,");
   EXPECT_EQ(vc_str_accessor->access(ChunkOffset{1}), "world");
   EXPECT_EQ(vc_str_accessor->access(ChunkOffset{2}), "!");
+  EXPECT_FALSE(vc_str_accessor->access(ChunkOffset{3}).has_value());
 }
 
 TEST_F(SegmentAccessorTest, TestDictionarySegmentInt) {
@@ -75,6 +86,7 @@ TEST_F(SegmentAccessorTest, TestDictionarySegmentInt) {
   EXPECT_EQ(dc_int_accessor->access(ChunkOffset{0}), 4);
   EXPECT_EQ(dc_int_accessor->access(ChunkOffset{1}), 6);
   EXPECT_EQ(dc_int_accessor->access(ChunkOffset{2}), 3);
+  EXPECT_FALSE(dc_int_accessor->access(ChunkOffset{3}).has_value());
 }
 
 TEST_F(SegmentAccessorTest, TestDictionarySegmentString) {
@@ -83,6 +95,7 @@ TEST_F(SegmentAccessorTest, TestDictionarySegmentString) {
   EXPECT_EQ(dc_str_accessor->access(ChunkOffset{0}), "Hello,");
   EXPECT_EQ(dc_str_accessor->access(ChunkOffset{1}), "world");
   EXPECT_EQ(dc_str_accessor->access(ChunkOffset{2}), "!");
+  EXPECT_FALSE(dc_str_accessor->access(ChunkOffset{3}).has_value());
 }
 
 TEST_F(SegmentAccessorTest, TestReferenceSegmentToValueSegmentInt) {
@@ -91,6 +104,8 @@ TEST_F(SegmentAccessorTest, TestReferenceSegmentToValueSegmentInt) {
   EXPECT_EQ(rc_int_accessor->access(ChunkOffset{0}), 6);
   EXPECT_EQ(rc_int_accessor->access(ChunkOffset{1}), 3);
   EXPECT_EQ(rc_int_accessor->access(ChunkOffset{2}), 4);
+  EXPECT_FALSE(rc_int_accessor->access(ChunkOffset{3}).has_value());
+  EXPECT_FALSE(rc_int_accessor->access(ChunkOffset{4}).has_value());
 }
 
 TEST_F(SegmentAccessorTest, TestReferenceSegmentToDictionarySegmentString) {
@@ -99,6 +114,8 @@ TEST_F(SegmentAccessorTest, TestReferenceSegmentToDictionarySegmentString) {
   EXPECT_EQ(rc_str_accessor->access(ChunkOffset{0}), "world");
   EXPECT_EQ(rc_str_accessor->access(ChunkOffset{1}), "!");
   EXPECT_EQ(rc_str_accessor->access(ChunkOffset{2}), "Hello,");
+  EXPECT_FALSE(rc_str_accessor->access(ChunkOffset{3}).has_value());
+  EXPECT_FALSE(rc_str_accessor->access(ChunkOffset{4}).has_value());
 }
 
 }  // namespace opossum
