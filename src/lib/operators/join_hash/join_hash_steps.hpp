00b14a8fd1eae54b8510c79745d8b0adf50f31f0
mapp <mflueggen@student.hpi.de>
2019-01-22 09:43:22 -0500

    Refactors code for multi predicate join

--- src/lib/operators/join_hash/join_hash_steps.hpp
+++ src/lib/operators/join_hash/join_hash_steps.hpp
@@ -4,10 +4,7 @@
 #include <boost/lexical_cast.hpp>
 
 #include "bytell_hash_map.hpp"
-#include "expression/binary_predicate_expression.hpp"
-#include "expression/evaluation/expression_evaluator.hpp"
-#include "expression/evaluation/expression_result.hpp"
-#include "expression/value_expression.hpp"
+#include "operators/multi_predicate_join.hpp"
 #include "resolve_type.hpp"
 #include "scheduler/abstract_task.hpp"
 #include "scheduler/current_scheduler.hpp"
@@ -370,36 +367,6 @@ RadixContainer<T> partition_radix_parallel(const RadixContainer<T>& radix_contai
   CurrentScheduler::wait_for_tasks(jobs);
 
   return radix_output;
-}
-
-inline AllTypeVariant _get_value(const Table& table, const RowID row_id, const ColumnID& column_id) {
-  const auto& segment = *table.get_chunk(row_id.chunk_id)->segments()[column_id];
-  return segment[row_id.chunk_offset];
-}
-
-inline bool _fulfills_join_predicates(const Table& left_table, const Table& right_table, const RowID left_row_id,
-                                      const RowID right_row_id, const std::vector<JoinPredicate>& join_predicates) {
-  if (join_predicates.empty()) {
-    return true;
-  }
-
-  const auto& evaluator = std::make_shared<ExpressionEvaluator>();
-
-  for (const auto& join_predicate : join_predicates) {
-    const auto left_value = _get_value(left_table, left_row_id, join_predicate.column_id_pair.first);
-    const auto right_value = _get_value(right_table, right_row_id, join_predicate.column_id_pair.second);
-    const auto left_value_expression = std::make_shared<ValueExpression>(left_value);
-    const auto right_value_expression = std::make_shared<ValueExpression>(right_value);
-    const auto predicate_expression = std::make_shared<BinaryPredicateExpression>(
-        join_predicate.predicate_condition, left_value_expression, right_value_expression);
-    const auto result = evaluator->evaluate_expression_to_result<ExpressionEvaluator::Bool>(*predicate_expression);
-
-    if (!result->value(0)) {
-      return false;
-    }
-  }
-
-  return true;
 }
 
 /*
