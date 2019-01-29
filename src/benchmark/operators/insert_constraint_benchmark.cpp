92879702a32739a5d0f3b24b0732dc891c934152
Nils Thamm <hsv.forever3@web.de>
2019-01-29 22:02:07 +0100

    Added benchmark on dict compressed columns

--- src/benchmark/operators/insert_constraint_benchmark.cpp
+++ src/benchmark/operators/insert_constraint_benchmark.cpp
@@ -25,10 +25,6 @@ auto reset_table(bool use_constraints, int values_to_insert, int num_rows = 0) {
   manager.reset();
   manager.add_table("table", std::make_shared<Table>(column_definitions, TableType::Data, chunk_size, UseMvcc::Yes));
   auto table = manager.get_table("table");
-  if (use_constraints) {
-    table->add_unique_constraint({ColumnID{0}});
-    table->add_unique_constraint({ColumnID{1}});
-  }
 
   // Insert rows to table, if num_rows != 0
   int row_preinserted = 0;
@@ -47,6 +43,11 @@ auto reset_table(bool use_constraints, int values_to_insert, int num_rows = 0) {
   pre_insert->execute();
   pre_insert_context->commit();
 
+  if (use_constraints) {
+    table->add_unique_constraint({ColumnID{0}});
+    table->add_unique_constraint({ColumnID{1}});
+  }
+
   // Create insert operators depending on values to insert operator
   std::vector<std::shared_ptr<Insert>> table_inserts;
   for (int row_to_insert = row_preinserted; row_to_insert < values_to_insert + num_rows; row_to_insert++) {
@@ -62,31 +63,31 @@ auto reset_table(bool use_constraints, int values_to_insert, int num_rows = 0) {
   return table_inserts;
 }
 
-BENCHMARK_DEFINE_F(MicroBenchmarkBasicFixture, BM_InsertEmptyTableWithConstraint)(benchmark::State& state) {
-  while (state.KeepRunning()) {
-    state.PauseTiming();
+// BENCHMARK_DEFINE_F(MicroBenchmarkBasicFixture, BM_InsertEmptyTableWithConstraint)(benchmark::State& state) {
+//   while (state.KeepRunning()) {
+//     state.PauseTiming();
 
-    auto table_inserts = reset_table(state.range(0), 1000);
+//     auto table_inserts = reset_table(state.range(0), 1000);
 
-    state.ResumeTiming();
+//     state.ResumeTiming();
 
-    for (const auto& table_insert : table_inserts) {
-      auto table_context = TransactionManager::get().new_transaction_context();
-      table_insert->set_transaction_context(table_context);
-      table_insert->execute();
-      table_context->commit();
-    }
-  }
-}
+//     for (const auto& table_insert : table_inserts) {
+//       auto table_context = TransactionManager::get().new_transaction_context();
+//       table_insert->set_transaction_context(table_context);
+//       table_insert->execute();
+//       table_context->commit();
+//     }
+//   }
+// }
 
-static void InsertRanges(benchmark::internal::Benchmark* b) {
-  for (uint32_t j = 1; j <= 256; j *= 2) {
-    b->Args({true, j});
-    b->Args({false, j});
-  }
-}
+// static void InsertRanges(benchmark::internal::Benchmark* b) {
+//   for (uint32_t j = 1; j <= 256; j *= 2) {
+//     b->Args({true, j});
+//     b->Args({false, j});
+//   }
+// }
 
-BENCHMARK_REGISTER_F(MicroBenchmarkBasicFixture, BM_InsertEmptyTableWithConstraint)->Apply(InsertRanges);
+// BENCHMARK_REGISTER_F(MicroBenchmarkBasicFixture, BM_InsertEmptyTableWithConstraint)->Apply(InsertRanges);
 
 BENCHMARK_DEFINE_F(MicroBenchmarkBasicFixture, BM_InsertFilledTableWithConstraint)(benchmark::State& state) {
   while (state.KeepRunning()) {
@@ -106,12 +107,77 @@ BENCHMARK_DEFINE_F(MicroBenchmarkBasicFixture, BM_InsertFilledTableWithConstrain
 }
 
 static void PreInsertRanges(benchmark::internal::Benchmark* b) {
-  for (uint32_t j = 100; j <= 100000; j *= 10) {
+  for (uint32_t j = 10000; j <= 1000000; j *= 10) {
     b->Args({true, j});
     b->Args({false, j});
   }
 }
 
 BENCHMARK_REGISTER_F(MicroBenchmarkBasicFixture, BM_InsertFilledTableWithConstraint)->Apply(PreInsertRanges);
+
+BENCHMARK_DEFINE_F(MicroBenchmarkBasicFixture, BM_InsertOnCompressedTable)(benchmark::State& state) {
+  auto& manager = StorageManager::get();
+
+  auto chunk_size = ChunkID(opossum::ChunkID{static_cast<uint32_t>(state.range(1))/100});
+
+  TableColumnDefinitions column_definitions;
+  column_definitions.emplace_back("column0", DataType::Int, true);
+  column_definitions.emplace_back("column1", DataType::Int, false);
+
+  // Create table on which the insert operators will work on
+  manager.reset();
+  manager.add_table("table", std::make_shared<Table>(column_definitions, TableType::Data, chunk_size, UseMvcc::Yes));
+  auto table = manager.get_table("table");
+
+  // Insert rows to table, if num_rows != 0
+  int row_preinserted = 0;
+  auto pre_insert_table_temp = std::make_shared<Table>(column_definitions, TableType::Data, chunk_size, UseMvcc::Yes);
+  manager.add_table("pre_insert_table_temp", pre_insert_table_temp);
+
+  for (; row_preinserted < state.range(1); row_preinserted++) {
+    pre_insert_table_temp->append({row_preinserted, row_preinserted * 2});
+  }
+
+  auto pre_insert_gt = std::make_shared<GetTable>("pre_insert_table_temp");
+  pre_insert_gt->execute();
+  auto pre_insert = std::make_shared<Insert>("table", pre_insert_gt);
+  auto pre_insert_context = TransactionManager::get().new_transaction_context();
+  pre_insert->set_transaction_context(pre_insert_context);
+  pre_insert->execute();
+  pre_insert_context->commit();
+  
+  if(state.range(0)) {
+    table->add_unique_constraint({ColumnID{0}});
+    table->add_unique_constraint({ColumnID{1}});
+  }
+
+  ChunkEncoder::encode_all_chunks(table, SegmentEncodingSpec{EncodingType::Dictionary});
+
+  // Create insert operators depending on values to insert operator
+
+  while (state.KeepRunning()) {
+    state.PauseTiming();
+
+    auto table_temp = std::make_shared<Table>(column_definitions, TableType::Data, chunk_size, UseMvcc::Yes);
+    int const row_count = table->row_count();
+    manager.add_table("table_temp" + std::to_string(row_count), table_temp);
+
+    table_temp->append({row_count, row_count * 2});
+    auto gt = std::make_shared<GetTable>("table_temp" + std::to_string(row_count));
+    gt->execute();
+
+
+    auto table_insert = std::make_shared<Insert>("table", gt);
+    auto table_context = TransactionManager::get().new_transaction_context();
+    table_insert->set_transaction_context(table_context);
+
+    state.ResumeTiming();
+
+    table_insert->execute();
+    table_context->commit();
+  }
+}
+
+BENCHMARK_REGISTER_F(MicroBenchmarkBasicFixture, BM_InsertOnCompressedTable)->Apply(PreInsertRanges)->Iterations(100);
 
 }  // namespace opossum
