46a74f34b00425a972a3858cfb043403d4d18ef1
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2019-01-30 12:29:28 +0100

    Refactor lz4 segment

--- src/lib/storage/lz4_segment.cpp
+++ src/lib/storage/lz4_segment.cpp
@@ -9,12 +9,39 @@
 namespace opossum {
 
 template <typename T>
-LZ4Segment<T>::LZ4Segment(const int decompressed_size, const int max_compressed_size,
-                             std::shared_ptr<std::vector<char>> compressed_data)
+LZ4Segment<T>::LZ4Segment(const std::shared_ptr<const pmr_vector<char>>& compressed_data,
+                          const std::shared_ptr<const pmr_vector<bool>>& null_values,
+                          const std::shared_ptr<const pmr_vector<size_t>>& offsets,
+                          const int compressed_size,
+                          const int decompressed_size,
+                          const size_t num_elements)
     : BaseEncodedSegment{data_type_from_type<T>()},
+      _compressed_data{compressed_data},
+      _null_values{null_values},
+      _offsets{offsets},
+      _compressed_size{compressed_size},
       _decompressed_size{decompressed_size},
-      _max_compressed_size{max_compressed_size},
-      _compressed_data{std::move(compressed_data)} {}
+      _num_elements{num_elements} {}
+
+template <typename T>
+std::shared_ptr<const pmr_vector<char>> LZ4Segment<T>::compressed_data() const {
+  return _compressed_data;
+}
+
+template <typename T>
+std::shared_ptr<const pmr_vector<bool>> LZ4Segment<T>::null_values() const {
+  return _null_values;
+}
+
+template <typename T>
+std::shared_ptr<const pmr_vector<size_t>> LZ4Segment<T>::offsets() const {
+  return _offsets;
+}
+
+template <typename T>
+int LZ4Segment<T>::compressed_size() const {
+  return _compressed_size;
+}
 
 template <typename T>
 int LZ4Segment<T>::decompressed_size() const {
@@ -22,85 +49,106 @@ int LZ4Segment<T>::decompressed_size() const {
 }
 
 template <typename T>
-int LZ4Segment<T>::max_compressed_size() const {
-  return _max_compressed_size;
-}
-
-template <typename T>
-const std::vector<char>& LZ4Segment<T>::compressed_data() const {
-  return *_compressed_data;
-}
-
-template <typename T>
 const AllTypeVariant LZ4Segment<T>::operator[](const ChunkOffset chunk_offset) const {
   PerformanceWarning("operator[] used");
-  PerformanceWarning("LZ4::operator[]: decompressing the whole LZ4 segment");
   DebugAssert(chunk_offset < size(), "Passed chunk offset must be valid.");
 
-  auto decompressed_segment = decompress();
-  return AllTypeVariant{(*decompressed_segment)[chunk_offset]};
+  const auto typed_value = get_typed_value(chunk_offset);
+  if (!typed_value.has_value()) {
+    return NULL_VALUE;
+  }
+  return *typed_value;
 }
 
 template <typename T>
 const std::optional<T> LZ4Segment<T>::get_typed_value(const ChunkOffset chunk_offset) const {
   PerformanceWarning("LZ4::get_typed_value: decompressing the whole LZ4 segment");
   auto decompressed_segment = decompress();
+
+  const auto is_null = (*_null_values)[chunk_offset];
+  if (is_null) {
+    return std::nullopt;
+  }
+
   return (*decompressed_segment)[chunk_offset];
 }
 
 template <typename T>
 size_t LZ4Segment<T>::size() const {
-  return _compressed_data->size();
+  return _num_elements;
 }
 
 template <typename T>
 std::shared_ptr<std::vector<T>> LZ4Segment<T>::decompress() const {
+//  auto decompressed_data = pmr_vector<T>();
+//  decompressed_data.reserve(_decompressed_size / sizeof(T));
   auto decompressed_data = std::make_shared<std::vector<T>>(_decompressed_size / sizeof(T));
-  int compressed_size = static_cast<int>(_compressed_data->size());
   const int decompressed_result = LZ4_decompress_safe(_compressed_data->data(),
                                                       reinterpret_cast<char*>(decompressed_data->data()),
-                                                      compressed_size, _decompressed_size);
+                                                      _compressed_size, _decompressed_size);
   if (decompressed_result <= 0) {
     throw std::runtime_error("LZ4 decompression failed");
   }
 
+//  return std::allocate_shared<pmr_vector<T>>(std::move(decompressed_data));
   return decompressed_data;
 }
 
 template <>
 std::shared_ptr<std::vector<std::string>> LZ4Segment<std::string>::decompress() const {
+//  auto decompressed_data = pmr_vector<char>();
+//  decompressed_data.reserve(_decompressed_size);
   auto decompressed_data = std::make_shared<std::vector<char>>(_decompressed_size);
-  int compressed_size = static_cast<int>(_compressed_data->size());
-  const int decompressed_result = LZ4_decompress_safe(_compressed_data->data(),
-                                                      decompressed_data->data(),
-                                                      compressed_size, _decompressed_size);
+  const int decompressed_result = LZ4_decompress_safe(_compressed_data->data(), decompressed_data->data(),
+                                                      _compressed_size, _decompressed_size);
   if (decompressed_result <= 0) {
     throw std::runtime_error("LZ4 decompression failed");
   }
 
+//  auto string_data = pmr_vector<std::string>();
   auto string_data = std::make_shared<std::vector<std::string>>();
-  std::string current_element = "";
-  for (const auto& element : (*decompressed_data)) {
-    if (element == '\0') {
-      string_data->emplace_back(current_element);
-      current_element = "";
-      continue;
+  for (auto it = _offsets->cbegin(); it != _offsets->cend(); ++it) {
+    auto begin = *it;
+    size_t end;
+    if (it + 1 == _offsets->cend()) {
+      end = _decompressed_size;
+    } else {
+      end = *(it + 1);
     }
-    current_element += element;
+    std::string current_element;
+    const auto data_begin = decompressed_data->cbegin() + begin;
+    const auto data_end = data_begin + end;
+    for (auto data_it = data_begin; data_it != data_end; ++data_it) {
+      current_element += (*data_it);
+    }
+    string_data->emplace_back(current_element);
   }
 
+//  return std::allocate_shared<pmr_vector<std::string>>(std::move(string_data));
   return string_data;
 }
 
 template <typename T>
 std::shared_ptr<BaseSegment> LZ4Segment<T>::copy_using_allocator(
     const PolymorphicAllocator<size_t>& alloc) const {
-  return std::allocate_shared<LZ4Segment>(alloc, _decompressed_size, _max_compressed_size, std::move(_compressed_data));
+  auto new_compressed_data = pmr_vector<char>{*_compressed_data, alloc};
+  auto new_null_values = pmr_vector<bool>{*_null_values, alloc};
+
+  std::shared_ptr<pmr_vector<size_t>> new_offsets_ptr = nullptr;
+  if (_offsets != nullptr) {
+    auto new_offsets = pmr_vector<size_t>{*_offsets, alloc};
+    new_offsets_ptr = std::allocate_shared<pmr_vector<size_t>>(alloc, std::move(new_offsets));
+  }
+  auto new_compressed_data_ptr = std::allocate_shared<pmr_vector<char>>(alloc, std::move(new_compressed_data));
+  auto new_null_values_ptr = std::allocate_shared<pmr_vector<bool>>(alloc, std::move(new_null_values));
+
+  return std::allocate_shared<LZ4Segment>(alloc, new_compressed_data_ptr, new_null_values_ptr, new_offsets_ptr,
+                                          _decompressed_size, _compressed_size, _num_elements);
 }
 
 template <typename T>
 size_t LZ4Segment<T>::estimate_memory_usage() const {
-  return static_cast<size_t>(_max_compressed_size);
+  return _compressed_size;
 }
 
 template <typename T>
--- src/lib/storage/lz4_segment.hpp
+++ src/lib/storage/lz4_segment.hpp
@@ -21,12 +21,18 @@ template <typename T>
 class LZ4Segment : public BaseEncodedSegment {
  public:
 
-  explicit LZ4Segment(const int decompressed_size, const int max_compressed_size,
-                      std::shared_ptr<std::vector<char>> compressed_data);
+  explicit LZ4Segment(const std::shared_ptr<const pmr_vector<char>>& compressed_data,
+                      const std::shared_ptr<const pmr_vector<bool>>& null_values,
+                      const std::shared_ptr<const pmr_vector<size_t>>& offsets,
+                      const int compressed_size,
+                      const int decompressed_size,
+                      const size_t num_elements);
 
+  std::shared_ptr<const pmr_vector<char>> compressed_data() const;
+  std::shared_ptr<const pmr_vector<bool>> null_values() const;
+  std::shared_ptr<const pmr_vector<size_t>> offsets() const;
+  int compressed_size() const;
   int decompressed_size() const;
-  int max_compressed_size() const;
-  const std::vector<char>& compressed_data() const;
 
   /**
    * @defgroup BaseSegment interface
@@ -58,9 +64,12 @@ class LZ4Segment : public BaseEncodedSegment {
   /**@}*/
 
  private:
+  const std::shared_ptr<const pmr_vector<char>> _compressed_data;
+  const std::shared_ptr<const pmr_vector<bool>> _null_values;
+  const std::shared_ptr<const pmr_vector<size_t>> _offsets;
+  const int _compressed_size;
   const int _decompressed_size;
-  const int _max_compressed_size;
-  const std::shared_ptr<std::vector<char>> _compressed_data;
+  size_t _num_elements;
 };
 
 }  // namespace opossum
