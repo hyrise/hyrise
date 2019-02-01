f03b6e603ae7aae7d8b7259d97ef6748bbbbb0c8
Moritz Eyssen <Moritz.Eyssen@student.hpi.de>
2019-02-01 09:55:08 +0100

    Change

--- src/bin/playground.cpp
+++ src/bin/playground.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 
 #include "types.hpp"
+#include "version.hpp"
 
 using namespace opossum;  // NOLINT
 
 int main() {
-  std::cout << "Hello world!!" << std::endl;
+  std::cout << "Hello worlssd!! " << GIT_HEAD_SHA1 << " - " << GIT_IS_DIRTY << std::endl;
   return 0;
 }
