c79ff7d824d9980c1d65c3a4b74a8bfe3dadf57f
Moritz Eyssen <Moritz.Eyssen@student.hpi.de>
2019-02-01 10:37:08 +0100

    lss remants

--- src/bin/playground.cpp
+++ src/bin/playground.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 
 #include "types.hpp"
-#include "version.hpp"
 
 using namespace opossum;  // NOLINT
 
 int main() {
-  std::cout << "Hello worlssd!! " << GIT_HEAD_SHA1 << " - " << GIT_IS_DIRTY << std::endl;
+  std::cout << "Hello world!!" << std::endl;
   return 0;
 }
