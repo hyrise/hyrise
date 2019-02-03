9c706955f64fedd1032fa3d29d427938987728b4
Fabian Engel <engelfab@gmail.com>
2019-02-03 15:21:08 +0100

    Close #16: Describe input and output of script.

--- scripts/visualize_benchmark_mvcc_performance.py
+++ scripts/visualize_benchmark_mvcc_performance.py
@@ -2,27 +2,32 @@ import pandas
 import re
 import sys
 
-def main():
-    points = []
-    with open(sys.argv[1]) as f:
-        for line in f:
-            if line.startswith('"'):        
-                line = line.split(',')
-                time = re.search(r'[0-9]+', line[0]).group(0)
-                value = line[2]
-                points.append((time, value))
+'''
+    Input: GoogleTest output saved as CSV.
+      name,iterations,real_time,cpu_time,time_unit,bytes_per_second,items_per_second,label,error_occurred,error_message
+      "MVCC_Benchmark_Fixture/BM_MVCC_VALIDATE/1",100,13476.1,13470,ns,,,,,
+    Output: Graph of time in nanosecdonds per number of iterations for validate and update operations.
+'''
 
-    dfs = [points[:len(points)//2], points[len(points)//2:]]
-    print(dfs)
-    df1 = pandas.DataFrame(dfs[0], columns=['Iterations', 'Validate'])
-    df2 = pandas.DataFrame(dfs[1], columns=['Iterations', 'Update'])
-    df1 = df1.apply(pandas.to_numeric)
-    df2 = df2.apply(pandas.to_numeric)
-    #import pdb;pdb.set_trace()
-    ax = df1.plot(x=0, y=1)
-    plot = df2.plot(x=0, y=1, ax=ax)
-    plot.set_ylabel("Nanoseconds")
-    plot.get_figure().savefig('mvcc_benchmark', bbox_inches='tight')
+def main():
+  points = []
+  with open(sys.argv[1]) as f:
+    for line in f:
+      if line.startswith('"'):        
+        line = line.split(',')
+        iteration = re.search(r'[0-9]+', line[0]).group(0)
+        time = line[2]
+        points.append((iteration, time))
+
+  dfs = [points[:len(points)//2], points[len(points)//2:]]
+
+  df1 = pandas.DataFrame(dfs[0], columns=['Iterations', 'Validate']).apply(pandas.to_numeric)
+  df2 = pandas.DataFrame(dfs[1], columns=['Iterations', 'Update']).apply(pandas.to_numeric)
+
+  graph1 = df1.plot(x=0, y=1)
+  graph2 = df2.plot(x=0, y=1, ax=graph1)
+  graph2.set_ylabel("Nanoseconds")
+  graph2.get_figure().savefig('mvcc_benchmark', bbox_inches='tight')
 
 if __name__ == '__main__':
-    main()
+  main()
