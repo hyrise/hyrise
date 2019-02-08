cc0f5ff03480d04170353a152e8b19281132dfe1
Moritz Eyssen <mrzesn@t-online.de>
2019-02-08 19:31:41 +0100

    Update delete_node.cpp

--- src/lib/logical_query_plan/delete_node.cpp
+++ src/lib/logical_query_plan/delete_node.cpp
@@ -22,8 +22,8 @@ std::string DeleteNode::description() const {
 bool DeleteNode::is_column_nullable(const ColumnID column_id) const { Fail("Delete does not output any columns"); }
 
 const std::vector<std::shared_ptr<AbstractExpression>>& DeleteNode::column_expressions() const {
-  static std::vector<std::shared_ptr<AbstractExpression>> dummy_column_expressions;
-  return dummy_column_expressions;
+  static std::vector<std::shared_ptr<AbstractExpression>> empty_vector;
+  return empty_vector;
 }
 
 std::shared_ptr<AbstractLQPNode> DeleteNode::_on_shallow_copy(LQPNodeMapping& node_mapping) const {
