ef09640661b90b93079a75fc4abd324eeaa2978d
Moritz Eyssen <mrzesn@t-online.de>
2019-02-13 09:27:26 +0100

    Update sqlite_testrunner_encodings.cpp

--- src/test/sql/sqlite_testrunner/sqlite_testrunner_encodings.cpp
+++ src/test/sql/sqlite_testrunner/sqlite_testrunner_encodings.cpp
@@ -4,6 +4,7 @@
 
 namespace opossum {
 
+  
 INSTANTIATE_TEST_CASE_P(SQLiteTestRunnerEncodings, SQLiteTestRunner,
                         testing::Combine(testing::ValuesIn(SQLiteTestRunner::queries()), testing::ValuesIn({false}),
                                          testing::ValuesIn({EncodingType::Dictionary, EncodingType::RunLength,
