c107c1aea739adaea070c20118d5f8b0dea0ad3d
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2019-02-26 13:37:09 +0100

    remove null values if else

--- src/lib/storage/lz4_segment.cpp
+++ src/lib/storage/lz4_segment.cpp
@@ -133,7 +133,7 @@ std::shared_ptr<BaseSegment> LZ4Segment<T>::copy_using_allocator(const Polymorph
 
 template <typename T>
 size_t LZ4Segment<T>::estimate_memory_usage() const {
-  auto bool_size = (_null_values ? _null_values->size() * sizeof(bool) : 0u);
+  auto bool_size = _null_values->size() * sizeof(bool);
   auto offset_size = (_offsets ? _offsets->size() * sizeof(size_t) : 0u);
   return sizeof(*this) + static_cast<size_t>(_compressed_size) + bool_size + offset_size;
 }
