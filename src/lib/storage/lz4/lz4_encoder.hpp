92bdf027c7f410581982f43e27b8772626b47142
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2019-03-02 18:00:12 +0100

    debug

--- src/lib/storage/lz4/lz4_encoder.hpp
+++ src/lib/storage/lz4/lz4_encoder.hpp
@@ -370,19 +370,19 @@ class LZ4Encoder : public SegmentEncoder<LZ4Encoder> {
       dictionary = pmr_vector<char>{values.get_allocator()};
       dictionary.resize(max_dictionary_size);
 
-      size_t sample_offset = 0u;
-      size_t num_samples = 10u;
-      for (size_t i = 0u; i < samples_copy.size() - num_samples; ++i) {
-        sample_offset += samples_copy[i];
-      }
-      for (size_t i = num_samples; i > 0; --i) {
-        auto sample_index = samples_copy.size() - i;
-        auto sample_length = samples_copy[sample_index];
-        auto start = values_copy.begin() + sample_offset;
-        auto end = start + sample_length;
-        auto sample = pmr_string{start, end};
-        std::cout << "Sample at index " << i << " has length " << sample_length << " and is: " << sample << std::endl;
-      }
+//      size_t sample_offset = 0u;
+//      size_t num_samples = 10u;
+//      for (size_t i = 0u; i < samples_copy.size() - num_samples; ++i) {
+//        sample_offset += samples_copy[i];
+//      }
+//      for (size_t i = num_samples; i > 0; --i) {
+//        auto sample_index = samples_copy.size() - i;
+//        auto sample_length = samples_copy[sample_index];
+//        auto start = values_copy.begin() + sample_offset;
+//        auto end = start + sample_length;
+//        auto sample = pmr_string{start, end};
+//        std::cout << "Sample at index " << i << " has length " << sample_length << " and is: " << sample << std::endl;
+//      }
 
       std::cout << "Trying dictionary with " << values_copy.size() << " values" << std::endl;
       dictionary_size = ZDICT_trainFromBuffer(dictionary.data(), max_dictionary_size, values_copy.data(),
@@ -392,7 +392,7 @@ class LZ4Encoder : public SegmentEncoder<LZ4Encoder> {
       values_copy.insert(values_copy.end(), values.begin(), values.end());
 
 //      samples_copy.insert(samples_copy.end(), sample_sizes.begin(), sample_sizes.end());
-      samples_copy.emplace_back(values.size());
+//      samples_copy.emplace_back(values.size());
 
 //      for (size_t index = 0u; index < sample_sizes.size(); index += sample_length_increase) {
 //        auto size = 0u;
@@ -401,6 +401,14 @@ class LZ4Encoder : public SegmentEncoder<LZ4Encoder> {
 //        }
 //        samples_copy.emplace_back(size);
 //      }
+
+      for (size_t i = 0u; i < values.size(); i += 10u) {
+        if (i + 10u < values.size()) {
+          samples_copy.emplace_back(10u);
+        } else {
+          samples_copy.emplace_back(values.size() - i);
+        }
+      }
 
       max_dictionary_size = max_dictionary_size < 5000000u ? max_dictionary_size * 2 : max_dictionary_size;
       ++sample_length_increase;
