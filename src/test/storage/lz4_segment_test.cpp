8e16d31143675035480cec2214017f4e9304014f
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2019-03-02 16:21:58 +0100

    debug

--- src/test/storage/lz4_segment_test.cpp
+++ src/test/storage/lz4_segment_test.cpp
@@ -150,9 +150,9 @@ TEST_F(StorageLZ4SegmentTest, CompressSingleCharSegmentString) {
 TEST_F(StorageLZ4SegmentTest, CompressZeroOneSegmentString) {
   for (size_t i = 0; i < row_count; ++i) {
     if (i % 2) {
-      vs_str->append("aasjhdahcdhsvk ahlcsdacnc djaskkaskjhd ash dlkjshjhdj");
+      vs_str->append("aasjhdah");
     } else {
-      vs_str->append("baslkdjaskldjkwlajdkjdsakljdaklsjdaskljdaslkjdalskjdalkjdlasjdaljdslk");
+      vs_str->append("hrkujeku");
     }
   }
 
