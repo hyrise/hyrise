fb5c5b9a7d8fe4da3c16b17fde40d3915b866291
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2019-03-02 18:02:08 +0100

    debug

--- src/test/storage/lz4_segment_test.cpp
+++ src/test/storage/lz4_segment_test.cpp
@@ -150,9 +150,9 @@ TEST_F(StorageLZ4SegmentTest, CompressSingleCharSegmentString) {
 TEST_F(StorageLZ4SegmentTest, CompressZeroOneSegmentString) {
   for (size_t i = 0; i < row_count; ++i) {
     if (i % 2) {
-      vs_str->append("0101");
+      vs_str->append("010101");
     } else {
-      vs_str->append("1010");
+      vs_str->append("10100101");
     }
   }
 
