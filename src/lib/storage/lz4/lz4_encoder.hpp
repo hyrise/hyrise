cdcf9389d6dec8cdf519d7e0cb2be94de6ecace7
Jan Ehmueller <jan.ehmueller@student.hpi.de>
2019-03-02 16:43:25 +0100

    debug

--- src/lib/storage/lz4/lz4_encoder.hpp
+++ src/lib/storage/lz4/lz4_encoder.hpp
@@ -340,8 +340,6 @@ class LZ4Encoder : public SegmentEncoder<LZ4Encoder> {
     // But the size also has to be at least 1KB (smaller dictionaries won't work).
     max_dictionary_size = max_dictionary_size < 1000u ? 1000u : max_dictionary_size;
 
-    std::cout << "Dictionary max size: " << max_dictionary_size << std::endl;
-
     pmr_vector<char> dictionary;
 //    auto dictionary = pmr_vector<char>{values.get_allocator()};
 //    dictionary.resize(max_dictionary_size);
@@ -350,6 +348,7 @@ class LZ4Encoder : public SegmentEncoder<LZ4Encoder> {
     auto values_copy = pmr_vector<char>{};
     auto samples_copy = pmr_vector<size_t>{};
     do {
+      std::cout << "Dictionary max size: " << max_dictionary_size << std::endl;
       max_dictionary_size = max_dictionary_size < 1000u ? 1000u : max_dictionary_size / 2;
 
       dictionary = pmr_vector<char>{values.get_allocator()};
