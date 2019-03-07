53d655716b3d8c4634aec1d0de1ec3b1f3ee239e
Moritz Eyssen <Moritz.Eyssen@student.hpi.de>
2019-03-07 22:33:21 +0100

    fullci

--- src/lib/storage/segment_iterables/base_segment_iterators.hpp
+++ src/lib/storage/segment_iterables/base_segment_iterators.hpp
@@ -9,6 +9,14 @@
 namespace opossum {
 
 /**
+ * @brief template-free base class of all iterators used by iterables
+ *
+ * The class allows the JitOperatorWrapper to keep pointers to differently specialized versions
+ * of the iterators in a common data structure.
+ */
+class JitBaseSegmentIterator {};
+
+/**
  * @brief base class of all iterators used by iterables
  *
  * Instantiations of this template are part of the segment iterable
@@ -38,7 +46,8 @@ namespace opossum {
  * };
  */
 template <typename Derived, typename Value>
-class BaseSegmentIterator : public boost::iterator_facade<Derived, Value, boost::random_access_traversal_tag, Value> {};
+class BaseSegmentIterator : public boost::iterator_facade<Derived, Value, boost::random_access_traversal_tag, Value>,
+                            public JitBaseSegmentIterator {};
 
 /**
  * Mapping between chunk offset into a reference segment and
