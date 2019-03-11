b93d20b3b01a54fe216e21e8d266f656fe1b7c52
Jonathan Janetzki <jonathan.janetzki@student.hpi.de>
2019-03-11 12:03:38 +0100

    :white_check_mark: #6 Add test NoRewriteUncorrelatedExists

--- src/test/optimizer/strategy/subquery_to_join_rule_test.cpp
+++ src/test/optimizer/strategy/subquery_to_join_rule_test.cpp
@@ -237,11 +237,23 @@ TEST_F(SubqueryToJoinRuleTest, NoRewriteOfDoubleCorrelatedIn) {
 TEST_F(SubqueryToJoinRuleTest, NoRewriteConstantIn) {
   // SELECT * FROM a WHERE IN (1, 2, 3)
   // clang-format off
-  const auto subquery_lqp =
-  ProjectionNode::make(expression_vector(b_a), node_b);
-
   const auto input_lqp =
   PredicateNode::make(in_(a_a, list_(1, 2, 3)), node_a);
+  const auto expected_lqp = input_lqp->deep_copy();
+  // clang-format on
+  const auto actual_lqp = StrategyBaseTest::apply_rule(_rule, input_lqp);
+
+  EXPECT_LQP_EQ(actual_lqp, expected_lqp);
+}
+
+TEST_F(SubqueryToJoinRuleTest, NoRewriteUncorrelatedExists) {
+  // SELECT * FROM a WHERE EXISTS (SELECT * FROM b)
+  // clang-format off
+  const auto subquery =
+  lqp_subquery_(node_b);
+
+  const auto input_lqp =
+  PredicateNode::make(exists_(subquery), node_a);
   const auto expected_lqp = input_lqp->deep_copy();
   // clang-format on
   const auto actual_lqp = StrategyBaseTest::apply_rule(_rule, input_lqp);
