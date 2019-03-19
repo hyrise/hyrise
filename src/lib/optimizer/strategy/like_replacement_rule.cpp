11d4c8a5f755b5bf4280d6492c65f5c203431f06
Simon Siegert <sisiegert@uni-potsdam.de>
2019-03-19 17:41:01 +0000

    Add note on replacing predicate expressions with between

--- src/lib/optimizer/strategy/like_replacement_rule.cpp
+++ src/lib/optimizer/strategy/like_replacement_rule.cpp
@@ -63,6 +63,7 @@ void LikeReplacementRule::apply_to(const std::shared_ptr<AbstractLQPNode>& node)
   const auto current_character_value = static_cast<int>(lower_bound.at(lower_bound.length() - 1));
   // Find next value according to ASCII-table
   if (current_character_value < MAX_ASCII_VALUE) {
+    // TODO(anyone): Replace with right inclusive between
     const auto next_character = static_cast<char>(current_character_value + 1);
     const auto upper_bound = lower_bound.substr(0, offset - 1) + next_character;
     const auto lower_bound_node = PredicateNode::make(std::make_shared<BinaryPredicateExpression>(
