a603408a573681f801a8a9aa1ba949175553cc40
Markus Dreseler <markus.dreseler@hpi.de>
2019-03-27 16:05:56 +0100

    boah

--- src/benchmarklib/tpcc/procedures/tpcc_order_status.cpp
+++ src/benchmarklib/tpcc/procedures/tpcc_order_status.cpp
@@ -21,8 +21,8 @@ TpccOrderStatus::TpccOrderStatus(const int num_warehouses) {
   if (_select_customer_by_name) {
     _customer = pmr_string{_tpcc_random_generator.last_name(_tpcc_random_generator.nurand(255, 0, 999))};
   } else {
-    // TODO this should be [1, 3000], but our data generator does [0, 2999]
-    _customer = _tpcc_random_generator.nurand(1023, 0, 2999);
+    // TODO this should be [1, 3000], but our data generator does [1, 2999]
+    _customer = _tpcc_random_generator.nurand(1023, 1, 2999);
   }
 }
 
