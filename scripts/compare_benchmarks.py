6b7b820770bb2a6fa86777980aa179c91cfa5663
martin.boissier <martin.boissier@hpi.de>
2019-04-09 11:47:14 +0800

    Wrong change, no correct change for sign var assignment

--- scripts/compare_benchmarks.py
+++ scripts/compare_benchmarks.py
@@ -90,9 +90,7 @@ for old, new in zip(old_data['benchmarks'], new_data['benchmarks']):
         diff = items_per_second_new / items_per_second_old - 1
         average_diff_sum += diff
         if items_per_second_new > 0.0:
-            sign = 1
-            if (items_per_second_old > items_per_second_new):
-                sign = -1
+            sign = -1 if items_per_second_old > items_per_second_new else 1
             change = sign * (max(items_per_second_old, items_per_second_new) / min(items_per_second_old, items_per_second_new))
             changes.append(change)
         else:
