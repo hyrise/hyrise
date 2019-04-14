8bd5e175261502c416993665955a5b6635b5b74b
David Justen <mail@davidjusten.de>
2019-04-14 14:18:39 +0200

    Add comment and return nullptr reference

--- src/lib/sql/sql_pipeline_statement.cpp
+++ src/lib/sql/sql_pipeline_statement.cpp
@@ -199,11 +199,13 @@ const std::shared_ptr<const Table>& SQLPipelineStatement::get_result_table() {
 
     if (!success) {
       _query_has_output = false;
-      return _result_table = nullptr;
+      _result_table = nullptr;
+      return _result_table; // return nullptr, but as a reference
     }
   } else if (_transaction_context && _transaction_context->aborted()) {
     _query_has_output = false;
-    return _result_table = nullptr;
+    _result_table = nullptr;
+    return _result_table; // return nullptr, but as a reference
   }
 
   const auto done = std::chrono::high_resolution_clock::now();
