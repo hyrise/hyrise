1bbca9c99b9f79a9262614638046559f4051eb74
Markus Dreseler <markus.dreseler@hpi.de>
2019-04-15 18:24:25 +0200

    Generate column statistics: use string_view

--- src/lib/statistics/generate_column_statistics.cpp
+++ src/lib/statistics/generate_column_statistics.cpp
@@ -13,15 +13,14 @@ namespace opossum {
 template <>
 std::shared_ptr<BaseColumnStatistics> generate_column_statistics<pmr_string>(const Table& table,
                                                                              const ColumnID column_id) {
-  // It would be nice to store string_views in the set, but the iterables hold copies of the values, not references.
-  // SegmentPosition would have to be changed to `T& _value` and this brings a whole bunch of problems in iterators
-  // that create stack copies of the accessed values (e.g., for ReferenceSegments)
-
   auto temp_buffer = boost::container::pmr::monotonic_buffer_resource(table.row_count() * 10);
   auto distinct_set =
-      std::unordered_set<pmr_string, std::hash<pmr_string>, std::equal_to<>, PolymorphicAllocator<pmr_string>>(
-          PolymorphicAllocator<pmr_string>{/*&temp_buffer*/});
+      std::unordered_set<std::string_view, std::hash<std::string_view>, std::equal_to<>, PolymorphicAllocator<std::string_view>>(
+          PolymorphicAllocator<std::string_view>{&temp_buffer});
   distinct_set.reserve(table.row_count());
+
+  // For segments where the iterators do not return stable references, we temporarily copy the strings into temp_strings
+  auto temp_strings = std::list<pmr_string, PolymorphicAllocator<pmr_string>>{&temp_buffer};
 
   auto null_value_count = size_t{0};
 
@@ -33,26 +32,35 @@ std::shared_ptr<BaseColumnStatistics> generate_column_statistics<pmr_string>(con
   for (ChunkID chunk_id{0}; chunk_id < table.chunk_count(); ++chunk_id) {
     const auto base_segment = table.get_chunk(chunk_id)->get_segment(column_id);
 
-    segment_iterate<pmr_string>(*base_segment, [&](const auto& position) {
-      if (position.is_null()) {
-        ++null_value_count;
-      } else {
-        // One would expect distinct_set.emplace() to be the same as the code below. However, "The element may be
-        // constructed even if there already is an element with the key in the container, in which case the newly
-        // constructed element will be destroyed immediately."
-        // This is the case here, where simply using emplace takes ~50% longer.
-        auto it = distinct_set.find(position.value());
-        if (it != distinct_set.end()) return;
-
-        it = distinct_set.emplace_hint(it, std::move(position.value()));
-
-        if (distinct_set.size() == 1) {
-          min = *it;
-          max = *it;
+    segment_with_iterators<pmr_string>(*base_segment, [&](auto segment_it, const auto segment_end) {
+      while (segment_it != segment_end) {
+        if (segment_it->is_null()) {
+          ++null_value_count;
         } else {
-          if (*it < min) min = *it;
-          if (*it > max) max = *it;
+          // One would expect distinct_set.emplace() to be the same as the code below. However, "The element may be
+          // constructed even if there already is an element with the key in the container, in which case the newly
+          // constructed element will be destroyed immediately."
+          // This is the case here, where simply using emplace takes ~50% longer.
+          auto it = distinct_set.find(segment_it->value());
+          if (it != distinct_set.end()) return;
+
+          if constexpr(segment_it.ReferenceIsStable) {
+            it = distinct_set.emplace_hint(it, segment_it->value());
+          } else {
+            temp_strings.emplace_back(segment_it->value());
+            it = distinct_set.emplace_hint(it, temp_strings.back());
+          }
+
+          if (distinct_set.size() == 1) {
+            min = *it;
+            max = *it;
+          } else {
+            if (*it < min) min = *it;
+            if (*it > max) max = *it;
+          }
         }
+
+        ++segment_it;
       }
     });
   }
