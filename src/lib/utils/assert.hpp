5159ee37eb7e5753af7c8fa0d37fd20a07c830d1
mrks <markus.dreseler@hpi.de>
2019-04-16 08:57:31 +0200

    namespace detail

--- src/lib/utils/assert.hpp
+++ src/lib/utils/assert.hpp
@@ -42,13 +42,15 @@
 
 namespace opossum {
 
+namespace detail {
 // We need this indirection so that we can throw exceptions from destructors without the compiler complaining. That is
 // generally forbidden and might lead to std::terminate, but since we don't want to handle most errors anyway,
 // that's fine.
 [[noreturn]] inline void fail(const std::string& msg) { throw std::logic_error(msg); }
+}  // namespace detail
 
-#define Fail(msg)                                                                                       \
-  opossum::fail(opossum::trim_source_file_path(__FILE__) + ":" BOOST_PP_STRINGIZE(__LINE__) " " + msg); \
+#define Fail(msg)                                                                                               \
+  opossum::detail::fail(opossum::trim_source_file_path(__FILE__) + ":" BOOST_PP_STRINGIZE(__LINE__) " " + msg); \
   static_assert(true, "End call of macro with a semicolon")
 
 [[noreturn]] inline void FailInput(const std::string& msg) {
