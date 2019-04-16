90e6285e40092583f1b6c26028f3ae9c229686cb
Moritz Eyssen <moritz.eyssen@student.hpi.de>
2019-04-16 15:08:16 +0200

    good

--- src/bin/tpch_table_generator.cpp
+++ src/bin/tpch_table_generator.cpp
@@ -3,10 +3,16 @@
 #include "storage/storage_manager.hpp"
 #include "tpch/tpch_table_generator.hpp"
 
-int main() {
+#include "utils/assert.hpp"
+
+int main(int argc, char **argv) {
+  Assert(argc == 2, "Usage: tpchTableGenerator <scale_factor>");
+
+  const auto scale_factor = std::stof(argv[1]);
+
   std::cout << "TPCH" << std::endl;
   std::cout << " > Generating tables" << std::endl;
-  opossum::TpchTableGenerator(1.0f, 1000).generate_and_store();
+  opossum::TpchTableGenerator(scale_factor, 1000).generate_and_store();
 
   std::cout << " > Dumping as CSV" << std::endl;
   opossum::StorageManager::get().export_all_tables_as_csv(".");
