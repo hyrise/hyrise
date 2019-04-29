71b64bee51bae13df37d9ea109ff1e5e0ccc4a91
Fabian Engel <engelfab@gmail.com>
2019-04-29 14:15:44 +0200

    Remove visualize test.

--- src/test/console/console_output_test.py
+++ src/test/console/console_output_test.py
@@ -15,40 +15,32 @@ def main():
 	delimiter = "hyrise/"
 	hyrise_path = console_path.split(delimiter)[0] + delimiter
 	
-	console = pexpect.spawn(console_path, timeout=10)
+	console = pexpect.spawn(console_path, timeout=5, dimensions=(1920,1080))
+	console.logfile = open('mylogfilename', 'wb')
 
-	# test print
+	# Test print command
 	console.sendline("print test")
 	console.expect("Exception thrown while loading table:")
 	
-	# test load
+	# Test load command
 	console.sendline("load " + hyrise_path + "resources/test_data/tbl/10_ints.tbl test")
 	console.expect('Loading .*tbl/10_ints.tbl into table "test"')
 	console.expect('Encoding "test" using Unencoded')
 
-	# test sql statement
+	# Test sql statement
 	console.sendline("select sum(a) from test")
 	console.expect("786")
 	console.expect("1 rows total")
 
-	# test visualize
-	console.sendline("visualize")
-	print(console.before)
-	print(console.after)
-	time.wait(2000)
-	# console.expect("iTerm2")
-	with open(hyrise_path + '.pqp.dot') as f: s = f.read()
-
-	if len(f) <= 5:
-		return 1
-
-	# test tpch generation
+	# Test TPCH generation
 	console.sendline("generate_tpch 0.001")
-	console.expect("Generating all TPCH tables \(this might take a while\) ...")
+	console.expect("Generating tables done")
 
-	# test tpch tables
+	# Test TPCH tables
 	console.sendline("select * from nation")
 	console.expect("25 rows total")
+
+	console.logfile.close()
 
 	return
 
