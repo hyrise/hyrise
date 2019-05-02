45a0dc1713f7d030ed542f0da99ffede02ae00ee
Fabian Engel <engelfab@gmail.com>
2019-05-02 14:57:52 +0200

    Add exit status handling.

--- scripts/test/hyriseConsole_test.py
+++ scripts/test/hyriseConsole_test.py
@@ -5,9 +5,16 @@ import sys
 import pexpect
 import time
 
+def check_exit_status(console):
+	print(console.exitstatus, console.signalstatus)
+	if console.exitstatus:
+		sys.exit(console.exitstatus)
+	else:
+		sys.exit(console.signalstatus)
+
 def main():
 	if len(sys.argv) == 1:
-		print ("Usage: ./scripts/test/console_output_test.py <build_dir>")
+		print ("Usage: ./scripts/test/hyriseConsole_test.py <build_dir>")
 		sys.exit(1)
 
 	if not os.path.isdir("resources/test_data/tbl"):
@@ -32,19 +39,34 @@ def main():
 	console.expect("786")
 	console.expect("Execution info:")
 
+	console.sendline("exit")
+	console.expect("Bye.")
+	time.sleep(1)
+	console.close()
+
+	check_exit_status(console)
+	#print(os.WIFEXITED(console.status), os.WEXITSTATUS(console.status),os.WIFSIGNALED(console.status), os.WTERMSIG(console.status), os.WIFSTOPPED(console.status), os.WSTOPSIG(console.status))	
+
+	console = pexpect.spawn(console_path + "/hyriseConsole", timeout=15, dimensions=(200, 64))
+	console.logfile = open('logfile', 'wb')
+
 	# Test visualize command
 	console.sendline("visualize")
-	
+
 	while not os.path.exists('.pqp.dot'):
+		print("test")
 		time.sleep(1)
 
 	with open('.pqp.dot') as f: s = f.read()
 
 	while len(s) < 6:
+		print("test2")
 		with open('.pqp.dot') as f: s = f.read()
 
+	console.logfile.close()
+
 	# Start new console because is_iterm2.sh called by visualize command freezes emulated terminal
-	console = pexpect.spawn(console_path + "/hyriseConsole", timeout=15, dimensions=(1920,1080))
+	console = pexpect.spawn(console_path + "/hyriseConsole", timeout=15, dimensions=(200, 64))
 
 	# Test TPCH generation
 	console.sendline("generate_tpch 0.001")
@@ -53,6 +75,8 @@ def main():
 	# Test TPCH tables
 	console.sendline("select * from nation")
 	console.expect("25 rows total")
+
+	check_exit_status(console)
 
 if __name__ == '__main__':
 	main()
