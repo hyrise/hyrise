bc7a587c727c900b31a6b78d1e33ec524e535acf
martin.boissier <martin.boissier@hpi.de>
2019-05-08 21:47:24 +0200

    Small comment change.

--- src/lib/storage/fixed_string_dictionary_segment/fixed_string_vector.cpp
+++ src/lib/storage/fixed_string_dictionary_segment/fixed_string_vector.cpp
@@ -60,7 +60,7 @@ FixedString FixedStringVector::at(const size_t pos) {
 const pmr_string FixedStringVector::get_string_at(const size_t pos) const {
   const auto string_start = &_chars[pos * _string_length];
   // String end checks if the string length is zero to avoid reading the data directly "in front" of `chars`.
-  // If the string length is >0, it is the position of the last char.
+  // If the string length is > 0, it is the position of the last char.
   const auto string_end = _string_length == 0 ? 0 : _string_length - 1;
 
   if (*(string_start + string_end) == '\0') {
