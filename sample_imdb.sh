ffdf276ecaeac981f70e3c3e441376745b7c8287
Fabian Engel <engelfab@gmail.com>
2019-05-15 17:45:58 +0200

    Add join order benchmark and sample imdb script.

---
+++ sample_imdb.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+rm -rf imdb_sample
+mkdir imdb_sample
+
+tail -n 1                             imdb_data/aka_name.csv          > imdb_sample/aka_name.csv
+tail -n 1                             imdb_data/aka_title.csv         > imdb_sample/aka_title.csv
+tail -n 1                             imdb_data/cast_info.csv         > imdb_sample/cast_info.csv
+tail -n 1                             imdb_data/char_name.csv         > imdb_sample/char_name.csv
+tail -n 1                             imdb_data/company_name.csv      > imdb_sample/company_name.csv
+tail -n 1                             imdb_data/company_type.csv      > imdb_sample/company_type.csv
+tail -n 1                             imdb_data/comp_cast_type.csv    > imdb_sample/comp_cast_type.csv
+tail -n 1                             imdb_data/complete_cast.csv     > imdb_sample/complete_cast.csv
+tail -n 1                             imdb_data/info_type.csv         > imdb_sample/info_type.csv
+grep -w 1136                          imdb_data/keyword.csv           > imdb_sample/keyword.csv
+tail -n 1                             imdb_data/kind_type.csv         > imdb_sample/kind_type.csv
+tail -n 1                             imdb_data/link_type.csv         > imdb_sample/link_type.csv
+tail -n 1                             imdb_data/movie_companies.csv   > imdb_sample/movie_companies.csv
+grep -r -m 1 3292787.*\"Bulgaria\"    imdb_data/movie_info.csv        > imdb_sample/movie_info.csv
+tail -n 1                             imdb_data/movie_info_idx.csv    > imdb_sample/movie_info_idx.csv
+grep -w 3292787,1136                  imdb_data/movie_keyword.csv     > imdb_sample/movie_keyword.csv
+tail -n 1                             imdb_data/movie_link.csv        > imdb_sample/movie_link.csv
+tail -n 1                             imdb_data/name.csv              > imdb_sample/name.csv
+tail -n 1                             imdb_data/person_info.csv       > imdb_sample/person_info.csv
+tail -n 1                             imdb_data/role_type.csv         > imdb_sample/role_type.csv
+grep -w 3292787                       imdb_data/title.csv             > imdb_sample/title.csv
+
+cp imdb_data/aka_name.csv.json          imdb_sample/aka_name.csv.json
+cp imdb_data/aka_title.csv.json         imdb_sample/aka_title.csv.json
+cp imdb_data/cast_info.csv.json         imdb_sample/cast_info.csv.json
+cp imdb_data/char_name.csv.json         imdb_sample/char_name.csv.json
+cp imdb_data/company_name.csv.json      imdb_sample/company_name.csv.json
+cp imdb_data/company_type.csv.json      imdb_sample/company_type.csv.json
+cp imdb_data/comp_cast_type.csv.json    imdb_sample/comp_cast_type.csv.json
+cp imdb_data/complete_cast.csv.json     imdb_sample/complete_cast.csv.json
+cp imdb_data/info_type.csv.json         imdb_sample/info_type.csv.json
+cp imdb_data/keyword.csv.json           imdb_sample/keyword.csv.json
+cp imdb_data/kind_type.csv.json         imdb_sample/kind_type.csv.json
+cp imdb_data/link_type.csv.json         imdb_sample/link_type.csv.json
+cp imdb_data/movie_companies.csv.json   imdb_sample/movie_companies.csv.json
+cp imdb_data/movie_info.csv.json        imdb_sample/movie_info.csv.json
+cp imdb_data/movie_info_idx.csv.json    imdb_sample/movie_info_idx.csv.json
+cp imdb_data/movie_keyword.csv.json     imdb_sample/movie_keyword.csv.json
+cp imdb_data/movie_link.csv.json        imdb_sample/movie_link.csv.json
+cp imdb_data/name.csv.json              imdb_sample/name.csv.json
+cp imdb_data/person_info.csv.json       imdb_sample/person_info.csv.json
+cp imdb_data/role_type.csv.json         imdb_sample/role_type.csv.json
+cp imdb_data/title.csv.json             imdb_sample/title.csv.json
