6be880c9addf5a1600a81eb9d9bd5c2fd4892bc8
pascal <pascal.fuehrlich@student.hpi.de>
2019-05-16 15:08:47 +0200

    table builder with nullable columns working :D

--- src/bin/playground.cpp
+++ src/bin/playground.cpp
@@ -29,24 +29,50 @@ class TableBuilder {
 public:
   // NOLINTNEXTLINE(fuchsia-trailing-return) - clang-tidy does not like the template parameter list
   template <class... ColumnDefinitions>
-  TableBuilder(size_t chunk_size, const hana::tuple<ColumnDefinitions...>& column_definitions, UseMvcc use_mvcc,
+  TableBuilder(size_t chunk_size, const hana::tuple<ColumnDefinitions...>& column_definition_tuples, UseMvcc use_mvcc,
       size_t estimated_rows = 0)
     : _use_mvcc(use_mvcc)
-    , _estimated_rows_per_chunk(estimated_rows < chunk_size ? estimated_rows : chunk_size){
+    , _estimated_rows_per_chunk(std::min(estimated_rows, chunk_size)){
 
     // Iterate over the column types/names and create the columns.
-    auto ccolumn_definitions = TableColumnDefinitions{};
-    hana::fold_left(column_definitions, ccolumn_definitions,
+    auto column_definitions = TableColumnDefinitions{};
+    hana::fold_left(column_definition_tuples, column_definitions,
                            [](auto& definitions, auto name_and_type_and_is_nullable) -> decltype(definitions) {
-                             definitions.emplace_back(name_and_type_and_is_nullable[hana::llong_c<0>],
-                                                      name_and_type_and_is_nullable[hana::llong_c<1>],
-                                                      name_and_type_and_is_nullable[hana::llong_c<2>]);
+                             const auto& name = name_and_type_and_is_nullable[hana::llong_c<0>];
+
+                             using T = typename decltype(+name_and_type_and_is_nullable[hana::llong_c<1>])::type;
+                             auto data_type = data_type_from_type<T>();
+
+                             auto is_nullable = name_and_type_and_is_nullable[hana::llong_c<2>];
+
+                             definitions.emplace_back(name, data_type, is_nullable);
+
                              return definitions;
                            });
-    _table = std::make_shared<Table>(ccolumn_definitions, TableType::Data, chunk_size, use_mvcc);
+    _table = std::make_shared<Table>(column_definitions, TableType::Data, chunk_size, use_mvcc);
 
     // Reserve some space in the vectors
     hana::for_each(_data_vectors, [&](auto&& vector) { vector.reserve(_estimated_rows_per_chunk); });
+
+    // Create the is_null_vectors only for nullable columns and reserve some space in them
+    auto column_definition_tuples_and_is_null_vectors = hana::zip_with(
+      [](auto& column_definition, auto& is_null_vector) {
+        return hana::make_tuple(std::reference_wrapper(column_definition), std::reference_wrapper(is_null_vector));
+      },
+      column_definition_tuples, _is_null_vectors);
+
+    hana::for_each(column_definition_tuples_and_is_null_vectors,
+      [&](auto& column_definition_tuple_and_is_null_vector){
+        const auto& column_definition = column_definition_tuple_and_is_null_vector[hana::llong_c<0>].get();
+        auto& is_null_vector = column_definition_tuple_and_is_null_vector[hana::llong_c<1>].get();
+
+        if (column_definition[hana::llong_c<2>]) {  // if column is nullable
+          is_null_vector = std::vector<bool>();
+          is_null_vector.value().reserve(_estimated_rows_per_chunk);
+        } else {
+          is_null_vector = std::nullopt;
+        }
+    });
   }
 
   std::shared_ptr<Table> finish_table() {
@@ -72,13 +98,20 @@ public:
       auto& is_null_vector = data_vector_and_is_null_vector_and_value[hana::llong_c<1>].get();
       auto& optional_value = data_vector_and_is_null_vector_and_value[hana::llong_c<2>];
 
+      auto column_is_nullable = is_null_vector.has_value();
       auto is_null = !optional_value.has_value();
+
+      DebugAssert(column_is_nullable || !is_null, "cannot insert null value into not-null-column");
+
       if(is_null) {
         data_vector.emplace_back();
+        is_null_vector.value().emplace_back(true);
       } else {
         data_vector.emplace_back(std::move(optional_value.value()));
+        if (column_is_nullable){
+          is_null_vector.value().emplace_back(false);
+        }
       }
-      is_null_vector.emplace_back(is_null);
     });
 
     if (_current_chunk_row_count() >= _table->max_chunk_size()) {
@@ -94,10 +127,11 @@ private:
   hana::tuple<std::vector<DataTypes>...> _data_vectors;
   // what we want: a hana::tuple containing a bool vector for each data vector
   // in other words: std::array<std::vector<bool>, sizeof...(DataTypes)> as hana::tuple
-  // so we define a struct which defines an alias for boolean and consumes the template parameter, so we can use "..."
-  template<typename T>
-  struct ConsumeTemplateParameterAndDefineBoolean { using boolean = bool; };
-  hana::tuple<std::vector<typename ConsumeTemplateParameterAndDefineBoolean<DataTypes>::boolean>...> _is_null_vectors;
+  // so we define an alias for boolean which consumes the template parameter, so we can use "..."
+  template <typename ...>
+  using bool_t = bool;
+  hana::tuple<std::optional<std::vector<bool_t<DataTypes>>>...> _is_null_vectors;
+  // TODO(pascal): use hana::optional instead of std::optional
 
   size_t _current_chunk_row_count() const { return _data_vectors[hana::llong_c<0>].size(); }
 
@@ -117,14 +151,18 @@ private:
       auto& is_null_vector = data_vector_and_is_null_vector[hana::llong_c<1>].get();
 
       using T = typename std::decay_t<decltype(data_vector)>::value_type;
-      // reason for nolint: clang-tidy wants this to be a forward, but that doesn't work
-      segments.push_back(std::make_shared<ValueSegment<T>>(std::move(data_vector), std::move(is_null_vector)));  // NOLINT
+      // reason for nolint: clang-tidy wants this to be a forward, but that doesn't work TODO: really?
+      if (is_null_vector.has_value()){
+        segments.push_back(std::make_shared<ValueSegment<T>>(std::move(data_vector), std::move(is_null_vector.value())));
+
+        is_null_vector = std::vector<bool>();
+        is_null_vector.value().reserve(_estimated_rows_per_chunk);
+      } else {
+        segments.push_back(std::make_shared<ValueSegment<T>>(std::move(data_vector)));
+      }
 
       data_vector = std::decay_t<decltype(data_vector)>();
       data_vector.reserve(_estimated_rows_per_chunk);
-
-      is_null_vector = std::decay_t<decltype(is_null_vector)>();
-      is_null_vector.reserve(_estimated_rows_per_chunk);
     });
 
     // Create initial MvccData if MVCC is enabled
@@ -141,12 +179,12 @@ private:
 
 using namespace opossum;  // NOLINT
 
-const auto column_definitions = hana::make_tuple(
+const auto names_and_nullable = hana::make_tuple(
   hana::make_tuple("a", hana::type_c<int64_t>, false),
   hana::make_tuple("b", hana::type_c<int64_t>, true));
 
 int main() {
-  auto table_builder = TableBuilder<int64_t, int64_t>(2, column_definitions, UseMvcc::Yes, size_t{3});
+  auto table_builder = TableBuilder<int64_t, int64_t>(2, names_and_nullable, UseMvcc::Yes, size_t{3});
   table_builder.append_row(11, {});
   table_builder.append_row(12, 22);
   table_builder.append_row(13, {});
