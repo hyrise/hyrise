935ccc9087f656408ca4122c46658b275afa98ec
Moritz Eyssen <mrzesn@t-online.de>
2019-05-20 07:55:00 +0200

    Perform column pruning in GetTable/StoredTableNode (#1664)
    Removes the need for that "Pruning Projection" that we previously had.

--- src/lib/logical_query_plan/stored_table_node.hpp
+++ src/lib/logical_query_plan/stored_table_node.hpp
@@ -11,14 +11,29 @@ namespace opossum {
 
 class LQPColumnExpression;
 
+/**
+ * Represents a Table from the StorageManager in an LQP
+ *
+ * Holds Column and Chunk pruning information.
+ */
 class StoredTableNode : public EnableMakeForLQPNode<StoredTableNode>, public AbstractLQPNode {
  public:
   explicit StoredTableNode(const std::string& table_name);
 
   LQPColumnReference get_column(const std::string& name) const;
 
-  void set_excluded_chunk_ids(const std::vector<ChunkID>& chunks);
-  const std::vector<ChunkID>& excluded_chunk_ids() const;
+  /**
+   * @defgroup ColumnIDs and ChunkIDs to be pruned from the stored Table.
+   * Both vectors need to be sorted and must no contain duplicates when passed to `set_pruned_{chunk/column}_ids()`
+   *
+   * @{
+   */
+  void set_pruned_chunk_ids(const std::vector<ChunkID>& pruned_chunk_ids);
+  const std::vector<ChunkID>& pruned_chunk_ids() const;
+
+  void set_pruned_column_ids(const std::vector<ColumnID>& pruned_column_ids);
+  const std::vector<ColumnID>& pruned_column_ids() const;
+  /** @} */
 
   std::string description() const override;
   const std::vector<std::shared_ptr<AbstractExpression>>& column_expressions() const override;
@@ -34,8 +49,9 @@ class StoredTableNode : public EnableMakeForLQPNode<StoredTableNode>, public Abs
   bool _on_shallow_equals(const AbstractLQPNode& rhs, const LQPNodeMapping& node_mapping) const override;
 
  private:
-  mutable std::optional<std::vector<std::shared_ptr<AbstractExpression>>> _expressions;
-  std::vector<ChunkID> _excluded_chunk_ids;
+  mutable std::optional<std::vector<std::shared_ptr<AbstractExpression>>> _column_expressions;
+  std::vector<ChunkID> _pruned_chunk_ids;
+  std::vector<ColumnID> _pruned_column_ids;
 };
 
 }  // namespace opossum
