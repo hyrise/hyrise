f26c75448f79da3297d08bf8e8832e1334a0f483
Bouncner <martin+github@boissier.de>
2019-05-20 17:14:23 +0200

    Adapt supported encodings of FoR

--- src/lib/storage/encoding_type.hpp
+++ src/lib/storage/encoding_type.hpp
@@ -37,7 +37,7 @@ constexpr auto supported_data_types_for_encoding_type = hana::make_map(
     hana::make_pair(enum_c<EncodingType, EncodingType::Dictionary>, data_types),
     hana::make_pair(enum_c<EncodingType, EncodingType::RunLength>, data_types),
     hana::make_pair(enum_c<EncodingType, EncodingType::FixedStringDictionary>, hana::tuple_t<pmr_string>),
-    hana::make_pair(enum_c<EncodingType, EncodingType::FrameOfReference>, hana::tuple_t<int32_t, int64_t>),
+    hana::make_pair(enum_c<EncodingType, EncodingType::FrameOfReference>, hana::tuple_t<int32_t>),
     hana::make_pair(enum_c<EncodingType, EncodingType::LZ4>, data_types));
 
 /**
