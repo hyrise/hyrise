7d01af3a69b35224c223dcb45026ac3beb9493aa
Markus Dreseler <markus.dreseler@hpi.de>
2019-05-21 16:12:23 +0200

    ...

--- src/benchmarklib/benchmark_runner.hpp
+++ src/benchmarklib/benchmark_runner.hpp
@@ -35,7 +35,6 @@ class BenchmarkRunner {
  public:
   BenchmarkRunner(const BenchmarkConfig& config, std::unique_ptr<AbstractBenchmarkItemRunner> benchmark_item_runner,
                   std::unique_ptr<AbstractTableGenerator> table_generator, const nlohmann::json& context);
-  ~BenchmarkRunner();
 
   void run();
 
