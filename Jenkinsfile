cc30ec9ff9a57d2c2bce9e6d13fc067fd16fd9db
Fabian Engel <engelfab@gmail.com>
2019-05-22 12:28:48 +0200

    Add hyriseBenchmarkTPCH_test.py.

--- Jenkinsfile
+++ Jenkinsfile
@@ -76,20 +76,22 @@ try {
           stage("clang-debug") {
             sh "export CCACHE_BASEDIR=`pwd`; cd clang-debug && make all -j \$(( \$(cat /proc/cpuinfo | grep processor | wc -l) / 3))"
             sh "./clang-debug/hyriseTest clang-debug"
-            sh "./scripts/test/hyriseConsole_test.py clang-debug"
             sh "mv resources/test_data/imdb_sample/ imdb_data/"
+            sh "./scripts/test/hyriseConsole_test.py clang-debug"
             sh "./scripts/test/hyriseBenchmarkJoinOrder_test.py clang-debug"
             sh "./scripts/test/hyriseBenchmarkFileBased_test.py clang-debug"
+            sh "./scripts/test/hyriseBenchmarkTCPH_test.py clang-debug"
           }
         }, gccDebug: {
           stage("gcc-debug") {
             sh "export CCACHE_BASEDIR=`pwd`; cd gcc-debug && make all -j \$(( \$(cat /proc/cpuinfo | grep processor | wc -l) / 3))"
             // Test that running the binary from the build folder works
-            sh "./scripts/test/hyriseConsole_test.py gcc-debug"
-            sh "cd gcc-debug && ./hyriseTest"
+            sh "cd gcc-debug && ./hyriseTest && cd .."
             sh "mv resources/test_data/imdb_sample/ imdb_data/"
+            sh "./scripts/test/hyriseConsole_test.py gcc-debug"
             sh "./scripts/test/hyriseBenchmarkJoinOrder_test.py gcc-debug"
             sh "./scripts/test/hyriseBenchmarkFileBased_test.py gcc-debug"
+            sh "./scripts/test/hyriseBenchmarkTCPH_test.py gcc-debug"
           }
         }, lint: {
           stage("Linting") {
---
+++ scripts/test/hyriseBenchmarkTPCH_test.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+
+import os
+import sys
+import pexpect
+
+arguments1 = {}
+arguments1["--scale"] = ".01"
+arguments1["--use_prepared_statements"] = "true"
+arguments1["--queries"] = "'1,13,19'"
+arguments1["--time"] = "1"
+arguments1["--runs"] = "100"
+arguments1["--mode"] = "'PermutedQuerySet'"
+arguments1["--encoding"] = "'Unencoded'"
+arguments1["--compression"] = "'SIMD-BP128'"
+arguments1["--scheduler"] = "true"
+arguments1["--clients"] = "4"
+arguments1["--mvcc"] = "true"
+arguments1["--visualize"] = "true"
+
+arguments2 = {}
+arguments2["--scale"] = ".005"
+arguments2["--queries"] = "'2,4,6'"
+arguments2["--time"] = "1"
+arguments2["--runs"] = "100"
+arguments2["--warmup"] = "1"
+arguments2["--compression"] = "'Fixed-size byte-aligned'"
+arguments2["--verify"] = "true"
+
+def initialize(arguments):
+	if len(sys.argv) == 1:
+		print ("Usage: ./scripts/test/hyriseBenchmarkTPCH_test.py <build_dir>")
+		sys.exit(1)
+
+	build_dir = sys.argv[1]
+
+	concat_arguments = ' '.join(['='.join(map(str, x)) for x in arguments.items()])
+	print(concat_arguments)
+
+	benchmark = pexpect.spawn(build_dir + "/hyriseBenchmarkTPCH " + concat_arguments, maxread=1000000,timeout=None, dimensions=(200, 64))
+	benchmark.logfile = sys.stdout
+	return benchmark
+
+def close_benchmark(benchmark):
+	benchmark.expect(pexpect.EOF, timeout=None)
+	benchmark.close()
+
+def check_exit_status(benchmark):
+	if benchmark.exitstatus == None:
+		sys.exit(benchmark.signalstatus)
+
+def main():
+	return_error = False
+	
+	benchmark = initialize(arguments1)
+	close_benchmark(benchmark)
+	check_exit_status(benchmark)
+
+	benchmark = initialize(arguments2)
+	benchmark.expect("-> Executed")
+
+	if benchmark.before.count('Verification failed'):
+		return_error = True
+
+	close_benchmark(benchmark)
+	check_exit_status(benchmark)
+
+	if return_error:
+		sys.exit(1)
+
+if __name__ == '__main__':
+	main()
