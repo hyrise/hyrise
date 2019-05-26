a0dfc039d7591f030e9f06649ce4ecb834e8ed9a
Julian Menzler <julianmenzler@gmail.com>
2019-05-26 23:18:11 +0200

    Change comment in MvccDeletePlugin

--- src/plugins/mvcc_delete_plugin.cpp
+++ src/plugins/mvcc_delete_plugin.cpp
@@ -164,8 +164,10 @@ void MvccDeletePlugin::_delete_chunk_physically(const std::shared_ptr<Table>& ta
   const auto& chunk = table->get_chunk(chunk_id);
 
   /**
-   * The chunk should be referenced only by the plugin in methods _delete_chunk_physically()
-   * & _physical_delete_loop() and the table's chunk vector.
+   * For a safe physical deletion, the Chunk has to be referenced as follows:
+   *  1. _chunks - Table's chunk vector
+   *  2. _delete_chunk_physically() - this function
+   *  3. _physical_delete_loop() - this function's caller
    */
   Assert(chunk.use_count() == 3,
          "At this point, the chunk should be referenced only by the plugin in methods _delete_chunk_physically() "
