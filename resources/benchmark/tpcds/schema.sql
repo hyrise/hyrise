3819840dbf6403c97044a904caf6754fa5475cb6
mweisgut <marcel-weisgut@web.de>
2019-06-07 14:01:54 +0200

    Revert "removed table dbgen_version"
    This reverts commit d4374fd34563e837336c753f29bd89756a31e385.

--- resources/benchmark/tpcds/schema.sql
+++ resources/benchmark/tpcds/schema.sql
@@ -1,3 +1,11 @@
+create table dbgen_version
+(
+    dv_version                varchar(16)                   ,
+    dv_create_date            date                          ,
+    dv_create_time            time                          ,
+    dv_cmdline_args           varchar(200)                  
+);
+
 create table customer_address
 (
     ca_address_sk             integer               not null,
