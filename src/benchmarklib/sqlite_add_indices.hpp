0b65190723ba1b5509459daf5ace920d02403b17
mweisgut <marcel-weisgut@web.de>
2019-06-07 14:27:34 +0200

    :art: formatting

--- src/benchmarklib/sqlite_add_indices.hpp
+++ src/benchmarklib/sqlite_add_indices.hpp
@@ -7,9 +7,9 @@ namespace opossum {
 class BenchmarkRunner;
 
 /**
-	 * Add indexes to SQLite. This is a hack until we support CREATE INDEX ourselves and pass that on to SQLite.
-   * Without this, SQLite would never finish.
-   */
+ * Add indexes to SQLite. This is a hack until we support CREATE INDEX ourselves and pass that on to SQLite.
+ * Without this, SQLite would never finish.
+ */
 void add_indices_to_sqlite(std::string schema_file_path, std::string create_indices_file_path,
                            BenchmarkRunner& benchmark_runner);
 
