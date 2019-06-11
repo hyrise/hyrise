778bad89a88a6f01a7ff9d0f3d29d1f7cd55b539
mweisgut <marcel-weisgut@web.de>
2019-06-11 18:29:30 +0200

    :ok_hand: integrated feedback

--- src/benchmark/tpcds_benchmark.cpp
+++ src/benchmark/tpcds_benchmark.cpp
@@ -35,11 +35,11 @@ int main(int argc, char* argv[]) {
 
   // clang-format off
   cli_options.add_options()
-    ("s,scale", "Database scale factor (1 ~ 1GB)", cxxopts::value<int>()->default_value("1"));
+    ("s,scale", "Database scale factor (1 ~ 1GB)", cxxopts::value<int32_t>()->default_value("1"));
   // clang-format on
 
   std::shared_ptr<opossum::BenchmarkConfig> config;
-  int scale_factor;
+  int32_t scale_factor;
 
   if (opossum::CLIConfigParser::cli_has_json_config(argc, argv)) {
     // JSON config file was passed in
@@ -61,17 +61,17 @@ int main(int argc, char* argv[]) {
         std::make_shared<opossum::BenchmarkConfig>(opossum::CLIConfigParser::parse_basic_cli_options(cli_parse_result));
   }
 
-  // For the data generation, the official tpc-ds toolkit is used.
-  // This toolkit does not provide the option to generate data using a scale factor less than 1.
-  Assert(scale_factor >= 1, "For now, TPC-DS benchmark only supports scale factor 1");
+  const std::vector<int32_t> valid_scale_factors{1, 1000, 3000, 10000, 30000, 100000};
+
+  const auto& find_result = std::find(valid_scale_factors.begin(), valid_scale_factors.end(), scale_factor);
+  Assert(find_result != valid_scale_factors.end(),
+         "TPC-DS benchmark only supports scale factor 1 (qualification only), 1000, 3000, 10000, 30000 and 100000.");
 
   auto context = opossum::BenchmarkRunner::create_context(*config);
 
   std::cout << "- TPC-DS scale factor is " << scale_factor << std::endl;
 
   // TPC-DS FileBasedQueryGenerator specification
-  std::optional<std::unordered_set<std::string>> query_subset;
-  const auto query_filename_blacklist = std::unordered_set<std::string>{};
   std::string query_path = "resources/benchmark/tpcds/queries/supported";
   std::string table_path = "resources/benchmark/tpcds/tables";
 
@@ -83,22 +83,23 @@ int main(int argc, char* argv[]) {
 
   if (!data_files_available(table_path)) {
     if (std::filesystem::exists(std::filesystem::path{"third_party/tpcds-dsdgen/dsdgen"})) {
-      const auto setup_table_data_files_return_code = system(
-          "cd ./third_party/tpcds-dsdgen/ &&"
-          "./dsdgen -scale 1 -dir ../../resources/benchmark/tpcds/tables -terminate n -verbose -f &&"
-          "cd ../../resources/benchmark/tpcds/tables &&"
-          "for x in *.dat; do mv $x ${x%.dat}.csv; done &&"
-          "cd ../../../../");
-      Assert(setup_table_data_files_return_code == 0, "Generating table data files failed.");
+      const auto files_setup_return = system(("cd ./third_party/tpcds-dsdgen/ &&"
+                                              "./dsdgen -scale " +
+                                              std::to_string(scale_factor) +
+                                              " -dir ../../resources/benchmark/tpcds/tables -terminate n -verbose -f &&"
+                                              "cd ../../resources/benchmark/tpcds/tables &&"
+                                              "for x in *.dat; do mv $x ${x%.dat}.csv; done &&"
+                                              "cd ../../../../")
+                                                 .c_str());
+      Assert(files_setup_return == 0, "Generating table data files failed.");
     } else {
       Fail("Could not find 'third_party/tpcds-dsdgen/dsdgen'. Did you run the benchmark from the project root dir?");
     }
   }
 
-  Assert(data_files_available(table_path), "Table data files are missing.");
+  Assert(data_files_available(table_path), "Generating table data files failed.");
 
-  auto query_generator =
-      std::make_unique<FileBasedBenchmarkItemRunner>(config, query_path, query_filename_blacklist, query_subset);
+  auto query_generator = std::make_unique<FileBasedBenchmarkItemRunner>(config, query_path);
   auto table_generator = std::make_unique<FileBasedTableGenerator>(config, table_path);
   auto benchmark_runner = BenchmarkRunner{*config, std::move(query_generator), std::move(table_generator), context};
 
@@ -112,31 +113,34 @@ int main(int argc, char* argv[]) {
   benchmark_runner.run();
 }
 
-bool data_files_available(std::string table_path) {
-  // clang-format off
-  return std::filesystem::exists(table_path + "/call_center.csv") &&
-         std::filesystem::exists(table_path + "/catalog_page.csv") &&
-         std::filesystem::exists(table_path + "/catalog_returns.csv") &&
-         std::filesystem::exists(table_path + "/catalog_sales.csv") &&
-         std::filesystem::exists(table_path + "/customer_address.csv") &&
-         std::filesystem::exists(table_path + "/customer.csv") &&
-         std::filesystem::exists(table_path + "/customer_demographics.csv") &&
-         std::filesystem::exists(table_path + "/date_dim.csv") &&
-         std::filesystem::exists(table_path + "/household_demographics.csv") &&
-         std::filesystem::exists(table_path + "/income_band.csv") &&
-         std::filesystem::exists(table_path + "/inventory.csv") &&
-         std::filesystem::exists(table_path + "/item.csv") &&
-         std::filesystem::exists(table_path + "/promotion.csv") &&
-         std::filesystem::exists(table_path + "/reason.csv") &&
-         std::filesystem::exists(table_path + "/ship_mode.csv") &&
-         std::filesystem::exists(table_path + "/store.csv") &&
-         std::filesystem::exists(table_path + "/store_returns.csv") &&
-         std::filesystem::exists(table_path + "/store_sales.csv") &&
-         std::filesystem::exists(table_path + "/time_dim.csv") &&
-         std::filesystem::exists(table_path + "/warehouse.csv") &&
-         std::filesystem::exists(table_path + "/web_page.csv") &&
-         std::filesystem::exists(table_path + "/web_returns.csv") &&
-         std::filesystem::exists(table_path + "/web_sales.csv") &&
-         std::filesystem::exists(table_path + "/web_site.csv");
-  // clang-format off
+bool data_files_available(const std::string table_path) {
+  for (const auto& table : {"call_center",
+                            "catalog_page",
+                            "catalog_returns",
+                            "catalog_sales",
+                            "customer_address",
+                            "customer",
+                            "customer_demographics",
+                            "date_dim",
+                            "household_demographics",
+                            "income_band",
+                            "inventory",
+                            "item",
+                            "promotion",
+                            "reason",
+                            "ship_mode",
+                            "store",
+                            "store_returns",
+                            "store_sales",
+                            "time_dim",
+                            "warehouse",
+                            "web_page",
+                            "web_returns",
+                            "web_sales",
+                            "web_site"}) {
+    if (!std::filesystem::exists(table_path + "/" + table + ".csv")) {
+      return true;
+    }
+  }
+  return false;
 }
