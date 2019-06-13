63113603706018f5fea77e7ce97b661543c8b8d7
Jan Kossmann <jan.kossmann@hpi.de>
2019-06-13 13:03:04 +0200

    Deactivate chunk pruning

--- src/lib/optimizer/optimizer.cpp
+++ src/lib/optimizer/optimizer.cpp
@@ -92,7 +92,7 @@ std::shared_ptr<Optimizer> Optimizer::create_default_optimizer() {
 
   // optimizer->add_rule(std::make_unique<ColumnPruningRule>());
 
-  optimizer->add_rule(std::make_unique<ChunkPruningRule>());
+  // optimizer->add_rule(std::make_unique<ChunkPruningRule>());
 
   // Run before SubqueryToJoinRule, since the Semi/Anti Joins it introduces are opaque to the JoinOrderingRule
   optimizer->add_rule(std::make_unique<JoinOrderingRule>(std::make_unique<CostModelLogical>()));
