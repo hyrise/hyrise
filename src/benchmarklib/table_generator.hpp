677b46da3e545f3be5de65c94c252d564300af02
Martin Boisser <Martin.Boissier@sidon.eaalab.hpi.uni-potsdam.de>
2019-06-16 22:19:36 +0200

    Fix GCC build on NUMA machines

--- src/benchmarklib/table_generator.hpp
+++ src/benchmarklib/table_generator.hpp
@@ -104,7 +104,7 @@ class TableGenerator {
     } else {
       // TODO(anyone): generation of strings is not perfect yet (nonetheless, probably sufficient), as 
       // values do not start with '         0' and the transition to the next used char is not perfect.
-      const std::vector<const char> chars = {
+      const std::vector<char> chars = {
           '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
           'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
           'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y'};
