7868c4d2f8410b19135ef5d6485e1357ee240efa
Markus Dreseler <github.com@mrks.info>
2019-06-20 13:38:52 +0200

    Resolve SingleChunkReferenceSegmentAccessor statically

--- src/lib/storage/segment_accessor.cpp
+++ src/lib/storage/segment_accessor.cpp
@@ -12,8 +12,30 @@ std::unique_ptr<AbstractSegmentAccessor<T>> CreateSegmentAccessor<T>::create(
   resolve_segment_type<T>(*segment, [&](const auto& typed_segment) {
     using SegmentType = std::decay_t<decltype(typed_segment)>;
     if constexpr (std::is_same_v<SegmentType, ReferenceSegment>) {
-      if (typed_segment.pos_list()->references_single_chunk() && typed_segment.pos_list()->size() > 0) {
-        accessor = std::make_unique<SingleChunkReferenceSegmentAccessor<T>>(typed_segment);
+      const auto& pos_list = *typed_segment.pos_list();
+      if (pos_list.references_single_chunk() && pos_list.size() > 0) {
+        // If the first chunk_id references a non-existing chunk, the entry is NULL. Since all entries reference
+        // the same chunk_id, we can safely assume that all other entries are also NULL and always return std::nullopt.
+        if (pos_list[ChunkOffset{0}].is_null()) {
+          accessor = std::make_unique<NullAccessor<T>>();
+        } else {
+          auto chunk_id = pos_list[ChunkOffset{0}].chunk_id;
+          auto referenced_segment =
+              typed_segment.referenced_table()->get_chunk(chunk_id)->get_segment(typed_segment.referenced_column_id());
+
+          // If only a single segment is referenced, we can resolve it right here and now so that we can avoid some
+          // virtual method calls later.
+          resolve_segment_type<T>(*referenced_segment, [&](const auto& typed_referenced_segment) {
+            if constexpr (!std::is_same_v<std::decay_t<decltype(typed_referenced_segment)>, ReferenceSegment>) {
+              auto accessor_into_referenced =
+                  SegmentAccessor<T, decltype(typed_referenced_segment)>(typed_referenced_segment);
+              accessor = std::make_unique<SingleChunkReferenceSegmentAccessor<T, decltype(accessor_into_referenced)>>(
+                  pos_list, chunk_id, std::move(accessor_into_referenced));
+            } else {
+              Fail("Encountered nested ReferenceSegments");
+            }
+          });
+        }
       } else {
         accessor = std::make_unique<MultipleChunkReferenceSegmentAccessor<T>>(typed_segment);
       }
