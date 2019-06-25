250fb030b54f422963aac83e88f778f68a18145e
mweisgut <marcel-weisgut@web.de>
2019-06-25 18:52:52 +0200

    using pruning_column_id_mapping in stored table node

--- src/lib/utils/pruning_column_id_mapping.hpp
+++ src/lib/utils/pruning_column_id_mapping.hpp
@@ -2,9 +2,9 @@
 
 #include "types.hpp"
 
-namespace opossum{
-	
+namespace opossum {
+
 const std::vector<std::optional<ColumnID>> column_ids_after_pruning(const size_t& original_table_column_count,
-	const std::vector<ColumnID>& pruned_column_ids);
+                                                                    const std::vector<ColumnID>& pruned_column_ids);
 
 }
