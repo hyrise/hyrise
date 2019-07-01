8a6798fcd6f123964f1bc4c3b5bf7d58f612eccb
mrks <markus.dreseler@hpi.de>
2019-07-01 19:34:30 +0200

    review

--- src/lib/storage/segment_accessor.cpp
+++ src/lib/storage/segment_accessor.cpp
@@ -14,8 +14,11 @@ std::unique_ptr<AbstractSegmentAccessor<T>> CreateSegmentAccessor<T>::create(
     if constexpr (std::is_same_v<SegmentType, ReferenceSegment>) {
       const auto& pos_list = *typed_segment.pos_list();
       if (pos_list.references_single_chunk() && pos_list.size() > 0) {
-        // If the first chunk_id references a non-existing chunk, the entry is NULL. Since all entries reference
-        // the same chunk_id, we can safely assume that all other entries are also NULL and always return std::nullopt.
+        // If the pos list stores a NULL value, its chunk_id references a non-existing chunk. If all entries reference
+        // the same chunk_id, we can safely assume that all other entries are also NULL. Instead of using an accessor
+        // that checks for the reference being NULL, we can simply use the NullAccessor, which always returns nullopt,
+        // i.e., the accessors representation of NULL values.
+        // Note that this is independent of the row being pointed to holding a NULL value.
         if (pos_list[ChunkOffset{0}].is_null()) {
           accessor = std::make_unique<NullAccessor<T>>();
         } else {
@@ -23,7 +26,7 @@ std::unique_ptr<AbstractSegmentAccessor<T>> CreateSegmentAccessor<T>::create(
           auto referenced_segment =
               typed_segment.referenced_table()->get_chunk(chunk_id)->get_segment(typed_segment.referenced_column_id());
 
-          // If only a single segment is referenced, we can resolve it right here and now so that we can avoid some
+          // If only a single segment is referenced, we can resolve it once and avoid some more expensive
           // virtual method calls later.
           resolve_segment_type<T>(*referenced_segment, [&](const auto& typed_referenced_segment) {
             using ReferencedSegment = std::decay_t<decltype(typed_referenced_segment)>;
