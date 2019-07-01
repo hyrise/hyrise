3820a373234580810876d3d9e41ee4f844e29260
martin.boissier <martin.boissier@hpi.de>
2019-07-01 23:07:59 +0200

    Revert to output strings for encoding and vector compression.

--- src/lib/cost_estimation/feature/column_features.cpp
+++ src/lib/cost_estimation/feature/column_features.cpp
@@ -11,9 +11,12 @@ const std::map<std::string, AllTypeVariant> ColumnFeatures::serialize() const {
   //  DebugAssert(column_data_type, "Data Type not set in ColumnFeatures");
   const auto data_type_string = column_data_type ? data_type_to_string.left.at(*column_data_type) : "";
 
+  const auto vector_compression_string = (column_segment_vector_compression) ? vector_compression_type_to_string.left.at(*column_segment_vector_compression) : "None";
+
+
   return {
-      {_prefix + "_column_segment_encoding", static_cast<int32_t>(column_segment_encoding)},
-      {_prefix + "_column_segment_vector_compression", (column_segment_vector_compression) ? static_cast<int32_t>(*column_segment_vector_compression) : static_cast<int32_t>(255)},
+      {_prefix + "_column_segment_encoding", pmr_string(encoding_type_to_string.left.at(column_segment_encoding))},
+      {_prefix + "_column_segment_vector_compression", pmr_string(vector_compression_string)},
       {_prefix + "_column_is_reference_segment", column_is_reference_segment},
       {_prefix + "_column_data_type", pmr_string(data_type_string)},
       {_prefix + "_column_memory_usage_bytes", static_cast<int64_t>(column_memory_usage_bytes)},
