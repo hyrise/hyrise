160a4218390a23ae6373f4b0866e76be294d3006
Pascal Fuehrlich <pascal.fuehrlich@student.hpi.de>
2019-07-03 15:25:56 +0200

    generate scale factor 10

--- src/bin/tpcds_table_generator.cpp
+++ src/bin/tpcds_table_generator.cpp
@@ -4,14 +4,18 @@
 #include "tpcds/tpcds_table_generator.hpp"
 
 int main() {
-  std::cout << "TPCDS" << std::endl;
-  std::cout << " > Generating tables" << std::endl;
-  opossum::TpcdsTableGenerator(1, 1000).generate_and_store();
+//  std::cout << "TPCDS" << std::endl;
+//  std::cout << " > Generating tables" << std::endl;
 
-  std::cout << " > Dumping as CSV" << std::endl;
-  opossum::StorageManager::get().export_all_tables_as_csv(".");
+//  opossum::TpcdsTableGenerator(1, 1000).generate_and_store();
 
-  std::cout << " > Done" << std::endl;
+//  std::cout << " > Dumping as CSV" << std::endl;
+//  opossum::StorageManager::get().export_all_tables_as_csv(".");
+//
+//  std::cout << " > Done" << std::endl;
+//
+//  return 0;
 
-  return 0;
+  opossum::TpcdsTableGenerator(10, 1000).generate();
+  // TODO
 }
