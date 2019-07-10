0f0615d57e3cfcc636774575ac7eceea28adeb63
mweisgut <marcel-weisgut@web.de>
2019-07-10 13:40:24 +0200

    naming

--- src/lib/operators/join_index.cpp
+++ src/lib/operators/join_index.cpp
@@ -438,9 +438,9 @@ void JoinIndex::_write_output_segments(Segments& output_segments, const std::sha
             input_table->get_chunk(ChunkID{0})->get_segment(column_id));
 
         // de-reference to the correct RowID so the output can be used in a Multi Join
-        const auto& pos_list_ = *pos_list;  // dereference pos_list here to avoid dereferencing in the loop
-        for (auto pos_list_offset = ChunkOffset{0}; pos_list_offset < pos_list_.size(); ++pos_list_offset) {
-          const auto& row = pos_list_[pos_list_offset];
+        const auto& position_list = *pos_list;  // dereference pos_list here to avoid dereferencing in the loop
+        for (auto pos_list_offset = ChunkOffset{0}; pos_list_offset < position_list.size(); ++pos_list_offset) {
+          const auto& row = position_list[pos_list_offset];
           if (row.is_null()) {
             new_pos_list->push_back(NULL_ROW_ID);
             continue;
