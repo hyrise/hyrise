c91715678a9f222b2870acb8a916b082fa8b7b03
mweisgut <marcel-weisgut@web.de>
2019-07-10 12:19:31 +0200

    using structured binding declaration

--- src/benchmarklib/benchmark_sql_executor.cpp
+++ src/benchmarklib/benchmark_sql_executor.cpp
@@ -36,7 +36,7 @@ std::pair<SQLPipelineStatus, std::shared_ptr<const Table>> BenchmarkSQLExecutor:
   metrics.emplace_back(std::move(pipeline.metrics()));
 
   if (_expected_result_table) {
-    _compare_tables(_expected_result_table, std::get<1>(pipeline.get_result_table()),
+    _compare_tables(_expected_result_table, result_table,
                     "Using dedicated expected result table");
   } else if (_sqlite_wrapper) {
     _verify_with_sqlite(pipeline);
