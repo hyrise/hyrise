0baa5af7efd1e9b8afbbe65ed5715cd4a44dbb94
mrks <markus.dreseler@hpi.de>
2019-07-11 12:24:15 +0200

    embarassing

--- src/test/logical_query_plan/create_view_node_test.cpp
+++ src/test/logical_query_plan/create_view_node_test.cpp
@@ -22,13 +22,13 @@ class CreateViewNodeTest : public ::testing::Test {
 
 TEST_F(CreateViewNodeTest, Description) {
   EXPECT_EQ(_create_view_node->description(),
-            "[CreateView] Name: 'some_view', Columns: a FROM (\n"
+            "[CreateView] Name: some_view, Columns: a FROM (\n"
             "[0] [MockNode 'Unnamed'] pruned: 0/1 columns\n"
             ")");
 
   const auto _create_view_node_2 = CreateViewNode::make("some_view", _view, true);
   EXPECT_EQ(_create_view_node_2->description(),
-            "[CreateView] IfNotExists Name: 'some_view', Columns: a FROM (\n"
+            "[CreateView] IfNotExists Name: some_view, Columns: a FROM (\n"
             "[0] [MockNode 'Unnamed'] pruned: 0/1 columns\n"
             ")");
 }
