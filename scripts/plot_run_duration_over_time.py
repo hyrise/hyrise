a437e2c310a3ca5ad7a3830b0b62cf359d55b788
Markus Dreseler <markus@Markuss-MacBook-Pro.local>
2019-07-23 14:43:15 +0200

    review

--- scripts/plot_run_duration_over_time.py
+++ scripts/plot_run_duration_over_time.py
@@ -26,9 +26,8 @@ for benchmark_json in data_json:
 	for run_json in benchmark_json['runs']:
 		begin = run_json['begin']
 		duration = run_json['duration']
-		data.append({'name': name, 'begin': begin, 'duration': duration})
+		data.append({'name': name, 'begin': begin / 1e9, 'duration': duration})
 
-fig, ax = plt.subplots()
 df = pd.DataFrame(data)
 
 # Set the colors
@@ -44,17 +43,35 @@ for i in range(1, len(benchmark_names)):
 	name_to_color[benchmark_names[i]] = color
 	colors[df['name'] == benchmark_names[i]] = color
 
-# Plot
-df.reset_index().plot(kind='scatter', x='begin', y='duration', c=colors, s=.3)
-ax.grid(True)
+# Plot combined graph
+ax = df.reset_index().plot(kind='scatter', x='begin', y='duration', c=colors, s=.3, figsize=(12, 9))
+ax.set_xlabel('Seconds since start')
+ax.set_ylabel('Execution duration [ns]')
+ax.grid(True, alpha=.3)
 
-# Add legend
+# Add legend to combined graph
 handles = []
 for name, color in name_to_color.items():
 	handles.append(mpatches.Patch(label=name, color=color))
 plt.legend(handles=handles)
 
-# Write to file
+# Write combined graph to file
 basename = sys.argv[1].replace('.json', '')
 plt.savefig(basename + '.pdf')
 plt.savefig(basename + '.png')
+
+# Plot detailed graph
+grouped_df = df.reset_index().groupby('name')
+fig, axes = plt.subplots(nrows=len(benchmark_names), ncols=1, figsize=(12, 3*len(benchmark_names)), sharex=True)
+for (key, ax) in zip(grouped_df.groups.keys(), axes.flatten()):
+	grouped_df.get_group(key).plot(ax=ax, kind='scatter', x='begin', y='duration', s=.6, c=name_to_color[key])
+	ax.set_title(key)
+	ax.set_xlabel('Seconds since start')
+	ax.set_ylabel('Execution duration [ns]')
+	ax.yaxis.set_label_coords(-.08, 0.5)
+	ax.grid(True, alpha=.3)
+fig.tight_layout()
+
+# Write detailed graph to file
+plt.savefig(basename + '_detailed.pdf')
+plt.savefig(basename + '_detailed.png')
