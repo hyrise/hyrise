c29fb8f7a252244e4936aa275f52b6d6db9f784b
Markus Dreseler <markus.dreseler@hpi.de>
2019-08-02 15:14:28 +0200

    Add edge case test

--- src/lib/expression/evaluation/expression_evaluator.cpp
+++ src/lib/expression/evaluation/expression_evaluator.cpp
@@ -614,7 +614,7 @@ std::shared_ptr<ExpressionResult<Result>> ExpressionEvaluator::_evaluate_case_ex
           nulls.resize(result_size);
 
           for (auto chunk_offset = ChunkOffset{0}; chunk_offset < result_size; ++chunk_offset) {
-            if (when->value(chunk_offset) /* && !when->is_null(chunk_offset)*/) {
+            if (when->value(chunk_offset) && !when->is_null(chunk_offset)) {
               values[chunk_offset] = to_value<Result>(then_result.value(chunk_offset));
               nulls[chunk_offset] = then_result.is_null(chunk_offset);
             } else {
