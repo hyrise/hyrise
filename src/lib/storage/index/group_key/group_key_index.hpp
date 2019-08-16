888df42b495866eac79dcbf48e74033d5026cd31
mweisgut <marcel-weisgut@web.de>
2019-08-16 10:54:50 +0200

    updated schema in hpp

--- src/lib/storage/index/group_key/group_key_index.hpp
+++ src/lib/storage/index/group_key/group_key_index.hpp
@@ -18,37 +18,39 @@ class GroupKeyIndexTest;
 /**
  *
  * The GroupKeyIndex works on a single dictionary compressed segment.
- * It uses two structures, one being a postings list containing record positions (ie ChunkOffsets)
- * in the attribute vector. The other structure is an index offset, mapping value-ids to offsets
+ * It uses three structures, one being a postings list containing record positions (ie ChunkOffsets)
+ * for non-null values in the attribute vector, the second is a postings list containing record positions
+ * for null vlaues and the third structure is an index offset, mapping value-ids to offsets
  * in the postings list.
+ * For the offset structure, the posting structures are considered as one overall structure with null postings
+ * at the end. 
  * For null values, an additional structure is used for storing the record positions.
  * Since this structure only contains null value positions, a further structure for mapping value-ids to
  * offsets isn't needed. 
  *
  * An example structure along with the corresponding dictionary segment might look like this:
- *  +----+-----------+------------+---------+----------------+
- *  | (i)| Attribute | Dictionary |  Index  | Index Postings |
- *  |    |  Vector   |            | Offsets |                |
- *  +----+-----------+------------+---------+----------------+
- *  |  0 |         6 | apple    ------->  0 ------------>  7 |  ie "apple" can be found at i = 7 in the AV
- *  |  1 |         4 | charlie  ------->  1 ------------>  8 |
- *  |  2 |         2 | delta    ------->  3 ----------|    9 |
- *  |  3 |         3 | frank    ------->  5 --------| |->  2 |
- *  |  4 |         2 | hotel    ------->  6 ------| |      4 |  ie "delta" can be found at i = 2 and 4
- *  |  5 |         6 | inbox    ------->  7 ----| | |--->  3 |
- *  |  6 |         6 |            | (x¹)  8 --| | |----->  1 |
- *  |  7 |         0 |            | (x²) 12 | | |-------> 10 |  ie "inbox" can be found at i = 10 in the AV
- *  |  8 |         1 |            |         | |-------|    0 |
- *  |  9 |         1 |            |         |         |    5 |
- *  | 10 |         5 |            |         |         |    6 |
- *  | 11 |         6 |            |         |         |-> 11 |  ie null can be found at i = 0, 5, 6 and 11
- *  +----+-----------+------------+---------+----------------+
- *
- * // TODO adapt figure
+ *  +----+-----------+------------+---------+----------------+----------------+
+ *  | (i)| Attribute | Dictionary |  Index  | Index Postings | Index Postings |
+ *  |    |  Vector   |            | Offsets | (non-null)     | (null)         |
+ *  +----+-----------+------------+---------+----------------+----------------+
+ *  |  0 |         6 | apple    ------->  0 ------------>  7 |  |-------|>  0 | ie null can be found at i = 0, 5, 6 and 11
+ *  |  1 |         4 | charlie  ------->  1 ------------>  8 |  |       |>  5 |
+ *  |  2 |         2 | delta    ------->  3 ----------|    9 |  |       |>  6 |
+ *  |  3 |         3 | frank    ------->  5 --------| |->  2 |  |       |> 11 |
+ *  |  4 |         2 | hotel    ------->  6 ------| |      4 |  |             |                 
+ *  |  5 |         6 | inbox    ------->  7 ----| | |--->  3 |  |             |
+ *  |  6 |         6 |            | (x¹)  8 --| | |----->  1 |  |             |
+ *  |  7 |         0 |            | (x²) 12 | | |-------> 10 |  |             |  
+ *  |  8 |         1 |            |         | |-----------------|             |
+ *  |  9 |         1 |            |         |                |                |
+ *  | 10 |         5 |            |         |                |                |
+ *  | 11 |         6 |            |         |                |                |  
+ *  +----+-----------+------------+---------+----------------+----------------+
  * 
  * Null is represented in the Attribute Vector by a ValueID which is the highest available
  * ValueID in the dictionary + 1, i.e. ValieID{6} in this example.
- * x¹: Starting offset for null values in the postings list.
+ * x¹: Starting offset for null values in the null postings list can be calculated by 
+ *     the offset minus the number of non-null values in the segment to index.
  * x²: Mark for the ending position.
  * Find more information about this in our Wiki: https://github.com/hyrise/hyrise/wiki/GroupKey-Index
  */
