fad6f168eced129e1c04f47e7b22a8f96678a402
Jonathan Janetzki <jonathan.janetzki@student.hpi.de>
2019-08-16 11:48:13 +0200

    :bulb: Add class doc
     #51

--- src/lib/optimizer/strategy/disjunction_to_union_rule.hpp
+++ src/lib/optimizer/strategy/disjunction_to_union_rule.hpp
@@ -1,15 +1,21 @@
 #pragma once
 
 #include "abstract_rule.hpp"
-#include "expression/abstract_expression.hpp"
-#include "types.hpp"
 
 namespace opossum {
 
+/**
+ * This rule turns PredicateNodes with disjunctive chains (e.g., `PredicateNode(a OR b OR c)`) as their scan expression
+ *    into n-1 consecutive UnionNodes that combine the results of n PredicateNodes with simple expressions.
+ *
+ * Doing so enables other Optimizer rules to process these PredicateNodes.
+ *
+ * EXAMPLE: TPC-DS query 35.
+ *   Without this rule `EXISTS (...) OR EXISTS (...)` would not be split up and therefore would not be reformulated to
+ *   two semi-joins.
+ */
 class DisjunctionToUnionRule : public AbstractRule {
- public:
   std::string name() const override;
-
   void apply_to(const std::shared_ptr<AbstractLQPNode>& root) const override;
 };
 
