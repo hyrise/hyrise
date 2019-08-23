5d8a7ddcc7ed529b831612f0d590fe7b06486687
Jonathan Janetzki <jonathan.janetzki@student.hpi.de>
2019-08-23 14:58:22 +0200

    :bulb: Improve classdoc
     #51

--- src/lib/optimizer/strategy/disjunction_to_union_rule.hpp
+++ src/lib/optimizer/strategy/disjunction_to_union_rule.hpp
@@ -6,17 +6,18 @@ namespace opossum {
 
 /**
  * This rule turns PredicateNodes with disjunctive chains (e.g., `PredicateNode(a OR b OR c)`) as their scan expression
- *    into n-1 consecutive UnionNodes that combine the results of n PredicateNodes with simple expressions.
- *    This rule is similar to the PredicateSplitUpRule, which handles ANDs instead of ORs. The DisjunctionToUnionRule
- *    runs after the PredicateSplitUpRule, so that expressions that are not in conjunctive normal form (ANDs of ORs)
- *    cannot be fully handled right now. Because the PredicateSplitUpRule already ran, the DisjunctionToUnionRule
- *    assumes that no ANDs are left in the PredicateExpressions.
+ *    into n-1 consecutive UnionNodes that combine the results of n simpler PredicateNodes.
+ *    The DisjunctionToUnionRule runs after the PredicateSplitUpRule, so when flattening the top-level logical
+ *    expression, we expect one or more top-level expressions that were connected using a disjunction ("or").
+ *    Conjunctions ("and") might occur nested within such a top-level expression. As the PredicateSplitUpRule already
+ *    ran, they will not be split up into successive PredicateNodes. That might be a thing for the future, if it ever
+ *    comes up.
  *
  * Doing so enables other Optimizer rules to process these PredicateNodes.
  *
  * EXAMPLE: TPC-DS query 35.
- *   Without this rule `EXISTS (...) OR EXISTS (...)` would not be split up and therefore would not be reformulated to
- *   two semi-joins.
+ *   This rule splits up `EXISTS (...) OR EXISTS (...)` into two expressions that can later be rewritten into two
+ *   semi-joins.
  */
 class DisjunctionToUnionRule : public AbstractRule {
   void apply_to(const std::shared_ptr<AbstractLQPNode>& root) const override;
