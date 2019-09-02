00a2bb567f25e27ba491756f4fd07617de98ec4b
mweisgut <marcel-weisgut@web.de>
2019-09-02 17:48:03 +0200

    changed comment

--- src/test/storage/table_column_definition_test.cpp
+++ src/test/storage/table_column_definition_test.cpp
@@ -20,7 +20,8 @@ TEST_F(TableColumnDefinitionTest, HashEquals) {
   TableColumnDefinition different_column_definition_c{"a", DataType::Int, true};
 
   EXPECT_EQ(column_definition, equal_column_definition);
-  // `operator!=` is not implemented for TableColumnDefinition, therefore EXPECT_NE fails
+  // `operator!=` is not implemented for TableColumnDefinition,
+  // therefore EXPECT_FALSE is used instead of EXPECT_NE
   EXPECT_FALSE(column_definition == different_column_definition_a);
   EXPECT_FALSE(column_definition == different_column_definition_b);
   EXPECT_FALSE(column_definition == different_column_definition_c);
