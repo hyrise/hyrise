43db359bb2fdfe0194f39affc3dc2688be4abf47
Martin Boissier <>
2019-09-16 14:14:22 +0200

    Why even bother to test locally? ... the second

--- src/test/sql/sqlite_testrunner/sqlite_testrunner_jit.cpp
+++ src/test/sql/sqlite_testrunner/sqlite_testrunner_jit.cpp
@@ -9,7 +9,7 @@ INSTANTIATE_TEST_SUITE_P(
     SQLiteTestRunnerJIT, SQLiteTestRunner,
     testing::Combine(testing::ValuesIn(SQLiteTestRunner::queries()), testing::ValuesIn({true}),
                      testing::ValuesIn({EncodingType::Unencoded,
-                                        EncodingType::Dictionary})), );  // NOLINT(whitespace/parens)  // NOLINT
+                                        EncodingType::Dictionary})));
 
 #endif
 
