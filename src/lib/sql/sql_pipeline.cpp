4d5f4498352b061dc71c335467fde3609d04b57f
Markus Dreseler <markus.dreseler@hpi.de>
2019-09-16 13:55:46 +0200

    Fix get_result_table on xvalue

--- src/lib/sql/sql_pipeline.cpp
+++ src/lib/sql/sql_pipeline.cpp
@@ -193,7 +193,7 @@ const std::vector<std::vector<std::shared_ptr<OperatorTask>>>& SQLPipeline::get_
   return _tasks;
 }
 
-std::pair<SQLPipelineStatus, const std::shared_ptr<const Table>&> SQLPipeline::get_result_table() {
+std::pair<SQLPipelineStatus, const std::shared_ptr<const Table>&> SQLPipeline::get_result_table() & {
   const auto& [pipeline_status, tables] = get_result_tables();
 
   if (pipeline_status != SQLPipelineStatus::Success) {
@@ -204,7 +204,7 @@ std::pair<SQLPipelineStatus, const std::shared_ptr<const Table>&> SQLPipeline::g
   return {SQLPipelineStatus::Success, tables.back()};
 }
 
-std::pair<SQLPipelineStatus, const std::vector<std::shared_ptr<const Table>>&> SQLPipeline::get_result_tables() {
+std::pair<SQLPipelineStatus, const std::vector<std::shared_ptr<const Table>>&> SQLPipeline::get_result_tables() & {
   if (_pipeline_status != SQLPipelineStatus::NotExecuted) {
     return {_pipeline_status, _result_tables};
   }
