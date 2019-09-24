bdf7f8eb3c6b22df9ffaa3f03b83fab01d220de2
Bouncner <martin+github@boissier.de>
2019-09-24 07:30:12 +0200

    Faster synthetic table creation (#1856)
    This PR improves the runtime of the synthetic table generator. Mainly by using std::threads (similar to the benchmarking encoder).
    
    Time to create a large table for the model calibration went from 166s to 56s on a four core laptop.
    
    It further does the following:
    
        add tests for the synthetic table generator
        add test to the chunk encoder
        fixes LZ4 to SimdBp128 vector compression (which was the idea from the beginning since LZ4 is all about compression ratios) and adapts the encodings listings accordingly
        fix a bug concerning MVCC data in the table generation

--- src/lib/storage/chunk_encoder.cpp
+++ src/lib/storage/chunk_encoder.cpp
@@ -1,6 +1,7 @@
 #include "chunk_encoder.hpp"
 
 #include <memory>
+#include <thread>
 #include <vector>
 
 #include "base_value_segment.hpp"
@@ -93,12 +94,12 @@ std::shared_ptr<BaseSegment> ChunkEncoder::encode_segment(const std::shared_ptr<
 
 void ChunkEncoder::encode_chunk(const std::shared_ptr<Chunk>& chunk, const std::vector<DataType>& column_data_types,
                                 const ChunkEncodingSpec& chunk_encoding_spec) {
-  Assert((column_data_types.size() == chunk->column_count()),
-         "Number of column types must match the chunk’s column count.");
-  Assert((chunk_encoding_spec.size() == chunk->column_count()),
+  const auto column_count = chunk->column_count();
+  Assert((column_data_types.size() == column_count), "Number of column types must match the chunk’s column count.");
+  Assert((chunk_encoding_spec.size() == column_count),
          "Number of column encoding specs must match the chunk’s column count.");
 
-  for (ColumnID column_id{0}; column_id < chunk->column_count(); ++column_id) {
+  for (auto column_id = ColumnID{0}; column_id < column_count; ++column_id) {
     const auto spec = chunk_encoding_spec[column_id];
 
     const auto data_type = column_data_types[column_id];
