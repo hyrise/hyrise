f20d861c35e1c7fba77570e2bd9cd24edbbea499
Markus Dreseler <markus.dreseler@hpi.de>
2019-09-25 11:53:46 +0200

    bla

--- src/lib/optimizer/strategy/subquery_to_join_rule.cpp
+++ src/lib/optimizer/strategy/subquery_to_join_rule.cpp
@@ -619,6 +619,7 @@ void SubqueryToJoinRule::apply_to(const std::shared_ptr<AbstractLQPNode>& node) 
     return;
   }
 
+  // TODO if semi, anti, or inner, split up into multiple joins
   const auto join_mode = predicate_node_info->join_mode;
   const auto join_node = JoinNode::make(join_mode, join_predicates);
   lqp_replace_node(node, join_node);
