fa14abfe739403faecb8408ce816b39440c9ab1d
Markus Dreseler <markus.dreseler@hpi.de>
2019-10-15 15:43:14 +0200

    odr

--- src/test/operators/jit_operator/operators/jit_filter_test.cpp
+++ src/test/operators/jit_operator/operators/jit_filter_test.cpp
@@ -1,37 +1,9 @@
 #include "base_test.hpp"
+#include "jit_mocks.hpp"
 #include "operators/jit_operator/operators/jit_expression.hpp"
 #include "operators/jit_operator/operators/jit_filter.hpp"
 
 namespace opossum {
-
-// Mock JitOperator that records whether tuples are passed to it
-class MockSink : public AbstractJittable {
- public:
-  std::string description() const final { return "MockSink"; }
-
-  void reset() const { _consume_was_called = false; }
-
-  bool consume_was_called() const { return _consume_was_called; }
-
- private:
-  void _consume(JitRuntimeContext& context) const final { _consume_was_called = true; }
-
-  // Must be static, since _consume is const
-  static bool _consume_was_called;
-};
-
-bool MockSink::_consume_was_called = false;
-
-// Mock JitOperator that passes on individual tuples
-class MockSource : public AbstractJittable {
- public:
-  std::string description() const final { return "MockSource"; }
-
-  void emit(JitRuntimeContext& context) { _emit(context); }
-
- private:
-  void _consume(JitRuntimeContext& context) const final {}
-};
 
 class JitFilterTest : public BaseTest {};
 
--- src/test/operators/jit_operator/operators/jit_validate_test.cpp
+++ src/test/operators/jit_operator/operators/jit_validate_test.cpp
@@ -5,39 +5,6 @@
 
 namespace opossum {
 
-namespace {
-
-// Mock JitOperator that records whether tuples are passed to it
-class MockSink : public AbstractJittable {
- public:
-  std::string description() const final { return "MockSink"; }
-
-  void reset() const { _consume_was_called = false; }
-
-  bool consume_was_called() const { return _consume_was_called; }
-
- private:
-  void _consume(JitRuntimeContext& context) const final { _consume_was_called = true; }
-
-  // Must be static, since _consume is const
-  static bool _consume_was_called;
-};
-
-bool MockSink::_consume_was_called = false;
-
-// Mock JitOperator that passes on individual tuples
-class MockSource : public AbstractJittable {
- public:
-  std::string description() const final { return "MockSource"; }
-
-  void emit(JitRuntimeContext& context) { _emit(context); }
-
- private:
-  void _consume(JitRuntimeContext& context) const final {}
-};
-
-}  // namespace
-
 class JitValidateTest : public BaseTest {
  public:
   JitValidateTest() {
