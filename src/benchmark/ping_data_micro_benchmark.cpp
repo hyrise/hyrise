91cc80994599e7d5697801b5314b62787b0b6d1e
Keven Richly <keven.richly@hpi.uni-potsdam.de>
2019-10-17 11:14:00 +0200

    run benchmark with multiple scan values, adds equals benchmark

--- src/benchmark/ping_data_micro_benchmark.cpp
+++ src/benchmark/ping_data_micro_benchmark.cpp
@@ -17,11 +17,22 @@ using namespace opossum::expression_functional;  // NOLINT
 namespace {
 using namespace opossum;
 
+// benchmark seetings
 constexpr auto TBL_FILE = "../../data/10mio_pings.tbl";
+//constexpr auto TBL_FILE = "../../data/100_pings.tbl";
 constexpr auto TABLE_NAME = "ping";
 const auto CHUNK_SIZES = std::vector{size_t{1000000}};
+//const auto CHUNK_SIZES = std::vector{size_t{10}};
 const auto ORDER_COLUMNS = std::vector{"captain_id", "latitude", "timestamp", "captain_status"};
 const auto CHUNK_ENCODINGS = std::vector{EncodingType::Unencoded, EncodingType::Dictionary, EncodingType::LZ4, EncodingType::RunLength};
+
+// single value benchmark values (median, min, max)
+// determined by column stats python script
+const auto BM_SCAN_VALUES = 3;
+const auto BM_VAL_CAPTAIN_ID = std::vector{298186, 59547, 1211286};
+const auto BM_VAL_CAPTAIN_STATUS = std::vector{2, 1, 2};
+const auto BM_VAL_LATITUDE = std::vector{25.1388267, 24.9154559, 25.1916198};
+const auto BM_VAL_TIMESTAMP = std::vector{"2018-12-06 23:57:34", "2018-12-22 23:22:11", "2018-11-05 05:38:15"};
 
 std::shared_ptr<Table> sort_table_chunk_wise(const std::shared_ptr<const Table>& input_table,
     const std::string order_by_column_name, const size_t chunk_size, const std::optional<ChunkEncodingSpec>& chunk_encoding_spec = std::nullopt,
@@ -132,13 +143,14 @@ static void BM_Ping_Print_Sorted_Tables(benchmark::State& state) {
 }
 BENCHMARK(BM_Ping_Print_Sorted_Tables);
 
-BENCHMARK_DEFINE_F(PingDataMicroBenchmarkFixture, BM_Keven_OrderingScanPerformance)(benchmark::State& state) {
+BENCHMARK_DEFINE_F(PingDataMicroBenchmarkFixture, BM_Keven_OrderingGreaterThanEqualsPerformance)(benchmark::State& state) {
   auto& storage_manager = Hyrise::get().storage_manager;
   
   const auto chunk_size = CHUNK_SIZES[state.range(0)];
   const auto order_by_column = ORDER_COLUMNS[state.range(1)];
   const auto encoding = CHUNK_ENCODINGS[state.range(2)];
   const auto sort_column = ORDER_COLUMNS[state.range(3)];
+
 
   const auto encoding_type = encoding_type_to_string.left.at(encoding);
   const auto table_name = get_table_name(TABLE_NAME, chunk_size, order_by_column, encoding_type);
@@ -150,10 +162,43 @@ BENCHMARK_DEFINE_F(PingDataMicroBenchmarkFixture, BM_Keven_OrderingScanPerforman
   // scan
   std::shared_ptr<BinaryPredicateExpression> predicate;
   // should by nicer dicer
-  if (state.range(3) == 0) {predicate = std::make_shared<BinaryPredicateExpression>(PredicateCondition::GreaterThanEquals, operand, value_(44046));}
-  if (state.range(3) == 1) {predicate = std::make_shared<BinaryPredicateExpression>(PredicateCondition::GreaterThanEquals, operand, value_(25.1677883));}
-  if (state.range(3) == 2) {predicate = std::make_shared<BinaryPredicateExpression>(PredicateCondition::GreaterThanEquals, operand, value_("2018-11-05 04:23:12.0000000"));}
-  if (state.range(3) == 3) {predicate = std::make_shared<BinaryPredicateExpression>(PredicateCondition::GreaterThanEquals, operand, value_(1));}
+  if (state.range(3) == 0) {predicate = std::make_shared<BinaryPredicateExpression>(PredicateCondition::GreaterThanEquals, operand, value_(BM_VAL_CAPTAIN_ID[state.range(4)]));}
+  if (state.range(3) == 1) {predicate = std::make_shared<BinaryPredicateExpression>(PredicateCondition::GreaterThanEquals, operand, value_(BM_VAL_LATITUDE[state.range(4)]));}
+  if (state.range(3) == 2) {predicate = std::make_shared<BinaryPredicateExpression>(PredicateCondition::GreaterThanEquals, operand, value_(BM_VAL_TIMESTAMP[state.range(4)]));}
+  if (state.range(3) == 3) {predicate = std::make_shared<BinaryPredicateExpression>(PredicateCondition::GreaterThanEquals, operand, value_(BM_VAL_CAPTAIN_STATUS[state.range(4)]));}
+
+  auto table_wrapper = std::make_shared<TableWrapper>(table);
+  table_wrapper->execute();
+  
+  for (auto _ : state) {
+    const auto table_scan = std::make_shared<TableScan>(table_wrapper, predicate);
+    table_scan->execute();
+  }
+}
+
+BENCHMARK_DEFINE_F(PingDataMicroBenchmarkFixture, BM_Keven_OrderingEqualsPerformance)(benchmark::State& state) {
+  auto& storage_manager = Hyrise::get().storage_manager;
+  
+  const auto chunk_size = CHUNK_SIZES[state.range(0)];
+  const auto order_by_column = ORDER_COLUMNS[state.range(1)];
+  const auto encoding = CHUNK_ENCODINGS[state.range(2)];
+  const auto sort_column = ORDER_COLUMNS[state.range(3)];
+
+
+  const auto encoding_type = encoding_type_to_string.left.at(encoding);
+  const auto table_name = get_table_name(TABLE_NAME, chunk_size, order_by_column, encoding_type);
+  
+  auto table = storage_manager.get_table(table_name);
+
+  auto operand = pqp_column_(table->column_id_by_name(sort_column), table->column_data_type(table->column_id_by_name(sort_column)), false, sort_column);
+
+  // scan
+  std::shared_ptr<BinaryPredicateExpression> predicate;
+  // should by nicer dicer
+  if (state.range(3) == 0) {predicate = std::make_shared<BinaryPredicateExpression>(PredicateCondition::Equals, operand, value_(BM_VAL_CAPTAIN_ID[state.range(4)]));}
+  if (state.range(3) == 1) {predicate = std::make_shared<BinaryPredicateExpression>(PredicateCondition::Equals, operand, value_(BM_VAL_LATITUDE[state.range(4)]));}
+  if (state.range(3) == 2) {predicate = std::make_shared<BinaryPredicateExpression>(PredicateCondition::Equals, operand, value_(BM_VAL_TIMESTAMP[state.range(4)]));}
+  if (state.range(3) == 3) {predicate = std::make_shared<BinaryPredicateExpression>(PredicateCondition::Equals, operand, value_(BM_VAL_CAPTAIN_STATUS[state.range(4)]));}
 
   auto table_wrapper = std::make_shared<TableWrapper>(table);
   table_wrapper->execute();
@@ -169,12 +214,16 @@ static void CustomArguments(benchmark::internal::Benchmark* b) {
     for (std::size_t order_by_column_id = 0; order_by_column_id < ORDER_COLUMNS.size(); ++order_by_column_id) {
       for (std::size_t encoding_id = 0; encoding_id < CHUNK_ENCODINGS.size(); ++encoding_id) {
         for (std::size_t scan_column_id = 0; scan_column_id < ORDER_COLUMNS.size(); ++scan_column_id) {
-          b->Args({static_cast<long long>(chunk_size_id),static_cast<long long>(order_by_column_id), static_cast<long long>(encoding_id), static_cast<long long>(scan_column_id)});
+          for (std::size_t scan_value_id = 0; scan_value_id < BM_SCAN_VALUES; ++scan_value_id)
+          {
+            b->Args({static_cast<long long>(chunk_size_id),static_cast<long long>(order_by_column_id), static_cast<long long>(encoding_id), static_cast<long long>(scan_column_id), static_cast<long long>(scan_value_id)});
+          }
         }
       }
     }
   }
 }
-BENCHMARK_REGISTER_F(PingDataMicroBenchmarkFixture, BM_Keven_OrderingScanPerformance)->Apply(CustomArguments);
+BENCHMARK_REGISTER_F(PingDataMicroBenchmarkFixture, BM_Keven_OrderingGreaterThanEqualsPerformance)->Apply(CustomArguments);
+BENCHMARK_REGISTER_F(PingDataMicroBenchmarkFixture, BM_Keven_OrderingEqualsPerformance)->Apply(CustomArguments);
 
 }  // namespace opossum
