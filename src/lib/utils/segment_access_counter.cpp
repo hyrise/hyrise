0787627465c14571a6a06be89a461c267de5ed5e
mapp <Mathias.Flueggen@student.hpi.de>
2019-10-18 10:59:40 +0200

    Adds SegmentAccessCounter

---
+++ src/lib/utils/segment_access_counter.cpp
@@ -0,0 +1,49 @@
+
+#include "segment_access_counter.hpp"
+#include "types.hpp"
+
+#include <fstream>
+#include <iostream>
+
+namespace opossum {
+
+const SegmentAccessCounter::AccessStatistics SegmentAccessCounter::no_statistics{};
+
+SegmentAccessCounter& SegmentAccessCounter::instance() {
+  static SegmentAccessCounter instance;
+  return instance;
+}
+
+void SegmentAccessCounter::increase(const uint32_t segment_id, AccessType type) {
+  ++_statistics[segment_id].count[type];
+}
+
+const SegmentAccessCounter::AccessStatistics& SegmentAccessCounter::statistics(const uint32_t segment_id) const {
+  const auto id_statistics_pair = _statistics.find(segment_id);
+  if (id_statistics_pair == _statistics.cend()) {
+    return SegmentAccessCounter::no_statistics;
+  }
+  return id_statistics_pair->second;
+}
+
+void SegmentAccessCounter::save_to_csv(const std::map<std::string, std::shared_ptr<Table>>& tables,
+                                       const std::string& path) const {
+  std::ofstream output_file{path};
+  // iterate over all tables, chunks and segments
+  for (const auto&[table_name, table_ptr] : tables) {
+    for (auto chunk_id = ChunkID{0}; chunk_id < table_ptr->chunk_count(); ++chunk_id) {
+      const auto chunk_ptr = table_ptr->get_chunk(chunk_id);
+      for (auto column_id = ColumnID{0}, count = static_cast<ColumnID>(chunk_ptr->column_count());
+           column_id < count; ++column_id) {
+        const auto& column_name = table_ptr->column_name(column_id);
+        const auto& segment_ptr = chunk_ptr->get_segment(column_id);
+        const auto& access_statistics = statistics(segment_ptr->id());
+        // table_name.column_name.segment_id,type1,type2,...
+        output_file << table_name << '.' << column_name << '.' << segment_ptr->id() << ','
+                    << access_statistics.to_string() << std::endl;
+      }
+    }
+  }
+}
+
+}  // namespace opossum
---
+++ src/lib/utils/segment_access_counter.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <algorithm>
+#include <array>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+
+#include "types.hpp"
+#include "storage/base_segment.hpp"
+#include "storage/table.hpp"
+
+namespace opossum {
+
+class SegmentAccessCounter : Noncopyable {
+ public:
+  enum AccessType {
+    DirectRead,
+    Append,
+    Reserve,
+    IteratorAccess,
+    IteratorPointAccess,
+    AccessorAccess,
+    ReferenceAccessorAccess,
+    Materialization,
+    // number of elements in enum
+    Count
+  };
+
+  struct AccessStatistics {
+    std::array<uint32_t, AccessType::Count> count;
+    std::string to_string() const {
+      std::string str;
+      str.reserve(AccessType::Count * 4);
+      str.append(std::to_string(count[0]));
+      for (auto it = count.cbegin() + 1, end_it = count.cend() - 1; it < end_it; ++it) {
+        str.append(",");
+        str.append(std::to_string(*it));
+      }
+      return str;
+    }
+  };
+
+  const static AccessStatistics no_statistics;
+
+  static SegmentAccessCounter& instance();
+  void increase(const uint32_t segment_id,  AccessType type);
+  void save_to_csv(const std::map<std::string, std::shared_ptr<Table>>& tables, const std::string& path) const;
+  const AccessStatistics& statistics(const uint32_t segment_id) const;
+
+ protected:
+  SegmentAccessCounter() = default;
+  ~SegmentAccessCounter() = default;
+  SegmentAccessCounter(const SegmentAccessCounter&) = delete;
+  const SegmentAccessCounter& operator=(const SegmentAccessCounter&) = delete;
+// TODO: change type og segment_id
+  std::unordered_map<uint32_t, AccessStatistics> _statistics;
+};
+
+}  // namespace opossum
