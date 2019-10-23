6f0d1f41f60207f970bbd9e0f2c1e5358dfb14da
Markus Dreseler <markus.dreseler@hpi.de>
2019-10-23 17:11:23 +0200

    awed

--- src/lib/server/use_boost_future.hpp
+++ src/lib/server/use_boost_future.hpp
@@ -12,17 +12,16 @@
 #define BOOST_ASIO_USE_BOOST_FUTURE_HPP
 
 #if defined(_MSC_VER) && (_MSC_VER >= 1200)
-# pragma once
-#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
+#pragma once
+#endif  // defined(_MSC_VER) && (_MSC_VER >= 1200)
 
 #include <boost/asio/detail/config.hpp>
 #include <boost/asio/detail/future.hpp>
 
-#if defined(BOOST_ASIO_HAS_STD_FUTURE_CLASS) \
-  || defined(GENERATING_DOCUMENTATION)
+#if defined(BOOST_ASIO_HAS_STD_FUTURE_CLASS) || defined(GENERATING_DOCUMENTATION)
 
-#include <memory>
 #include <boost/asio/detail/type_traits.hpp>
+#include <memory>
 
 #include <boost/asio/detail/push_options.hpp>
 
@@ -36,7 +35,7 @@ class boost_packaged_token;
 template <typename Function, typename Allocator, typename Result>
 class boost_packaged_handler;
 
-} // namespace detail
+}  // namespace detail
 
 /// Class used to specify that an asynchronous operation should return a future.
 /**
@@ -54,45 +53,34 @@ class boost_packaged_handler;
  * system_error and passed back to the caller via the future.
  */
 template <typename Allocator = std::allocator<void> >
-class use_boost_future_t
-{
-public:
+class use_boost_future_t {
+ public:
   /// The allocator type. The allocator is used when constructing the
   /// @c std::promise object for a given asynchronous operation.
   typedef Allocator allocator_type;
 
   /// Construct using default-constructed allocator.
-  BOOST_ASIO_CONSTEXPR use_boost_future_t()
-  {
-  }
+  BOOST_ASIO_CONSTEXPR use_boost_future_t() {}
 
   /// Construct using specified allocator.
-  explicit use_boost_future_t(const Allocator& allocator)
-    : allocator_(allocator)
-  {
-  }
+  explicit use_boost_future_t(const Allocator& allocator) : allocator_(allocator) {}
 
 #if !defined(BOOST_ASIO_NO_DEPRECATED)
   /// (Deprecated: Use rebind().) Specify an alternate allocator.
   template <typename OtherAllocator>
-  use_boost_future_t<OtherAllocator> operator[](const OtherAllocator& allocator) const
-  {
+  use_boost_future_t<OtherAllocator> operator[](const OtherAllocator& allocator) const {
     return use_boost_future_t<OtherAllocator>(allocator);
   }
-#endif // !defined(BOOST_ASIO_NO_DEPRECATED)
+#endif  // !defined(BOOST_ASIO_NO_DEPRECATED)
 
   /// Specify an alternate allocator.
   template <typename OtherAllocator>
-  use_boost_future_t<OtherAllocator> rebind(const OtherAllocator& allocator) const
-  {
+  use_boost_future_t<OtherAllocator> rebind(const OtherAllocator& allocator) const {
     return use_boost_future_t<OtherAllocator>(allocator);
   }
 
   /// Obtain allocator.
-  allocator_type get_allocator() const
-  {
-    return allocator_;
-  }
+  allocator_type get_allocator() const { return allocator_; }
 
   /// Wrap a function object in a packaged task.
   /**
@@ -114,29 +102,21 @@ public:
   template <typename Function>
 #if defined(GENERATING_DOCUMENTATION)
   unspecified
-#else // defined(GENERATING_DOCUMENTATION)
+#else   // defined(GENERATING_DOCUMENTATION)
   detail::boost_packaged_token<typename decay<Function>::type, Allocator>
-#endif // defined(GENERATING_DOCUMENTATION)
+#endif  // defined(GENERATING_DOCUMENTATION)
   operator()(BOOST_ASIO_MOVE_ARG(Function) f) const;
 
-private:
+ private:
   // Helper type to ensure that use_boost_future can be constexpr default-constructed
   // even when std::allocator<void> can't be.
-  struct std_allocator_void
-  {
-    BOOST_ASIO_CONSTEXPR std_allocator_void()
-    {
-    }
+  struct std_allocator_void {
+    BOOST_ASIO_CONSTEXPR std_allocator_void() {}
 
-    operator std::allocator<void>() const
-    {
-      return std::allocator<void>();
-    }
+    operator std::allocator<void>() const { return std::allocator<void>(); }
   };
 
-  typename conditional<
-    is_same<std::allocator<void>, Allocator>::value,
-    std_allocator_void, Allocator>::type allocator_;
+  typename conditional<is_same<std::allocator<void>, Allocator>::value, std_allocator_void, Allocator>::type allocator_;
 };
 
 /// A special value, similar to std::nothrow.
@@ -149,14 +129,14 @@ constexpr use_boost_future_t<> use_boost_future;
 __declspec(selectany) use_boost_future_t<> use_boost_future;
 #endif
 
-} // namespace asio
-} // namespace boost
+}  // namespace asio
+}  // namespace boost
 
 #include <boost/asio/detail/pop_options.hpp>
 
 #include "use_boost_future_impl.hpp"
 
-#endif // defined(BOOST_ASIO_HAS_STD_FUTURE_CLASS)
-       //   || defined(GENERATING_DOCUMENTATION)
+#endif  // defined(BOOST_ASIO_HAS_STD_FUTURE_CLASS) \
+        //   || defined(GENERATING_DOCUMENTATION)
 
-#endif // BOOST_ASIO_USE_BOOST_FUTURE_HPP
+#endif  // BOOST_ASIO_USE_BOOST_FUTURE_HPP
