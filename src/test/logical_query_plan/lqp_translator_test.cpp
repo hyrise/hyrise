270b2fdd8cb24dbbfd781dea3575357340955be8
martin.boissier <martin.boissier@hpi.de>
2019-11-18 09:49:19 +0100

    remove comment

--- src/test/logical_query_plan/lqp_translator_test.cpp
+++ src/test/logical_query_plan/lqp_translator_test.cpp
@@ -411,7 +411,6 @@ TEST_F(LQPTranslatorTest, LqpNodeAccess) {
 
     const auto lqp_node = op->get_lqp_node();
     const auto recovered_node = std::dynamic_pointer_cast<const PredicateNode>(lqp_node);
-    // TODO(anyone): is this test to compare the input and recovered nodes sufficient?
     EXPECT_EQ(recovered_node, predicate_node);
   }
 
