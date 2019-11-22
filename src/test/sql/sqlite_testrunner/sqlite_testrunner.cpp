fe39700d240c5197529469b98565e1a82191cb1b
mrks <markus.dreseler@hpi.de>
2019-11-22 05:04:39 -0800

    SQLite test runner: Print line numbers (#1934)
    By making the line number within the .sql file part of the test names, it will be easier to trace this back to the SQL query.

--- src/test/sql/sqlite_testrunner/sqlite_testrunner.cpp
+++ src/test/sql/sqlite_testrunner/sqlite_testrunner.cpp
@@ -46,8 +46,8 @@ void SQLiteTestRunner::SetUpTestCase() {
 
   _table_cache_per_encoding.emplace(EncodingType::Unencoded, unencoded_table_cache);
 
-  opossum::Hyrise::get().topology.use_numa_topology();
-  opossum::Hyrise::get().set_scheduler(std::make_shared<NodeQueueScheduler>());
+  Hyrise::get().topology.use_numa_topology();
+  Hyrise::get().set_scheduler(std::make_shared<NodeQueueScheduler>());
 }
 
 void SQLiteTestRunner::SetUp() {
@@ -129,26 +129,31 @@ void SQLiteTestRunner::SetUp() {
   }
 }
 
-std::vector<std::string> SQLiteTestRunner::queries() {
-  static std::vector<std::string> queries;
+std::vector<std::pair<size_t, std::string>> SQLiteTestRunner::queries() {
+  static std::vector<std::pair<size_t, std::string>> queries;
 
   if (!queries.empty()) return queries;
 
   std::ifstream file("resources/test_data/sqlite_testrunner_queries.sql");
   std::string query;
 
+  auto next_line = size_t{0};  // Incremented before first use
   while (std::getline(file, query)) {
+    ++next_line;
     if (query.empty() || query.substr(0, 2) == "--") continue;
-    queries.emplace_back(std::move(query));
+
+    queries.emplace_back(next_line, std::move(query));
   }
 
   return queries;
 }
 
 TEST_P(SQLiteTestRunner, CompareToSQLite) {
-  const auto [sql, encoding_type] = GetParam();
+  const auto [query_pair, encoding_type] = GetParam();
+  const auto& [line, sql] = query_pair;
 
-  SCOPED_TRACE("Query '" + sql + "' with encoding " + encoding_type_to_string.left.at(encoding_type));
+  SCOPED_TRACE("Query '" + sql + "' from line " + std::to_string(line) + " with encoding " +
+               encoding_type_to_string.left.at(encoding_type));
 
   auto sql_pipeline = SQLPipelineBuilder{sql}.create_pipeline();
 
