782d8cacb80f2f6c5c867cb29d773841fde2a26d
Markus Dreseler <markus.dreseler@hpi.de>
2019-11-29 11:10:58 +0100

    test

--- src/lib/types.hpp
+++ src/lib/types.hpp
@@ -299,8 +299,9 @@ struct hash<std::basic_string<char, std::char_traits<char>, opossum::Polymorphic
 }  // namespace std
 
 namespace boost::asio::ip {
-class tcp;
-class tcp::socket;
+class tcp {
+  class socket;
+};
 }
 
 namespace opossum {
