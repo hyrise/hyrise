102d0ce9889c5fa3f191d4e2ec4d4d173adf4cae
Markus Dreseler <markus.dreseler@hpi.de>
2019-11-30 13:53:11 +0100

    python

--- scripts/analyze_ccache_usage.py
+++ scripts/analyze_ccache_usage.py
@@ -11,15 +11,19 @@ import sys
 files = {}
 for filename in Path('src').rglob('*.ccache-log'):
   with open(filename, 'r') as file:
-    for line in file:
-      source_file_match = re.findall(r'Source file: (.*)', line)
-      if source_file_match:
-        source_file = source_file_match[0]
-      result_match = re.findall(r'Result: cache (.*)', line)
-      if result_match:
-        result = result_match[0]
-        files[source_file] = result
-        break
+    try:
+      for line in file:
+        source_file_match = re.findall(r'Source file: (.*)', line)
+        if source_file_match:
+          source_file = source_file_match[0]
+        result_match = re.findall(r'Result: cache (.*)', line)
+        if result_match:
+          result = result_match[0]
+          files[source_file] = result
+          break
+    except UnicodeDecodeError:
+      print("UnicodeDecodeError in %s" % (filename))
+      continue
 
 if len(files) == 0:
   print("No *.ccache-log files found. Did you compile with ccache and the environment variable CCACHE_DEBUG=1?")
