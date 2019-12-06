6a8404be3a2af6e01c80359f69dab75c24abaca7
dey4ss <27929897+dey4ss@users.noreply.github.com>
2019-12-06 21:13:00 +0100

    tIdY

--- src/lib/logical_query_plan/lqp_translator.cpp
+++ src/lib/logical_query_plan/lqp_translator.cpp
@@ -487,6 +487,7 @@ std::shared_ptr<AbstractOperator> LQPTranslator::_translate_drop_table_node(
   return std::make_shared<DropTable>(drop_table_node->table_name, drop_table_node->if_exists);
 }
 
+// NOLINTNEXTLINE - while this particular method could be made static, others cannot.
 std::shared_ptr<AbstractOperator> LQPTranslator::_translate_import_node(
     const std::shared_ptr<AbstractLQPNode>& node) const {
   const auto import_node = std::dynamic_pointer_cast<ImportNode>(node);
