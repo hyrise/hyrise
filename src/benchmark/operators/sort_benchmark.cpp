55d7317ed2be59497d3d555adf6d7f90eb886478
jonasnoki <jonasnoackgruen@gmail.com>
2019-12-09 22:22:49 +0100

    Add String and Null Micro-Benchmarks for Sorting.

--- src/benchmark/operators/sort_benchmark.cpp
+++ src/benchmark/operators/sort_benchmark.cpp
@@ -4,12 +4,11 @@
 #include "benchmark/benchmark.h"
 
 #include "../micro_benchmark_basic_fixture.hpp"
+#include "operators/limit.hpp"
 #include "operators/sort.hpp"
 #include "operators/table_wrapper.hpp"
 #include "synthetic_table_generator.hpp"
-#include "operators/limit.hpp"
-#include "operators/table_scan.hpp"
-#include "expression/expression_functional.hpp"
+#include "operators/join_hash.hpp"
 
 namespace opossum {
 
@@ -27,7 +26,8 @@ class SortBenchmark : public MicroBenchmarkBasicFixture {
   }
 
  protected:
-  void GenerateCustomTable(const ChunkOffset& chunk_size, const size_t& row_count, const DataType data_type = DataType::Int) {
+  void GenerateCustomTable(const ChunkOffset& chunk_size, const size_t& row_count,
+                           const DataType data_type = DataType::Int) {
     const auto table_generator = std::make_shared<SyntheticTableGenerator>();
     const int num_columns = 1;
     const int max_different_value = 10'000;
@@ -35,21 +35,18 @@ class SortBenchmark : public MicroBenchmarkBasicFixture {
 
     // We only set _table_wrapper_a because this is the only table (currently) used in the Sort Benchmarks
     _table_wrapper_a = std::make_shared<TableWrapper>(table_generator->generate_table(
-        {num_columns, {ColumnDataDistribution::make_uniform_config(0.0, max_different_value)}},
-        column_data_types,
-        row_count,
-        chunk_size,
-        std::vector<SegmentEncodingSpec>(num_columns, {EncodingType::Unencoded}),
-        std::nullopt,
+        {num_columns, {ColumnDataDistribution::make_uniform_config(0.0, max_different_value)}}, column_data_types,
+        row_count, chunk_size, std::vector<SegmentEncodingSpec>(num_columns, {EncodingType::Unencoded}), std::nullopt,
         UseMvcc::No));
     _table_wrapper_a->execute();
   }
 
-  void MakeReferenceTable(){
-    auto limit = std::make_shared<Limit>(_table_wrapper_a, expression_functional::to_expression(std::numeric_limits<int64_t>::max()));
+  void MakeReferenceTable() {
+    auto limit = std::make_shared<Limit>(_table_wrapper_a,
+                                         expression_functional::to_expression(std::numeric_limits<int64_t>::max()));
     limit->execute();
     _table_wrapper_a = std::make_shared<TableWrapper>(limit->get_output());
-
+    _table_wrapper_a->execute();
   }
 };
 
@@ -100,6 +97,63 @@ class SortReferenceLargeBenchmark : public SortLargeBenchmark {
   }
 };
 
+class SortStringSmallBenchmark : public SortBenchmark {
+ public:
+  void SetUp(benchmark::State& st) override {
+    GenerateCustomTable(ChunkOffset{2'000}, size_t{4'000}, DataType::String);
+  }
+};
+
+class SortStringBenchmark : public SortBenchmark {
+ public:
+  void SetUp(benchmark::State& st) override {
+    GenerateCustomTable(ChunkOffset{2'000}, size_t{40'000}, DataType::String);
+  }
+};
+
+class SortStringLargeBenchmark : public SortBenchmark {
+ public:
+  void SetUp(benchmark::State& st) override {
+    GenerateCustomTable(ChunkOffset{2'000}, size_t{400'000}, DataType::String);
+  }
+};
+
+class SortNullBenchmark : public SortBenchmark {
+ public:
+  void SetUp(benchmark::State& st) override {
+    const auto table_generator = std::make_shared<SyntheticTableGenerator>();
+    const int num_columns = 1;
+    const int max_different_value_a = 40'000;
+    const int max_different_value_b = 20'000;
+    const size_t chunk_size{2'000};
+    const std::vector<DataType> column_data_types = {num_columns, DataType::Int};
+
+    _table_wrapper_a = std::make_shared<TableWrapper>(table_generator->generate_table(
+        {num_columns, {ColumnDataDistribution::make_uniform_config(0.0, max_different_value_a)}}, column_data_types,
+        ChunkOffset{max_different_value_a}, chunk_size,
+        std::vector<SegmentEncodingSpec>(num_columns, {EncodingType::Unencoded}), std::nullopt, UseMvcc::No));
+    _table_wrapper_a->execute();
+
+    _table_wrapper_b = std::make_shared<TableWrapper>(table_generator->generate_table(
+        {num_columns, {ColumnDataDistribution::make_uniform_config(0.0, max_different_value_b)}}, column_data_types,
+        ChunkOffset{max_different_value_b}, chunk_size,
+        std::vector<SegmentEncodingSpec>(num_columns, {EncodingType::Unencoded}), std::nullopt, UseMvcc::No));
+    _table_wrapper_b->execute();
+
+    auto join = std::make_shared<JoinHash>(
+        _table_wrapper_a, _table_wrapper_b, JoinMode::Left,
+        OperatorJoinPredicate{ColumnIDPair(ColumnID{0}, ColumnID{0}), PredicateCondition::Equals});
+    join->execute();
+
+
+    // TODO(sorting-group): Find a more reliable way to produce an exct number of NULL values. Right now this produces
+    //  ~20% NULL Values of the bigger table
+
+    _table_wrapper_a = std::make_shared<TableWrapper>(join->get_output());
+    _table_wrapper_a->execute();
+  }
+};
+
 BENCHMARK_F(SortBenchmark, BM_Sort)(benchmark::State& state) { BM_Sort(state); }
 
 BENCHMARK_F(SortPicoBenchmark, BM_SortPico)(benchmark::State& state) { BM_Sort(state); }
@@ -115,5 +169,13 @@ BENCHMARK_F(SortReferencePicoBenchmark, BM_SortReferencePico)(benchmark::State& 
 BENCHMARK_F(SortReferenceSmallBenchmark, BM_SortReferenceSmall)(benchmark::State& state) { BM_Sort(state); }
 
 BENCHMARK_F(SortReferenceLargeBenchmark, BM_SortReferenceLarge)(benchmark::State& state) { BM_Sort(state); }
+
+BENCHMARK_F(SortStringBenchmark, BM_SortString)(benchmark::State& state) { BM_Sort(state); }
+
+BENCHMARK_F(SortStringSmallBenchmark, BM_SortStringSmall)(benchmark::State& state) { BM_Sort(state); }
+
+BENCHMARK_F(SortStringLargeBenchmark, BM_SortStringLarge)(benchmark::State& state) { BM_Sort(state); }
+
+BENCHMARK_F(SortNullBenchmark, BM_SortNullBenchmark)(benchmark::State& state) { BM_Sort(state); }
 
 }  // namespace opossum
