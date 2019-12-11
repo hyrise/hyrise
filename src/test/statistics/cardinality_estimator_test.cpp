a89b18a2c6e7f44a33f5dfa9936f5230583c70b3
Markus Dreseler <github.com@mrks.info>
2019-12-11 22:36:56 +0100

    I can't count

--- src/test/statistics/cardinality_estimator_test.cpp
+++ src/test/statistics/cardinality_estimator_test.cpp
@@ -611,11 +611,11 @@ TEST_F(CardinalityEstimatorTest, PredicateOrDoesNotIncreaseCardinality) {
 TEST_F(CardinalityEstimatorTest, PredicateIn) {
   // clang-format off
   const auto input_lqp =
-  PredicateNode::make(in_(a_a, list_(10, 11, 12, 13, 14)),
+  PredicateNode::make(in_(a_a, list_(10, 11, 12, 13)),
       node_a);
   // clang-format on
 
-  EXPECT_FLOAT_EQ(estimator.estimate_cardinality(input_lqp), 20);
+  EXPECT_FLOAT_EQ(estimator.estimate_cardinality(input_lqp), 40);
 }
 
 TEST_F(CardinalityEstimatorTest, PredicateTwoOnDifferentColumns) {
