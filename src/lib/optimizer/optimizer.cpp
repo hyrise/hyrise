323f931c0cce555d3ae5fbb1aaa1ba6f05b1312f
mrks <markus.dreseler@hpi.de>
2019-12-11 18:07:38 +0100

    Update optimizer.cpp

--- src/lib/optimizer/optimizer.cpp
+++ src/lib/optimizer/optimizer.cpp
@@ -106,8 +106,6 @@ std::shared_ptr<Optimizer> Optimizer::create_default_optimizer() {
 
   optimizer->add_rule(std::make_unique<BetweenCompositionRule>());
 
-  // Position the predicates after the JoinOrderingRule ran. The JOR manipulates predicate placement as well, but
-  // for now we want the PredicateReorderingRule to have the final say on predicate positions
   optimizer->add_rule(std::make_unique<PredicatePlacementRule>());
 
   optimizer->add_rule(std::make_unique<PredicateSplitUpRule>());
