5abc95004ca92a6936d12ce23990df6b9184f610
martin.boissier <martin.boissier@hpi.de>
2019-12-20 16:55:05 +0100

    Add missing files

---
+++ src/lib/utils/size_estimation_utils.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <random>
+
+#include "types.hpp"
+
+namespace opossum {
+
+// In case the initially preallocated capacity is too small, the string is allocated on the heap.
+template <typename T>
+size_t memory_usage_of_string_objects(const T& string_vector) {
+  // https://stackoverflow.com/questions/2037209/what-is-a-null-terminated-string
+  const auto sso_string_capacity = std::string("").capacity();
+
+  // Add up sizes for vector object and the pre-initialized strings.
+  auto bytes = string_vector.capacity() * sizeof(pmr_string);
+
+  for (const auto& v : string_vector) {
+    if (v.length() > sso_string_capacity) {
+      // Length is guaranteed to return the number of bytes, independent from any potential encoding.
+      // For each string, \0 is appended to denote the end of the string.
+      bytes += v.length() + 1;
+    }
+  }
+
+  return bytes;
+}
+
+template <typename T>
+size_t estimate_string_vector_memory_usage(const T& string_vector,
+                                           const MemoryUsageCalculationMode mode) {
+  // std::cout << "BBBBBBB" << std::endl;
+  const auto base_size = sizeof(T);
+
+  if (string_vector.empty()) return base_size;
+
+  if (mode == MemoryUsageCalculationMode::Full) {
+  	// Simple (but expensive case): the whole vector's string sizes are aggregated.
+  	// std::cout << "estimating fully" << std::endl;
+    return base_size + memory_usage_of_string_objects(string_vector);
+  }
+
+  // For the sampled estimation case, we first create a vector of sample string values and estimate that vector fully.
+  constexpr auto sampling_factor = 0.01f;
+  constexpr auto min_rows = size_t{10};
+
+  auto samples_to_draw = std::max(min_rows, static_cast<size_t>(std::ceil(sampling_factor * string_vector.size())));
+  samples_to_draw = std::min(samples_to_draw, string_vector.size());
+  const auto actual_sampling_factor = static_cast<float>(samples_to_draw) / string_vector.size();
+
+  pmr_vector<pmr_string> samples;
+  samples.reserve(samples_to_draw);
+
+  std::sample(string_vector.begin(), string_vector.end(), std::back_inserter(samples),
+              samples_to_draw, std::random_device{});
+  const auto sample_vector_memory_usage = memory_usage_of_string_objects(samples);
+
+  // std::cout << "estimating " << samples_to_draw << " samples of " << string_vector.size() << std::endl;
+
+  return base_size + static_cast<size_t>(std::ceil(sample_vector_memory_usage / actual_sampling_factor));
+}
+
+}  // namespace opossum
---
+++ src/test/utils/size_estimation_utils_test.cpp
@@ -0,0 +1,50 @@
+#include "../base_test.hpp"
+
+#include "utils/size_estimation_utils.hpp"
+#include "types.hpp"
+
+namespace opossum {
+
+// Check early out of estimation
+TEST(SizeEstimationUtilsTest, EmptyVector) {
+  const auto empty_vector = pmr_vector<pmr_string>{};
+  const auto expected_size = sizeof(pmr_vector<pmr_string>);
+
+  EXPECT_EQ(expected_size, estimate_string_vector_memory_usage(empty_vector, MemoryUsageCalculationMode::Sampled));
+  EXPECT_EQ(expected_size, estimate_string_vector_memory_usage(empty_vector, MemoryUsageCalculationMode::Full));
+}
+
+// Check correct sampling for shorter vectors than samples being taken
+TEST(SizeEstimationUtilsTest, SizeSmallerThanSampleSize) {
+  // Small vector with strings that are stored within the initial string object (SSO)
+  pmr_vector<pmr_string> small_vector{"a", "b", "c"};
+
+  const auto sso_string_size = sizeof(pmr_string);
+  const auto expected_size = sizeof(pmr_vector<pmr_string>) + small_vector.size() * sso_string_size;
+
+  EXPECT_EQ(expected_size, estimate_string_vector_memory_usage(small_vector, MemoryUsageCalculationMode::Sampled));
+  EXPECT_EQ(expected_size, estimate_string_vector_memory_usage(small_vector, MemoryUsageCalculationMode::Full));
+}
+
+TEST(SizeEstimationUtilsTest, StringVectorExceedingSSOLengths) {
+  constexpr auto large_string_length = size_t{500};
+  constexpr auto vector_length = size_t{200};
+  pmr_vector<pmr_string> string_vector{vector_length, ""};
+  string_vector[0] = std::string(large_string_length, '#');
+  string_vector[50] = std::string(large_string_length, '#');
+  string_vector[100] = std::string(large_string_length, '#');
+  string_vector[150] = std::string(large_string_length, '#');
+
+  // For the sampled method, we do not know wheter the SSO-exceeding elements will be in the taken sample. Hence, we
+  // only run a sanity check, assuming all values fit within the SSO capacity.
+  const auto expected_size_sample = sizeof(pmr_vector<pmr_string>) + vector_length * sizeof(pmr_string);
+  EXPECT_LE(expected_size_sample, estimate_string_vector_memory_usage(string_vector, MemoryUsageCalculationMode::Sampled));
+
+  // For the full estimation, we should expect an accurate measurement. Four strings should reside in separate heap
+  // allocations (+ 1 for the added \0 termination).
+  const auto expected_size_full = expected_size_sample + 4 * (large_string_length + 1);
+  EXPECT_EQ(expected_size_full, estimate_string_vector_memory_usage(string_vector, MemoryUsageCalculationMode::Full));
+  EXPECT_GT(expected_size_full * 1.05, estimate_string_vector_memory_usage(string_vector, MemoryUsageCalculationMode::Full));
+}
+
+}  // namespace opossum
