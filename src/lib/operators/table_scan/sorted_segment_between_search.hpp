a379ab239ff38b7435ca4373df41f662da475d6c
Carl Gödecken <carl.julius.g@me.com>
2019-12-21 21:30:48 +0100

    Implement find_middle_element optimization

--- src/lib/operators/table_scan/sorted_segment_between_search.hpp
+++ src/lib/operators/table_scan/sorted_segment_between_search.hpp
@@ -36,7 +36,7 @@ class SortedSegmentBetweenSearch {
 
     if (it_first == it_last) return;
     // If no null values are present
-    if (_is_nulls_first && !(*(it_first)).is_null()) {
+    if (_is_nulls_first && !it_first->is_null()) {
       return;
     }
 
@@ -69,7 +69,7 @@ class SortedSegmentBetweenSearch {
   }
 
 
-  /**
+/**
    * _get_first_bound and _get_last_bound are used to retrieve the lower and upper bound in a sorted segment but are
    * independent of its sort order. _get_first_bound will always return the bound with the smaller offset and
    * _get_last_bound will return the bigger offset.
@@ -78,69 +78,118 @@ class SortedSegmentBetweenSearch {
    * first offset will always point to an entry matching the search value, whereas last offset points to the entry
    * behind the last matching one.
    */
-  IteratorType _get_first_bound(const SearchValueType& search_value) {
+  IteratorType _get_first_bound(const SearchValueType& search_value, const IteratorType begin, const IteratorType end) {
     if (_is_ascending) {
-      auto result = std::lower_bound(_begin, _end, search_value, [](const auto& segment_position, const auto& value) {
+      return std::lower_bound(begin, end, search_value, [](const auto& segment_position, const auto& value) {
         return segment_position.value() < value;
       });
-      return result;
     } else {
-      return std::lower_bound(_begin, _end, search_value, [](const auto& segment_position, const auto& value) {
+      return std::lower_bound(begin, end, search_value, [](const auto& segment_position, const auto& value) {
         return segment_position.value() > value;
       });
     }
   }
 
-  IteratorType _get_last_bound(const SearchValueType& search_value) {
+  IteratorType _get_last_bound(const SearchValueType& search_value, const IteratorType begin, const IteratorType end) {
     if (_is_ascending) {
-      auto result = std::upper_bound(_begin, _end, search_value, [](const auto& value, const auto& segment_position) {
+      return std::upper_bound(begin, end, search_value, [](const auto& value, const auto& segment_position) {
         return segment_position.value() > value;
       });
-      return result;
     } else {
-      return std::upper_bound(_begin, _end, search_value, [](const auto& value, const auto& segment_position) {
+      return std::upper_bound(begin, end, search_value, [](const auto& value, const auto& segment_position) {
         return segment_position.value() < value;
       });
     }
+  }
+
+  IteratorType _find_middle_element() {
+    DebugAssert(_left_value <= _right_value, "_left_value must be less than or equal to _right_value");
+    // find an element with value in range [_left_value, _right_value] as a starting point for the searches
+    // also update _begin and _end accordingly
+    typename std::iterator_traits<IteratorType>::difference_type count, step;
+    count = std::distance(_begin, _end);
+
+    while (count > 0) {
+      auto it = _begin;
+      step = count / 2;
+      std::advance(it, step);
+      auto value = it->value();
+      if (_is_ascending) {
+        // if ascending, check if value is smaller than left_value
+        if (_left_value > value) {
+          // there are no larger values left of the current iterator position
+          // continue search from the next position
+          _begin = ++it;
+          count -= step + 1;
+          // otherwise, check if value is larger than right_value
+        } else if (value > _right_value) {
+          // there are no smaller values right of the current iterator position
+          // continue search up to this point
+          _end = it;
+          count = step;
+        }
+        else  // iterator value is in range [_left_value, _right_value]
+          return it;
+      } else {  // descending
+        if (_left_value > value) {
+          // there are no larger values right of the current iterator position
+          // continue search up to this point
+          _end = it;
+          count = step;
+        } else if (value > _right_value) {
+          // there are no smaller values left of the current iterator position
+          // continue search from the next position
+          _begin = ++it;
+          count -= step + 1;
+        }
+        else // iterator value is in range [_left_value, _right_value]
+          return it;
+      }
+    }
+    return _begin;
   }
 
   // This function sets the offset(s) which delimit the result set based on the predicate condition and the sort order
   void _set_begin_and_end() {
+    const IteratorType& middle = _find_middle_element();
+
+    if (_begin == _end) return;
+
     if (_is_ascending) {
       switch (_predicate_condition) {
         case PredicateCondition::BetweenInclusive:
           {
-            std::thread get_lower_bound_thread ([this](){
-              _begin = _get_first_bound(_left_value);
+            std::thread get_first_bound_thread ([this, &middle](){
+              _begin = _get_first_bound(_left_value, _begin, middle + 1);
             });
-            _end = _get_last_bound(_right_value);
-            get_lower_bound_thread.join();
+            _end = _get_last_bound(_right_value, middle, _end);
+            get_first_bound_thread.join();
           }
           return;
         case PredicateCondition::BetweenLowerExclusive:  // upper inclusive
           {
-            std::thread get_lower_bound_thread ([this](){
-              _begin = _get_last_bound(_left_value);
+            std::thread get_lower_bound_thread ([this, &middle](){
+              _begin = _get_last_bound(_left_value, _begin, middle + 1);
             });
-            _end = _get_last_bound(_right_value);
+            _end = _get_last_bound(_right_value, middle, _end);
             get_lower_bound_thread.join();
           }
           return;
         case PredicateCondition::BetweenUpperExclusive:
           {
-            std::thread get_lower_bound_thread ([this](){
-              _begin = _get_first_bound(_left_value);
+            std::thread get_lower_bound_thread ([this, &middle](){
+              _begin = _get_first_bound(_left_value, _begin, middle + 1);
             });
-            _end = _get_first_bound(_right_value);
+            _end = _get_first_bound(_right_value, middle, _end);
             get_lower_bound_thread.join();
           }
           return;
         case PredicateCondition::BetweenExclusive:
           {
-            std::thread get_lower_bound_thread ([this](){
-              _begin = _get_last_bound(_left_value);
+            std::thread get_lower_bound_thread ([this, &middle](){
+              _begin = _get_last_bound(_left_value, _begin, middle + 1);
             });
-            _end = _get_first_bound(_right_value);
+            _end = _get_first_bound(_right_value, middle, _end);
             get_lower_bound_thread.join();
           }
           return;
@@ -151,37 +200,37 @@ class SortedSegmentBetweenSearch {
       switch (_predicate_condition) {
         case PredicateCondition::BetweenInclusive:
           {
-            std::thread get_lower_bound_thread ([this](){
-              _begin = _get_first_bound(_right_value);
+            std::thread get_lower_bound_thread ([this, &middle](){
+              _begin = _get_first_bound(_right_value, _begin, middle + 1);
             });
-            _end = _get_last_bound(_left_value);
+            _end = _get_last_bound(_left_value, middle, _end);
             get_lower_bound_thread.join();
           }
           return;
         case PredicateCondition::BetweenLowerExclusive:  // upper inclusive
           {
-            std::thread get_lower_bound_thread ([this](){
-              _begin = _get_first_bound(_right_value);
+            std::thread get_lower_bound_thread ([this, &middle](){
+              _begin = _get_first_bound(_right_value, _begin, middle + 1);
             });
-            _end = _get_first_bound(_left_value);
+            _end = _get_first_bound(_left_value, middle, _end);
             get_lower_bound_thread.join();
           }
           return;
         case PredicateCondition::BetweenUpperExclusive:
           {
-            std::thread get_lower_bound_thread ([this](){
-              _begin = _get_last_bound(_right_value);
+            std::thread get_lower_bound_thread ([this, &middle](){
+              _begin = _get_last_bound(_right_value, _begin, middle + 1);
             });
-            _end = _get_last_bound(_left_value);
+            _end = _get_last_bound(_left_value, middle, _end);
             get_lower_bound_thread.join();
           }
           return;
         case PredicateCondition::BetweenExclusive:
           {
-            std::thread get_lower_bound_thread ([this](){
-              _begin = _get_last_bound(_right_value);
+            std::thread get_lower_bound_thread ([this, &middle](){
+              _begin = _get_last_bound(_right_value, _begin, middle + 1);
             });
-            _end = _get_first_bound(_left_value);
+            _end = _get_first_bound(_left_value, middle, _end);
             get_lower_bound_thread.join();
           }
           return;
