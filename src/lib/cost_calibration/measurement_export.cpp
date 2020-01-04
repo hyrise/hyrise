e2285c4b15ab6d3aa198039ffb572d3c1b194323
Lukas Böhme <lukas.boehme@icloud.com>
2020-01-04 16:44:15 +0100

    Added basic measurement iteration

--- src/lib/cost_calibration/measurement_export.cpp
+++ src/lib/cost_calibration/measurement_export.cpp
@@ -5,5 +5,86 @@
 #include "measurement_export.hpp"
 
 namespace opossum {
-    
+
+    void MeasurementSave::export_to_csv(std::shared_ptr<const AbstractOperator> op) const {
+      if (op != nullptr){
+        export_specific_operator(op);
+
+        while (op->input_left() != nullptr && op->input_right() != nullptr){
+          export_to_csv(op->input_left());
+          export_to_csv(op->input_right());
+        }
+      }
+    }
+
+    void MeasurementSave::export_specific_operator(std::shared_ptr<const AbstractOperator> op) const {
+      switch(op->type()){
+        case OperatorType::Aggregate:
+          break;
+        case OperatorType::Alias:
+          break;
+        case OperatorType::Delete:
+          break;
+        case OperatorType::Difference:
+          break;
+        case OperatorType::ExportBinary:
+          break;
+        case OperatorType::ExportCsv:
+          break;
+        case OperatorType::GetTable:
+          break;
+        case OperatorType::ImportBinary:
+          break;
+        case OperatorType::ImportCsv:
+          break;
+        case OperatorType::IndexScan:
+          break;
+        case OperatorType::Insert:
+          break;
+        case OperatorType::JoinHash:
+          break;
+        case OperatorType::JoinIndex:
+          break;
+        case OperatorType::JoinNestedLoop:
+          break;
+        case OperatorType::JoinSortMerge:
+          break;
+        case OperatorType::JoinVerification:
+          break;
+        case OperatorType::Limit:
+          break;
+        case OperatorType::Print:
+          break;
+        case OperatorType::Product:
+          break;
+        case OperatorType::Projection:
+          break;
+        case OperatorType::Sort:
+          break;
+        case OperatorType::TableScan:
+          break;
+        case OperatorType::TableWrapper:
+          break;
+        case OperatorType::UnionAll:
+          break;
+        case OperatorType::UnionPositions:
+          break;
+        case OperatorType::Update:
+          break;
+        case OperatorType::Validate:
+          break;
+        case OperatorType::CreateTable:
+          break;
+        case OperatorType::CreatePreparedPlan:
+          break;
+        case OperatorType::CreateView:
+          break;
+        case OperatorType::DropTable:
+          break;
+        case OperatorType::DropView:
+          break;
+        case OperatorType::Mock:
+          break;
+      }
+    }
 }  // namespace opossum
--- src/lib/cost_calibration/measurement_export.hpp
+++ src/lib/cost_calibration/measurement_export.hpp
@@ -2,16 +2,19 @@
 // Created by Lukas Böhme on 03.01.20.
 //
 
+#include <operators/abstract_operator.hpp>
 #include <string>
 
 namespace opossum {
 class MeasurementSave {
  public:
-  void export() const;
-  MeasurementSave(std::string path_to_dir);
+  MeasurementSave(std::string path_to_dir): path_to_dir(path_to_dir);
+
+  void export_to_csv(std::shared_ptr<const AbstractOperator> op) const;
 
  private:
 
-
+    void export_specific_operator(std::shared_ptr<const AbstractOperator> op) const;
+    const std::string path_to_dir;
 };
 }  // namespace opossum
