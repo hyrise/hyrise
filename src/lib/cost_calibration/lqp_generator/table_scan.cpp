1b2e881efc823bfe9eaec1f143cfb2a84b3153e9
Lukas Rambold <lukas@rambold.io>
2020-01-06 20:27:20 +0100

    moar features

--- src/lib/cost_calibration/lqp_generator/table_scan.cpp
+++ src/lib/cost_calibration/lqp_generator/table_scan.cpp
@@ -12,20 +12,47 @@ namespace opossum {
 
       int selectivity_steps = 10;
 
-      // TODO actually implement this
+      // TODO check whether this should be done in a different phase
       Hyrise::get().storage_manager.add_table("t_a", _table->getTable());
       const auto _t_a = StoredTableNode::make("t_a");
-      //const auto _t_a_a = StaticTableNode::make(table);
 
-      const auto _a = _t_a->get_column("_a");
-      const auto _b = _t_a->get_column("_b");
+      int column_count = _table->getTable()->column_count();
+      std::vector<std::string> column_names = _table->getTable()->column_names();
+      auto column_data_types = _table->getTable()->column_data_types();
 
-      for (int i = 0; i < selectivity_steps; i++) {
-        const auto _projection_node_a =
-                ProjectionNode::make(expression_vector(_a),
-                                     PredicateNode::make(greater_than_(_b, 10 * selectivity_steps), _t_a));
-        _lqps.emplace_back(_projection_node_a);
+      for (int j = 0; j < column_count; ++j) {
+
+        auto distribution = _table->get_column_data_distribution(static_cast<ColumnID>(j));
+
+        auto column = _t_a->get_column(column_names.at(j));
+
+          switch (column_data_types.at(j)) {
+            case DataType::Null:
+              break;
+            case DataType::String:
+              // TODO remove Projection Node
+              _lqps.push_back(ProjectionNode::make(expression_vector(column),
+                                         PredicateNode::make(greater_than_(column, "yo"), _t_a)));
+              break;
+            case DataType::Int:
+            case DataType::Long:
+            case DataType::Double:
+            case DataType::Float:
+              if (distribution.distribution_type == DataDistributionType::Uniform) {
+                // change selecticitc programatically
+//                auto step = distribution.min_value + (distribution.max_value - distribution.min_value);
+                for (int i = 0; i < selectivity_steps; i++) {
+
+                _lqps.push_back(ProjectionNode::make(expression_vector(column),
+                                           PredicateNode::make(greater_than_(column, 10), _t_a)));
+                }
+              } else {
+                _lqps.push_back(ProjectionNode::make(expression_vector(column),
+                                           PredicateNode::make(greater_than_(column, 10), _t_a)));
+              }
+              break;
+
+        }
       }
     }
-
 }
