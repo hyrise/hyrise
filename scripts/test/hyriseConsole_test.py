c03dd144b7c03583c386cdb9621f562d98b7b27f
martin.boissier <martin.boissier@hpi.de>
2020-01-09 13:32:00 +0100

    Use multi chunk binary

--- scripts/test/hyriseConsole_test.py
+++ scripts/test/hyriseConsole_test.py
@@ -43,11 +43,13 @@ def main():
 	console.expect('Encoding "test_bin" using Unencoded')	
 
 	# Reload table with a specified encoding and check meta tables for applied encoding
-	console.sendline("load resources/test_data/bin/float.bin test_bin RunLength")
-	console.expect('Loading .*bin/float.bin into table "test_bin"')
-	console.expect('Table "test_bin" already existed. Replacing it.')
-	console.expect('Encoding "test_bin" using RunLength')
-	console.sendline("select encoding_name from meta_segments where table_name='test_bin' and chunk_id=0 and column_id=0")
+	console.sendline("load resources/test_data/bin/MultipleChunkSingleFloatColumn/Dictionary.bin test_bin_multichunk")
+	console.expect('Loading .*MultipleChunkSingleFloatColumn/Dictionary.bin into table "test_bin_multichunk"')
+	console.sendline("""load resources/test_data/bin/MultipleChunkSingleFloatColumn/Dictionary.bin test_bin_multichunk RunLength""")
+	console.expect('Loading .*MultipleChunkSingleFloatColumn/Dictionary.bin into table "test_bin_multichunk"')
+	console.expect('Table "test_bin_multichunk" already existed. Replacing it.')
+	console.expect('Encoding "test_bin_multichunk" using RunLength')
+	console.sendline("""select encoding_name from meta_segments where table_name='test_bin_multichunk' and chunk_id=0 and column_id=0""")
 	console.expect('RunLength')
 
 	# Test SQL statement
