5d9ce7d06d1ae438073cf8e4dcb3612dc9a2c2bc
Jan Kossmann <jan.kossmann@hpi.de>
2020-01-09 15:20:28 +0100

    Fix

--- src/calibration/lqp_generator.cpp
+++ src/calibration/lqp_generator.cpp
@@ -1,8 +1,8 @@
-
-#include <expression/expression_functional.hpp>
-#include "logical_query_plan/stored_table_node.hpp"
-#include "logical_query_plan/predicate_node.hpp"
 #include "lqp_generator.hpp"
+
+#include "expression/expression_functional.hpp"
+#include "logical_query_plan/predicate_node.hpp"
+
 #include "calibration_table_wrapper.hpp"
 
 using namespace opossum::expression_functional;
@@ -112,9 +112,9 @@ namespace opossum {
       // own predicate
       list.emplace_back(get_predicate(std::shared_ptr<AbstractLQPNode>(table)));
       // full pos list in reference segment
-      list.emplace_back(get_predicate(PredicateNode::make(is_not_null_, table)));
+      list.emplace_back(get_predicate(PredicateNode::make(is_not_null_(column), table)));
       // empty pos list in reference segment
-      list.emplace_back(get_predicate(PredicateNode::make(is_null_, table)));
+      list.emplace_back(get_predicate(PredicateNode::make(is_null_(column), table)));
     }
 
 }
