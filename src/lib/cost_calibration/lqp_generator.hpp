d56bd5b7b87c2c39c2ff1a08d6185fbf6bcb0e18
Lukas Rambold <lukas@rambold.io>
2020-01-14 19:45:33 +0100

    removed uninteionally pasted link

--- src/lib/cost_calibration/lqp_generator.hpp
+++ src/lib/cost_calibration/lqp_generator.hpp
@@ -12,4 +12,4 @@ class LQPGenerator {
   std::vector<std::shared_ptr<AbstractLQPNode>> _generate_table_scans(const std::shared_ptr<const CalibrationTableWrapper>& table) const;
   void _add_lqps_with_reference_scans(std::vector<std::shared_ptr<AbstractLQPNode>>& list, const double lower_bound_predicate, const double upper_bound, std::shared_ptr<StoredTableNode> table, const std::string& column_name) const;
 };
-}https://www.weizenbaum-institut.de/index.php?id=38
+}
