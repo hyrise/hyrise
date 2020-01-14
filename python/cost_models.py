fbb8dc8d5c7feba1544dd7fe91628711a931c2ea
fefschumann <fefschumann@users.noreply.github.com>
2020-01-14 12:12:27 +0100

    update the model distinction on data_type and save the trained models

--- python/cost_models.py
+++ python/cost_models.py
@@ -3,16 +3,16 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 import sys
 import matplotlib.pyplot as plt
+import joblib
 
 
 # store model and metadata about the model in one object (there might be better ways, tbd)
 class CostModel:
 
-    def __init__(self, model, scan_type, encoding):
+    def __init__(self, model, encoding, data_type):
         self.model = model
-        self.scan_type = scan_type
         self.encoding = encoding
-        #self.compression = compression
+        self.data_type = data_type
 
 
 def import_test_data(tpch):
@@ -33,6 +33,7 @@ def import_test_data(tpch):
                                'DISTINCT_VALUE_COUNT', 'IS_NULLABLE',
                                'ENCODING', 'COMPRESSION', 'SIZE_IN_BYTES']]
     joined_data = joined_data.rename(columns={"TABLE_NAME_x": "TABLE_NAME", "ROW_COUNT_x": "ROW_COUNT"})
+    joined_data['SELECTIVITY'] = (joined_data['OUTPUT_ROWS'] / joined_data['INPUT_ROWS'])
 
     return joined_data
 
@@ -51,8 +52,9 @@ def import_train_data(input_data):
     joined_data = joined_data.merge(columns_data, on="TABLE_NAME", how="left")
     joined_data = joined_data.rename(columns={"INPUT_ROWS_LEFT": "INPUT_ROWS", "CHUNK_SIZE": "MAX_CHUNK_SIZE",
                                               "COLUMN_DATA_TYPE": "DATA_TYPE", "ENCODING_TYPE": "ENCODING",
-                                              "TABLE_NAME_x": "TABLE_NAME", "COLUMN_NAME_y": "COLUMN_NAME",
+                                              "TABLE_NAME": "TABLE_NAME", "COLUMN_NAME": "COLUMN_NAME",
                                               "COLUMN_DATA_TYPE": "DATA_TYPE", "COLUMN_TYPE": "SCAN_TYPE"})
+    joined_data['SELECTIVITY'] = (joined_data['OUTPUT_ROWS'] / joined_data['INPUT_ROWS'])
 
     return joined_data
 
@@ -73,7 +75,7 @@ def train_model(train_data):
 
     ohe_data = train_data.drop(labels=['TABLE_NAME', 'COLUMN_NAME'], axis=1)
     ohe_data = pd.get_dummies(ohe_data, columns=['SCAN_TYPE', 'DATA_TYPE', 'ENCODING'])
-
+    print('train ' + ohe_data.columns)
     y = ohe_data[['RUNTIME_NS']]
     X = ohe_data.drop(labels=['RUNTIME_NS'], axis=1)
     model = LinearRegression().fit(X, y)
@@ -92,7 +94,7 @@ def generate_output(costmodel, test_data, out):
     ohe_data['DATA_TYPE_long'] = 0
     ohe_data['DATA_TYPE_string'] = 0
     ohe_data['DATA_TYPE_double'] = 0
-
+    print('predict ' + ohe_data.columns)
     y_true = ohe_data[['RUNTIME_NS']]
     ohe_data = ohe_data.drop(labels=['RUNTIME_NS'], axis=1)
     y_pred = costmodel.model.predict(ohe_data)
@@ -102,8 +104,12 @@ def generate_output(costmodel, test_data, out):
     # print and put the plots in the output folder
     fig = plt.figure()
     plt.boxplot(error)
-    output_path = '{}error_model_{}_{}'.format(out,costmodel.scan_type, costmodel.encoding)
+    output_path = '{}error_model_{}_{}'.format(out, costmodel.data_type, costmodel.encoding)
     fig.savefig(output_path, bbox_inches='tight')
+
+    f = plt.figure()
+    #plt.scatter(scatter_data)
+    plt.show()
     return mse
 
 
@@ -131,23 +137,24 @@ def main():
 
     # also keep one 'universal' model in case some thing comes up, that we don't have a specific model for?
     u_cost_model = train_model(train_data)
-    cost_models.append(CostModel(u_cost_model, scan_type='None', encoding='None'))
+    cost_models.append(CostModel(u_cost_model, data_type='None', encoding='None'))
     # make models for different scan operators and combinations of encodings/compressions
     test_data_splitted = []
     leftover_test_data = []
-    for scan_type in train_data['SCAN_TYPE'].unique():
-        for encoding in train_data['ENCODING'].unique():
-            #for compression in train_data['COMPRESSION'].unique():
+    for encoding in train_data['ENCODING'].unique():
+        for data_type in train_data['DATA_TYPE'].unique():
 
-            model_train_data = train_data.loc[(train_data['SCAN_TYPE'] == scan_type) & (train_data['ENCODING'] == encoding)]
-            model_test_data = test_data.loc[(test_data['SCAN_TYPE'] == scan_type) & (test_data['ENCODING'] == encoding)]
+            model_train_data = train_data.loc[(train_data['DATA_TYPE'] == data_type) & (train_data['ENCODING'] == encoding)]
+            model_test_data = test_data.loc[(test_data['DATA_TYPE'] == data_type) & (test_data['ENCODING'] == encoding)]
             # if we have train data for this combination, train a model
 
             if not model_train_data.empty:
                 model = train_model(model_train_data)
 
-                cost_model = CostModel(model, scan_type, encoding)
+                cost_model = CostModel(model, encoding, data_type)
                 cost_models.append(cost_model)
+                filename = 'models/{}{}_model.sav'.format(data_type, encoding)
+                joblib.dump(model, filename)
 
                 # if there is no test_data for this combination, continue
                 if model_test_data.empty:
@@ -156,14 +163,11 @@ def main():
 
                 # predict the runtime for the queries of the test_data that correspond to this model and calculate the
                 # error (SSE)
-                model_mse = generate_output(cost_model, model_test_data, output_folder)
+                #model_mse = generate_output(cost_model, model_test_data, output_folder)
 
             # if we don't have a model for this combination but test_data, add the test_data to test on the universal model
             if not model_test_data.empty:
                 leftover_test_data.append(model_test_data)
-
-            # testing:
-            print(model_mse)
 
 
     # umodel_mse = generate_output(u_cost_model, leftover_test_data, output_folder)
