6dad1061518af764dbfceab64eec07ca5ff3c0e9
Daniel Lindner <27929897+dey4ss@users.noreply.github.com>
2020-01-15 19:15:07 +0100

    remove double table existence check

--- src/lib/sql/sql_translator.cpp
+++ src/lib/sql/sql_translator.cpp
@@ -1219,11 +1219,7 @@ std::shared_ptr<AbstractLQPNode> SQLTranslator::_translate_import(const hsql::Im
 
 // NOLINTNEXTLINE - while this particular method could be made static, others cannot.
 std::shared_ptr<AbstractLQPNode> SQLTranslator::_translate_export(const hsql::ExportStatement& export_statement) {
-  // Make sure the table exists
-  AssertInput(Hyrise::get().storage_manager.has_table(export_statement.tableName),
-              "There is no table '" + export_statement.tableName + "'.");
-
-  // Create a ValidateNode
+  // Get validated table as input
   auto sql_identifier_resolver = std::make_shared<SQLIdentifierResolver>();
   auto lqp = _translate_stored_table(export_statement.tableName, sql_identifier_resolver);
 
