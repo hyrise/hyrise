3ffcae59712065427f87a2969c5deca4db3d66d3
martin.boissier <martin.boissier@hpi.de>
2020-01-17 11:32:18 +0100

    Remove outdated comment

--- src/bin/playground.cpp
+++ src/bin/playground.cpp
@@ -4,12 +4,13 @@
 using namespace opossum;  // NOLINT
 
 int main(int argc, const char* argv[]) {
-  if (argc > 1) {  // first argument is benchmark
+  if (argc > 1) {
     for (auto plugin_id = 1; plugin_id < argc; ++plugin_id) {
       const std::filesystem::path plugin_path(argv[plugin_id]);
       const auto plugin_name = plugin_name_from_path(plugin_path);
       Hyrise::get().plugin_manager.load_plugin(plugin_path);
     }
   }
+
   return 0;
 }
