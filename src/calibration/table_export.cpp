9b2e9d83e0f335f3057955c1fa6bd2d521999247
Lukas Böhme <lukas.boehme@icloud.com>
2020-01-21 17:06:28 +0100

    Added chunk export

--- src/calibration/table_export.cpp
+++ src/calibration/table_export.cpp
@@ -1,20 +1,22 @@
-
-#include <storage/base_encoded_segment.hpp>
 #include "string"
 #include "fstream"
 
+#include <storage/base_encoded_segment.hpp>
 #include "table_export.hpp"
+#include "storage/vector_compression/compressed_vector_type.hpp"
 
 namespace opossum {
     TableExport::TableExport(const std::string &path_to_dir) : _path_to_dir(path_to_dir) {
       //TODO Check if file already exists;
       _create_table_meta_file();
       _create_column_meta_file();
+      _create_chunk_meta_file();
     }
 
     void TableExport::export_table(std::shared_ptr<const CalibrationTableWrapper> table_wrapper) const {
         _append_to_file(_get_table_meta_relative_path(), _export_table_meta_data(table_wrapper));
         _append_to_file(_get_column_meta_relative_path(), _export_column_meta_data(table_wrapper));
+        _append_to_file(_get_chunk_meta_relative_path(), _export_chunk_meta_data(table_wrapper));
     }
 
     std::string TableExport::_get_table_meta_header() const {
@@ -38,12 +40,28 @@ namespace opossum {
       return column_meta_header.str();
     }
 
+    std::string TableExport::_get_chunk_meta_header() const {
+      std::stringstream chunk_meta_header;
+
+      chunk_meta_header << "TABLE_NAME" << _separator;
+      chunk_meta_header << "COLUMN_NAME" << _separator;
+      chunk_meta_header << "CHUNK_ID" << _separator;
+      chunk_meta_header << "ENCODING_TYPE" << "\n";
+
+      return chunk_meta_header.str();
+    }
+
+    //TODO Rewrite this
     void TableExport::_create_table_meta_file() const {
       _append_to_file(_get_table_meta_relative_path(), _get_table_meta_header());
     }
 
     void TableExport::_create_column_meta_file() const {
       _append_to_file(_get_column_meta_relative_path(), _get_column_meta_header());
+    }
+
+    void TableExport::_create_chunk_meta_file() const {
+      _append_to_file(_get_chunk_meta_relative_path(), _get_chunk_meta_header());
     }
 
     void TableExport::_append_to_file(const std::string& path,const std::string& str) const {
@@ -61,6 +79,11 @@ namespace opossum {
     const std::string TableExport::_get_column_meta_relative_path() const {
       return _path_to_dir + "/" + _column_meta_file_name + ".csv";
     }
+
+    const std::string TableExport::_get_chunk_meta_relative_path() const {
+      return _path_to_dir + "/" + _chunk_meta_file_name + ".csv";
+    }
+
 
     std::string TableExport::_export_table_meta_data(std::shared_ptr<const CalibrationTableWrapper> table_wrapper) const {
       std::stringstream ss;
@@ -87,10 +110,54 @@ namespace opossum {
         auto const segment =  table->get_chunk(ChunkID {0})->get_segment(column_id);
         auto const encoded_segment = std::dynamic_pointer_cast<BaseEncodedSegment>(segment);
 
-        auto const encoded_type_string = encoded_segment != nullptr ? encoded_segment->encoding_type() : EncodingType::Unencoded; //TODO in if and loop though chunk_id
+        auto const encoded_type_string = encoded_segment != nullptr ? encoded_segment->encoding_type() : EncodingType::Unencoded; //TODO remove this as soon we know how to use encoding
         ss << encoded_type_string << "\n";
       }
 
       return ss.str();
     }
+
+    std::string TableExport::_export_chunk_meta_data(std::shared_ptr<const CalibrationTableWrapper> table_wrapper) const {
+      std::stringstream ss;
+      const auto table = table_wrapper->get_table();
+      const auto table_name = table_wrapper->get_name();
+      const auto chunk_count = table->chunk_count();
+      const auto column_count = table->column_count();
+
+      for (ColumnID column_id{0}; column_id < column_count; ++column_id){
+        const auto column_name = table->column_name(column_id);
+
+        for (ChunkID chunk_id{0}; chunk_id < chunk_count; ++chunk_id) {
+          ss << table_name << _separator;
+          ss << column_name << _separator;
+          ss << chunk_id << _separator;
+
+          auto const segment =  table->get_chunk(chunk_id)->get_segment(column_id);
+
+          // if segment is encoded write out values
+          if (const auto encoded_segment = std::dynamic_pointer_cast<BaseEncodedSegment>(segment)){
+            // Encoding Type
+            ss << encoded_segment->encoding_type() << _separator;
+
+            // Compressed Vector Type
+            if (const auto compressed_vector_type = encoded_segment->compressed_vector_type()){
+              ss << *compressed_vector_type << "\n";
+            } else {
+              ss << "null" << "\n";
+            }
+
+          // if segment is not encoded write default values for chunk;
+          } else {
+            // Encoding Type
+            ss << EncodingType::Unencoded << _separator;
+
+            // Compressed Vector Type
+            ss << "null" << "\n";  //FixedSize4ByteAligned is uncompressed
+          }
+        }
+      }
+      return ss.str();
+    }
+
+
 }
--- src/calibration/table_export.hpp
+++ src/calibration/table_export.hpp
@@ -14,20 +14,25 @@ private:
 
   const std::string _table_meta_file_name = "table_meta";
   const std::string _column_meta_file_name = "column_meta";
+  const std::string _chunk_meta_file_name = "chunk_meta";
 
   const std::string _separator = ",";
 
   std::string _get_table_meta_header() const;
   std::string _get_column_meta_header() const;
+  std::string _get_chunk_meta_header() const;
 
   const std::string _get_table_meta_relative_path() const ;
   const std::string _get_column_meta_relative_path() const;
+  const std::string _get_chunk_meta_relative_path() const;
 
   void _create_table_meta_file() const;
   void _create_column_meta_file() const;
+  void _create_chunk_meta_file() const;
 
   std::string _export_table_meta_data(std::shared_ptr<const CalibrationTableWrapper> table_wrapper) const;
   std::string _export_column_meta_data(std::shared_ptr<const CalibrationTableWrapper> table_wrapper) const ;
+  std::string _export_chunk_meta_data(std::shared_ptr<const CalibrationTableWrapper> table_wrapper) const ;
 
   void _append_to_file(const std::string &path, const std::string &str) const;
 };
