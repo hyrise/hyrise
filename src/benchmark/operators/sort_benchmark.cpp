4b16c695a4bb701cadead86e35612d71ee28391f
bakoe <ba.koe96@gmail.com>
2020-02-08 15:05:48 +0000

    Fix non-explicit SortColumnDefinition constructor

--- src/benchmark/operators/sort_benchmark.cpp
+++ src/benchmark/operators/sort_benchmark.cpp
@@ -11,7 +11,8 @@ namespace opossum {
 BENCHMARK_F(MicroBenchmarkBasicFixture, BM_Sort)(benchmark::State& state) {
   _clear_cache();
 
-  const auto sort_definitions = std::vector<SortColumnDefinition>{{ColumnID{0}, OrderByMode::Ascending}};
+  const auto sort_definitions =
+      std::vector<SortColumnDefinition>{{SortColumnDefinition{ColumnID{0}, OrderByMode::Ascending}}};
 
   auto warm_up = std::make_shared<Sort>(_table_wrapper_a, sort_definitions);
   warm_up->execute();
