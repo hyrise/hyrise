89160260e920d01656b4242242a18b5211844b47
martin.boissier <martin.boissier@hpi.de>
2020-02-10 11:43:22 +0100

    Lint and format

--- src/lib/storage/vector_compression/simd_bp128/simd_bp128_iterator.hpp
+++ src/lib/storage/vector_compression/simd_bp128/simd_bp128_iterator.hpp
@@ -1,4 +1,4 @@
-#pragma once  
+#pragma once
 
 #include <array>
 #include <memory>
@@ -6,8 +6,8 @@
 #include "storage/vector_compression/base_compressed_vector.hpp"
 
 #include "oversized_types.hpp"
-#include "simd_bp128_packing.hpp"
 #include "simd_bp128_decompressor.hpp"
+#include "simd_bp128_packing.hpp"
 
 #include "types.hpp"
 #include "utils/performance_warning.hpp"
@@ -19,17 +19,15 @@ class SimdBp128Iterator : public BaseCompressedVectorIterator<SimdBp128Iterator>
   using Packing = SimdBp128Packing;
 
  public:
-  SimdBp128Iterator(std::unique_ptr<SimdBp128Decompressor>&& decompressor, const size_t absolute_index = 0u)
-  : _decompressor{std::move(decompressor)},
-    _absolute_index{absolute_index} {}
+  explicit SimdBp128Iterator(std::unique_ptr<SimdBp128Decompressor>&& decompressor, const size_t absolute_index = 0u)
+      : _decompressor{std::move(decompressor)}, _absolute_index{absolute_index} {}
 
-  SimdBp128Iterator(const SimdBp128Iterator& other)
-  : _decompressor{std::make_unique<SimdBp128Decompressor>(SimdBp128Decompressor(*other._decompressor))},
-    _absolute_index{other._absolute_index} {}
+  explicit SimdBp128Iterator(const SimdBp128Iterator& other)
+      : _decompressor{std::make_unique<SimdBp128Decompressor>(SimdBp128Decompressor(*other._decompressor))},
+        _absolute_index{other._absolute_index} {}
 
-  SimdBp128Iterator(SimdBp128Iterator&& other)
-  : _decompressor{std::move(other._decompressor)},
-    _absolute_index{std::move(other._absolute_index)} {}
+  explicit SimdBp128Iterator(SimdBp128Iterator&& other)
+      : _decompressor{std::move(other._decompressor)}, _absolute_index{std::move(other._absolute_index)} {}
 
   SimdBp128Iterator& operator=(const SimdBp128Iterator& other) {
     if (this == &other) return *this;
@@ -46,29 +44,17 @@ class SimdBp128Iterator : public BaseCompressedVectorIterator<SimdBp128Iterator>
  private:
   friend class boost::iterator_core_access;  // grants the boost::iterator_facade access to the private interface
 
-  void increment() {
-    ++_absolute_index;
-  }
+  void increment() { ++_absolute_index; }
 
-  void decrement() {
-    --_absolute_index;
-  }
+  void decrement() { --_absolute_index; }
 
-  void advance(std::ptrdiff_t n) {
-    _absolute_index += n;
-  }
+  void advance(std::ptrdiff_t n) { _absolute_index += n; }
 
-  bool equal(const SimdBp128Iterator& other) const {
-    return _absolute_index == other._absolute_index;
-  }
+  bool equal(const SimdBp128Iterator& other) const { return _absolute_index == other._absolute_index; }
 
-  std::ptrdiff_t distance_to(const SimdBp128Iterator& other) const {
-    return other._absolute_index - _absolute_index;
-  }
+  std::ptrdiff_t distance_to(const SimdBp128Iterator& other) const { return other._absolute_index - _absolute_index; }
 
-  uint32_t dereference() const {
-    return _decompressor->get(_absolute_index);
-  }
+  uint32_t dereference() const { return _decompressor->get(_absolute_index); }
 
  private:
   std::unique_ptr<SimdBp128Decompressor> _decompressor;
