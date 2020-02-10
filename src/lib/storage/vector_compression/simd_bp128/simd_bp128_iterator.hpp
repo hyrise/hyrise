b2c045779e694ba8019f848a1061df3b7d62eb6b
Bouncner <martin+github@boissier.de>
2020-02-10 15:29:38 +0100

    I have no effin' idea why the excess explicit did cause such scary errors

--- src/lib/storage/vector_compression/simd_bp128/simd_bp128_iterator.hpp
+++ src/lib/storage/vector_compression/simd_bp128/simd_bp128_iterator.hpp
@@ -19,14 +19,14 @@ class SimdBp128Iterator : public BaseCompressedVectorIterator<SimdBp128Iterator>
   using Packing = SimdBp128Packing;
 
  public:
-  explicit SimdBp128Iterator(std::unique_ptr<SimdBp128Decompressor>&& decompressor, const size_t absolute_index = 0u)
+  SimdBp128Iterator(std::unique_ptr<SimdBp128Decompressor>&& decompressor, const size_t absolute_index = 0u)
       : _decompressor{std::move(decompressor)}, _absolute_index{absolute_index} {}
 
   explicit SimdBp128Iterator(const SimdBp128Iterator& other)
       : _decompressor{std::make_unique<SimdBp128Decompressor>(SimdBp128Decompressor(*other._decompressor))},
         _absolute_index{other._absolute_index} {}
 
-  explicit SimdBp128Iterator(SimdBp128Iterator&& other)
+  SimdBp128Iterator(SimdBp128Iterator&& other)
       : _decompressor{std::move(other._decompressor)}, _absolute_index{std::move(other._absolute_index)} {}
 
   SimdBp128Iterator& operator=(const SimdBp128Iterator& other) {
