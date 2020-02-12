25c40442f88d6dbd11025938885f4aadb7cc488c
Richard Ebeling <He3lixxx@users.noreply.github.com>
2020-02-12 17:25:19 +0000

    Code.

--- src/lib/storage/abstract_pos_list.cpp
+++ src/lib/storage/abstract_pos_list.cpp
@@ -13,8 +13,16 @@ namespace opossum {
 
   template <>
   AbstractPosList::PosListIterator<false>::DereferenceReturnType AbstractPosList::PosListIterator<false>::dereference() const {
+    // As of 986b98ce, we still come by here very often, even if we're only creating PosList instances in the code
+    // -> Somewhere, the type is not resolved.
+    /* std::cout << "Slow dereference\n"; */
+
     DebugAssert(_chunk_offset < _max_size, "You foool");
 
+#if 1
     return (*_pl_pointer)[_chunk_offset];
+#else
+    return static_cast<const PosList&>(*_pl_pointer)[_chunk_offset];
+#endif
   }
 }  // namespace opossum
