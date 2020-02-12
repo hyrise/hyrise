716ad8443dad7aa949bbb3d4d318a4166ce1a95c
bakoe <ba.koe96@gmail.com>
2020-02-12 19:21:43 +0000

    Fix Lint offenses in pos_list_utils.hpp

--- src/lib/utils/pos_list_utils.hpp
+++ src/lib/utils/pos_list_utils.hpp
@@ -8,7 +8,8 @@
 
 namespace opossum {
 
-// TODO Add comment
-std::shared_ptr<std::vector<std::shared_ptr<PosList>>> split_pos_list(const std::shared_ptr<PosList>& pos_list, size_t number_of_parts);
+// TODO(anyone): Add comment
+std::shared_ptr<std::vector<std::shared_ptr<PosList>>> split_pos_list(const std::shared_ptr<PosList>& pos_list,
+                                                                      size_t number_of_parts);
 
 }  // namespace opossum
