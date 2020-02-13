67a6cd680bee7fb9bc9b07fc95f8299df95d50b1
birneamstiel <luckifri@yahoo.de>
2020-02-13 21:02:48 +0100

    Add documentation.

--- src/lib/utils/pos_list_utils.hpp
+++ src/lib/utils/pos_list_utils.hpp
@@ -8,7 +8,10 @@
 
 namespace opossum {
 
-// TODO(anyone): Add comment
+/**
+ * Splits up a PosList into n PosLists of equal length. If PosList length isn't dividable into n equal parts without
+ * remainder, the last PosList will have fewer elements.  
+ */
 std::shared_ptr<std::vector<std::shared_ptr<PosList>>> split_pos_list(const std::shared_ptr<PosList>& pos_list,
                                                                       size_t number_of_parts);
 
