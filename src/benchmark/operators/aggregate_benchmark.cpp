d6ee4b8f39a1530f58bb46170e4570deae6744dd
Hendrik Tjabben <hendrik.tjabben@gmail.com>
2020-02-20 17:02:52 +0000

    Add more microbenchmarks for aggregate sort with different group_bys and pre-sorted as well as non-pre-sorted columns

--- src/benchmark/operators/aggregate_benchmark.cpp
+++ src/benchmark/operators/aggregate_benchmark.cpp
@@ -30,7 +30,99 @@ BENCHMARK_F(MicroBenchmarkBasicFixture, BM_Aggregate)(benchmark::State& state) {
   }
 }
 
-BENCHMARK_F(MicroBenchmarkBasicFixture, BM_AggregateSort)(benchmark::State& state) {
+BENCHMARK_F(MicroBenchmarkBasicFixture, BM_AggregateSortNotSortedNoGroupBy)(benchmark::State& state) {
+  _clear_cache();
+
+  auto aggregates = std::vector<std::shared_ptr<AggregateExpression>>{
+    std::static_pointer_cast<AggregateExpression>(min_(pqp_column_(ColumnID{1}, DataType::Int, false, "b")))};
+
+  std::vector<ColumnID> groupby = {};
+
+  auto warm_up = std::make_shared<AggregateSort>(_table_wrapper_a, aggregates, groupby);
+  warm_up->execute();
+  for (auto _ : state) {
+    auto aggregate = std::make_shared<AggregateSort>(_table_wrapper_a, aggregates, groupby);
+    aggregate->execute();
+  }
+}
+
+BENCHMARK_F(MicroBenchmarkBasicFixture, BM_AggregateSortSortedNoGroupBy)(benchmark::State& state) {
+  _clear_cache();
+
+  auto aggregates = std::vector<std::shared_ptr<AggregateExpression>>{
+    std::static_pointer_cast<AggregateExpression>(min_(pqp_column_(ColumnID{1}, DataType::Int, false, "b")))};
+
+  std::vector<ColumnID> groupby = {};
+
+  const auto sort = std::make_shared<Sort>(_table_wrapper_a, ColumnID{1});
+  sort->execute();
+
+  auto table_wrapper_sorted = std::make_shared<TableWrapper>(sort->get_output());
+  table_wrapper_sorted->execute();
+
+  auto warm_up = std::make_shared<AggregateSort>(_table_wrapper_a, aggregates, groupby);
+  warm_up->execute();
+  for (auto _ : state) {
+    auto aggregate = std::make_shared<AggregateSort>(table_wrapper_sorted, aggregates, groupby);
+    aggregate->execute();
+  }
+}
+
+BENCHMARK_F(MicroBenchmarkBasicFixture, BM_AggregateSortNotSortedOneGroupBy)(benchmark::State& state) {
+  _clear_cache();
+
+  auto aggregates = std::vector<std::shared_ptr<AggregateExpression>>{
+    std::static_pointer_cast<AggregateExpression>(min_(pqp_column_(ColumnID{1}, DataType::Int, false, "b")))};
+
+  std::vector<ColumnID> groupby = {ColumnID{1}};
+
+  auto warm_up = std::make_shared<AggregateSort>(_table_wrapper_a, aggregates, groupby);
+  warm_up->execute();
+  for (auto _ : state) {
+    auto aggregate = std::make_shared<AggregateSort>(_table_wrapper_a, aggregates, groupby);
+    aggregate->execute();
+  }
+}
+
+BENCHMARK_F(MicroBenchmarkBasicFixture, BM_AggregateSortSortedOneGroupBy)(benchmark::State& state) {
+  _clear_cache();
+
+  auto aggregates = std::vector<std::shared_ptr<AggregateExpression>>{
+    std::static_pointer_cast<AggregateExpression>(min_(pqp_column_(ColumnID{1}, DataType::Int, false, "b")))};
+
+  std::vector<ColumnID> groupby = {ColumnID{1}};
+
+  const auto sort = std::make_shared<Sort>(_table_wrapper_a, ColumnID{1});
+  sort->execute();
+
+  auto table_wrapper_sorted = std::make_shared<TableWrapper>(sort->get_output());
+  table_wrapper_sorted->execute();
+
+  auto warm_up = std::make_shared<AggregateSort>(_table_wrapper_a, aggregates, groupby);
+  warm_up->execute();
+  for (auto _ : state) {
+    auto aggregate = std::make_shared<AggregateSort>(table_wrapper_sorted, aggregates, groupby);
+    aggregate->execute();
+  }
+}
+
+BENCHMARK_F(MicroBenchmarkBasicFixture, BM_AggregateSortNotSortedMultipleGroupBy)(benchmark::State& state) {
+  _clear_cache();
+
+  auto aggregates = std::vector<std::shared_ptr<AggregateExpression>>{
+    std::static_pointer_cast<AggregateExpression>(min_(pqp_column_(ColumnID{1}, DataType::Int, false, "b")))};
+
+  std::vector<ColumnID> groupby = {ColumnID{0}, ColumnID{1}};
+
+  auto warm_up = std::make_shared<AggregateSort>(_table_wrapper_a, aggregates, groupby);
+  warm_up->execute();
+  for (auto _ : state) {
+    auto aggregate = std::make_shared<AggregateSort>(_table_wrapper_a, aggregates, groupby);
+    aggregate->execute();
+  }
+}
+
+BENCHMARK_F(MicroBenchmarkBasicFixture, BM_AggregateSortSortedMultipleGroupBy)(benchmark::State& state) {
   _clear_cache();
 
   auto aggregates = std::vector<std::shared_ptr<AggregateExpression>>{
