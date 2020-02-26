c07b8a71929cd9a439118afe4583d4732f12cb5f
Markus Dreseler <markus.dreseler@hpi.de>
2020-02-26 16:07:29 +0100

    bla

--- scripts/varying_chunk_size.py
+++ scripts/varying_chunk_size.py
@@ -4,4 +4,4 @@ import subprocess
 
 for log_chunk_count in range(1, 21): # up to 1M chunks
   chunk_count = 2 ** log_chunk_count
-  subprocess.run(["./hyriseBenchmarkTPCH", "-c", str(chunk_count), "-o", str(chunk_count) + "chunks.json"])
+  subprocess.run(["./hyriseBenchmarkTPCH", "-c", str(chunk_count), "-o", str(chunk_count) + "chunks.json", "--query_path=/home/Jan.Kossmann/hyrise_partitioning/resources/benchmark/acdoca/queries", "--table_path=/home/Jan.Kossmann/hyrise_partitioning/resources/benchmark/acdoca/tables/10M"])
