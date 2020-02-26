d36dd5dcae1037c07dd400fffd0e36930831fe2d
Markus Dreseler <markus.dreseler@hpi.de>
2020-02-26 16:09:40 +0100

    bla

--- scripts/varying_chunk_size.py
+++ scripts/varying_chunk_size.py
@@ -2,6 +2,6 @@
 
 import subprocess
 
-for log_chunk_count in range(1, 21): # up to 1M chunks
-  chunk_count = 2 ** log_chunk_count
-  subprocess.run(["./hyriseBenchmarkTPCH", "-c", str(chunk_count), "-o", str(chunk_count) + "chunks.json", "--query_path=/home/Jan.Kossmann/hyrise_partitioning/resources/benchmark/acdoca/queries", "--table_path=/home/Jan.Kossmann/hyrise_partitioning/resources/benchmark/acdoca/tables/10M"])
+for log_chunk_size in range(8, 25): # from 256 to 16M rows per chunk
+  chunk_size = 2 ** log_chunk_size
+  subprocess.run(["./hyriseBenchmarkFileBased", "-c", str(chunk_size), "-o", "chunksize_" + str(chunk_size) + ".json", "--query_path=/home/Jan.Kossmann/hyrise_partitioning/resources/benchmark/acdoca/queries", "--table_path=/home/Jan.Kossmann/hyrise_partitioning/resources/benchmark/acdoca/tables/10M"])
