62752b88386672c08b2828c118a31b7af713476c
Markus Dreseler <markus.dreseler@hpi.de>
2020-02-26 16:15:45 +0100

    bla

--- scripts/varying_chunk_size.py
+++ scripts/varying_chunk_size.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
-import subprocess
+import subprocess, os
 
-for log_chunk_size in range(8, 25): # from 256 to 16M rows per chunk
-  chunk_size = 2 ** log_chunk_size
-  subprocess.run(["./hyriseBenchmarkFileBased", "-c", str(chunk_size), "-o", "chunksize_" + str(chunk_size) + ".json", "--query_path=/home/Jan.Kossmann/hyrise_partitioning/resources/benchmark/acdoca/queries", "--table_path=/home/Jan.Kossmann/hyrise_partitioning/resources/benchmark/acdoca/tables/10M"])
+for log_chunk_count in range(1, 21): # up to 1M chunks
+  chunk_count = 2**log_chunk_count
+
+  env = os.environ.copy()
+  env["PARTITIONING"] = "partitioning.json"
+
+  print("""
+{
+  "ACDOCA" : [
+        {"column_name": "DOCNR", "partitions": %i, "mode": "size"},
+  ]
+}
+    """ % (chunk_count), file=open("partitioning.json", "w"))
+
+  subprocess.run(["echo", "./hyriseBenchmarkFileBased", "-o", str(chunk_count) + "chunks.json", "--query_path=/home/Jan.Kossmann/hyrise_partitioning/resources/benchmark/acdoca/queries", "--table_path=/home/Jan.Kossmann/hyrise_partitioning/resources/benchmark/acdoca/tables/10M"], env=env)
