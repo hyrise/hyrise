a469d3a549eb920eab3afb24eba02d3aee3f4b53
Till Later <lutrinus@googlemail.com>
2020-03-10 00:20:47 +0100

    Implements further PR feedback

--- src/lib/concurrency/transaction_manager.hpp
+++ src/lib/concurrency/transaction_manager.hpp
@@ -55,6 +55,8 @@ class TransactionManager : public Noncopyable {
 
   /**
    * Creates a new transaction context
+   * @param is_auto_commit Declares whether the transaction is created (and will also commit)
+   * automatically or explicitly created and committed through a user command.
    */
   std::shared_ptr<TransactionContext> new_transaction_context(
       IsAutoCommitTransaction is_auto_commit = IsAutoCommitTransaction::Yes);
