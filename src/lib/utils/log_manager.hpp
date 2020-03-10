3a557883e0799c0cfa367c17d994c98447c18a8f
Jan Kossmann <jan.kossmann@hpi.de>
2020-03-10 14:02:31 +0100

    renaming

--- src/lib/utils/log_manager.hpp
+++ src/lib/utils/log_manager.hpp
@@ -11,13 +11,13 @@ namespace opossum {
 
 struct LogEntry {
   std::chrono::milliseconds timestamp;
-  std::string sender;
+  std::string reporter;
   std::string message;
 };
 
 class LogManager : public Noncopyable {
  public:
-  void add_message(const std::string& sender, const std::string& message);
+  void add_message(const std::string& reporter, const std::string& message);
 
   const tbb::concurrent_vector<LogEntry>& log_entries() const;
 
