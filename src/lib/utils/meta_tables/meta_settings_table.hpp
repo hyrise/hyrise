640875a0a605e94bbba17dcdf87855b9f92ec938
Jan Kossmann <jan.kossmann@hpi.de>
2020-03-11 18:39:41 +0100

    Make some methods final

--- src/lib/utils/meta_tables/meta_settings_table.hpp
+++ src/lib/utils/meta_tables/meta_settings_table.hpp
@@ -13,13 +13,13 @@ class MetaSettingsTable : public AbstractMetaTable {
 
   const std::string& name() const final;
 
-  bool can_update() const;
+  bool can_update() const final;
 
  protected:
   friend class MetaSettingsTest;
-  std::shared_ptr<Table> _on_generate() const;
+  std::shared_ptr<Table> _on_generate() const final;
 
-  void _on_update(const std::vector<AllTypeVariant>& selected_values, const std::vector<AllTypeVariant>& update_values);
+  void _on_update(const std::vector<AllTypeVariant>& selected_values, const std::vector<AllTypeVariant>& update_values) final;
 };
 
 }  // namespace opossum
