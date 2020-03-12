92f47d01c2ad4451c445ab5ec59b9dd0de4d4fa1
Jan Kossmann <jan.kossmann@hpi.de>
2020-03-12 14:18:51 +0100

    Clustering fix

--- src/benchmarklib/tpch/tpch_table_generator.cpp
+++ src/benchmarklib/tpch/tpch_table_generator.cpp
@@ -325,7 +325,8 @@ AbstractTableGenerator::IndexesByTable TPCHTableGenerator::_indexes_by_table() c
 
 AbstractTableGenerator::SortOrderByTable TPCHTableGenerator::_sort_order_by_table() const {
   // Allowed as per TPC-H Specification, paragraph 1.5.2
-  return {{"lineitem", "l_shipdate"}, {"orders", "o_orderdate"}};
+  // return {{"lineitem", "l_shipdate"}, {"orders", "o_orderdate"}};
+  return {};
 }
 
 void TPCHTableGenerator::_add_constraints(
