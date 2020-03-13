67744040806ef7e24d4d424e437de915a7ec1aa3
martin.boissier <martin.boissier@hpi.de>
2020-03-13 10:11:15 +0100

    Merging my stuff in here

--- src/lib/utils/meta_tables/meta_log_table.cpp
+++ src/lib/utils/meta_tables/meta_log_table.cpp
@@ -19,11 +19,11 @@ std::shared_ptr<Table> MetaLogTable::_on_generate() const {
   auto output_table = std::make_shared<Table>(_column_definitions, TableType::Data, std::nullopt, UseMvcc::Yes);
 
   for (const auto& entry : Hyrise::get().log_manager.log_entries()) {
-    output_table->append({static_cast<int64_t>(entry.timestamp.count()), pmr_string{entry.reporter}, pmr_string{entry.message}});
+    output_table->append(
+        {static_cast<int64_t>(entry.timestamp.count()), pmr_string{entry.reporter}, pmr_string{entry.message}});
   }
 
   return output_table;
 }
-
 
 }  // namespace opossum
