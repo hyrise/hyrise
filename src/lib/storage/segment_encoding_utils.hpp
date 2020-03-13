7b5498620480fd974795faa943fb63912747557b
martin.boissier <martin.boissier@hpi.de>
2020-03-13 10:03:54 +0100

    Initial

--- src/lib/storage/segment_encoding_utils.hpp
+++ src/lib/storage/segment_encoding_utils.hpp
@@ -20,8 +20,10 @@ class BaseValueSegment;
 std::unique_ptr<BaseSegmentEncoder> create_encoder(EncodingType encoding_type);
 
 /**
- * @brief Encodes a segment by the given encoding specification (i.e., the encoding method
- * and -- when given and applicable -- the vector compression type)
+ * @brief Encodes a segment by the given encoding specification (i.e., the encoding method and -- when given and
+ * applicable -- the vector compression type). In general, do not use this method but rather
+ * ChunkEncoder::encode_segment() which supports reencoding to ValueSegments and avoids reencoding with
+ * SegmentEncodingSpec already in place.
  *
  * @return encoded segment if data type is supported, otherwise throws exception.
  */
@@ -29,6 +31,16 @@ std::shared_ptr<BaseEncodedSegment> encode_and_compress_segment(const std::share
                                                                 const DataType data_type,
                                                                 const SegmentEncodingSpec& encoding_spec);
 
+/**
+ * @return the segment encoding spec for thes given segment.
+ */
+SegmentEncodingSpec get_segment_encoding_spec(const std::shared_ptr<const BaseSegment>& segment);
+
+/**
+ * @brief Returns the vector compression type for a given compressed vector type.
+ *
+ * For the difference of the two, please take a look at compressed_vector_type.hpp.
+ */
 VectorCompressionType parent_vector_compression_type(const CompressedVectorType compressed_vector_type);
 
 }  // namespace opossum
