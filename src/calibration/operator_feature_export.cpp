c7a175e1b0b88032e7115088bde6ca72db7b7526
Lukas Böhme <lukas.boehme@icloud.com>
2020-03-17 13:43:19 +0100

    Added comments to operator_feature_export

--- src/calibration/operator_feature_export.cpp
+++ src/calibration/operator_feature_export.cpp
@@ -9,6 +9,7 @@
 #include "storage/table.hpp"
 #include "hyrise.hpp"
 #include "csv_writer.hpp"
+#include <utils/assert.hpp>
 
 namespace opossum {
 
@@ -33,12 +34,13 @@ namespace opossum {
       }
     }
 
-    // TABLE_SCAN
+    // Export features of a table scan operator
     void OperatorFeatureExport::_export_table_scan(std::shared_ptr<const AbstractOperator> op) const {
+      DebugAssert(op->type() == OperatorType::TableScan, "Expected operator of type: TableScan but got another one");
+
       auto csv_writer = _csv_writers.at(op->type());
 
       const auto node = op->lqp_node;
-      // const auto predicate_node = std::dynamic_pointer_cast<const PredicateNode>(node);
 
       for (const auto& el : node->node_expressions) {
 
@@ -51,31 +53,31 @@ namespace opossum {
 
               if (original_node->type == LQPNodeType::StoredTable) {
 
-                // INPUT_ROWS_LEFT
+                // Get values for INPUT_ROWS_LEFT
                 if (op->input_left() != nullptr){
                   csv_writer->set_value("INPUT_ROWS_LEFT", op->input_table_left()->row_count());
                 } else {
                   csv_writer->set_value("INPUT_ROWS_LEFT", CSVWriter::NA);
                 }
 
-                // OUTPUT_ROWS
+                // Get values for OUTPUT_ROWS
                 if (op->get_output() != nullptr){
                   csv_writer->set_value("OUTPUT_ROWS", op->get_output()->row_count());
                 } else {
                   csv_writer->set_value("OUTPUT_ROWS", CSVWriter::NA);
                 }
 
-                // RUNTIME_NS
+                // Get values for RUNTIME_NS
                 csv_writer->set_value("RUNTIME_NS", op->performance_data().walltime.count());
 
-                // SCAN_IMPLEMENTATION
+                // Get values for SCAN_IMPLEMENTATION
                 if (original_node == node->left_input()) {
                   csv_writer->set_value("SCAN_TYPE", "COLUMN_SCAN");
                 } else {
                   csv_writer->set_value("SCAN_TYPE", "REFERENCE_SCAN");
                 }
 
-                // TABLE_NAME
+                // Get values for TABLE_NAME
                 const auto stored_table_node = std::dynamic_pointer_cast<const StoredTableNode>(original_node);
                 const auto& table_name = stored_table_node->table_name;
 
@@ -83,12 +85,11 @@ namespace opossum {
 
                 const auto original_column_id = column_reference.original_column_id();
 
-                // COLUMN_NAME
+                // Get values for COLUMN_NAME
                 const auto table = Hyrise::get().storage_manager.get_table(table_name);
-
                 csv_writer->set_value("COLUMN_NAME", table->column_names()[original_column_id]);
 
-                // SCAN_IMPLEMENTATION
+                // Get values for SCAN_IMPLEMENTATION
                 auto description = op->description();
                 std::smatch matches;
 
@@ -99,6 +100,7 @@ namespace opossum {
                   csv_writer->set_value("SCAN_IMPLEMENTATION", CSVWriter::NA);
                 }
 
+                // Write row to file (the function validates if we have added all values)
                 csv_writer->write_row();
               }
             }
