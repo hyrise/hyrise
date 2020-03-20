b774cf94024cdb149f298241c6c7778b1bbc13bd
Jan Kossmann <jan.kossmann@hpi.de>
2020-03-20 13:18:08 +0100

    Add IndexSelectionPlugin

---
+++ src/plugins/index_selection_plugin.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <algorithm>
+#include <mutex>
+#include <numeric>
+#include <queue>
+#include <thread>
+
+#include "gtest/gtest_prod.h"
+#include "hyrise.hpp"
+#include "storage/chunk.hpp"
+#include "utils/abstract_plugin.hpp"
+#include "utils/pausable_loop_thread.hpp"
+#include "utils/singleton.hpp"
+
+namespace opossum {
+
+class IndexSelectionPlugin : public AbstractPlugin {
+ public:
+  const std::string description() const final;
+
+  void start() final;
+
+  void stop() final;
+};
+
+}  // namespace opossum
