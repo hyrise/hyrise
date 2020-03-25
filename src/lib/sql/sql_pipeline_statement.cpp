f9570eda36ffbc1ca5fb77ed7acb1216517d707f
Leonard Pabst <leopab@gmx.de>
2020-03-25 16:46:16 +0100

    oops !

--- src/lib/sql/sql_pipeline_statement.cpp
+++ src/lib/sql/sql_pipeline_statement.cpp
@@ -148,9 +148,9 @@ const std::shared_ptr<AbstractLQPNode>& SQLPipelineStatement::get_split_unoptimi
       return LQPVisitation::VisitInputs;
     });
 
-    std::cout << "================== contains non uniform distribution?: " << contains_non_uniform_distribution << " ============" << std::endl;
+    //std::cout << "================== contains non uniform distribution?: " << contains_non_uniform_distribution << " ============" << std::endl;
 
-    if (contains_non_uniform_distribution) {
+    if (!contains_non_uniform_distribution) {
       visit_lqp(unoptimized_lqp, [&values, &parameter_id](const auto& node) {
           if (node) {
               for (auto& root_expression : node->node_expressions) {
@@ -158,11 +158,11 @@ const std::shared_ptr<AbstractLQPNode>& SQLPipelineStatement::get_split_unoptimi
                       if (expression->type == ExpressionType::Value) {
                         if (expression->replaced_by) {
                           const auto valexp = std::dynamic_pointer_cast<ValueExpression>(expression);
-                          std::cout << "================== took out expression again: " << valexp->value << " ============" << std::endl;
+                          //std::cout << "================== took out expression again: " << valexp->value << " ============" << std::endl;
                           expression = expression->replaced_by;
                         } else {
                           const auto valexp = std::dynamic_pointer_cast<ValueExpression>(expression);
-                          std::cout << "================== took out expression: " << valexp->value << " ============" << std::endl;
+                          //std::cout << "================== took out expression: " << valexp->value << " ============" << std::endl;
                           assert(expression->arguments.size() == 0);
                           values.push_back(expression);
                           auto new_expression = std::make_shared<TypedPlaceholderExpression>(parameter_id, expression->data_type());
