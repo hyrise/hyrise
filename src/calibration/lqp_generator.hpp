549660cdc4e8d398f71f1496800b694b9726cd26
Lukas Rambold <lukas@rambold.io>
2020-03-27 22:38:47 +0100

    applied linting rules to lqp_generator.cpp

--- src/calibration/lqp_generator.hpp
+++ src/calibration/lqp_generator.hpp
@@ -1,40 +1,34 @@
 #pragma once
 
 #include "string"
+
 #include <logical_query_plan/abstract_lqp_node.hpp>
-#include "logical_query_plan/stored_table_node.hpp"
-#include <string>
 #include <expression/expression_functional.hpp>
 
-#include "calibration_table_wrapper.hpp"
-#include "logical_query_plan/abstract_lqp_node.hpp"
 #include "logical_query_plan/stored_table_node.hpp"
+#include "calibration_table_wrapper.hpp"
 
-namespace opossum{
+namespace opossum {
 class LQPGenerator {
-
  public:
     LQPGenerator();
     void generate(OperatorType operator_type, const std::shared_ptr<const CalibrationTableWrapper> &table);
-    const std::vector<std::shared_ptr<AbstractLQPNode>>& get_lqps();
+    const std::vector<std::shared_ptr<AbstractLQPNode>> &get_lqps();
 
-  private:
+ private:
     using ColumnPair = std::pair<const std::string, const std::string>;
-    void _generate_table_scans(const std::shared_ptr<const CalibrationTableWrapper>& table);
-
-    void _generate_joins(const std::shared_ptr<const CalibrationTableWrapper>& sharedPtr);
-
+    void _generate_table_scans(const std::shared_ptr<const CalibrationTableWrapper> &table);
+    void _generate_joins(const std::shared_ptr<const CalibrationTableWrapper> &sharedPtr);
     void _generate_column_vs_column_scans(const std::shared_ptr<const CalibrationTableWrapper> &table);
-
-    [[nodiscard]] std::vector<LQPGenerator::ColumnPair> _get_column_pairs(const std::shared_ptr<const CalibrationTableWrapper> &table) const;
+    [[nodiscard]] std::vector<LQPGenerator::ColumnPair> _get_column_pairs(
+            const std::shared_ptr<const CalibrationTableWrapper> &table) const;
 
     std::vector<std::shared_ptr<AbstractLQPNode>> _generated_lpqs;
 
-    // these are the feature flags for the LQPGeneration
-    // They are currently set in a way to produce the best results for TCP-H (ie. reflect this workload most representative)
+    // feature flags for the LQPGeneration
     const bool _enable_like_predicates = true;
     const bool _enable_reference_scans = true;
     const bool _enable_column_vs_column_scans = true;
     const bool _enable_between_predicates = true;
 };
-}
+}  // namespace opossum
