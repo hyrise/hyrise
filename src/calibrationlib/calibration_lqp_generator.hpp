c3145eb5de0198adab75d1e13a843b284acf7ede
Lukas Böhme <lukas.boehme@icloud.com>
2020-03-31 23:20:43 +0200

    Removed joins

--- src/calibrationlib/calibration_lqp_generator.hpp
+++ src/calibrationlib/calibration_lqp_generator.hpp
@@ -19,7 +19,6 @@ class CalibrationLQPGenerator {
  private:
   using ColumnPair = std::pair<const std::string, const std::string>;
   void _generate_table_scans(const std::shared_ptr<const CalibrationTableWrapper>& table);
-  void _generate_joins(const std::shared_ptr<const CalibrationTableWrapper>& sharedPtr);
   void _generate_column_vs_column_scans(const std::shared_ptr<const CalibrationTableWrapper>& table);
   [[nodiscard]] std::vector<CalibrationLQPGenerator::ColumnPair> _get_column_pairs(
       const std::shared_ptr<const CalibrationTableWrapper>& table) const;
