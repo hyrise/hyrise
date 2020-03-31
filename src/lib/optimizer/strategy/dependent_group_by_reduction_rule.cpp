735f45fcf0b942720314253860353b5675c38d15
Julian Menzler <julianmenzler@gmail.com>
2020-03-31 22:06:03 +0200

    Implement FDs within GroupByReductionRule

--- src/lib/optimizer/strategy/dependent_group_by_reduction_rule.cpp
+++ src/lib/optimizer/strategy/dependent_group_by_reduction_rule.cpp
@@ -24,49 +24,60 @@ using namespace opossum;  // NOLINT
  *
  * @returns   Boolean value denoting whether at the group-by list of @param aggregate_node changed.
  */
-bool reduce_group_by_columns_for_constraint(const ExpressionsConstraintDefinition& constraint, ExpressionUnorderedSet& group_by_columns,
-                                            AggregateNode& aggregate_node) {
+bool reduce_group_by_columns_for_constraint(const ExpressionsConstraintDefinition& constraint, const std::vector<FunctionalDependency>& fds, ExpressionUnorderedSet& group_by_columns, AggregateNode& aggregate_node) {
   auto group_by_list_changed = false;
   const auto& constraint_columns = constraint.column_expressions;
 
-  // ToDo(Julian) Comment!
-  bool all_columns_present = std::all_of(constraint_columns.cbegin(), constraint_columns.cend(), [&group_by_columns](std::shared_ptr<AbstractExpression> constraint_col_expr) {
-      return group_by_columns.contains(constraint_col_expr);
-  });
-  if (!all_columns_present) {
-    // Skip the current constraint as the primary key/unique constraint is not completely present.
+  // The constraint's columns have to be part of the group-by list
+  if(!std::all_of(constraint_columns.cbegin(), constraint_columns.cend(), [&group_by_columns](std::shared_ptr<AbstractExpression> constraint_col_expr) {
+    return group_by_columns.contains(constraint_col_expr);
+  })) {
     return false;
   }
 
-  // Every column that is part of the input table but not part of the primary key/unique constraint is going to be moved from
-  // the group-by list to the list of aggregates wrapped in an ANY().
+  // Find functionally dependent columns
+  auto fd_columns = ExpressionUnorderedSet();
+  for(const auto& fd : fds) {
+    if (std::all_of(constraint_columns.begin(), constraint_columns.end(), [&fd](const auto constraint_column) {
+      return fd.first.contains(constraint_column);
+    })) {
+      fd_columns.insert(fd.second.begin(), fd.second.end());
+    }
+  }
+  if(fd_columns.empty()) return false;
+
+  // Every column that is functionally dependent on the column(s) of the unique constraint gets moved from
+  // the group-by list to the aggregate list. For this purpose it is wrapped in an ANY() expression.
   for (const auto& group_by_column : group_by_columns) {
     if (constraint_columns.contains(group_by_column)) {
-      // Do not touch primary key/unique colhumns of constraint.
+      // The constraint's columns remain in the group-by list.
       continue;
     }
 
-    // Remove column from group-by list.
-    // Further, decrement the aggregate's index which denotes the end of group-by expressions.
-    const auto begin_idx_before = aggregate_node.aggregate_expressions_begin_idx;
-    aggregate_node.node_expressions.erase(
-    std::remove_if(aggregate_node.node_expressions.begin(), aggregate_node.node_expressions.end(),
-                       [&](const auto node_expression) {
-                         if (*node_expression == *group_by_column) {
-                           // Adjust the number of group by expressions.
-                           --aggregate_node.aggregate_expressions_begin_idx;
-                           group_by_list_changed = true;
-                           return true;
-                         }
-                         return false;
-                       }),
-        aggregate_node.node_expressions.end());
-      Assert(aggregate_node.aggregate_expressions_begin_idx < begin_idx_before, "Failed to remove column from group-by list.");
+    if (fd_columns.contains(group_by_column)) {
+      // Remove column from group-by list.
+      // Further, decrement the aggregate's index which denotes the end of group-by expressions.
+      const auto begin_idx_before = aggregate_node.aggregate_expressions_begin_idx;
+      aggregate_node.node_expressions.erase(
+          std::remove_if(aggregate_node.node_expressions.begin(), aggregate_node.node_expressions.end(),
+                         [&](const auto node_expression) {
+                           if (*node_expression == *group_by_column) {
+                             // Adjust the number of group by expressions.
+                             --aggregate_node.aggregate_expressions_begin_idx;
+                             group_by_list_changed = true;
+                             return true;
+                           }
+                           return false;
+                         }),
+          aggregate_node.node_expressions.end());
+      Assert(aggregate_node.aggregate_expressions_begin_idx
+             < begin_idx_before, "Failed to remove column from group-by list.");
 
       // Add the ANY() aggregate to the list of aggregate columns.
       const auto aggregate_any_expression = any_(group_by_column);
       aggregate_node.node_expressions.emplace_back(aggregate_any_expression);
     }
+  }
 
   return group_by_list_changed;
 }
@@ -86,9 +97,7 @@ void DependentGroupByReductionRule::apply_to(const std::shared_ptr<AbstractLQPNo
     const auto input_constraints = aggregate_node.left_input()->constraints();
     if(input_constraints->empty()) return LQPVisitation::VisitInputs;
 
-    // Preparation:
-    // Store a copy of the root's column expressions.
-    const auto root_column_expressions = root_lqp->column_expressions();
+    // --- Preparation ---
     // Store copy of the aggregate's column expressions to later check if this order is the query's final column order
     const auto initial_aggregate_column_expressions = aggregate_node.column_expressions();
     auto group_by_list_changed = false;
@@ -116,7 +125,10 @@ void DependentGroupByReductionRule::apply_to(const std::shared_ptr<AbstractLQPNo
                 return left.column_expressions.size() < right.column_expressions.size();
     });
 
-    // Main:
+    // Fetch functional dependencies
+    const auto fds = aggregate_node.functional_dependencies();
+
+    // --- Main ---
     // Try to reduce the group-by list one constraint at a time, starting with the shortest constraint. Multiple
     // constraints might result in different group-by lists (depending on the number of columns of the constraint).
     // We stop as soon as one constraint successfully reduced the group-by list. There is no advantage in a second reduction if the first reduction was successful, because no further columns will be removed. Hence, as soon as one reduction took
@@ -124,16 +136,18 @@ void DependentGroupByReductionRule::apply_to(const std::shared_ptr<AbstractLQPNo
     for(size_t constraint_idx{0}; constraint_idx < input_constraints_sorted.size(); ++constraint_idx) {
       const auto& constraint = input_constraints_sorted[constraint_idx];
 
-      group_by_list_changed |= reduce_group_by_columns_for_constraint(constraint, group_by_columns_non_nullable,
+      group_by_list_changed |= reduce_group_by_columns_for_constraint(constraint, fds, group_by_columns_non_nullable,
           aggregate_node);
       if (group_by_list_changed) break;
     }
 
+    // --- Finish ---
     // In case the initial query plan root returned the same columns in the same column order and was not a projection,
     // it is likely that the result of the current aggregate was either the root itself or only operators followed that
     // do not modify the column order (e.g., sort or limit). In this case, we need to restore the initial column order
     // by adding a projection with the initial column_references since we changed the column order by moving columns
     // from the group-by list to the aggregations.
+    const auto root_column_expressions = root_lqp->column_expressions();
     if (group_by_list_changed && initial_aggregate_column_expressions == root_column_expressions &&
         root_lqp->type != LQPNodeType::Projection) {
       const auto projection_node = std::make_shared<ProjectionNode>(root_column_expressions);
@@ -144,4 +158,4 @@ void DependentGroupByReductionRule::apply_to(const std::shared_ptr<AbstractLQPNo
   });
 }
 
-}  // namespace opossum
+}  // namespace opossumww
