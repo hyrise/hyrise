0c5b9a9dde008a4eac89c0b6689a1d0f917e1baf
carla.terboven <carla.terboven@student.hpi.de>
2020-04-01 13:59:56 +0200

    init in TypedPlaceholderExpression

--- src/lib/expression/typed_placeholder_expression.cpp
+++ src/lib/expression/typed_placeholder_expression.cpp
@@ -2,9 +2,9 @@
 
 namespace opossum {
 
-TypedPlaceholderExpression::TypedPlaceholderExpression(const ParameterID parameter_id, DataType data_type) :
-  PlaceholderExpression(parameter_id),
-  _data_type{data_type} {}
+TypedPlaceholderExpression::TypedPlaceholderExpression(const ParameterID init_parameter_id, DataType init_data_type) :
+  PlaceholderExpression(init_parameter_id),
+  _data_type{init_data_type} {}
 
 std::shared_ptr<AbstractExpression> TypedPlaceholderExpression::deep_copy() const {
   return std::make_shared<TypedPlaceholderExpression>(parameter_id, _data_type);
