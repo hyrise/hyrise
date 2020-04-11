b49dcba0b3109156b2b022658b4ab36ae5d29597
Lukas Rambold <lukas@rambold.io>
2020-04-12 01:33:42 +0200

    refactor LQPGenerator Test

--- src/test/calibrationlib/calibration_lqp_generator_test.cpp
+++ src/test/calibrationlib/calibration_lqp_generator_test.cpp
@@ -2,6 +2,7 @@
 
 #include <operators/get_table.hpp>
 #include <synthetic_table_generator.hpp>
+#include <utility>
 #include "storage/table.hpp"
 
 #include "calibration_lqp_generator.hpp"
@@ -10,35 +11,65 @@
 namespace opossum {
 class CalibrationLQPGeneratorTest : public BaseTest {
  protected:
-  void SetUp() override {
-    auto table_config = std::make_shared<TableGeneratorConfig>(TableGeneratorConfig{
-        {DataType::Double, DataType::Float, DataType::Int, DataType::Long, DataType::String, DataType::Null},
+
+    void TearDown() override {
+      // Drop all tables that have been generated
+      auto table_names = Hyrise::get().storage_manager.table_names();
+      for (const auto& table_name : table_names) {
+        Hyrise::get().storage_manager.drop_table(table_name);
+      }
+    }
+
+    static std::vector<std::shared_ptr<const CalibrationTableWrapper>>
+    _generate_data(std::shared_ptr<TableGeneratorConfig> table_config) {
+      auto table_generator = CalibrationTableGenerator(std::move(table_config));
+      auto tables = table_generator.generate();
+
+      for (const auto& table : tables) {
+        Hyrise::get().storage_manager.add_table(table->get_name(), table->get_table());
+      }
+
+      return tables;
+    }
+};
+
+
+TEST_F(CalibrationLQPGeneratorTest, NullColumn) {
+  const auto& tables = _generate_data(std::make_shared<TableGeneratorConfig>(TableGeneratorConfig{
+        { DataType::Float, DataType::Null},
         {EncodingType::Dictionary},
         {ColumnDataDistribution::make_uniform_config(0.0, 1000.0)},
         {100},
-        {150, 3000, 6000, 100, 200, 300, 600, 25, 1500, 200, 800, 5, 100}});
-    auto table_generator = CalibrationTableGenerator(table_config);
-    _tables = table_generator.generate();
+        {10}}));
 
-    for (const auto& table : _tables) {
-      Hyrise::get().storage_manager.add_table(table->get_name(), table->get_table());
-    }
-  }
-
-  std::vector<std::shared_ptr<const CalibrationTableWrapper>> _tables;
-};
-
-TEST_F(CalibrationLQPGeneratorTest, NullSmokeTest) {
   auto lqp_generator = CalibrationLQPGenerator();
-  const auto table = _tables.at(5);
-  lqp_generator.generate(OperatorType::TableScan, table);
+  lqp_generator.generate(OperatorType::TableScan, tables[0]);
+  ASSERT_GT(lqp_generator.get_lqps().size(), 0);
+}
+
+TEST_F(CalibrationLQPGeneratorTest, EmptyColumn) {
+  const auto& tables = _generate_data(std::make_shared<TableGeneratorConfig>(TableGeneratorConfig{
+        { DataType::Int, },
+        {EncodingType::Dictionary},
+        {ColumnDataDistribution::make_uniform_config(0.0, 1000.0)},
+        {100},
+        {0}}));
+
+  auto lqp_generator = CalibrationLQPGenerator();
+  lqp_generator.generate(OperatorType::TableScan, tables[0]);
   ASSERT_GT(lqp_generator.get_lqps().size(), 0);
 }
 
 TEST_F(CalibrationLQPGeneratorTest, NumericTableScan) {
+  const auto& tables = _generate_data(std::make_shared<TableGeneratorConfig>(TableGeneratorConfig{
+      { DataType::Float},
+      {EncodingType::Dictionary},
+      {ColumnDataDistribution::make_uniform_config(0.0, 1000.0)},
+      {100},
+      {100}}));
+
   auto lqp_generator = CalibrationLQPGenerator();
-  const auto table = _tables.at(0);
-  lqp_generator.generate(OperatorType::TableScan, table);
+  lqp_generator.generate(OperatorType::TableScan, tables[0]);
   for (const std::shared_ptr<AbstractLQPNode>& lqp : lqp_generator.get_lqps()) {
     const auto pqp = LQPTranslator{}.translate_node(lqp);
     ASSERT_TRUE(pqp->type() == OperatorType::TableScan);
@@ -48,9 +79,15 @@ TEST_F(CalibrationLQPGeneratorTest, NumericTableScan) {
 }
 
 TEST_F(CalibrationLQPGeneratorTest, ReferenceScans) {
+  const auto& tables = _generate_data(std::make_shared<TableGeneratorConfig>(TableGeneratorConfig{
+    { DataType::Float},
+    {EncodingType::Dictionary},
+    {ColumnDataDistribution::make_uniform_config(0.0, 1000.0)},
+    {100},
+    {100}}));
+
   auto lqp_generator = CalibrationLQPGenerator();
-  const auto table = _tables.at(0);
-  lqp_generator.generate(OperatorType::TableScan, table);
+  lqp_generator.generate(OperatorType::TableScan, tables[0]);
 
   bool found_reference_scan = false;
   for (const std::shared_ptr<AbstractLQPNode>& lqp : lqp_generator.get_lqps()) {
@@ -63,5 +100,4 @@ TEST_F(CalibrationLQPGeneratorTest, ReferenceScans) {
 
   ASSERT_TRUE(found_reference_scan);
 }
-
 }  // namespace opossum
