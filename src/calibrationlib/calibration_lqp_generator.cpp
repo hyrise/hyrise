617225fdbda716abcdc3366c0593e749776aedc3
Lukas Rambold <lukas@rambold.io>
2020-04-12 02:04:37 +0200

    implemented code review feedback

--- src/calibrationlib/calibration_lqp_generator.cpp
+++ src/calibrationlib/calibration_lqp_generator.cpp
@@ -25,30 +25,41 @@ void CalibrationLQPGenerator::generate(OperatorType operator_type,
     default:
       break;
   }
-  throw std::runtime_error("Not implemented yet: Only TableScans and JoinHashes are currently supported");
+
+  Fail("Not implemented yet: Only TableScans are currently supported");
 }
 
-void CalibrationLQPGenerator::_generate_table_scans(const std::shared_ptr<const CalibrationTableWrapper>& table) {
-  const int selectivity_resolution = 10;
-  const int reference_scan_resolution = 10;
+void CalibrationLQPGenerator::_generate_table_scans
+(const std::shared_ptr<const CalibrationTableWrapper>& table_wrapper) {
+  // selectivity resolution determines in how many LQPs with a different selectivity are generated
+  // increase this value for providing more training data to the model
+  // The resulting LQPs are equally distributed from 0% to 100% selectivity.
+  constexpr uint32_t selectivity_resolution = 10;
 
-  const auto stored_table_node = StoredTableNode::make(table->get_name());
+  // for every table scan gets executed on the raw data as well as referenceSegments
+  // reference_scan_selectivity_resolution determines how many of these scans on referenceSegments are generated
+  // in addition to th raw scan.
+  // The resulting referenceSegments reduce the selectivity of the original scan by 0% to 100% selectivity
+  // in equally sized steps.
+  constexpr uint32_t reference_scan_selectivity_resolution = 10;
 
-  const int column_count = table->get_table()->column_count();
-  const std::vector<std::string> column_names = table->get_table()->column_names();
-  const auto column_data_types = table->get_table()->column_data_types();
+  const auto stored_table_node = StoredTableNode::make(table_wrapper->get_name());
+
+  const auto column_count = table_wrapper->get_table()->column_count();
+  const std::vector<std::string> column_names = table_wrapper->get_table()->column_names();
+  const auto column_data_types = table_wrapper->get_table()->column_data_types();
 
   for (ColumnID column_id = ColumnID{0}; column_id < column_count; ++column_id) {
     // Column specific values
-    const auto column = stored_table_node->get_column(column_names.at(column_id));
-    const auto distribution = table->get_column_data_distribution(column_id);
+    const auto column = stored_table_node->get_column(column_names[column_id]);
+    const auto distribution = table_wrapper->get_column_data_distribution(column_id);
     const auto step_size = (distribution.max_value - distribution.min_value) / selectivity_resolution;
 
     if (_enable_column_vs_column_scans) {
-      _generate_column_vs_column_scans(table);
+      _generate_column_vs_column_scans(table_wrapper);
     }
 
-    for (int selectivity_step = 0; selectivity_step < selectivity_resolution; selectivity_step++) {
+    for (uint32_t selectivity_step = 0; selectivity_step < selectivity_resolution; selectivity_step++) {
       // in this for-loop we iterate up and go from 100% selectivity to 0% by increasing the lower_bound in steps
       // for any step there
       resolve_data_type(column_data_types[column_id], [&](const auto column_data_type) {
@@ -56,7 +67,7 @@ void CalibrationLQPGenerator::_generate_table_scans(const std::shared_ptr<const 
 
         // Get value for current iteration
         // the cursor is a int representation of where the column has to be cut in this iteration
-        const auto step_cursor = static_cast<int>(selectivity_step * step_size);
+        const auto step_cursor = static_cast<uint32_t>(selectivity_step * step_size);
 
         const ColumnDataType lower_bound = SyntheticTableGenerator::generate_value<ColumnDataType>(step_cursor);
 
@@ -69,20 +80,22 @@ void CalibrationLQPGenerator::_generate_table_scans(const std::shared_ptr<const 
 
         // Add reference scans
         if (_enable_reference_scans) {
-          // generate reference scans that are executed before the actual predicate
+          // generate reference scans to base original LQP on
           // that reduce the overall selectivity stepwise
-          const double reference_scan_step_size = (distribution.max_value - step_cursor) / reference_scan_resolution;
-          for (int step = 0; step < reference_scan_resolution; step++) {
+          const double reference_scan_step_size =
+                  (distribution.max_value - step_cursor) / reference_scan_selectivity_resolution;
+          for (uint32_t step = 0; step < reference_scan_selectivity_resolution; step++) {
             const ColumnDataType upper_bound = SyntheticTableGenerator::generate_value<ColumnDataType>(
-                static_cast<int>(step * reference_scan_step_size));
+                static_cast<uint32_t>(step * reference_scan_step_size));
             _generated_lpqs.emplace_back(
                 get_predicate_node_based_on(PredicateNode::make(less_than_(column, upper_bound), stored_table_node)));
-            if (_enable_between_predicates && std::is_same<ColumnDataType, std::string>::value) {
+            if (_enable_between_predicates) {
               _generated_lpqs.emplace_back(
                   PredicateNode::make(between_inclusive_(column, lower_bound, upper_bound), stored_table_node));
             }
           }
           // add reference scan with full pos list
+          // TODO(ramboldio) ensure full and empty pos list also for NULL values
           _generated_lpqs.emplace_back(
               get_predicate_node_based_on(PredicateNode::make(is_not_null_(column), stored_table_node)));
           // add reference scan with empty pos list
@@ -92,7 +105,7 @@ void CalibrationLQPGenerator::_generate_table_scans(const std::shared_ptr<const 
 
         // LIKE and IN predicates for strings
         if (_enable_like_predicates && std::is_same<ColumnDataType, std::string>::value) {
-          for (int step = 0; step < 10; step++) {
+          for (uint32_t step = 0; step < 10; step++) {
             auto const upper_bound = (SyntheticTableGenerator::generate_value<pmr_string>(step));
             _generated_lpqs.emplace_back(PredicateNode::make(like_(column, upper_bound + "%"), stored_table_node));
           }
@@ -117,27 +130,28 @@ std::vector<CalibrationLQPGenerator::ColumnPair> CalibrationLQPGenerator::_get_c
        * In this case every value from one column has to be compared to every value of the other
        * making this operation somewhat costly and therefore requiring a dedicated test case.
        *
-       * We implement this creating a columnVsColumn scan in between all the columns with same data type
+       * We implement this creating a ColumnVsColumn scan in between all the columns with same data type
        */
   const auto column_definitions = table->get_table()->column_definitions();
   auto column_comparison_pairs = std::vector<ColumnPair>();
 
-  auto singles = std::vector<TableColumnDefinition>();
+  auto unmatched_columns = std::vector<TableColumnDefinition>();
 
-  for (const TableColumnDefinition& column : column_definitions) {
+  for (const auto& column : column_definitions) {
+    // TODO(ramboldio) simplify loop / ensure that first and second element are not the same
     bool matched = false;
-    auto single_iterator = singles.begin();
-    while (single_iterator < singles.end()) {
+    auto single_iterator = unmatched_columns.begin();
+    while (single_iterator < unmatched_columns.end()) {
       if (column.data_type == single_iterator->data_type) {
         matched = true;
         column_comparison_pairs.emplace_back(ColumnPair(single_iterator->name, column.name));
-        singles.erase(single_iterator);
+        unmatched_columns.erase(single_iterator);
         break;
       }
       single_iterator++;
     }
     if (!matched) {
-      singles.emplace_back(column);
+      unmatched_columns.emplace_back(column);
     }
   }
   return column_comparison_pairs;
@@ -145,6 +159,7 @@ std::vector<CalibrationLQPGenerator::ColumnPair> CalibrationLQPGenerator::_get_c
 
 void CalibrationLQPGenerator::_generate_column_vs_column_scans(
     const std::shared_ptr<const CalibrationTableWrapper>& table) {
+  // TODO(ramboldio) include statement of purpose like (ColumnVsColumn scans are an expensive special case)
   const auto stored_table_node = StoredTableNode::make(table->get_name());
   const auto column_vs_column_scan_pairs = _get_column_pairs(table);
 
--- src/calibrationlib/calibration_lqp_generator.hpp
+++ src/calibrationlib/calibration_lqp_generator.hpp
@@ -18,7 +18,7 @@ class CalibrationLQPGenerator {
 
  private:
   using ColumnPair = std::pair<const std::string, const std::string>;
-  void _generate_table_scans(const std::shared_ptr<const CalibrationTableWrapper>& table);
+  void _generate_table_scans(const std::shared_ptr<const CalibrationTableWrapper>& table_wrapper);
   void _generate_column_vs_column_scans(const std::shared_ptr<const CalibrationTableWrapper>& table);
   [[nodiscard]] std::vector<CalibrationLQPGenerator::ColumnPair> _get_column_pairs(
       const std::shared_ptr<const CalibrationTableWrapper>& table) const;
@@ -26,9 +26,9 @@ class CalibrationLQPGenerator {
   std::vector<std::shared_ptr<AbstractLQPNode>> _generated_lpqs;
 
   // feature flags for the LQPGeneration
-  const bool _enable_like_predicates = true;
-  const bool _enable_reference_scans = true;
-  const bool _enable_column_vs_column_scans = true;
-  const bool _enable_between_predicates = true;
+  static constexpr bool _enable_like_predicates = true;
+  static constexpr bool _enable_reference_scans = true;
+  static constexpr bool _enable_column_vs_column_scans = true;
+  static constexpr bool _enable_between_predicates = true;
 };
 }  // namespace opossum
