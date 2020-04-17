a9ee96831a9b22dc96fd676cfffacf7807895cae
martin.boissier <martin.boissier@hpi.de>
2020-04-17 18:58:41 +0200

    Working

--- src/lib/operators/operator_performance_data.hpp
+++ src/lib/operators/operator_performance_data.hpp
@@ -10,11 +10,13 @@ namespace opossum {
 /**
   * General execution information is stored in OperatorPerformanceData through AbstractOperator::execute().
   * Further, operators can store additional execution information by inheriting from OperatorPerformanceData (e.g.,
-  * JoinIndex) or StagedPerformanceData (when operator stage runtimes shall be tracked, e.g., HashJoin).
+  * JoinIndex) or StepOperatorPerformanceData (when operator steps shall be tracked, e.g., HashJoin).
   * Only used in PQP Visualize and plugins right now (analyze PQP).
   */
 struct OperatorPerformanceData : public Noncopyable {
   virtual ~OperatorPerformanceData() = default;
+
+  enum class NoStages { };
 
   bool executed{false};
   std::chrono::nanoseconds walltime{0};
@@ -29,10 +31,10 @@ struct OperatorPerformanceData : public Noncopyable {
                                 DescriptionMode description_mode = DescriptionMode::SingleLine) const;
 };
 
-struct StagedOperatorPerformanceData : public OperatorPerformanceData {
-  StagedOperatorPerformanceData() : OperatorPerformanceData{}, stage_runtimes{10} {}
+struct StepOperatorPerformanceData : public OperatorPerformanceData {
+  StepOperatorPerformanceData() : OperatorPerformanceData{} {}
 
-  std::vector<std::chrono::nanoseconds> stage_runtimes;
+  std::array<std::chrono::nanoseconds, 10> step_runtimes;
 
   std::chrono::nanoseconds get_stage_runtime(const size_t stage) const { return stage_runtimes[stage]; }
 
@@ -41,6 +43,6 @@ struct StagedOperatorPerformanceData : public OperatorPerformanceData {
 };
 
 std::ostream& operator<<(std::ostream& stream, const OperatorPerformanceData& performance_data);
-std::ostream& operator<<(std::ostream& stream, const StagedOperatorPerformanceData& performance_data);
+std::ostream& operator<<(std::ostream& stream, const StepOperatorPerformanceData& performance_data);
 
 }  // namespace opossum
