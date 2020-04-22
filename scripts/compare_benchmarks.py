900e6174f84db5c3db1cbeb3cdf2c28af1d53173
Markus Dreseler <markus.dreseler@hpi.de>
2020-04-22 16:42:09 +0200

    Dirty commit

--- scripts/compare_benchmarks.py
+++ scripts/compare_benchmarks.py
@@ -11,16 +11,22 @@ p_value_significance_threshold = 0.001
 min_iterations = 10
 min_runtime_ns = 59 * 1000 * 1000 * 1000
 
-def format_diff(diff):
-    select_color = lambda value, color: color if abs(value) > 0.05 else 'white'
+SUPERSCRIPT = str.maketrans("0123456789", "⁰¹²³⁴⁵⁶⁷⁸⁹")
 
+def format_diff(diff):
     diff -= 1  # adapt to show change in percent
     if diff < 0.0:
-        color = 'green'
-        return colored("{0:.0%}".format(diff), select_color(diff, color))
+        return "{0:.0%}".format(diff)
     else:
-        color = 'red'
-        return colored("+{0:.0%}".format(diff), select_color(diff, color))
+        return "+{0:.0%}".format(diff)
+
+def color_diff(diff, inverse_colors = False):
+    select_color = lambda value, color: color if abs(value - 1) > 0.05 else 'white'
+
+    diff_str = format_diff(diff)
+    color = 'green' if (diff_str[0] == '+') != (inverse_colors) else 'red'
+
+    return colored(format_diff(diff), select_color(diff, color))
 
 def geometric_mean(values):
     product = 1
@@ -57,10 +63,12 @@ def calculate_and_format_p_value(old, new):
         return "(run time too short)"
     elif (len(old_durations) < min_iterations or len(new_durations) < min_iterations):
         is_significant = False
-        return "(not enough runs)"
+        return colored('2'.translate(SUPERSCRIPT), 'yellow', attrs=['bold'])
     else:
-        color = 'green' if is_significant else 'white'
-        return colored("{0:.4f}".format(p_value), color)
+        if is_significant:
+            return colored("{0:.4f}".format(p_value), 'white')
+        else:
+            return colored("{0:.4f}".format(p_value), 'yellow', attrs=['bold'])
 
 
 if (not len(sys.argv) in [3, 4]):
@@ -78,86 +86,95 @@ with open(sys.argv[2]) as new_file:
 if old_data['context']['benchmark_mode'] != new_data['context']['benchmark_mode']:
     exit("Benchmark runs with different modes (ordered/shuffled) are not comparable")
 
-diffs = []
+diffs1 = []
+diffs2 = []
 total_runtime_old = 0
 total_runtime_new = 0
 
 table_data = []
-table_data.append(["Benchmark", "old ms/iter", "runs", "new ms/iter", "runs", "change [%]", "p-value"])
+table_data.append(["Item", "Latency\nms/iter", "Now\nms/iter", "Change\n%", "Throughput\niter/s", "Now\niter/s", "Change\n%", "p-value"])
 
 for old, new in zip(old_data['benchmarks'], new_data['benchmarks']):
     name = old['name']
     if old['name'] != new['name']:
         name += ' -> ' + new['name']
 
+    # TODO Check if avg_real_time_per_iteration is correctly calculated for multi-client benchmarks
     total_runtime_old += old['avg_real_time_per_iteration']
     total_runtime_new += new['avg_real_time_per_iteration']
 
     if float(old['avg_real_time_per_iteration']) > 0.0:
-        diff = float(new['avg_real_time_per_iteration']) / float(old['avg_real_time_per_iteration'])
-        diffs.append(diff)
+        diff1 = float(new['avg_real_time_per_iteration']) / float(old['avg_real_time_per_iteration'])
+        diffs1.append(diff1)
     else:
-        diff = float('nan')
+        diff1 = float('nan')
 
-    diff_formatted = format_diff(diff)
+    if float(old['items_per_second']) > 0.0:
+        diff2 = float(new['items_per_second']) / float(old['items_per_second'])
+        diffs2.append(diff2)
+    else:
+        diff2 = float('nan')
+
+    diff1_formatted = format_diff(diff1)
+    diff2_formatted = color_diff(diff2)
     p_value_formatted = calculate_and_format_p_value(old, new)
 
-    table_data.append([name, '{:>11.2f}'.format(old['avg_real_time_per_iteration'] / 1e6), str(len(old['successful_runs'])), '{:>11.2f}'.format(new['avg_real_time_per_iteration'] / 1e6), str(len(new['successful_runs'])), diff_formatted, p_value_formatted])
+    if old['name'] == 'TPC-H 06':
+        note = colored('1'.translate(SUPERSCRIPT), 'yellow', attrs=['bold'])
+    else:
+        note = ' '
 
-    if (len(old['unsuccessful_runs']) > 0 or len(new['unsuccessful_runs']) > 0):
-        old_unsuccessful_per_second = float(len(old['unsuccessful_runs'])) / (old['duration'] / 1e9)
-        new_unsuccessful_per_second = float(len(new['unsuccessful_runs'])) / (new['duration'] / 1e9)
+    table_data.append([name, '{:>.1f}'.format(old['avg_real_time_per_iteration'] / 1e6), '{:>.1f}'.format(new['avg_real_time_per_iteration'] / 1e6), diff1_formatted + note, '{:>.2f}'.format(old['items_per_second']), '{:>.2f}'.format(new['items_per_second']), diff2_formatted + note, p_value_formatted])
 
-        if len(old['unsuccessful_runs']) > 0:
-            diff_unsuccessful = float(new_unsuccessful_per_second / old_unsuccessful_per_second)
-        else:
-            diff_unsuccessful = float('nan')
+    # if (len(old['unsuccessful_runs']) > 0 or len(new['unsuccessful_runs']) > 0):
+    #     old_unsuccessful_per_second = float(len(old['unsuccessful_runs'])) / (old['duration'] / 1e9)
+    #     new_unsuccessful_per_second = float(len(new['unsuccessful_runs'])) / (new['duration'] / 1e9)
 
-        unsuccessful_info = [
-            '  unsucc.:',
-            ' ' + str(old_unsuccessful_per_second),
-            ' ' + str(len(old['unsuccessful_runs'])),
-            ' ' + str(new_unsuccessful_per_second),
-            ' ' + str(len(new['unsuccessful_runs'])),
-            ' ' + format_diff(diff_unsuccessful)
-        ]
+    #     if len(old['unsuccessful_runs']) > 0:
+    #         diff_unsuccessful = float(new_unsuccessful_per_second / old_unsuccessful_per_second)
+    #     else:
+    #         diff_unsuccessful = float('nan')
 
-        unsuccessful_info_colored = [colored(text, attrs=['dark']) for text in unsuccessful_info]
-        table_data.append(unsuccessful_info_colored)
+    #     unsuccessful_info = [
+    #         '  unsucc.:',
+    #         ' ' + str(old_unsuccessful_per_second),
+    #         ' ' + str(len(old['unsuccessful_runs'])),
+    #         ' ' + str(new_unsuccessful_per_second),
+    #         ' ' + str(len(new['unsuccessful_runs'])),
+    #         ' ' + color_diff(diff_unsuccessful)
+    #     ]
 
-# <...> indicates a span, which will be replaced later
-table_data.append(['Sum of items', '{:>11.2f}'.format(total_runtime_old / 1e6), '', '{:>11.2f}'.format(total_runtime_new / 1e6), '', format_diff(total_runtime_new / total_runtime_old), ''])
-table_data.append(['<', '', '', '', '>', format_diff(geometric_mean(diffs)), ''])
+    #     unsuccessful_info_colored = [colored(text, attrs=['dark']) for text in unsuccessful_info]
+    #     table_data.append(unsuccessful_info_colored)
+    # TODO bring this back
+
+table_data.append(['Sum', '{:>.1f}'.format(total_runtime_old / 1e6), '{:>.1f}'.format(total_runtime_new / 1e6), color_diff(total_runtime_new / total_runtime_old, True) + ' ', ''])
+table_data.append(['Geomean', '', '', '', '', '', color_diff(geometric_mean(diffs2)) + ' ', ''])
 
 table = AsciiTable(table_data)
+table.justify_columns[1] = 'right'
 table.justify_columns[2] = 'right'
+table.justify_columns[3] = 'right'
 table.justify_columns[4] = 'right'
 table.justify_columns[5] = 'right'
 table.justify_columns[6] = 'right'
 
 result = str(table.table)
 
-# terminaltables does not support spans. Add spans manually for the last two rows.
 new_result = ''
 lines = result.splitlines()
 for (line_number, line) in enumerate(lines):
-    if line_number == len(table_data):
+    if line_number == len(table_data) + 1:
         # Add another separation between benchmark items and aggregates
         new_result += lines[-1] + "\n"
 
-    if line_number != len(table_data) + 1:
-        new_result += line + "\n"
-        continue
+    new_result += line + "\n"
 
-    span_begin = line.find('<')
-    span_end = line.find('>') + 1
-    span_text = ''
-    if line_number == len(table_data) + 1:
-        span_text = 'Geometric mean (All items have the same weight)'
-
-    new_result += line[0:span_begin]
-    new_result += span_text + ' ' * (span_end - span_begin - len(span_text))
-    new_result += line[span_end:] + "\n"
+# TODO actually calculate column width
+# TODO only show these when applicable?
+new_result += '|    Notes | ' + '1'.translate(SUPERSCRIPT) + ' Execution stopped at 10000 runs                                     |' + '\n'
+new_result += '|          | ' + '2'.translate(SUPERSCRIPT) + ' Insufficient number of runs for p-value calculation                 |' + '\n'
+new_result += lines[-1] + "\n"
 
 result = new_result
 
@@ -173,9 +190,9 @@ if github_format:
     red_control_sequence = colored('', 'red')[0:5]
 
     for line in result.splitlines():
-        if green_control_sequence + '-' in line:
+        if green_control_sequence + '+' in line:
             new_result += '+'
-        elif red_control_sequence + '+' in line:
+        elif red_control_sequence + '-' in line:
             new_result += '-'
         else:
             new_result += ' '
