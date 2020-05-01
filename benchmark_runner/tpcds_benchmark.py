bf0bb566f2d50cda09c1773edf114ff655da43f8
Alexander Loeser <Alexander.Loeser@student.hpi.de>
2020-05-01 10:43:46 +0200

    current benchmark config state

--- benchmark_runner/tpcds_benchmark.py
+++ benchmark_runner/tpcds_benchmark.py
@@ -24,10 +24,38 @@ class TPCDSBenchmark(AbstractBenchmark):
     return [65000]
 
   def sort_orders(self):
+    # SF 1
     return {
-      'ss_net_profit-45_ss_quantity-1': {'store_sales': [('ss_net_profit', 45),
-   ('ss_quantity', 1)]},
+      'ss_ticket_number-45_ss_quantity-1': {'store_sales': [('ss_ticket_number', 45), ('ss_quantity', 1)]},
+      'ss_net_profit-45_ss_ticket_number-1': {'store_sales': [('ss_net_profit', 45), ('ss_ticket_number', 1)]},
+    }    
+
+
+    return {
+      'ss_item_sk-45_ss_quantity-1': {'store_sales': [('ss_item_sk', 45), ('ss_quantity', 1)]},
+      'ss_net_profit-45_ss_item_sk-1': {'store_sales': [('ss_net_profit', 45), ('ss_item_sk', 1)]},
     }
+
+    return {
+    'default': {},
+ 'cd_education_status-7_cd_marital_status-5_cd_education_status-1': {'customer_demographics': [('cd_education_status',
+   7),
+   ('cd_marital_status', 5),
+   ('cd_education_status', 1)]},
+ 'cd_education_status-8_cd_gender-4_cd_education_status-1': {'customer_demographics': [('cd_education_status',
+    8),
+   ('cd_gender', 4),
+   ('cd_education_status', 1)]},
+ 'cd_education_status-30': {'customer_demographics': [('cd_education_status',
+    30)]},
+ 'ss_net_profit-45_ss_quantity-1': {'store_sales': [('ss_net_profit', 45),
+   ('ss_quantity', 1)]},
+ 'ss_net_profit-11_ss_quantity-5': {'store_sales': [('ss_net_profit', 11),
+   ('ss_quantity', 5)]},
+ 'ss_net_profit-8_ss_wholesale_cost-6_ss_quantity-1': {'store_sales': [('ss_net_profit',
+    8),
+   ('ss_wholesale_cost', 6),
+   ('ss_quantity', 1)]}}
 
 
     # SF 10
@@ -62,34 +90,6 @@ class TPCDSBenchmark(AbstractBenchmark):
    ('ss_quantity', 1)]}}
 
 
-
-
-
-    # SF 1
-    return {'default': {},
- 'cd_education_status-7_cd_marital_status-5_cd_education_status-1': {'customer_demographics': [('cd_education_status',
-    7),
-   ('cd_marital_status', 5),
-   ('cd_education_status', 1)]},
- 'cd_education_status-8_cd_gender-4_cd_education_status-1': {'customer_demographics': [('cd_education_status',
-    8),
-   ('cd_gender', 4),
-   ('cd_education_status', 1)]},
- 'cd_education_status-30': {'customer_demographics': [('cd_education_status',
-    30)]},
- 't_minute-2_t_hour-1': {'time_dim': [('t_minute', 2), ('t_hour', 1)]},
- 't_hour-2_t_minute-2_t_hour-1': {'time_dim': [('t_hour', 2),
-   ('t_minute', 2),
-   ('t_hour', 1)]},
- 't_hour-2': {'time_dim': [('t_hour', 2)]},
- 'ss_net_profit-45_ss_quantity-1': {'store_sales': [('ss_net_profit', 45),
-   ('ss_quantity', 1)]},
- 'ss_net_profit-11_ss_quantity-5': {'store_sales': [('ss_net_profit', 11),
-   ('ss_quantity', 5)]},
- 'ss_net_profit-8_ss_wholesale_cost-6_ss_quantity-1': {'store_sales': [('ss_net_profit',
-    8),
-   ('ss_wholesale_cost', 6),
-   ('ss_quantity', 1)]}}
 
 
     return {
--- benchmark_runner/tpch_benchmark.py
+++ benchmark_runner/tpch_benchmark.py
@@ -8,21 +8,131 @@ class TPCHBenchmark(AbstractBenchmark):
     return "/home/Alexander.Loeser/hyrise/build-release/hyrisePlayground"
 
   def result_path(self):
-    return "/home/Alexander.Loeser/hyrise/benchmark_results/tpch_mdc_sf_10"
+    return "/home/Alexander.Loeser/hyrise/benchmark_results/tpch_disjoint_clusters_mvcc_removechunks2_sf_1"
 
   def max_runs(self):
-    return 1000
+    return -1
 
   def time(self):
-    return 180
+    return 60
 
   def scale(self):
-    return 10
+    return 1
   
   def chunk_sizes(self):
     return [25000]
 
   def sort_orders(self):
+    # SF 1 test disjoin_clusters
+    return  {
+      'l_shipdate-10_o_orderdate-10': {
+        'lineitem': [['l_shipdate', 10]],
+        'orders': [['o_orderdate', 10]]
+      },
+      'l_shipdate-25_o_orderdate-20': {
+        'lineitem': [['l_shipdate', 25]],
+        'orders': [['o_orderdate', 20]]
+      },
+      'l_shipdate-80_o_orderdate-40': {
+        'lineitem': [['l_shipdate', 80]],
+        'orders': [['o_orderdate', 40]]
+      },
+      'l_shipdate-40_l_discount-2_o_orderdate-40': {
+        'lineitem': [['l_shipdate', 40,], ['l_discount', 2]],
+        'orders': [['o_orderdate', 40]]
+      },
+      'l_shipdate-80_l_discount-3_o_orderdate-40': {
+        'lineitem': [['l_shipdate', 80,], ['l_discount', 3]],
+        'orders': [['o_orderdate', 40]]
+      },
+    }
+
+
+
+
+
+        # SF 1
+    return  {
+      #'l_shipdate-27_l_discount-9_o_orderdate-60': {
+      #  'lineitem': [['l_shipdate', 27], ['l_discount', 9]],
+      #  'orders': [['o_orderdate', 60]]
+      #},
+
+      #'l_shipdate-27_l_discount-9_l_shipdate-1_o_orderdate-60': {
+      #  'lineitem': [['l_shipdate', 27], ['l_discount', 9], ['l_shipdate', 1]],
+      #  'orders': [['o_orderdate', 60]]
+      #},
+
+      #'l_discount-9_l_shipdate-27_o_orderdate-60': {
+      #  'lineitem': [['l_discount', 9], ['l_shipdate', 27]],
+      #  'orders': [['o_orderdate', 60]]
+      #},
+      #'l_discount-9_l_shipdate-27_l_discount-1_o_orderdate-60': {
+      #  'lineitem': [['l_discount', 9], ['l_shipdate', 27], ['l_discount', 1]],
+      #  'orders': [['o_orderdate', 60]]
+      #},
+     #'l_shipdate-120_l_discount-2_o_orderdate-60': {
+     #   'lineitem': [['l_shipdate', 120], ['l_discount', 2]],
+     #   'orders': [['o_orderdate', 60]]
+     #},
+
+      #'l_shipdate-120_l_discount-2_l_shipdate-1_o_orderdate-60': {
+      #  'lineitem': [['l_shipdate', 120], ['l_discount', 2], ['l_shipdate', 1]],
+      #  'orders': [['o_orderdate', 60]]
+      #},
+
+      #'l_discount-2_l_shipdate-120_o_orderdate-60': {
+      #  'lineitem': [['l_discount', 2], ['l_shipdate', 120]],
+      #  'orders': [['o_orderdate', 60]]
+      #},
+      #'l_discount-2_l_shipdate-120_l_discount-1_o_orderdate-60': {
+      #  'lineitem': [['l_discount', 2], ['l_shipdate', 120], ['l_discount', 1]],
+      #  'orders': [['o_orderdate', 60]]
+      #},
+
+      #'nosort': {},
+      'default': {'lineitem': [['l_shipdate', 241]],
+  'orders': [['o_orderdate', 60]]},
+ 'l_shipdate-241_o_orderdate-16_o_orderstatus-4_o_orderdate-1': {'lineitem': [['l_shipdate',
+    241]],
+  'orders': [('o_orderdate', 16), ('o_orderstatus', 4), ('o_orderdate', 1)]},
+ 'l_shipdate-241_o_orderstatus-60_o_orderdate-1': {'lineitem': [['l_shipdate',
+    241]],
+  'orders': [('o_orderstatus', 60), ('o_orderdate', 1)]},
+ 'l_shipdate-241_o_orderdate-16_o_orderstatus-4': {'lineitem': [['l_shipdate',
+    241]],
+  'orders': [('o_orderdate', 16), ('o_orderstatus', 4)]},
+ 'l_shipdate-241_o_orderdate-60_p_name-5_p_size-2_p_type-1': {'lineitem': [['l_shipdate',
+    241]],
+  'orders': [['o_orderdate', 60]],
+  'part': [('p_name', 5), ('p_size', 2), ('p_type', 1)]},
+ 'l_shipdate-241_o_orderdate-60_p_brand-3_p_size-4_p_type-1': {'lineitem': [['l_shipdate',
+    241]],
+  'orders': [['o_orderdate', 60]],
+  'part': [('p_brand', 3), ('p_size', 4), ('p_type', 1)]},
+ 'l_shipdate-241_o_orderdate-60_p_container-3_p_size-4_p_type-1': {'lineitem': [['l_shipdate',
+    241]],
+  'orders': [['o_orderdate', 60]],
+  'part': [('p_container', 3), ('p_size', 4), ('p_type', 1)]},
+ 'l_discount-9_l_receiptdate-27_l_shipdate-1_o_orderdate-60': {'lineitem': [('l_discount',
+    9),
+   ('l_receiptdate', 27),
+   ('l_shipdate', 1)],
+  'orders': [['o_orderdate', 60]]},
+ 'l_quantity-12_l_receiptdate-21_l_shipdate-1_o_orderdate-60': {'lineitem': [('l_quantity',
+    12),
+   ('l_receiptdate', 21),
+   ('l_shipdate', 1)],
+  'orders': [['o_orderdate', 60]]},
+ 'l_receiptdate-16_l_shipdate-16_o_orderdate-60': {'lineitem': [('l_receiptdate',
+    16),
+   ('l_shipdate', 16)],
+  'orders': [['o_orderdate', 60]]},
+ 'l_shipdate-241_o_orderdate-60_c_mktsegment-6': {'lineitem': [['l_shipdate',
+    241]],
+  'orders': [['o_orderdate', 60]],
+  'customer': [('c_mktsegment', 6)]}
+    }
 
     # SF 10
     return {'default': {'lineitem': [['l_shipdate', 2400]],
@@ -69,98 +179,3 @@ class TPCHBenchmark(AbstractBenchmark):
   }
 
 
-
-    # SF 1
-    return  {
-      'l_shipdate-27_l_discount-9_o_orderdate-60': {
-        'lineitem': [['l_shipdate', 27], ['l_discount', 9]],
-        'orders': [['o_orderdate', 60]]
-      },
-
-      'l_shipdate-27_l_discount-9_l_shipdate-1_o_orderdate-60': {
-        'lineitem': [['l_shipdate', 27], ['l_discount', 9], ['l_shipdate', 1]],
-        'orders': [['o_orderdate', 60]]
-      },
-
-      'l_discount-9_l_shipdate-27_o_orderdate-60': {
-        'lineitem': [['l_discount', 9], ['l_shipdate', 27]],
-        'orders': [['o_orderdate', 60]]
-      },
-      'l_discount-9_l_shipdate-27_l_discount-1_o_orderdate-60': {
-        'lineitem': [['l_discount', 9], ['l_shipdate', 27], ['l_discount', 1]],
-        'orders': [['o_orderdate', 60]]
-      },
-    }
-
-
-    return {
-     'l_shipdate-120_l_discount-2_o_orderdate-60': {
-        'lineitem': [['l_shipdate', 120], ['l_discount', 2]],
-        'orders': [['o_orderdate', 60]]
-      },
-
-      'l_shipdate-120_l_discount-2_l_shipdate-1_o_orderdate-60': {
-        'lineitem': [['l_shipdate', 120], ['l_discount', 2], ['l_shipdate', 1]],
-        'orders': [['o_orderdate', 60]]
-      },
-
-      'l_discount-2_l_shipdate-120_o_orderdate-60': {
-        'lineitem': [['l_discount', 2], ['l_shipdate', 120]],
-        'orders': [['o_orderdate', 60]]
-      },
-      'l_discount-2_l_shipdate-120_l_discount-1_o_orderdate-60': {
-        'lineitem': [['l_discount', 2], ['l_shipdate', 120], ['l_discount', 1]],
-        'orders': [['o_orderdate', 60]]
-      },
-    }    
-
-
-
-
-
-
-
-    return {
-        'nosort': {},
-        'default': {'lineitem': [['l_shipdate', 240]],
-  'orders': [['o_orderdate', 60]]},
- 'l_shipdate-240_o_orderdate-16_o_orderstatus-4_o_orderdate-1': {'lineitem': [['l_shipdate',
-    240]],
-  'orders': [('o_orderdate', 16), ('o_orderstatus', 4), ('o_orderdate', 1)]},
- 'l_shipdate-240_o_orderstatus-60_o_orderdate-1': {'lineitem': [['l_shipdate',
-    240]],
-  'orders': [('o_orderstatus', 60), ('o_orderdate', 1)]},
- 'l_shipdate-240_o_orderdate-16_o_orderstatus-4': {'lineitem': [['l_shipdate',
-    240]],
-  'orders': [('o_orderdate', 16), ('o_orderstatus', 4)]},
- 'l_shipdate-240_o_orderdate-60_p_name-5_p_size-2_p_type-1': {'lineitem': [['l_shipdate',
-    240]],
-  'orders': [['o_orderdate', 60]],
-  'part': [('p_name', 5), ('p_size', 2), ('p_type', 1)]},
- 'l_shipdate-240_o_orderdate-60_p_brand-3_p_size-4_p_type-1': {'lineitem': [['l_shipdate',
-    240]],
-  'orders': [['o_orderdate', 60]],
-  'part': [('p_brand', 3), ('p_size', 4), ('p_type', 1)]},
- 'l_shipdate-240_o_orderdate-60_p_container-3_p_size-4_p_type-1': {'lineitem': [['l_shipdate',
-    240]],
-  'orders': [['o_orderdate', 60]],
-  'part': [('p_container', 3), ('p_size', 4), ('p_type', 1)]},
- 'l_discount-9_l_receiptdate-27_l_shipdate-1_o_orderdate-60': {'lineitem': [('l_discount',
-    9),
-   ('l_receiptdate', 27),
-   ('l_shipdate', 1)],
-  'orders': [['o_orderdate', 60]]},
- 'l_quantity-12_l_receiptdate-21_l_shipdate-1_o_orderdate-60': {'lineitem': [('l_quantity',
-    12),
-   ('l_receiptdate', 21),
-   ('l_shipdate', 1)],
-  'orders': [['o_orderdate', 60]]},
- 'l_receiptdate-16_l_shipdate-16_o_orderdate-60': {'lineitem': [('l_receiptdate',
-    16),
-   ('l_shipdate', 16)],
-  'orders': [['o_orderdate', 60]]},
- 'l_shipdate-240_o_orderdate-60_c_mktsegment-6': {'lineitem': [['l_shipdate',
-    240]],
-  'orders': [['o_orderdate', 60]],
-  'customer': [('c_mktsegment', 6)]}
-    }
