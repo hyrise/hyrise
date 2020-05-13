e5b171707b95fa9401a3ebe0395ba23ad222bd30
Markus Dreseler <markus.dreseler@hpi.de>
2020-05-13 17:54:30 +0200

    sh

--- scripts/clang_tidy_wrapper.sh
+++ scripts/clang_tidy_wrapper.sh
@@ -14,5 +14,9 @@ if grep "$file_relative_to_source" $cmake_source_dir/.clang-tidy-ignore > /dev/n
 	echo "clang-tidy: Ignoring $file_relative_to_source"
 	exit 0
 else
-	exec $(brew --prefix llvm)/bin/clang-tidy $file $@
+	if [ -x /usr/local/bin/brew ]; then
+		exec $(brew --prefix llvm)/bin/clang-tidy $file $@
+	else
+		exec clang-tidy $file $@
+	fi
 fi
