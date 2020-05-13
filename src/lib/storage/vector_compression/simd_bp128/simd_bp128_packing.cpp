99942cf7870c4718c9f94f9bbcae86409646b190
Markus Dreseler <markus.dreseler@hpi.de>
2020-05-13 17:23:42 +0200

    grr

--- src/lib/storage/vector_compression/simd_bp128/simd_bp128_packing.cpp
+++ src/lib/storage/vector_compression/simd_bp128/simd_bp128_packing.cpp
@@ -132,14 +132,14 @@ void unpack_128_zeros(uint32_t* out) {
 
 void SimdBp128Packing::write_meta_info(const uint8_t* in, uint128_t* const out) {
   const auto* const simd_in = reinterpret_cast<const simd_type*>(in);
-  auto* const simd_out = reinterpret_cast<simd_type* const>(out);
+  auto* const simd_out = reinterpret_cast<simd_type*>(out);
 
   *simd_out = *simd_in;
 }
 
 void SimdBp128Packing::read_meta_info(const uint128_t* in, uint8_t* const out) {
   const auto* const simd_in = reinterpret_cast<const simd_type*>(in);
-  auto* const simd_out = reinterpret_cast<simd_type* const>(out);
+  auto* const simd_out = reinterpret_cast<simd_type*>(out);
 
   *simd_out = *simd_in;
 }
