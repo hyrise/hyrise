251cdb671af8a9b411f88830de74cc0f8e2bed85
Markus Dreseler <markus.dreseler@hpi.de>
2020-05-14 11:04:16 +0200

    gcc

--- src/lib/utils/size_estimation_utils.hpp
+++ src/lib/utils/size_estimation_utils.hpp
@@ -10,7 +10,9 @@ namespace opossum {
  * Get the number of bytes that are allocated on the heap for the given string.
  */
 template <typename T>
+#ifdef __clang__
 __attribute__((optnone))  // Fixes issues with memcheck. As we are only accessing constant values, shouldn't cost much
+#endif
 size_t string_heap_size(const T& string) {
   // Get the default pre-allocated capacity of SSO strings. Note that the empty string has an unspecified capacity, so
   // we use a really short one here.
