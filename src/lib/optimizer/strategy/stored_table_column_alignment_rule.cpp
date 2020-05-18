241a35602899de62ae4fd9a76b007459c19a95b7
Marcel Weisgut <marcel-weisgut@web.de>
2020-05-18 18:28:31 +0200

    set of set approach, doc

--- src/lib/optimizer/strategy/stored_table_column_alignment_rule.cpp
+++ src/lib/optimizer/strategy/stored_table_column_alignment_rule.cpp
@@ -2,6 +2,9 @@
 
 #include <algorithm>
 #include <memory>
+#include <optional>
+#include <unordered_set>
+#include <vector>
 
 #include "logical_query_plan/abstract_lqp_node.hpp"
 #include "logical_query_plan/lqp_utils.hpp"
@@ -29,52 +32,56 @@ struct StoredTableNodeSharedPtrEqual final {
   }
 };
 
-template <typename Value>
-using ColumnPruningAgnosticStoredTableNodeMap =
-    std::unordered_map<std::shared_ptr<opossum::StoredTableNode>, Value, StoredTableNodeSharedPtrHash,
-                       StoredTableNodeSharedPtrEqual>;
+using ColumnPruningAgnosticSet = std::unordered_set<std::shared_ptr<opossum::StoredTableNode>,
+                                                    StoredTableNodeSharedPtrHash, StoredTableNodeSharedPtrEqual>;
+
+using ColumnPruningAgnosticMultiSet =
+    std::unordered_multiset<std::shared_ptr<opossum::StoredTableNode>, StoredTableNodeSharedPtrHash,
+                            StoredTableNodeSharedPtrEqual>;
 }  // namespace
 
 namespace opossum {
 
 void StoredTableColumnAlignmentRule::apply_to(const std::shared_ptr<AbstractLQPNode>& root) const {
-  ColumnPruningAgnosticStoredTableNodeMap<std::vector<std::shared_ptr<StoredTableNode>>>
-      gathered_stored_table_nodes_for_node;
-  ColumnPruningAgnosticStoredTableNodeMap<std::vector<ColumnID>> aligned_pruned_column_ids_for_node;
+  // stores exactly one representative for each StoredTableNode group. Members of the same StoredTableNode group
+  // have the same table name and pruned chunks.
+  auto group_representatives = ColumnPruningAgnosticSet{};
 
+  // stores all StoredTableNodes grouped by key.
+  auto grouped_stored_table_nodes = ColumnPruningAgnosticMultiSet{};
+
+  // iterate over the LQP and store all StoredTableNodes in multiple sets/groups: nodes of the same set/group have the
+  // same table name and the same pruned chunks.
   visit_lqp(root, [&](const auto& node) {
     if (node->type == LQPNodeType::StoredTable) {
       const auto stored_table_node = std::dynamic_pointer_cast<StoredTableNode>(node);
-
-      auto& gathered_stored_table_nodes = gathered_stored_table_nodes_for_node[stored_table_node];
-      gathered_stored_table_nodes.emplace_back(stored_table_node);
-
-      // update maximum superset of pruned column ids for a StoredTableNodes with the same table name and same set of
-      // pruned chunk ids
-      if (aligned_pruned_column_ids_for_node.contains(stored_table_node)) {
-        auto& aligned_pruned_column_ids = aligned_pruned_column_ids_for_node[stored_table_node];
-        std::vector<ColumnID> updated_pruned_column_ids{};
-        updated_pruned_column_ids.reserve(aligned_pruned_column_ids.size() +
-                                          stored_table_node->pruned_column_ids().size());
-        // sortedness of input containers is guaranteed by set_pruned_column_ids
-        std::set_intersection(aligned_pruned_column_ids.begin(), aligned_pruned_column_ids.end(),
-                              stored_table_node->pruned_column_ids().begin(),
-                              stored_table_node->pruned_column_ids().end(),
-                              std::back_inserter(updated_pruned_column_ids));
-        updated_pruned_column_ids.shrink_to_fit();
-        aligned_pruned_column_ids_for_node[stored_table_node] = updated_pruned_column_ids;
-      } else {
-        aligned_pruned_column_ids_for_node[stored_table_node] = stored_table_node->pruned_column_ids();
-      }
+      DebugAssert(stored_table_node, "LQPNode with type 'StoredTable' could not be casted to a StoredTableNode.");
+      group_representatives.emplace(stored_table_node);
+      grouped_stored_table_nodes.emplace(stored_table_node);
     }
     return LQPVisitation::VisitInputs;
   });
 
-  // iterate over groups of StoredTableNode and set the maximum superset of columns for each StoredTableNode in each
-  // StoredTableNode group. A group is characterized by the table name and the pruned chunk ids.
-  for (const auto& node_nodes_pair : gathered_stored_table_nodes_for_node) {
-    for (const auto& stored_table_node : node_nodes_pair.second) {
-      stored_table_node->set_pruned_column_ids(aligned_pruned_column_ids_for_node[stored_table_node]);
+  // for each group of StoredTableNodes, (1) iterate over the nodes and calculate the set intersection of pruned
+  // column ids and (2) iterate over the nodes and set the aligned pruned column ids.
+  for (const auto& group_representative : group_representatives) {
+    std::optional<std::vector<ColumnID>> aligned_pruned_column_ids;
+    auto group_range = grouped_stored_table_nodes.equal_range(group_representative);
+    for (auto iter = group_range.first; iter != group_range.second; ++iter) {
+      const auto& stored_table_node = *iter;
+      if (!aligned_pruned_column_ids) {
+        aligned_pruned_column_ids = stored_table_node->pruned_column_ids();
+      } else {
+        std::vector<ColumnID> updated_pruned_column_ids{};
+        std::set_intersection(aligned_pruned_column_ids->begin(), aligned_pruned_column_ids->end(),
+                              stored_table_node->pruned_column_ids().begin(),
+                              stored_table_node->pruned_column_ids().end(),
+                              std::back_inserter(updated_pruned_column_ids));
+        aligned_pruned_column_ids = std::move(updated_pruned_column_ids);
+      }
+    }
+    for (auto iter = group_range.first; iter != group_range.second; ++iter) {
+      (*iter)->set_pruned_column_ids(*aligned_pruned_column_ids);
     }
   }
 }
--- src/lib/optimizer/strategy/stored_table_column_alignment_rule.hpp
+++ src/lib/optimizer/strategy/stored_table_column_alignment_rule.hpp
@@ -9,15 +9,53 @@
 namespace opossum {
 
 // The PQP sub plan memoization for StoredTableNodes is sensitive to the node's table name, set of pruned chunks and
-// set of pruned columns. Consequently, for multiple nodes with the same table name, chunk and column pruning, only one
-// GetTable operator is created and executed. In some queries, the ColumnPruningRule and ChunkPruningRule provide an
-// LQP with multiple StoredTableNodes where the table names and sets of pruned chunks are equal but the sets of pruned
-// columns are different. This leads to the creation and execution of different GetTable operators.
-// TODO(Marcel) add an example, see github review feedback
+// set of pruned columns. Consequently, for multiple nodes with the same table name, same pruned chunks and same pruned
+// columns, only one GetTable operator is created and executed. In some queries, the ColumnPruningRule and
+// ChunkPruningRule provide an LQP with multiple StoredTableNodes where the table names and sets of pruned chunks are
+// equal but the sets of pruned columns are different. This leads to the creation and execution of different GetTable
+// operators instead of only one operator.
 //
 // This rule identifies StoredTableNodes that differ only in the pruned columns. It then intersects the lists of pruned
 // columns. While this means that some columns are left unpruned, it makes the job easier for the memoization in the
 // LQPTranslator. In our experiments, this has led to significant performance improvements and negligible reductions.
+//
+// Example: LQP sub plan before executing the StoredTableColumnAlignmentRule
+//
+//                      +------------------------+
+//                      | Join (1)               |
+//                      | Mode: Inner            |
+//                      | predicate: Li_a = Pa_a |
+//                      +------------------------+
+//                         /                \
+//       +------------------------+     +------------------------+
+//       | Join (2)               |     | Join (3)               |
+//       | Mode: Semi             |     | Mode: Semi             |
+//       | predicate: Li_a = Pa_a |     | predicate: Li_a = Pa_a |
+//       +------------------------+     +------------------------+
+//                   /          \           /           \
+// +---------------------+  +-----------------------+  +-----------------------+
+// | StoredTable (1)     |  | StoredTable (2)       |  | StoredTable (3)       |
+// | table name:     Li  |  | table name:     Pa    |  | table name:     Li    |
+// | pruned chunks:  { } |  | pruned chunks:  {1,2} |  | pruned chunks:  { }   |
+// | pruned columns: {3} |  | pruned columns: { }   |  | pruned columns: {3,4} |
+// +---------------------+  +-----------------------+  +-----------------------+
+//
+// Join (2) and Join (3) in the above sub plan have the same join mode and the same join predicate. Both use the same
+// input tables (Li and Pa). However, the input StoredTable (1) and (3) for the table Li are different since the set of
+// pruned columns are not equal. When an LQPNode n1 is translated, the PQP sub plan memoization reuses an operator that
+// was already created for another LQPNode n2 that is semantically equal to n1. The equality comparison of StoredTable
+// nodes is sensitive to the table name, pruned chunks and pruned columns. Consequently, Stored Table nodes (1) and (3)
+// unequal and two different GetTable operators would be created in the PQP. For LQPNode equality comparisons, the
+// input nodes are generally relevant. Joins (2) and (3) are not semantically equal since they have semantically
+// unequal input StoredTable nodes for table "Li". As a result, the LQPTranslator would create two Join operators.
+//
+// In order to create and execute less operators and therefore to reduce the execution time of the resulting PQP, this
+// rule would align the pruned columns of StoredTableNode (3): The pruned columns {3,4} are a super set of the set of
+// pruned columns for StoredTable node (1). Reducing the set of pruned columns to {3} would:
+// - still guarantee that all necessary columns for nodes on higher levels in the overall LQP are still available and
+// - make StoredTable node (1) and StoredTable node (3) semantically equal. Therefore only one GetTable operator for
+//   both nodes would be created and executed. Additionally, Join (2) and Join (3) would also be semantically equal
+//   and only one Join operator for both nodes would be created and executed.
 class StoredTableColumnAlignmentRule : public AbstractRule {
  public:
   void apply_to(const std::shared_ptr<AbstractLQPNode>& root) const override;
