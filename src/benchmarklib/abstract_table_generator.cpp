6cf9e5cc6563ee7d26f38b69e76196da6c9d17ca
Marcel Weisgut <marcel-weisgut@web.de>
2020-05-19 14:32:51 +0200

    Revert "lineitem as one big chunk"
    This reverts commit cf028cf679a1fa885b54720e8c2131588e937e3e.

--- src/benchmarklib/abstract_table_generator.cpp
+++ src/benchmarklib/abstract_table_generator.cpp
@@ -102,16 +102,9 @@ void AbstractTableGenerator::generate_and_store() {
 
       auto table_wrapper = std::make_shared<TableWrapper>(table);
       table_wrapper->execute();
-      std::shared_ptr<Sort> sort = nullptr;
-      if (table_name == "lineitem") {
-        sort = std::make_shared<Sort>(
-            table_wrapper, std::vector<SortColumnDefinition>{SortColumnDefinition{sort_column_id, order_by_mode}},
-            Chunk::MAX_SIZE, Sort::ForceMaterialization::Yes);  
-      } else {
-        sort = std::make_shared<Sort>(
+      auto sort = std::make_shared<Sort>(
           table_wrapper, std::vector<SortColumnDefinition>{SortColumnDefinition{sort_column_id, order_by_mode}},
           _benchmark_config->chunk_size, Sort::ForceMaterialization::Yes);
-      }
       sort->execute();
       const auto immutable_sorted_table = sort->get_output();
 
