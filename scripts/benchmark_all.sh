25534126def10a4b078b54191d5c067a916bebb0
Markus Dreseler <markus.dreseler@hpi.de>
2020-05-22 17:20:34 +0200

    doc

--- scripts/benchmark_all.sh
+++ scripts/benchmark_all.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-benchmarks='hyriseBenchmarkTPCH hyriseBenchmarkTPCDS hyriseBenchmarkTPCC hyriseBenchmarkJoinOrder'
-num_mt_clients=50
-
 if [ $# -ne 2 ]
 then
   echo 'This script is used to compare the performance impact of a change. Running it takes more than an hour.'
@@ -14,19 +11,24 @@ then
   exit 1
 fi
 
+benchmarks='hyriseBenchmarkTPCH hyriseBenchmarkTPCDS hyriseBenchmarkTPCC hyriseBenchmarkJoinOrder'
+num_mt_clients=50
+
+# Retrieve SHA-1 hashes from arguments (e.g., translate "master" into an actual hash)
 start_commit_reference=$1
 end_commit_reference=$2
 shift; shift
-
 start_commit=$(git rev-parse $start_commit_reference | head -n 1)
 end_commit=$(git rev-parse $end_commit_reference | head -n 1)
 
+# Check status of repository
 if [[ $(git status --untracked-files=no --porcelain) ]]
 then
   echo 'Cowardly refusing to execute on a dirty workspace'
-#  exit 1
+  exit 1
 fi
 
+# Ensure that this is run on a release build. Not really necessary, just a sanity check.
 output=$(grep 'CMAKE_BUILD_TYPE:STRING=Release' CMakeCache.txt || true)
 if [ -z "$output" ]
 then
@@ -34,6 +36,7 @@ then
   exit 1
 fi
 
+# Check whether to use ninja or make
 output=$(grep 'CMAKE_MAKE_PROGRAM' CMakeCache.txt | grep ninja || true)
 if [ ! -z "$output" ]
 then
@@ -42,26 +45,31 @@ else
   build='make'
 fi
 
+# Create the result folder if necessary
 mkdir benchmark_all_results 2>/dev/null || true
 
 build_folder=$(pwd)
 
+# Here comes the actual work
 for commit in $start_commit $end_commit
 do
+  # Skip commits where we already have all results
   if [ -f "benchmark_all_results/complete_${commit}" ]
   then
     echo "Benchmarks have already been executed for ${commit}. Skipping."
     echo "Run rm benchmark_all_results/*${commit}* to re-execute."
-    sleep 1
+    sleep 1  # Easiest way to make sure the above is read
     continue
   fi
 
+  # Checkout and build from scratch, tracking the compile time
   git checkout $commit
   git submodule update --init
   echo "Building $commit..."
   $build clean
   /usr/bin/time -p sh -c "( $build -j $(nproc) ${benchmarks} 2>&1 ) | tee benchmark_all_results/build_${commit}.log" 2>"benchmark_all_results/build_time_${commit}.txt"
 
+  # Run the benchmarks
   cd ..  # hyriseBenchmarkJoinOrder needs to run from project root 
   for benchmark in $benchmarks
   do
@@ -73,15 +81,17 @@ do
   done
   cd "${build_folder}"
 
+  # After all benchmarks are done, leave a marker so that this commit can be skipped next time
   touch "benchmark_all_results/complete_${commit}"
 done
 
-echo "Comparing ${start_commit} and ${end_commit}..."
+# Print the results
 
 echo ""
 echo "==========="
 echo ""
 
+# Print information about the system
 echo "**System**"
 echo "<details>"
 echo "<summary>$(hostname) - click to expand</summary>"
@@ -107,6 +117,7 @@ else
 fi
 echo "</details>"
 
+# Print information about the time spent building the commits
 echo ""
 echo "**Build Time**"
 echo "| commit | build time |"
@@ -116,6 +127,7 @@ cat "${build_folder}/benchmark_all_results/build_time_${start_commit}.txt" | xar
 echo -n "| ${end_commit} |"
 cat "${build_folder}/benchmark_all_results/build_time_${end_commit}.txt" | xargs | awk '{printf $0 "|\n"}'
 
+# Print information for each benchmark
 for benchmark in $benchmarks
 do
   for config in st mt
