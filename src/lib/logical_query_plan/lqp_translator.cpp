3d5e454adfa2516bac9774165ba1dcce6731b016
Keven Richly <keven.richly@hpi.uni-potsdam.de>
2020-05-22 23:14:06 +0200

    refactoring of pruned table index scan

--- src/lib/logical_query_plan/lqp_translator.cpp
+++ src/lib/logical_query_plan/lqp_translator.cpp
@@ -208,33 +208,28 @@ std::shared_ptr<AbstractOperator> LQPTranslator::_translate_predicate_node_to_in
   if (value2_variant) right_values2.emplace_back(*value2_variant);
 
   const auto stored_table_node = std::dynamic_pointer_cast<StoredTableNode>(node->left_input());
-  const auto already_pruned_chunk_ids = stored_table_node->pruned_chunk_ids();
+  const auto pruned_chunk_ids = stored_table_node->pruned_chunk_ids();
+  
   const auto table_name = stored_table_node->table_name;
   const auto table = Hyrise::get().storage_manager.get_table(table_name);
   std::vector<ChunkID> indexed_chunks;
 
   //std::vector<IndexStatistics> stored_table_node->indexes_statistics();
 
-  // Determine the chunk id in the pruned table
-  auto new_chunk_id = 0;
+  auto pruned_table_chunk_id = 0;
   
   const auto chunk_count = table->chunk_count();
   for (ChunkID chunk_id{0u}; chunk_id < chunk_count; ++chunk_id) {
-    // Determine if chunk was pruned 
-    auto pruned_chunk = false;
-    for (auto pruned_chunk_id : already_pruned_chunk_ids) {
-      if (chunk_id == pruned_chunk_id) {
-        pruned_chunk = true;
-      }
-    }
-    // If chunk not pruned check availability of GroupKey index 
-    if (!pruned_chunk){
+    // Check if vector of pruned chunk ids contains the current chunk id
+    if (!std::binary_search(pruned_chunk_ids.begin(), pruned_chunk_ids.end(), chunk_id)) {
+      // Check if segment has a GroupKey index
       const auto chunk = table->get_chunk(chunk_id);
       if (chunk && chunk->get_index(SegmentIndexType::GroupKey, column_ids)) {
-        indexed_chunks.emplace_back(new_chunk_id);
+        // Add pruned table chunk id to indexed chunks vector
+        indexed_chunks.emplace_back(pruned_table_chunk_id);
       }
-      new_chunk_id += 1;
-    } 
+      pruned_table_chunk_id += 1;
+    }
   }
 
   // All chunks that have an index on column_ids are handled by an IndexScan. All other chunks are handled by
