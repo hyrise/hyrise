17d6493900d71e4d880464ebc4e0d3bae18b5fdf
Markus Dreseler <markus.dreseler@hpi.de>
2020-05-25 08:34:34 +0200

    doc

--- src/lib/optimizer/join_ordering/abstract_join_ordering_algorithm.cpp
+++ src/lib/optimizer/join_ordering/abstract_join_ordering_algorithm.cpp
@@ -57,27 +57,10 @@ std::shared_ptr<AbstractLQPNode> AbstractJoinOrderingAlgorithm::_add_join_to_pla
     std::swap(left_lqp, right_lqp);
   }
 
-  /**
-   * Join two plans using a set of predicates; try to bring them into an efficient order.
-   *
-   * This is also done by the JoinPredicateOrderingRule, which is more general in that it also handles non-inner joins
-   * and, because of its position in the optimizer flow, optimizes subqueries that were flattened into joins. However,
-   * there are other rules between the JoinOrderingRule and the JoinPredicateOrderingRule. These depend on cardinality
-   * estimations, which currently only take the primary predicate into account (#1560). As such, we do some preliminary
-   * ordering here in order for the most selective predicate to be the primary predicate.
-   *
-   * One predicate ("primary predicate") becomes the join predicate, the others ("secondary predicates") are executed as
-   * table scans (column vs column) after the join.
-   * The primary predicate needs to be a simple "<column> <operator> <column>" predicate, otherwise the join operators
-   * won't be able to execute it.
-   *
-   * The optimality-ensuring way to order the predicates would be to find the cheapest of the predicates.size()!
-   * orders of them.
-   * For now, we just execute the scan operations in the order of increasing cost that they would have when executed
-   * directly on top of `lqp`, with the cheapest predicate becoming the primary predicate.
-   */
-
-  // Sort the predicates by increasing cost
+  // Sort the predicates by increasing cost. The optimality-ensuring way to order the predicates would be to find the
+  // cheapest of the predicates.size()! orders of them. For now, we just execute the scan operations in the order of
+  // increasing cost that they would have when executed directly on top of `lqp`, with the cheapest predicate becoming
+  // the primary predicate.
   auto join_predicates_and_cost = std::vector<std::pair<std::shared_ptr<AbstractExpression>, Cost>>{};
   join_predicates_and_cost.reserve(join_predicates.size());
   for (const auto& join_predicate : join_predicates) {
--- src/lib/optimizer/join_ordering/abstract_join_ordering_algorithm.hpp
+++ src/lib/optimizer/join_ordering/abstract_join_ordering_algorithm.hpp
@@ -12,18 +12,63 @@ class AbstractLQPNode;
 class JoinGraph;
 class CardinalityEstimationCache;
 
+/**
+ * Given a JoinGraph (created by the JoinGraphBuilder), the responsibility of a join ordering algorithm is to bring the
+ * joins into a close-to-optimal order. This means that the total cost of all joins, as estimated by the
+ * AbstractCostEstimator should be minimal.
+ *
+ * JoinOrderingAlgorithms operate on vertices, which are considered to be atomic, as described in the JoinGraph class.
+ * These may be StoredTableNodes, but could also be more complex subplans that are treated as a single table for the
+ * purpose of join ordering.
+ */
 class AbstractJoinOrderingAlgorithm {
  public:
   virtual ~AbstractJoinOrderingAlgorithm() = default;
 
- protected:
-  static std::shared_ptr<AbstractLQPNode> _add_predicates_to_plan(
-      const std::shared_ptr<AbstractLQPNode>& lqp, const std::vector<std::shared_ptr<AbstractExpression>>& predicates,
-      const std::shared_ptr<AbstractCostEstimator>& cost_estimator);
+  /*
+   * @param join_graph      A JoinGraph for a part of an LQP with further subplans as vertices. GreedyOperatorOrdering
+   *                        is only applied to this particular JoinGraph and doesn't modify the subplans in the
+   *                        vertices.
+   * @return                An LQP consisting of
+   *                         * the operations from the JoinGraph in a greedy order, trying to minimize intermediate
+   *                           cardinalities
+   *                         * the subplans from the vertices below them
+   */
+  virtual std::shared_ptr<AbstractLQPNode> operator()(const JoinGraph& join_graph,
+                                              const std::shared_ptr<AbstractCostEstimator>& cost_estimator);
 
+ protected:
+  /**
+   * Join two subplans using a set of predicates. This is called internally. For example, a greedy ordering algorithm
+   * would call this with the smallest table (more correctly: subplan) and the next bigger one.
+   *
+   * One predicate ("primary predicate") becomes the join predicate, the others ("secondary predicates") are executed as
+   * table scans (column vs column) after the join.
+   * The primary predicate needs to be a simple "<column> <operator> <column>" predicate, otherwise the join operators
+   * won't be able to execute it.
+   *
+   * Predicates are ordered so that the most selective predicate is executed first. This is also done by the
+   * JoinPredicateOrderingRule, which is more general in two regards: First, it also handles non-inner joins. Second,
+   * because of its position in the optimizer flow, optimizes subqueries that were flattened into joins. However, there
+   * are other rules between the JoinOrderingRule and the JoinPredicateOrderingRule. These depend on cardinality
+   * estimations, which currently only take the primary predicate into account (#1560). As such, we do some preliminary
+   * ordering here in order for the most selective predicate to be the primary predicate.
+   */
   static std::shared_ptr<AbstractLQPNode> _add_join_to_plan(
       std::shared_ptr<AbstractLQPNode> left_lqp, std::shared_ptr<AbstractLQPNode> right_lqp,
       const std::vector<std::shared_ptr<AbstractExpression>>& join_predicates,
+      const std::shared_ptr<AbstractCostEstimator>& cost_estimator);
+
+  /**
+   * Adds a non-join predicate, i.e., a predicate that filters the output of `lqp`, on top of `lqp` and returns
+   * the resulting node. This is called internally for predicates that are contained in the JoinGraph but do not link
+   * two subplans.
+   *
+   * Predicates are ordered so that the cheapest predicate is executed first. This has some overlap with the
+   * PredicateReorderingRule (see #1860).
+   */
+  static std::shared_ptr<AbstractLQPNode> _add_predicates_to_plan(
+      const std::shared_ptr<AbstractLQPNode>& lqp, const std::vector<std::shared_ptr<AbstractExpression>>& predicates,
       const std::shared_ptr<AbstractCostEstimator>& cost_estimator);
 };
 
