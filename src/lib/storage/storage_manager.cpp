7fdda7671338adb31442a31a2fe9de3684fed68a
martin.boissier <martin.boissier@hpi.de>
2020-06-13 14:37:17 +0200

    Simplified clustering

--- src/lib/storage/storage_manager.cpp
+++ src/lib/storage/storage_manager.cpp
@@ -243,212 +243,42 @@ void StorageManager::apply_partitioning() {
     const auto& table_name = entry.key();
     const auto& table = Hyrise::get().storage_manager.get_table(table_name);
     std::cout << "Clustering " << table_name << " according to " << config_file << std::endl;
-    const auto& dimensions = entry.value();
+    const auto& cluster_definition = entry.value();
 
-    auto partition_by_row_idx = std::vector<size_t>(table->row_count());
-    auto row_id_by_row_idx = std::vector<RowID>(table->row_count());
-
-    auto total_num_partitions = size_t{1};
-
-    for (auto dimension_id = size_t{0}; dimension_id < dimensions.size(); ++dimension_id) {
-      const auto& dimension = dimensions[dimension_id];
-      const auto partition_count = static_cast<size_t>(dimension["clusters"]);
-
-      bool partition_by_values;
-      if (dimension["mode"] == "size") {
-        partition_by_values = false;
-      } else if (dimension["mode"] == "values") {
-        std::cout << "WARNING: use 'values' clustering with care. It's not supposed to be used for the TuK exercise." << std::endl;
-        partition_by_values = true;
-      } else {
-        Fail("Unknown mode");
-      }
-
-      std::cout << "\tCalculating boundaries for " << dimension["column_name"] << std::endl;
-      Timer timer;
-
-      const auto column_id = table->column_id_by_name(dimension["column_name"]);
-      resolve_data_type(table->column_data_type(column_id), [&](auto type) {
-        using ColumnDataType = typename decltype(type)::type;
-
-        auto materialized = std::vector<std::pair<ColumnDataType, size_t>>{};
-        materialized.reserve(table->row_count());
-
-        {
-          auto row_idx = size_t{0};
-          for (auto chunk_id = ChunkID{0}; chunk_id < table->chunk_count(); ++chunk_id) {
-            const auto& chunk = table->get_chunk(chunk_id);
-            const auto& segment = chunk->get_segment(column_id);
-
-            segment_iterate<ColumnDataType>(*segment, [&](const auto& position) {
-              Assert(!position.is_null(), "Clustering on NULL values not yet supported");
-              materialized.emplace_back(std::pair<ColumnDataType, size_t>{position.value(), row_idx});
-              row_id_by_row_idx[row_idx] = RowID{chunk_id, position.chunk_offset()};
-
-              ++row_idx;
-            });
-          }
-        }
-
-        std::random_device rd;
-        std::mt19937 g(rd());
-        std::shuffle(materialized.begin(), materialized.end(), g);
-        std::sort(materialized.begin(), materialized.end());
-
-        auto distinct_values = std::vector<ColumnDataType>{};
-
-        if (partition_count == 1) {
-          std::cout << "WARNING: be cautious when using a cluster split count of 1" << std::endl;
-        }
-
-        if (partition_by_values) {
-          distinct_values.resize(materialized.size());
-          for (auto i = size_t{0}; i < materialized.size(); ++i) {
-            distinct_values[i] = materialized[i].first;
-          }
-          distinct_values.erase(std::unique(distinct_values.begin(), distinct_values.end()), distinct_values.end());
-          Assert(partition_count <= distinct_values.size(), "More clusters requested than distinct values found");
-        } else {
-          Assert(partition_count <= materialized.size(), "More clusters requested than rows in table");
-        }
-
-        total_num_partitions *= partition_count;
-
-        auto materialized_idx = size_t{0};
-        for (auto partition_id = size_t{0}; partition_id < partition_count; ++partition_id) {
-          ColumnDataType first_value_in_next_partition{};
-          if (partition_by_values) {
-            first_value_in_next_partition = distinct_values[std::min(
-                (partition_id + 1) * distinct_values.size() / partition_count, distinct_values.size() - 1)];
-          }
-
-          auto current_partition_size = size_t{0};
-
-          if (partition_by_values) {
-            std::cout << "\t\tfrom value '" << materialized[materialized_idx].first << "' to value '";
-          } else {
-            std::cout << "\t\tfrom row " << materialized_idx << " to row ";
-          }
-
-          while (materialized_idx < materialized.size()) {
-            const auto& [value, row_idx] = materialized[materialized_idx];
-
-            // Shift existing partitions
-            partition_by_row_idx[row_idx] *= partition_count;
-
-            // Set partition of this
-            partition_by_row_idx[row_idx] += partition_id;
-
-            ++materialized_idx;
-            ++current_partition_size;
-
-            if (partition_by_values && materialized[materialized_idx].first == first_value_in_next_partition &&
-                (partition_id < partition_count - 1 || materialized_idx == materialized.size() - 1)) {
-              std::cout << materialized[materialized_idx - 1].first << "' (" << current_partition_size << " rows)"
-                        << std::endl;
-              first_value_in_next_partition = distinct_values[std::min(
-                  (partition_id + 1) * distinct_values.size() / partition_count, distinct_values.size() - 1)];
-              break;
-            }
-
-            if (!partition_by_values && ((current_partition_size >= materialized.size() / partition_count &&
-                                          partition_id != partition_count - 1) ||
-                                         (materialized_idx == materialized.size() - 1))) {
-              std::cout << materialized_idx << " (" << current_partition_size << " rows)" << std::endl;
-              break;
-            }
-          }
-        }
-      });
-
-      std::cout << "\t\tdone (" << timer.lap_formatted() << ")" << std::endl;
-
-      std::cout << "################" << std::endl;
-      for (const auto& t : partition_by_row_idx) {
-        std::cout << t << std::endl;
-      }
-      std::cout << "################" << std::endl;
+    auto sort_column_definitions = std::vector<SortColumnDefinition>{};
+    for (const auto& column_name : cluster_definition["cluster_columns"]) {
+      const auto column_id = table->column_id_by_name(column_name);
+      sort_column_definitions.emplace_back(SortColumnDefinition{column_id, SortMode::Ascending});
     }
 
-    // Write segments
-    auto segments_by_partition = std::vector<Segments>(total_num_partitions, Segments(table->column_count()));
+    auto new_table = std::shared_ptr<const Table>{};
     {
-      std::cout << "\tWriting clustered columns in parallel" << std::flush;
+      /**
+       * CLUSTERING by using the sort operator
+       */
+      std::cout << "Clustering table " << table_name << std::flush;
       Timer timer;
 
-      auto threads = std::vector<std::thread>{};
-      for (auto column_id = ColumnID{0}; column_id < table->column_count(); ++column_id) {
-        threads.emplace_back(std::thread([&, column_id] {
-          resolve_data_type(table->column_data_type(column_id), [&](auto type) {
-            using ColumnDataType = typename decltype(type)::type;
-
-            auto original_dictionary_segments =
-                std::vector<std::shared_ptr<const DictionarySegment<ColumnDataType>>>(table->chunk_count());
-            for (auto chunk_id = ChunkID{0}; chunk_id < table->chunk_count(); ++chunk_id) {
-              const auto& original_chunk = table->get_chunk(chunk_id);
-              const auto& original_segment = original_chunk->get_segment(column_id);
-              const auto& original_dictionary_segment =
-                  std::static_pointer_cast<const DictionarySegment<ColumnDataType>>(original_segment);
-
-              original_dictionary_segments[chunk_id] = original_dictionary_segment;
-            }
-
-            auto values_by_partition = std::vector<pmr_vector<ColumnDataType>>(total_num_partitions);
-            for (auto& values : values_by_partition) {
-              values.reserve(table->row_count() / std::max(1ul, (total_num_partitions - 1)));
-            }
-
-            const auto row_count = table->row_count();
-            for (auto row_idx = size_t{0}; row_idx < row_count; ++row_idx) {
-              const auto [chunk_id, chunk_offset] = row_id_by_row_idx[row_idx];
-              const auto partition_id = partition_by_row_idx[row_idx];
-
-              const auto& original_dictionary_segment = original_dictionary_segments[chunk_id];
-
-              values_by_partition[partition_id].emplace_back(
-                  *original_dictionary_segment->get_typed_value(chunk_offset));
-            }
-
-            for (auto partition_id = size_t{0}; partition_id < total_num_partitions; ++partition_id) {
-              auto value_segment =
-                  std::make_shared<ValueSegment<ColumnDataType>>(std::move(values_by_partition[partition_id]));
-              segments_by_partition[partition_id][column_id] = value_segment;
-            }
-          });
-        }));
-      }
-      for (auto& thread : threads) thread.join();
+      auto table_cluster_wrapper = std::make_shared<TableWrapper>(table);
+      table_cluster_wrapper->execute();
+      auto table_cluster_sort = std::make_shared<Sort>(table_cluster_wrapper, sort_column_definitions,
+                                                    Chunk::DEFAULT_SIZE, Sort::ForceMaterialization::Yes);
+      table_cluster_sort->execute();
+      new_table = table_cluster_sort->get_output();
 
       std::cout << " - done (" << timer.lap_formatted() << ")" << std::endl;
     }
 
-    std::cout << "$$$$$$$$$$$$$$$$$$$" << std::endl;
-    for (const auto& t : partition_by_row_idx) {
-      std::cout << t << std::endl;
-    }
-    std::cout << "$$$$$$$$$$$$$$$$$$$" << std::endl;
-
-    Print::print(table);
-
-    // Write new table
-    auto new_table = std::make_shared<Table>(table->column_definitions(), TableType::Data, std::nullopt, UseMvcc::Yes);
-    for (auto partition_id = size_t{0}; partition_id < total_num_partitions; ++partition_id) {
-      const auto& segments = segments_by_partition[partition_id];
-      if (segments[0]->size() == 0) continue;
-      // Note that this makes all rows that have been deleted visible again
-      auto mvcc_data = std::make_shared<MvccData>(segments[0]->size(), CommitID{0});
-      new_table->append_chunk(segments, mvcc_data);
-      new_table->last_chunk()->finalize();
-    }
-
+    auto sorted_table = std::make_shared<Table>(new_table->column_definitions(), TableType::Data, Chunk::DEFAULT_SIZE, UseMvcc::Yes);
+    const auto sort_column_name = cluster_definition["sort_column"];
     {
       /**
        * SORTING each cluster by the last cluster column
        */
-      std::cout << "Sorting each chunk individually by " << dimensions[dimensions.size() - 1]["column_name"] << std::flush;
+      std::cout << "Sorting each chunk individually by " << sort_column_name << std::flush;
       Timer timer;
 
-      const auto sort_column_id = table->column_id_by_name(dimensions[dimensions.size() - 1]["column_name"]);
+      const auto sort_column_id = table->column_id_by_name(sort_column_name);
 
       auto get_segments_of_chunk = [&](const std::shared_ptr<const Table>& input_table, ChunkID chunk_id){
         Segments segments{};
@@ -458,7 +288,7 @@ void StorageManager::apply_partitioning() {
         return segments;
       };
 
-      auto sorted_table = std::make_shared<Table>(new_table->column_definitions(), TableType::Data, Chunk::DEFAULT_SIZE * 17, UseMvcc::Yes);
+      
       const auto chunk_count = new_table->chunk_count();
 
       for (auto chunk_id = ChunkID{0}; chunk_id < chunk_count; ++chunk_id) {
@@ -485,8 +315,6 @@ void StorageManager::apply_partitioning() {
         added_chunk->set_sorted_by(SortColumnDefinition(sort_column_id, SortMode::Ascending));
       }
 
-      new_table = sorted_table;
-
       std::cout << " - done (" << timer.lap_formatted() << ")" << std::endl;
     }
 
@@ -498,7 +326,7 @@ void StorageManager::apply_partitioning() {
       // Not using JobTasks here because we want parallelism even if the scheduler is disabled.
       auto next_chunk_id = std::atomic_uint{0};
       const auto thread_count =
-          std::min(static_cast<uint>(new_table->chunk_count()), std::thread::hardware_concurrency() + 1);
+          std::min(static_cast<uint>(sorted_table->chunk_count()), std::thread::hardware_concurrency() + 1);
       auto threads = std::vector<std::thread>{};
       threads.reserve(thread_count);
 
@@ -506,10 +334,10 @@ void StorageManager::apply_partitioning() {
         threads.emplace_back([&] {
           while (true) {
             auto my_chunk_id = next_chunk_id++;
-            if (my_chunk_id >= new_table->chunk_count()) return;
+            if (my_chunk_id >= sorted_table->chunk_count()) return;
 
-            const auto chunk = new_table->get_chunk(ChunkID{my_chunk_id});
-            ChunkEncoder::encode_chunk(chunk, new_table->column_data_types(),
+            const auto chunk = sorted_table->get_chunk(ChunkID{my_chunk_id});
+            ChunkEncoder::encode_chunk(chunk, sorted_table->column_data_types(),
                                        SegmentEncodingSpec{EncodingType::Dictionary});
           }
         });
@@ -520,13 +348,11 @@ void StorageManager::apply_partitioning() {
       std::cout << " - done (" << timer.lap_formatted() << ")" << std::endl;
     }
 
-    Print::print(new_table);
-
     {
       std::cout << "Generating statistics" << std::flush;
       Timer timer;
       drop_table(table_name);
-      add_table(table_name, new_table);
+      add_table(table_name, sorted_table);
       std::cout << " - done (" << timer.lap_formatted() << ")" << std::endl;
     }
   }
