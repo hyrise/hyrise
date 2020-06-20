6b294ee276f5236c80336491f4ab3ebf07b825a5
dey4ss <27929897+dey4ss@users.noreply.github.com>
2020-06-20 10:47:30 +0200

    add further tests

--- src/lib/operators/visit_pqp.hpp
+++ src/lib/operators/visit_pqp.hpp
@@ -1,9 +1,6 @@
 #pragma once
 
-#include <memory>
-#include <optional>
 #include <queue>
-#include <set>
 #include <unordered_set>
 
 #include "operators/abstract_operator.hpp"
@@ -14,11 +11,11 @@ enum class PQPVisitation { VisitInputs, DoNotVisitInputs };
 
 /**
  * Calls the passed @param visitor on @param pqp and recursively on its INPUTS.
- * The visitor returns `PQPVisitation`, indicating whether the current nodes's input should be visited
+ * The visitor returns `PQPVisitation`, indicating whether the current operator's input should be visited
  * as well. The algorithm is breadth-first search.
- * Each node is visited exactly once.
+ * Each operator is visited exactly once.
  *
- * @tparam Visitor      Functor called with every node as a param.
+ * @tparam Visitor      Functor called with every operator as a param.
  *                      Returns `PQPVisitation`
  */
 template <typename Operator, typename Visitor>
@@ -37,7 +34,7 @@ void visit_pqp(const std::shared_ptr<Operator>& lqp, Visitor visitor) {
     if (!visited_operators.emplace(op).second) continue;
 
     if (visitor(op) == PQPVisitation::VisitInputs) {
-      if constexpr (std::is_const<AbstractOperatorType>::value) {
+      if constexpr (std::is_const_v<AbstractOperatorType>) {
         if (op->left_input()) operator_queue.push(op->left_input());
         if (op->right_input()) operator_queue.push(op->right_input());
       } else {
--- src/test/lib/operators/visit_pqp_test.cpp
+++ src/test/lib/operators/visit_pqp_test.cpp
@@ -49,4 +49,32 @@ TEST_F(VisitPQPTest, DiamondStructure) {
   EXPECT_EQ(actual_nodes, expected_nodes);
 }
 
+TEST_F(VisitPQPTest, NonConstOperators) {
+  auto node_b = std::make_shared<Limit>(node_a, to_expression(int64_t{1}));
+  const auto expected_nodes = std::vector<std::shared_ptr<AbstractOperator>>{node_b, node_a};
+
+  auto actual_nodes = std::vector<std::shared_ptr<AbstractOperator>>{};
+  visit_pqp(expected_nodes[0], [&](const auto& node) {
+    actual_nodes.emplace_back(node);
+    node->clear_output();
+    return PQPVisitation::VisitInputs;
+  });
+
+  EXPECT_EQ(actual_nodes, expected_nodes);
+}
+
+TEST_F(VisitPQPTest, ConstOperators) {
+  auto node_b = std::make_shared<const GetTable>("bar");
+  auto node_c = std::make_shared<const Limit>(node_b, to_expression(int64_t{1}));
+  const auto expected_nodes = std::vector<std::shared_ptr<const AbstractOperator>>{node_c, node_b};
+
+  auto actual_nodes = std::vector<std::shared_ptr<const AbstractOperator>>{};
+  visit_pqp(expected_nodes[0], [&](const auto& node) {
+    actual_nodes.emplace_back(node);
+    return PQPVisitation::VisitInputs;
+  });
+
+  EXPECT_EQ(actual_nodes, expected_nodes);
+}
+
 }  // namespace opossum
