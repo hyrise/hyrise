54506bb032613c48ebff7b6dd384bf88a57e1f4c
Jan Kossmann <jan.kossmann@hpi.de>
2020-07-03 09:20:39 +0200

    Squashed commit of the following:
    commit 2aef19155e78099b0b16d33b96e7f856b6d96966
    Author: Jan Kossmann <jan.kossmann@hpi.de>
    Date:   Fri Jul 3 09:20:11 2020 +0200
    
        ah
    
    commit e6d35484eaada2b75600f13ce9b45d6437ad811d
    Author: Jan Kossmann <jan.kossmann@hpi.de>
    Date:   Thu Jul 2 18:47:34 2020 +0200
    
        improve
    
    commit e25da211600f12843290f496b6dab22909a5b38f
    Author: Jan Kossmann <jan.kossmann@hpi.de>
    Date:   Thu Jul 2 18:11:53 2020 +0200
    
        Revert "Remove it"
    
        This reverts commit 14d2a66aac60c9f5fa8c0c5c0cdee9dec1e6b72e.
    
    commit 14d2a66aac60c9f5fa8c0c5c0cdee9dec1e6b72e
    Author: Jan Kossmann <jan.kossmann@hpi.de>
    Date:   Thu Jul 2 14:32:46 2020 +0200
    
        Remove it

---
+++ src/lib/optimizer/strategy/null_scan_removal_rule.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "abstract_rule.hpp"
+
+namespace opossum {
+
+class AbstractLQPNode;
+class PredicateNode;
+
+class NullScanRemovalRule : public AbstractRule {
+ public:
+  void apply_to(const std::shared_ptr<AbstractLQPNode>& root) const override;
+private:
+  void _remove_nodes(const std::vector<std::shared_ptr<AbstractLQPNode>>& nodes) const;
+};
+
+}  // namespace opossum
