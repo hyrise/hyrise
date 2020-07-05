2a90325c54979cf5da4b69675aa67e675b1f20d4
Julian Menzler <julianmenzler@gmail.com>
2020-07-05 17:24:13 +0200

    Remove code

--- src/lib/constraints/lqp_unique_constraint.hpp
+++ src/lib/constraints/lqp_unique_constraint.hpp
@@ -22,19 +22,3 @@ struct LQPUniqueConstraint final {
 using LQPUniqueConstraints = std::vector<LQPUniqueConstraint>;
 
 }  // namespace opossum
-
-namespace std {
-
-// TODO(Julian) Doc: why do we need this?
-template <>
-struct hash<opossum::LQPUniqueConstraint> {
-  size_t operator()(const opossum::LQPUniqueConstraint& unique_constraint) const {
-    auto hash = boost::hash_value(unique_constraint.expressions.size());
-    for (const auto& expression : unique_constraint.expressions) {
-      boost::hash_combine(hash, expression->hash());
-    }
-    return hash;
-  }
-};
-
-}  // namespace std
