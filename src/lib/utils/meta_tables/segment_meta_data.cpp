e724f2a84586b4c377a15312ab29441772a671ab
martin.boissier <martin.boissier@hpi.de>
2020-07-07 13:02:07 +0200

    Add string data to meta_segments_accurate

--- src/lib/utils/meta_tables/segment_meta_data.cpp
+++ src/lib/utils/meta_tables/segment_meta_data.cpp
@@ -18,7 +18,45 @@ void gather_segment_meta_data(const std::shared_ptr<Table>& meta_table, const Me
 
         const auto data_type = pmr_string{data_type_to_string.left.at(table->column_data_type(column_id))};
 
+        std::vector<size_t> string_lengths(chunk->size());
+        auto value_switches = size_t{0};
+        resolve_data_and_segment_type(*segment, [&](auto segment_data_type, auto& typed_segment) {
+          using ColumnDataType = typename decltype(segment_data_type)::type;
+
+          auto write_index = size_t{0};
+          ColumnDataType previous_value = {};
+
+          auto iterable = create_iterable_from_segment<ColumnDataType>(typed_segment);
+          iterable.for_each([&](const auto& value) {
+            if constexpr (std::is_same_v<ColumnDataType, pmr_string>) {
+              string_lengths[write_index] = value.value().length();
+            }
+
+            // Execute if first iterat
+            if (write_index == 0) {
+              previous_value = value.value();
+              ++write_index;
+              return;
+            }
+
+            if (previous_value != value.value()) {
+              ++value_switches;
+              previous_value = value.value();
+            }
+
+            ++write_index;
+          });
+        });
+
         const auto estimated_size = segment->memory_usage(mode);
+
+        AllTypeVariant avg_string_length = NULL_VALUE;
+        AllTypeVariant max_string_length = NULL_VALUE;
+        if (data_type == "string") {
+          avg_string_length = static_cast<int64_t>(std::accumulate(string_lengths.cbegin(), string_lengths.cend(), 0ul) / string_lengths.size());
+          max_string_length = static_cast<int64_t>(*std::max_element(string_lengths.cbegin(), string_lengths.cend()));
+        }
+
         AllTypeVariant encoding = NULL_VALUE;
         AllTypeVariant vector_compression = NULL_VALUE;
         if (const auto& encoded_segment = std::dynamic_pointer_cast<AbstractEncodedSegment>(segment)) {
@@ -38,6 +76,9 @@ void gather_segment_meta_data(const std::shared_ptr<Table>& meta_table, const Me
           meta_table->append({pmr_string{table_name}, static_cast<int32_t>(chunk_id), static_cast<int32_t>(column_id),
                               pmr_string{table->column_name(column_id)}, data_type, distinct_value_count, encoding,
                               vector_compression, static_cast<int64_t>(estimated_size),
+                              static_cast<int64_t>(value_switches),
+                              avg_string_length,
+                              max_string_length,
                               static_cast<int64_t>(access_counter[SegmentAccessCounter::AccessType::Point]),
                               static_cast<int64_t>(access_counter[SegmentAccessCounter::AccessType::Sequential]),
                               static_cast<int64_t>(access_counter[SegmentAccessCounter::AccessType::Monotonic]),
@@ -46,6 +87,9 @@ void gather_segment_meta_data(const std::shared_ptr<Table>& meta_table, const Me
           meta_table->append({pmr_string{table_name}, static_cast<int32_t>(chunk_id), static_cast<int32_t>(column_id),
                               pmr_string{table->column_name(column_id)}, data_type, encoding, vector_compression,
                               static_cast<int64_t>(estimated_size),
+                              static_cast<int64_t>(value_switches),
+                              avg_string_length,
+                              max_string_length,
                               static_cast<int64_t>(access_counter[SegmentAccessCounter::AccessType::Point]),
                               static_cast<int64_t>(access_counter[SegmentAccessCounter::AccessType::Sequential]),
                               static_cast<int64_t>(access_counter[SegmentAccessCounter::AccessType::Monotonic]),
