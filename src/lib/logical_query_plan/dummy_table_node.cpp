5e9e4cfe3a10f4a46d71d8380dd5cd962785196e
Julian Menzler <julianmenzler@gmail.com>
2020-07-08 15:37:02 +0200

    Fix DummyTableNode

--- src/lib/logical_query_plan/dummy_table_node.cpp
+++ src/lib/logical_query_plan/dummy_table_node.cpp
@@ -10,9 +10,19 @@
 
 namespace opossum {
 
-DummyTableNode::DummyTableNode() : AbstractNonQueryNode(LQPNodeType::DummyTable) {}
+DummyTableNode::DummyTableNode() : AbstractLQPNode(LQPNodeType::DummyTable) {}
 
 std::string DummyTableNode::description(const DescriptionMode mode) const { return "[DummyTable]"; }
+
+std::vector<std::shared_ptr<AbstractExpression>> DummyTableNode::output_expressions() const { return {}; }
+
+bool DummyTableNode::is_column_nullable(const ColumnID column_id) const {
+  Fail("DummyTable does not output any columns");
+}
+
+std::shared_ptr<LQPUniqueConstraints> DummyTableNode::unique_constraints() const {
+  return std::make_shared<LQPUniqueConstraints>();
+};
 
 std::shared_ptr<AbstractLQPNode> DummyTableNode::_on_shallow_copy(LQPNodeMapping& node_mapping) const {
   return std::make_shared<DummyTableNode>();
--- src/lib/logical_query_plan/dummy_table_node.hpp
+++ src/lib/logical_query_plan/dummy_table_node.hpp
@@ -4,7 +4,7 @@
 #include <string>
 #include <vector>
 
-#include "abstract_non_query_node.hpp"
+#include "abstract_lqp_node.hpp"
 
 namespace opossum {
 
@@ -12,11 +12,15 @@ namespace opossum {
  * This node type represents a dummy table that is used to project literals.
  * See Projection::DummyTable for more details.
  */
-class DummyTableNode : public EnableMakeForLQPNode<DummyTableNode>, public AbstractNonQueryNode {
+class DummyTableNode : public EnableMakeForLQPNode<DummyTableNode>, public AbstractLQPNode {
  public:
   DummyTableNode();
 
   std::string description(const DescriptionMode mode = DescriptionMode::Short) const override;
+
+  std::vector<std::shared_ptr<AbstractExpression>> output_expressions() const override;
+  bool is_column_nullable(const ColumnID column_id) const override;
+  std::shared_ptr<LQPUniqueConstraints> unique_constraints() const override;
 
  protected:
   std::shared_ptr<AbstractLQPNode> _on_shallow_copy(LQPNodeMapping& node_mapping) const override;
