3d8dcdfc4195832badb82da797a6b974e4cb8e72
Julian Menzler <julianmenzler@gmail.com>
2020-07-12 22:03:58 +0200

    Simplify impl

--- src/test/utils/constraint_test_utils.hpp
+++ src/test/utils/constraint_test_utils.hpp
@@ -10,33 +10,29 @@ namespace opossum {
  * Verifies whether a given table key constraint is represented in a given set of unique constraints.
  */
 static bool find_unique_constraint_by_key_constraint(const TableKeyConstraint& table_key_constraint,
-                                                     std::shared_ptr<LQPUniqueConstraints> unique_constraints) {
-  return std::find_if(unique_constraints->cbegin(), unique_constraints->cend(),
-                      [&](const LQPUniqueConstraint& unique_constraint) {
-                        // Basic comparison: Column count
-                        if (table_key_constraint.columns().size() != unique_constraint.expressions.size()) return false;
+                                                     const std::shared_ptr<LQPUniqueConstraints>& unique_constraints) {
+  const auto column_ids = table_key_constraint.columns();
 
-                        // In-depth comparison: Column IDs
-                        for (const auto& column_id : table_key_constraint.columns()) {
-                          // Find column expression for column id
-                          const auto& find_column_expression = std::find_if(
-                              unique_constraint.expressions.cbegin(), unique_constraint.expressions.cend(),
-                              [&column_id](const auto& expression) {
-                                const auto& column_expression =
-                                    std::dynamic_pointer_cast<LQPColumnExpression>(expression);
-                                return (column_expression && column_expression->original_column_id == column_id);
-                              });
+  for (const auto& unique_constraint : *unique_constraints) {
+    // Basic comparison: Column count
+    if (column_ids.size() != unique_constraint.expressions.size()) continue;
 
-                          // Check whether a column expression has been found
-                          if (find_column_expression == unique_constraint.expressions.cend()) {
-                            // unique constraint does not match
-                            return false;
-                          }
-                        }
+    // In-depth comparison: Column IDs
+    auto unique_constraint_column_ids = std::unordered_set<ColumnID>();
+    for (const auto& expression : unique_constraint.expressions) {
+      const auto& column_expression = std::dynamic_pointer_cast<LQPColumnExpression>(expression);
+      if (column_expression) {
+        unique_constraint_column_ids.emplace(column_expression->original_column_id);
+      }
+    }
 
-                        // unique_constraint represents table_key_constraint since none of the above checks have failed
-                        return true;
-                      }) != unique_constraints->cend();
+    if (unique_constraint_column_ids == column_ids) {
+      return true;
+    }
+  }
+
+  // Did not find a matching unique constraint
+  return false;
 }
 
 }  // namespace opossum
