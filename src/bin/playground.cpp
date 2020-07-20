ac9cc127dea58aee70c3d17b70878f25fa0369e2
martin.boissier <martin.boissier@hpi.de>
2020-07-20 17:35:34 +0200

    Add back code necessary to avoid linking fuck up

--- src/bin/playground.cpp
+++ src/bin/playground.cpp
@@ -10,7 +10,6 @@
 #include "sql/sql_pipeline_builder.hpp"
 #include "tpch/tpch_benchmark_item_runner.hpp"
 #include "tpch/tpch_table_generator.hpp"
-#include "tpcds/tpcds_table_generator.hpp"
 
 using namespace opossum;  // NOLINT
 
@@ -160,6 +159,104 @@ int main(int argc, const char* argv[]) {
       size_result.close();
     }
   }
+
+  const auto BENCHMARKS = std::vector<std::string>{"TPC-H", "TPC-DS", "JOB"}; 
+
+  const auto env_var = std::getenv("BENCHMARK_TO_RUN");
+  if (env_var == nullptr) {
+    std::cerr << "Please pass environment variable \"BENCHMARK_TO_RUN\" to set a target benchmark.\nExiting Plugin." << std::flush;
+    exit(17);
+  } else if (strncmp(env_var, "foobar", 6ul)) {
+
+    /**
+     *
+     *    WE ARE DONE HERE.
+        Just some code copied to ensure that benchmarklib does include all necessary code. Other solutions include some unnice CMakeList modifications ...
+     *
+     */
+    exit(0);
+  }
+
+  auto BENCHMARK = std::string(env_var);
+  if (std::find(BENCHMARKS.begin(), BENCHMARKS.end(), BENCHMARK) == BENCHMARKS.end()) {
+    std::cerr << "Benchmark \"" << BENCHMARK << "\" not supported. Supported benchmarks: ";
+    for (const auto& benchmark : BENCHMARKS) std::cout << "\"" << benchmark << "\" ";
+    std::cerr << "\nExiting." << std::flush;
+    exit(17);
+  }
+  std::cout << "Running " << BENCHMARK << " ... " << std::endl;
+
+  auto config = std::make_shared<BenchmarkConfig>(BenchmarkConfig::get_default_config());
+  config->max_runs = 10;
+  config->enable_visualization = false;
+  config->chunk_size = 100'000;
+  config->cache_binary_tables = true;
+
+  constexpr auto USE_PREPARED_STATEMENTS = false;
+  auto SCALE_FACTOR = 17.0f;  // later overwritten
+
+
+  //
+  //  TPC-H
+  //
+  if (BENCHMARK == "TPC-H") {
+    SCALE_FACTOR = 1.0f;
+    config->max_runs = 100;
+    // const std::vector<BenchmarkItemID> tpch_query_ids_benchmark = {BenchmarkItemID{5}};
+    // auto item_runner = std::make_ unique<TPCHBenchmarkItemRunner>(config, USE_PREPARED_STATEMENTS, SCALE_FACTOR, tpch_query_ids_benchmark);
+    auto item_runner = std::make_unique<TPCHBenchmarkItemRunner>(config, USE_PREPARED_STATEMENTS, SCALE_FACTOR);
+    auto benchmark_runner = std::make_shared<BenchmarkRunner>(
+        *config, std::move(item_runner), std::make_unique<TPCHTableGenerator>(SCALE_FACTOR, config), BenchmarkRunner::create_context(*config));
+    Hyrise::get().benchmark_runner = benchmark_runner;
+    benchmark_runner->run();
+  }
+  //
+  //  /TPC-H
+  //
+
+
+  //
+  //  TPC-DS
+  //
+  else if (BENCHMARK == "TPC-DS") {
+    SCALE_FACTOR = 1.0f;
+    config->max_runs = 1;
+    const std::string query_path = "hyrise/resources/benchmark/tpcds/tpcds-result-reproduction/query_qualification";
+
+    auto query_generator = std::make_unique<FileBasedBenchmarkItemRunner>(config, query_path, std::unordered_set<std::string>{});
+    auto table_generator = std::make_unique<TpcdsTableGenerator>(SCALE_FACTOR, config);
+    auto benchmark_runner = std::make_shared<BenchmarkRunner>(*config, std::move(query_generator), std::move(table_generator),
+                                                              opossum::BenchmarkRunner::create_context(*config));
+    Hyrise::get().benchmark_runner = benchmark_runner;
+    benchmark_runner->run();
+  }
+  //
+  //  /TPC-DS
+  //
+
+  //
+  //  JOB
+  //
+  else if (BENCHMARK == "JOB") {
+    config->max_runs = 1;
+
+    const auto table_path = "hyrise/imdb_data";
+    const auto query_path = "hyrise/third_party/join-order-benchmark";
+    const auto non_query_file_names = std::unordered_set<std::string>{"fkindexes.sql", "schema.sql"};
+
+    auto benchmark_item_runner = std::make_unique<FileBasedBenchmarkItemRunner>(config, query_path, non_query_file_names);
+    auto table_generator = std::make_unique<FileBasedTableGenerator>(config, table_path);
+    auto benchmark_runner = std::make_shared<BenchmarkRunner>(*config, std::move(benchmark_item_runner), std::move(table_generator),
+                                                              BenchmarkRunner::create_context(*config));
+
+    Hyrise::get().benchmark_runner = benchmark_runner;
+    benchmark_runner->run();
+  }
+  //
+  //  /JOB
+  //
+
+  return 0;
   return 0;
 
 }
