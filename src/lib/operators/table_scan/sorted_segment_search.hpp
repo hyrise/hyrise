8159ffa249a44d2ab9a873344afaf2c698f1207b
Jan Kossmann <jan.kossmann@hpi.de>
2020-08-07 19:50:32 +0200

    Add fix (check for EntireChunkPosList)

--- src/lib/operators/table_scan/sorted_segment_search.hpp
+++ src/lib/operators/table_scan/sorted_segment_search.hpp
@@ -281,7 +281,7 @@ class SortedSegmentSearch {
      * ChunkOffsets in position_filter are not necessarily continuous. The same is true for
      * NotEquals because the result might consist of 2 ranges.
      */
-    if (position_filter || _predicate_condition == PredicateCondition::NotEquals) {
+    if ((position_filter && !std::dynamic_pointer_cast<const EntireChunkPosList>(position_filter)) || _predicate_condition == PredicateCondition::NotEquals) {
       for (; begin != end; ++begin) {
         matches[output_idx++] = RowID{chunk_id, begin->chunk_offset()};
       }
