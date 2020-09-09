74d93f4202e86d57a5453fd7a70dcdc4f545b588
Alexander Loeser <Alexander.Loeser@student.hpi.de>
2020-09-09 09:12:21 +0200

    boundaries: consider number of null values

--- src/plugins/clustering/disjoint_clusters_algo.cpp
+++ src/plugins/clustering/disjoint_clusters_algo.cpp
@@ -47,19 +47,18 @@ ClusterBoundaries DisjointClustersAlgo::_get_boundaries(const std::shared_ptr<co
   Assert(num_clusters <= histogram->bin_count(), "more clusters (" + std::to_string(num_clusters) + ") than histogram bins (" + std::to_string(histogram->bin_count()) + ")");
 
   // TODO: is that a good estimation?
-  auto num_null_values = row_count - histogram->total_count();
+  auto num_null_values = row_count - static_cast<size_t>(histogram->total_count());
   ClusterBoundaries boundaries;
   if (nullable) {
     boundaries.push_back(std::make_pair(NULL_VALUE, NULL_VALUE));
   } else {
     // For SF 10, there seems to be a bug that causes histograms to contain more entries than the table has values
-    num_null_values = std::max(num_null_values, 0.0f); // TODO use this variable
+    num_null_values = std::max(num_null_values, size_t{0});
   }
   // add the first non-null cluster boundaries. The boundary values will be set later
   boundaries.push_back(std::make_pair(NULL_VALUE, NULL_VALUE));
 
-  // TODO: consider the number of null values?
-  const auto ideal_rows_per_cluster = std::max(size_t{1}, row_count / num_clusters);
+  const auto ideal_rows_per_cluster = std::max(size_t{1}, (row_count - num_null_values) / num_clusters);
   AllTypeVariant lower_bound;
   AllTypeVariant upper_bound;
   size_t rows_in_cluster = 0;
