caeae6ff8c0ca82b24fa44946287c62361700c83
Julian Menzler <julianmenzler@gmail.com>
2020-09-15 15:33:24 +0200

    Remove test

--- src/test/lib/optimizer/strategy/column_pruning_rule_test.cpp
+++ src/test/lib/optimizer/strategy/column_pruning_rule_test.cpp
@@ -389,7 +389,7 @@ TEST_F(ColumnPruningRuleTest, InnerJoinToSemiJoinTwoPredicates) {
   EXPECT_LQP_EQ(actual_lqp, expected_lqp);
 }
 
-TEST_F(ColumnPruningRuleTest, DoNotTouchInnerJoinWithNonEquiPredicate) {
+TEST_F(ColumnPruningRuleTest, DoNotTouchInnerJoinWithoutEquiPredicates) {
   auto lqp = std::shared_ptr<AbstractLQPNode>{};
 
   {
@@ -423,53 +423,6 @@ TEST_F(ColumnPruningRuleTest, DoNotTouchInnerJoinWithNonEquiPredicate) {
   const auto expected_lqp =
   ProjectionNode::make(expression_vector(add_(pruned_a, 2)),
     JoinNode::make(JoinMode::Inner, greater_than_(pruned_a, column0),
-      ProjectionNode::make(expression_vector(pruned_a),
-        pruned_node_a),
-      stored_table_node));
-  // clang-format on
-
-  EXPECT_LQP_EQ(actual_lqp, expected_lqp);
-}
-
-TEST_F(ColumnPruningRuleTest, DoNotTouchInnerJoinWithEquiAndNonEquiPredicates) {
-  // Same as InnerJoinToSemiJoin, but with an additional join predicate that probhibits the reformulation into a
-  // semi join
-  auto lqp = std::shared_ptr<AbstractLQPNode>{};
-
-  {
-    TableColumnDefinitions column_definitions;
-    column_definitions.emplace_back("column0", DataType::Int, false);
-    column_definitions.emplace_back("column1", DataType::Int, false);
-    auto table = std::make_shared<Table>(column_definitions, TableType::Data, 2, UseMvcc::Yes);
-
-    auto& sm = Hyrise::get().storage_manager;
-    sm.add_table("table", table);
-
-    table->add_soft_key_constraint({{ColumnID{0}}, KeyConstraintType::UNIQUE});
-  }
-
-  const auto stored_table_node = StoredTableNode::make("table");
-  const auto column0 = stored_table_node->get_column("column0");
-  const auto column1 = stored_table_node->get_column("column1");
-
-  // clang-format off
-  lqp =
-  ProjectionNode::make(expression_vector(add_(a, 2)),
-    JoinNode::make(JoinMode::Inner, expression_vector(equals_(a, column0), not_equals_(a, column1)),
-      ProjectionNode::make(expression_vector(a, add_(b, 1)),
-        node_a),
-      stored_table_node));
-
-  const auto pruned_node_a = pruned(node_a, {ColumnID{1}, ColumnID{2}});
-  const auto pruned_a = pruned_node_a->get_column("a");
-
-  const auto actual_lqp = apply_rule(rule, lqp);
-
-  // Due to the second join predicate `not_equals`, we do not expect a semi join reformulation.
-  // Still expect it to prune b+1 though.
-  const auto expected_lqp =
-  ProjectionNode::make(expression_vector(add_(pruned_a, 2)),
-    JoinNode::make(JoinMode::Inner, expression_vector(equals_(pruned_a, column0), not_equals_(pruned_a, column1)),
       ProjectionNode::make(expression_vector(pruned_a),
         pruned_node_a),
       stored_table_node));
