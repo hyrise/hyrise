4bc06008e0f422249f3cc018209046d70257446f
Jan Kossmann <jan.kossmann@hpi.de>
2020-09-17 15:13:35 +0200

    Make ClusteringPlugin multi-threaded

--- src/plugins/clustering_plugin.cpp
+++ src/plugins/clustering_plugin.cpp
@@ -30,62 +30,73 @@ void ClusteringPlugin::_optimize_clustering() {
 
   std::map<std::string, std::string> sort_orders = {{"orders_tpch_1", "o_orderkey"},
                                                     {"lineitem_tpch_1", "l_orderkey"}};
+  auto threads = std::vector<std::thread>{};
+  threads.reserve(sort_orders.size());
 
-  for (auto& [table_name, column_name] : sort_orders) {
-    if (!Hyrise::get().storage_manager.has_table(table_name)) {
-      Hyrise::get().log_manager.add_message(
-          description(), "No optimization possible with given parameters for " + table_name + " table!",
-          LogLevel::Debug);
-      continue;
-    }
-    auto table = Hyrise::get().storage_manager.get_table(table_name);
+  for (const auto& sort_order : sort_orders) {
+    const auto table_name_outer = sort_order.first;
+    const auto column_name_outer = sort_order.second;
+    threads.emplace_back([&, table_name_outer, column_name_outer] {
+      const auto table_name = table_name_outer;
+      const auto column_name = column_name_outer;
 
-    const auto sort_column_id = table->column_id_by_name(column_name);
-
-    auto table_wrapper = std::make_shared<TableWrapper>(table);
-    table_wrapper->execute();
-    auto sort = Sort{table_wrapper,
-                     {SortColumnDefinition{sort_column_id, SortMode::Ascending}},
-                     Chunk::DEFAULT_SIZE,
-                     Sort::ForceMaterialization::Yes};
-    sort.execute();
-    const auto immutable_sorted_table = sort.get_output();
-
-    Assert(immutable_sorted_table->chunk_count() == table->chunk_count(), "Mismatching chunk_count");
-
-    table = std::make_shared<Table>(immutable_sorted_table->column_definitions(), TableType::Data,
-                                    table->target_chunk_size(), UseMvcc::Yes);
-    const auto column_count = immutable_sorted_table->column_count();
-    const auto chunk_count = immutable_sorted_table->chunk_count();
-    for (auto chunk_id = ChunkID{0}; chunk_id < chunk_count; ++chunk_id) {
-      const auto chunk = immutable_sorted_table->get_chunk(chunk_id);
-      auto mvcc_data = std::make_shared<MvccData>(chunk->size(), CommitID{0});
-      Segments segments{};
-      for (auto column_id = ColumnID{0}; column_id < column_count; ++column_id) {
-        const auto abstract_segment = chunk->get_segment(column_id);
-        std::shared_ptr<AbstractSegment> new_segment;
-        const auto data_type = table->column_data_type(column_id);
-        new_segment =
-            ChunkEncoder::encode_segment(abstract_segment, data_type, SegmentEncodingSpec{EncodingType::Dictionary});
-        segments.emplace_back(new_segment);
+      if (!Hyrise::get().storage_manager.has_table(table_name)) {
+        Hyrise::get().log_manager.add_message(
+            description(), "No optimization possible with given parameters for " + table_name + " table!",
+            LogLevel::Debug);
+        return;
       }
-      table->append_chunk(segments, mvcc_data);
-      auto sort_column_definition = SortColumnDefinition(ColumnID{sort_column_id}, SortMode::Ascending);
-      table->get_chunk(chunk_id)->finalize();
-      table->get_chunk(chunk_id)->set_individually_sorted_by(sort_column_definition);
-    }
+      auto table = Hyrise::get().storage_manager.get_table(table_name);
 
-    table->set_table_statistics(TableStatistics::from_table(*table));
-    generate_chunk_pruning_statistics(table);
+      const auto sort_column_id = table->column_id_by_name(column_name);
 
-    Hyrise::get().storage_manager.replace_table(table_name, table);
-    if (Hyrise::get().default_lqp_cache) Hyrise::get().default_lqp_cache->clear();
-    if (Hyrise::get().default_pqp_cache) Hyrise::get().default_pqp_cache->clear();
+      auto table_wrapper = std::make_shared<TableWrapper>(table);
+      table_wrapper->execute();
+      auto sort = Sort{table_wrapper,
+                       {SortColumnDefinition{sort_column_id, SortMode::Ascending}},
+                       Chunk::DEFAULT_SIZE,
+                       Sort::ForceMaterialization::Yes};
+      sort.execute();
+      const auto immutable_sorted_table = sort.get_output();
 
-    Hyrise::get().log_manager.add_message(
-        description(), "Applied new clustering configuration (" + column_name + ") to " + table_name + " table.",
-        LogLevel::Warning);
+      Assert(immutable_sorted_table->chunk_count() == table->chunk_count(), "Mismatching chunk_count");
+
+      table = std::make_shared<Table>(immutable_sorted_table->column_definitions(), TableType::Data,
+                                      table->target_chunk_size(), UseMvcc::Yes);
+      const auto column_count = immutable_sorted_table->column_count();
+      const auto chunk_count = immutable_sorted_table->chunk_count();
+      for (auto chunk_id = ChunkID{0}; chunk_id < chunk_count; ++chunk_id) {
+        const auto chunk = immutable_sorted_table->get_chunk(chunk_id);
+        auto mvcc_data = std::make_shared<MvccData>(chunk->size(), CommitID{0});
+        Segments segments{};
+        for (auto column_id = ColumnID{0}; column_id < column_count; ++column_id) {
+          const auto abstract_segment = chunk->get_segment(column_id);
+          std::shared_ptr<AbstractSegment> new_segment;
+          const auto data_type = table->column_data_type(column_id);
+          new_segment =
+              ChunkEncoder::encode_segment(abstract_segment, data_type, SegmentEncodingSpec{EncodingType::Dictionary});
+          segments.emplace_back(new_segment);
+        }
+        table->append_chunk(segments, mvcc_data);
+        auto sort_column_definition = SortColumnDefinition(ColumnID{sort_column_id}, SortMode::Ascending);
+        table->get_chunk(chunk_id)->finalize();
+        table->get_chunk(chunk_id)->set_individually_sorted_by(sort_column_definition);
+      }
+
+      table->set_table_statistics(TableStatistics::from_table(*table));
+      generate_chunk_pruning_statistics(table);
+
+      Hyrise::get().storage_manager.replace_table(table_name, table);
+      if (Hyrise::get().default_lqp_cache) Hyrise::get().default_lqp_cache->clear();
+      if (Hyrise::get().default_pqp_cache) Hyrise::get().default_pqp_cache->clear();
+
+      Hyrise::get().log_manager.add_message(
+          description(), "Applied new clustering configuration (" + column_name + ") to " + table_name + " table.",
+          LogLevel::Warning);
+    });
   }
+
+  for (auto& thread : threads) thread.join();
 }
 
 void ClusteringPlugin::stop() {}
