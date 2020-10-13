hyrise/hyrise

commit f518e3173f1aabef3983b361fc6aa7fa81190d51

PredicateSplitUpRule: Create UnionAll for NOT LIKE (#2238)
Optimization: The ExpressionReductionRule transforms x NOT LIKE 'foo%' into x < 'foo' OR x >= 'fop'. For this special case, we know that the two OR arguments are mutually exclusive. In this case, we do not need to use SetOperationMode::Positions but can use SetOperationMode::All. This surely applies to other cases, too, but those are not covered yet.

This only applies to TPC-H Q16; other benchmarks do not use this construct.

src/lib/optimizer/optimizer.cpp
@@ -114,14 +114,16 @@ std::shared_ptr<Optimizer> Optimizer::create_default_optimizer() {
 
   optimizer->add_rule(std::make_unique<PredicatePlacementRule>());
 
-  optimizer->add_rule(std::make_unique<PredicateSplitUpRule>());
-
   optimizer->add_rule(std::make_unique<SubqueryToJoinRule>());
 
   // Run the ColumnPruningRule before the PredicatePlacementRule, as it might turn joins into semi joins, which
   // can be treated as predicates and pushed further down. For the same reason, run it after the JoinOrderingRule,
   // which does not like semi joins (see above).
   optimizer->add_rule(std::make_unique<ColumnPruningRule>());
+
+  // Run the PredicateSplitUpRule after the ColumnPruningRule, as it may create UNION ALLs which are not handled in the
+  // column pruning rule yet.
+  optimizer->add_rule(std::make_unique<PredicateSplitUpRule>());
 
   optimizer->add_rule(std::make_unique<SemiJoinReductionRule>());
 

src/lib/optimizer/strategy/predicate_split_up_rule.cpp
@@ -1,11 +1,70 @@
 #include "predicate_split_up_rule.hpp"
 
+#include "expression/binary_predicate_expression.hpp"
 #include "expression/expression_utils.hpp"
 #include "expression/logical_expression.hpp"
+#include "expression/value_expression.hpp"
 #include "logical_query_plan/lqp_utils.hpp"
 #include "logical_query_plan/union_node.hpp"
 
 namespace opossum {
+
+namespace {
+bool predicates_are_mutually_exclusive(const std::vector<std::shared_ptr<AbstractExpression>>& predicates) {
+  // Optimization: The ExpressionReductionRule transforms `x NOT LIKE 'foo%'` into `x < 'foo' OR x >= 'fop'`. For this
+  // special case, we know that the two OR arguments are mutually exclusive. In this case, we do not need to use
+  // SetOperationMode::Positions but can use SetOperationMode::All. For now, we only cover cases with less-than and
+  // greater-than-equals. There are more cases, too, but those are not covered yet. For example, the optimization
+  // expects the two predicates in the order mentioned above. Also, it does not cover less-than/greater-than (not
+  // greater-than-equals)
+  //
+  // Note that this is not equal to XOR. In the case handled here, we know that both sides cannot be true at the same
+  // time. XOR would require us to check if both are true and discard the row if they are. As such, even if we later
+  // introduce PredicateCondition::Xor, we cannot automatically use ::All for that expression.
+  if (predicates.size() != 2) return false;
+
+  const auto first_binary_predicate = std::dynamic_pointer_cast<BinaryPredicateExpression>(predicates[0]);
+  const auto second_binary_predicate = std::dynamic_pointer_cast<BinaryPredicateExpression>(predicates[1]);
+  if (!first_binary_predicate || !second_binary_predicate) return false;
+
+  // Check for pattern `col_x < 'val' OR col_x >= 'vam'`
+  if (first_binary_predicate->predicate_condition != PredicateCondition::LessThan ||
+      second_binary_predicate->predicate_condition != PredicateCondition::GreaterThanEquals) {
+    // Wrong predicates
+    return false;
+  }
+
+  if (first_binary_predicate->left_operand()->type != ExpressionType::LQPColumn ||
+      first_binary_predicate->left_operand() != second_binary_predicate->left_operand()) {
+    // Left side of predicates is not a column or is a different column for both expressions
+    return false;
+  }
+
+  if (first_binary_predicate->right_operand()->type != ExpressionType::Value ||
+      second_binary_predicate->right_operand()->type != ExpressionType::Value) {
+    // Right side of predicates is not a value
+    return false;
+  }
+
+  if (first_binary_predicate->right_operand()->data_type() != second_binary_predicate->right_operand()->data_type()) {
+    // Different data types - to keep things simple, we do not handle this case here.
+    return false;
+  }
+
+  const auto first_value_expression = static_cast<const ValueExpression&>(*first_binary_predicate->right_operand());
+  const auto second_value_expression = static_cast<const ValueExpression&>(*second_binary_predicate->right_operand());
+
+  auto first_less_than_second = false;
+  boost::apply_visitor(
+      [&](const auto first_value) {
+        const auto second_value = boost::get<decltype(first_value)>(second_value_expression.value);
+        first_less_than_second = first_value < second_value;
+      },
+      first_value_expression.value);
+
+  return first_less_than_second;
+}
+}  // namespace
 
 PredicateSplitUpRule::PredicateSplitUpRule(const bool split_disjunctions) : _split_disjunctions(split_disjunctions) {}
 
@@ -83,7 +142,9 @@ void PredicateSplitUpRule::_split_disjunction(const std::shared_ptr<PredicateNod
   }
 
   // Step 1: Insert initial diamond
-  auto top_union_node = UnionNode::make(SetOperationMode::Positions);
+  const auto set_operation_mode =
+      predicates_are_mutually_exclusive(flat_disjunction) ? SetOperationMode::All : SetOperationMode::Positions;
+  auto top_union_node = UnionNode::make(set_operation_mode);
   const auto diamond_bottom = predicate_node->left_input();
   lqp_replace_node(predicate_node, top_union_node);
   {

src/test/lib/optimizer/strategy/predicate_split_up_rule_test.cpp
@@ -14,9 +14,12 @@ namespace opossum {
 class PredicateSplitUpRuleTest : public StrategyBaseTest {
  public:
   void SetUp() override {
-    node_a = MockNode::make(MockNode::ColumnDefinitions{{DataType::Int, "a"}, {DataType::Int, "b"}});
+    node_a = MockNode::make(MockNode::ColumnDefinitions{
+        {DataType::Int, "a"}, {DataType::Int, "b"}, {DataType::String, "c"}, {DataType::String, "d"}});
     a_a = node_a->get_column("a");
     a_b = node_a->get_column("b");
+    a_c = node_a->get_column("c");
+    a_d = node_a->get_column("d");
 
     node_b = MockNode::make(MockNode::ColumnDefinitions{{DataType::Int, "a"}, {DataType::Int, "b"}}, "b");
     b_a = node_b->get_column("a");
@@ -26,7 +29,7 @@ class PredicateSplitUpRuleTest : public StrategyBaseTest {
   }
 
   std::shared_ptr<MockNode> node_a, node_b;
-  std::shared_ptr<LQPColumnExpression> a_a, a_b, b_a, b_b;
+  std::shared_ptr<LQPColumnExpression> a_a, a_b, a_c, a_d, b_a, b_b;
   std::shared_ptr<PredicateSplitUpRule> rule;
 };
 
@@ -58,17 +61,17 @@ TEST_F(PredicateSplitUpRuleTest, SplitUpConjunctionInPredicateNode) {
 }
 
 TEST_F(PredicateSplitUpRuleTest, SplitUpSimpleDisjunctionInPredicateNode) {
-  // SELECT * FROM a WHERE a < 3 OR a >= 5
+  // SELECT * FROM a WHERE a < 3 OR a > 5
   // clang-format off
   const auto input_lqp =
-  PredicateNode::make(or_(less_than_(a_a, value_(3)), greater_than_equals_(a_a, value_(5))),
+  PredicateNode::make(or_(less_than_(a_a, value_(3)), greater_than_(a_a, value_(5))),
     node_a);
 
   const auto expected_lqp =
   UnionNode::make(SetOperationMode::Positions,
     PredicateNode::make(less_than_(a_a, value_(3)),
       node_a),
-    PredicateNode::make(greater_than_equals_(a_a, value_(5)),
+    PredicateNode::make(greater_than_(a_a, value_(5)),
       node_a));
   // clang-format on
 
@@ -96,6 +99,90 @@ TEST_F(PredicateSplitUpRuleTest, SplitUpComplexDisjunctionInPredicateNode) {
           node_a),
         PredicateNode::make(less_than_(9, a_b),
           node_a))));
+  // clang-format on
+
+  const auto actual_lqp = StrategyBaseTest::apply_rule(rule, input_lqp);
+
+  EXPECT_LQP_EQ(actual_lqp, expected_lqp);
+}
+
+TEST_F(PredicateSplitUpRuleTest, SplitUpNotLike) {
+  // SELECT * FROM a WHERE a < 'foo' OR a >= 'fop' - should identify the conditions as mutually exclusive and use
+  // SetOperationMode::All. The expression could have been the result of the ExpressionReductionRule being applied
+  // to `a NOT LIKE 'foo%'`.
+
+  // clang-format off
+  const auto input_lqp =
+  PredicateNode::make(or_(less_than_(a_c, value_("foo")), greater_than_equals_(a_c, value_("fop"))),
+    node_a);
+
+  const auto expected_lqp =
+  UnionNode::make(SetOperationMode::All,
+    PredicateNode::make(less_than_(a_c, value_("foo")),
+      node_a),
+    PredicateNode::make(greater_than_equals_(a_c, value_("fop")),
+      node_a));
+  // clang-format on
+
+  const auto actual_lqp = StrategyBaseTest::apply_rule(rule, input_lqp);
+
+  EXPECT_LQP_EQ(actual_lqp, expected_lqp);
+}
+
+TEST_F(PredicateSplitUpRuleTest, SplitUpNotLikeDoesNotApply1) {
+  // SELECT * FROM a WHERE c < 'foo' OR c >= 'bar' - looks like SplitUpNotLike, but the optimization does not apply
+  // clang-format off
+  const auto input_lqp =
+  PredicateNode::make(or_(less_than_(a_c, value_("foo")), greater_than_equals_(a_c, value_("bar"))),
+    node_a);
+
+  const auto expected_lqp =
+  UnionNode::make(SetOperationMode::Positions,
+    PredicateNode::make(less_than_(a_c, value_("foo")),
+      node_a),
+    PredicateNode::make(greater_than_equals_(a_c, value_("bar")),
+      node_a));
+  // clang-format on
+
+  const auto actual_lqp = StrategyBaseTest::apply_rule(rule, input_lqp);
+
+  EXPECT_LQP_EQ(actual_lqp, expected_lqp);
+}
+
+TEST_F(PredicateSplitUpRuleTest, SplitUpNotLikeDoesNotApply2) {
+  // SELECT * FROM a WHERE c < 'foo' OR d >= 'fop' - looks like SplitUpNotLike, but the optimization does not apply
+  // clang-format off
+  const auto input_lqp =
+  PredicateNode::make(or_(less_than_(a_c, value_("foo")), greater_than_equals_(a_d, value_("fop"))),
+    node_a);
+
+  const auto expected_lqp =
+  UnionNode::make(SetOperationMode::Positions,
+    PredicateNode::make(less_than_(a_c, value_("foo")),
+      node_a),
+    PredicateNode::make(greater_than_equals_(a_d, value_("fop")),
+      node_a));
+  // clang-format on
+
+  const auto actual_lqp = StrategyBaseTest::apply_rule(rule, input_lqp);
+
+  EXPECT_LQP_EQ(actual_lqp, expected_lqp);
+}
+
+TEST_F(PredicateSplitUpRuleTest, SplitUpMutuallyExclusiveInts) {
+  // SELECT * FROM a WHERE a < 5 OR b >= 8 - should identify the conditions as mutually exclusive and use
+  // SetOperationMode::All
+  // clang-format off
+  const auto input_lqp =
+  PredicateNode::make(or_(less_than_(a_a, value_(5)), greater_than_equals_(a_a, value_(8))),
+    node_a);
+
+  const auto expected_lqp =
+  UnionNode::make(SetOperationMode::All,
+    PredicateNode::make(less_than_(a_a, value_(5)),
+      node_a),
+    PredicateNode::make(greater_than_equals_(a_a, value_(8)),
+      node_a));
   // clang-format on
 
   const auto actual_lqp = StrategyBaseTest::apply_rule(rule, input_lqp);

commit 136486ff281a12cb3b6ebc008a8aa15c5ebdfd81

Fix Performance Regression caused by f518e3173f1aabef3983b361fc6aa7fa81190d51

src/lib/optimizer/optimizer.cpp
@@ -114,16 +114,14 @@ std::shared_ptr<Optimizer> Optimizer::create_default_optimizer() {
 
   optimizer->add_rule(std::make_unique<PredicatePlacementRule>());
 
+  optimizer->add_rule(std::make_unique<PredicateSplitUpRule>());
+
   optimizer->add_rule(std::make_unique<SubqueryToJoinRule>());
 
   // Run the ColumnPruningRule before the PredicatePlacementRule, as it might turn joins into semi joins, which
   // can be treated as predicates and pushed further down. For the same reason, run it after the JoinOrderingRule,
   // which does not like semi joins (see above).
   optimizer->add_rule(std::make_unique<ColumnPruningRule>());
-
-  // Run the PredicateSplitUpRule after the ColumnPruningRule, as it may create UNION ALLs which are not handled in the
-  // column pruning rule yet.
-  optimizer->add_rule(std::make_unique<PredicateSplitUpRule>());
 
   optimizer->add_rule(std::make_unique<SemiJoinReductionRule>());
 

src/lib/optimizer/strategy/column_pruning_rule.cpp
@@ -147,11 +147,27 @@ ExpressionUnorderedSet gather_locally_required_expressions(
 
     case LQPNodeType::Union: {
       const auto& union_node = static_cast<const UnionNode&>(*node);
-      Assert(union_node.set_operation_mode == SetOperationMode::Positions,
-             "Currently, ColumnPruningRule can only handle UnionNodes in Positions mode");
-      // UnionNode does not require any expressions itself for the Positions mode. Once we add actual unions from two
-      // tables, we will probably have to change something here in this rule that the required expressions are kept on
-      // both the left and right sides.
+      switch (union_node.set_operation_mode) {
+        case SetOperationMode::Positions: {
+          // UnionNode does not require any expressions itself for the Positions mode. Once we add actual unions from two
+          // tables, we will probably have to change something here in this rule that the required expressions are kept on
+          // both the left and right sides.
+        } break;
+
+        case SetOperationMode::All: {
+          // Similarly, if the two input tables are only glued together, the UnionNode itself does not require any
+          // expressions. Currently, this mode is used to merge the result of two mutually exclusive or conditions (see
+          // PredicateSplitUpRule). Once we have a union operator that merges data from different tables, we have to look
+          // into this more deeply.
+          Assert(union_node.left_input()->output_expressions() == union_node.right_input()->output_expressions(),
+                 "Can only handle SetOperationMode::All if both inputs have the same expressions");
+        } break;
+
+        case SetOperationMode::Unique: {
+          // This probably needs all expressions, as all of them are used to establish uniqueness
+          Fail("SetOperationMode::Unique is not supported yet");
+        }
+      }
     } break;
 
     case LQPNodeType::Intersect:

src/test/lib/optimizer/strategy/column_pruning_rule_test.cpp
@@ -83,38 +83,42 @@ TEST_F(ColumnPruningRuleTest, NoUnion) {
 }
 
 TEST_F(ColumnPruningRuleTest, WithUnion) {
-  auto lqp = std::shared_ptr<AbstractLQPNode>{};
+  for (auto union_mode : {SetOperationMode::Positions, SetOperationMode::All}) {
+    SCOPED_TRACE(std::string{"union_mode: "} + set_operation_mode_to_string.left.at(union_mode));
 
-  // clang-format off
-  lqp =
-  ProjectionNode::make(expression_vector(a),
-    UnionNode::make(SetOperationMode::Positions,
-      PredicateNode::make(greater_than_(a, 5),
-        node_a),
-      PredicateNode::make(greater_than_(b, 5),
-        node_a)));
+    auto lqp = std::shared_ptr<AbstractLQPNode>{};
 
-  // Create deep copy so we can set pruned ColumnIDs on node_a below without manipulating the input LQP
-  lqp = lqp->deep_copy();
+    // clang-format off
+    lqp =
+    ProjectionNode::make(expression_vector(a),
+      UnionNode::make(SetOperationMode::Positions,
+        PredicateNode::make(greater_than_(a, 5),
+          node_a),
+        PredicateNode::make(greater_than_(b, 5),
+          node_a)));
+
+    // Create deep copy so we can set pruned ColumnIDs on node_a below without manipulating the input LQP
+    lqp = lqp->deep_copy();
 
 
-  const auto pruned_node_a = pruned(node_a, {ColumnID{2}});
-  const auto pruned_a = pruned_node_a->get_column("a");
-  const auto pruned_b = pruned_node_a->get_column("b");
+    const auto pruned_node_a = pruned(node_a, {ColumnID{2}});
+    const auto pruned_a = pruned_node_a->get_column("a");
+    const auto pruned_b = pruned_node_a->get_column("b");
 
-  const auto actual_lqp = apply_rule(rule, lqp);
+    const auto actual_lqp = apply_rule(rule, lqp);
 
-  // Column c is not used anywhere above the union, so it can be pruned at least in the Positions mode
-  const auto expected_lqp =
-  ProjectionNode::make(expression_vector(pruned_a),
-    UnionNode::make(SetOperationMode::Positions,
-      PredicateNode::make(greater_than_(pruned_a, 5),
-        pruned_node_a),
-      PredicateNode::make(greater_than_(pruned_b, 5),
-        pruned_node_a)));
-  // clang-format on
+    // Column c is not used anywhere above the union, so it can be pruned at least in the Positions mode
+    const auto expected_lqp =
+    ProjectionNode::make(expression_vector(pruned_a),
+      UnionNode::make(SetOperationMode::Positions,
+        PredicateNode::make(greater_than_(pruned_a, 5),
+          pruned_node_a),
+        PredicateNode::make(greater_than_(pruned_b, 5),
+          pruned_node_a)));
+    // clang-format on
 
-  EXPECT_LQP_EQ(actual_lqp, expected_lqp);
+    EXPECT_LQP_EQ(actual_lqp, expected_lqp);
+  }
 }
 
 TEST_F(ColumnPruningRuleTest, WithMultipleProjections) {