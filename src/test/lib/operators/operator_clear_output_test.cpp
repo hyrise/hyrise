8d0b629facad6115089fd7144b78a1fdbdf2435a
Julian Menzler <julianmenzler@gmail.com>
2020-11-11 17:28:22 +0100

    Add test

--- src/test/lib/operators/operator_clear_output_test.cpp
+++ src/test/lib/operators/operator_clear_output_test.cpp
@@ -2,6 +2,7 @@
 
 #include "operators/projection.hpp"
 #include "operators/get_table.hpp"
+#include "operators/join_hash.hpp"
 #include "operators/table_wrapper.hpp"
 #include "operators/table_scan.hpp"
 #include "operators/validate.hpp"
@@ -22,7 +23,7 @@ class OperatorClearOutputTest : public BaseTest {
 //  std::shared_ptr<TableWrapper> _table_wrapper_a, _table_wrapper_b, _table_wrapper_c, _table_wrapper_d;
 };
 
-TEST_F(OperatorClearOutputTest, ConsumerTrackingOperators) {
+TEST_F(OperatorClearOutputTest, ConsumerTracking) {
   // Prerequisite
   EXPECT_EQ(_gt->consumer_count(), 0);
   EXPECT_NE(_gt->get_output(), nullptr);
@@ -45,23 +46,49 @@ TEST_F(OperatorClearOutputTest, ConsumerTrackingOperators) {
   EXPECT_EQ(_gt->get_output(), nullptr);
 }
 
-TEST_F(OperatorClearOutputTest, ConsumerTrackingProjectionSubqueryUncorrelated) {
+TEST_F(OperatorClearOutputTest, ConsumerTrackingUncorrelatedSubqueryProjection) {
 
-    // Prepare: SELECT 9 + 2
-    auto dummy_table_wrapper = std::make_shared<TableWrapper>(Projection::dummy_table());
-    auto literal_projection = std::make_shared<Projection>(dummy_table_wrapper,expression_vector(add_(value_(9),value_(2))));
-    auto subquery_expression = pqp_subquery_(literal_projection, DataType::Int, false);
+}
 
-    auto validate = std::make_shared<Validate>(_gt);
-    auto table_scan = std::make_shared<TableScan>(validate, expression_vector());
+TEST_F(OperatorClearOutputTest, ConsumerTrackingUncorrelatedSubqueryTableScan) {
+  /**
+   * Models the PQP of the following SQL query
+   *  SELECT a
+   *  FROM int_int_int
+   *  WHERE a IN (SELECT 9 + 2) AND b > (SELECT 9 + 2)
+   */
 
+  // (SELECT 9 + 2)
+  auto dummy_table_wrapper = std::make_shared<TableWrapper>(Projection::dummy_table());
+  auto literal_projection = std::make_shared<Projection>(dummy_table_wrapper, expression_vector(add_(value_(9),value_(2))));
+
+  // b > (SELECT 9 + 2)
+  auto validate = std::make_shared<Validate>(_gt);
+  auto pqp_subquery_expression = pqp_subquery_(literal_projection, DataType::Int, false);
+  auto table_scan = std::make_shared<TableScan>(validate, greater_than_(pqp_column_(ColumnID{1}, DataType::Int,false, "b"), pqp_subquery_expression));
+
+  // a IN (SELECT 9 + 2)
+  const auto semi_join_predicate = OperatorJoinPredicate{{ColumnID{0}, ColumnID{0}}, PredicateCondition::Equals};
+  const auto semi_join = std::make_shared<JoinHash>(table_scan, literal_projection, JoinMode::Semi, semi_join_predicate);
+
+  // Check consumer registering
+  EXPECT_EQ(_gt->consumer_count(), 1);
+  EXPECT_EQ(validate->consumer_count(), 1);
+  EXPECT_EQ(table_scan->consumer_count(), 1);
+  EXPECT_EQ(semi_join->consumer_count(), 0);
+
+  EXPECT_EQ(dummy_table_wrapper->consumer_count(), 1);
+  EXPECT_EQ(literal_projection->consumer_count(), 2);
+
+  // Check consumer deregistering
+  validate->execute();
+  table_scan->execute();
+  EXPECT_EQ(literal_projection->consumer_count(), 1);
+  semi_join->execute();
+  EXPECT_EQ(literal_projection->consumer_count(), 0);
 }
 
 TEST_F(OperatorClearOutputTest, ConsumerTrackingTableScanSubqueryCorrelated) {
-
-}
-
-TEST_F(OperatorClearOutputTest, ConsumerTrackingTableScanSubqueryUncorrelated) {
 
 }
 
