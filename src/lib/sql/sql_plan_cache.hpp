e8ab9e8cef260df19931c5c9d61b30aa4a2b9c18
Justin Trautmann <justin.trautmann@student.hpi.de>
2020-11-18 15:04:26 +0100

    deduplication

--- src/lib/sql/sql_plan_cache.hpp
+++ src/lib/sql/sql_plan_cache.hpp
@@ -12,20 +12,8 @@ namespace opossum {
 class AbstractOperator;
 class AbstractLQPNode;
 
-class LQPNodePointerHash {
- public:
-  size_t operator()(const std::shared_ptr<AbstractLQPNode>& key) const { return key->hash(); }
-};
-
-class NodePointerDeepEquality {
- public:
-  bool operator()(const std::shared_ptr<AbstractLQPNode>& key1, const std::shared_ptr<AbstractLQPNode>& key2) const {
-    return *key1 == *key2;
-  }
-};
-
 using SQLPhysicalPlanCache = GDFSCache<std::string, std::shared_ptr<AbstractOperator>>;
 using SQLLogicalPlanCache = GDFSCache<std::shared_ptr<AbstractLQPNode>, std::shared_ptr<PreparedPlan>,
-                                      LQPNodePointerHash, NodePointerDeepEquality>;
+                                      LQPNodeSharedPtrHash, LQPNodeSharedPtrEqual>;
 
 }  // namespace opossum
