33de60b53bd65430ec4796dca434285058951992
Julian Menzler <julianmenzler@gmail.com>
2020-11-24 17:48:59 +0100

    Move recursion logic to collect_subquery_expressions_by_lqp()

--- src/lib/optimizer/strategy/abstract_rule.cpp
+++ src/lib/optimizer/strategy/abstract_rule.cpp
@@ -34,9 +34,8 @@ void AbstractRule::apply(const std::shared_ptr<AbstractLQPNode>& lqp_root) const
   for (const auto& [lqp, subquery_expressions] : subquery_expressions_by_lqp) {
     // (2.1) Prepare
     const auto local_lqp_root = LogicalPlanRootNode::make(lqp);
-    std::cout << "Subquery LQP \n\n" << *lqp << "\n\n" << std::endl;
     // (2.2) Optimize subquery LQP
-    apply(local_lqp_root);
+    _apply_to(local_lqp_root);
     // (2.3) Assign optimized LQP to all corresponding SubqueryExpressions
     for (const auto& subquery_expression : subquery_expressions) {
       subquery_expression->lqp = local_lqp_root->left_input();
@@ -68,6 +67,9 @@ void collect_subquery_expressions_by_lqp(SubqueryExpressionsByLQP& subquery_expr
         }
       }
       subquery_expressions_by_lqp.emplace_back(subquery_expression->lqp, std::vector{subquery_expression});
+
+      // We want to cover deeply nested subqueries as well
+      collect_subquery_expressions_by_lqp(subquery_expressions_by_lqp, subquery_expression->lqp, visited_nodes);
 
       return ExpressionVisitation::DoNotVisitArguments;
     });
