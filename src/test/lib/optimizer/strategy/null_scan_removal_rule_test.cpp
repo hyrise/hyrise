df37d6bf7b3ea1a1654ad438752bd97afd8439eb
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2020-12-08 17:37:00 +0100

    Refactor tests

--- src/test/lib/optimizer/strategy/null_scan_removal_rule_test.cpp
+++ src/test/lib/optimizer/strategy/null_scan_removal_rule_test.cpp
@@ -11,23 +11,36 @@ using namespace opossum::expression_functional;  // NOLINT
 
 namespace opossum {
 
-class NullScanRemovalRuleTest : public StrategyBaseTest {};
+class NullScanRemovalRuleTest : public StrategyBaseTest {
+ public:
+  void SetUp() override {
+    rule = std::make_shared<NullScanRemovalRule>();
+    mock_node = MockNode::make(MockNode::ColumnDefinitions{{DataType::Int, "a"}, {DataType::Float, "b"}});
+    mock_node_column = mock_node->get_column("a");
+
+    Hyrise::get().storage_manager.add_table("nullable_table",
+                                            load_table("resources/test_data/tbl/int_float_null_1.tbl", 2));
+    Hyrise::get().storage_manager.add_table("table", load_table("resources/test_data/tbl/int_float4_or_1.tbl", 2));
+    nullable_table_node = StoredTableNode::make("nullable_table");
+    table_node = StoredTableNode::make("table");
+    nullable_table_node_column = lqp_column_(nullable_table_node, ColumnID{0});
+    table_node_column = lqp_column_(table_node, ColumnID{0});
+  }
+  std::shared_ptr<MockNode> mock_node;
+  std::shared_ptr<NullScanRemovalRule> rule;
+  std::shared_ptr<LQPColumnExpression> mock_node_column, nullable_table_node_column, table_node_column;
+  std::shared_ptr<StoredTableNode> nullable_table_node, table_node;
+};
 
 TEST_F(NullScanRemovalRuleTest, LQPNodeTypeIsNotPredicate) {
-  auto input_lqp = MockNode::make(MockNode::ColumnDefinitions{{DataType::Int, "a"}, {DataType::Float, "b"}});
-  auto rule = std::make_shared<NullScanRemovalRule>();
+  const auto actual_lqp = apply_rule(rule, mock_node);
+  const auto expected_lqp = mock_node->deep_copy();
 
-  const auto actual_lqp = apply_rule(rule, input_lqp);
-  const auto expected_lqp = MockNode::make(MockNode::ColumnDefinitions{{DataType::Int, "a"}, {DataType::Float, "b"}});
   EXPECT_LQP_EQ(actual_lqp, expected_lqp);
 }
 
 TEST_F(NullScanRemovalRuleTest, PredicateIsNotNullExpression) {
-  auto node_a = MockNode::make(MockNode::ColumnDefinitions{{DataType::Int, "a"}, {DataType::Float, "b"}});
-  auto rule = std::make_shared<NullScanRemovalRule>();
-  auto a_a = node_a->get_column("a");
-
-  const auto input_lqp = PredicateNode::make(equals_(a_a, 42), node_a);
+  const auto input_lqp = PredicateNode::make(equals_(mock_node_column, 42), mock_node);
   const auto actual_lqp = apply_rule(rule, input_lqp);
   const auto expected_lqp = input_lqp->deep_copy();
 
@@ -35,11 +48,7 @@ TEST_F(NullScanRemovalRuleTest, PredicateIsNotNullExpression) {
 }
 
 TEST_F(NullScanRemovalRuleTest, PredicateConditionIsNotNull) {
-  auto node_a = MockNode::make(MockNode::ColumnDefinitions{{DataType::Int, "a"}, {DataType::Float, "b"}});
-  auto rule = std::make_shared<NullScanRemovalRule>();
-  auto a_a = node_a->get_column("a");
-
-  const auto input_lqp = PredicateNode::make(is_null_(a_a), node_a);
+  const auto input_lqp = PredicateNode::make(is_null_(mock_node_column), mock_node);
   const auto actual_lqp = apply_rule(rule, input_lqp);
   const auto expected_lqp = input_lqp->deep_copy();
 
@@ -47,11 +56,7 @@ TEST_F(NullScanRemovalRuleTest, PredicateConditionIsNotNull) {
 }
 
 TEST_F(NullScanRemovalRuleTest, PredicateOperandIsNotLQPColumnExpression) {
-  auto node_a = MockNode::make(MockNode::ColumnDefinitions{{DataType::Int, "a"}, {DataType::Float, "b"}});
-  auto rule = std::make_shared<NullScanRemovalRule>();
-  auto a_a = node_a->get_column("a");
-
-  const auto input_lqp = PredicateNode::make(is_not_null_(42), node_a);
+  const auto input_lqp = PredicateNode::make(is_not_null_(42), mock_node);
   const auto actual_lqp = apply_rule(rule, input_lqp);
   const auto expected_lqp = input_lqp->deep_copy();
 
@@ -60,11 +65,7 @@ TEST_F(NullScanRemovalRuleTest, PredicateOperandIsNotLQPColumnExpression) {
 
 TEST_F(NullScanRemovalRuleTest, LQPColumnOriginalNodeIsNotStoredTableNode) {
   // Test that column original node is not a stored table node.
-  auto node_a = MockNode::make(MockNode::ColumnDefinitions{{DataType::Int, "a"}, {DataType::Float, "b"}});
-  auto rule = std::make_shared<NullScanRemovalRule>();
-  auto a_a = node_a->get_column("a");
-
-  const auto input_lqp = PredicateNode::make(is_not_null_(a_a), node_a);
+  const auto input_lqp = PredicateNode::make(is_not_null_(mock_node_column), mock_node);
   const auto actual_lqp = apply_rule(rule, input_lqp);
   const auto expected_lqp = input_lqp->deep_copy();
 
@@ -73,12 +74,7 @@ TEST_F(NullScanRemovalRuleTest, LQPColumnOriginalNodeIsNotStoredTableNode) {
 
 TEST_F(NullScanRemovalRuleTest, TableColumnDefinitionIsNullable) {
   // table column is nullable, so no node should be removed.
-  Hyrise::get().storage_manager.add_table("table_a", load_table("resources/test_data/tbl/int_float_null_1.tbl", 2));
-  auto rule = std::make_shared<NullScanRemovalRule>();
-  auto _table_node = StoredTableNode::make("table_a");
-  auto column = lqp_column_(_table_node, ColumnID{0});
-
-  const auto input_lqp = PredicateNode::make(is_not_null_(column), _table_node);
+  const auto input_lqp = PredicateNode::make(is_not_null_(nullable_table_node_column), nullable_table_node);
   const auto actual_lqp = apply_rule(rule, input_lqp);
   const auto expected_lqp = input_lqp->deep_copy();
 
@@ -86,14 +82,9 @@ TEST_F(NullScanRemovalRuleTest, TableColumnDefinitionIsNullable) {
 }
 
 TEST_F(NullScanRemovalRuleTest, TableColumnDefinitionIsNotNullable) {
-  Hyrise::get().storage_manager.add_table("table_a", load_table("resources/test_data/tbl/int_float4_or_1.tbl", 2));
-  auto rule = std::make_shared<NullScanRemovalRule>();
-  auto _table_node = StoredTableNode::make("table_a");
-  auto column = lqp_column_(_table_node, ColumnID{0});
-
-  const auto input_lqp = PredicateNode::make(is_not_null_(column), _table_node);
+  const auto input_lqp = PredicateNode::make(is_not_null_(table_node_column), table_node);
   const auto actual_lqp = apply_rule(rule, input_lqp);
-  const auto expected_lqp = _table_node->deep_copy();
+  const auto expected_lqp = table_node->deep_copy();
 
   EXPECT_LQP_EQ(actual_lqp, expected_lqp);
 }
