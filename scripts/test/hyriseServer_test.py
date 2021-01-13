5d64702a225bd82afbd1acc864cf748d38ba7ba5
martin.boissier <martin.boissier@hpi.de>
2021-01-13 12:02:38 +0100

    Add test

---
+++ scripts/test/hyriseServer_test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import pexpect
+
+from hyriseBenchmarkCore import close_benchmark, check_exit_status, initialize, run_benchmark
+
+
+def main():
+    build_dir = initialize()
+
+    arguments = {}
+    arguments["--benchmark_data"] = "tpc-h:0.01"
+
+    benchmark = run_benchmark(build_dir, arguments, "hyriseServer", True)
+
+    benchmark.expect_exact("Loading/Generating tables", timeout = 2)
+    benchmark.expect_exact("Encoding 'lineitem'", timeout = 10)
+    benchmark.expect_exact("Server started at 0.0.0.0 and port 5432", timeout = 10)
+
+    # Not using close_benchmark() here, as a server is started and a timeout of None would wait forever
+    benchmark.close()
+
+
+if __name__ == "__main__":
+    main()
