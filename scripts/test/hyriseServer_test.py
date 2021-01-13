330bf839339dc3844b60d936584b3950755bf674
martin.boissier <martin.boissier@hpi.de>
2021-01-13 20:22:04 +0100

    Test

--- scripts/test/hyriseServer_test.py
+++ scripts/test/hyriseServer_test.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-from hyriseBenchmarkCore import initialize, run_benchmark
+import pexpect
+
+from hyriseBenchmarkCore import initialize
 
 
 def main():
@@ -9,13 +11,21 @@ def main():
     arguments = {}
     arguments["--benchmark_data"] = "tpc-h:0.01"
 
-    benchmark = run_benchmark(build_dir, arguments, "hyriseServer", True)
+    benchmark = pexpect.spawn(f"{build_dir}/hyriseServer --benchmark_data=tpc-h:0.01", timeout=10)
 
     benchmark.expect_exact("Loading/Generating tables", timeout=2)
     benchmark.expect_exact("Encoding 'lineitem'", timeout=10)
     benchmark.expect_exact("Server started at 0.0.0.0 and port 5432", timeout=10)
 
+    client = pexpect.spawn("psql -h localhost -p 5432", timeout=10)
+
+    client.sendline("select count(*) from region;")
+    client.expect_exact("COUNT(*)")
+    client.expect_exact("5")
+    client.expect_exact("(1 row)")
+
     # Not using close_benchmark() here, as a server is started and a timeout of None would wait forever
+    client.close()
     benchmark.close()
 
 
