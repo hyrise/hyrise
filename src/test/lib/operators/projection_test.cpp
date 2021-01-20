05b6407d55d3f99b80253ac02d9672aa1e97f68c
Julian Menzler <julianmenzler@gmail.com>
2021-01-20 23:25:16 +0100

    Fix set-parameters-Assert (WIP)

--- src/test/lib/operators/projection_test.cpp
+++ src/test/lib/operators/projection_test.cpp
@@ -161,12 +161,18 @@ TEST_F(OperatorsProjectionTest, ForwardsReferenceTable) {
 }
 
 TEST_F(OperatorsProjectionTest, SetParameters) {
-  const auto table_scan_a = create_table_scan(table_wrapper_b, ColumnID{1}, PredicateCondition::GreaterThan, 5);
+  // Cannot use table_wrapper_a & table_wrapper_b because they have already executed
+  const auto table_wrapper_a_clone = table_wrapper_a->deep_copy();
+  const auto table_wrapper_b_clone = table_wrapper_b->deep_copy();
+
+  // TODO(Julian) create_table_scan calls get_column_expression() which requires input operators to have executed.
+  // Maybe create TableScans manually
+  const auto table_scan_a = create_table_scan(table_wrapper_b_clone, ColumnID{1}, PredicateCondition::GreaterThan, 5);
   const auto projection_a = std::make_shared<Projection>(table_scan_a, expression_vector(b_a));
   const auto subquery_expression =
       std::make_shared<PQPSubqueryExpression>(table_scan_a, DataType::Int, false, PQPSubqueryExpression::Parameters{});
   const auto projection_b = std::make_shared<Projection>(
-      table_wrapper_a, expression_vector(correlated_parameter_(ParameterID{2}, a_a), subquery_expression));
+      table_wrapper_a_clone, expression_vector(correlated_parameter_(ParameterID{2}, a_a), subquery_expression));
 
   const auto parameters = std::unordered_map<ParameterID, AllTypeVariant>{{ParameterID{5}, AllTypeVariant{12}},
                                                                           {ParameterID{2}, AllTypeVariant{13}}};
