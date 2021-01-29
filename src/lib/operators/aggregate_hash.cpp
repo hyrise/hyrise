15c0ef78f17201c292f5a80d80063521115d4364
Markus Dreseler <markus.dreseler@hpi.de>
2021-01-29 21:02:52 +0100

    bla

--- src/lib/operators/aggregate_hash.cpp
+++ src/lib/operators/aggregate_hash.cpp
@@ -79,7 +79,7 @@ typename Results::reference get_or_add_result(CacheResultIds, ResultIds& result_
         // column(s) later. By default, the newly created values have an invalid RowID and are later ignored. We grow
         // the vector slightly more than necessary. Otherwise,  monotonically increasing result_ids would lead to one
         // resize per row.
-        results.resize(std::max(results.size(), static_cast<size_t>(result_id + 1) * 1.5));
+        results.resize(std::max(results.size(), static_cast<size_t>((result_id + 1) * 1.5)));
         results[result_id].row_id = row_id;
 
         return results[result_id];
