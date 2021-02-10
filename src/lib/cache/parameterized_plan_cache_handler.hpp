b8c1deffb8822729ad04798c986dd5ccdd8da136
Justin Trautmann <justin.trautmann@student.hpi.de>
2021-02-10 12:37:42 +0100

    comment

--- src/lib/cache/parameterized_plan_cache_handler.hpp
+++ src/lib/cache/parameterized_plan_cache_handler.hpp
@@ -4,6 +4,43 @@
 
 namespace opossum {
 
+/* The ParameterizedPlanCacheHandler is a facade of the actual lqp cache that makes it possible to cache
+ * parameterized plans without much additional cache logic in the sql_pipeline_statement as well as the
+ * actual cache.
+ * Caching parameterized plans makes it possible to skip optimization and reuse already optimized plans
+ * even if the actual parameters of the query differ from the cached one.
+ * This is especially relevant for fast running (TPC-C) queries where the optimization contributes a large
+ * share to the overall runtime.
+
+ * Instantiation:
+ *                       ┌───> parameterized unoptimized lqp^
+ *   unoptimized_lqp ────┤
+ *                       └───> value expressions^
+
+ * try get:
+ *
+ * lqp_cache->try_get(parameterized unoptimized lqp^) ───> parameterized optimized lqp ───┐
+ *																						  ├───> optimized lqp
+ *          														value expressions^ ───┘
+
+ * set:
+ *									value expressions^ ───┐
+ *														  ├───> rearrange parameters ───> lqp_cache->set(parameterized optimized lqp)
+ *                   ┌───> parameterized optimized lqp ───┘
+ * optimized_lqp ────┤
+ *                   └───> value expressions (dropped)
+ *
+
+ * ^ : member variables
+
+ * The unoptimized_lqp that serves as cache key for the caller is split into a parameterized lqp (all
+ * value expressions get replaced by placeholder expressions) and a vector of value expressions.
+ * Since the value expressions of the unoptimized_lqp are necessary to cache and instantiate the parameterized
+ * optimized plan that has been cached, this class is intended to be newly instantiated for each access
+ * with the same key and cannot be reused when accessing the cache with a new key. This is ensured by
+ * setting the key (unoptimized lqp) in the constructor.
+ */
+
 class ParameterizedPlanCacheHandler {
  public:
   ParameterizedPlanCacheHandler(const std::shared_ptr<SQLLogicalPlanCache>& lqp_cache,
