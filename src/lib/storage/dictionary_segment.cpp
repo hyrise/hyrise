cae4e41e63c4ed5404ebd3f804c960f43564e212
Ben Hurdelhey <26392770+ben243871694@users.noreply.github.com"$REMOTE" -ancestor "$BASE" -merge "$MERGED>
2021-02-13 15:29:24 +0000

    print av sizes

--- src/lib/storage/dictionary_segment.cpp
+++ src/lib/storage/dictionary_segment.cpp
@@ -62,6 +62,7 @@ template <typename T>
 size_t DictionarySegment<T>::memory_usage([[maybe_unused]] const MemoryUsageCalculationMode mode) const {
   const auto common_elements_size = sizeof(*this) + _attribute_vector->data_size();
 
+  std::cout << "attribute_vector_size," << size() << ",dictionary_size," << _dictionary->size() << std::endl;
   if constexpr (std::is_same_v<T, pmr_string>) {
     return common_elements_size + string_vector_memory_usage(*_dictionary, mode);
   }
