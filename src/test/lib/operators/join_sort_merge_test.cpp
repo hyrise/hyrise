dce04be8a5a7dd1b27faba75331f031dfc81bf6a
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2021-03-05 16:28:47 +0100

    Remove unneeded test

--- src/test/lib/operators/join_sort_merge_test.cpp
+++ src/test/lib/operators/join_sort_merge_test.cpp
@@ -8,12 +8,6 @@ namespace opossum {
 
 class OperatorsJoinSortMergeTest : public BaseTest {
  public:
-  static void SetUpTestCase() {
-    _table_wrapper =
-        std::make_shared<TableWrapper>(load_table("resources/test_data/tbl/join_operators/anti_int4.tbl", 2));
-    _table_wrapper->execute();
-  }
-
   void SetUp() override {
     const auto dummy_table =
         std::make_shared<Table>(TableColumnDefinitions{{"a", DataType::Int, false}}, TableType::Data);
@@ -21,8 +15,6 @@ class OperatorsJoinSortMergeTest : public BaseTest {
   }
 
   std::shared_ptr<AbstractOperator> dummy_input;
-
-  inline static std::shared_ptr<TableWrapper> _table_wrapper;
 
 };
 
@@ -125,31 +117,5 @@ TEST_F(OperatorsJoinSortMergeTest, SetSortedFlagOnJoinColumns) {
     EXPECT_EQ(actual_sorted_columns, expected_sorted_columns);
   }
 }
-
-TEST_F(OperatorsJoinSortMergeTest, UnsupportedJoinModes) {
-  if (!HYRISE_DEBUG) GTEST_SKIP();
-
-  const auto execute_join = [&](const JoinMode mode, const PredicateCondition predicate_condition) {
-    std::make_shared<JoinSortMerge>(_table_wrapper, _table_wrapper, mode, OperatorJoinPredicate{{ColumnID{0}, ColumnID{0}}, predicate_condition});
-  };
-
-  // The sort merge join supports both anti and semi joins, but only with equal predicates
-  EXPECT_NO_THROW(execute_join(JoinMode::AntiNullAsTrue, PredicateCondition::Equals));
-  EXPECT_NO_THROW(execute_join(JoinMode::AntiNullAsFalse, PredicateCondition::Equals));
-  EXPECT_NO_THROW(execute_join(JoinMode::Semi, PredicateCondition::Equals));
-  EXPECT_THROW(execute_join(JoinMode::AntiNullAsTrue, PredicateCondition::LessThan), std::logic_error);
-  EXPECT_THROW(execute_join(JoinMode::AntiNullAsFalse, PredicateCondition::LessThan), std::logic_error);
-  EXPECT_THROW(execute_join(JoinMode::Semi, PredicateCondition::LessThan), std::logic_error);
-
-  // The sort merge join does not support cross joins.
-  EXPECT_THROW(execute_join(JoinMode::Cross, PredicateCondition::Equals), std::logic_error);
-
-  // The sort merge join supports the != join predicate, but not for outer joins
-  EXPECT_NO_THROW(execute_join(JoinMode::Inner, PredicateCondition::NotEquals));
-  EXPECT_NO_THROW(execute_join(JoinMode::Inner, PredicateCondition::NotEquals));
-  EXPECT_THROW(execute_join(JoinMode::Left, PredicateCondition::NotEquals), std::logic_error);
-  EXPECT_THROW(execute_join(JoinMode::Right, PredicateCondition::NotEquals), std::logic_error);
-}
-
 
 }  // namespace opossum
