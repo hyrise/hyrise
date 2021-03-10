7a4cdc0db592b6b3d6a8be40b4ce6d3ac1065acd
martin.boissier <martin.boissier@hpi.de>
2021-03-10 08:22:27 +0100

    Working on signle figure

--- scripts/plot_operator_breakdown.py
+++ scripts/plot_operator_breakdown.py
@@ -7,6 +7,7 @@ that information without blowing up the code of the Hyrise core.
 """
 
 import glob
+import math
 import matplotlib.pyplot as plt
 import pandas as pd
 import re
@@ -14,7 +15,9 @@ import sys
 import seaborn as sns
 import matplotlib.colors as mplcolors
 import matplotlib.ticker as ticker
+from collections import OrderedDict
 from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.gridspec import GridSpec
 
 if len(sys.argv) not in [1, 2] or len(glob.glob("*-PQP.svg")) == 0:
     exit("Call in a folder containing *-PQP.svg files, pass `paper` as an argument to change legend and hatching")
@@ -65,27 +68,44 @@ df = df.reindex(sorted(df.columns, reverse=True), axis=1)
 
 df = df.fillna(0)
 
-df_nonnorm = df.copy()
+df_norm = df.copy()
 
 # Calculate share of total execution time (i.e., longer running benchmark items are weighted more)
-df.loc["Total"] = df.sum() / df.count()
+df_norm.loc["Total"] = df_norm.sum() / df_norm.count()
 
 # Normalize data from nanoseconds to percentage of total cost (calculated by dividing the cells value by the total of
 # the row it appears in)
-df.iloc[:, 0:] = df.iloc[:, 0:].apply(lambda x: x / x.sum(), axis=1)
+df_norm.iloc[:, 0:] = df_norm.iloc[:, 0:].apply(lambda x: x / x.sum(), axis=1)
 
 # Print the dataframe for easy access to the raw numbers
-print(df)
+print(df_norm)
 
 # Drop all operators that do not exceed 1% in any query
-df = df[df > 0.01].dropna(axis="columns", how="all")
+df_norm = df_norm[df_norm > 0.01].dropna(axis="columns", how="all")
+
+print(df_norm)
 
 # Setup colorscheme - using cubehelix, which provides a color mapping that gracefully degrades to grayscale
-colors = sns.cubehelix_palette(n_colors=len(df), rot=2, reverse=True, light=0.9, dark=0.1, hue=1)
+colors = sns.cubehelix_palette(n_colors=len(df_norm), rot=2, reverse=True, light=0.9, dark=0.1, hue=1)
 cmap = LinearSegmentedColormap.from_list("my_colormap", colors)
 
+
+
+fig = plt.figure(figsize=(20,6))
+
+# Simple heuristic to have at least a 3:1 ratio and scale to larger workloads.
+# TODO: Explain 2 + sqrt  ... + 1 (that's the summary)
+# The sqrt() ensures that huge workloads
+# (e.g., join order benchmarks) do not lead to 100:1 ratios.
+gs = GridSpec(2, 5 + int(round(math.sqrt(len(df_norm)))))
+
+ax_1 = fig.add_subplot(gs[0,:-1]) # Queries, relative
+ax_2 = fig.add_subplot(gs[0,-1]) # Summary, relative
+ax_3 = fig.add_subplot(gs[1,:-1]) # Queries, relative
+ax_4 = fig.add_subplot(gs[1,-1]) # Summary, relative
+
 # Plot it
-ax = df.plot.bar(stacked=True, figsize=(2 + len(df) / 4, 4), colormap=cmap)
+df_norm.plot.bar(ax=ax_1, stacked=True, colormap=cmap)
 
 if paper_mode:
     # Add hatches in paper mode, where graphs may be printed in grayscale
@@ -104,60 +124,40 @@ if paper_mode:
         "",
         "/\\/\\/\\/\\/\\",
     )
-    hatches = [p for p in patterns for i in range(len(df))]
-    for bar, hatch in zip(ax.patches, hatches):
-        # Calculate color so that the hatches are visible but not pushy
-        hsv = mplcolors.rgb_to_hsv(bar.get_facecolor()[:3])
-        hatch_color_hsv = hsv
-        hatch_color_hsv[2] = hsv[2] + 0.2 if hsv[2] < 0.5 else hsv[2] - 0.2
-        bar.set_edgecolor(mplcolors.hsv_to_rgb(hatch_color_hsv))
+    hatches = [p for p in patterns for i in range(len(df_norm))]
+    for axis in [ax_1, ax_2, ax_3, ax_4]:
+        for bar, hatch in zip(axis.patches, hatches):
+            # Calculate color so that the hatches are visible but not pushy
+            hsv = mplcolors.rgb_to_hsv(bar.get_facecolor()[:3])
+            hatch_color_hsv = hsv
+            hatch_color_hsv[2] = hsv[2] + 0.2 if hsv[2] < 0.5 else hsv[2] - 0.2
+            bar.set_edgecolor(mplcolors.hsv_to_rgb(hatch_color_hsv))
 
-        bar.set_hatch(hatch)
-        bar.set_linewidth(0)
+            bar.set_hatch(hatch)
+            bar.set_linewidth(0)
 
 # Set labels
-ax.yaxis.set_major_formatter(ticker.PercentFormatter(xmax=1.0))
-ax.set_ylabel("Share of run time\n(Hiding ops <1%)")
+ax_1.yaxis.set_major_formatter(ticker.PercentFormatter(xmax=1.0))
+ax_1.set_ylabel("Share of run time\n(hiding operators <1%)")
+ax_1.legend().remove()
 
-# Reverse legend so that it matches the stacked bars
-handles, labels = ax.get_legend_handles_labels()
+df = df / 1e9  # to seconds
+df = df[df_norm.columns]  # only show filtered columns of relative chart (>= 1%)
 
-if paper_mode:
-    # Plot the legend under the graph (good for papers)
-    box = ax.get_position()
-    ax.set_position([box.x0, box.y0, box.width, box.height * 0.8])
-    legend = ax.legend(reversed(handles), reversed(labels), loc="lower center", ncol=3, bbox_to_anchor=(0.5, -0.45))
+df.plot.bar(ax=ax_3, stacked=True, colormap=cmap)
+ax_3.set_ylabel("Operator run time [s]\n(hiding operators <1%)")
+ax_3.set_xlabel("Query")
+ax_3.legend().remove()
 
-    print("Plotting in 'paper' mode (legend below graph, hatching) - remove 'paper' argument to change")
-else:
-    # Plot the legend to the right of the graph (good for screens)
-    legend = ax.legend(reversed(handles), reversed(labels), loc="center left", ncol=1, bbox_to_anchor=(1.0, 0.5))
-    print("Plotting in 'screen' mode (legend on right, no hatching) - use 'paper' argument to change")
-
-# Layout and save
-plt.tight_layout()
-plt.savefig("operator_breakdown_relative.pdf", bbox_extra_artists=(legend,), bbox_inches="tight")
-
-
-# reset plt
-import matplotlib as mpl
-mpl.rcParams.update(mpl.rcParamsDefault)
-
-df_nonnorm = df_nonnorm / 1e9  # to seconds
-df_nonnorm = df_nonnorm[df.columns]  # only show filtered columns of relative chart (>= 1%)
-
-fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(2 + len(df_nonnorm) / 4, 4), gridspec_kw={'width_ratios': [len(df_nonnorm), 2]})
-df_nonnorm.plot.bar(ax=ax[0], stacked=True, colormap=cmap)
-ax[0].legend(ncol=17, loc='lower center', bbox_to_anchor=(0.5, 1.05))
-ax[0].set_ylabel("Operator run time [s]\n(hiding operators <1%)")
-ax[0].set_xlabel("Query")
-
-sum_df = df_nonnorm.sum(axis='index').to_frame().T
+sum_df = df.sum(axis='index').to_frame().T
 sum_df.rename(index={0:'Sum'},inplace=True)
 
-sum_df.plot.bar(ax=ax[1], stacked=True, colormap=cmap)
-ax[1].legend().remove()
-ax[1].set_ylabel("Cumulative operater run time [s]")
+sum_df.plot.bar(ax=ax_4, stacked=True, colormap=cmap)
+ax_4.legend().remove()
+ax_4.set_ylabel("Cumulative operater run time [s]")
 
-fig.tight_layout()
-plt.savefig("operator_breakdown_absolute.pdf", bbox_inches="tight")
+
+handles, labels = ax_1.get_legend_handles_labels()
+by_label = OrderedDict(zip(labels, handles))
+fig.legend(by_label.values(), by_label.keys(), loc=9, ncol=10)
+fig.savefig("operator_breakdown_absolute.pdf", bbox_inches="tight")
