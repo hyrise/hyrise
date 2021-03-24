b89273f08c8be8473007cc3142a68f41aec2c577
Julian Menzler <julianmenzler@gmail.com>
2021-03-24 17:30:26 +0100

    Add std::

--- src/benchmarklib/tpch/tpch_benchmark_item_runner.hpp
+++ src/benchmarklib/tpch/tpch_benchmark_item_runner.hpp
@@ -56,7 +56,7 @@ class TPCHBenchmarkItemRunner : public AbstractBenchmarkItemRunner {
   const ClusteringConfiguration _clustering_configuration;
 
   // Used for naming the views generated in query 15
-  atomic_size_t _q15_view_id = 0;
+  std::atomic_size_t _q15_view_id = 0;
 
   // We want deterministic seeds, but since the engine is thread-local, we need to make sure that each thread has its
   // own seed.
