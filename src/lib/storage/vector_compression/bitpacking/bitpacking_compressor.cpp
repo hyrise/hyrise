541f93e591f2e7e514bbb59da1a05afd032bbe9f
Ben Hurdelhey <26392770+ben243871694@users.noreply.github.com>
2021-04-04 23:26:34 +0200

    debug max value errors

--- src/lib/storage/vector_compression/bitpacking/bitpacking_compressor.cpp
+++ src/lib/storage/vector_compression/bitpacking/bitpacking_compressor.cpp
@@ -10,25 +10,26 @@ class BitpackingVector;
 std::unique_ptr<const BaseCompressedVector> BitpackingCompressor::compress(
     const pmr_vector<uint32_t>& vector, const PolymorphicAllocator<size_t>& alloc,
     const UncompressedVectorInfo& meta_info) {
- 
+
   const auto max_value = _find_max_value(vector);
-  uint32_t b_1;
-  if (max_value <= 0) {
-    b_1 = 1;
-  } else if (max_value == 1) {
-    b_1 = 1;
-  } else {
-    b_1 = static_cast<uint32_t>(std::ceil(log2(max_value + 1)));
+  const auto max_value_1 = _find_max_value_1(vector);
+  const auto max_value_2 = meta_info.max_value ? *meta_info.max_value : max_value;
+  
+  if (max_value != max_value_1 || max_value_1 != max_value_2) {
+    std::cerr << "NOT EQUAL" << max_value << "   " << max_value_1 << "  " << max_value_2 << std::endl;
   }
-  uint32_t b = std::min(b_1, std::max(compact::vector<unsigned int, 32>::required_bits(max_value), 1u));
+  
+  auto b = _get_b(max_value);
+  auto b_1 = _get_b(max_value_1);
+  auto b_2 = _get_b(max_value_2);
+
+  if (b != b_1 || b_1 != b_2) {
+    std::cerr << "B NOT EQUAL" << b << "   " << b_1 << "  " << b_2 << std::endl;
+  }
 
   auto data = pmr_bitpacking_vector<uint32_t>(b, alloc);
   data.resize(vector.size());
-  for (size_t i = 0; i < vector.size(); i++) {
-    data[i] = vector[i];
-  }
-  // data.resize(data.size());
-
+  std::copy(vector.begin(), vector.end(), data.begin());
 
   return std::make_unique<BitpackingVector>(std::move(data));
 }
@@ -43,6 +44,24 @@ uint32_t BitpackingCompressor::_find_max_value(const pmr_vector<uint32_t>& vecto
     max |= v;
   }
   return max;
+}
+
+uint32_t BitpackingCompressor::_find_max_value_1(const pmr_vector<uint32_t>& vector) const {
+  const auto it = std::max_element(vector.cbegin(), vector.cend());
+  return *it;
+}
+
+uint32_t BitpackingCompressor::_get_b(uint32_t max_value) const {
+  uint32_t b_1;
+  if (max_value <= 0) {
+    b_1 = 1;
+  } else if (max_value == 1) {
+    b_1 = 1;
+  } else {
+    b_1 = static_cast<uint32_t>(std::ceil(log2(max_value + 1)));
+  }
+  uint32_t b = std::min(b_1, std::max(compact::vector<unsigned int, 32>::required_bits(max_value), 1u));
+  return b;
 }
 
 
