23f38a836cd9020e196573e693372eabeb1eaa74
Julian Menzler <julianmenzler@gmail.com>
2021-04-15 16:16:08 +0200

    Finish Doc

--- src/lib/logical_query_plan/lqp_translator.cpp
+++ src/lib/logical_query_plan/lqp_translator.cpp
@@ -599,8 +599,8 @@ std::shared_ptr<AbstractExpression> LQPTranslator::_translate_expression(
        *  b) In contrast to uncorrelated subqueries, correlated subqueries cannot share identical parts with outer
        *     queries because ExpressionEvaluator::_evaluate_subquery_expression_for_row always deep-copies the whole PQP
        *     at evaluation time. The deep copy includes both correlated and uncorrelated parts.
-       *     Consequently, a new LQPTranslator instance is used for correlated subqueries to prevent deduplication
-       *     with outer queries. Besides, we also prevent correlated subqueries from increasing the consumer count of
+       *     Consequently, a new LQPTranslator instance is used for correlated subqueries to avoid deduplication
+       *     with outer queries. This prevents correlated subqueries from increasing the consumer count of
        *     outer query operators, which would otherwise block the automatic clearing of results.
        */
       auto subquery_pqp = std::shared_ptr<AbstractOperator>();
