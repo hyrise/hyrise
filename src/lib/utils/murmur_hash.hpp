fd2f8d7514a1b620e4382d1d99f88475f6ba1b81
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2021-04-19 17:03:11 +0200

    Fix definietion

--- src/lib/utils/murmur_hash.hpp
+++ src/lib/utils/murmur_hash.hpp
@@ -24,13 +24,13 @@ std::pair<uint64_t, uint64_t> murmur_hash3(const void* key, const int len, const
 
 // murmur hash for builtin number types (int, double)
 template <typename T>
-typename std::enable_if<std::is_arithmetic<T>::value, uint32_t>::type murmur3(T key, uint32_t seed) {
+typename std::enable_if<std::is_arithmetic<T>::value, std::pair<uint64_t, uint64_t>>::type murmur3(T key, uint32_t seed) {
   return murmur_hash3(&key, sizeof(T), seed);
 }
 
 // murmur hash for std::string
 template <typename T>
-typename std::enable_if<std::is_same<T, std::string>::value, uint32_t>::type murmur3(T key, uint32_t seed) {
+typename std::enable_if<std::is_same<T, std::string>::value, std::pair<uint64_t, uint64_t>>::type murmur3(T key, uint32_t seed) {
   return murmur_hash3(key.c_str(), key.size(), seed);
 }
 
