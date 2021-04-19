99a0227b0ef6924765712996a7e026bdacb08cb9
Alexander-Dubrawski <dubrawski.alexander@googlemail.com>
2021-04-19 20:43:18 +0200

    Remove murmur 2 hash

--- src/lib/utils/murmur_hash.hpp
+++ src/lib/utils/murmur_hash.hpp
@@ -6,20 +6,6 @@
 
 namespace opossum {
 
-unsigned int murmur_hash2(const void* key, int len, unsigned int seed);
-
-// murmur hash for builtin number types (int, double)
-template <typename T>
-typename std::enable_if<std::is_arithmetic<T>::value, unsigned int>::type murmur2(T key, unsigned int seed) {
-  return murmur_hash2(&key, sizeof(T), seed);
-}
-
-// murmur hash for std::string
-template <typename T>
-typename std::enable_if<std::is_same<T, pmr_string>::value, unsigned int>::type murmur2(T key, unsigned int seed) {
-  return murmur_hash2(key.c_str(), static_cast<int>(key.size()), seed);
-}
-
 std::pair<uint64_t, uint64_t> murmur_hash3_x64_128(const void* key, const int len, const uint32_t seed);
 
 // murmur hash for builtin number types
