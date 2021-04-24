46b368e5e1df5e1370c31b6dbb0a07362bfe8e0a
Ben Hurdelhey <26392770+ben243871694@users.noreply.github.com>
2021-04-24 16:16:02 +0200

    lint

--- src/lib/storage/vector_compression/bitpacking/bitpacking_compressor.cpp
+++ src/lib/storage/vector_compression/bitpacking/bitpacking_compressor.cpp
@@ -16,14 +16,14 @@ std::unique_ptr<const BaseCompressedVector> BitPackingCompressor::compress(const
 
   auto required_bits = 1u;
   if (max_element_it != vector.cend() && *max_element_it != 0) {
-      // add 1 to max_value because log2(1) = 0 but we need one bit to represent it.
+    // add 1 to max_value because log2(1) = 0 but we need one bit to represent it.
     required_bits = static_cast<uint32_t>(std::ceil(log2(*max_element_it + 1u)));
   }
 
   auto data = pmr_compact_vector<uint32_t>(required_bits, vector.size(), alloc);
 
   // compactvector does not zero initialize it's memory, which leads to non-reproducible tests that
-  // use the binary writer (due to alignment, some memory at the end is not overwritten). 
+  // use the binary writer (due to alignment, some memory at the end is not overwritten).
   // Hence, fill the internal memory with zeroes.
   std::fill_n(data.get(), data.bytes() / 8, 0);
 
