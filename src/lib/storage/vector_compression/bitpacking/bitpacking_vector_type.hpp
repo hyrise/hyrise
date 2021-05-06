a1690f8418f8fc798e507bb63d7d49902e9bf435
Ben Hurdelhey <26392770+ben243871694@users.noreply.github.com>
2021-05-06 15:51:57 +0200

    trigger pipeline

--- src/lib/storage/vector_compression/bitpacking/bitpacking_vector_type.hpp
+++ src/lib/storage/vector_compression/bitpacking/bitpacking_vector_type.hpp
@@ -5,6 +5,7 @@
 
 namespace opossum {
 
+// right now, only uint32_t is used as value type
 template <typename T>
 using pmr_compact_vector = compact::vector<T, 0u, uint64_t, PolymorphicAllocator<uint64_t>>;
 
