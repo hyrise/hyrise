ec31eeb5d189038bb17cbcb799770eb5dad983fa
Ben Hurdelhey <26392770+ben243871694@users.noreply.github.com>
2021-05-07 15:53:32 +0200

    add vector type include again

--- src/lib/import_export/binary/binary_writer.hpp
+++ src/lib/import_export/binary/binary_writer.hpp
@@ -11,6 +11,7 @@
 #include "storage/reference_segment.hpp"
 #include "storage/run_length_segment.hpp"
 #include "storage/value_segment.hpp"
+#include "storage/vector_compression/bitpacking/bitpacking_vector_type.hpp"
 #include "utils/assert.hpp"
 
 namespace opossum {
