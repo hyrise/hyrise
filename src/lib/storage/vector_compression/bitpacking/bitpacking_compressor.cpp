8a904b19bc0d0d6e4fa79b63b845cffc74897bfc
Ben Hurdelhey <26392770+ben243871694@users.noreply.github.com>
2021-05-09 18:03:21 +0200

    update docs to reflect compact vector alignment

--- src/lib/storage/vector_compression/bitpacking/bitpacking_compressor.cpp
+++ src/lib/storage/vector_compression/bitpacking/bitpacking_compressor.cpp
@@ -22,11 +22,15 @@ std::unique_ptr<const BaseCompressedVector> BitPackingCompressor::compress(const
 
   auto data = pmr_compact_vector(required_bits, vector.size(), alloc);
 
-  // compactvector does not zero initialize its memory, which leads to non-reproducible tests that
-  // use the binary writer (due to alignment, some memory at the end is not overwritten).
-  // Hence, fill the internal memory with zeroes.
-  // data.bytes() gives the number of allocated bytes for the internal memory.
-  // std::fill_n(data.get(), data.bytes() / sizeof(*data.get()), 0);
+  /**
+   * The compact::vector does not zero initialize its memory, which leads to non-reproducible tests that
+   * use the binary writer. The compact::vector allocates memory aligned to its word size and due to this,
+   * some memory at the end is not always overwritten. Hence, fill the internal memory with zeroes.
+   * Therefore, data.bytes() gives the number of allocated bytes for the internal memory (word-aligned).
+   * (see bitpacking_vector_type.hpp)
+   *
+   */
+  std::fill_n(data.get(), data.bytes() / sizeof(*data.get()), 0);
 
   std::copy(vector.cbegin(), vector.cend(), data.begin());
 
--- src/lib/storage/vector_compression/bitpacking/bitpacking_vector_type.hpp
+++ src/lib/storage/vector_compression/bitpacking/bitpacking_vector_type.hpp
@@ -5,7 +5,15 @@
 
 namespace opossum {
 
-// Right now, compact_vector is only used in the BitpackingVector where the value type uint32_t is sufficient.
+/**
+ * @brief Template arguments of compact::vector<typename IDX, unsigned BITS, typename W, typename Allocator>
+ *
+ * IDX: right now, compact_vector is only used in the BitpackingVector where the value type uint32_t is sufficient.
+ * BITS: 0 means that the bit width is determined at runtime
+ * W: the compact::vector allocates its memory in chunks of the specified word size.
+ * Allocator: the allocator should allocate the same value_type as W
+ */
+
 using pmr_compact_vector = compact::vector<uint32_t, 0u, uint64_t, PolymorphicAllocator<uint64_t>>;
 
 }  // namespace opossum
