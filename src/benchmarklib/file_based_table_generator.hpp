53cc73aeaf2789f057f052c399f296a2c81d83f3
Julian Menzler <julianmenzler@gmail.com>
2021-05-19 12:43:16 +0200

    Add FileBasedTableGenerator::set_add_constraints_callback

--- src/benchmarklib/file_based_table_generator.hpp
+++ src/benchmarklib/file_based_table_generator.hpp
@@ -13,8 +13,16 @@ class FileBasedTableGenerator : virtual public AbstractTableGenerator {
 
   std::unordered_map<std::string, BenchmarkTableInfo> generate() override;
 
+  /**
+   * @param add_constraints_callback is called after the table data has been added to the StorageManager.
+   * It should be used to define table constraints, if available.
+   */
+  void set_add_constraints_callback(const std::function<void()>& add_constraints_callback);
+
  protected:
+  void _add_constraints(std::unordered_map<std::string, BenchmarkTableInfo>& table_info_by_name) const override;
   const std::string _path;
+  std::function<void()> _add_constraints_callback;
 };
 
 }  // namespace opossum
