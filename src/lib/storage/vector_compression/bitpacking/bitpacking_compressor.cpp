2c813a7bfc14dd3ab098e26e5c1667377c56bc76
Ben Hurdelhey <26392770+ben243871694@users.noreply.github.com>
2021-05-22 23:04:11 +0200

    rename compact::vector to compact_vector

--- src/lib/storage/vector_compression/bitpacking/bitpacking_compressor.cpp
+++ src/lib/storage/vector_compression/bitpacking/bitpacking_compressor.cpp
@@ -16,20 +16,20 @@ std::unique_ptr<const BaseCompressedVector> BitPackingCompressor::compress(const
 
   auto required_bits = 1u;
   if (max_element_it != vector.cend() && *max_element_it != 0) {
-    // add 1 to max_value because log2(1) = 0 but we need one bit to represent it.
+    // add 1 to the maximum value because log2(1) = 0 but we need one bit to represent it.
     required_bits = static_cast<uint32_t>(std::ceil(log2(*max_element_it + 1u)));
   }
 
   auto data = pmr_compact_vector(required_bits, vector.size(), alloc);
 
   /**
-   * The compact::vector does not zero initialize its memory, which leads to non-reproducible tests that
-   * use the binary writer. The compact::vector allocates memory aligned to its word size and due to this,
+   * The compact_vector does not zero initialize its memory, which leads to non-reproducible tests that
+   * use the binary writer. The compact_vector allocates memory aligned to its word size and due to this,
    * some memory at the end is not always overwritten. Hence, fill the internal memory with zeroes.
-   * Unfortunately, compact::vector does not give us a better interface. Therefore, we proceed to work with
-   * a raw pointer to the compact::vector's internal memory and fill it with zeroes. For this, data.bytes() 
-   * gives the number of allocated bytes for the internal memory 
-   * (word-aligned, see bitpacking_vector_type.hpp). 
+   * Unfortunately, compact_vector does not give us a better interface. Therefore, we proceed to work with
+   * a raw pointer to the compact_vector's internal memory and fill it with zeroes. For this, data.bytes()
+   * gives the number of allocated bytes for the internal memory
+   * (word-aligned, see bitpacking_vector_type.hpp).
    *
    */
   std::fill_n(data.get(), data.bytes() / sizeof(*data.get()), 0ULL);
--- src/lib/storage/vector_compression/bitpacking/bitpacking_vector_type.hpp
+++ src/lib/storage/vector_compression/bitpacking/bitpacking_vector_type.hpp
@@ -6,11 +6,11 @@
 namespace opossum {
 
 /**
- * @brief Template arguments of compact::vector<typename IDX, unsigned BITS, typename W, typename Allocator>
+ * @brief Template arguments of compact_vector<typename IDX, unsigned BITS, typename W, typename Allocator>
  *
  * IDX: right now, compact_vector is only used in the BitpackingVector where the value type uint32_t is sufficient.
  * BITS: 0 means that the bit width is determined at runtime
- * W: the compact::vector allocates its memory in chunks of the specified word size.
+ * W: the compact_vector allocates its memory in chunks of the specified word size.
  * Allocator: the allocator should allocate the same value_type as W
  */
 
