ae548cfac90eaf230e2c492bb625ff97660ad151
Moritz Manner <moritz.manner@gmail.com>
2021-06-07 14:00:14 +0200

    Fix missing variable

--- src/lib/storage/fsst_segment/fsst_encoder.hpp
+++ src/lib/storage/fsst_segment/fsst_encoder.hpp
@@ -18,6 +18,7 @@ namespace opossum {
 class FSSTEncoder : public SegmentEncoder<FSSTEncoder> {
  public:
   static constexpr auto _encoding_type = enum_c<EncodingType, EncodingType::FSST>;
+  static constexpr auto _uses_vector_compression = true;
 
   // TODO (anyone): check if this method is needed for FSST
   template <typename T>
@@ -27,14 +28,14 @@ class FSSTEncoder : public SegmentEncoder<FSSTEncoder> {
 //    const auto num_elements = value_segment->size();
 
     //TODO (anyone): add parameters to create empty Segment
-    return std::make_shared<FSSTSegment<T>>(...);
+    return nullptr;
   }
 
   std::shared_ptr<AbstractEncodedSegment> _on_encode(const AnySegmentIterable<pmr_string> segment_iterable,
                                                      const PolymorphicAllocator<pmr_string>& allocator) {
 
     //TODO (anyone): add parameters to create empty Segment
-    return std::make_shared<FSSTSegment<T>>();
+    return nullptr;
   }
 };
 
