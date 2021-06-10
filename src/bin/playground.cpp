6b4bec127b5fcad70cb0aadf3c0386be3e84bb81
Christopher Aust <christopher.aust@student.hpi.uni-potsdam.de>
2021-06-10 17:33:32 +0200

    Playground testing
    Co-authored-by: Moritz Manner <moritz.manner@gmail.com>
    Co-authored-by: Peter Tsayun <petr.tsayun@student.hpi.de>

--- src/bin/playground.cpp
+++ src/bin/playground.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
 
+#include <fsst.h>
 #include "types.hpp"
 
 using namespace opossum;  // NOLINT
 
 int main() {
-  std::cout << "Hello world!!" << std::endl;
+  std::vector<std::string> values{"Moritz", "Chris", "Christopher", "Mo", "Peter", "Petrus"};
+  std::vector<unsigned long> row_lens, compressedRowLens;
+  std::vector<unsigned char*> row_ptrs, compressedRowPtrs;
+
+  row_lens.reserve(values.size());
+  row_ptrs.reserve(values.size());
+  compressedRowLens.resize(values.size());
+  compressedRowPtrs.resize(values.size() + 1);
+
+  unsigned totalLen = 0;
+
+  for(std::string& value: values){
+    totalLen += value.size();
+    row_lens.push_back(value.size());
+    row_ptrs.push_back(reinterpret_cast<unsigned char*>(const_cast<char*>(value.data())));
+  }
+
+  std::vector<unsigned char> compressionBuffer, fullBuffer;
+
+  compressionBuffer.resize(16 + 2 * totalLen);
+
+  fsst_encoder_t* encoder = fsst_create(values.size(), row_lens.data(), row_ptrs.data(), 1);
+  fsst_compress(encoder, values.size(), row_lens.data(), row_ptrs.data(), compressionBuffer.size(), compressionBuffer.data(), compressedRowLens.data(), compressedRowPtrs.data());
+
+  for(size_t i = 0; i < compressedRowPtrs.size(); i ++) {
+    for(size_t j = 0; j < compressedRowLens[i]; j++){
+
+      printf("%d ", compressedRowPtrs[i][j]);
+    }
+    printf("\n");
+  }
+//  std::cout << "blaaa";
   return 0;
 }
