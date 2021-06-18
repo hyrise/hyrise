d6aa321f46b7ab0b55829a9aac573db0f887467b
Peter Tsayun <petr.tsayun@student.hpi.de>
2021-06-18 08:48:16 +0000

    Adjust FSST test
    Co-authored-by: Moritz Manner <moritz.manner@gmail.com>
    Co-authored-by: Christopher Aust  <christopher.aust@student.hpi.uni-potsdam.de>

--- src/test/lib/storage/fsst_segment_test.cpp
+++ src/test/lib/storage/fsst_segment_test.cpp
@@ -17,8 +17,8 @@ namespace opossum {
 
 class StorageFSSTSegmentTest : public BaseTest {
  protected:
-//  static constexpr auto row_count = FSSTEncoder::_block_size + size_t{1000u};
-//  std::shared_ptr<ValueSegment<pmr_string>> vs_str = std::make_shared<ValueSegment<pmr_string>>(true);
+  //  static constexpr auto row_count = FSSTEncoder::_block_size + size_t{1000u};
+  //  std::shared_ptr<ValueSegment<pmr_string>> vs_str = std::make_shared<ValueSegment<pmr_string>>(true);
 };
 
 template <typename T>
@@ -27,8 +27,14 @@ std::shared_ptr<FSSTSegment<T>> compress(std::shared_ptr<ValueSegment<T>> segmen
   return std::dynamic_pointer_cast<FSSTSegment<T>>(encoded_segment);
 }
 
-TEST_F(StorageFSSTSegmentTest, IdleTest) {
-  auto empty_str_segment = compress(std::make_shared<ValueSegment<pmr_string>>(true), DataType::String);
+//TEST_F(StorageFSSTSegmentTest, IdleTest) {
+//  auto empty_str_segment = compress(std::make_shared<ValueSegment<pmr_string>>(true), DataType::String);
+//}
+
+TEST_F(StorageFSSTSegmentTest, CreateFSSTSegmentTest) {
+  pmr_vector<pmr_string> values{"Moritz", "ChrisChr", "Christopher", "Mo", "Peter", "Petrus", "ababababababababababab"};
+  pmr_vector<bool> null_values = {false};
+  FSSTSegment<pmr_string> segment(values, null_values);
 }
 
 }  // namespace opossum
