536624665fd114e114b8952e0f79685afefbf517
Halil Göcer <Halil.Goecer@student.hpi.uni-potsdam.de>
2021-06-24 19:37:19 +0000

    Fix bug, add memory usage output and refactoring
    Co-authored-by: kris-fillip <kris-fillip@users.noreply.github.com>
    Co-authored-by: Maximilian Stiede <maximilian.stiede@student.hpi.de>

--- src/bin/playground.cpp
+++ src/bin/playground.cpp
@@ -14,6 +14,7 @@
 #include "storage/dictionary_segment.hpp"
 #include "synthetic_table_generator.hpp"
 #include "types.hpp"
+#include "utils/size_estimation_utils.hpp"
 
 using namespace opossum;  // NOLINT
 
@@ -21,22 +22,39 @@ using namespace opossum;  // NOLINT
 // IDEA: set_union can be optimized if jaccard index threshold is not reachable by exiting earlier.
 
 template <typename T>
-struct SegmentLocationData {
+struct SegmentChunkColumn {
   std::shared_ptr<DictionarySegment<T>> segment;
   std::shared_ptr<Chunk> chunk;
   ColumnID column_id;
 };
 
+template <typename T>
+size_t calc_dictionary_memory_usage(const std::shared_ptr<const pmr_vector<T>> dictionary) {
+  // TODO(hig): It can happen that the new dictionary is bigger than the old one, although the dictionaries have the same values
+  // Is there maybe a string compression?
+
+  if constexpr (std::is_same_v<T, pmr_string>) {
+    return string_vector_memory_usage(*dictionary, MemoryUsageCalculationMode::Sampled);
+  }
+  return dictionary->size() * sizeof(typename decltype(dictionary)::element_type::value_type);
+}
+
 /**
 * Replaces the dictionary segments of the given segments by new dictionary segments that have a shared dictionary.
-*/
+* Returns the difference of the estimated memory usage.
+**/
 template <typename T>
-void apply_shared_dictionary_to_segments(const std::vector<SegmentLocationData<T>>& segmentLocationData,
-                                         const std::shared_ptr<pmr_vector<T>> shared_dictionary,
-                                         const PolymorphicAllocator<T>& allocator) {
+int64_t apply_shared_dictionary_to_segments(const std::vector<SegmentChunkColumn<T>>& segment_chunk_columns,
+                                            const std::shared_ptr<const pmr_vector<T>> shared_dictionary,
+                                            const PolymorphicAllocator<T>& allocator) {
+  auto previous_attribute_vector_memory_usage = 0l;
+  auto new_attribute_vector_memory_usage = 0l;
+  auto previous_dictionary_memory_usage = 0l;
+  auto new_dictionary_memory_usage = static_cast<int64_t>(calc_dictionary_memory_usage(shared_dictionary));
+
   const auto max_value_id = static_cast<uint32_t>(shared_dictionary->size());
-  for (auto segmentLocation : segmentLocationData) {
-    const auto segment = segmentLocation.segment;
+  for (auto segment_chunk_column : segment_chunk_columns) {
+    const auto segment = segment_chunk_column.segment;
     const auto chunk_size = segment->size();
 
     // Construct new attribute vector that maps to the shared dictionary
@@ -66,42 +84,58 @@ void apply_shared_dictionary_to_segments(const std::vector<SegmentLocationData<T
         std::make_shared<DictionarySegment<T>>(shared_dictionary, compressed_attribute_vector);
 
     // Replace dictionary segment with new one that has a shared dictionary
-    segmentLocation.chunk->replace_segment(segmentLocation.column_id, new_dictionary_segment);
+    previous_attribute_vector_memory_usage += segment->attribute_vector()->data_size();
+    previous_dictionary_memory_usage += calc_dictionary_memory_usage(segment->dictionary());
+
+    segment_chunk_column.chunk->replace_segment(segment_chunk_column.column_id, new_dictionary_segment);
+
+    new_attribute_vector_memory_usage += new_dictionary_segment->attribute_vector()->data_size();
   }
+
+  
+
+  const auto attribute_vector_memory_usage_diff =
+      new_attribute_vector_memory_usage - previous_attribute_vector_memory_usage;
+  const auto dictionary_memory_usage_diff = new_dictionary_memory_usage - previous_dictionary_memory_usage;
+  
+  std::cout << "Merged " << segment_chunk_columns.size()
+            << " dictionaries. (dict_mem_diff = " << dictionary_memory_usage_diff
+            << ", attr_vec_diff = " << attribute_vector_memory_usage_diff << ")" << std::endl;
+
+  return attribute_vector_memory_usage_diff + dictionary_memory_usage_diff;
 }
 
-/**
-* Calculates and returns Jaccard index.
-*/
 double calc_jaccard_index(size_t union_size, size_t intersection_size) { return intersection_size * 1.0 / union_size; }
 
 void log_jaccard_index(const double jaccard_index, const std::string& table_name, const std::string& column_name,
                        const std::string& compare_type, const DataType& column_data_type, const ChunkID chunk_id,
                        std::ofstream& output_file_stream) {
   const auto data_type_name = data_type_to_string.left.at(column_data_type);
-  std::cout << "Jaccard index = " << jaccard_index << " (" << compare_type << ") (Table=" << table_name
-            << ", Column=" << column_name << ", DataType=" << column_data_type << ", Chunk=" << chunk_id << "\n";
+  // std::cout << "Jaccard index = " << jaccard_index << " (" << compare_type << ") (Table=" << table_name
+  //           << ", Column=" << column_name << ", DataType=" << column_data_type << ", Chunk=" << chunk_id << "\n";
   output_file_stream << table_name << ";" << column_name << ";" << data_type_name << ";" << chunk_id << ";"
                      << compare_type << ";" << jaccard_index << std::endl;
 }
 
 int main() {
   std::cout << "Playground: Jaccard-Index" << std::endl;
-  uint64_t total_merged_dictionaries = 0;
-  uint64_t total_new_shared_dictionaries = 0;
+  auto total_merged_dictionaries = 0ul;
+  auto total_new_shared_dictionaries = 0ul;
+  auto memory_usage_difference = 0l;
+
   // Generate benchmark data
   const auto jaccard_index_threshold = 0.6;
-  const auto table_path = std::string{"/home/Halil.Goecer/imdb/"};
-  
-  // const auto table_generator = std::make_unique<FileBasedTableGenerator>(
-  //     std::make_shared<BenchmarkConfig>(BenchmarkConfig::get_default_config()), table_path);
-  // table_generator->generate_and_store();
+  const auto table_path = std::string{"/home/Halil.Goecer/imdb_bin/"};
+
+  const auto table_generator = std::make_unique<FileBasedTableGenerator>(
+      std::make_shared<BenchmarkConfig>(BenchmarkConfig::get_default_config()), table_path);
+  table_generator->generate_and_store();
 
   // Get tables using storage manager
   auto& sm = Hyrise::get().storage_manager;
 
-  const auto table = BinaryParser::parse(table_path + "company_name.bin");
-  sm.add_table("company_name", table);
+  //const auto table = BinaryParser::parse(table_path + "company_name.bin");
+  //sm.add_table("company_name", table);
 
   // Create output file
   auto output_file_stream = std::ofstream("jaccard_index_log.csv", std::ofstream::out | std::ofstream::trunc);
@@ -114,7 +148,7 @@ int main() {
   // The jaccard index is calculated between a dictionary segment and its preceding dictionary segment
   for (const auto table_name : table_names) {
     const auto table = sm.get_table(table_name);
-    // BinaryWriter::write(*table, table_path + "company_name.bin");
+    //BinaryWriter::write(*table, table_path + table_name + ".bin");
     const auto column_count = table->column_count();
     const auto chunk_count = table->chunk_count();
 
@@ -128,7 +162,7 @@ int main() {
 
         auto previous_shared_dictionary = std::make_shared<pmr_vector<ColumnDataType>>(allocator);
         auto current_shared_dictionary = std::make_shared<pmr_vector<ColumnDataType>>(allocator);
-        auto dictionary_segments_to_merge = std::vector<SegmentLocationData<ColumnDataType>>{};
+        auto dictionary_segments_to_merge = std::vector<SegmentChunkColumn<ColumnDataType>>{};
 
         std::shared_ptr<DictionarySegment<ColumnDataType>> previous_dictionary_segment = nullptr;
         std::shared_ptr<DictionarySegment<ColumnDataType>> current_dictionary_segment = nullptr;
@@ -188,7 +222,8 @@ int main() {
 
           if (current_jaccard_index >= jaccard_index_threshold) {
             // The jaccard index matches the threshold, so we add the segment to the collection
-            const auto segmentLocation = SegmentLocationData<ColumnDataType>{current_dictionary_segment, chunk, column_id};
+            const auto segmentLocation =
+                SegmentChunkColumn<ColumnDataType>{current_dictionary_segment, chunk, column_id};
             dictionary_segments_to_merge.push_back(segmentLocation);
             current_shared_dictionary = potential_new_shared_dictionary;
           }
@@ -197,19 +232,20 @@ int main() {
           if (current_jaccard_index < jaccard_index_threshold || is_last_chunk) {
             // The jaccard index is below the threshold or we processed the last chunk.
             // This means that we apply the the shared dictionary to the collected segments.
-            if (!dictionary_segments_to_merge.empty()) {
+            if (dictionary_segments_to_merge.size() >= 2) {
               total_merged_dictionaries += dictionary_segments_to_merge.size();
               total_new_shared_dictionaries++;
-              apply_shared_dictionary_to_segments<ColumnDataType>(dictionary_segments_to_merge,
-                                                                  current_shared_dictionary, allocator);
 
-              // Save current_shared_dictionary to previous_shared_dictionary and reset
+              memory_usage_difference += apply_shared_dictionary_to_segments<ColumnDataType>(
+                  dictionary_segments_to_merge, current_shared_dictionary, allocator);
+
+              // Save current_shared_dictionary to previous_shared_dictionary
               std::swap(previous_shared_dictionary, current_shared_dictionary);
-              current_shared_dictionary->clear();
-              dictionary_segments_to_merge.clear();
-            } else {
-              current_shared_dictionary->clear();
             }
+
+            // Reset
+            current_shared_dictionary->clear();
+            dictionary_segments_to_merge.clear();
           }
 
           log_jaccard_index(current_jaccard_index, table_name, column_name, current_compare_type, column_data_type,
@@ -221,7 +257,9 @@ int main() {
     }
   }
 
-  std::cout << "Merged " << total_merged_dictionaries << " dictionaries to " << total_new_shared_dictionaries << " shared dictionaries.\n";
+  std::cout << "Merged " << total_merged_dictionaries << " dictionaries to " << total_new_shared_dictionaries
+            << " shared dictionaries.\n";
+  std::cout << "The estimated memory change is: " << memory_usage_difference << " bytes.\n";
   output_file_stream.close();
   return 0;
 }
