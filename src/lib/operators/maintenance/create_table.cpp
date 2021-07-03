6c2461ae0578b0695826f521baa80d58515c2cb9
Jostafarr <jost.goette@web.de>
2021-07-03 12:33:26 +0200

    adds table key constraint support to create table operator and tests it

--- src/lib/operators/maintenance/create_table.cpp
+++ src/lib/operators/maintenance/create_table.cpp
@@ -10,10 +10,12 @@
 namespace opossum {
 
 CreateTable::CreateTable(const std::string& init_table_name, const bool init_if_not_exists,
-                         const std::shared_ptr<const AbstractOperator>& input_operator)
+                         const std::shared_ptr<const AbstractOperator>& input_operator,
+                         std::shared_ptr<TableKeyConstraints> init_key_constraints)
     : AbstractReadWriteOperator(OperatorType::CreateTable, input_operator),
       table_name(init_table_name),
-      if_not_exists(init_if_not_exists) {}
+      if_not_exists(init_if_not_exists),
+      key_constraints(init_key_constraints){}
 
 const std::string& CreateTable::name() const {
   static const auto name = std::string{"CreateTable"};
@@ -69,14 +71,22 @@ std::shared_ptr<const Table> CreateTable::_on_execute(std::shared_ptr<Transactio
     _insert->set_transaction_context(context);
     _insert->execute();
   }
-  return std::make_shared<Table>(TableColumnDefinitions{{"OK", DataType::Int, false}}, TableType::Data);  // Dummy table
+  auto table = std::make_shared<Table>(TableColumnDefinitions{{"OK", DataType::Int, false}}, TableType::Data);  // Dummy table
+
+  if (key_constraints != nullptr ){
+    for (const TableKeyConstraint& table_key_constraint : *key_constraints){
+        table->Table::add_soft_key_constraint(table_key_constraint);
+      }
+  }
+
+  return table;
 }
 
 std::shared_ptr<AbstractOperator> CreateTable::_on_deep_copy(
     const std::shared_ptr<AbstractOperator>& copied_left_input,
     const std::shared_ptr<AbstractOperator>& copied_right_input,
     std::unordered_map<const AbstractOperator*, std::shared_ptr<AbstractOperator>>& copied_ops) const {
-  return std::make_shared<CreateTable>(table_name, if_not_exists, copied_left_input);
+  return std::make_shared<CreateTable>(table_name, if_not_exists, copied_left_input, key_constraints);
 }
 
 void CreateTable::_on_set_parameters(const std::unordered_map<ParameterID, AllTypeVariant>& parameters) {
