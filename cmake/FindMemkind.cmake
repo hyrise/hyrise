80297165af7f737afd695262b75c306e795eaa42
Keven Richly <keven.richly@hpi.uni-potsdam.de>
2021-07-09 10:53:12 +0200

    add hint to Markus memkind installation

--- cmake/FindMemkind.cmake
+++ cmake/FindMemkind.cmake
@@ -1,10 +1,10 @@
 FIND_PATH(MEMKIND_INCLUDE_DIR NAME memkind.h
-    HINTS /home/keven.richly/memkind/include /home/Keven.Richly/memkind/include /mnt/nvrams1/epic/ubuntu/memkind/include /hpi/fs00/home/keven.richly/env/memkind/include
+    HINTS /home/keven.richly/memkind/include /home/Keven.Richly/memkind/include /mnt/nvrams1/epic/ubuntu/memkind/include /hpi/fs00/home/keven.richly/env/memkind/include /home/Markus.Dreseler/memkind/include
     NO_DEFAULT_PATH NO_CMAKE_ENVIRONMENT_PATH NO_CMAKE_PATH NO_SYSTEM_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH 
 )
 
 FIND_LIBRARY(MEMKIND_LIBRARY NAME memkind
-    HINTS /home/keven.richly/memkind/lib /home/Keven.Richly/memkind/lib /mnt/nvrams1/epic/ubuntu/memkind/.libs /hpi/fs00/home/keven.richly/env/memkind/lib
+    HINTS /home/keven.richly/memkind/lib /home/Keven.Richly/memkind/lib /mnt/nvrams1/epic/ubuntu/memkind/.libs /hpi/fs00/home/keven.richly/env/memkind/lib /home/Markus.Dreseler/memkind/lib
     )
 
 IF (MEMKIND_INCLUDE_DIR AND MEMKIND_LIBRARY)
