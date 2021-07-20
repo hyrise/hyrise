94e9a9d36d1712e15cbb2617dfe713b749d127ac
Vincent Xeno Rahn <vinni@akv.i24.cc>
2021-07-21 01:19:57 +0200

    Change get_table_indexes return type to pmr_vector

--- src/lib/storage/table.cpp
+++ src/lib/storage/table.cpp
@@ -398,7 +398,7 @@ void Table::set_value_clustered_by(const std::vector<ColumnID>& value_clustered_
   _value_clustered_by = value_clustered_by;
 }
 
-std::vector<std::shared_ptr<AbstractTableIndex>> Table::get_table_indexes() const { return _table_indexes; }
+pmr_vector<std::shared_ptr<AbstractTableIndex>> Table::get_table_indexes() const { return _table_indexes; }
 
 std::vector<std::shared_ptr<AbstractTableIndex>> Table::get_table_indexes(const ColumnID column_id) const {
   auto result = std::vector<std::shared_ptr<AbstractTableIndex>>();
