15c083b78a2b714392c55f400cae7b0e61841ecd
Marc Rosenau <Marc.Rosenau@vm-nordes.eaalab.hpi.uni-potsdam.de>
2021-07-30 10:58:34 +0000

    Finish test to check if correct number of buckets are created with valuecount under K

--- src/test/lib/statistics/statistics_objects/top_k_uniform_distribution_histogram_test.cpp
+++ src/test/lib/statistics/statistics_objects/top_k_uniform_distribution_histogram_test.cpp
@@ -23,41 +23,58 @@ class TopKUniformDistributionHistogramTest : public BaseTest {
   std::shared_ptr<Table> _string2;
 };
 
+/* Test for strings if the number of values is below K, every value has its own bucket */
 TEST_F(TopKUniformDistributionHistogramTest, FromColumnString) {
   StringHistogramDomain default_domain;
   const auto default_domain_histogram =
       TopKUniformDistributionHistogram<pmr_string>::from_column(*_string2, ColumnID{0}, default_domain);
 
-  ASSERT_EQ(default_domain_histogram->bin_count(), 4u);
-  EXPECT_EQ(default_domain_histogram->bin(BinID{0}), HistogramBin<pmr_string>("aa", "birne", 3, 3));
-  EXPECT_EQ(default_domain_histogram->bin(BinID{1}), HistogramBin<pmr_string>("bla", "ttt", 4, 3));
-  EXPECT_EQ(default_domain_histogram->bin(BinID{2}), HistogramBin<pmr_string>("uuu", "xxx", 4, 3));
+  // _string2 has 11 distinct values
+  ASSERT_EQ(default_domain_histogram->bin_count(), 11u);
+  EXPECT_EQ(default_domain_histogram->bin(BinID{0}), HistogramBin<pmr_string>("aa", "aa", 1, 1));
+  EXPECT_EQ(default_domain_histogram->bin(BinID{1}), HistogramBin<pmr_string>("b", "b", 1, 1));
+  EXPECT_EQ(default_domain_histogram->bin(BinID{2}), HistogramBin<pmr_string>("birne", "birne", 1, 1));
+  EXPECT_EQ(default_domain_histogram->bin(BinID{3}), HistogramBin<pmr_string>("bla", "bla", 1, 1));
+  EXPECT_EQ(default_domain_histogram->bin(BinID{4}), HistogramBin<pmr_string>("bums", "bums", 1, 1));
+  EXPECT_EQ(default_domain_histogram->bin(BinID{5}), HistogramBin<pmr_string>("ttt", "ttt", 2, 1));
+  EXPECT_EQ(default_domain_histogram->bin(BinID{6}), HistogramBin<pmr_string>("uuu", "uuu", 2, 1));
+  EXPECT_EQ(default_domain_histogram->bin(BinID{7}), HistogramBin<pmr_string>("www", "www", 1, 1));
+  EXPECT_EQ(default_domain_histogram->bin(BinID{8}), HistogramBin<pmr_string>("xxx", "xxx", 1, 1));
+  EXPECT_EQ(default_domain_histogram->bin(BinID{9}), HistogramBin<pmr_string>("yyy", "yyy", 1, 1));
+  EXPECT_EQ(default_domain_histogram->bin(BinID{10}), HistogramBin<pmr_string>("zzz", "zzz", 3, 1));
 
-  StringHistogramDomain reduced_histogram{'a', 'c', 9};
-  const auto reduced_domain_histogram =
-      TopKUniformDistributionHistogram<pmr_string>::from_column(*_string2, ColumnID{0}, default_domain);
-
-  ASSERT_EQ(default_domain_histogram->bin_count(), 4u);
-  EXPECT_EQ(default_domain_histogram->bin(BinID{0}), HistogramBin<pmr_string>("aa", "birne", 3, 3));
-  EXPECT_EQ(default_domain_histogram->bin(BinID{1}), HistogramBin<pmr_string>("bla", "ttt", 4, 3));
-  EXPECT_EQ(default_domain_histogram->bin(BinID{2}), HistogramBin<pmr_string>("uuu", "xxx", 4, 3));
 }
 
+
+/* Test for ints if the number of values is below K, every value has its own bucket */
 TEST_F(TopKUniformDistributionHistogramTest, FromColumnInt) {
   const auto hist = TopKUniformDistributionHistogram<int32_t>::from_column(*_int_float4, ColumnID{0});
 
-  ASSERT_EQ(hist->bin_count(), 2u);
-  EXPECT_EQ(hist->bin(BinID{0}), HistogramBin<int32_t>(12, 123, 2, 2));
-  EXPECT_EQ(hist->bin(BinID{1}), HistogramBin<int32_t>(12345, 123456, 5, 2));
+  // _int_float_4 has 4 distinct values
+  ASSERT_EQ(hist->bin_count(), 4u);
+  EXPECT_EQ(hist->bin(BinID{0}), HistogramBin<int32_t>(12, 12, 1, 1));
+  EXPECT_EQ(hist->bin(BinID{1}), HistogramBin<int32_t>(123, 123, 1, 1));
+  EXPECT_EQ(hist->bin(BinID{2}), HistogramBin<int32_t>(12345, 12345, 2, 1));
+  EXPECT_EQ(hist->bin(BinID{3}), HistogramBin<int32_t>(123456, 123456, 3, 1));
 }
 
+
+/* Test for floats if the number of values is below K, every value has its own bucket */
 TEST_F(TopKUniformDistributionHistogramTest, FromColumnFloat) {
   auto hist = TopKUniformDistributionHistogram<float>::from_column(*_float2, ColumnID{0});
 
-  ASSERT_EQ(hist->bin_count(), 3u);
-  EXPECT_EQ(hist->bin(BinID{0}), HistogramBin<float>(0.5f, 2.2f, 4, 4));
-  EXPECT_EQ(hist->bin(BinID{1}), HistogramBin<float>(2.5f, 3.3f, 6, 3));
-  EXPECT_EQ(hist->bin(BinID{2}), HistogramBin<float>(3.6f, 6.1f, 4, 3));
+  // _float_2 has 10 distinct values
+  ASSERT_EQ(hist->bin_count(), 10u);
+  EXPECT_EQ(hist->bin(BinID{0}), HistogramBin<float>(0.5f, 0.5f, 1, 1));
+  EXPECT_EQ(hist->bin(BinID{1}), HistogramBin<float>(1.1f, 1.1f, 1, 1));
+  EXPECT_EQ(hist->bin(BinID{2}), HistogramBin<float>(1.9f, 1.9f, 1, 1));
+  EXPECT_EQ(hist->bin(BinID{3}), HistogramBin<float>(2.2f, 2.2f, 1, 1));
+  EXPECT_EQ(hist->bin(BinID{4}), HistogramBin<float>(2.5f, 2.5f, 3, 1));
+  EXPECT_EQ(hist->bin(BinID{5}), HistogramBin<float>(3.1f, 3.1f, 1, 1));
+  EXPECT_EQ(hist->bin(BinID{6}), HistogramBin<float>(3.3f, 3.3f, 2, 1));
+  EXPECT_EQ(hist->bin(BinID{7}), HistogramBin<float>(3.6f, 3.6f, 1, 1));
+  EXPECT_EQ(hist->bin(BinID{8}), HistogramBin<float>(4.4f, 4.4f, 2, 1));
+  EXPECT_EQ(hist->bin(BinID{9}), HistogramBin<float>(6.1f, 6.1f, 1, 1));
 }
 
 }  // namespace opossum
