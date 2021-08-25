c0bac4fd20da74e5624b1a7394a95a27e4d982c0
Julian Menzler <julianmenzler@gmail.com>
2021-08-25 13:30:50 +0200

    Improve comment

--- src/lib/optimizer/strategy/expression_reduction_rule.cpp
+++ src/lib/optimizer/strategy/expression_reduction_rule.cpp
@@ -293,8 +293,8 @@ void ExpressionReductionRule::remove_duplicate_aggregate(
       // Found matching SUM and COUNT (either COUNT(a) or COUNT(*) for a non-NULL a) - add it to the replacements list.
       // Notes on casting:
       //  As stated in expression_common_type, the division of integer types will result in an integer result as well.
-      //  Since COUNT results are always of type integer, the average precision will depend on the data type of SUM,
-      //  which could be floating type or integer. To guarantee high precision, we thus must use a CastExpression.
+      //  Since COUNT results are always of type integer, the calculated average depends on the data type of SUM,
+      //  which can be floating type or integer. To guarantee correct results, we thus must use a CastExpression.
       replacements[avg_expression_ptr] = div_(cast_(sum_it->get(), DataType::Double), count_it->get());
     }
   }
