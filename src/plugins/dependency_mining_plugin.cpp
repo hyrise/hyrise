9fa834a9612d50c66abd8ef8edfb4b55dba981aa
Jan Kossmann <jan.kossmann@hpi.de>
2021-09-02 18:27:49 +0200

    Remove comments from DependencyMiningPlugin

--- src/plugins/dependency_mining_plugin.cpp
+++ src/plugins/dependency_mining_plugin.cpp
@@ -9,23 +9,15 @@ std::string DependencyMiningPlugin::description() const { return "Dependency Min
 
 DependencyMiningPlugin::DependencyMiningPlugin()
     : _queue(std::make_shared<DependencyCandidateQueue>()), _pqp_analyzer(_queue) {
-  //_queue = std::make_shared<DependencyCandidateQueue>();
-  //_pqp_analyzer(_queue);
-  //_dependency_validator(_queue);
 }
 
 void DependencyMiningPlugin::start() {
-  //_queue = std::make_shared<tbb::concurrent_priority_queue<DependencyCandidate>>();
-  //_pqp_analyzer.set_queue(_queue);
-  //_dependency_validator.set_queue(_queue);
   Timer timer;
   std::cout << "====================================================\nStarting DependencyMiningPlugin\n";
   _pqp_analyzer.run();
   if (DO_VALIDATE) {
     std::vector<std::thread> validator_threads;
-    //auto table_constraint_mutexes = tbb::concurrent_unordered_map<std::string, std::shared_ptr<std::mutex>>{};
     for (size_t validator_id{0}; validator_id < NUM_VALIDATORS; ++validator_id) {
-      //_dependency_validators.emplace_back(std::make_unique<DependencyValidator>(_queue));
       validator_threads.emplace_back(
           [&](size_t i) {
             const auto validator = std::make_unique<DependencyValidator>(_queue, i);
@@ -33,9 +25,6 @@ void DependencyMiningPlugin::start() {
           },
           validator_id);
     }
-    /*for (auto& validator : _dependency_validators) {
-      validator->start();
-    }*/
     for (auto& thread : validator_threads) thread.join();
     std::cout << "Clear Cache" << std::endl;
     Hyrise::get().default_pqp_cache->clear();
