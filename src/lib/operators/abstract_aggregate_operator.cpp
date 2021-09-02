0f41e95fe34a24e4a68955201cb2909df825087f
Julian Menzler <julianmenzler@gmail.com>
2021-09-02 15:08:38 +0200

    Add ->as_column_name()

--- src/lib/operators/abstract_aggregate_operator.cpp
+++ src/lib/operators/abstract_aggregate_operator.cpp
@@ -41,7 +41,7 @@ std::string AbstractAggregateOperator::description(DescriptionMode description_m
     }
     const size_t group_by_column_id = _groupby_column_ids[groupby_column_idx];
     if (lqp_node) {
-      desc << lqp_node->left_input()->output_expressions()[group_by_column_id];
+      desc << lqp_node->left_input()->output_expressions()[group_by_column_id]->as_column_name();
     } else {
       desc << "Column #" + std::to_string(group_by_column_id);
     }
