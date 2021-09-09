816dee4758a3ab384b38bb128b221756bf1a7ee6
mweisgut <marcel.weisgut@hpi.de>
2021-09-09 19:04:16 +0200

    bla

--- src/benchmarklib/abstract_table_generator.cpp
+++ src/benchmarklib/abstract_table_generator.cpp
@@ -36,8 +36,8 @@ void AbstractTableGenerator::generate_and_store() {
   Timer timer;
 
   // Encoding table data and generating table statistics are time consuming processes. To reduce the required execution
-  // time, we execute these data preparation steps in a multithreaded way. We store the current scheduler here in case
-  // a single-threaded scheduler is used.
+  // time, we execute these data preparation steps in a multi-threaded way. We store the current scheduler here in case
+  // a single-threaded scheduler is used. After data preparation, we switch back to the initially used scheduler.
   const auto initial_scheduler = Hyrise::get().scheduler();
   Hyrise::get().topology.use_default_topology(_benchmark_config->data_preparation_cores);
   Hyrise::get().set_scheduler(std::make_shared<NodeQueueScheduler>());
