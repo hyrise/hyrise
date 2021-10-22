8bf352ef02332572002f6bf20d8211afbf03c014
dey4ss <27929897+dey4ss@users.noreply.github.com>
2021-10-22 12:27:25 +0200

    only dgr

--- src/plugins/dependency_mining/pqp_analyzer.hpp
+++ src/plugins/dependency_mining/pqp_analyzer.hpp
@@ -19,8 +19,8 @@ class PQPAnalyzer {
 
   // switches for mining specific optimizations
   constexpr static bool ENABLE_GROUPBY_REDUCTION = true;
-  constexpr static bool ENABLE_JOIN_TO_SEMI = true;
-  constexpr static bool ENABLE_JOIN_TO_PREDICATE = true;
+  constexpr static bool ENABLE_JOIN_TO_SEMI = false;
+  constexpr static bool ENABLE_JOIN_TO_PREDICATE = false;
   constexpr static bool ENABLE_JOIN_ELIMINATION = false;
 
  private:
