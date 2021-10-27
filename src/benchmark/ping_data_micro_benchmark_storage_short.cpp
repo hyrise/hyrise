63453d47eee849dccffa52baba852542531b0c4a
Keven Richly <keven.richly@hpi.uni-potsdam.de>
2021-10-27 18:05:54 +0200

    add short benchmark for storage

--- src/benchmark/ping_data_micro_benchmark_storage_short.cpp
+++ src/benchmark/ping_data_micro_benchmark_storage_short.cpp
@@ -57,49 +57,25 @@ const auto CREATE_INDEX = true;
 
 // quantile benchmark values (int table)
 // timestamp values --> unix timestamp
-// [0.0001, 0.01, 0.02, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.4, 0.5, 0.6, 0.7, 0.75, 0.8, 0.9, 1.0]
+// [0.0001, 0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.7, 0.8, 1.0]
 ///////////////////////////////
-const auto BM_VAL_CAPTAIN_ID = std::vector{11, 507, 1021, 1909, 4593, 8817, 17926, 37285, 57997, 121885, 207423, 391081, 538892, 670817, 811067, 1157846, 1280358};
-const auto BM_VAL_CAPTAIN_STATUS = std::vector{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2};
-const auto BM_VAL_LATITUDE = std::vector{243921506, 249199220, 250004849, 250302032, 250451594, 250558091, 250683994, 250767202, 250834293, 251013562, 251229783, 251682671, 251944398, 252085778, 252179506, 252447268, 525276124};
-const auto BM_VAL_LONGITUDE = std::vector{543605120, 550208361, 551151508, 551340698, 551411140, 551464705, 551527705, 551661225, 551769067, 552056083, 552420155, 552663696, 552803335, 552904369, 553155586, 553625274, 2100403189};
-const auto BM_VAL_TIMESTAMP = std::vector{1541026998, 1541032124, 1541034919, 1541040924, 1541049411, 1541056831, 1541065715, 1541076481, 1541089685, 1541117270, 1541138605, 1541154132, 1541171747, 1541180035, 1541190288, 1541219269, 1541236836};
+const auto BM_VAL_CAPTAIN_ID = std::vector{11, 507, 1909, 4593, 17926, 57997, 121885, 207423, 538892, 811067, 1280358};
+const auto BM_VAL_CAPTAIN_STATUS = std::vector{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2};
+const auto BM_VAL_LATITUDE = std::vector{243921506, 249199220, 250302032, 250451594, 250683994, 250834293, 251013562, 251229783, 251944398, 252179506, 525276124};
+const auto BM_VAL_LONGITUDE = std::vector{543605120, 550208361, 551340698, 551411140, 551527705, 551769067, 552056083, 552420155, 552803335, 553155586, 2100403189};
+const auto BM_VAL_TIMESTAMP = std::vector{1541026998, 1541032124, 1541040924, 1541049411, 1541065715, 1541089685, 1541117270, 1541138605, 1541171747, 1541190288, 1541236836};
 
 const auto BM_SCAN_VALUES = BM_VAL_CAPTAIN_ID.size();
 
 // quantile between benchmark values (int table)
 // timestamp values --> unix timestamp
-// [0.0001, 0.01, 0.02, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.4, 0.5, 0.6, 0.7, 0.75, 0.8, 0.9, 1.0]
+// [0.0001, 0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.7, 0.8, 1.0]
 ///////////////////////////////
-const std::vector<std::vector<int>> BM_BETWEEN_VAL_CAPTAIN_ID {{207423, 208069}, {203688, 212469}, {198608, 217921}, {176038, 235600}, {159431, 259789}, {142036, 313442}, {121885, 391081}, {99640, 413556}, {80675, 444767}, {57997, 538892}, {37285, 670817}, {17926, 811067}, {8817, 931727}, {6250, 1103790}, {4593, 1157846}, {1909, 1209832}, {8, 1280358}};
-const std::vector<std::vector<int>> BM_BETWEEN_VAL_CAPTAIN_STATUS {{1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}};
-const std::vector<std::vector<int>> BM_BETWEEN_VAL_LATITUDE {{251229635, 251229817}, {251205580, 251244877}, {251186382, 251258863}, {251156067, 251327418}, {251118305, 251435758}, {251076483, 251562689}, {251013562, 251682671}, {250957584, 251801594}, {250915520, 251867848}, {250834293, 251944398}, {250767202, 252085778}, {250683994, 252179506}, {250558091, 252303117}, {250495668, 252364041}, {250451594, 252447268}, {250302032, 252608756}, {-31439997, 525276124}};
-const std::vector<std::vector<int>> BM_BETWEEN_VAL_LONGITUDE {{552419834, 552420525}, {552401293, 552446079}, {552375420, 552467950}, {552297534, 552509809}, {552205344, 552567678}, {552121165, 552621719}, {552056083, 552663696}, {551998586, 552712421}, {551937507, 552746462}, {551769067, 552803335}, {551661225, 552904369}, {551527705, 553155586}, {551464705, 553433091}, {551435500, 553528324}, {551411140, 553625274}, {551340698, 553989117}, {-1347527969, 2100403189}};
-const std::vector<std::vector<int>> BM_BETWEEN_VAL_TIMESTAMP {{1541138598, 1541138612}, {1541137889, 1541139310}, {1541137112, 1541139993}, {1541134602, 1541141812}, {1541130169, 1541146881}, {1541125209, 1541150508}, {1541117270, 1541154132}, {1541106624, 1541158501}, {1541100595, 1541162844}, {1541089685, 1541171747}, {1541076481, 1541180035}, {1541065715, 1541190288}, {1541056831, 1541209600}, {1541053133, 1541215064}, {1541049411, 1541219269}, {1541040924, 1541227207}, {1541026811, 1541236836}};
-
-// 400 mio pings table
-
-// quantile benchmark values (int table)
-// timestamp values --> unix timestamp
-// [0.0001, 0.01, 0.02, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.4, 0.5, 0.6, 0.7, 0.75, 0.8, 0.9, 1.0]
-///////////////////////////////
-//const auto BM_VAL_CAPTAIN_ID = std::vector{4, 511, 1051, 2156, 5075, 11309, 26152, 51264, 71463, 153884, 261690, 444765, 681250, 830979, 951600, 1209929, 1419878};
-//const auto BM_VAL_CAPTAIN_STATUS = std::vector{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2};
-//const auto BM_VAL_LATITUDE = std::vector{243318475, 249128864, 249867308, 250278868, 250441577, 250552193, 250681596, 250763657, 250831026, 251008836, 251205836, 251695319, 251959544, 252079334, 252166598, 252442895, 601671321};
-//const auto BM_VAL_LONGITUDE = std::vector{543464250, 550118701, 551136315, 551335287, 551413500, 551467322, 551533330, 551663587, 551771444, 552061945, 552444439, 552686763, 552805030, 552905709, 553158162, 553615144, 2137369825};
-//const auto BM_VAL_TIMESTAMP = std::vector{1541029693, 1541117270, 1541190288, 1541473344, 1541871716, 1542301289, 1542701374, 1543132978, 1543613596, 1544417407, 1545152896, 1545879041, 1546772391, 1547148186, 1547525107, 1548196911, 1548975682};
-
-//const auto BM_SCAN_VALUES = BM_VAL_CAPTAIN_ID.size();
-
-// quantile between benchmark values (int table)
-// timestamp values --> unix timestamp
-// [0.0001, 0.01, 0.02, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.4, 0.5, 0.6, 0.7, 0.75, 0.8, 0.9, 1.0]
-///////////////////////////////
-//const std::vector<std::vector<int>> BM_BETWEEN_VAL_CAPTAIN_ID {{261690, 261690}, {257105, 265387}, {249105, 274829}, {232076, 313448}, {206962, 391070}, {172746, 413552}, {153884, 444765}, {135981, 491021}, {111718, 538905}, {71463, 681250}, {51264, 830979}, {26152, 951600}, {11309, 1157854}, {7739, 1188073}, {5075, 1209929}, {2156, 1267757}, {4, 1419878}};
-//const std::vector<std::vector<int>> BM_BETWEEN_VAL_CAPTAIN_STATUS {{1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 2}, {1, 2}, {1, 2}, {1, 2}, {1, 2}};
-//const std::vector<std::vector<int>> BM_BETWEEN_VAL_LATITUDE {{251205594, 251206088}, {251186996, 251229239}, {251170200, 251244361}, {251150524, 251307385}, {251111686, 251424324}, {251063392, 251567452}, {251008836, 251695319}, {250959444, 251817667}, {250916446, 251875782}, {250831026, 251959544}, {250763657, 252079334}, {250681596, 252166598}, {250552193, 252294631}, {250485765, 252349413}, {250441577, 252442895}, {250278868, 252584668}, {-348142571, 601671321}};
-//const std::vector<std::vector<int>> BM_BETWEEN_VAL_LONGITUDE {{552444244, 552444647}, {552421152, 552466309}, {552403773, 552484039}, {552334887, 552512683}, {552219192, 552585334}, {552133587, 552638424}, {552061945, 552686763}, {552006222, 552727166}, {551944417, 552758956}, {551771444, 552805030}, {551663587, 552905709}, {551533330, 553158162}, {551467322, 553438739}, {551440083, 553526687}, {551413500, 553615144}, {551335287, 553972207}, {-2144769047, 2137369825}};
-//const std::vector<std::vector<int>> BM_BETWEEN_VAL_TIMESTAMP {{1545152588, 1545153213}, {1545125220, 1545195923}, {1545101636, 1545220279}, {1544986222, 1545315256}, {1544806072, 1545537953}, {1544611869, 1545697161}, {1544417407, 1545879041}, {1544199073, 1546078740}, {1544001704, 1546335968}, {1543613596, 1546772391}, {1543132978, 1547148186}, {1542701374, 1547525107}, {1542301289, 1547873717}, {1542094512, 1548046293}, {1541871716, 1548196911}, {1541473344, 1548555299}, {1541026811, 1548975682}};
+const std::vector<std::vector<int>> BM_BETWEEN_VAL_CAPTAIN_ID {{207423, 208069}, {203688, 212469}, {176038, 235600}, {159431, 259789}, {121885, 391081}, {80675, 444767}, {57997, 538892}, {37285, 670817}, {8817, 931727}, {4593, 1157846}, {8, 1280358}};
+const std::vector<std::vector<int>> BM_BETWEEN_VAL_CAPTAIN_STATUS {{1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 1}, {1, 2}, {1, 2}, {1, 2}, {1, 2}};
+const std::vector<std::vector<int>> BM_BETWEEN_VAL_LATITUDE {{251229635, 251229817}, {251205580, 251244877}, {251156067, 251327418}, {251118305, 251435758}, {251013562, 251682671}, {250915520, 251867848}, {250834293, 251944398}, {250767202, 252085778}, {250558091, 252303117}, {250451594, 252447268}, {-31439997, 525276124}};
+const std::vector<std::vector<int>> BM_BETWEEN_VAL_LONGITUDE {{552419834, 552420525}, {552401293, 552446079}, {552297534, 552509809}, {552205344, 552567678}, {552056083, 552663696}, {551937507, 552746462}, {551769067, 552803335}, {551661225, 552904369}, {551464705, 553433091}, {551411140, 553625274}, {-1347527969, 2100403189}};
+const std::vector<std::vector<int>> BM_BETWEEN_VAL_TIMESTAMP {{1541138598, 1541138612}, {1541137889, 1541139310}, {1541134602, 1541141812}, {1541130169, 1541146881}, {1541117270, 1541154132}, {1541100595, 1541162844}, {1541089685, 1541171747}, {1541076481, 1541180035}, {1541056831, 1541209600}, {1541049411, 1541219269}, {1541026811, 1541236836}};
 
 ///////////////////////////////
 // methods
@@ -179,7 +155,7 @@ namespace opossum {
 class TableWrapper;
 
 // Defining the base fixture class
-class PingDataStorageMicroBenchmarkFixture : public MicroBenchmarkBasicFixture {
+class PingStorageMicroBenchmarkFixture : public MicroBenchmarkBasicFixture {
  public:
   void SetUp(::benchmark::State& state) override {
     auto& storage_manager = Hyrise::get().storage_manager;
@@ -291,7 +267,7 @@ class PingDataStorageMicroBenchmarkFixture : public MicroBenchmarkBasicFixture {
 // benchmarks
 ///////////////////////////////
 
-BENCHMARK_DEFINE_F(PingDataStorageMicroBenchmarkFixture, BM_STORAGE_Keven_OrderingLessThanEqualsPerformance)(benchmark::State& state) {
+BENCHMARK_DEFINE_F(PingStorageMicroBenchmarkFixture, BM_STORAGE_Kevin_OrderingLessThanEqualsPerformance)(benchmark::State& state) {
   Assert(BM_VAL_CAPTAIN_ID.size() == BM_VAL_CAPTAIN_STATUS.size(), "Sample search values for columns should have the same length.");
   Assert(BM_VAL_CAPTAIN_ID.size() == BM_VAL_LATITUDE.size(), "Sample search values for columns should have the same length.");
   Assert(BM_VAL_CAPTAIN_ID.size() == BM_VAL_TIMESTAMP.size(), "Sample search values for columns should have the same length.");
@@ -378,7 +354,7 @@ BENCHMARK_DEFINE_F(PingDataStorageMicroBenchmarkFixture, BM_STORAGE_Keven_Orderi
   }
 }
 
-BENCHMARK_DEFINE_F(PingDataStorageMicroBenchmarkFixture, BM_STORAGE_Keven_IndexScan)(benchmark::State& state) {
+BENCHMARK_DEFINE_F(PingStorageMicroBenchmarkFixture, BM_STORAGE_Kevin_IndexScan)(benchmark::State& state) {
   Assert(BM_VAL_CAPTAIN_ID.size() == BM_VAL_CAPTAIN_STATUS.size(), "Sample search values for columns should have the same length. 1");
   Assert(BM_VAL_CAPTAIN_ID.size() == BM_VAL_LATITUDE.size(), "Sample search values for columns should have the same length. 2");
   Assert(BM_VAL_CAPTAIN_ID.size() == BM_VAL_TIMESTAMP.size(), "Sample search values for columns should have the same length. 3");
@@ -425,7 +401,7 @@ BENCHMARK_DEFINE_F(PingDataStorageMicroBenchmarkFixture, BM_STORAGE_Keven_IndexS
   }
 }
 
-BENCHMARK_DEFINE_F(PingDataStorageMicroBenchmarkFixture, BM_STORAGE_Keven_BetweenPerformance)(benchmark::State& state) {
+BENCHMARK_DEFINE_F(PingStorageMicroBenchmarkFixture, BM_STORAGE_Kevin_BetweenPerformance)(benchmark::State& state) {
   auto& storage_manager = Hyrise::get().storage_manager;
 
   const auto order_by_column = ORDER_COLUMNS[state.range(0)];
@@ -463,7 +439,7 @@ BENCHMARK_DEFINE_F(PingDataStorageMicroBenchmarkFixture, BM_STORAGE_Keven_Betwee
   }
 }
 
-BENCHMARK_DEFINE_F(PingDataStorageMicroBenchmarkFixture, BM_STORAGE_Keven_BetweenIndexScan)(benchmark::State& state) {
+BENCHMARK_DEFINE_F(PingStorageMicroBenchmarkFixture, BM_STORAGE_Kevin_BetweenIndexScan)(benchmark::State& state) {
   auto& storage_manager = Hyrise::get().storage_manager;
 
   const auto order_by_column = ORDER_COLUMNS[state.range(0)];
@@ -535,9 +511,9 @@ static void CustomArguments(benchmark::internal::Benchmark* b) {
     }
   }
 }
-BENCHMARK_REGISTER_F(PingDataStorageMicroBenchmarkFixture, BM_STORAGE_Keven_OrderingLessThanEqualsPerformance)->Apply(CustomArguments);
-BENCHMARK_REGISTER_F(PingDataStorageMicroBenchmarkFixture, BM_STORAGE_Keven_IndexScan)->Apply(CustomArguments);
-BENCHMARK_REGISTER_F(PingDataStorageMicroBenchmarkFixture, BM_STORAGE_Keven_BetweenPerformance)->Apply(CustomArguments);
-BENCHMARK_REGISTER_F(PingDataStorageMicroBenchmarkFixture, BM_STORAGE_Keven_BetweenIndexScan)->Apply(CustomArguments);
+BENCHMARK_REGISTER_F(PingStorageMicroBenchmarkFixture, BM_STORAGE_Kevin_OrderingLessThanEqualsPerformance)->Apply(CustomArguments);
+BENCHMARK_REGISTER_F(PingStorageMicroBenchmarkFixture, BM_STORAGE_Kevin_IndexScan)->Apply(CustomArguments);
+BENCHMARK_REGISTER_F(PingStorageMicroBenchmarkFixture, BM_STORAGE_Kevin_BetweenPerformance)->Apply(CustomArguments);
+BENCHMARK_REGISTER_F(PingStorageMicroBenchmarkFixture, BM_STORAGE_Kevin_BetweenIndexScan)->Apply(CustomArguments);
 
 }  // namespace opossum
