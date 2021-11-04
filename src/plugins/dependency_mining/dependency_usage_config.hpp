e10ff4bbf9407414859da21678103c4907ff520b
dey4ss <27929897+dey4ss@users.noreply.github.com>
2021-11-04 13:22:57 +0100

    only join elimination

--- src/plugins/dependency_mining/dependency_usage_config.hpp
+++ src/plugins/dependency_mining/dependency_usage_config.hpp
@@ -6,8 +6,8 @@ class DependencyUsageConfig {
  public:
   constexpr static bool ENABLE_GROUPBY_REDUCTION = false;
   constexpr static bool ENABLE_JOIN_TO_SEMI = false;
-  constexpr static bool ENABLE_JOIN_TO_PREDICATE = true;
-  constexpr static bool ENABLE_JOIN_ELIMINATION = false;
+  constexpr static bool ENABLE_JOIN_TO_PREDICATE = false;
+  constexpr static bool ENABLE_JOIN_ELIMINATION = true;
 
   constexpr static bool ALLOW_PRESET_CONSTRAINTS = false;
 };
