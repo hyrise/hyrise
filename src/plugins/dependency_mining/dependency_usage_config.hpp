afff5a7d2dd89618c4556ed09cf1a72244dc6483
dey4ss <27929897+dey4ss@users.noreply.github.com>
2021-11-04 13:22:11 +0100

    only dgr

--- src/plugins/dependency_mining/dependency_usage_config.hpp
+++ src/plugins/dependency_mining/dependency_usage_config.hpp
@@ -4,7 +4,7 @@ namespace opossum {
 
 class DependencyUsageConfig {
  public:
-  constexpr static bool ENABLE_GROUPBY_REDUCTION = false;
+  constexpr static bool ENABLE_GROUPBY_REDUCTION = true;
   constexpr static bool ENABLE_JOIN_TO_SEMI = false;
   constexpr static bool ENABLE_JOIN_TO_PREDICATE = false;
   constexpr static bool ENABLE_JOIN_ELIMINATION = false;
