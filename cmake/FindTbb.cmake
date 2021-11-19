726493fd9e78907ca70edae6cfb93e91be00f88f
mweisgut <marcel.weisgut@hpi.de>
2021-11-19 16:46:34 +0100

    progress

--- cmake/FindTbb.cmake
+++ cmake/FindTbb.cmake
@@ -3,16 +3,21 @@
 #  TBB_INCLUDE_DIR : e.g., /usr/include/.
 #  TBB_LIBRARY     : Library path of Intel Threading Building Blocks library
 #  TBB_FOUND       : True if found.
-FIND_PATH(TBB_INCLUDE_DIR NAME tbb/tbb.h
-    HINTS $ENV{HOME}/local/include /opt/local/include /usr/local/include /usr/include)
 
-FIND_LIBRARY(TBB_LIBRARY NAME tbb
-    HINTS $ENV{HOME}/local/lib64 $ENV{HOME}/local/lib /usr/local/lib64 /usr/local/lib /opt/local/lib64 /opt/local/lib /usr/lib64 /usr/lib)
+find_path(TBB_INCLUDE_DIR NAME tbb/tbb.h
+    PATHS $ENV{HOME}/local/include /opt/local/include /usr/local/include /usr/include)
 
-IF (TBB_INCLUDE_DIR AND TBB_LIBRARY)
-    SET(TBB_FOUND TRUE)
-    MESSAGE(STATUS "Found tbb library: inc=${TBB_INCLUDE_DIR}, lib=${TBB_LIBRARY}")
-ELSE ()
-    SET(TBB_FOUND FALSE)
-    MESSAGE(STATUS "WARNING: tbb library not found.")
-ENDIF ()
+find_library(TBB_LIBRARY NAME tbb
+    PATHS $ENV{HOME}/local/lib64 $ENV{HOME}/local/lib /usr/local/lib64 /usr/local/lib /opt/local/lib64 /opt/local/lib /usr/lib64 /usr/lib)
+
+if (TBB_INCLUDE_DIR AND TBB_LIBRARY)
+    set(TBB_FOUND TRUE)
+    message(STATUS "Found tbb library: inc=${TBB_INCLUDE_DIR}, lib=${TBB_LIBRARY}")
+else ()
+    set(TBB_FOUND FALSE)
+    message(STATUS "WARNING: tbb library not found.")
+endif ()
+
+add_library(tbb INTERFACE)
+target_include_directories(tbb INTERFACE ${TBB_INCLUDE_DIR})
+target_link_libraries(tbb INTERFACE ${TBB_LIBRARY})
