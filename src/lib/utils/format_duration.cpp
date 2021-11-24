12465e51347f3c1d7b55a7f21e91b5246f71dfe9
dey4ss <27929897+dey4ss@users.noreply.github.com>
2021-11-24 16:13:48 +0100

    variable name

--- src/lib/utils/format_duration.cpp
+++ src/lib/utils/format_duration.cpp
@@ -18,20 +18,20 @@ std::string format_duration(const std::chrono::nanoseconds& total_nanoseconds) {
   std::chrono::nanoseconds remaining_nanoseconds = total_nanoseconds;
   std::vector<uint64_t> floor_durations;
   std::vector<uint64_t> round_durations;
-  boost::hana::for_each(TIME_UNIT_ORDER, [&](const auto join_operator_t) {
-    using DurationType = typename decltype(join_operator_t)::type;
-    auto floor_value = std::chrono::floor<DurationType>(total_nanoseconds);
-    floor_durations.emplace_back(floor_value.count());
+  boost::hana::for_each(TIME_UNIT_ORDER, [&](const auto duration_t) {
+    using DurationType = typename decltype(duration_t)::type;
+    auto floored_duration = std::chrono::floor<DurationType>(total_nanoseconds);
+    floor_durations.emplace_back(floored_duration.count());
     round_durations.emplace_back(std::chrono::round<DurationType>(remaining_nanoseconds).count());
-    remaining_nanoseconds -= floor_value;
+    remaining_nanoseconds -= floored_duration;
   });
 
   std::stringstream stream;
   for (size_t unit_iterator{0}; unit_iterator < unit_strings.size(); ++unit_iterator) {
-    const auto& floor_value = floor_durations.at(unit_iterator);
+    const auto& floored_duration = floor_durations.at(unit_iterator);
     const auto is_last_element = unit_iterator == unit_strings.size() - 1;
-    if (floor_value > 0 || is_last_element) {
-      stream << floor_value << unit_strings.at(unit_iterator);
+    if (floored_duration > 0 || is_last_element) {
+      stream << floored_duration << unit_strings.at(unit_iterator);
       if (!is_last_element) {
         stream << " " << round_durations.at(unit_iterator + 1) << unit_strings.at(unit_iterator + 1);
       }
