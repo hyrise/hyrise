c21f4a7d544250da094e363ae93b205e1e6aa552
mweisgut <marcel.weisgut@hpi.de>
2021-12-02 08:46:44 +0100

    sadf

--- cmake/FindSqlite3.cmake
+++ cmake/FindSqlite3.cmake
@@ -4,26 +4,26 @@
 #  SQLITE3_LIBRARY     : Library path of sqlite3 library
 #  SQLITE3_FOUND       : True if found.
 
-FIND_PATH(SQLITE3_INCLUDE_DIR NAME sqlite3.h HINTS
+add_library(sqlite3 INTERFACE)
+
+find_path(SQLITE3_INCLUDE_DIR NAME sqlite3.h HINTS
     "$ENV{LIB_DIR}/include"
     "$ENV{LIB_DIR}/include/sqlite"
     "$ENV{INCLUDE}"
 )
 
-FIND_LIBRARY(SQLITE3_LIBRARY NAMES sqlite3_i sqlite3 PATHS
+find_library(SQLITE3_LIBRARY NAMES sqlite3_i sqlite3 PATHS
   "$ENV{LIB_DIR}/lib"
   "$ENV{LIB}/lib"
   )
 
-IF (SQLITE3_INCLUDE_DIR AND SQLITE3_LIBRARY)
-    SET(SQLITE3_FOUND TRUE)
-    MESSAGE(STATUS "Found sqlite3 library: inc=${SQLITE3_INCLUDE_DIR}, lib=${SQLITE3_LIBRARY}")
-ELSE ()
-    SET(SQLITE3_FOUND FALSE)
-    MESSAGE(STATUS "WARNING: sqlite3 library not found.")
-    MESSAGE(STATUS "Try: 'sudo yum install libsqlite3-dev' (or sudo apt-get install libsqlite3-dev)")
-ENDIF ()
-
-add_library(sqlite3 INTERFACE)
-target_include_directories(sqlite3 INTERFACE ${SQLITE3_INCLUDE_DIR})
-target_link_libraries(sqlite3 INTERFACE ${SQLITE3_LIBRARY})
+if (SQLITE3_INCLUDE_DIR AND SQLITE3_LIBRARY)
+    set(SQLITE3_FOUND TRUE)
+    target_include_directories(sqlite3 INTERFACE ${SQLITE3_INCLUDE_DIR})
+    target_link_libraries(sqlite3 INTERFACE ${SQLITE3_LIBRARY})
+    message(STATUS "Found sqlite3 library: inc=${SQLITE3_INCLUDE_DIR}, lib=${SQLITE3_LIBRARY}")
+else ()
+    set(SQLITE3_FOUND FALSE)
+    message(STATUS "WARNING: sqlite3 library not found.")
+    message(STATUS "Try: 'sudo yum install libsqlite3-dev' (or sudo apt-get install libsqlite3-dev)")
+endif ()
