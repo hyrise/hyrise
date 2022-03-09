92e2c31d39fed42c59f43d75c7aca5ff81a224e2
Bouncner <martin.boissier@hpi.de>
2022-03-09 09:31:08 +0000

    std::streamsize

--- src/lib/import_export/binary/binary_parser.cpp
+++ src/lib/import_export/binary/binary_parser.cpp
@@ -37,7 +37,7 @@ template <typename T>
 pmr_compact_vector BinaryParser::_read_values_compact_vector(std::ifstream& file, const size_t count) {
   const auto bit_width = _read_value<uint8_t>(file);
   auto values = pmr_compact_vector(bit_width, count);
-  file.read(reinterpret_cast<char*>(values.get()), static_cast<long>(values.bytes()));
+  file.read(reinterpret_cast<char*>(values.get()), static_cast<std::streamsize>(values.bytes()));
   return values;
 }
 
