a914397f0d47893064ae216c396de01b7763edd6
Julian Menzler <julianmenzler@gmail.com>
2022-03-16 11:54:56 +0100

    Revert change in SemiJoinReductionRule

--- src/lib/optimizer/strategy/semi_join_reduction_rule.cpp
+++ src/lib/optimizer/strategy/semi_join_reduction_rule.cpp
@@ -138,11 +138,12 @@ void SemiJoinReductionRule::_apply_to_plan_without_subqueries(const std::shared_
       }
 
       // On the left side we must not create semi join reductions for anti joins as those rely on the very existence of
-      // non-matching values on the right side.
-      // We also reduce semi joins, if beneficial.
+      // non-matching values on the right side. Also, we should not create semi join reductions for semi joins as those
+      // would simply duplicate the original join.
       if (join_node->join_mode != JoinMode::Left && join_node->join_mode != JoinMode::FullOuter &&
-          join_node->join_mode != JoinMode::AntiNullAsTrue && join_node->join_mode != JoinMode::AntiNullAsFalse) {
-        reduce_if_beneficial(LQPInputSide::Left);
+          join_node->join_mode != JoinMode::AntiNullAsTrue && join_node->join_mode != JoinMode::AntiNullAsFalse &&
+          join_node->join_mode != JoinMode::Semi) {
+       reduce_if_beneficial(LQPInputSide::Left);
       }
     }
 
