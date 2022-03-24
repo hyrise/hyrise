44b0d44f7c72b46cb8f2f57e18707a4db191dfcc
Julian Menzler <julianmenzler@gmail.com>
2022-03-24 11:26:17 +0100

    ...

--- src/lib/logical_query_plan/lqp_utils.cpp
+++ src/lib/logical_query_plan/lqp_utils.cpp
@@ -506,11 +506,12 @@ void remove_invalid_fds(const std::shared_ptr<const AbstractLQPNode>& lqp, std::
    */
 }
 
-std::shared_ptr<AbstractLQPNode> find_diamond_bottom_node(const std::shared_ptr<AbstractLQPNode>& diamond_root_node) {
-  Assert(diamond_root_node->input_count() > 1, "Diamond root node does not have two inputs.");
+std::shared_ptr<AbstractLQPNode> find_diamond_bottom_node(const std::shared_ptr<AbstractLQPNode>& union_node) {
+  Assert(union_node->type == LQPNodeType::Union, "Expecting UnionNode as the diamond's root node.");
+  Assert(union_node->input_count() > 1, "Diamond root node does not have two inputs.");
   bool is_diamond = true;
   std::optional<std::shared_ptr<AbstractLQPNode>> diamond_bottom_node;
-  visit_lqp(diamond_root_node, [&](const auto& diamond_node) {
+  visit_lqp(union_node, [&](const auto& diamond_node) {
         if (!is_diamond) return LQPVisitation::DoNotVisitInputs;
         if (diamond_node->output_count() > 1) {
           if (!diamond_bottom_node.has_value()) {
@@ -525,8 +526,7 @@ std::shared_ptr<AbstractLQPNode> find_diamond_bottom_node(const std::shared_ptr<
         return LQPVisitation::VisitInputs;
   });
 
-  if (!is_diamond) return nullptr;
-  Assert(diamond_bottom_node.has_value(), "Expected to find diamond bottom node.");
+  if (!is_diamond || !diamond_bottom_node.has_value()) return nullptr;
   return diamond_bottom_node.value();
 }
 
