26cb2f4b31801393046ec64aba2c045d9ab9d03b
mweisgut <marcel.weisgut@hpi.de>
2022-04-14 11:33:31 +0200

    bla

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -70,6 +70,7 @@ if echo $REPLY | grep -E '^[Yy]$' > /dev/null; then
                 # Packages added here should also be added to the Dockerfile
                 sudo apt-get install --no-install-recommends -y autoconf bash-completion bc clang-9 clang-format-9 clang-tidy-9 cmake curl dos2unix g++-9 gcc-9 gcovr git graphviz libhwloc-dev libncurses5-dev libnuma-dev libnuma1 libpq-dev libreadline-dev libsqlite3-dev libtbb-dev lld man parallel postgresql-server-dev-all python3 python3-pip systemtap systemtap-sdt-dev valgrind &
 
+                pwd
                 if ! git submodule update --jobs 5 --init --recursive; then
                     echo "Error during git fetching submodules."
                     exit 1
