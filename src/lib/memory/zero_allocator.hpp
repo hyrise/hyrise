f66ad87c513a35004400e4f2d1a687e89da1a845
mweisgut <marcel.weisgut@hpi.de>
2022-04-25 17:59:42 +0200

    copy constr

--- src/lib/memory/zero_allocator.hpp
+++ src/lib/memory/zero_allocator.hpp
@@ -9,6 +9,9 @@ class ZeroAllocator : public tbb::cache_aligned_allocator<ValueType> {
  public:
   ZeroAllocator() = default;
 
+  template<typename U>
+  explicit ZeroAllocator(const ZeroAllocator<U>&) noexcept {};
+
   ValueType* allocate(std::size_t n) {
     ValueType* ptr = tbb::cache_aligned_allocator<ValueType>::allocate(n);
     std::memset(static_cast<void*>(ptr), 0, n * sizeof(ValueType));
