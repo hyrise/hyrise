fd5e2e0828045e1c3fb95090ce1f647c85556250
martin.boissier <martin.boissier@hpi.de>
2022-05-04 21:14:03 +0200

    Nolint for boost memory resource

--- src/lib/memory/boost_default_memory_resource.cpp
+++ src/lib/memory/boost_default_memory_resource.cpp
@@ -7,14 +7,16 @@
 
 namespace boost::container::pmr {
 
+// NOLINTBEGIN(cppcoreguidelines-owning-memory,cppcoreguidelines-no-malloc,hicpp-no-malloc)
+// Usually, we want to avoid manual memory management. But in this cases it's by intention.
 class default_resource_impl : public memory_resource {  // NOLINT
  public:
   void* do_allocate(std::size_t bytes, std::size_t alignment) override {
     return std::malloc(bytes);
   }  // NOLINT
 
-  void do_deallocate(void* p, std::size_t bytes, std::size_t alignment) override {
-    std::free(p);
+  void do_deallocate(void* ptr, std::size_t bytes, std::size_t alignment) override {
+    std::free(ptr);
   }  // NOLINT
 
   [[nodiscard]] bool do_is_equal(const memory_resource& other) const BOOST_NOEXCEPT override {
@@ -29,6 +31,7 @@ memory_resource* get_default_resource() BOOST_NOEXCEPT {
   static auto* default_resource_instance = new default_resource_impl();  // NOLINT
   return default_resource_instance;
 }
+// NOLINTEND(cppcoreguidelines-owning-memory,cppcoreguidelines-no-malloc,hicpp-no-malloc)
 
 memory_resource* new_delete_resource() BOOST_NOEXCEPT {
   return get_default_resource();
