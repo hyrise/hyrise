29167e43c01ece6e762f371cf0f35dc6fb662051
Julian Menzler <julianmenzler@gmail.com>
2022-05-07 20:12:53 +0200

    Refactor EnableMakeForLQPNode (#2456)
    Improve code readability & Doc.

--- src/lib/logical_query_plan/enable_make_for_lqp_node.hpp
+++ src/lib/logical_query_plan/enable_make_for_lqp_node.hpp
@@ -10,68 +10,75 @@ namespace opossum {
 class AbstractLQPNode;
 
 /**
- * LQP node types should derive from this in order to enable the <NodeType>::make() function that allows for a clean
- * notation when building LQPs via code by allowing to pass in a nodes input(ren) as the last argument(s).
+ * With this class, LQP nodes inherit a static <DerivedNode>::make construction function that allows for convenience and
+ * a clean notation in tests. The ::make function creates an LQP node by passing the first few function arguments to the
+ * appropriate LQP node constructor. Furthermore, the left and right inputs of the generated LQP node are set from the
+ * last (two) function argument(s), if provided.
  *
- * const auto input_lqp =
- * PredicateNode::make(_mock_node_a, PredicateCondition::Equals, 42,
- *   PredicateNode::make(_mock_node_b, PredicateCondition::GreaterThan, 50,
- *     PredicateNode::make(_mock_node_b, PredicateCondition::GreaterThan, 40,
- *       ProjectionNode::make_pass_through(
- *         PredicateNode::make(_mock_node_a, PredicateCondition::GreaterThanEquals, 90,
- *           PredicateNode::make(_mock_node_c, PredicateCondition::LessThan, 500,
- *             _mock_node))))));
+ * The usage of ::make is intended as follows:
+ *  auto lqp =
+ *  JoinNode::make(JoinMode::Semi, equals_(b_y, literal),
+ *    JoinNode::make(JoinMode::Inner, equals_(a_a, b_x),
+ *      UnionNode::Make(SetOperationMode::All,
+ *        PredicateNode::make(greater_than_(a_a, 700),
+ *          node_a),
+ *        PredicateNode::make(less_than_(a_b, 123),
+ *          node_a)),
+ *      node_b),
+ *    ProjectionNode::make(expression_vector(literal),
+ *      dummy_table_node));
  */
 template <typename DerivedNode>
 class EnableMakeForLQPNode {
  public:
-  template <int N, typename... Ts>
-  using NthTypeOf = std::tuple_element_t<N, std::tuple<Ts...>>;
+  // The following declaration is a variadic function template taking any number of arguments of arbitrary types.
+  template <typename... ArgumentTypes>
+  static std::shared_ptr<DerivedNode> make(ArgumentTypes&&... arguments) {
+    if constexpr (sizeof...(ArgumentTypes) > 0) {
+      auto arguments_tuple = std::forward_as_tuple(arguments...);
 
-  template <typename... Args>
-  static std::shared_ptr<DerivedNode> make(Args&&... args) {
-    // clang-format off
-
-    // - using nesting instead of && because both sides of the && would need to be valid
-    // - redundant else paths instead of one fallthrough at the end, because it too, needs to be valid.
-    if constexpr (sizeof...(Args) > 0) {
-      if constexpr (std::is_convertible_v<NthTypeOf<sizeof...(Args)-1, Args...>, std::shared_ptr<AbstractLQPNode>>) {
-        auto args_tuple = std::forward_as_tuple(args...);
-        if constexpr (sizeof...(Args) > 1) {
-          if constexpr (std::is_convertible_v<NthTypeOf<sizeof...(Args)-2, Args...>, std::shared_ptr<AbstractLQPNode>>) {  // NOLINT - too long, but better than breaking
-            // last two arguments are shared_ptr<AbstractLQPNode>
-            auto node = make_impl(args_tuple, std::make_index_sequence<sizeof...(Args) - 2>());
-            node->set_left_input(std::get<sizeof...(Args) - 2>(args_tuple));
-            node->set_right_input(std::get<sizeof...(Args) - 1>(args_tuple));
+      // Check if the last function argument represents an LQP node (that can be set as an input).
+      if constexpr (IsLQPNodeArgument<sizeof...(ArgumentTypes) - 1, ArgumentTypes...>::value) {
+        if constexpr (sizeof...(ArgumentTypes) > 1) {
+          // Check if the second to last function argument represents an LQP node as well.
+          if constexpr (IsLQPNodeArgument<sizeof...(ArgumentTypes) - 2, ArgumentTypes...>::value) {
+            // Use function arguments, except for the last two, to construct the LQP node.
+            auto node = create_lqp_node(arguments_tuple, std::make_index_sequence<sizeof...(ArgumentTypes) - 2>());
+            node->set_left_input(std::get<sizeof...(ArgumentTypes) - 2>(arguments_tuple));
+            node->set_right_input(std::get<sizeof...(ArgumentTypes) - 1>(arguments_tuple));
             return node;
           } else {
-            // last argument is shared_ptr<AbstractLQPNode>
-            auto node = make_impl(args_tuple, std::make_index_sequence<sizeof...(Args)-1>());
-            node->set_left_input(std::get<sizeof...(Args)-1>(args_tuple));
+            // Use function arguments, except for the last, to construct the LQP node.
+            auto node = create_lqp_node(arguments_tuple, std::make_index_sequence<sizeof...(ArgumentTypes) - 1>());
+            node->set_left_input(std::get<sizeof...(ArgumentTypes) - 1>(arguments_tuple));
             return node;
           }
         } else {
-          // last argument is shared_ptr<AbstractLQPNode>
-          auto node = make_impl(args_tuple, std::make_index_sequence<sizeof...(Args)-1>());
-          node->set_left_input(std::get<sizeof...(Args)-1>(args_tuple));
+          // Only one LQP input node was provided as an argument.
+          auto node = std::make_shared<DerivedNode>();
+          node->set_left_input(std::get<0>(arguments_tuple));
           return node;
         }
       } else {
-        // no shared_ptr<AbstractLQPNode> was passed at the end
-        return make_impl(std::forward_as_tuple(args...), std::make_index_sequence<sizeof...(Args)-0>());
+        // Additional LQP input nodes were not passed as last arguments.
+        return create_lqp_node(arguments_tuple, std::make_index_sequence<sizeof...(ArgumentTypes)>());
       }
     } else {
-      // no shared_ptr<AbstractLQPNode> was passed at the end
-      return make_impl(std::forward_as_tuple(args...), std::make_index_sequence<sizeof...(Args)-0>());
+      // No arguments were passed to this function.
+      return std::make_shared<DerivedNode>();
     }
-    // clang-format on
   }
 
  private:
-  template <class Tuple, size_t... I>
-  static std::shared_ptr<DerivedNode> make_impl(const Tuple& constructor_arguments,
-                                                std::index_sequence<I...> num_constructor_args) {
-    return std::make_shared<DerivedNode>(std::get<I>(constructor_arguments)...);
+  template <class ArgumentsTupleType, size_t... ConstructorIndices>
+  static std::shared_ptr<DerivedNode> create_lqp_node(const ArgumentsTupleType& arguments_tuple,
+                                                      std::index_sequence<ConstructorIndices...> /* indices */) {
+    return std::make_shared<DerivedNode>(std::get<ConstructorIndices>(arguments_tuple)...);
   }
+
+  template <size_t ArgumentIndex, typename... ArgumentTypes>
+  using IsLQPNodeArgument = std::is_convertible<std::tuple_element_t<ArgumentIndex, std::tuple<ArgumentTypes...>>,
+                                                std::shared_ptr<AbstractLQPNode>>;
 };
+
 }  // namespace opossum
