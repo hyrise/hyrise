7436d4423329b21d4cd2d0e8ae3f49d93a49b4ec
martin.boissier <martin.boissier@hpi.de>
2022-06-10 13:47:05 +0200

    try fix

--- src/lib/memory/boost_default_memory_resource.cpp
+++ src/lib/memory/boost_default_memory_resource.cpp
@@ -10,11 +10,11 @@ namespace boost::container::pmr {
 class default_resource_impl : public memory_resource {  // NOLINT(readability-identifier-naming)
  public:
   void* do_allocate(std::size_t bytes, std::size_t alignment) override {
-    return std::malloc(bytes);  // NOLINT(cppcoreguidelines-owning-memory,hicpp-no-malloc)
+    return std::malloc(bytes);  // NOLINT(cppcoreguidelines-owning-memory,cppcoreguidelines-no-malloc,hicpp-no-malloc)
   }
 
   void do_deallocate(void* p, std::size_t bytes, std::size_t alignment) override {
-    std::free(p);  // NOLINT(cppcoreguidelines-owning-memory,hicpp-no-malloc)
+    std::free(p);  // NOLINT(cppcoreguidelines-owning-memory,cppcoreguidelines-no-malloc,hicpp-no-malloc)
   }
 
   [[nodiscard]] bool do_is_equal(const memory_resource& other) const BOOST_NOEXCEPT override {
@@ -26,7 +26,7 @@ memory_resource* get_default_resource() BOOST_NOEXCEPT {
   // Yes, this leaks. We have had SO many problems with the default memory resource going out of scope
   // before the other things were cleaned up that we decided to live with the leak, rather than
   // running into races over and over again.
-  static auto* default_resource_instance = new default_resource_impl();
+  static auto* default_resource_instance = new default_resource_impl();  // NOLINT(cppcoreguidelines-owning-memory)
   return default_resource_instance;
 }
 
