384146a69fbf2ebb14b05af4b1824da53fe62569
martin.boissier <martin.boissier@hpi.de>
2022-06-10 11:16:08 +0200

    Change

--- src/lib/memory/boost_default_memory_resource.cpp
+++ src/lib/memory/boost_default_memory_resource.cpp
@@ -7,17 +7,19 @@
 
 namespace boost::container::pmr {
 
-// NOLINTBEGIN(cppcoreguidelines-owning-memory,cppcoreguidelines-no-malloc,hicpp-no-malloc)
-// Usually, we want to avoid manual memory management. But in this cases it's by intention.
-class default_resource_impl : public memory_resource {  // NOLINT
+// We discourage manual memory management in Hyrise (such as malloc, or new), but in case of allocator/memory resource
+// implementations, it is fine.
+// NOLINTBEGIN(cppcoreguidelines-owning-memory,hicpp-no-malloc)
+
+class default_resource_impl : public memory_resource {
  public:
   void* do_allocate(std::size_t bytes, std::size_t alignment) override {
     return std::malloc(bytes);
-  }  // NOLINT
+  }
 
-  void do_deallocate(void* ptr, std::size_t bytes, std::size_t alignment) override {
-    std::free(ptr);
-  }  // NOLINT
+  void do_deallocate(void* p, std::size_t bytes, std::size_t alignment) override {
+    std::free(p);
+  }
 
   [[nodiscard]] bool do_is_equal(const memory_resource& other) const BOOST_NOEXCEPT override {
     return &other == this;
@@ -28,18 +30,19 @@ memory_resource* get_default_resource() BOOST_NOEXCEPT {
   // Yes, this leaks. We have had SO many problems with the default memory resource going out of scope
   // before the other things were cleaned up that we decided to live with the leak, rather than
   // running into races over and over again.
-  static auto* default_resource_instance = new default_resource_impl();  // NOLINT
+  static auto* default_resource_instance = new default_resource_impl();
   return default_resource_instance;
 }
-// NOLINTEND(cppcoreguidelines-owning-memory,cppcoreguidelines-no-malloc,hicpp-no-malloc)
 
 memory_resource* new_delete_resource() BOOST_NOEXCEPT {
   return get_default_resource();
 }
 
-memory_resource* set_default_resource(memory_resource* resource) BOOST_NOEXCEPT {
+memory_resource* set_default_resource(memory_resource* r) BOOST_NOEXCEPT {
   // Do nothing
   return get_default_resource();
 }
 
 }  // namespace boost::container::pmr
+
+// NOLINTEND(cppcoreguidelines-owning-memory,hicpp-no-malloc)
