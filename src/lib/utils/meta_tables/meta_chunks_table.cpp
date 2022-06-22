60d659c1fa1ad1157e67ef5d42362b4aebdc0630
Bouncner <martin+github@boissier.de>
2022-06-22 18:33:50 +0200

    Update src/lib/utils/meta_tables/meta_chunks_table.cpp
    Co-authored-by: Daniel Lindner <27929897+dey4ss@users.noreply.github.com>

--- src/lib/utils/meta_tables/meta_chunks_table.cpp
+++ src/lib/utils/meta_tables/meta_chunks_table.cpp
@@ -22,8 +22,9 @@ std::shared_ptr<Table> MetaChunksTable::_on_generate() const {
   for (const auto& [table_name, table] : Hyrise::get().storage_manager.tables()) {
     for (auto chunk_id = ChunkID{0}; chunk_id < table->chunk_count(); ++chunk_id) {
       const auto& chunk = table->get_chunk(chunk_id);
+      // Skip physically deleted chunks
       if (!chunk) {
-        continue;  // Skip physically deleted chunks
+        continue;
       }
 
       const auto cleanup_commit_id = chunk->get_cleanup_commit_id()
