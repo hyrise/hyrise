3117404a0a09d8de628306c24659a4e03ab3e0a7
Tom Wollnik <tom.wollnik@student.hpi.uni-potsdam.de>
2022-07-07 17:39:39 +0000

    lint memory tracking tests

--- src/test/lib/memory/tracking_memory_resource_test.cpp
+++ src/test/lib/memory/tracking_memory_resource_test.cpp
@@ -3,18 +3,16 @@
 
 namespace opossum {
 
-using namespace std::chrono_literals;
-
 class TrackingMemoryResourceTest : public BaseTest {
  protected:
   TrackingMemoryResource tracking_memory_resource;
 };
 
 TEST_F(TrackingMemoryResourceTest, PerformAllocations) {
-  auto int_ptr = (int*)tracking_memory_resource.allocate(sizeof(int));
+  auto int_ptr = reinterpret_cast<int*>(tracking_memory_resource.allocate(sizeof(int)));
   EXPECT_NO_THROW((*int_ptr) = 1);
 
-  auto float_ptr = (float*)tracking_memory_resource.allocate(sizeof(float) * 2);
+  auto float_ptr = reinterpret_cast<float*>(tracking_memory_resource.allocate(sizeof(float) * 2));
   EXPECT_NO_THROW(float_ptr[0] = 1);
   EXPECT_NO_THROW(float_ptr[1] = 2);
 }
@@ -27,14 +25,14 @@ TEST_F(TrackingMemoryResourceTest, RecordAllocations) {
   const auto& [recorded_time_int, recorded_amount_int] = tracking_memory_resource.memory_timeseries()[0];
   EXPECT_EQ(recorded_amount_int, sizeof(int));
   EXPECT_LT(recorded_time_int, std::chrono::system_clock::now());
-  EXPECT_GT(recorded_time_int, std::chrono::system_clock::now() - 100ms);
+  EXPECT_GT(recorded_time_int, std::chrono::system_clock::now() - std::chrono::milliseconds(100));
 
   tracking_memory_resource.allocate(sizeof(float) * 2);
   ASSERT_EQ(tracking_memory_resource.memory_timeseries().size(), 2);
   const auto& [recorded_time_float, recorded_amount_float] = tracking_memory_resource.memory_timeseries()[1];
   EXPECT_EQ(recorded_amount_float, sizeof(float) * 2);
   EXPECT_LT(recorded_time_float, std::chrono::system_clock::now());
-  EXPECT_GT(recorded_time_float, std::chrono::system_clock::now() - 100ms);
+  EXPECT_GT(recorded_time_float, std::chrono::system_clock::now() - std::chrono::milliseconds(100));
 }
 
 TEST_F(TrackingMemoryResourceTest, RecordDeallocations) {
@@ -46,7 +44,7 @@ TEST_F(TrackingMemoryResourceTest, RecordDeallocations) {
   const auto& [recorded_time_1, recorded_amount_1] = tracking_memory_resource.memory_timeseries()[0];
   EXPECT_EQ(recorded_amount_1, -10);
   EXPECT_LT(recorded_time_1, std::chrono::system_clock::now());
-  EXPECT_GT(recorded_time_1, std::chrono::system_clock::now() - 100ms);
+  EXPECT_GT(recorded_time_1, std::chrono::system_clock::now() - std::chrono::milliseconds(100));
 
   auto mem_ptr_2 = std::malloc(20);
   tracking_memory_resource.deallocate(mem_ptr_2, 20);
@@ -54,7 +52,7 @@ TEST_F(TrackingMemoryResourceTest, RecordDeallocations) {
   const auto& [recorded_time_2, recorded_amount_2] = tracking_memory_resource.memory_timeseries()[1];
   EXPECT_EQ(recorded_amount_2, -20);
   EXPECT_LT(recorded_time_2, std::chrono::system_clock::now());
-  EXPECT_GT(recorded_time_2, std::chrono::system_clock::now() - 100ms);
+  EXPECT_GT(recorded_time_2, std::chrono::system_clock::now() - std::chrono::milliseconds(100));
 }
 
 TEST_F(TrackingMemoryResourceTest, CreateAllocatorFromMemoryResource) {
@@ -66,7 +64,7 @@ TEST_F(TrackingMemoryResourceTest, CreateAllocatorFromMemoryResource) {
   const auto& [recorded_time_1, recorded_amount_1] = tracking_memory_resource.memory_timeseries()[0];
   EXPECT_EQ(recorded_amount_1, sizeof(int) * 5);
   EXPECT_LT(recorded_time_1, std::chrono::system_clock::now());
-  EXPECT_GT(recorded_time_1, std::chrono::system_clock::now() - 100ms);
+  EXPECT_GT(recorded_time_1, std::chrono::system_clock::now() - std::chrono::milliseconds(100));
 }
 
 }  // namespace opossum
