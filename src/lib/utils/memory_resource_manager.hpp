cc37f29a6f354b4caa2023da6c5bca07070c5e17
Tom Wollnik <tom.wollnik@student.hpi.uni-potsdam.de>
2022-07-07 15:26:30 +0000

    rename TrackedResource to ResourceRecord for better distinction with TrackedMemoryResource

--- src/lib/utils/memory_resource_manager.hpp
+++ src/lib/utils/memory_resource_manager.hpp
@@ -10,28 +10,28 @@
 
 namespace opossum {
 
-struct TrackedResource {
+struct ResourceRecord {
   OperatorType operator_type;
   std::string operator_data_structure;
   std::shared_ptr<TrackingMemoryResource> resource_ptr;
 
-  TrackedResource(const OperatorType ot, const std::string& ods, std::shared_ptr<TrackingMemoryResource>& tmr)
+  ResourceRecord(const OperatorType ot, const std::string& ods, std::shared_ptr<TrackingMemoryResource>& tmr)
       : operator_type{ot}, operator_data_structure{ods}, resource_ptr{tmr} {}
 };
 
 class MemoryResourceManager : public Noncopyable {
  public:
-  // TODO comment
-  const tbb::concurrent_vector<TrackedResource>& memory_resources() const;
+  const tbb::concurrent_vector<ResourceRecord>& memory_resources() const;
   std::shared_ptr<TrackingMemoryResource> get_memory_resource(const OperatorType operator_type,
                                                               const std::string& operator_data_structure);
 
  protected:
-  // make sure that only Hyrise can create new instances
+  // make sure that only Hyrise (and tests) can create new instances
   friend class Hyrise;
+  friend class MemoryResourceManagerTest;
   MemoryResourceManager() = default;
 
-  tbb::concurrent_vector<TrackedResource> _memory_resources;
+  tbb::concurrent_vector<ResourceRecord> _memory_resources;
 };
 
 }  // namespace opossum
