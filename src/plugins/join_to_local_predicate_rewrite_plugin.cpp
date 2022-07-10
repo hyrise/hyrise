723e3d487694522f3fcff70e9e73e66e4704db1d
LukasBudach <lukas.budach@student.hpi.de>
2022-07-10 20:40:42 +0000

    add more UCC candidates to be checked - both join columns on inner join

--- src/plugins/join_to_local_predicate_rewrite_plugin.cpp
+++ src/plugins/join_to_local_predicate_rewrite_plugin.cpp
@@ -30,7 +30,7 @@ std::string JoinToLocalPredicateRewritePlugin::description() const {
 
 void JoinToLocalPredicateRewritePlugin::start() {
     auto t = Timer();
-    // std::cout << "The Hyrise JoinToLocalPredicateRewritePlugin was started..." << std::endl;
+    std::cout << "The Hyrise JoinToLocalPredicateRewritePlugin was started..." << std::endl;
 
     const auto ucc_candidates = *identify_ucc_candidates();
 
@@ -40,20 +40,7 @@ void JoinToLocalPredicateRewritePlugin::start() {
         const auto table = Hyrise::get().storage_manager.get_table(candidate.table_name());
         const auto col_id = candidate.column_id();
 
-        // std::cout << "Attempting Unique Constraing Validation for:" << std::endl;
-        // std::cout << candidate.table_name() << " -- " << table->column_name(col_id) << std::endl;
-
-        //std::cout << "Existing Key Constraints:" << std::endl;
         const auto& soft_key_constraints = table->soft_key_constraints();
-        /*
-        for (const auto& key_constraint: soft_key_constraints) {
-            for (const auto column: key_constraint.columns()) {
-                std::cout << table->column_name(column) << " ";
-            }
-            std::cout << std::endl;
-        }
-        std::cout << std::endl;
-        */
 
         const auto chunk_count = table->chunk_count();
 
@@ -119,10 +106,6 @@ void JoinToLocalPredicateRewritePlugin::start() {
                         // If not all elements have been inserted, there must have occured a duplicate, so the UCC constraint is violated.
                         return;
                     }
-
-                    // std::cout << dict->size() << std::endl;
-                    // std::cout << attr_vector->size() << std::endl;
-                    // std::cout << "----" << std::en   dl;
                 } else {
                     Fail("The given segment type is not supported for the discovery of UCCs.");
                 }
@@ -131,15 +114,13 @@ void JoinToLocalPredicateRewritePlugin::start() {
             // We save UCC constraints directly inside the table so they can be forwarded to nodes in a query plan.
             std::cout << "  - Validated UCC on: " << table->column_name(col_id) << std::endl;
             table->add_soft_key_constraint(TableKeyConstraint(std::unordered_set(std::initializer_list<ColumnID>{col_id}), KeyConstraintType::UNIQUE));
-
-            // std::cout << std::endl;
         });
     }
     std::cout << "  - Time for UCC discovery: " << format_duration(t.lap()) << std::endl;
 }
 
 void JoinToLocalPredicateRewritePlugin::stop() {
-    // std::cout << "The Hyrise JoinToLocalPredicateRewritePlugin was stopped..." << std::endl;
+    std::cout << "The Hyrise JoinToLocalPredicateRewritePlugin was stopped..." << std::endl;
 }
 
 std::shared_ptr<std::vector<UCCCandidate>> JoinToLocalPredicateRewritePlugin::generate_valid_candidates(std::shared_ptr<AbstractLQPNode> root_node, std::shared_ptr<LQPColumnExpression> column_candidate) {
@@ -153,8 +134,6 @@ std::shared_ptr<std::vector<UCCCandidate>> JoinToLocalPredicateRewritePlugin::ge
             if (node->type != LQPNodeType::Predicate) {
                 return LQPVisitation::VisitInputs;
             }
-
-            // std::cout << "\t\t\t\t" << node->description() << std::endl;
             
             // when looking at predicate node, check whether the searched column is filtered in this predicate
             // -> if so, it is a valid UCC candidate; if not, still continue search
@@ -208,10 +187,7 @@ UCCCandidates* JoinToLocalPredicateRewritePlugin::identify_ucc_candidates() {
     auto ucc_candidates = new UCCCandidates{};
 
     for (const auto& [query, entry] : snapshot) {
-        // std::cout << "\n" << query << std::endl;
         const auto& root_node = entry.value;
-
-        // TODO: really use a vector, or deduplicate by using a set?
 
         visit_lqp(root_node, [&](auto& node) {
             const auto type = node->type;
@@ -219,8 +195,6 @@ UCCCandidates* JoinToLocalPredicateRewritePlugin::identify_ucc_candidates() {
                 // Non-Join and Non-Aggregate (Groupby) nodes are not considered for optimization using UCCs
                 return LQPVisitation::VisitInputs;
             }
-
-            // std::cout << "\t" << node->description() << std::endl;
 
             if (type == LQPNodeType::Aggregate) {
                 // in case of aggregate, extract all predicates used in groupby operations, then try to generate UCCCandidate objects from each of them
@@ -234,9 +208,7 @@ UCCCandidates* JoinToLocalPredicateRewritePlugin::identify_ucc_candidates() {
                     }
                     // every ColumnExpression used as a GroupBy expression should be checked for uniqueness
                     const auto table = std::static_pointer_cast<const StoredTableNode>(casted_candidate->original_node.lock());
-                    const auto candidate = UCCCandidate{table->table_name, casted_candidate->original_column_id};
-                    // std::cout << "\t\t\tAdding candidate " << candidate->table_name() << candidate->column_id() << std::endl;
-                    ucc_candidates->insert(candidate);
+                    ucc_candidates->insert(UCCCandidate{table->table_name, casted_candidate->original_column_id});
                 }
                 
                 return LQPVisitation::VisitInputs;
@@ -274,11 +246,9 @@ UCCCandidates* JoinToLocalPredicateRewritePlugin::identify_ucc_candidates() {
                         column_candidate = std::static_pointer_cast<LQPColumnExpression>(join_predicate->right_operand());
                     }
 
-                    // std::cout << "\t\tChecking for candidate " << column_candidate->as_column_name() << std::endl;
                     auto candidates = generate_valid_candidates(subtree_root, column_candidate);
                     if (candidates) {
                         for (const auto& candidate : *candidates) {
-                            // std::cout << "\t\t\tAdding candidate " << candidate.table_name() << candidate.column_id() << std::endl;
                             ucc_candidates->insert(candidate);
                         }
                     }
@@ -288,16 +258,21 @@ UCCCandidates* JoinToLocalPredicateRewritePlugin::identify_ucc_candidates() {
                 case JoinMode::Inner: {
                     auto column_candidates = std::vector<std::shared_ptr<AbstractExpression>>{join_predicate->left_operand(), join_predicate->right_operand()};
                     for (const auto& column_candidate : column_candidates) {
-                        // std::cout << "\t\tChecking for candidate " << column_candidate->as_column_name() << std::endl;
+                        const auto casted_candidate = std::static_pointer_cast<LQPColumnExpression>(column_candidate);
+
                         // determine which subtree (left or right) belongs to the ColumnExpression
                         auto subtree_root = join_node.left_input();
                         if (!expression_evaluable_on_lqp(column_candidate, *subtree_root.get())) {
                             subtree_root = join_node.right_input();
                         }
-                        auto candidates = generate_valid_candidates(subtree_root, std::static_pointer_cast<LQPColumnExpression>(column_candidate));
+
+                        // for Join2Semi, this column is already interesting for optimization, so add it as a potential candidate anyways
+                        const auto table = std::static_pointer_cast<const StoredTableNode>(casted_candidate->original_node.lock());
+                        ucc_candidates->insert(UCCCandidate{table->table_name, casted_candidate->original_column_id});
+
+                        auto candidates = generate_valid_candidates(subtree_root, casted_candidate);
                         if (candidates) {
                             for (const auto& candidate : *candidates) {
-                                // std::cout << "\t\t\tAdding candidate " << candidate.table_name() << candidate.column_id() << std::endl;
                                 ucc_candidates->insert(candidate);
                             }
                         }
@@ -314,13 +289,10 @@ UCCCandidates* JoinToLocalPredicateRewritePlugin::identify_ucc_candidates() {
                     if (!expression_evaluable_on_lqp(column_candidate, *subtree_root.get())) {
                         column_candidate = std::static_pointer_cast<LQPColumnExpression>(join_predicate->left_operand());
                     }
-
-                    // std::cout << "\t\tChecking for candidate " << column_candidate->as_column_name() << std::endl;
                     
                     auto candidates = generate_valid_candidates(subtree_root, column_candidate);
                     if (candidates) {
                         for (const auto& candidate : *candidates) {
-                            // std::cout << "\t\t\tAdding candidate " << candidate.table_name() << candidate.column_id() << std::endl;
                             ucc_candidates->insert(candidate);
                         }
                     }
@@ -330,29 +302,9 @@ UCCCandidates* JoinToLocalPredicateRewritePlugin::identify_ucc_candidates() {
                 default: {}
             }
 
-            // TO DO
-
-            // Identify whether the join operation could be improved by using UCCs
-            // .. Check Node type - we only want to utilize UCCs for join elimination and Aggregation simplifying
-            // -- which join types can actually be optimized? Only Inner? In case of left/right inner/outer, the one being dropped is the interesting side
-            // ... If we see join, check whether predicate filter is done on same table and different column prior to join
-            // ... Column being filtered on and join column are candidates
-            // INTERFACE: UCC Validation Candidate: std::vector of table name & column ID -> get table name by traversing to leaf, then it's instance variable of StoredTableNode
-            // Validate UCCs
-            // .. Iterate over UCCCandidates; For each candidate:
-            // ... Get all of its segments
-            // ... Start with Dictionary Segments -> if len(dictionary) == len(attributevector) then UCC constraint still intact
-            // ... If all individual segments match constraint, do value validation -> merge dictionaries, if collision UCC broken
-            // ... If still everything valid, look at non-dictionary segments, check whether all values stored are not yet in your merged value list
-            // --- If we want to use the GroupBy (Aggregation operator) -> put all values in one big vector, run operator on it, check whether #bins == #values
-            // ... Add information gained to table -> also remember which columns are not UCCs?
-
             return LQPVisitation::VisitInputs;
         });
     }
-
-    // std::cout << "------------------" << std::endl;
-    // std::cout << "UCC candidates: " << ucc_candidates->size() << std::endl;
 
     return ucc_candidates;
 }
