013e29a92279085ca1160836aa49bc991e2f8eb1
Tobias Jordan <Tobias.Jordan@vm-appleton.eaalab.hpi.uni-potsdam.de>
2022-07-19 13:52:44 +0000

    temporary activate temporary memory tracking per default

--- src/lib/utils/memory_resource_manager.hpp
+++ src/lib/utils/memory_resource_manager.hpp
@@ -44,7 +44,7 @@ class MemoryResourceManager : public Noncopyable {
   MemoryResourceManager() = default;
 
   tbb::concurrent_vector<ResourceRecord> _memory_resources;
-  bool _tracking_is_enabled = false;
+  bool _tracking_is_enabled = true;
 };
 
 }  // namespace opossum
