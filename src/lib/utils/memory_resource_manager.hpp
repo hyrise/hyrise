5acb2f2a0dac56837bcbe703f0e641a994c0fbb8
Tobias Jordan <Tobias.Jordan@vm-appleton.eaalab.hpi.uni-potsdam.de>
2022-07-22 10:35:35 +0000

    disabled tracking

--- src/lib/utils/memory_resource_manager.hpp
+++ src/lib/utils/memory_resource_manager.hpp
@@ -45,7 +45,7 @@ class MemoryResourceManager : public Noncopyable {
   MemoryResourceManager() = default;
 
   tbb::concurrent_vector<ResourceRecord> _memory_resources;
-  bool _tracking_is_enabled = true;
+  bool _tracking_is_enabled = false;
 };
 
 }  // namespace opossum
