37caa960e15ae514d058a2560a307480c6519f9d
Tobias Jordan <tobias.jordan@student.hpi.uni-potsdam.de>
2022-08-04 09:57:59 +0200

    linter issue

--- src/lib/strong_typedef.hpp
+++ src/lib/strong_typedef.hpp
@@ -19,57 +19,57 @@
  */
 
 #define STRONG_TYPEDEF(T, D)                                                                                      \
-namespace hyrise {                                                                                             \
-struct D : boost::totally_ordered1<D> {                                                                         \
-  typedef T base_type;                                                                                          \
-  T t;                                                                                                          \
-  constexpr explicit D(const T& t_) BOOST_NOEXCEPT_IF(boost::has_nothrow_copy_constructor<T>::value) : t(t_) {} \
-  D() BOOST_NOEXCEPT_IF(boost::has_nothrow_default_constructor<T>::value) : t() {}                              \
-  D& operator=(const T& other) BOOST_NOEXCEPT_IF(boost::has_nothrow_assign<T>::value) {                         \
-    t = other;                                                                                                  \
-    return *this;                                                                                               \
-  }                                                                                                             \
-  operator const T&() const {                                                                                   \
-    return t;                                                                                                   \
-  }                                                                                                             \
-  operator T&() {                                                                                               \
-    return t;                                                                                                   \
-  }                                                                                                             \
-  bool operator==(const D& other) const {                                                                       \
-    return t == other.t;                                                                                        \
-  }                                                                                                             \
-  bool operator<(const D& other) const {                                                                        \
-    return t < other.t;                                                                                         \
-  }                                                                                                             \
-};                                                                                                              \
-                                                                                                                \
-inline std::ostream& operator<<(std::ostream& stream, const D& value) {                                         \
-  return stream << value.t;                                                                                     \
-}                                                                                                               \
-                                                                                                                \
-} /* NOLINT */                                                                                                  \
-                                                                                                                \
-namespace std {                                                                                                 \
-template <>                                                                                                     \
-struct hash<::hyrise::D> : public unary_function<::hyrise::D, size_t> {                                       \
-  size_t operator()(const ::hyrise::D& x) const {                                                              \
-    return hash<T>{}(x);                                                                                        \
-  }                                                                                                             \
-};                                                                                                              \
-template <>                                                                                                     \
-                                                                                                                \
-struct numeric_limits<::hyrise::D> {                                                                           \
-  static typename std::enable_if_t<std::is_arithmetic_v<T>, ::hyrise::D> min() {                               \
-    return ::hyrise::D(numeric_limits<T>::min());                                                              \
-  }                                                                                                             \
-  static typename std::enable_if_t<std::is_arithmetic_v<T>, ::hyrise::D> max() {                               \
-    return ::hyrise::D(numeric_limits<T>::max());                                                              \
-  }                                                                                                             \
-};                                                                                                              \
-} /* NOLINT */                                                                                                  \
-namespace hyrise {                                                                                             \
-inline std::size_t hash_value(const D& d) {                                                                     \
-  return std::hash<D>()(d);                                                                                     \
-}                                                                                                               \
-} /* NOLINT */                                                                                                  \
-static_assert(true, "End call of macro with a semicolon") /* NOLINT */
+  namespace hyrise {                                                                                              \
+  struct D : boost::totally_ordered1<D> {                                                                         \
+    typedef T base_type;                                                                                          \
+    T t;                                                                                                          \
+    constexpr explicit D(const T& t_) BOOST_NOEXCEPT_IF(boost::has_nothrow_copy_constructor<T>::value) : t(t_) {} \
+    D() BOOST_NOEXCEPT_IF(boost::has_nothrow_default_constructor<T>::value) : t() {}                              \
+    D& operator=(const T& other) BOOST_NOEXCEPT_IF(boost::has_nothrow_assign<T>::value) {                         \
+      t = other;                                                                                                  \
+      return *this;                                                                                               \
+    }                                                                                                             \
+    operator const T&() const {                                                                                   \
+      return t;                                                                                                   \
+    }                                                                                                             \
+    operator T&() {                                                                                               \
+      return t;                                                                                                   \
+    }                                                                                                             \
+    bool operator==(const D& other) const {                                                                       \
+      return t == other.t;                                                                                        \
+    }                                                                                                             \
+    bool operator<(const D& other) const {                                                                        \
+      return t < other.t;                                                                                         \
+    }                                                                                                             \
+  };                                                                                                              \
+                                                                                                                  \
+  inline std::ostream& operator<<(std::ostream& stream, const D& value) {                                         \
+    return stream << value.t;                                                                                     \
+  }                                                                                                               \
+                                                                                                                  \
+  } /* NOLINT */                                                                                                  \
+                                                                                                                  \
+  namespace std {                                                                                                 \
+  template <>                                                                                                     \
+  struct hash<::hyrise::D> : public unary_function<::hyrise::D, size_t> {                                         \
+    size_t operator()(const ::hyrise::D& x) const {                                                               \
+      return hash<T>{}(x);                                                                                        \
+    }                                                                                                             \
+  };                                                                                                              \
+  template <>                                                                                                     \
+                                                                                                                  \
+  struct numeric_limits<::hyrise::D> {                                                                            \
+    static typename std::enable_if_t<std::is_arithmetic_v<T>, ::hyrise::D> min() {                                \
+      return ::hyrise::D(numeric_limits<T>::min());                                                               \
+    }                                                                                                             \
+    static typename std::enable_if_t<std::is_arithmetic_v<T>, ::hyrise::D> max() {                                \
+      return ::hyrise::D(numeric_limits<T>::max());                                                               \
+    }                                                                                                             \
+  };                                                                                                              \
+  } /* NOLINT */                                                                                                  \
+  namespace hyrise {                                                                                              \
+  inline std::size_t hash_value(const D& d) {                                                                     \
+    return std::hash<D>()(d);                                                                                     \
+  }                                                                                                               \
+  } /* NOLINT */                                                                                                  \
+  static_assert(true, "End call of macro with a semicolon")
