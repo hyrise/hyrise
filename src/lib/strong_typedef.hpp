863d0338b55e370aa1c74db415da9847859d3144
Tobias Jordan <tobias.jordan@student.hpi.uni-potsdam.de>
2022-08-04 10:04:06 +0200

    linter issue

--- src/lib/strong_typedef.hpp
+++ src/lib/strong_typedef.hpp
@@ -73,3 +73,4 @@
   }                                                                                                               \
   } /* NOLINT */                                                                                                  \
   static_assert(true, "End call of macro with a semicolon")
+  
