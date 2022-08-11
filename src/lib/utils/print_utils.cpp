7ad231f3f096aab62a954e334c90a98051bf8d30
dey4ss <27929897+dey4ss@users.noreply.github.com>
2022-08-11 10:21:12 +0200

    format comments

--- src/lib/utils/print_utils.cpp
+++ src/lib/utils/print_utils.cpp
@@ -10,10 +10,12 @@ using namespace opossum;  // NOLINT
 
 /**
  *
- * @param indentation   its size determines the indentation of a node, a true means a vertical line "|",
- *                      a false means, a space " " should be used to increase the indentation
- * @param id_by_node    used to determine whether a node was already printed and which id it had
- * @param id_counter    used to generate ids for nodes
+ * @param indentation   Helper structure for the indentation of a node. Its size determines the level of indentation.
+ *                      For each position, `true` means a vertical line "|", and `false` means a space " " should be
+ *                      used to increase the indentation.
+ * @param id_by_node    Mapping of nodes to their IDs. Used to determine whether a node was already printed and which ID
+ *                      it had.
+ * @param id_counter    ID of the last node. Used to generate the ID of the current node.
  */
 template <typename Node>
 void print_directed_acyclic_graph_impl(const std::shared_ptr<Node>& node,
@@ -22,9 +24,7 @@ void print_directed_acyclic_graph_impl(const std::shared_ptr<Node>& node,
                                        std::vector<bool>& indentation,
                                        std::unordered_map<std::shared_ptr<const Node>, size_t>& id_by_node,
                                        size_t& id_counter) {
-  /**
-   * Indent whilst drawing the edges
-   */
+  // Indent whilst drawing the edges.
   const auto max_indentation = indentation.empty() ? 0 : indentation.size() - 1;
   for (size_t level = 0; level < max_indentation; ++level) {
     if (indentation[level]) {
@@ -34,14 +34,12 @@ void print_directed_acyclic_graph_impl(const std::shared_ptr<Node>& node,
     }
   }
 
-  // Only the root node is not "pointed at" with "\_<node_info>"
+  // Only the root node is not "pointed at" with "\_<node_info>".
   if (!indentation.empty()) {
     stream << " \\_";
   }
 
-  /**
-   * Check whether the node has been printed before
-   */
+  // Check whether the node has been printed before.
   const auto iter = id_by_node.find(node);
   if (iter != id_by_node.end()) {
     stream << "Recurring Node --> [" << iter->second << "]" << std::endl;
@@ -52,9 +50,7 @@ void print_directed_acyclic_graph_impl(const std::shared_ptr<Node>& node,
   id_counter++;
   id_by_node.emplace(node, this_node_id);
 
-  /**
-   * Print node info
-   */
+  // Print node info.
   stream << "[" << this_node_id << "] ";
   node_print_fn(node, stream);
   stream << std::endl;
@@ -62,9 +58,7 @@ void print_directed_acyclic_graph_impl(const std::shared_ptr<Node>& node,
   const auto children = get_children_fn(node);
   indentation.emplace_back(true);
 
-  /**
-   * Recursively progress to children
-   */
+  // Recursively progress to children.
   for (size_t child_idx = 0; child_idx < children.size(); ++child_idx) {
     if (child_idx + 1 == children.size()) {
       indentation.back() = false;
--- src/lib/utils/print_utils.hpp
+++ src/lib/utils/print_utils.hpp
@@ -18,7 +18,7 @@ using NodePrintFn = std::function<void(const std::shared_ptr<Node>&, std::ostrea
 /**
  * Utility for formatted printing of any Directed Acyclic Graph.
  *
- * Results look comparable to this
+ * Results look comparable to this:
  *
  * [0] [Cross Join]
  *  \_[1] [Cross Join]
@@ -33,11 +33,11 @@ using NodePrintFn = std::function<void(const std::shared_ptr<Node>&, std::ostrea
  *     \_Recurring Node --> [3]
  *     \_Recurring Node --> [5]
  *
- * @param node              The root node originating from which the graph should be printed
- * @param get_children_fn   Callback that returns a nodes children
+ * @param node              The root node originating from which the graph should be printed.
+ * @param get_children_fn   Callback that returns a nodes children.
  * @param print_node_fn     Callback that prints the information about a node, e.g. "[Predicate] a = 42" in the example
- *                              above
- * @param stream            The stream to print on
+ *                          above.
+ * @param stream            The stream to print on.
  */
 template <typename Node>
 void print_directed_acyclic_graph(const std::shared_ptr<Node>& node, const NodeGetChildrenFn<Node>& get_children_fn,
@@ -46,13 +46,13 @@ void print_directed_acyclic_graph(const std::shared_ptr<Node>& node, const NodeG
 /**
  * Utility for formatted printing of table key constraints.
  *
- * Results look comparable to this
+ * Results look comparable to this (columns are ordered by ColumnID):
  *
  * PRIMARY_KEY(a), UNIQUE(b, c)
  *
- * @param table             Table the key constraints belong to
- * @param stream            The stream to print on
- * @param separator         String that is printed between different table key constraints
+ * @param table             Table the key constraints belong to.
+ * @param stream            The stream to print on.
+ * @param separator         String that is printed between different table key constraints.
  */
 void print_table_key_constraints(const std::shared_ptr<const Table>& table, std::ostream& stream,
                                  const std::string& separator = ", ");
