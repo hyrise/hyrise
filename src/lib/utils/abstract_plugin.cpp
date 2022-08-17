2fad0e7af1fa4b070b3879b94cd97ae98b3205f9
dey4ss <27929897+dey4ss@users.noreply.github.com>
2022-08-17 21:08:21 +0200

    avoid llvm-cov warning

---
+++ src/lib/utils/abstract_plugin.cpp
@@ -0,0 +1,13 @@
+#include "abstract_plugin.hpp"
+
+namespace hyrise {
+
+// We have to instantiate this function here because clang-12(+) does not instantiate it and llvm-cov throws a warning
+// (functions have mismatched data). See
+// https://stackoverflow.com/questions/57331600/llvm-cov-statistics-for-uninstantiated-functions
+std::vector<std::pair<PluginFunctionName, PluginFunctionPointer>> AbstractPlugin::provided_user_executable_functions()
+    const {
+  return {};
+}
+
+}  // namespace hyrise
