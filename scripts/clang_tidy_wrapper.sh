0f1eb39fcc74544af81bef425723a8de2ae1c3db
Martin Boissier <martin.boissier@hpi.de>
2022-08-24 20:07:15 +0000

    No console spamming

--- scripts/clang_tidy_wrapper.sh
+++ scripts/clang_tidy_wrapper.sh
@@ -14,7 +14,7 @@ if grep "$file_relative_to_source" $cmake_source_dir/.clang-tidy-ignore > /dev/n
     echo "clang-tidy: Ignoring $file_relative_to_source"
     exit 0
 else
-    which brew
+    which brew > /dev/null
     if [[ $? != 0 ]] ; then
         exec clang-tidy $file $@
     else
