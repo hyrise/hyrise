72ec803a15f0eb68ac8c2875cd5cc50acd3b872d
Tobias Jordan <tobias.jordan@student.hpi.uni-potsdam.de>
2022-09-02 10:55:30 +0200

    Added test for utils plus renaming

--- src/test/lib/utils/column_pruning_utils_test.cpp
+++ src/test/lib/utils/column_pruning_utils_test.cpp
@@ -4,9 +4,9 @@
 
 namespace hyrise {
 
-class ColumnIdsAfterPruningTest : public BaseTest {};
+class ColumnPruningUtilsTest : public BaseTest {};
 
-TEST_F(ColumnIdsAfterPruningTest, ColumnIDMapping) {
+TEST_F(ColumnPruningUtilsTest, ColumnIDMapping) {
   auto actual_column_mapping = column_ids_after_pruning(6, {ColumnID{0}, ColumnID{1}, ColumnID{2}});
   auto expected_column_mapping = std::vector<std::optional<ColumnID>>{std::nullopt, std::nullopt, std::nullopt,
                                                                       ColumnID{0},  ColumnID{1},  ColumnID{2}};
@@ -26,6 +26,22 @@ TEST_F(ColumnIdsAfterPruningTest, ColumnIDMapping) {
   expected_column_mapping = std::vector<std::optional<ColumnID>>{std::nullopt, ColumnID{0},  std::nullopt,
                                                                  ColumnID{1},  std::nullopt, ColumnID{2}};
   EXPECT_EQ(actual_column_mapping, expected_column_mapping);
+}
+
+TEST_F(ColumnPruningUtilsTest, ColumnIDBeforePruning) {
+  const auto pruned_column_ids = {ColumnID{0}, ColumnID{1}, ColumnID{2}};
+  const auto column_id_after_pruning = ColumnID{0};
+  const auto expected_column_id_before_pruning = ColumnID{3};
+
+  auto actual_column_id_before_pruning = column_id_before_pruning(column_id_after_pruning, pruned_column_ids);
+  EXPECT_EQ(actual_column_id_before_pruning, expected_column_id_before_pruning);
+}
+
+TEST_F(ColumnPruningUtilsTest, ThrowWhenPrunedColumnIDsUnsorted) {
+  const auto pruned_column_ids = {ColumnID{3}, ColumnID{1}, ColumnID{2}};
+  const auto column_id_after_pruning = ColumnID{0};
+
+  EXPECT_THROW(column_id_before_pruning(column_id_after_pruning, pruned_column_ids), std::exception);
 }
 
 }  // namespace hyrise
