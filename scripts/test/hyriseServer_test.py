50243cc4b8f91c7eb97cc21ba7dec82b2961799a
dey4ss <27929897+dey4ss@users.noreply.github.com>
2022-10-14 16:16:17 +0200

    more doc

--- scripts/test/hyriseServer_test.py
+++ scripts/test/hyriseServer_test.py
@@ -18,9 +18,11 @@ def main():
     server.expect(search_regex, timeout=120)
 
     server_port = int(re.search(search_regex, str(server.after)).group(1))
-    # Recent Postgres/psql versions changed the authentication behavior. Disabling an environment variable solves the
-    # issue. Since hyriseServer does not implement authentication at all, this is no problem.
-    # See https://github.com/psycopg/psycopg2/issues/1084#issuecomment-656778107
+    # Recent Postgres/psql versions changed the authentication behavior, resulting in connection errors on some setups.
+    # Disabling encrypted connections solves the issue. Since hyriseServer does not implement authentication at all,
+    # this is no problem.
+    # See https://github.com/psycopg/psycopg2/issues/1084#issuecomment-656778107 and
+    # https://www.postgresql.org/docs/13/libpq-connect.html#LIBPQ-CONNECT-GSSENCMODE
     environment_variables = os.environ.copy()
     environment_variables.update({"PGGSSENCMODE": "disable"})
     client = pexpect.spawn(f"psql -h localhost -p {server_port}", timeout=20, env=environment_variables)
