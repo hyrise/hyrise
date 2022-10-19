023e47a910d05abedb60a6fb4f4216ccd68923e8
dey4ss <27929897+dey4ss@users.noreply.github.com>
2022-10-19 14:49:48 +0200

    typo

--- src/test/lib/optimizer/strategy/join_to_predicate_rewrite_rule_test.cpp
+++ src/test/lib/optimizer/strategy/join_to_predicate_rewrite_rule_test.cpp
@@ -174,7 +174,7 @@ TEST_F(JoinToPredicateRewriteRuleTest, NoUnusedJoinSide) {
 }
 
 TEST_F(JoinToPredicateRewriteRuleTest, Union) {
-  // Do not rewrite if ther is a union on table b.
+  // Do not rewrite if there is a union on table b.
   auto key_constraints = TableKeyConstraints{};
   key_constraints.emplace(TableKeyConstraint({u->original_column_id}, KeyConstraintType::UNIQUE));
   key_constraints.emplace(TableKeyConstraint({v->original_column_id}, KeyConstraintType::UNIQUE));
