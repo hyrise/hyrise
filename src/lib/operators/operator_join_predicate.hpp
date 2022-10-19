bdbd0c5b7a94d256fdec513641118f5c51859ca7
dey4ss <27929897+dey4ss@users.noreply.github.com>
2022-10-19 14:44:41 +0200

    Revert "Refactor SortedSegmentSearch and add information to performance data  (#2497)"
    This reverts commit a09367849d9a3c025d9f8344797844d586a095b9.

--- src/lib/operators/operator_join_predicate.hpp
+++ src/lib/operators/operator_join_predicate.hpp
@@ -40,7 +40,7 @@ struct OperatorJoinPredicate {
 };
 
 // For gtest
-bool operator<(const OperatorJoinPredicate& lhs, const OperatorJoinPredicate& rhs);
-bool operator==(const OperatorJoinPredicate& lhs, const OperatorJoinPredicate& rhs);
+bool operator<(const OperatorJoinPredicate& l, const OperatorJoinPredicate& r);
+bool operator==(const OperatorJoinPredicate& l, const OperatorJoinPredicate& r);
 
 }  // namespace hyrise
