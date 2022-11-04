a415d7240f7716fceb35494f742009fc2e041b2a
phoeinx <hradilak@uni-potsdam.de>
2022-11-04 10:18:08 +0100

    Add parameterization to file io write benchmark

--- src/benchmark/file_io_write_micro_benchmark.cpp
+++ src/benchmark/file_io_write_micro_benchmark.cpp
@@ -11,8 +11,9 @@ namespace hyrise {
 // Defining the base fixture class
 class FileIOWriteMicroBenchmarkFixture : public MicroBenchmarkBasicFixture {
  public:
-  void SetUp(::benchmark::State& /*state*/) override {
-    // 32 bit * 250 000 000 entry equals 1GB data
+  void SetUp(::benchmark::State& state) override {
+    ssize_t BUFFER_SIZE = state.range(0);
+		// each int32_t contains four bytes
     int32_t vector_element_count = BUFFER_SIZE / 4;
     data_to_write = std::vector<int32_t>(vector_element_count, 42);
     if (creat("file.txt", O_WRONLY) < 1) {
@@ -29,29 +30,33 @@ class FileIOWriteMicroBenchmarkFixture : public MicroBenchmarkBasicFixture {
 
  protected:
   std::vector<int32_t> data_to_write;
-  const ssize_t BUFFER_SIZE = 100000000;
 };
 
-BENCHMARK_F(FileIOWriteMicroBenchmarkFixture, WRITE_NON_ATOMIC)(benchmark::State& state) {
+BENCHMARK_DEFINE_F(FileIOWriteMicroBenchmarkFixture, WRITE_NON_ATOMIC)(benchmark::State& state) {// open file
+  int32_t fd;
+  if ((fd = open("file.txt", O_WRONLY)) < 0) {
+		std::cout << "open error " << errno << std::endl;
+  }
   for (auto _ : state) {
-    // open file
-    int32_t fd;
-    if ((fd = open("file.txt", O_WRONLY)) < 0)
-      std::cout << "open error" << std::endl;
-    if (write(fd, std::data(data_to_write), BUFFER_SIZE) != BUFFER_SIZE)
-      std::cout << "write error" << std::endl;
+    if (write(fd, std::data(data_to_write), state.range(0)) != state.range(0)) {
+			std::cout << "write error " << errno << std::endl;
+		}
   }
 }
 
-BENCHMARK_F(FileIOWriteMicroBenchmarkFixture, PWRITE_ATOMIC)(benchmark::State& state) {
-  for (auto _ : state) {
-    // open file
-    int32_t fd;
-    if ((fd = open("file.txt", O_WRONLY)) < 0)
-      std::cout << "open error" << std::endl;
-    if (pwrite(fd, std::data(data_to_write), BUFFER_SIZE, 0) != BUFFER_SIZE)
-      std::cout << "write error" << std::endl;
-  }
+BENCHMARK_DEFINE_F(FileIOWriteMicroBenchmarkFixture, PWRITE_ATOMIC)(benchmark::State& state) {
+  int32_t fd;
+	if ((fd = open("file.txt", O_WRONLY)) < 0) {
+		std::cout << "open error " << errno << std::endl;
+	}
+	for (auto _ : state) {
+		if (pwrite(fd, std::data(data_to_write), state.range(0), 0) != state.range(0)) {
+			std::cout << "write error " << errno << std::endl;
+		}
+	}
 }
+
+BENCHMARK_REGISTER_F(FileIOWriteMicroBenchmarkFixture, WRITE_NON_ATOMIC)->Arg(10000000)->Arg(100000000);
+BENCHMARK_REGISTER_F(FileIOWriteMicroBenchmarkFixture, PWRITE_ATOMIC)->Arg(10000000)->Arg(100000000);
 
 }  // namespace hyrise
