bc7942cf49772b7dbbba1fdde6b19f9d3672962b
Robert Richter <robert.richter@rrcomtech.com>
2022-11-06 11:29:26 +0100

    Added MAP_SHARED Benchmark

--- src/benchmark/file_io_write_micro_benchmark.cpp
+++ src/benchmark/file_io_write_micro_benchmark.cpp
@@ -92,8 +92,62 @@ BENCHMARK_DEFINE_F(FileIOWriteMicroBenchmarkFixture, MMAP_ATOMIC_MAP_PRIVATE)(be
 
     // Getting the mapping to memory.
     off_t OFFSET = 0;
-		//TODO: Investigate impact of MAP_* flags on performance
+		/*
+    mmap man page: 
+    MAP_PRIVATE:
+      "Updates to the mapping are not visible to other processes 
+      mapping the same file"
+      "changes are not carried through to the underlying files"
+    */ 
     auto map = mmap(NULL, NUMBER_OF_BYTES, PROT_WRITE, MAP_PRIVATE, fd, OFFSET);
+    if (map == MAP_FAILED) {
+      std::cout << "Mapping Failed. " << std::strerror(errno) << std::endl;
+      continue;
+    }
+    
+    // Due to mmap, only writing in memory is required.
+		//TODO: check if we should use our data_to_write vector here
+    memset(map, 42, NUMBER_OF_BYTES);
+    // After writing, sync changes to filesystem.
+		if (msync(map, NUMBER_OF_BYTES, MS_SYNC) == -1) {
+			std::cout << "Write error " << errno << std::endl;
+		}
+
+    // Remove memory mapping after job is done.
+    if (munmap(map, NUMBER_OF_BYTES) != 0) {
+      std::cout << "Unmapping failed." << std::endl;
+    }
+	}
+}
+
+BENCHMARK_DEFINE_F(FileIOWriteMicroBenchmarkFixture, MMAP_ATOMIC_MAP_SHARED)(benchmark::State& state) {
+	const int32_t NUMBER_OF_BYTES = state.range(0) * MB;
+
+	int32_t fd;
+	if ((fd = open("file.txt", O_RDWR | O_CREAT | O_TRUNC)) < 0) {
+		std::cout << "open error " << errno << std::endl;
+	}
+
+	// set output file size
+	if (ftruncate(fd, NUMBER_OF_BYTES) < 0)	{
+		std::cout << "ftruncate error " << errno << std::endl;
+	}
+
+	for (auto _ : state) {
+		state.PauseTiming();
+		micro_benchmark_clear_disk_cache();
+		state.ResumeTiming();
+
+    // Getting the mapping to memory.
+    off_t OFFSET = 0;
+		/*
+    mmap man page: 
+    MAP_SHARED:
+      "Updates to the mapping are visible to other processes mapping 
+      the same region"
+      "changes are carried through to the underlying files"
+    */ 
+    auto map = mmap(NULL, NUMBER_OF_BYTES, PROT_WRITE, MAP_SHARED, fd, OFFSET);
     if (map == MAP_FAILED) {
       std::cout << "Mapping Failed. " << std::strerror(errno) << std::endl;
       continue;
@@ -118,5 +172,6 @@ BENCHMARK_DEFINE_F(FileIOWriteMicroBenchmarkFixture, MMAP_ATOMIC_MAP_PRIVATE)(be
 BENCHMARK_REGISTER_F(FileIOWriteMicroBenchmarkFixture, WRITE_NON_ATOMIC)->Arg(10)->Arg(100)->Arg(1000);
 BENCHMARK_REGISTER_F(FileIOWriteMicroBenchmarkFixture, PWRITE_ATOMIC)->Arg(10)->Arg(100)->Arg(1000);
 BENCHMARK_REGISTER_F(FileIOWriteMicroBenchmarkFixture, MMAP_ATOMIC_MAP_PRIVATE)->Arg(10)->Arg(100)->Arg(1000);
+BENCHMARK_REGISTER_F(FileIOWriteMicroBenchmarkFixture, MMAP_ATOMIC_MAP_SHARED)->Arg(10)->Arg(100)->Arg(1000);
 
 }  // namespace hyrise
