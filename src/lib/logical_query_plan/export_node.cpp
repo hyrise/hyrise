e708b17ff6c68e2c447ba74d585211b5cef40962
dey4ss <27929897+dey4ss@users.noreply.github.com>
2022-11-08 11:22:58 +0100

    export meta tables

--- src/lib/logical_query_plan/export_node.cpp
+++ src/lib/logical_query_plan/export_node.cpp
@@ -6,34 +6,26 @@
 
 namespace hyrise {
 
-ExportNode::ExportNode(const std::string& init_table_name, const std::string& init_file_name,
-                       const FileType init_file_type)
-    : AbstractNonQueryNode(LQPNodeType::Export),
-      table_name(init_table_name),
-      file_name(init_file_name),
-      file_type(init_file_type) {}
+ExportNode::ExportNode(const std::string& init_file_name, const FileType init_file_type)
+    : AbstractNonQueryNode(LQPNodeType::Export), file_name(init_file_name), file_type(init_file_type) {}
 
 std::string ExportNode::description(const DescriptionMode mode) const {
-  std::ostringstream stream;
-  stream << "[Export] Name: '" << table_name << "'";
-  return stream.str();
+  return "[Export]";
 }
 
 size_t ExportNode::_on_shallow_hash() const {
-  auto hash = boost::hash_value(table_name);
-  boost::hash_combine(hash, file_name);
+  auto hash = boost::hash_value(file_name);
   boost::hash_combine(hash, file_type);
   return hash;
 }
 
 std::shared_ptr<AbstractLQPNode> ExportNode::_on_shallow_copy(LQPNodeMapping& node_mapping) const {
-  return ExportNode::make(table_name, file_name, file_type);
+  return ExportNode::make(file_name, file_type);
 }
 
 bool ExportNode::_on_shallow_equals(const AbstractLQPNode& rhs, const LQPNodeMapping& node_mapping) const {
   const auto& export_node = static_cast<const ExportNode&>(rhs);
-  return table_name == export_node.table_name && file_name == export_node.file_name &&
-         file_type == export_node.file_type;
+  return file_name == export_node.file_name && file_type == export_node.file_type;
 }
 
 }  // namespace hyrise
--- src/test/lib/optimizer/strategy/column_pruning_rule_test.cpp
+++ src/test/lib/optimizer/strategy/column_pruning_rule_test.cpp
@@ -683,7 +683,7 @@ TEST_F(ColumnPruningRuleTest, DoNotPruneExportInputs) {
 
   // clang-format off
   const auto lqp =
-  ExportNode::make("dummy", "dummy.csv", FileType::Auto,
+  ExportNode::make("dummy.csv", FileType::Auto,
     PredicateNode::make(greater_than_(a, 5),
       node_a));
   // clang-format on
