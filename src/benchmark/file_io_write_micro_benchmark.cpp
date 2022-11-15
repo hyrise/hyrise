e5cb241986eac2a741c439a8cc3460a8f19ec4d9
Jan.Lemcke <janlemcke@live.de>
2022-11-15 12:28:08 +0100

    Sanity check added.

--- src/benchmark/file_io_write_micro_benchmark.cpp
+++ src/benchmark/file_io_write_micro_benchmark.cpp
@@ -5,7 +5,7 @@
 #include <unistd.h>
 
 #include <algorithm>
-
+#include <numeric>
 #include "micro_benchmark_basic_fixture.hpp"
 
 namespace hyrise {
@@ -14,17 +14,46 @@ const int32_t MB = 1000000;
 
 class FileIOWriteMicroBenchmarkFixture : public MicroBenchmarkBasicFixture {
  public:
+  uint64_t control_sum = uint64_t{0};
+
   void SetUp(::benchmark::State& state) override {
     // TODO(phoeinx): Make setup/teardown global per file size to improve benchmark speed
     ssize_t BUFFER_SIZE_MB = state.range(0);
     // each int32_t contains four bytes
     int32_t vector_element_count = (BUFFER_SIZE_MB * MB) / 4;
     data_to_write = std::vector<int32_t>(vector_element_count, 42);
+    control_sum = vector_element_count * 42;
 
     if (creat("file.txt", O_WRONLY) < 1) {
       std::cout << "create error" << std::endl;
     }
     chmod("file.txt", S_IRWXU);  // enables owner to rwx file
+  }
+
+  void SanityCheck(int32_t NUMBER_OF_BYTES){
+    int32_t fd;
+    if ((fd = open("file.txt", O_RDONLY)) < 0) {
+      std::cout << "open error " << errno << std::endl;
+    }
+    chmod("file.txt", S_IRWXU);  // enables owner to rwx file
+    std::vector<uint32_t> read_data;
+    read_data.resize(NUMBER_OF_BYTES / 4);
+
+    // Getting the mapping to memory.
+    off_t OFFSET = 0;
+
+    int32_t* map = reinterpret_cast<int32_t*>(mmap(NULL, NUMBER_OF_BYTES, PROT_READ, MAP_PRIVATE, fd, OFFSET));
+    if (map == MAP_FAILED) {
+      std::cout << "Mapping Failed. " << std::strerror(errno) << std::endl;
+    }
+
+    memcpy(std::data(read_data), map, NUMBER_OF_BYTES);
+    auto sum = std::accumulate(read_data.begin(), read_data.end(), uint64_t{0});
+    Assert(control_sum == sum, "Sanity check failed: Not the same result");
+    // Remove memory mapping after job is done.
+    if (munmap(map, NUMBER_OF_BYTES) != 0) {
+      std::cout << "Unmapping failed." << std::endl;
+    }
   }
 
   void TearDown(::benchmark::State& /*state*/) override {
@@ -52,6 +81,10 @@ BENCHMARK_DEFINE_F(FileIOWriteMicroBenchmarkFixture, WRITE_NON_ATOMIC)(benchmark
     if (write(fd, std::data(data_to_write), NUMBER_OF_BYTES) != NUMBER_OF_BYTES) {
       std::cout << "write error " << errno << std::endl;
     }
+
+    state.PauseTiming();
+    SanityCheck(NUMBER_OF_BYTES);
+    state.ResumeTiming();
   }
 }
 
@@ -70,6 +103,10 @@ BENCHMARK_DEFINE_F(FileIOWriteMicroBenchmarkFixture, PWRITE_ATOMIC)(benchmark::S
     if (pwrite(fd, std::data(data_to_write), NUMBER_OF_BYTES, 0) != NUMBER_OF_BYTES) {
       std::cout << "write error " << errno << std::endl;
     }
+
+    state.PauseTiming();
+    SanityCheck(NUMBER_OF_BYTES);
+    state.ResumeTiming();
   }
 }
 
@@ -155,6 +192,10 @@ void FileIOWriteMicroBenchmarkFixture::mmap_write_benchmark(benchmark::State& st
     if (munmap(map, NUMBER_OF_BYTES) != 0) {
       std::cout << "Unmapping failed." << std::endl;
     }
+
+    state.PauseTiming();
+    SanityCheck(NUMBER_OF_BYTES);
+    state.ResumeTiming();
   }
 }
 
