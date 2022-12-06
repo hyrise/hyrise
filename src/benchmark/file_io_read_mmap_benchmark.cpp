cbe0a111ebbf3f9dbee83552c536bb1f10426f1c
Sandro Sp <38314662+SanJSp@users.noreply.github.com>
2022-12-06 16:25:39 +0100

    mmap read threaded
    * sequential mmap benchmarks threaded
    
    * mmap threaded random benchmarks
    
    * Refactored file, introduced flags enum
    
    * fixed worng parameter flags
    
    * PR feedback: const and formatting
    
    Co-authored-by: Sandro Speh <sandrospeh@googlemail.com>

--- src/benchmark/file_io_read_mmap_benchmark.cpp
+++ src/benchmark/file_io_read_mmap_benchmark.cpp
@@ -4,27 +4,43 @@ namespace hyrise {
 
 class FileIOReadMmapBenchmarkFixture : public FileIOMicroReadBenchmarkFixture {};
 
-BENCHMARK_DEFINE_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_PRIVATE_RANDOM)(benchmark::State& state) {
+void read_mmap_chunk_sequential(const size_t from, const size_t to, const int32_t* map, uint64_t& sum) {
+  for (auto index = size_t{0} + from; index < to; ++index) {
+    sum += map[index];
+  }
+}
+
+void read_mmap_chunk_random(const size_t from, const size_t to, const int32_t* map, uint64_t& sum,
+                            const std::vector<uint32_t>& random_indexes) {
+  for (auto index = size_t{0} + from; index < to; ++index) {
+    sum += map[random_indexes[index]];
+  }
+}
+
+void FileIOMicroReadBenchmarkFixture::mmap_read_single_threaded(benchmark::State state, const int mmap_mode_flag,
+                                                                const int access_order) {
   auto fd = int32_t{};
   Assert(((fd = open(filename, O_RDONLY)) >= 0), fail_and_close_file(fd, "Open error: ", errno));
 
   for (auto _ : state) {
     state.PauseTiming();
-    const auto random_indices = generate_random_indexes(NUMBER_OF_ELEMENTS);
     micro_benchmark_clear_disk_cache();
     state.ResumeTiming();
 
     // Getting the mapping to memory.
     const auto OFFSET = off_t{0};
 
-    auto* map = reinterpret_cast<int32_t*>(mmap(NULL, NUMBER_OF_BYTES, PROT_READ, MAP_PRIVATE, fd, OFFSET));
-    Assert((map != MAP_FAILED), fail_and_close_file(fd, "Mapping failed: ", errno));
+    auto* map = reinterpret_cast<int32_t*>(mmap(NULL, NUMBER_OF_BYTES, PROT_READ, mmap_mode_flag, fd, OFFSET));
+    Assert((map != MAP_FAILED), fail_and_close_file(fd, "Mapping Failed: ", errno));
 
-    madvise(map, NUMBER_OF_BYTES, MADV_RANDOM);
-
+    if (access_order == RANDOM) {
+      madvise(map, NUMBER_OF_BYTES, MADV_RANDOM);
+    } else {
+      madvise(map, NUMBER_OF_BYTES, MADV_SEQUENTIAL);
+    }
     auto sum = uint64_t{0};
-    for (size_t index = 0; index < NUMBER_OF_ELEMENTS; ++index) {
-      sum += map[random_indices[index]];
+    for (auto index = size_t{0}; index < NUMBER_OF_ELEMENTS; ++index) {
+      sum += map[index];
     }
 
     state.PauseTiming();
@@ -34,113 +50,122 @@ BENCHMARK_DEFINE_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_PRIVATE_RAND
     // Remove memory mapping after job is done.
     Assert((munmap(map, NUMBER_OF_BYTES) == 0), fail_and_close_file(fd, "Unmapping failed: ", errno));
   }
+
   close(fd);
+}
+
+void FileIOMicroReadBenchmarkFixture::mmap_read_multi_threaded(benchmark::State& state, const int mmap_mode_flag,
+                                                               const uint16_t thread_count, const int access_order) {
+  auto fd = int32_t{};
+  Assert(((fd = open(filename, O_RDONLY)) >= 0), fail_and_close_file(fd, "Open error: ", errno));
+
+  auto threads = std::vector<std::thread>(thread_count);
+  const auto batch_size = static_cast<uint64_t>(std::ceil(static_cast<float>(NUMBER_OF_ELEMENTS) / thread_count));
+
+  for (auto _ : state) {
+    state.PauseTiming();
+    micro_benchmark_clear_disk_cache();
+    auto sums = std::vector<uint64_t>(thread_count);
+    state.ResumeTiming();
+
+    // Getting the mapping to memory.
+    const auto OFFSET = off_t{0};
+
+    auto* map = reinterpret_cast<int32_t*>(mmap(NULL, NUMBER_OF_BYTES, PROT_READ, mmap_mode_flag, fd, OFFSET));
+    Assert((map != MAP_FAILED), fail_and_close_file(fd, "Mapping Failed: ", errno));
+
+    if (access_order == RANDOM) {
+      state.PauseTiming();
+      const auto random_indexes = generate_random_indexes(NUMBER_OF_ELEMENTS);
+      state.ResumeTiming();
+
+      madvise(map, NUMBER_OF_BYTES, MADV_RANDOM);
+
+      for (auto i = size_t{0}; i < thread_count; ++i) {
+        const auto from = batch_size * i;
+        const auto to = std::min(from + batch_size, uint64_t{NUMBER_OF_ELEMENTS});
+        // std::ref fix from https://stackoverflow.com/a/73642536
+        threads[i] = std::thread(read_mmap_chunk_random, from, to, map, std::ref(sums[i]), random_indexes);
+      }
+    } else {
+      madvise(map, NUMBER_OF_BYTES, MADV_SEQUENTIAL);
+
+      for (auto i = size_t{0}; i < thread_count; ++i) {
+        const auto from = batch_size * i;
+        const auto to = std::min(from + batch_size, uint64_t{NUMBER_OF_ELEMENTS});
+        // std::ref fix from https://stackoverflow.com/a/73642536
+        threads[i] = std::thread(read_mmap_chunk_sequential, from, to, map, std::ref(sums[i]));
+      }
+    }
+    for (auto i = size_t{0}; i < thread_count; ++i) {
+      // Blocks the current thread until the thread identified by *this finishes its execution
+      threads[i].join();
+    }
+    state.PauseTiming();
+    const auto total_sum = std::accumulate(sums.begin(), sums.end(), uint64_t{0});
+
+    Assert(control_sum == total_sum, "Sanity check failed: Not the same result");
+    state.ResumeTiming();
+
+    Assert(msync(map, NUMBER_OF_BYTES, MS_SYNC) != -1, "Mapping Syncing Failed:" + std::strerror(errno));
+    state.PauseTiming();
+
+    // Remove memory mapping after job is done.
+    Assert((munmap(map, NUMBER_OF_BYTES) == 0), fail_and_close_file(fd, "Unmapping failed: ", errno));
+    state.ResumeTiming();
+  }
+
+  close(fd);
+}
+
+BENCHMARK_DEFINE_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_PRIVATE_RANDOM)(benchmark::State& state) {
+  const auto thread_count = static_cast<uint16_t>(state.range(1));
+  if (thread_count == 1) {
+    mmap_read_single_threaded(state, PRIVATE, RANDOM);
+  } else {
+    mmap_read_multi_threaded(state, PRIVATE, thread_count, RANDOM);
+  }
 }
 
 BENCHMARK_DEFINE_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_PRIVATE_SEQUENTIAL)(benchmark::State& state) {
-  auto fd = int32_t{};
-  Assert(((fd = open(filename, O_RDONLY)) >= 0), fail_and_close_file(fd, "Open error: ", errno));
-
-  for (auto _ : state) {
-    state.PauseTiming();
-    micro_benchmark_clear_disk_cache();
-    state.ResumeTiming();
-
-    // Getting the mapping to memory.
-    const auto OFFSET = off_t{0};
-
-    auto* map = reinterpret_cast<int32_t*>(mmap(NULL, NUMBER_OF_BYTES, PROT_READ, MAP_PRIVATE, fd, OFFSET));
-    Assert((map != MAP_FAILED), fail_and_close_file(fd, "Mapping Failed: ", errno));
-
-    madvise(map, NUMBER_OF_BYTES, MADV_SEQUENTIAL);
-
-    auto sum = uint64_t{0};
-    for (auto index = size_t{0}; index < NUMBER_OF_ELEMENTS; ++index) {
-      sum += map[index];
-    }
-
-    state.PauseTiming();
-    Assert(control_sum == sum, "Sanity check failed: Not the same result");
-    state.ResumeTiming();
-
-    // Remove memory mapping after job is done.
-    Assert((munmap(map, NUMBER_OF_BYTES) == 0), fail_and_close_file(fd, "Unmapping failed: ", errno));
+  const auto thread_count = static_cast<uint16_t>(state.range(1));
+  if (thread_count == 1) {
+    mmap_read_single_threaded(state, PRIVATE, SEQUENTIAL);
+  } else {
+    mmap_read_multi_threaded(state, PRIVATE, thread_count, SEQUENTIAL);
   }
-
-  close(fd);
 }
 
 BENCHMARK_DEFINE_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_SHARED_RANDOM)(benchmark::State& state) {
-  auto fd = int32_t{};
-  Assert(((fd = open(filename, O_RDONLY)) >= 0), fail_and_close_file(fd, "Open error: ", errno));
-
-  for (auto _ : state) {
-    state.PauseTiming();
-    const auto random_indices = generate_random_indexes(NUMBER_OF_ELEMENTS);
-    micro_benchmark_clear_disk_cache();
-    state.ResumeTiming();
-
-    // Getting the mapping to memory.
-    const auto OFFSET = off_t{0};
-
-    auto* map = reinterpret_cast<int32_t*>(mmap(NULL, NUMBER_OF_BYTES, PROT_READ, MAP_SHARED, fd, OFFSET));
-    Assert((map != MAP_FAILED), fail_and_close_file(fd, "Mapping failed: ", errno));
-
-    madvise(map, NUMBER_OF_BYTES, MADV_RANDOM);
-
-    auto sum = uint64_t{0};
-    for (auto index = size_t{0}; index < NUMBER_OF_ELEMENTS; ++index) {
-      sum += map[random_indices[index]];
-    }
-
-    state.PauseTiming();
-    Assert(control_sum == sum, "Sanity check failed: Not the same result");
-    state.ResumeTiming();
-
-    // Remove memory mapping after job is done.
-    Assert((munmap(map, NUMBER_OF_BYTES) == 0), fail_and_close_file(fd, "Unmapping failed: ", errno));
+  const auto thread_count = static_cast<uint16_t>(state.range(1));
+  if (thread_count == 1) {
+    mmap_read_single_threaded(state, SHARED, RANDOM);
+  } else {
+    mmap_read_multi_threaded(state, SHARED, thread_count, RANDOM);
   }
-
-  close(fd);
 }
 
 BENCHMARK_DEFINE_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_SHARED_SEQUENTIAL)(benchmark::State& state) {
-  auto fd = int32_t{};
-  Assert(((fd = open(filename, O_RDONLY)) >= 0), fail_and_close_file(fd, "Open error: ", errno));
-
-  for (auto _ : state) {
-    state.PauseTiming();
-    micro_benchmark_clear_disk_cache();
-    state.ResumeTiming();
-
-    // Getting the mapping to memory.
-    const auto OFFSET = off_t{0};
-
-    auto* map = reinterpret_cast<int32_t*>(mmap(NULL, NUMBER_OF_BYTES, PROT_READ, MAP_SHARED, fd, OFFSET));
-    Assert((map != MAP_FAILED), fail_and_close_file(fd, "Mapping failed: ", errno));
-
-    madvise(map, NUMBER_OF_BYTES, MADV_SEQUENTIAL);
-
-    auto sum = uint64_t{0};
-    for (size_t index = 0; index < NUMBER_OF_ELEMENTS; ++index) {
-      sum += map[index];
-    }
-
-    state.PauseTiming();
-    Assert(control_sum == sum, "Sanity check failed: Not the same result");
-    state.ResumeTiming();
-
-    // Remove memory mapping after job is done.
-    Assert((munmap(map, NUMBER_OF_BYTES) == 0), fail_and_close_file(fd, "Unmapping failed: ", errno));
+  const auto thread_count = static_cast<uint16_t>(state.range(1));
+  if (thread_count == 1) {
+    mmap_read_single_threaded(state, SHARED, SEQUENTIAL);
+  } else {
+    mmap_read_multi_threaded(state, SHARED, thread_count, SEQUENTIAL);
   }
-
-  close(fd);
 }
 
-BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_PRIVATE_SEQUENTIAL)->Arg(10)->Arg(100)->Arg(1000);
-BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_PRIVATE_RANDOM)->Arg(10)->Arg(100)->Arg(1000);
+BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_PRIVATE_SEQUENTIAL)
+    ->ArgsProduct({{100}, {1, 2, 4, 8, 16, 32}})
+    ->UseRealTime();
+BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_PRIVATE_RANDOM)
+    ->ArgsProduct({{100}, {1, 2, 4, 8, 16, 32}})
+    ->UseRealTime();
 
-BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_SHARED_SEQUENTIAL)->Arg(10)->Arg(100)->Arg(1000);
-BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_SHARED_RANDOM)->Arg(10)->Arg(100)->Arg(1000);
+BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_SHARED_SEQUENTIAL)
+    ->ArgsProduct({{100}, {1, 2, 4, 8, 16, 32}})
+    ->UseRealTime();
+BENCHMARK_REGISTER_F(FileIOMicroReadBenchmarkFixture, MMAP_ATOMIC_MAP_SHARED_RANDOM)
+    ->ArgsProduct({{100}, {1, 2, 4, 8, 16, 32}})
+    ->UseRealTime();
 
 }  // namespace hyrise
