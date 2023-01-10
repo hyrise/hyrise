f05a6629f8639aa699da0a5ec4cf69d6c5f4fc15
I577796 <s.speh@sap.com>
2023-01-10 10:42:23 +0100

    cache clearing now with sudo

--- src/benchmark/micro_benchmark_utils.cpp
+++ src/benchmark/micro_benchmark_utils.cpp
@@ -5,6 +5,8 @@
 #include <stddef.h>
 #include <unistd.h>
 #include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include <algorithm>
 #include <cstring>
@@ -23,8 +25,11 @@ void micro_benchmark_clear_cache() {
 void micro_benchmark_clear_disk_cache() {
   // TODO(phoenix): better documentation of which caches we are clearing
   sync();
-  std::ofstream ofs("/proc/sys/vm/drop_caches");
-  ofs << "3" << std::endl;
+#ifdef __APPLE__
+  system("purge");
+#else
+  system("echo 3 > /proc/sys/vm/drop_caches");
+#endif
 }
 
 void aio_error_handling(aiocb* aiocb, uint32_t expected_bytes) {
