ebb71925d90c0ba9db31b51feeb0ee9ebd523e62
Robert Richter <robert.richter@rrcomtech.com>
2023-02-28 18:04:29 +0100

    memory.max set & TPCH as default executing target.

--- scripts/cgroups/setup-cgroups.sh
+++ scripts/cgroups/setup-cgroups.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+# Fail on first error.
+set -e
+
 if [[ "$USER" != "root" ]];
 then
 	echo "Please run program as superuser!"
 	exit 1
 fi
+
+limit1=$((50*1024*1024*1024))
+limit2=$((50*1024*1024*1024*10))
+limit3=$((50*1024*1024*1024*100))
+
+# This is in Bytes! Too low levels will fail since Hyrise can't run without memory!
+memory_limits=($limit1 $limit2 $limit3)
 
 ####################################################################
 #
@@ -22,7 +32,13 @@ sudo cgcreate -g memory:$CGROUP_NAME
 # sudo cgget -g memory:$CGROUP_NAME
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-echo $SCRIPT_DIR
 cd "$SCRIPT_DIR/../../cmake-build-debug"
 
-sudo cgexec -g memory:$CGROUP_NAME sudo ./hyriseMicroIOReadBenchmark > output.log
+for limit in ${memory_limits[@]}; do
+	# Set memory.max
+	sudo cgset -r memory.max=$limit $CGROUP_NAME
+	# Get the result of setting operation.
+	sudo cgget -r memory.max $CGROUP_NAME
+	# Execute the benchmark.
+	sudo cgexec -g memory:$CGROUP_NAME sudo ./hyriseBenchmarkTPCH > output.log
+done
