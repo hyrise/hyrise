9f1cfa04cf24e2c807b3fcfe546983beaa89e262
phoeinx <hradilak@uni-potsdam.de>
2023-03-05 20:47:14 +0100

    Adapt to lower size benchmark, add output

--- scripts/cgroups/run_limited_memory_benchmarks.sh
+++ scripts/cgroups/run_limited_memory_benchmarks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # list of memory limits in GB
-memory_limits=(10 20 30 50 100)
+memory_limits=(1 2 3 5 10 20)
 
 #if zero parameters are given, print usage
 if [ $# -eq 0 ]; then
@@ -17,6 +17,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 for limit in "${memory_limits[@]}"
 do
   echo "Running $executable with memory limit of $limit GB"
-  source $SCRIPT_DIR/run_with_limited_memory.sh $limit $executable
+  source $SCRIPT_DIR/run_with_limited_memory.sh $limit $executable --benchmark_format=json > "benchmark_memory_limit_$limit.json"
 done
 
