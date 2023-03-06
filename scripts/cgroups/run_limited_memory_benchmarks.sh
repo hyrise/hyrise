5536d079627d438ea068ac36e2c01b8f48049c4b
phoeinx <hradilak@uni-potsdam.de>
2023-03-06 11:36:36 +0100

    Hot fix to fix output file parameter being added correctly

--- scripts/cgroups/run_limited_memory_benchmarks.sh
+++ scripts/cgroups/run_limited_memory_benchmarks.sh
@@ -16,8 +16,9 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # for each memory limit run the executable
 for limit in "${memory_limits[@]}"
 do
-  echo "Running $executable with memory limit of $limit GB"
-  source $SCRIPT_DIR/run_with_limited_memory.sh $limit $executable -o "benchmark_memory_limit_$limit.json"
+  run_executable="$executable -o benchmark_memory_limit_$limit.json"
+  echo "Running $run_executable with memory limit of $limit GB"
+  source $SCRIPT_DIR/run_with_limited_memory.sh $limit $run_executable
   #hot fix as binary file removal is not yet working
   sudo rm -f *.bin
 done
