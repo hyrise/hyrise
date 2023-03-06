35c943cad699e040eb9aabdd59d07ec4b550e66c
Niklas Riekenbrauck <nikriek@gmail.com>
2023-03-06 17:58:18 +0100

    Improve SSD region api

--- src/lib/storage/buffer/ssd_region.hpp
+++ src/lib/storage/buffer/ssd_region.hpp
@@ -14,13 +14,15 @@ class SSDRegion {
  public:
   enum class DeviceType { BLOCK, REGULAR_FILE };
 
-  SSDRegion(const std::filesystem::path& file_name, const uint64_t initial_num_bytes = 1UL << 32);
+  SSDRegion(const std::filesystem::path& file_name, const uint64_t initial_num_bytes = 1UL << 25);
   ~SSDRegion();
 
   void write_page(const PageID page_id, const PageSizeType size_type, const std::byte* source);
   void read_page(const PageID page_id, const PageSizeType size_type, std::byte* destination);
 
   DeviceType get_device_type() const;
+
+  std::filesystem::path get_file_name();
 
  private:
   const int _fd;
