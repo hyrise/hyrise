30a6bac2f6623595085d0e7a78d8535fd1d50aac
Niklas Riekenbrauck <nikriek@gmail.com>
2023-03-08 10:35:41 +0100

    Update ssd region benchmark

--- src/benchmark/storage/buffer/ssd_region_benchmark.cpp
+++ src/benchmark/storage/buffer/ssd_region_benchmark.cpp
@@ -15,9 +15,15 @@
 
 namespace hyrise {
 
-SSDRegion _create_ssd_region(benchmark::State& state, const SSDRegion::DeviceType device_type) {
+std::optional<SSDRegion> _create_ssd_region(benchmark::State& state, const SSDRegion::DeviceType device_type) {
   if (device_type == SSDRegion::DeviceType::BLOCK) {
-    return SSDRegion(ssd_region_block_path());
+    const auto block_path = ssd_region_block_path();
+    // On some devices, the block path does not exists. For these, we skip the test
+    if (!std::filesystem::exists(block_path)) {
+      state.SkipWithError("Block path does not exist.");
+      return std::nullopt;
+    }
+    return SSDRegion(block_path);
   } else if (device_type == SSDRegion::DeviceType::REGULAR_FILE) {
     std::filesystem::create_directories(ssd_region_scratch_path() / state.name());
     return SSDRegion(ssd_region_scratch_path() / state.name() / "data.bin");
@@ -33,7 +39,7 @@ static void BM_SSDRegionReadPagesSingle(benchmark::State& state, const SSDRegion
   const auto num_pages = state.range(0);
   for (auto _ : state) {
     for (auto page_id = PageID{0}; page_id < num_pages; page_id++) {
-      ssd_region.read_page(PageID{0}, outputPage.size_type(), outputPage.data());
+      ssd_region->read_page(PageID{0}, outputPage.size_type(), outputPage.data());
     }
   }
 
@@ -48,7 +54,7 @@ static void BM_SSDRegionReadPagesSerial(benchmark::State& state, const SSDRegion
 
   for (auto _ : state) {
     for (auto page_id = PageID{0}; page_id < num_pages; page_id++) {
-      ssd_region.read_page(page_id, pages[page_id].size_type(), pages[page_id].data());
+      ssd_region->read_page(page_id, pages[page_id].size_type(), pages[page_id].data());
     }
   }
 
@@ -68,7 +74,7 @@ static void BM_SSDRegionReadPagesRandom(benchmark::State& state, const SSDRegion
 
   for (auto _ : state) {
     for (auto read_index = int64_t{0}; read_index < num_pages; read_index++) {
-      ssd_region.read_page(random_page_ids[read_index], pages[read_index].size_type(), pages[read_index].data());
+      ssd_region->read_page(random_page_ids[read_index], pages[read_index].size_type(), pages[read_index].data());
     }
   }
 
@@ -82,7 +88,7 @@ static void BM_SSDRegionWritePagesSingle(benchmark::State& state, const SSDRegio
   const auto num_pages = state.range(0);
   for (auto _ : state) {
     for (auto page_id = PageID{0}; page_id < num_pages; page_id++) {
-      ssd_region.write_page(PageID{0}, outputPage.size_type(), outputPage.data());
+      ssd_region->write_page(PageID{0}, outputPage.size_type(), outputPage.data());
     }
   }
 
@@ -96,7 +102,7 @@ static void BM_SSDRegionWritePagesSerial(benchmark::State& state, const SSDRegio
   std::vector<Page32KiB, boost::alignment::aligned_allocator<Page32KiB>> pages(num_pages);
   for (auto _ : state) {
     for (auto page_id = PageID{0}; page_id < num_pages; page_id++) {
-      ssd_region.write_page(page_id, pages[page_id].size_type(), pages[page_id].data());
+      ssd_region->write_page(page_id, pages[page_id].size_type(), pages[page_id].data());
       auto size = pages.size();
       benchmark::DoNotOptimize(size);
     }
