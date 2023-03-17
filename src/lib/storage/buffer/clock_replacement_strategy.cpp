cb3a25c92e08f5e43a0d7d13446c06c9ad4aad1f
Niklas Riekenbrauck <nikriek@gmail.com>
2023-03-17 17:25:06 +0100

    Backup clock

--- src/lib/storage/buffer/clock_replacement_strategy.cpp
+++ src/lib/storage/buffer/clock_replacement_strategy.cpp
@@ -4,63 +4,47 @@
 // TODO: Allow removal of frame
 
 namespace hyrise {
-ClockReplacementStrategy::ClockReplacementStrategy(const size_t num_frames)
-    : _num_frames(num_frames), _current_frame_id(0) {
-  _pinned_frames.resize(num_frames);
-  _reference_bits.resize(num_frames);
-  _used_frames.resize(num_frames);
-};
+ClockReplacementStrategy::ClockReplacementStrategy(std::vector<Frame>& frames) : _frames(frames) {}
 
-void ClockReplacementStrategy::record_frame_access(FrameID frame_id) {
-  Assert(frame_id < _num_frames, "frame_id cannot be larger than the total number of frames");
+void ClockReplacementStrategy::record_frame_access(const FrameID frame_id) {
+  Assert(frame_id < _frames->capacity(), "frame_id cannot be larger than the total number of frames");
+  auto frame = _frames[frame_id];
 
-  // When the frame is pinned, we can just ignore it. Unpinning should happen manually at some other point.
-  if (_pinned_frames[frame_id]) {
-    return;
-  }
-  _used_frames[frame_id] = true;
-  _reference_bits[frame_id] = true;
+  frame.referenced = true;
+  frame.in_use = true;
 }
 
-FrameID ClockReplacementStrategy::find_victim() {
-  Assert(!_pinned_frames.all(), "All frames are currently pinned");
+Frame* ClockReplacementStrategy::find_victim() {
+  // Assert(!_pinned_frames.all(), "All frames are currently pinned");
 
   // Avoid an endless-loop if most frames are pinned. In this case, it should return an INVALID_FRAME_ID
   // and the buffer manager should handle things itself. We loop at most 2 times, so the the reference-bits
   // can be reset after one round.
-  for (auto frames_left = _num_frames * 2; frames_left > 0;
-       frames_left--, _current_frame_id = (_current_frame_id + 1) % _num_frames) {
-    if (_pinned_frames[_current_frame_id]) {
+  const auto num_frames = _frames.size();
+  for (auto frames_left = num_frames * 2; frames_left > 0;
+       frames_left--, _current_frame_id = (_current_frame_id + 1) % num_frames) {
+    auto frame = _frames[_current_frame_id];
+
+    if (frame.pin_count.load() > 0) {
       continue;
     }
 
-    if (_used_frames[_current_frame_id]) {
-      if (_reference_bits[_current_frame_id]) {
-        // Remove the reference bit. This frame could become a victim in the next round.
-        _reference_bits[_current_frame_id] = false;
-      } else {
-        // Victimize this frame and remove it from the clock
-        _used_frames[_current_frame_id] = false;
-        return _current_frame_id;
-      }
+    if (!frame.in_use) {
+      continue;
+    }
+
+    DebugAssert(frame.data != nullptr, "Data should be set");
+
+    if (frame.referenced) {
+      // Remove the reference bit. This frame could become a victim in the next round.
+      frame.referenced = false;
     } else {
-      _reference_bits[_current_frame_id] = true;
-      _used_frames[_current_frame_id] = true;
-      return _current_frame_id;
+      // Victimize this frame and remove it from the clock
+      return &frame;
     }
   }
 
-  return INVALID_FRAME_ID;
-}
-
-void ClockReplacementStrategy::pin(FrameID frame_id) {
-  Assert(frame_id < _num_frames, "frame_id cannot be larger than the total number of frames");
-  _pinned_frames[frame_id] = true;
-};
-
-void ClockReplacementStrategy::unpin(FrameID frame_id) {
-  Assert(frame_id < _num_frames, "frame_id cannot be larger than the total number of frames");
-  _pinned_frames[frame_id] = false;
+  return nullptr;
 }
 
 }  // namespace hyrise
--- src/lib/storage/buffer/clock_replacement_strategy.hpp
+++ src/lib/storage/buffer/clock_replacement_strategy.hpp
@@ -10,56 +10,32 @@ namespace hyrise {
 
 /**
  * @brief ClockReplacementStrategy implements the clock or second chance algorithm to select frames
- * for replacement in a FIFO-like fashion. Frames can be pinned to avoid replacement.
+ * for replacement in a FIFO-like fashion.
  * 
  */
 class ClockReplacementStrategy {
  public:
-  ClockReplacementStrategy(const size_t num_frames);
+  ClockReplacementStrategy(std::vector<Frame>& frames);
 
   /**
    * @brief Record the usage of a frame to avoid replacement in the near future
    * 
    * @param frame_id FrameID
    */
-  void record_frame_access(FrameID frame_id);
+  void record_frame_access(const FrameID frame_id);
 
   /**
    * @brief Find a FrameID that can be replaced with another page.
    * 
    * @return FrameID FrameID that can be used for replacement
    */
-  FrameID find_victim();
-
-  /**
-   * @brief Pins a frame that cannot replaced unitl unpinned.
-   * 
-   * @param frame_id FrameID
-   */
-  void pin(FrameID frame_id);
-
-  /**
-   * @brief Unpin a frame after it was pinned.
-   * 
-   * @param page_id FrameID
-   */
-  void unpin(FrameID page_id);
+  Frame* find_victim();
 
  private:
-  // Total number of frame slots
-  const size_t _num_frames;
-
   // Current pointer to a frame
   FrameID _current_frame_id;
 
-  // Frames that are actually used by a clock algorithm
-  boost::dynamic_bitset<> _used_frames;
-
-  // Save frames that are pinned and cannot be victimized
-  boost::dynamic_bitset<> _pinned_frames;
-
-  // Save which frames where recently accessed and thus get a second-chance
-  boost::dynamic_bitset<> _reference_bits;
+  std::vector<Frame>& _frames;
 };
 
 }  // namespace hyrise
