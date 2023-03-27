3e5aac6d5a883d6c0e8a00028f058dce934c0c2a
Niklas Riekenbrauck <nikriek@gmail.com>
2023-03-27 14:48:20 +0200

    Refactor page size type with enum

--- src/lib/storage/buffer/types.hpp
+++ src/lib/storage/buffer/types.hpp
@@ -2,11 +2,8 @@
 
 #include <tbb/concurrent_queue.h>
 #include <bit>
-#include <boost/describe.hpp>
-#include <boost/describe/enum.hpp>
-#include <boost/mp11.hpp>
-#include <boost/mp11/list.hpp>
 #include <limits>
+#include <magic_enum.hpp>
 #include "boost/integer/static_log2.hpp"
 #include "strong_typedef.hpp"
 #include "utils/assert.hpp"
@@ -19,33 +16,24 @@ namespace hyrise {
 constexpr PageID INVALID_PAGE_ID{std::numeric_limits<PageID::base_type>::max()};
 constexpr FrameID INVALID_FRAME_ID{std::numeric_limits<FrameID::base_type>::max()};
 
-// TODO: Check magic_enum
-BOOST_DEFINE_ENUM_CLASS(PageSizeType, KiB8, KiB16, KiB32, KiB64, KiB128, KiB256);
+enum class PageSizeType { KiB8, KiB16, KiB32, KiB64, KiB128, KiB256 };
 
 constexpr size_t bytes_for_size_type(const PageSizeType size) {
   return 1 << (13 + static_cast<size_t>(size));
 }
 
 constexpr PageSizeType find_fitting_page_size_type(const std::size_t value) {
-  if (value <= bytes_for_size_type(PageSizeType::KiB8)) {
-    return PageSizeType::KiB8;
-  } else if (value <= bytes_for_size_type(PageSizeType::KiB16)) {
-    return PageSizeType::KiB16;
-  } else if (value <= bytes_for_size_type(PageSizeType::KiB32)) {
-    return PageSizeType::KiB32;
-  } else if (value <= bytes_for_size_type(PageSizeType::KiB64)) {
-    return PageSizeType::KiB64;
-  } else if (value <= bytes_for_size_type(PageSizeType::KiB128)) {
-    return PageSizeType::KiB128;
-  } else if (value <= bytes_for_size_type(PageSizeType::KiB256)) {
-    return PageSizeType::KiB256;
+  for (auto page_size_type : magic_enum::enum_values<PageSizeType>()) {
+    if (value <= bytes_for_size_type(page_size_type)) {
+      return page_size_type;
+    }
   }
   Fail("Cannot fit value of " + std::to_string(value) + " bytes to a PageSizeType");
 }
 
-constexpr size_t NUM_PAGE_SIZE_TYPES = boost::mp11::mp_size<boost::describe::describe_enumerators<PageSizeType>>::value;
-constexpr PageSizeType MIN_PAGE_SIZE_TYPE = PageSizeType::KiB8;
-constexpr PageSizeType MAX_PAGE_SIZE_TYPE = PageSizeType::KiB256;
+constexpr size_t NUM_PAGE_SIZE_TYPES = magic_enum::enum_count<PageSizeType>();
+constexpr PageSizeType MIN_PAGE_SIZE_TYPE = magic_enum::enum_value<PageSizeType>(0);
+constexpr PageSizeType MAX_PAGE_SIZE_TYPE = magic_enum::enum_value<PageSizeType>(NUM_PAGE_SIZE_TYPES - 1);
 constexpr size_t MAX_PAGE_OFFSET = boost::static_log2<bytes_for_size_type(MAX_PAGE_SIZE_TYPE)>::value;
 constexpr size_t BITS_PAGE_SIZE_TYPES = std::bit_width(NUM_PAGE_SIZE_TYPES);
 
@@ -99,5 +87,4 @@ enum BufferManagerMode {
 };
 
 enum class PageType { Invalid, Dram, Numa };
-
 }  // namespace hyrise
