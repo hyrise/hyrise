cf631e5fbf969c2f7b921ae68d8cd1defee60859
martin.boissier <martin.boissier@hpi.de>
2023-06-01 13:33:47 +0200

    Working on review

--- src/lib/utils/column_pruning_utils.cpp
+++ src/lib/utils/column_pruning_utils.cpp
@@ -8,8 +8,13 @@ namespace hyrise {
 
 std::vector<std::optional<ColumnID>> column_ids_after_pruning(const size_t original_table_column_count,
                                                               const std::vector<ColumnID>& pruned_column_ids) {
-  std::vector<std::optional<ColumnID>> column_id_mapping(original_table_column_count);
-  std::vector<bool> column_pruned_bitvector(original_table_column_count);
+  DebugAssert(std::is_sorted(pruned_chunk_ids.begin(), pruned_chunk_ids.end()),
+              "Expected a sorted vector of pruned chunk IDs.");
+  DebugAssert(pruned_chunk_ids.size() < original_table_chunk_count,
+              "List of pruned chunks longer than chunks in actual table.");
+
+  auto column_id_mapping = std::vector<std::optional<ColumnID>>(original_table_column_count);
+  auto column_pruned_bitvector = std::vector<bool>(original_table_column_count);
 
   // Fill the bitvector
   for (const auto& pruned_column_id : pruned_column_ids) {
@@ -20,7 +25,8 @@ std::vector<std::optional<ColumnID>> column_ids_after_pruning(const size_t origi
   auto next_updated_column_id = ColumnID{0};
   for (auto column_index = ColumnID{0}; column_index < column_pruned_bitvector.size(); ++column_index) {
     if (!column_pruned_bitvector[column_index]) {
-      column_id_mapping[column_index] = next_updated_column_id++;
+      column_id_mapping[column_index] = next_updated_column_id;
+      ++next_updated_column_id;
     }
   }
   return column_id_mapping;
@@ -31,23 +37,21 @@ std::unordered_map<ChunkID, ChunkID> chunk_ids_after_pruning(const size_t origin
   DebugAssert(std::is_sorted(pruned_chunk_ids.begin(), pruned_chunk_ids.end()),
               "Expected a sorted vector of pruned chunk IDs.");
   DebugAssert(pruned_chunk_ids.size() < original_table_chunk_count,
-              "List of pruned chunks longer than chunks in actual table.");
+              "List of pruned chunks longer than chunks in actual table.");  
 
   auto chunk_id_mapping = std::unordered_map<ChunkID, ChunkID>{};
-  auto chunk_pruned_bitvector = std::vector<bool>(original_table_chunk_count);
-
-  // Fill the bitvector
-  for (const auto& pruned_chunk_id : pruned_chunk_ids) {
-    chunk_pruned_bitvector[pruned_chunk_id] = true;
-  }
+  auto pruned_chunk_ids_iter = pruned_chunk_ids.begin();
 
   // Calculate new chunk ids
   auto next_updated_chunk_id = ChunkID{0};
   for (auto chunk_index = ChunkID{0}; chunk_index < original_table_chunk_count; ++chunk_index) {
-    if (!chunk_pruned_bitvector[chunk_index]) {
-      chunk_id_mapping[chunk_index] = next_updated_chunk_id;
-      ++next_updated_chunk_id;
+    if (pruned_chunk_ids_iter != pruned_chunk_ids.end() && chunk_index == *pruned_chunk_ids_iter) {
+      ++pruned_chunk_ids_iter;
+      continue;
     }
+
+    chunk_id_mapping[chunk_index] = next_updated_chunk_id;
+    ++next_updated_chunk_id;
   }
   return chunk_id_mapping;
 }
