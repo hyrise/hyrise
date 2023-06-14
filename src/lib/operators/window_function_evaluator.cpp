01ae82a45dda7a61a933992234a85288adb9fb51
Niklas Mohrin <dev@niklasmohrin.de>
2023-06-14 17:02:21 +0200

    Write out the plan

--- src/lib/operators/window_function_evaluator.cpp
+++ src/lib/operators/window_function_evaluator.cpp
@@ -1,3 +1,6 @@
+#include <utility>
+
+#include "storage/value_segment.hpp"
 #include "window_function_evaluator.hpp"
 
 namespace hyrise {
@@ -5,6 +8,39 @@ namespace hyrise {
 const std::string& WindowFunctionEvaluator::name() const {
   static const auto name = std::string{"WindowFunctionEvaluator"};
   return name;
+}
+
+std::shared_ptr<const Table> WindowFunctionEvaluator::_on_execute() {
+  auto partitioned_data = partition_and_sort();
+
+  // TODO: need to know output type of window function
+  using T = int;
+
+  std::vector<ValueSegment<T>> result_segments(left_input_table()->chunk_count());
+  compute_window_function(partitioned_data, [&](RowID row_id, AllTypeVariant computed_value) {
+    result_segments[row_id.chunk_id][row_id.chunk_offset] = std::move(computed_value);
+  });
+
+  return annotate_input_table(std::move(result_segments));
+}
+
+WindowFunctionEvaluator::PartitionedData WindowFunctionEvaluator::partition_and_sort() const {
+  (void)left_input_table();
+  Fail("unimplemented");
+}
+
+void WindowFunctionEvaluator::compute_window_function(const PartitionedData& partitioned_data,
+                                                      auto&& emit_computed_value) const {
+  (void)partitioned_data;
+  (void)emit_computed_value;
+  Fail("unimplemented");
+}
+
+template <typename T>
+std::shared_ptr<const Table> WindowFunctionEvaluator::annotate_input_table(
+    std::vector<ValueSegment<T>> segments_for_output_column) const {
+  (void)segments_for_output_column;
+  Fail("Unimplemented");
 }
 
 }  // namespace hyrise
--- src/lib/operators/window_function_evaluator.hpp
+++ src/lib/operators/window_function_evaluator.hpp
@@ -1,15 +1,35 @@
 #pragma once
 
+#include <memory>
 #include <string>
+#include <utility>
+#include <vector>
 
 #include "abstract_read_only_operator.hpp"
+#include "all_type_variant.hpp"
+#include "concurrency/transaction_context.hpp"
+#include "storage/table.hpp"
 
 namespace hyrise {
 
 class WindowFunctionEvaluator : public AbstractReadOnlyOperator {
   // TODO: Think about not copying the data, but adding segments to all chunks of the input table
 
+ public:
+  using AbstractReadOnlyOperator::AbstractReadOnlyOperator;
+
   const std::string& name() const override;
+
+ protected:
+  std::shared_ptr<const Table> _on_execute() override;
+
+ private:
+  using PartitionedData = std::vector<std::vector<std::pair<AllTypeVariant, RowID>>>;
+
+  PartitionedData partition_and_sort() const;
+  void compute_window_function(const PartitionedData& partitioned_data, auto&& emit_computed_value) const;
+  template <typename T>
+  std::shared_ptr<const Table> annotate_input_table(std::vector<ValueSegment<T>> segments_for_output_column) const;
 };
 
 }  // namespace hyrise
