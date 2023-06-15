f394767229a3e985d589ba4981b54fe8a34165b1
Niklas Mohrin <dev@niklasmohrin.de>
2023-06-15 10:28:59 +0200

    Inheriting from `AbstractReadOnlyOperator` is correct
    Upstream said that we should create new `Chunk`s that re-use the
    previous segments (they are stored as shared pointers).

--- src/lib/operators/window_function_evaluator.hpp
+++ src/lib/operators/window_function_evaluator.hpp
@@ -13,8 +13,6 @@
 namespace hyrise {
 
 class WindowFunctionEvaluator : public AbstractReadOnlyOperator {
-  // TODO(group): Think about not copying the data, but adding segments to all chunks of the input table
-
  public:
   using AbstractReadOnlyOperator::AbstractReadOnlyOperator;
 
