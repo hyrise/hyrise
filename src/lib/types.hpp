85bfbfad92a9c5b3defdd6b084c143e8691e954a
Niklas Riekenbrauck <nikriek@gmail.com>
2023-06-25 16:09:13 +0200

    Another test

--- src/lib/types.hpp
+++ src/lib/types.hpp
@@ -78,7 +78,7 @@ using Cost = float;
 //
 // TODO(anyone): replace this with std::pmr once libc++ supports PMR.
 template <typename T>
-using PolymorphicAllocator = boost::container::scoped_allocator_adaptor<BufferPoolAllocator<T>>;
+using PolymorphicAllocator = BufferPoolAllocator<T>;
 
 // The string type that is used internally to store data. It's hard to draw the line between this and std::string or
 // give advice when to use what. Generally, everything that is user-supplied data (mostly, data stored in a table) is a
