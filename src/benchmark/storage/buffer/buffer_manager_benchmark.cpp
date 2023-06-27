bbc9c942d3c9ba240df0dccd69e3338fc4f9aaa7
Niklas Riekenbrauck <nikriek@gmail.com>
2023-06-27 18:01:03 +0200

    Fix

--- src/benchmark/storage/buffer/buffer_manager_benchmark.cpp
+++ src/benchmark/storage/buffer/buffer_manager_benchmark.cpp
@@ -1,6 +1,7 @@
 #include <memory>
 #include <vector>
 
+#include <random>
 #include "benchmark/benchmark.h"
 #include "storage/buffer/buffer_manager.hpp"
 
@@ -28,14 +29,12 @@ class BufferManagerFixture : public benchmark::Fixture {
 BENCHMARK_DEFINE_F(BufferManagerFixture, BM_BufferManagerPinForWrite)(benchmark::State& state) {
   constexpr auto PAGE_SIZE_TYPE = MIN_PAGE_SIZE_TYPE;
 
-  auto page_ids = std::vector<PageID>{DEFAULT_DRAM_BUFFER_POOL_SIZE / bytes_for_size_type(PAGE_SIZE_TYPE)};
-  auto i = size_t{0};
-  for (auto& page_id : page_ids) {
-    page_id = PageID{PAGE_SIZE_TYPE, i++};
-  }
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> distr(0, DEFAULT_DRAM_BUFFER_POOL_SIZE / bytes_for_size_type(PAGE_SIZE_TYPE));
 
   for (auto _ : state) {
-    auto page_id = INVALID_PAGE_ID;
+    auto page_id = PageID{PAGE_SIZE_TYPE, static_cast<size_t>(distr(gen))};
     _buffer_manager.pin_for_write(page_id);
     state.PauseTiming();
     std::memset(_buffer_manager._get_page_ptr(page_id), 0x1337, page_id.num_bytes());
