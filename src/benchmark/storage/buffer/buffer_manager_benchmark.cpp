55b8b2d468caf7e2c6c7508b555e79f3fe1652d7
Niklas Riekenbrauck <nikriek@gmail.com>
2023-06-28 09:36:48 +0200

    Update

--- src/benchmark/storage/buffer/buffer_manager_benchmark.cpp
+++ src/benchmark/storage/buffer/buffer_manager_benchmark.cpp
@@ -89,9 +89,93 @@ BENCHMARK_DEFINE_F(BufferManagerFixture, BM_BufferManagerPinForRead)(benchmark::
   state.SetBytesProcessed(int64_t(state.iterations()) * bytes_for_size_type(PAGE_SIZE_TYPE));
 }
 
+BENCHMARK_DEFINE_F(BufferManagerFixture, BM_BufferManagerMultiplePageSizesInMemory)(benchmark::State& state) {
+  const auto page_size_type = static_cast<PageSizeType>(state.range(0));
+  const auto max_page_idx = DEFAULT_DRAM_BUFFER_POOL_SIZE / bytes_for_size_type(page_size_type);
+
+  if (state.thread_index() == 0) {
+    for (auto idx = size_t{0}; idx < max_page_idx; ++idx) {
+      auto page_id = PageID{page_size_type, static_cast<size_t>(idx)};
+      std::memset(_buffer_manager._get_page_ptr(page_id), 0x1337, page_id.num_bytes());
+    }
+  }
+
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> distr(0, max_page_idx);
+
+  for (auto _ : state) {
+    auto page_id = PageID{page_size_type, static_cast<size_t>(distr(gen))};
+    _buffer_manager.pin_for_read(page_id);
+
+    state.PauseTiming();
+    auto page_ptr = _buffer_manager._get_page_ptr(page_id);
+    for (auto i = 0; i < page_id.num_bytes(); i += CACHE_LINE_SIZE) {
+      benchmark::DoNotOptimize(page_ptr[i]);
+    }
+    state.ResumeTiming();
+
+    _buffer_manager.unpin_for_read(page_id);
+    // benchmark::ClobberMemory();
+  }
+
+  state.SetItemsProcessed(int64_t(state.iterations()));
+  state.SetBytesProcessed(int64_t(state.iterations()) * bytes_for_size_type(PAGE_SIZE_TYPE));
+}
+
+BENCHMARK_DEFINE_F(BufferManagerFixture, BM_BufferManagerMultiplePageSizesOutMemory)(benchmark::State& state) {
+  const auto page_size_type = static_cast<PageSizeType>(state.range(0));
+  constexpr auto out_memory_ratio = size_t{4};
+  const auto max_page_idx = out_memory_ratio * DEFAULT_DRAM_BUFFER_POOL_SIZE / bytes_for_size_type(page_size_type);
+
+  if (state.thread_index() == 0) {
+    for (auto idx = size_t{0}; idx < max_page_idx; ++idx) {
+      auto page_id = PageID{page_size_type, static_cast<size_t>(idx)};
+      std::memset(_buffer_manager._get_page_ptr(page_id), 0x1337, page_id.num_bytes());
+    }
+  }
+
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> distr(0, max_page_idx);
+
+  for (auto _ : state) {
+    auto page_id = PageID{page_size_type, static_cast<size_t>(distr(gen))};
+    _buffer_manager.pin_for_read(page_id);
+
+    state.PauseTiming();
+    auto page_ptr = _buffer_manager._get_page_ptr(page_id);
+    for (auto i = 0; i < page_id.num_bytes(); i += CACHE_LINE_SIZE) {
+      benchmark::DoNotOptimize(page_ptr[i]);
+    }
+    state.ResumeTiming();
+
+    _buffer_manager.unpin_for_read(page_id);
+    // benchmark::ClobberMemory();
+  }
+
+  state.SetItemsProcessed(int64_t(state.iterations()));
+  state.SetBytesProcessed(int64_t(state.iterations()) * bytes_for_size_type(PAGE_SIZE_TYPE));
+}
+
 BENCHMARK_REGISTER_F(BufferManagerFixture, BM_BufferManagerPinForWrite)
     ->DenseRange(1, 4)
     ->ThreadRange(1, 128)
+    ->UseRealTime();
+
+BENCHMARK_REGISTER_F(BufferManagerFixture, BM_BufferManagerPinForRead)
+    ->DenseRange(1, 4)
+    ->ThreadRange(1, 128)
+    ->UseRealTime();
+
+BENCHMARK_REGISTER_F(BufferManagerFixture, BM_BufferManagerMultiplePageSizesInMemory)
+    ->DenseRange(static_cast<size_t>(MIN_PAGE_SIZE_TYPE), static_cast<size_t>(MAX_PAGE_SIZE_TYPE))
+    ->ThreadRange(1, 48)
+    ->UseRealTime();
+
+BENCHMARK_REGISTER_F(BufferManagerFixture, BM_BufferManagerMultiplePageSizesOutMemory)
+    ->DenseRange(static_cast<size_t>(MIN_PAGE_SIZE_TYPE), static_cast<size_t>(MAX_PAGE_SIZE_TYPE))
+    ->ThreadRange(1, 48)
     ->UseRealTime();
 
 }  // namespace hyrise
