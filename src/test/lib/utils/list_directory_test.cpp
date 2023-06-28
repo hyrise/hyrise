bbae576ac710df47e69aecf6cef91bde37ff43fe
Robert Richter <robert.richter@rrcomtech.com>
2023-06-28 14:46:23 +0200

    Feedback added

--- src/test/lib/utils/list_directory_test.cpp
+++ src/test/lib/utils/list_directory_test.cpp
@@ -3,43 +3,69 @@
 #include <sys/stat.h>
 #include <filesystem>
 #include <fstream>
+#include <unordered_set>
 
 #include "base_test.hpp"
 
 namespace hyrise {
 
-class ListDirectoryTest : public BaseTest {};
-
-TEST_F(ListDirectoryTest, ListBasicDirectory) {
-  // Setup: Creates test-directory in the folder, the test is executed in.
-  const auto TEST_DIRECTORY = std::string("./test-directory");
-
-  struct stat s;
-  auto directory_exists = (stat(TEST_DIRECTORY.c_str(), &s) == 0);
-  if (!directory_exists) {
-    directory_exists = std::filesystem::create_directory(TEST_DIRECTORY);
+class ListDirectoryTest : public BaseTest {
+ protected:
+  void SetUp() override {
+    std::filesystem::create_directory(_directory_path);
   }
 
-  EXPECT_TRUE(directory_exists);
+  void TearDown() override {
+    std::filesystem::remove_all(_directory_path);
+  }
 
-  const auto files = std::vector<std::string>{TEST_DIRECTORY + "/one.test", TEST_DIRECTORY + "/two.test",
-                                              TEST_DIRECTORY + "/three.test", TEST_DIRECTORY + "/four.test"};
+  const std::string _directory_path = test_data_path + "/test-directory";
+};
+
+TEST_F(ListDirectoryTest, ListDirectoryWithFilesAndSubdirectory) {
+  const auto files = std::unordered_set<std::string>{_directory_path + "/one.test", _directory_path + "/two.test",
+                                                     _directory_path + "/three.test", _directory_path + "/four.test"};
 
   for (const auto& file : files) {
     std::ofstream{file};
   }
-  std::filesystem::create_directory(TEST_DIRECTORY + "/test-subdirectory");
 
-  const auto entries = list_directory(TEST_DIRECTORY);
+  std::filesystem::create_directory(_directory_path + "/test-subdirectory");
+
+  const auto entries = list_directory(_directory_path);
 
   EXPECT_EQ(entries.size(), files.size());
   for (const auto& entry : entries) {
-    const auto position = std::find(files.begin(), files.end(), entry);
-    EXPECT_TRUE(position != files.end());
+    EXPECT_TRUE(files.contains(entry));
+  }
+}
+
+TEST_F(ListDirectoryTest, ListEmptyDirectory) {
+  EXPECT_EQ(list_directory(_directory_path).size(), 0);
+}
+
+TEST_F(ListDirectoryTest, ListDirectoryWithFilesOnly) {
+  const auto files = std::unordered_set<std::string>{_directory_path + "/one.test", _directory_path + "/two.test",
+                                                     _directory_path + "/three.test", _directory_path + "/four.test"};
+  for (const auto& file : files) {
+    std::ofstream{file};
   }
 
-  // Clean up
-  std::filesystem::remove_all(TEST_DIRECTORY);
+  const auto entries = list_directory(_directory_path);
+
+  EXPECT_EQ(entries.size(), files.size());
+  for (const auto& entry : entries) {
+    EXPECT_TRUE(files.contains(entry));
+  }
+}
+
+TEST_F(ListDirectoryTest, ListDirectoryWithSubdirectoriesOnly) {
+  std::filesystem::create_directory(_directory_path + "/subdir1");
+  std::filesystem::create_directory(_directory_path + "/subdir2");
+  std::filesystem::create_directory(_directory_path + "/subdir3");
+
+  const auto entries = list_directory(_directory_path);
+  EXPECT_EQ(entries.size(), 0);
 }
 
 }  // namespace hyrise
