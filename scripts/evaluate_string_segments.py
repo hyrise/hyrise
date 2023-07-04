bc86009f235b3a502cc664809f1f2e1cb02e956c
ClFeSc <68013019+ClFeSc@users.noreply.github.com>
2023-07-04 20:06:56 +0200

    WIP: Some work in the script

--- scripts/evaluate_string_segments.py
+++ scripts/evaluate_string_segments.py
@@ -2,7 +2,7 @@
 import json
 import shutil
 from abc import ABC, abstractmethod
-from argparse import ArgumentParser, ArgumentTypeError
+from argparse import ArgumentParser, ArgumentTypeError, BooleanOptionalAction
 from dataclasses import dataclass
 import multiprocessing
 from os import path
@@ -18,6 +18,7 @@ import matplotlib.pyplot as plt
 
 
 VERBOSITY_LEVEL = 0
+FAIL_FAST = False
 
 def print_debug(*args, required_verbosity_level: int, **kwargs) -> None:
     if VERBOSITY_LEVEL >= required_verbosity_level:
@@ -57,11 +58,78 @@ class Configuration:
     encoding_under_test: str
     time_limit: int
     verbosity_level: int
-    
 
-def plot(results: Mapping[str, float], *, title: str, yaxis: str, path: str, figsize: tuple[int, int]=(15, 10)) -> None:
+
+class DictConvertible(ABC):
+    @abstractmethod
+    def as_dict(self) -> dict:
+        ...
+
+
+@dataclass(frozen=True, slots=True)
+class Runtime(DictConvertible):
+    benchmark_name: str
+    items_per_second: float
+
+    def as_dict(self) -> dict:
+        return {
+            'benchmark_name': self.benchmark_name,
+            'items_per_second': self.items_per_second,
+        }
+
+
+@dataclass(slots=True)
+class Runtimes(DictConvertible):
+    runtimes: list[Runtime]
+
+    def as_dict(self) -> dict:
+        return {
+            'runtimes': list(runtime.as_dict() for runtime in self.runtimes)
+        }
+
+
+
+@dataclass(frozen=True, slots=True)
+class MemoryConsumption(DictConvertible):
+    column_name: str
+    column_type: str
+    memory_consumption: int
+
+    def as_dict(self) -> dict:
+        return {
+            'column_name': self.column_name,
+            'column_type': self.column_type,
+            'memory_consumption': self.memory_consumption,
+        }
+
+
+@dataclass(slots=True)
+class Metrics(DictConvertible):
+    memory_consumptions: list[MemoryConsumption]
+    
+    def as_dict(self) -> dict:
+        return {
+            'memory_consumption': list(consumption.as_dict() for consumption in self.memory_consumptions)
+        }
+
+    def min(self) -> int:
+        return min(map(lambda x: x.memory_consumption, self.memory_consumptions))
+
+    def max(self) -> int:
+        return max(map(lambda x: x.memory_consumption, self.memory_consumptions))
+
+    def average(self) -> float:
+        return statistics.fmean(map(lambda x: x.memory_consumption, self.memory_consumptions))
+
+    def median(self) -> float:
+        return statistics.median(map(lambda x: x.memory_consumption, self.memory_consumptions))
+
+
+def plot(results: dict[str, list], *, title: str, yaxis: str, path: str, figsize: tuple[int, int]=(15, 10)) -> None:
     f, axiis = plt.subplots(1, 1, figsize=figsize)
-    data=pd.DataFrame(data=results)
+    # data=pd.DataFrame(data=results)
+    data = pd.DataFrame.from_dict(results, orient='index')
+    data = data.transpose()
     print_debug(data, required_verbosity_level=3)
     if data.empty:
         print_error('Data Frame is empty; no result data to show!')
@@ -79,6 +147,7 @@ def plot(results: Mapping[str, float], *, title: str, yaxis: str, path: str, fig
 
 
 class Benchmark(ABC):
+    name = 'Benchmark'
     _config: Configuration
 
     # These encodings are supported by default for string types.
@@ -100,42 +169,54 @@ class Benchmark(ABC):
         }
         return classes[name](*args, **kwargs)
     
-    def run(self, config: Configuration) -> None:
+    def run(self, config: Configuration) -> Mapping[str, Runtimes] | None:
         try:
             self._config = config
             output(f'## Benchmark {self.name}:', required_verbosity_level=1)
-            for threading in ['ST', 'MT']:
+            threading_options: list[Literal['ST', 'MT']] = ['ST', 'MT']
+            for threading in threading_options:
                 output(f'### {"Single Thread" if threading == "ST" else "Mulitple Threads"}', required_verbosity_level=1)
                 result_jsons = [self._run(threading, encoding, False) for encoding in self._encodings]
                 test_json = self._run(threading, self._config.encoding_under_test, False)
-                times = {encoding: self._compare_run(result_json) for result_json, encoding in zip(result_jsons, self._encodings)}
+                times = {
+                    encoding: self._compare_run(result_json)
+                    for result_json, encoding
+                    in zip(result_jsons, self._encodings)
+                }
                 times[self._config.encoding_under_test] = self._compare_run(test_json)
                 plot_path = path.join(self._config.output_directory, 'runtime', 'plots', f'{self.name}-{threading}.png')
                 Path(plot_path).parent.mkdir(parents=True, exist_ok=True)
-                plot(times, title=f'Items per second for {self.name}', yaxis='Items per second of individual benchmark tests', path=plot_path)
+                times_to_plot = {
+                    encoding: list(map(lambda x: x.items_per_second, runtimes.runtimes))
+                    for encoding, runtimes
+                    in times.items()
+                }
+                plot(times_to_plot, title=f'Items per second for {self.name}', yaxis='Items per second of individual benchmark tests', path=plot_path)
                 # Dump raw data
                 raw_file_path = path.join(self._config.output_directory, 'runtime', 'raw', f'{self.name}-{threading}.json')
                 Path(raw_file_path).parent.mkdir(parents=True, exist_ok=True)
                 with open(raw_file_path, 'w') as f:
-                    json.dump(times, f)
+                    raw_times = {
+                        encoding: runtimes.as_dict()
+                        for encoding, runtimes
+                        in times.items()
+                    }
+                    json.dump(raw_times, f)
                 # for (encoding, json) in zip(self._encodings, result_jsons):
                 #     check_command = ['./scripts/compare_benchmarks.py', json, test_json]
                 #     compare_result = check_output(check_command)
                 #     output(f'Result for {encoding} vs. {self._config.encoding_under_test}:', required_verbosity_level=3)
                 #     output(compare_result.decode(encoding='utf-8'), required_verbosity_level=3)
+                return times
         except Exception as ex:
+            if FAIL_FAST:
+                raise ex
             error_message = f'Skipped {self.name} due to error {ex}.'
             print_error(error_message)
             output(error_message, required_verbosity_level=4)
+            return None
 
-    def _compare_run(self, json: str) -> list[float]:
-        @dataclass(frozen=True)
-        class Runtime:
-            benchmark_name: str
-            runtime: float
-        @dataclass
-        class Runtimes:
-            runtimes: list[Runtime]
+    def _compare_run(self, json: str) -> Runtimes:
         json_file = read_json(json)
         runtimes = Runtimes([])
         for benchmark in json_file['benchmarks']:
@@ -145,9 +226,10 @@ class Benchmark(ABC):
             # duration = statistics.mean(float(run['duration']) for run in successful_runs)
             runtime = Runtime(name, duration)
             runtimes.runtimes.append(runtime)
-        return list(map(lambda x: x.runtime, runtimes.runtimes))
+        # return list(map(lambda x: x.runtime, runtimes.runtimes))
+        return runtimes
 
-    def compare_metrics(self, config: Configuration) -> None:
+    def compare_metrics(self, config: Configuration) -> Mapping[str, Metrics] | None:
         try:
             self._config = config
             output(f'### Collecting Metrics for {self.name}:', required_verbosity_level=1)
@@ -157,41 +239,34 @@ class Benchmark(ABC):
             metrics[self._config.encoding_under_test]= self._compare_metrics(test_json)
             plot_path = path.join(self._config.output_directory, 'metrics', 'plots', f'{self.name}.png')
             Path(plot_path).parent.mkdir(parents=True, exist_ok=True)
-            plot(metrics, title=f'Sizes for {self.name}', yaxis='Size of Segments', path=plot_path)
+            metrics_to_plot: dict[str, list[int]] = {}
+            for encoding, metric in metrics.items():
+                metrics_to_plot[f'{encoding} (String)'] = list(map(lambda x: x.memory_consumption, filter(lambda x: x.column_type == 'string', metric.memory_consumptions)))
+                metrics_to_plot[f'{encoding} (All)'] = list(map(lambda x: x.memory_consumption, metric.memory_consumptions))
+            plot(metrics_to_plot, title=f'Sizes for {self.name}', yaxis='Size of Segments', path=plot_path, figsize=(22, 10))
             # Dump raw data
             raw_file_path = path.join(self._config.output_directory, 'metrics', 'raw', f'{self.name}.json')
             Path(raw_file_path).parent.mkdir(parents=True, exist_ok=True)
             with open(raw_file_path, 'w') as f:
-                json.dump(metrics, f)
+                raw_metrics = {
+                        encoding: metric.as_dict()
+                        for encoding, metric
+                        in metrics.items()
+                    }
+                json.dump(raw_metrics, f)
             # for encoding, json in zip(self._encodings, result_jsons):
             #     self._compare_metrics(json, test_json, encoding, self._config.encoding_under_test)
+            return metrics
         except Exception as ex:
+            if FAIL_FAST:
+                raise ex
             error_message = f'Skipped {self.name} due to error {ex}.'
             print_error(error_message)
             output(error_message, required_verbosity_level=4)
+            return None
 
     # def _compare_metrics(self, reference_json: str, test_json: str, reference_encoding: str, test_encoding: str) -> 'Metrics':
-    def _compare_metrics(self, json: str) -> list[int]:
-        @dataclass(frozen=True)
-        class MemoryConsumption:
-            column_name: str
-            column_type: str
-            memory_consumption: int
-        @dataclass
-        class Metrics:
-            memory_consumptions: list[MemoryConsumption]
-
-            def min(self) -> int:
-                return min(map(lambda x: x.memory_consumption, self.memory_consumptions))
-
-            def max(self) -> int:
-                return max(map(lambda x: x.memory_consumption, self.memory_consumptions))
-
-            def average(self) -> float:
-                return statistics.fmean(map(lambda x: x.memory_consumption, self.memory_consumptions))
-
-            def median(self) -> float:
-                return statistics.median(map(lambda x: x.memory_consumption, self.memory_consumptions))
+    def _compare_metrics(self, json: str) -> Metrics:
         json_file = read_json(json)
         # test = read_json(test_json)
         metrics = Metrics([])
@@ -200,14 +275,15 @@ class Benchmark(ABC):
         # for ref_segment, test_segment in zip(reference['segments'], test['segments']):
             column_type = segment['column_data_type']
             column_name = segment['column_name']
-            # Only look at string columns
-            if column_type != 'string':
-                continue
+            # # Only look at string columns
+            # if column_type != 'string':
+            #     continue
             metrics.memory_consumptions.append(MemoryConsumption(column_name, column_type, segment["estimated_size_in_bytes"]))
             # test_metrics.memory_consumptions.append(MemoryConsumption(column_name, column_type, test_segment["estimated_size_in_bytes"]))
         # output(f'For reference encoding {reference_encoding}: {{ min: {reference_metrics.min()}, max: {reference_metrics.max()}, average: {reference_metrics.average()}, median: {reference_metrics.median()} }}; ' +
         #        f'For test encoding {test_encoding}: {{ min: {test_metrics.min()}, max: {test_metrics.max()}, average: {test_metrics.average()}, median: {test_metrics.median()} }}', required_verbosity_level=2)
-        return list(map(lambda x: x.memory_consumption, metrics.memory_consumptions))
+        # return list(map(lambda x: x.memory_consumption, metrics.memory_consumptions))
+        return metrics
 
     def _run(self, threading: Literal['ST', 'MT'], encoding: str, metrics: bool) -> str:
         self._pre_run_cleanup()
@@ -232,11 +308,11 @@ class Benchmark(ABC):
 
     @abstractmethod
     def _pre_run_cleanup(self) -> None:
-        pass
+        ...
 
     @property
     def _path(self) -> str:
-        pass
+        ...
 
     def _output_path(self, threading: Literal['ST'] | Literal['MT'], encoding: str, metrics: bool) -> str:
         return path.join(self._config.tmp_path, f'{self.name}-{threading}-{encoding}-{metrics}.json')
@@ -295,7 +371,8 @@ class StarSchemaBenchmark(Benchmark):
 
 def parse_arguments() -> Configuration:
     global VERBOSITY_LEVEL
-    def check_positive(value: any) -> int:
+    global FAIL_FAST
+    def check_positive(value) -> int:
         ivalue = int(value)
         if ivalue <= 0:
             raise ArgumentTypeError("%s is an invalid positive int value" % value)
@@ -360,10 +437,21 @@ def parse_arguments() -> Configuration:
         action='count',
         help='Verbosity level. Supports multiple ocurrences (like `-vvv`) to increase verbosity.'
     )
+    parser.add_argument(
+        '--fail-fast',
+        dest='fail_fast',
+        action=BooleanOptionalAction,
+        required=False,
+        default=False,
+        help='Whether to fail early when an error occurs'
+    )
     namespace = parser.parse_args()
     if namespace.verbosity_level > 0:
         VERBOSITY_LEVEL = namespace.verbosity_level
         print_debug(f'Verbosity Mode enabled on level {VERBOSITY_LEVEL}.', required_verbosity_level=VERBOSITY_LEVEL)
+    if namespace.fail_fast:
+        FAIL_FAST = True
+        print_debug(f'Fail Fast Mode enabled', required_verbosity_level=2)
     now = datetime.now()
     now_date = f'{now.year}{now.month:02d}{now.day:02d}'
     now_time = f'{now.hour}{now.minute:02d}{now.second:02d}'
@@ -393,6 +481,27 @@ def locate_benchmarks(benchmarks: list[str], config: Configuration) -> list[Benc
     return benchmark_objects
 
 
+def plot_stats(stats: dict[str, tuple[Mapping[str, Runtimes], Mapping[str, Metrics]]], *, figsize: tuple[int, int]=(15, 10)) -> None:
+    f, axiis = plt.subplots(2, 2, figsize=figsize)
+    # data=pd.DataFrame(data=results)
+    data = pd.DataFrame.from_dict(stats, orient='index')
+    data = data.transpose()
+    print_debug(data, required_verbosity_level=3)
+    if data.empty:
+        print_error('Data Frame is empty; no result data to show!')
+        return
+    data.plot(
+        kind='box',
+        ax=axiis,
+        title=title,
+        xlabel='Encodings',
+        ylabel=f'{yaxis} (Logarithmic Scale)',
+        logy=True,
+    )
+    f.tight_layout()
+    f.savefig(path)
+
+
 def main():
     global output_file
     config = parse_arguments()
@@ -403,9 +512,13 @@ def main():
         print_debug(f'Running benchmark comparing {config.encoding_under_test} Encoding against built-in encodings.', required_verbosity_level=1)
         benchmarks_names = ['hyriseBenchmarkTPCH', 'hyriseBenchmarkTPCDS', 'hyriseBenchmarkJoinOrder', 'hyriseBenchmarkStarSchema']
         benchmarks = locate_benchmarks(benchmarks_names, config)
+        stats: dict[str, tuple[Mapping[str, Runtimes], Mapping[str, Metrics]]] = {}
         for benchmark in benchmarks:
-            benchmark.run(config)
-            benchmark.compare_metrics(config)
+            runtimes = benchmark.run(config)
+            metrics = benchmark.compare_metrics(config)
+            if runtimes is not None and metrics is not None:
+                stats[benchmark.name] = (runtimes, metrics)
+        plot_stats(stats)
 
 
 if __name__ == '__main__':
