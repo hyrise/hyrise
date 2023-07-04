077d4863f241555e876d6b9522f043da8eeb7f52
Finn Schoellkopf <schoellkopf.finn@gmail.com>
2023-07-04 19:06:36 +0200

    Add test queries for more complicated ColumnExpressions

--- resources/test_data/sqlite_testrunner_queries.sql
+++ resources/test_data/sqlite_testrunner_queries.sql
@@ -257,6 +257,9 @@ SELECT id, ROW_NUMBER() OVER (PARTITION BY a ORDER BY b) AS r FROM mixed
 -- partition on nullable column
 SELECT a, b, RANK() OVER (PARTITION BY b ORDER BY a) AS r FROM mixed_null
 SELECT a, b, RANK() OVER (PARTITION BY a ORDER BY b) AS r FROM mixed_null
+-- column expressions with offset
+SELECT id, RANK() OVER (PARTITION BY b + 2 ORDER BY c + 3) AS r FROM mixed
+SELECT id, a, b, c, SUM(b + 5) OVER (PARTITION BY a ORDER BY c + 5 ROWS BETWEEN UNBOUNDED PRECEDING AND 10 FOLLOWING) as s FROM mixed
 
 -- Aggregates
 SELECT SUM(b + b) AS sum_b_b FROM mixed;
