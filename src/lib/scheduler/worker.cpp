82af59082125f7ce7937d1a4fbe0c31225d487ef
Martin Boissier <martin.boissier@hpi.de>
2023-07-06 13:34:02 +0200

    Fix

--- src/lib/scheduler/worker.cpp
+++ src/lib/scheduler/worker.cpp
@@ -111,15 +111,15 @@ void Worker::_work(const bool allow_sleep) {
   auto task = std::shared_ptr<AbstractTask>{};
 
   const auto& queues = Hyrise::get().scheduler()->queues();
-  
+
   const auto got_local_task = get_task(true, _shutdown_flag, task, _next_task, _queue, queues);
   if (!got_local_task) {
     const auto got_distant_task = get_task(false, _shutdown_flag, task, _next_task, _queue, queues);
 
     if (!got_distant_task) {
-      // If there is no ready task neither in our queue nor in any other, worker waits for a new task to be pushed to the
-      // own queue. The waiting is skipped in case the scheduler is shutting down or sleep is not allowed (e.g., when
-      // _work() in called for a known number of unfinished jobs, see wait_for_tasks()).
+      // If there is no ready task neither in our queue nor in any other, worker waits for a new task to be pushed to
+      // the own queue. The waiting is skipped in case the scheduler is shutting down or sleep is not allowed (e.g.,
+      // when _work() in called for a known number of unfinished jobs, see wait_for_tasks()).
       if (allow_sleep && !get_task(true, _shutdown_flag, task, _next_task, _queue, queues)) {
         _queue->semaphore.wait();
         task = _queue->pull();
