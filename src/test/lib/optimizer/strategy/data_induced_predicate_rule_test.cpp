27fa8e287436dc3174ba5fdbb58486585c69fcd0
Tarik Alnawa <talnawa@outlook.de>
2023-07-06 10:40:11 +0200

    Add test for dip rule
    Co-authored-by: Paula <67064988+postmartem@users.noreply.github.com>

--- src/test/lib/optimizer/strategy/data_induced_predicate_rule_test.cpp
+++ src/test/lib/optimizer/strategy/data_induced_predicate_rule_test.cpp
@@ -30,10 +30,19 @@ class DataInducedPredicateRuleTest : public StrategyBaseTest {
       _b_a = _node_b->get_column("a");
       _b_b = _node_b->get_column("b");
     }
+
+    {
+      const auto histogram_column_a = GenericHistogram<int32_t>::with_single_bin(1, 50, 40, 40);
+      const auto histogram_column_b = GenericHistogram<int32_t>::with_single_bin(10, 15, 40, 5);
+      _node_c = create_mock_node_with_statistics({{DataType::Int, "a"}, {DataType::Int, "b"}}, 40,
+                                                 {histogram_column_a, histogram_column_b});
+      _c_a = _node_c->get_column("a");
+      _c_b = _node_c->get_column("b");
+    }
   }
 
-  std::shared_ptr<MockNode> _node_a, _node_b;
-  std::shared_ptr<LQPColumnExpression> _a_a, _a_b, _b_a, _b_b, _c_a;
+  std::shared_ptr<MockNode> _node_a, _node_b, _node_c;
+  std::shared_ptr<LQPColumnExpression> _a_a, _a_b, _b_a, _b_b, _c_a, _c_b;
   std::shared_ptr<DataInducedPredicateRule> _rule{std::make_shared<DataInducedPredicateRule>()};
 };
 
@@ -59,10 +68,53 @@ TEST_F(DataInducedPredicateRuleTest, CreateSimpleReductionOnLeftSide) {
   // clang-format on
 
   auto actual_lqp = StrategyBaseTest::apply_rule(_rule, input_lqp);
-        std::cout << "*expected_lqp" << std::endl;
-        std::cout << *expected_lqp << std::endl;
-        std::cout << "*actual_lqp" << std::endl;
-        std::cout << *actual_lqp << std::endl;
+
+  EXPECT_LQP_EQ(actual_lqp, expected_lqp);
+}
+
+TEST_F(DataInducedPredicateRuleTest, CreateSimpleReductionOnRightSide) {
+  // The _b_a side of the inner join has values from 1-50, the _a_a side has values from 10-20. Based on that
+  // selectivity, a data induced predicate should be created.
+
+  // clang-format off
+  const auto input_lqp =
+  JoinNode::make(JoinMode::Inner, equals_(_a_a, _b_a),
+    _node_b,
+    _node_a);
+
+  const auto subquery = AggregateNode::make(expression_vector(), expression_vector(min_(_b_a), max_(_b_a)), _node_b);
+  const auto min = ProjectionNode::make(expression_vector(min_(_b_a)), subquery);
+  const auto max = ProjectionNode::make(expression_vector(max_(_b_a)), subquery);
+  const auto expected_reduction = PredicateNode::make(between_inclusive_(_a_a,lqp_subquery_(min),lqp_subquery_(max)), _node_a);
+
+  const auto expected_lqp =
+  JoinNode::make(JoinMode::Inner, equals_(_a_a, _b_a),
+    _node_b,
+    expected_reduction);
+  // clang-format on
+
+  auto actual_lqp = StrategyBaseTest::apply_rule(_rule, input_lqp);
+
+  EXPECT_LQP_EQ(actual_lqp, expected_lqp);
+}
+
+TEST_F(DataInducedPredicateRuleTest, NoReductionForNonBeneficial) {
+  // The _a_a side of the inner join has values from 1-50, the _c_a side has the same value range as _a_a. Based on that
+  // selectivity, no data induced predicate should be created.
+
+  // clang-format off
+  const auto input_lqp =
+  JoinNode::make(JoinMode::Inner, equals_(_a_a, _c_a),
+    _node_a,
+    _node_c);
+
+  const auto expected_lqp =
+  JoinNode::make(JoinMode::Inner, equals_(_a_a, _c_a),
+    _node_a,
+    _node_c);
+  // clang-format on
+
+  auto actual_lqp = StrategyBaseTest::apply_rule(_rule, input_lqp);
 
   EXPECT_LQP_EQ(actual_lqp, expected_lqp);
 }
