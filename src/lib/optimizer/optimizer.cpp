6f922c7bd8731a84ffe227bddcd069f00268bc82
Sandro Speh <sandrospeh@gmail.com>
2023-07-26 16:13:54 +0200

    BENCHMARK COMMIT: Master (with SJR Rule)

--- src/lib/optimizer/optimizer.cpp
+++ src/lib/optimizer/optimizer.cpp
@@ -217,7 +217,7 @@ std::shared_ptr<Optimizer> Optimizer::create_default_optimizer() {
   // beneficial. However, TPC-H Q 21 (that is already long-running) degrades drastically. See:
   // https://github.com/hyrise/hyrise/pull/2536#issuecomment-1423076256
   // TODO(anyone): Re-evaluate this in the future.
-  // optimizer->add_rule(std::make_unique<SemiJoinReductionRule>());
+  optimizer->add_rule(std::make_unique<SemiJoinReductionRule>());
   // optimizer->add_rule(std::make_unique<DataInducedPredicateRule>());
 
   // TODO replace semi_join_r rule with our rule
