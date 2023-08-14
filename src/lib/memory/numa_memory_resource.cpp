5166bac18c1cd6f0631330b0d6f0b1e615ff82b4
Fabian Mahling <FabianMahling@web.de>
2023-08-14 16:06:51 +0200

    implement feedback on numa memory resource

--- src/lib/memory/numa_memory_resource.cpp
+++ src/lib/memory/numa_memory_resource.cpp
@@ -4,32 +4,21 @@
 #include <sys/mman.h>
 #include <unistd.h>
 
+#include <shared_mutex>
+
 #include "utils/assert.hpp"
 
 namespace hyrise {
 
 const auto PAGE_SIZE = getpagesize();
 
-// TODO(everyone): Find a better way to hold the mapping from NodeID to ArenaID.
-// Could be done as part of a memory manager.
-std::unordered_map<ArenaID, NodeID> NumaExtentHooks::node_id_for_arena_id = std::unordered_map<ArenaID, NodeID>{};
+static auto node_id_for_arena_id = std::unordered_map<ArenaID, NodeID>{};
+static auto node_id_for_arena_id_mutex = std::shared_mutex{};
 
-void NumaExtentHooks::store_node_id_for_arena(ArenaID arena_id, NodeID node_id) {
-  if (NumaExtentHooks::node_id_for_arena_id.contains(arena_id)) {
-    Fail("Tried to assign node id to an already assigned arena id.");
-  }
-  node_id_for_arena_id[arena_id] = node_id;
-}
+namespace numa_extent_hooks {
 
-/**
- * Allocates bytes on a specific node.
- *
- * Similar to the behavior, that libnuma employs for numa_alloc_onnode(..).
- * First, allocate memory with mmap and then use numa_tonode_memory(..) to move
- * the allocated memory to a specific node.
- */
-void* NumaExtentHooks::alloc(extent_hooks_t* extent_hooks, void* new_addr, size_t size, size_t alignment, bool* zero,
-                             bool* commit, unsigned arena_index) {
+void* alloc(extent_hooks_t* extent_hooks, void* new_addr, size_t size, size_t alignment, bool* zero, bool* commit,
+            unsigned arena_index) {
   auto off = size_t{size % PAGE_SIZE};
   if (off > 0) {
     size += PAGE_SIZE - off;
@@ -37,25 +26,37 @@ void* NumaExtentHooks::alloc(extent_hooks_t* extent_hooks, void* new_addr, size_
 
   auto addr = mmap(nullptr, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
   DebugAssert(addr != nullptr, "Failed to mmap pages.");
-  DebugAssert(NumaExtentHooks::node_id_for_arena_id.contains(arena_index),
-              "Tried allocation for arena without numa node assignment.");
 
-  auto& node_id = NumaExtentHooks::node_id_for_arena_id[arena_index];
+  auto node_id = get_node_id_of_arena(arena_index);
   numa_tonode_memory(addr, size, node_id);
   return addr;
 }
 
-bool NumaExtentHooks::dalloc(extent_hooks_t* extent_hooks, void* addr, size_t size, bool committed,
-                             unsigned arena_ind) {
+bool dalloc(extent_hooks_t* extent_hooks, void* addr, size_t size, bool committed, unsigned arena_ind) {
   munmap(addr, size);
   return true;
 }
 
-/**
- * Create for each NumaMemoryResource exactly one jemalloc arena.
- * We then use Extent Hooks, to customize the (de)allocation behavior,
- * where we use libnuma to allocate on a specific node.
- */
+NodeID get_node_id_of_arena(ArenaID arena_id) {
+  std::shared_lock<std::shared_mutex> lock(node_id_for_arena_id_mutex);
+
+  DebugAssert(node_id_for_arena_id.contains(arena_id), "Tried allocation for arena without numa node assignment.");
+
+  return node_id_for_arena_id[arena_id];
+}
+
+void store_node_id_for_arena(ArenaID arena_id, NodeID node_id) {
+  std::unique_lock<std::shared_mutex> lock(node_id_for_arena_id_mutex);
+
+  if (node_id_for_arena_id.contains(arena_id)) {
+    Fail("Tried to assign node id to an already assigned arena id.");
+  }
+
+  node_id_for_arena_id[arena_id] = node_id;
+}
+
+}  // namespace numa_extent_hooks
+
 NumaMemoryResource::NumaMemoryResource(const NodeID node_id) : _node_id(node_id) {
   // Setup arena.
   auto arena_id = uint32_t{0};
@@ -63,12 +64,12 @@ NumaMemoryResource::NumaMemoryResource(const NodeID node_id) : _node_id(node_id)
   Assert(mallctl("arenas.create", static_cast<void*>(&arena_id), &size, nullptr, 0) == 0, "mallctl failed");
 
   // Add extent hooks to arena.
-  _hooks.alloc = NumaExtentHooks::alloc;
+  _hooks.alloc = numa_extent_hooks::alloc;
 
   auto hooks_ptr = &_hooks;
   char command[64];
   snprintf(command, sizeof(command), "arena.%u.extent_hooks", arena_id);
-  NumaExtentHooks::store_node_id_for_arena(arena_id, _node_id);
+  numa_extent_hooks::store_node_id_for_arena(arena_id, _node_id);
   Assert(mallctl(command, nullptr, nullptr, static_cast<void*>(&hooks_ptr), sizeof(extent_hooks_t*)) == 0,
          "mallctl failed");
   _allocation_flags = MALLOCX_ARENA(arena_id) | MALLOCX_TCACHE_NONE;
--- src/lib/memory/numa_memory_resource.hpp
+++ src/lib/memory/numa_memory_resource.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <unordered_map>
-
 #include <jemalloc/jemalloc.h>
 #include <boost/container/pmr/memory_resource.hpp>
 
@@ -11,29 +9,41 @@ namespace hyrise {
 
 using ArenaID = uint32_t;
 
-class NumaExtentHooks {
- public:
-  static void* alloc(extent_hooks_t* extent_hooks, void* new_addr, size_t size, size_t alignment, bool* zero,
-                     bool* commit, unsigned arena_index);
-  static bool dalloc(extent_hooks_t* extent_hooks, void* addr, size_t size, bool committed, unsigned arena_ind);
+namespace numa_extent_hooks {
 
-  static void store_node_id_for_arena(ArenaID, NodeID);
+/**
+ * Allocates bytes on a specific node.
+ *
+ * Similar to the behavior, that libnuma employs for numa_alloc_onnode(..).
+ * First, allocate memory with mmap and then use numa_tonode_memory(..) to move
+ * the allocated memory to a specific node.
+ */
+void* alloc(extent_hooks_t* extent_hooks, void* new_addr, size_t size, size_t alignment, bool* zero, bool* commit,
+            unsigned arena_index);
+bool dalloc(extent_hooks_t* extent_hooks, void* addr, size_t size, bool committed, unsigned arena_ind);
 
- private:
-  static std::unordered_map<ArenaID, NodeID> node_id_for_arena_id;
-};
+NodeID get_node_id_of_arena(ArenaID arena_index);
+void store_node_id_for_arena(ArenaID arena_id, NodeID node_id);
+
+};  // namespace numa_extent_hooks
 
 class NumaMemoryResource : public boost::container::pmr::memory_resource {
  public:
+
+  /**
+   * Create for each NumaMemoryResource exactly one jemalloc arena.
+   * We then use Extent Hooks, to customize the (de)allocation behavior,
+   * where we use libnuma to allocate on a specific node.
+   */
   explicit NumaMemoryResource(const NodeID node_id);
   void* do_allocate(std::size_t bytes, std::size_t alignment) override;
   void do_deallocate(void* p, std::size_t bytes, std::size_t alignment) override;
   bool do_is_equal(const memory_resource& other) const noexcept override;
 
  protected:
-  NodeID _node_id{0};
-  extent_hooks_t _hooks{NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL};
-  int32_t _allocation_flags{0};
+  NodeID _node_id;
+  extent_hooks_t _hooks{};
+  int32_t _allocation_flags;
 };
 
 }  // namespace hyrise
