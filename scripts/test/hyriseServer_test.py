14d26c5d7e82d4fdee6a8980e183248bbe903abf
dey4ss <27929897+dey4ss@users.noreply.github.com>
2023-08-26 11:16:14 +0200

    next timeout

--- scripts/test/hyriseServer_test.py
+++ scripts/test/hyriseServer_test.py
@@ -53,6 +53,7 @@ def main():
     # 30s is a test. We found large servers, such as the CI server, to take a considerable amount of time to boot up
     # and shut down (e.g., the used CI server in 2023 has 128 threads, thus, 128 worker threads).
     server.delayafterterminate = 30  # seconds
+    server.delayafterclose = 30  # seconds
     server.close()
 
 
