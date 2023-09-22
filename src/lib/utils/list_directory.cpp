7d0561782f477cde3e9f3c1b569e1424405abf73
Bouncner <martin+github@boissier.de>
2023-09-22 23:30:02 +0200

    Update list_directory.cpp

--- src/lib/utils/list_directory.cpp
+++ src/lib/utils/list_directory.cpp
@@ -7,7 +7,7 @@
 namespace hyrise {
 
 std::vector<std::filesystem::path> list_directory(const std::string& directory) {
-  std::vector<std::filesystem::path> files;
+  auto files = std::vector<std::filesystem::path>{};
 
   for (const auto& directory_entry : std::filesystem::recursive_directory_iterator(directory)) {
     if (!std::filesystem::is_regular_file(directory_entry)) {
