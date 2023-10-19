216eef657cd56df840d93ad34707a283944ff3bd
Niklas Riekenbrauck <nikriek@gmail.com>
2023-10-19 10:17:31 +0200

    Apply some code comments

--- src/lib/storage/buffer/page_id.hpp
+++ src/lib/storage/buffer/page_id.hpp
@@ -1,6 +1,6 @@
 #pragma once
-#include <magic_enum.hpp>
-#include "boost/integer/static_log2.hpp"
+
+#include "magic_enum.hpp"
 
 namespace hyrise {
 
@@ -20,43 +20,57 @@ constexpr inline size_t bytes_for_size_type(const PageSizeType size) {
 constexpr size_t NUM_PAGE_SIZE_TYPES = magic_enum::enum_count<PageSizeType>();
 constexpr PageSizeType MIN_PAGE_SIZE_TYPE = magic_enum::enum_value<PageSizeType>(0);
 constexpr PageSizeType MAX_PAGE_SIZE_TYPE = magic_enum::enum_value<PageSizeType>(NUM_PAGE_SIZE_TYPES - 1);
-constexpr size_t PAGE_SIZE_TYPE_BITS = boost::static_log2<NUM_PAGE_SIZE_TYPES>::value + 1;
+constexpr size_t PAGE_SIZE_TYPE_BITS = std::bit_width(NUM_PAGE_SIZE_TYPES);
 
 /**
  * PageIDs are used for addressing pages. They consist of a valid flag, a PageSizeType and an index. A Page ID can be unambiguously
  * converted into a virtual address and back. If the valid flag is set, the page is stored in the buffer pool. Otherwise, the page
  * does not exist or the virtual memory address is outside of the buffer pool.
+ * 
+ * For the implementation, we use C++ bitfields to compress multiple fields into a single 64-bit values. The valid flag is stored in the 
+ * least significant bit. The PageSizeType is stored in the next PAGE_SIZE_TYPE_BITS bits. The index is stored in the remaining bits.
 */
 struct PageID {
   using PageIDType = uint64_t;
 
-  PageIDType _valid : 1;
-  PageIDType _size_type : PAGE_SIZE_TYPE_BITS;
-  PageIDType index : (sizeof(PageIDType) * CHAR_BIT - PAGE_SIZE_TYPE_BITS - 1);
+  PageID() = default;
+
+  constexpr PageID(const PageSizeType size_type, const PageIDType index, bool valid = true)
+      : _valid(valid), _size_type(static_cast<PageIDType>(size_type)), _index(index) {}
 
   PageSizeType size_type() const {
-    return magic_enum::enum_value<PageSizeType>(this->_size_type);
+    return magic_enum::enum_value<PageSizeType>(_size_type);
   }
 
-  size_t num_bytes() const {
+  size_t byte_count() const {
     return bytes_for_size_type(size_type());
+  }
+
+  PageIDType index() const {
+    return _index;
   }
 
   bool valid() const {
     return _valid;
   }
 
-  PageID() = default;
+  bool operator==(const PageID& other) const {
+    // return _valid == other._valid && _size_type == other._size_type && _index == other._index;
+  }
 
-  constexpr PageID(const PageSizeType size_type, const PageIDType index, bool valid = true)
-      : _valid(valid), _size_type(static_cast<PageIDType>(size_type)), index(index) {}
+ private:
+  PageIDType _valid : 1;
+  PageIDType _size_type : PAGE_SIZE_TYPE_BITS;
+  PageIDType _index : sizeof(PageIDType) * std::numeric_limits<unsigned char>::digits - PAGE_SIZE_TYPE_BITS - 1;
 };
 
 inline std::ostream& operator<<(std::ostream& os, const PageID& page_id) {
   os << "PageID(valid = " << page_id.valid() << ", size_type = " << magic_enum::enum_name(page_id.size_type())
-     << ", index = " << page_id.index << ")";
+     << ", index = " << page_id.index() << ")";
   return os;
 }
 
+// The invalid page id is used to indicate that a page id is not part of a buffer pool. The valid flag is set to false. All other values are ignored.
 static constexpr PageID INVALID_PAGE_ID = PageID{MIN_PAGE_SIZE_TYPE, 0, false};
+
 }  // namespace hyrise
--- src/test/lib/storage/buffer/page_id_test.cpp
+++ src/test/lib/storage/buffer/page_id_test.cpp
@@ -8,15 +8,15 @@ class PageIDTest : public BaseTest {};
 TEST_F(PageIDTest, TestPageIDForDifferentSizeTypes) {
   const auto small_page = PageID{PageSizeType::KiB16, 29, true};
   EXPECT_EQ(small_page.size_type(), PageSizeType::KiB16);
-  EXPECT_EQ(small_page.num_bytes(), 16384);
+  EXPECT_EQ(small_page.byte_count(), 16384);
   EXPECT_TRUE(small_page.valid());
-  EXPECT_EQ(small_page.index, 29);
+  EXPECT_EQ(small_page.index(), 29);
 
   const auto larger_page = PageID{PageSizeType::KiB256, 59, true};
   EXPECT_EQ(larger_page.size_type(), PageSizeType::KiB256);
-  EXPECT_EQ(larger_page.num_bytes(), 262144);
+  EXPECT_EQ(larger_page.byte_count(), 262144);
   EXPECT_TRUE(larger_page.valid());
-  EXPECT_EQ(larger_page.index, 59);
+  EXPECT_EQ(larger_page.index(), 59);
 }
 
 TEST_F(PageIDTest, TestInvalidPageID) {
