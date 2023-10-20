47a1944b50defcd215df28f5d0e956bbd5a0deeb
Niklas Riekenbrauck <nikriek@gmail.com>
2023-10-20 12:57:26 +0200

    Add more comments

--- src/test/lib/storage/buffer/persistence_manager_test.cpp
+++ src/test/lib/storage/buffer/persistence_manager_test.cpp
@@ -27,44 +27,44 @@ class PersistenceManagerTest : public BaseTest {
 };
 
 TEST_F(PersistenceManagerTest, TestWriteAndReadPagesOnRegularFile) {
+  // Check that one file is created per page size type
   const auto files = list_directory(db_path);
-
   EXPECT_EQ(files.size(), NUM_PAGE_SIZE_TYPES) << "Expected one file per page size type";
   EXPECT_EQ(persistence_manager->mode(), PersistenceManager::Mode::FILE_PER_SIZE_TYPE);
 
+  // Write out 3 different pages
   auto write_pages = std::map<PageID, Page>{{PageID{PageSizeType::KiB16, 20}, Page{{std::byte{0x11}}}},
                                             {PageID{PageSizeType::KiB32, 20}, Page{{std::byte{0x22}}}},
                                             {PageID{PageSizeType::KiB16, 13}, Page{{std::byte{0x33}}}}};
   EXPECT_EQ(write_pages.size(), 3);
-
   for (auto& [page_id, page] : write_pages) {
     // Copy over the first byte to the whole page
     std::memset(page.data.data(), std::to_integer<int>(*page.data.data()), page_id.num_bytes());
     persistence_manager->write_page(page_id, page.data.data());
   }
 
+  // Create buffers to read into to check if reading works after writing to the same page id
   auto read_pages = std::map<PageID, Page>{{PageID{PageSizeType::KiB16, 20}, Page{{}}},
                                            {PageID{PageSizeType::KiB32, 20}, Page{{}}},
                                            {PageID{PageSizeType::KiB16, 13}, Page{{}}}};
   EXPECT_EQ(read_pages.size(), 3);
-
   for (auto& [page_id, page] : write_pages) {
     EXPECT_NE(std::memcmp(page.data.data(), read_pages[page_id].data.data(), page_id.num_bytes()), 0);
   }
-
   for (auto& [page_id, page] : read_pages) {
     persistence_manager->read_page(page_id, page.data.data());
   }
-
   for (auto& [page_id, page] : write_pages) {
     EXPECT_EQ(std::memcmp(page.data.data(), read_pages[page_id].data.data(), page_id.num_bytes()), 0);
   }
 
+  // Verifiy that the same amount of bytes was written and read
   EXPECT_EQ(persistence_manager->total_bytes_written(),
             2 * bytes_for_size_type(PageSizeType::KiB16) + bytes_for_size_type(PageSizeType::KiB32));
   EXPECT_EQ(persistence_manager->total_bytes_read(),
             2 * bytes_for_size_type(PageSizeType::KiB16) + bytes_for_size_type(PageSizeType::KiB32));
 
+  // Check the the buffer manager files are deleted after the persistence manager is destroyed
   persistence_manager = nullptr;
   const auto files_after_cleanup = list_directory(db_path);
   EXPECT_EQ(files_after_cleanup.size(), 0);
