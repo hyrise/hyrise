6c3983d194b75f3e98449ea9e963329e47d4676d
j-hellenberg <47941549+j-hellenberg@users.noreply.github.com>
2023-11-02 20:36:44 +0100

    [DYOD] Team 5 - Sprint4 (#2578)
    Co-authored-by: Daniel Lindner <27929897+dey4ss@users.noreply.github.com>
    Co-authored-by: Bouncner <martin+github@boissier.de>

--- src/test/lib/scheduler/operator_task_test.cpp
+++ src/test/lib/scheduler/operator_task_test.cpp
@@ -242,4 +242,30 @@ TEST_F(OperatorTaskTest, DetectCycles) {
   }
 }
 
+TEST_F(OperatorTaskTest, SkipOperatorTask) {
+  const auto table = std::make_shared<GetTable>("table_a");
+  table->execute();
+
+  const auto task = std::make_shared<OperatorTask>(table);
+  task->skip_operator_task();
+  EXPECT_TRUE(task->is_done());
+}
+
+TEST_F(OperatorTaskTest, NotExecutedOperatorTaskCannotBeSkipped) {
+  const auto table = std::make_shared<GetTable>("table_a");
+
+  const auto task = std::make_shared<OperatorTask>(table);
+  EXPECT_THROW(task->skip_operator_task(), std::logic_error);
+}
+
+TEST_F(OperatorTaskTest, DoNotSkipOperatorTaskWithMultiOwners) {
+  const auto table = std::make_shared<GetTable>("table_a");
+  table->execute();
+
+  const auto task = std::make_shared<OperatorTask>(table);
+  const auto another_task_pointer = task;
+  EXPECT_EQ(task.use_count(), 2);
+  EXPECT_THROW(task->skip_operator_task(), std::logic_error);
+}
+
 }  // namespace hyrise
