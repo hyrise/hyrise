eed573584f998c13cd1f28a5b3c69f05231f6402
Martin Boissier <martin.boissier@hpi.de>
2024-01-18 07:30:01 +0100

    Cleaning up

--- src/lib/utils/threading_utils.cpp
+++ src/lib/utils/threading_utils.cpp
@@ -5,16 +5,7 @@
 #include <sched.h>
 #endif
 
-#include <bitset>
-
 #include "types.hpp"
-
-namespace {
-
-#if HYRISE_NUMA_SUPPORT
-#endif
-
-}  // namespace
 
 namespace hyrise {
 
@@ -23,24 +14,6 @@ void SetThreadAffinity(const CpuID cpu_id) {
   auto cpuset = cpu_set_t{};
   CPU_ZERO(&cpuset);
   CPU_SET(cpu_id, &cpuset);
-  //std::cout << "mask: " << std::bitset<64>(cpuset) << std::endl;
-  const auto return_code = pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset);
-  Assert(return_code == 0, "Error calling pthread_setaffinity_np (return code: " + std::to_string(return_code) + ").");
-#endif
-}
-
-void UnsetThreadAffinity() {
-#if HYRISE_NUMA_SUPPORT
-  auto cpuset = cpu_set_t{};
-  /*
-  auto a = int64_t{1 << 32};
-  --a;
-  std::cout << "mask: " << std::bitset<64>(static_cast<int64_t>(a)) << std::endl;
-  auto b = static_cast<cpu_set_t>(a);
-  */
-  for (int i = 0; i < 1'024; ++i) {
-    CPU_SET(i, &cpuset);
-  }
   const auto return_code = pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset);
   Assert(return_code == 0, "Error calling pthread_setaffinity_np (return code: " + std::to_string(return_code) + ").");
 #endif
