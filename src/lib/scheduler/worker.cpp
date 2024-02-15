56d9c91990cc4bd2a50ec182c1da427ba254faa6
Martin Boissier <martin.boissier@hpi.de>
2024-02-14 22:48:16 -0800

    Fix

--- src/lib/scheduler/worker.cpp
+++ src/lib/scheduler/worker.cpp
@@ -234,7 +234,7 @@ void Worker::_set_affinity() {
   CPU_ZERO(&cpuset);
   CPU_SET(_cpu_id, &cpuset);
   const auto return_code = pthread_setaffinity_np(pthread_self(), sizeof(cpu_set_t), &cpuset);
-  Assert(return_code == 0, "Error calling pthread_setaffinity_np (return code: " + std::to_string{return_code} + ").");
+  Assert(return_code == 0, "Error calling pthread_setaffinity_np (return code: " + std::to_string(return_code) + ").");
 #endif
 }
 
