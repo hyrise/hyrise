f99f92ab8c3230787fed9043e333dd301e47ec1f
Robert Richter <robert.richter03@sap.com>
2024-02-22 17:16:29 +0100

    Comments in shell.nix

--- shell.nix
+++ shell.nix
@@ -1,3 +1,16 @@
+#
+# Nix Setup for Hyrise
+#
+# First install Nix (see https://nixos.org/manual/nix/stable/installation/).
+# Then run 
+#     nix-shell    
+# in the root of the repository to open a shell that is enabled to compile
+# Hyrise. Even though the setup might work with other systems, the 
+# following are recommended:
+# - x86_64 Linux
+# - x86_64 MacOS
+# - ARM MacOS
+
 let
   nixPkgsUrl = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
   pkgs = import nixPkgsUrl { config = {}; overlays = []; };
