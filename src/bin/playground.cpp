bea6e3a28f1b065d76de0188bb063e94da45fdc1
Bouncner <martin+github@boissier.de>
2024-03-12 23:36:34 +0100

    Finalize test

--- src/bin/playground.cpp
+++ src/bin/playground.cpp
@@ -1,29 +1,11 @@
 #include <iostream>
 
-#include <jemalloc/jemalloc.h>
-
 #include "types.hpp"
 
 using namespace hyrise;  // NOLINT(build/namespaces)
 
 int main() {
-  uint64_t epoch = 1;
-  size_t  sz = sizeof(epoch);
-  mallctl("epoch", &epoch, &sz, &epoch, sz);
-
-  const char *version_str;
-  size_t version_size = sizeof(version_str);
-  auto c0 = mallctl("version", &version_str, &version_size, nullptr, 0);
-  std::cout << c0 << " - version: " << version_str << std::endl;
-
-  size_t bgt;
-  sz = sizeof(bgt);
-  auto c00 = mallctl("max_background_threads", &bgt, &sz, nullptr, 0);
-  std::cout << c00 << " - background threads: " << bgt << std::endl;
-
-  size_t ovth;
-  auto c1 = mallctl("opt.oversize_threshold", &ovth, &sz, nullptr, 0);
-  std::cout << c1 << " - oversize threshold: " << ovth << std::endl;
-
+  const auto world = pmr_string{"world"};
+  std::cout << "Hello " << world << "!\n";
   return 0;
 }
--- src/test/lib/memory/jemalloc_test.cpp
+++ src/test/lib/memory/jemalloc_test.cpp
@@ -1,6 +1,7 @@
 #include <limits>
 #include <string>
 #include <thread>
+#include <type_traits>
 #include <utility>
 #include <variant>
 #include <vector>
@@ -15,10 +16,10 @@ namespace hyrise {
 
 class JemallocTest : public BaseTest {};
 
-
 /**
- * We test for the exact version of jemalloc. We found that performance can vary significantly between jemalloc 
- * versions (see https://github.com/hyrise/hyrise/discussions/2628). This test is supposed to motivate re-evaluating
+ * We test for the exact version of jemalloc and the used configuration. We found that performance can vary
+ * significantly between jemallo versions (see https://github.com/hyrise/hyrise/discussions/2628). This test is
+& * supposed to motivate re-evaluating
  * the configuration of jemalloc. When updating jemalloc, please evaluate multiple CPU architectures using
  * `benchmark_all.sh`. For jemalloc v5.3, we use the option `oversize_threshold:0`. This might be no longer necessary
  * with upcoming releases (see https://github.com/jemalloc/jemalloc/issues/2495).
@@ -28,54 +29,43 @@ TEST_F(JemallocTest, ConfigurationSettings) {
   GTEST_SKIP();
 #endif
 
-  auto expected_settings = std::vector<std::pair<std::string, std::variant<bool, size_t, unsigned, std::string>>>{{"opt.oversize_threshold", size_t{0}},
-                                                                                                  {"opt.dirty_decay_ms", size_t{20'000}},
-                                                                                                  {"opt.dirty_decay_ms", size_t{20'000}},
-                                                                                                  {"opt.narenas", unsigned{std::thread::hardware_concurrency()}},
-                                                                                                  {"opt.background_thread", true},
-                                                                                                  {"version", std::string{"5.3.0-0-g54eaed1d8b56b1aa528be3bdd1877e59c56fa90c"}},
-{"opt.percpu_arena", std::string{"percpu"}},
-{"opt.metadata_thp", std::string{"auto"}}};
-  
-    //set(JEMALLOC_CONFIG "--with-malloc-conf=\"oversize_threshold:0,background_thread:true,percpu_arena:percpu,metadata_thp:auto,muzzy_decay_ms:20000,dirty_decay_ms:20000\"")
+  const auto jemalloc_version = std::string{"5.3.0-0-g54eaed1d8b56b1aa528be3bdd1877e59c56fa90c"};
+  const auto percpu_setting = std::string{"percu"};
+  const auto metadata_thp_setting = std::string{"auto"};
+  auto expected_settings = std::vector<std::pair<std::string, std::variant<bool, size_t, unsigned, const char*>>>{
+      {"opt.oversize_threshold", size_t{0}},
+      {"opt.dirty_decay_ms", size_t{20'000}},
+      {"opt.dirty_decay_ms", size_t{20'000}},
+      {"opt.narenas", unsigned{std::thread::hardware_concurrency()}},
+      {"opt.background_thread", true},
+      {"version", jemalloc_version.data()},
+      {"opt.percpu_arena", percpu_setting.data()},
+      {"opt.metadata_thp", metadata_thp_setting.data()}};
+
   for (const auto& [setting_name, setting_value] : expected_settings) {
-    std::cerr << setting_name << std::endl;
-    if (std::holds_alternative<size_t>(setting_value)) {
-      auto setting = std::numeric_limits<size_t>::max();
-      auto setting_size = sizeof(setting);
-      const auto return_code = mallctl(setting_name.c_str(), &setting, &setting_size, nullptr, 0);
-      EXPECT_EQ(return_code, 0);
-      EXPECT_EQ(setting, std::get<size_t>(setting_value));
-    } else if (std::holds_alternative<unsigned>(setting_value)) {
-      auto setting = std::numeric_limits<unsigned>::max();
-      auto setting_size = sizeof(setting);
-      const auto return_code = mallctl(setting_name.c_str(), &setting, &setting_size, nullptr, 0);
-      EXPECT_EQ(return_code, 0);
-      EXPECT_EQ(setting, std::get<unsigned>(setting_value));
-    } else if (std::holds_alternative<std::string>(setting_value)) {
-      const char* str_setting;
-      auto str_setting_size = sizeof(str_setting);
-      const auto return_code = mallctl(setting_name.c_str(), &str_setting, &str_setting_size, nullptr, 0);
-      EXPECT_EQ(return_code, 0);
-      EXPECT_EQ(str_setting, std::get<std::string>(setting_value));
-    } else {
-      Assert(std::holds_alternative<bool>(setting_value), "Expected bool value.");
+    std::visit(
+        [setting_name = setting_name, setting_value = setting_value](auto&& argument) {
+          using T = std::decay_t<decltype(argument)>;
+          auto setting = T{};
 
-      auto setting = false;
-      auto setting_size = sizeof(setting);
-      const auto return_code = mallctl(setting_name.c_str(), &setting, &setting_size, nullptr, 0);
-      EXPECT_EQ(return_code, 0);
-      EXPECT_EQ(setting, std::get<bool>(setting_value));
-    }
+          if constexpr (std::is_same_v<T, bool>) {
+            setting = false;
+          } else if constexpr (std::is_integral_v<T>) {
+            setting = std::numeric_limits<T>::max();
+          }
+
+          auto setting_size = sizeof(setting);
+          const auto return_code = mallctl(setting_name.c_str(), &setting, &setting_size, nullptr, 0);
+          EXPECT_EQ(return_code, 0);
+
+          if constexpr (std::is_same_v<T, const char*>) {
+            EXPECT_EQ(*setting, *std::get<T>(setting_value));
+          } else {
+            EXPECT_EQ(setting, std::get<T>(setting_value));
+          }
+        },
+        setting_value);
   }
-
-  auto oversize_threshold_setting = std::numeric_limits<size_t>::max();
-  auto oversize_threshold_setting_size = sizeof(oversize_threshold_setting);
-  const auto return_code_threshold = mallctl("opt.oversize_threshold", &oversize_threshold_setting,
-		                             &oversize_threshold_setting_size, nullptr, 0);
-  EXPECT_EQ(return_code_threshold, 0);
-  EXPECT_EQ(oversize_threshold_setting, 0);
-
 }
 
 }  // namespace hyrise
