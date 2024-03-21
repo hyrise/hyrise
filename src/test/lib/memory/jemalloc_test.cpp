7b659fed3cdf3c4473f5642750ca26ea713926e9
Martin Boissier <martin.boissier@hpi.de>
2024-03-21 13:46:40 -0700

    Fix issue with MacOS and validate simplified test

--- src/test/lib/memory/jemalloc_test.cpp
+++ src/test/lib/memory/jemalloc_test.cpp
@@ -18,29 +18,21 @@ class JemallocTest : public BaseTest {};
 
 /**
  * We test for the exact version of jemalloc and the used configuration. We found that performance can vary
- * significantly between jemallo versions (see https://github.com/hyrise/hyrise/discussions/2628). This test is
-& * supposed to motivate re-evaluating
- * the configuration of jemalloc. When updating jemalloc, please evaluate multiple CPU architectures using
- * `benchmark_all.sh`. For jemalloc v5.3, we use the option `oversize_threshold:0`. This might be no longer necessary
- * with upcoming releases (see https://github.com/jemalloc/jemalloc/issues/2495).
+ * significantly between jemalloc versions (see https://github.com/hyrise/hyrise/discussions/2628). This test is
+ * supposed to motivate re-evaluating the configuration of jemalloc. When updating jemalloc, please evaluate multiple
+ * CPU architectures using `benchmark_all.sh`. For jemalloc v5.3, we use the option `oversize_threshold:0`. This might
+ * be no longer necessary with upcoming releases (see https://github.com/jemalloc/jemalloc/issues/2495).
  */
 TEST_F(JemallocTest, ConfigurationSettings) {
-#ifndef HYRISE_WITH_JEMALLOC
-  GTEST_SKIP();
-#endif
-
+#ifdef HYRISE_WITH_JEMALLOC  // GTEST_SKIP() is not sufficient here, because there is no mallctl() on MacOS.
   const auto jemalloc_version = std::string{"5.3.0-0-g54eaed1d8b56b1aa528be3bdd1877e59c56fa90c"};
-  const auto percpu_setting = std::string{"percu"};
+  // const auto percpu_setting = std::string{};
   const auto metadata_thp_setting = std::string{"auto"};
   auto expected_settings = std::vector<std::pair<std::string, std::variant<bool, size_t, unsigned, const char*>>>{
-      {"opt.oversize_threshold", size_t{0}},
-      {"opt.dirty_decay_ms", size_t{20'000}},
-      {"opt.dirty_decay_ms", size_t{20'000}},
-      {"opt.narenas", unsigned{std::thread::hardware_concurrency()}},
-      {"opt.background_thread", true},
-      {"version", jemalloc_version.data()},
-      {"opt.percpu_arena", percpu_setting.data()},
-      {"opt.metadata_thp", metadata_thp_setting.data()}};
+      {"opt.oversize_threshold", size_t{0}},  {"opt.dirty_decay_ms", size_t{20'000}},
+      {"opt.dirty_decay_ms", size_t{20'000}}, {"opt.narenas", unsigned{std::thread::hardware_concurrency()}},
+      {"opt.background_thread", true},        {"version", jemalloc_version.data()},
+      {"opt.percpu_arena", "percu"},          {"opt.metadata_thp", metadata_thp_setting.data()}};
 
   for (const auto& [setting_name, setting_value] : expected_settings) {
     std::visit(
@@ -66,6 +58,7 @@ TEST_F(JemallocTest, ConfigurationSettings) {
         },
         setting_value);
   }
+#endif
 }
 
 }  // namespace hyrise
