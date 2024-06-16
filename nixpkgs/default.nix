8ca9950661429362779b1e42b8400be566a0c6ca
Robert Richter <robert.richter@rrcomtech.com>
2024-06-16 12:29:11 +0200

    Remove import of leftover artifact

--- nixpkgs/default.nix
+++ nixpkgs/default.nix
@@ -1,7 +1,13 @@
+# How to update this?
+#
+# 1. Find the new desired commit hash on github.com/nixos/nixpkgs.
+# 2. Run `nix-shell --pure` in this directory. You will likely run into an error.
+#    The error will contain a sha256 hash that fetchTarball requires.
+# 3. Take the commit hash from the error and copy it here. -> Done.
+
 {}:
 
 let
-  hashes = import ./hashes.nix;
   fetchCommit = commit: fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${commit}.tar.gz";
     sha256 = "0043xxvs81chfbn01irfj81pj8j9nzwnv4srmaqy38y3bzacv6ra";
