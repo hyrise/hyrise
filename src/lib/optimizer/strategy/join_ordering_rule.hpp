a8570ebe32f549ce3a95e21aeb046a139621940c
dey4ss <27929897+dey4ss@users.noreply.github.com>
2024-06-22 21:55:05 +0900

    GOO for 7 vertices

--- src/lib/optimizer/strategy/join_ordering_rule.hpp
+++ src/lib/optimizer/strategy/join_ordering_rule.hpp
@@ -22,7 +22,7 @@ class JoinOrderingRule : public AbstractRule {
   //                in #2626 and #2642, we see two main bottlenecks in cardinality estimation:
   //     (i) Scaling histograms because we do it very often (thousands of times for some TPC-DS and JOB queries).
   //    (ii) Creating new histograms for joined tables.
-  constexpr static auto MIN_VERTICES_FOR_HEURISTIC = size_t{8};
+  constexpr static auto MIN_VERTICES_FOR_HEURISTIC = size_t{7};
 
  protected:
   void _apply_to_plan_without_subqueries(const std::shared_ptr<AbstractLQPNode>& lqp_root) const override;
