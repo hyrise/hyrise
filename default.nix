cbd18c86f2f294fded8db7e01980ce8efaffa227
Robert Richter <robert.richter@rrcomtech.com>
2024-07-09 19:27:18 +0200

    Adding LD_LIBRARY_PATH

--- default.nix
+++ default.nix
@@ -17,14 +17,17 @@ let
   pkgs = import ./nixpkgs {};
 in
 
-pkgs.stdenv.mkDerivation {
+pkgs.mkShell {
   name = "hyrise";
   version = "0.0.1";
 
   # nativeBuildInputs vs. buildInputs
   # - nativeBuildInputs are packages required during the runtime of the shell process.
-  # - buildInputs are packages required during the build of the shell process, but not anymore during the shell's runtime.
-  # As this is intended for usage with nix-shell (i.e., there are no build steps in this Nix derivation and users will execute processes only after the shell is already available), only nativeBuildInputs are needed.
+  # - buildInputs are packages required during the build of the shell process, but not
+  # anymore during the shell's runtime. As this is intended for usage with nix-shell
+  # (i.e., there are no build steps in this Nix derivation and users will execute
+  # processes only after the shell is already available), only nativeBuildInputs are
+  # needed.
   nativeBuildInputs = with pkgs; [
     clang
     autoconf
@@ -49,4 +52,19 @@ pkgs.stdenv.mkDerivation {
     "fortify"
     "fortify3"
   ];
+
+  # Running Hyrise executables requires the LD_LIBRARY_PATH variable set. This does not
+  # happen by other hooks, therefore it happens manually here. Each of the derivations
+  # listed here provides a dynamically linked library.
+  # View the results by running `nix-shell --pure --run "echo $LD_LIBRARY_PATH"`.
+  shellHook = ''
+	export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [
+		pkgs.lld
+		pkgs.sqlite
+		pkgs.tbb_2021_11
+		pkgs.boost
+		pkgs.libgccjit
+		pkgs.stdenv.cc.cc.lib
+	]}:$LD_LIBRARY_PATH"
+  '';
 }
