e75435988b35ae8817b8d499c59bf2f15bc2181d
Martin Boissier <martin.boissier@hpi.de>
2024-08-21 17:22:53 +0200

    Playground for deallocation

--- src/bin/playground.cpp
+++ src/bin/playground.cpp
@@ -6,11 +6,49 @@
 
 using namespace hyrise;  // NOLINT(build/namespaces)
 
+struct Task {
+  Task(int init_id) : id{init_id} {
+    std::cout << i << " constructed\n";
+  }
+
+  Task(const Task& copy_task) {
+    id = copy_task.id;
+    successor = copy_task.successor;
+  }
+
+  ~Task() {
+    std::cout << i << " destructed\n";
+    successor.reset();
+  }
+
+  void set_successor(const std::shared_ptr<Task>& init_successor) {
+    successor = init_successor;
+  }
+
+  int id{};
+  std::shared_ptr<Task> successor;
+};
+
 int main() {
-  std::vector<std::reference_wrapper<const std::shared_ptr<AbstractTask>>> _successors;
+  auto element_count = 40;
+  auto tasks = std::vector<std::shared_ptr<Task>>{};
 
-  const std::shared_ptr<AbstractTask> task = std::make_shared<JobTask>([]() { return 0; });
+  for (auto i = 0; i < element_count; ++i) {
+    tasks.emplace_back(std::make_shared<Task>(i));
+  }
 
-  _successors.emplace_back(std::ref(task));
+  // for (auto i = 0; i < element_count / 4; ++i) {
+  //   tasks[i]->set_successor(tasks[element_count-i-1]);
+  // }
+  
+  for (auto i = 1; i < element_count; ++i) {
+    if (i % 10 == 0) {
+      continue;
+    }
+    tasks[i-1]->set_successor(tasks[i]);
+  }
+
+  std::cerr << "\n\nDone\n\n";
+
   return 0;
 }
