88501eaf5ae6178e61c985dedc6167e8541f94c8
Martin Boissier <martin.boissier@hpi.de>
2024-10-02 11:16:04 +0200

    auto auto auto

--- src/test/lib/all_parameter_variant_test.cpp
+++ src/test/lib/all_parameter_variant_test.cpp
@@ -11,37 +11,37 @@ class AllParameterVariantTest : public BaseTest {};
 
 TEST_F(AllParameterVariantTest, GetCurrentType) {
   {
-    AllParameterVariant parameter(ColumnID{0});
+    const auto parameter = AllParameterVariant{ColumnID{0}};
     EXPECT_EQ(parameter.type(), typeid(ColumnID));
     EXPECT_NE(parameter.type(), typeid(AllTypeVariant));
   }
   {
-    AllParameterVariant parameter("string");
+    const auto parameter = AllParameterVariant{"string"};
     EXPECT_NE(parameter.type(), typeid(ColumnID));
     EXPECT_EQ(parameter.type(), typeid(AllTypeVariant));
   }
   {
-    AllParameterVariant parameter(true);
+    const auto parameter = AllParameterVariant{true};
     EXPECT_NE(parameter.type(), typeid(ColumnID));
     EXPECT_EQ(parameter.type(), typeid(AllTypeVariant));
   }
   {
-    AllParameterVariant parameter(static_cast<int32_t>(123));
+    const auto parameter = AllParameterVariant{static_cast<int32_t>(123)};
     EXPECT_NE(parameter.type(), typeid(ColumnID));
     EXPECT_EQ(parameter.type(), typeid(AllTypeVariant));
   }
   {
-    AllParameterVariant parameter(static_cast<int64_t>(123456789l));
+    const auto parameter = AllParameterVariant{static_cast<int64_t>(123456789l)};
     EXPECT_NE(parameter.type(), typeid(ColumnID));
     EXPECT_EQ(parameter.type(), typeid(AllTypeVariant));
   }
   {
-    AllParameterVariant parameter(123.4f);
+    const auto parameter = AllParameterVariant{123.4f};
     EXPECT_NE(parameter.type(), typeid(ColumnID));
     EXPECT_EQ(parameter.type(), typeid(AllTypeVariant));
   }
   {
-    AllParameterVariant parameter(123.4);
+    const auto parameter = AllParameterVariant{123.4};
     EXPECT_NE(parameter.type(), typeid(ColumnID));
     EXPECT_EQ(parameter.type(), typeid(AllTypeVariant));
   }
@@ -49,52 +49,52 @@ TEST_F(AllParameterVariantTest, GetCurrentType) {
 
 TEST_F(AllParameterVariantTest, GetCurrentValue) {
   {
-    AllParameterVariant parameter(ColumnID{0});
+    const auto parameter = AllParameterVariant{ColumnID{0}};
     EXPECT_EQ(static_cast<uint16_t>(boost::get<ColumnID>(parameter)), static_cast<uint16_t>(0u));
   }
   {
-    AllParameterVariant parameter("string");
-    auto value = boost::get<pmr_string>(boost::get<AllTypeVariant>(parameter));
+    const auto parameter = AllParameterVariant{"string"};
+    const auto value = boost::get<pmr_string>(boost::get<AllTypeVariant>(parameter));
     EXPECT_EQ(value, "string");
   }
   {
-    AllParameterVariant parameter(static_cast<int32_t>(123));
-    auto value = boost::get<int32_t>(boost::get<AllTypeVariant>(parameter));
+    const auto parameter = AllParameterVariant{static_cast<int32_t>(123)};
+    const auto value = boost::get<int32_t>(boost::get<AllTypeVariant>(parameter));
     EXPECT_EQ(value, static_cast<int32_t>(123));
   }
   {
-    AllParameterVariant parameter(static_cast<int64_t>(123456789l));
-    auto value = boost::get<int64_t>(boost::get<AllTypeVariant>(parameter));
+    const auto parameter = AllParameterVariant{static_cast<int64_t>(123456789l)};
+    const auto value = boost::get<int64_t>(boost::get<AllTypeVariant>(parameter));
     EXPECT_EQ(value, static_cast<int64_t>(123456789l));
   }
   {
-    AllParameterVariant parameter(123.4f);
-    auto value = boost::get<float>(boost::get<AllTypeVariant>(parameter));
+    const auto parameter = AllParameterVariant{123.4f};
+    const auto value = boost::get<float>(boost::get<AllTypeVariant>(parameter));
     EXPECT_EQ(value, 123.4f);
   }
   {
-    AllParameterVariant parameter(123.4);
-    auto value = boost::get<double>(boost::get<AllTypeVariant>(parameter));
+    const auto parameter = AllParameterVariant{123.4};
+    const auto value = boost::get<double>(boost::get<AllTypeVariant>(parameter));
     EXPECT_EQ(value, 123.4);
   }
 }
 
 TEST_F(AllParameterVariantTest, OutputToStream) {
   {
-    const AllParameterVariant parameter(ParameterID{17});
-    std::ostringstream stream;
+    const auto parameter = AllParameterVariant{ParameterID{17}};
+    auto stream = std::ostringstream{};
     stream << parameter;
     EXPECT_EQ(stream.str(), "Placeholder #17");
   }
   {
-    const AllParameterVariant parameter(ColumnID{17});
-    std::ostringstream stream;
+    const auto parameter = AllParameterVariant{ColumnID{17}};
+    auto stream = std::ostringstream{};
     stream << parameter;
     EXPECT_EQ(stream.str(), "Column #17");
   }
   {
-    const AllParameterVariant parameter("string");
-    std::ostringstream stream;
+    const auto parameter = AllParameterVariant{"string"};
+    auto stream = std::ostringstream{};
     stream << parameter;
     EXPECT_EQ(stream.str(), "string");
   }
