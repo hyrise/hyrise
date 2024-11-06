a0401ad913268505882c5bdcb2c3911cee482159
Jan-Eric Hellenberg <janeric.hellenberg@gmail.com>
2024-11-06 12:01:43 +0100

    Run scheduler in pdgf test

--- src/bin/pdgf_test.cpp
+++ src/bin/pdgf_test.cpp
@@ -7,6 +7,7 @@
 #include "utils/assert.hpp"
 #include "utils/format_duration.hpp"
 #include "utils/timer.hpp"
+#include "scheduler/node_queue_scheduler.hpp"
 
 #define SHARED_MEMORY_NAME "/PDGF_SHARED_MEMORY"
 #define DATA_READY_SEM "/PDGF_DATA_READY_SEM"
@@ -15,11 +16,19 @@
 using namespace hyrise;  // NOLINT(build/namespaces)
 
 int main(int argc, char* argv[]) {
-  Assert(argc >= 3, "Expected scale factor and num cores arguments");
+  Assert(argc >= 5, "Expected scale factor and num cores arguments");
   Assert(strcmp(argv[1], "--scale") == 0, "First argument should be --scale");
   auto scale_factor = static_cast<float>(atof(argv[2]));
-  auto num_cores = atoi(argv[3]);
+  Assert(strcmp(argv[3], "--scheduler") == 0, "Third argument should be --scheduler");
+  Assert(strcmp(argv[4], "--data_preparation_cores") == 0, "Fourth argument should be --data_preparation_cores");
+  auto num_cores = atoi(argv[5]);
 
+  // Set up scheduler
+  Hyrise::get().topology.use_default_topology(num_cores);
+  const auto scheduler = std::make_shared<NodeQueueScheduler>(num_cores);
+  Hyrise::get().set_scheduler(scheduler);
+
+  // Run
   auto reader = SharedMemoryReader<128, 16>(Chunk::DEFAULT_SIZE, SHARED_MEMORY_NAME, DATA_READY_SEM, BUFFER_FREE_SEM);
   auto pdgf_schema = PdgfProcess::for_schema_generation(PDGF_DIRECTORY_ROOT, num_cores, scale_factor);
   pdgf_schema.run();
@@ -44,5 +53,9 @@ int main(int argc, char* argv[]) {
   std::cerr << "Loading/Generating tables done (" << format_duration(time) << ")\n";
 
   std::cerr << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n";
+
+  // Teardown
+  scheduler->finish();
+
   return 0;
 }
