e987739f09bce2e3160cb3f4327e4650aa3c4f6a
Tobias Jordan <git@tobiasjordan.io>
2025-01-06 08:10:07 +0000

    quicksave

--- src/lib/storage/split_pos_list_by_chunk_id.cpp
+++ src/lib/storage/split_pos_list_by_chunk_id.cpp
@@ -19,22 +19,25 @@ PosListsByChunkID split_pos_list_by_chunk_id(const std::shared_ptr<const Abstrac
   // shared_ptr<const RowIDPosList>, we first create regular PosLists, add the values to them, and then convert these.
 
   // Create RowIDPosLists and set them as `references_single_chunk`
-  auto pos_lists_by_chunk_id = PosListsByChunkID{number_of_chunks};
+  auto pos_lists_by_chunk_id = PosListsByChunkID{number_of_chunks + 1};
 
-  for (auto chunk_id = ChunkID{0}; chunk_id < number_of_chunks; ++chunk_id) {
-    DebugAssert(chunk_id < number_of_chunks, "Inconsistent number_of_chunks passed");
+  for (auto chunk_id = ChunkID{0}; chunk_id < number_of_chunks + 1; ++chunk_id) {
+    DebugAssert(chunk_id < number_of_chunks + 1, "Inconsistent number_of_chunks + 1 passed");
     auto& mapping = pos_lists_by_chunk_id[chunk_id];
     mapping.row_ids = std::make_shared<RowIDPosList>();
     mapping.row_ids->guarantee_single_chunk();
-    mapping.row_ids->reserve(input_pos_list->size() / number_of_chunks);
-    mapping.original_positions.reserve(input_pos_list->size() / number_of_chunks);
+    mapping.row_ids->reserve(input_pos_list->size() / number_of_chunks + 1);
+    mapping.original_positions.reserve(input_pos_list->size() / number_of_chunks + 1);
   }
 
   // Iterate over the input_pos_list and split the entries by chunk_id
   auto original_position = ChunkOffset{0};
   for (const auto row_id : *input_pos_list) {
     if (row_id.is_null()) {
-      original_position++;
+      auto& mapping = pos_lists_by_chunk_id[number_of_chunks];
+
+      mapping.row_ids->emplace_back(row_id);
+      mapping.original_positions.emplace_back(original_position++);
       continue;
     }
 
