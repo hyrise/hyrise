dcc7f1f9689c3ab469e20b85885ac6f434eefa8d
Robert Weeke <Robert.Weeke@student.hpi.uni-potsdam.de>
2025-02-12 15:49:14 +0100

    use curly braces for member initializer list

--- src/lib/logical_query_plan/import_node.cpp
+++ src/lib/logical_query_plan/import_node.cpp
@@ -19,10 +19,10 @@ namespace hyrise {
 ImportNode::ImportNode(const std::string& init_table_name, const std::string& init_file_name,
                        const FileType init_file_type, const std::optional<EncodingType>& init_table_encoding)
     : AbstractNonQueryNode(LQPNodeType::Import),
-      table_name(init_table_name),
-      file_name(init_file_name),
-      file_type(init_file_type),
-      table_encoding(init_table_encoding) {}
+      table_name{init_table_name},
+      file_name{init_file_name},
+      file_type{init_file_type},
+      table_encoding{init_table_encoding} {}
 
 std::string ImportNode::description(const DescriptionMode /*mode*/) const {
   auto file_type_str = std::string{magic_enum::enum_name(file_type)};
