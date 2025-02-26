e52961b499688a45fdde85d01d59368aa29db929
Tobias Jordan <git@tobiasjordan.io>
2025-02-26 11:34:29 +0100

    .

--- src/test/lib/operators/reduce_test.cpp
+++ src/test/lib/operators/reduce_test.cpp
@@ -35,7 +35,7 @@ class OperatorsReduceTest : public BaseTest {
 
 TEST_F(OperatorsReduceTest, ScanTableWithFilterCreatedOnItself) {
   const auto expected = load_table("resources/test_data/tbl/int_int_shuffled.tbl", ChunkOffset{7});
-  auto reduce = std::make_shared<Reduce>(_int_int, _int_int, OperatorJoinPredicate{ColumnIDPair(ColumnID{0}, ColumnID{0}), PredicateCondition::Equals});
+  auto reduce = std::make_shared<Reduce>(_int_int, _int_int, OperatorJoinPredicate{ColumnIDPair(ColumnID{0}, ColumnID{0}), PredicateCondition::Equals}, false);
   reduce->execute();
   EXPECT_TABLE_EQ_UNORDERED(reduce->get_output(), expected);
 }
