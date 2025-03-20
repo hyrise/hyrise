f3b6d0f700a48d6a0862aa52dfaf3dbb0b77d393
Tobias Jordan <git@tobiasjordan.io>
2025-03-20 08:18:54 +0100

    sync

--- src/lib/operators/reduce.cpp
+++ src/lib/operators/reduce.cpp
@@ -218,6 +218,18 @@ std::shared_ptr<Table> Reduce::_create_reduced_table() {
 
   const auto output_table = std::make_shared<Table>(input_table->column_definitions(), TableType::References, std::move(output_chunks));
   // std::cout << "Output row count: " << output_table->row_count() << std::endl;
+  
+  std::ofstream output_file;
+  output_file.open("reduce_stats.csv", std::ios_base::app);
+
+  if (!std::filesystem::exists("reduce_stats.csv")) {
+        output_file << "benchmark,query,input_count,output_count\n";
+  }
+  outputFile  << Hyrise::get().benchmark_name << ","
+              << Hyrise::get().query_name << ","
+              << input_table->row_count() << ","
+              << output_table->row_count() << "\n";
+
   return output_table;
 }
 
