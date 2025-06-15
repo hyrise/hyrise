7a1e8d4311377d725126a0142882f5f2a0c836db
BeerJamin <lorik.mucolli@gmail.com>
2025-06-15 17:02:00 +0200

    stick to coding guidelines, use auto and remove unnecessary test macro

--- src/test/lib/utils/pausable_loop_thread_test.cpp
+++ src/test/lib/utils/pausable_loop_thread_test.cpp
@@ -13,7 +13,7 @@ class PausableLoopThreadTest : public BaseTest {
 };
 
 TEST_F(PausableLoopThreadTest, BasicExecution) {
-  PausableLoopThread loop(std::chrono::milliseconds(10), [&](size_t) {
+  auto loop = PausableLoopThread(std::chrono::milliseconds(10), [&](size_t) {
     ++call_count;
   });
 
@@ -23,14 +23,14 @@ TEST_F(PausableLoopThreadTest, BasicExecution) {
 }
 
 TEST_F(PausableLoopThreadTest, PauseAndResume) {
-  PausableLoopThread loop(std::chrono::milliseconds(5), [&](size_t) {
+  auto loop = PausableLoopThread(std::chrono::milliseconds(5), [&](size_t) {
     ++call_count;
   });
 
   std::this_thread::sleep_for(std::chrono::milliseconds(20));
   loop.pause();
 
-  auto paused_count = call_count.load();
+  const auto paused_count = call_count.load();
   std::this_thread::sleep_for(std::chrono::milliseconds(30));
   EXPECT_EQ(paused_count, call_count.load());
 
@@ -40,32 +40,30 @@ TEST_F(PausableLoopThreadTest, PauseAndResume) {
 }
 
 TEST_F(PausableLoopThreadTest, DestructorStopsThread) {
-  std::atomic<size_t> temp{0};
+  auto temp = std::atomic<size_t>(0);
   {
-    PausableLoopThread loop(std::chrono::milliseconds(10), [&](size_t) {
+    auto loop = PausableLoopThread(std::chrono::milliseconds(10), [&](size_t) {
       ++temp;
       ++call_count;
     });
     std::this_thread::sleep_for(std::chrono::milliseconds(30));
   }
 
-  auto call_count_after_destruction = call_count.load();
-  auto temp_count_after_destruction = temp.load();
+  const auto call_count_after_destruction = call_count.load();
+  const auto temp_count_after_destruction = temp.load();
 
   std::this_thread::sleep_for(std::chrono::milliseconds(std::chrono::milliseconds(50)));
 
   EXPECT_EQ(call_count.load(), call_count_after_destruction);
   EXPECT_EQ(temp.load(), temp_count_after_destruction);
-
-  SUCCEED();
 }
 
 TEST_F(PausableLoopThreadTest, SetLoopSleepTimeChangesInterval) {
   auto timestamps = std::vector<std::chrono::steady_clock::time_point>();
-  std::mutex timestamps_mutex;
+  auto timestamps_mutex = std::mutex();
 
-  PausableLoopThread loop(std::chrono::milliseconds(50), [&](size_t) {
-    std::lock_guard lock(timestamps_mutex);
+  auto loop = PausableLoopThread(std::chrono::milliseconds(50), [&](size_t) {
+    auto lock = std::lock_guard(timestamps_mutex);
     timestamps.push_back(std::chrono::steady_clock::now());
   });
 
@@ -73,9 +71,9 @@ TEST_F(PausableLoopThreadTest, SetLoopSleepTimeChangesInterval) {
 
   loop.pause();
 
-  size_t initial_timestamp_count;
+  auto initial_timestamp_count = 0;
   {
-    std::lock_guard lock(timestamps_mutex);
+    auto lock = std::lock_guard(timestamps_mutex);
     initial_timestamp_count = timestamps.size();
   }
 
@@ -83,7 +81,7 @@ TEST_F(PausableLoopThreadTest, SetLoopSleepTimeChangesInterval) {
 
   loop.set_loop_sleep_time(std::chrono::milliseconds(10));
   {
-    std::lock_guard lock(timestamps_mutex);
+    auto lock = std::lock_guard(timestamps_mutex);
     EXPECT_EQ(initial_timestamp_count, timestamps.size());
   }
 
@@ -91,7 +89,7 @@ TEST_F(PausableLoopThreadTest, SetLoopSleepTimeChangesInterval) {
 
   std::this_thread::sleep_for(std::chrono::milliseconds(55));
   {
-    std::lock_guard lock(timestamps_mutex);
+    auto lock = std::lock_guard(timestamps_mutex);
     // We expect at least 4-5 new timestamps in this period.
     // Check for more than initial + 3 to be safe.
     EXPECT_GT(timestamps.size(), initial_timestamp_count + 3);
