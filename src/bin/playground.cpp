ace5d90a680ef749be366bfbd951155adeb67dfb
Bouncner <martin+github@boissier.de>
2025-06-16 14:24:36 +0200

    Code for DYOD week 9

--- src/bin/playground.cpp
+++ src/bin/playground.cpp
@@ -1,10 +1,16 @@
 #include <algorithm>
+#include <fstream>
 #include <iostream>
 #include <random>
 
-// This playground only compiles on Linux as we require `perf`.
+// This playground only compiles on Linux as we require Linux's perf and perfetto.
 #include "perfcpp/event_counter.h"
+#include "perfetto.h"
 
+#include "hyrise.hpp"
+#include "scheduler/job_task.hpp"
+#include "scheduler/node_queue_scheduler.hpp"
+#include "storage/chunk.hpp"
 #include "types.hpp"
 #include "utils/assert.hpp"
 
@@ -12,10 +18,21 @@ using namespace hyrise;  // NOLINT(build/namespaces)
 
 constexpr auto STRING_COUNT = size_t{100'000'000};  // Careful: 100M has an RSS of ~30GB.
 
+
+// Define trace categories
+PERFETTO_DEFINE_CATEGORIES(
+    perfetto::Category("Sort")
+        .SetDescription("Benchmark parallel sorting"),
+);
+
+PERFETTO_TRACK_EVENT_STATIC_STORAGE();
+
+
 std::vector<std::string> get_top_by_stdsort(const size_t k, std::vector<std::string>& input) {
   std::ranges::sort(input);
   return std::vector<std::string>(input.begin(), input.begin() + k);
 }
+
 
 std::vector<std::string> get_top_by_indexsort(const size_t k, std::vector<std::string>& input) {
   static_assert(std::numeric_limits<uint32_t>::max() >= STRING_COUNT);
@@ -62,6 +79,44 @@ std::vector<std::string> get_top_by_priorityqueue(const size_t k, std::vector<st
   return result;
 }
 
+
+template <typename Iterator>
+void merge_sort(Iterator first, Iterator last) {
+  TRACE_EVENT("Sort", "MergeSort");
+  if (std::distance(first, last) <= Chunk::DEFAULT_SIZE) {
+    TRACE_EVENT("Sort", "MergeSort::sort");
+    std::sort(first, last);
+    return;
+  }
+
+  auto middle = first + (std::distance(first, last) / 2);
+  auto tasks = std::vector<std::shared_ptr<AbstractTask>>{};
+  tasks.emplace_back(std::make_shared<JobTask>([&]() {
+    merge_sort(first, middle);
+  }));
+  tasks.emplace_back(std::make_shared<JobTask>([&]() {
+    merge_sort(middle, last);
+  }));
+
+  Hyrise::get().scheduler()->schedule_and_wait_for_tasks(tasks);
+
+  TRACE_EVENT("Sort", "MergeSort::merge");
+  std::inplace_merge(first, middle, last);
+}
+
+void parallel_merge_sort(std::vector<std::string>& input) {
+  Hyrise::get().set_scheduler(std::make_shared<NodeQueueScheduler>());
+
+  TRACE_EVENT("Sort", "ParallelMergeSort");
+  merge_sort(input.begin(), input.end());
+
+  if (!std::ranges::is_sorted(input)) {
+    std::cerr << "Input not sorted.\n";
+  }
+
+  Hyrise::get().scheduler()->finish();
+}
+
 std::vector<std::string> get_std_strings(auto& generator,
                                          auto& random_distribution) {
   auto std_strings = std::vector<std::string>(STRING_COUNT);
@@ -83,17 +138,30 @@ int main() {
   auto compare_result_data_copy = base_data;
   const auto compare_result = get_top_by_stdsort(100, compare_result_data_copy);
 
-  // Initialize the counter.
+
+  /**
+   * The following usage of perf-cpp and Perfetto is just to show case how to use these tools. Feel free to add helper
+   * methods, classes, ... whatever you need.
+   *
+   * Initialize the perf-cpp counters.
+   */
   auto counters = perf::CounterDefinition{};
   auto event_counter = perf::EventCounter{counters};
 
   // Specify hardware events to count.
   event_counter.add({"seconds", "instructions", "cycles", "cache-misses", "dTLB-miss-ratio"});
 
-  for (const auto& function : {get_top_by_stdsort, get_top_by_indexsort, get_top_by_priorityqueue}) {
+  //const auto functions = std::vector<std::pair<std::string, std::function<std::vector<std::string>(const size_t, std::vector<std::string>&)>>{{"get_top_by_stdsort", get_top_by_stdsort}, {"get_top_by_stdsort", get_top_by_indexsort}, {"get_top_by_stdsort", get_top_by_priorityqueue}};
+  const auto functions = std::vector<std::pair<std::string, std::function<std::vector<std::string>(const size_t, std::vector<std::string>&)>>>{
+    {"get_top_by_stdsort", get_top_by_stdsort},
+    {"get_top_by_indexsort", get_top_by_indexsort},
+    {"get_top_by_priorityqueue", get_top_by_priorityqueue}
+};
+
+  for (const auto& [function_name, function] : functions) {
     auto benchmark_data = base_data;
 
-    // Run the workload.
+    // Run the workload and track with perf-cpp.
     event_counter.start();
     const auto result = function(100, benchmark_data);
     event_counter.stop();
@@ -103,7 +171,8 @@ int main() {
       Assert(result[index] == compare_result[index], "Wrong item at position " + std::to_string(index) + ".");
     }
 
-    // Print the result to the console.
+    // Print the results.
+    std::cout << ">>> " << function_name << '\n';
     const auto perf_result = event_counter.result();
     for (const auto& [event_name, value] : perf_result) {
         std::cout << event_name << ": " << value << '\n';
@@ -111,6 +180,38 @@ int main() {
     std::cout << '\n';
   }
 
+  /**
+   * Initialize Perfetto.
+   */
+  auto track_event_cfg = perfetto::protos::gen::TrackEventConfig{};
+  track_event_cfg.add_enabled_categories("Sort");
+
+  auto args = perfetto::TracingInitArgs{};
+  args.backends = perfetto::kInProcessBackend;
+  perfetto::Tracing::Initialize(args);
+  perfetto::TrackEvent::Register();
+
+  auto cfg = perfetto::TraceConfig{};
+  cfg.add_buffers()->set_size_kb(4096);
+  auto* ds_cfg = cfg.add_data_sources()->mutable_config();
+  ds_cfg->set_name("track_event");
+  ds_cfg->set_track_event_config_raw(track_event_cfg.SerializeAsString());
+
+  auto tracing_session = std::unique_ptr<perfetto::TracingSession>(perfetto::Tracing::NewTrace());
+  tracing_session->Setup(cfg);
+  tracing_session->StartBlocking();
+
+  auto sort_data = base_data;
+  parallel_merge_sort(sort_data);
+
+  tracing_session->StopBlocking();
+  auto trace_data = std::vector<char>(tracing_session->ReadTraceBlocking());
+
+  auto output = std::ofstream{};
+  output.open("dyod2025_mergesort.perfetto-trace", std::ios::out | std::ios::binary);
+  output.write(&trace_data[0], trace_data.size());
+  output.close();
+  
   return 0;
 }
 
