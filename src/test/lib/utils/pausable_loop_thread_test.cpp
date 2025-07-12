0f2d79d52a55a7b687b6fe2eeb6a7b0ea46d82e9
BeerJamin <lorik.mucolli@gmail.com>
2025-07-12 09:40:23 +0200

    address review comments in PausableLoopThreadTest

--- src/test/lib/utils/pausable_loop_thread_test.cpp
+++ src/test/lib/utils/pausable_loop_thread_test.cpp
@@ -13,46 +13,46 @@ class PausableLoopThreadTest : public BaseTest {
 };
 
 TEST_F(PausableLoopThreadTest, BasicExecution) {
-  auto loop = PausableLoopThread(std::chrono::milliseconds(10), [&](size_t) {
+  auto loop = PausableLoopThread(std::chrono::milliseconds(std::chrono::milliseconds(10)), [&](size_t) {
     ++call_count;
   });
 
-  std::this_thread::sleep_for(std::chrono::milliseconds(30));
+  std::this_thread::sleep_for(std::chrono::milliseconds(std::chrono::milliseconds(30)));
 
   EXPECT_GE(call_count.load(), 1);
 }
 
 TEST_F(PausableLoopThreadTest, PauseAndResume) {
-  auto loop = PausableLoopThread(std::chrono::milliseconds(5), [&](size_t) {
+  auto loop = PausableLoopThread(std::chrono::milliseconds(std::chrono::milliseconds(5)), [&](size_t) {
     ++call_count;
   });
 
-  std::this_thread::sleep_for(std::chrono::milliseconds(20));
+  std::this_thread::sleep_for(std::chrono::milliseconds(std::chrono::milliseconds(20)));
   loop.pause();
 
   const auto paused_count = call_count.load();
-  std::this_thread::sleep_for(std::chrono::milliseconds(30));
+  std::this_thread::sleep_for(std::chrono::milliseconds(std::chrono::milliseconds(30)));
   EXPECT_EQ(paused_count, call_count.load());
 
   loop.resume();
-  std::this_thread::sleep_for(std::chrono::milliseconds(20));
+  std::this_thread::sleep_for(std::chrono::milliseconds(std::chrono::milliseconds(20)));
   EXPECT_GT(call_count.load(), paused_count);
 }
 
 TEST_F(PausableLoopThreadTest, DestructorStopsThread) {
   auto temp = std::atomic<size_t>(0);
   {
-    auto loop = PausableLoopThread(std::chrono::milliseconds(10), [&](size_t) {
+    auto loop = PausableLoopThread(std::chrono::milliseconds(std::chrono::milliseconds(10)), [&](size_t) {
       ++temp;
       ++call_count;
     });
-    std::this_thread::sleep_for(std::chrono::milliseconds(30));
+    std::this_thread::sleep_for(std::chrono::milliseconds(std::chrono::milliseconds(30)));
   }
 
   const auto call_count_after_destruction = call_count.load();
   const auto temp_count_after_destruction = temp.load();
 
-  std::this_thread::sleep_for(std::chrono::milliseconds(std::chrono::milliseconds(50)));
+  std::this_thread::sleep_for(std::chrono::milliseconds(std::chrono::milliseconds(std::chrono::milliseconds(50))));
 
   EXPECT_EQ(call_count.load(), call_count_after_destruction);
   EXPECT_EQ(temp.load(), temp_count_after_destruction);
@@ -62,16 +62,16 @@ TEST_F(PausableLoopThreadTest, SetLoopSleepTimeChangesInterval) {
   auto timestamps = std::vector<std::chrono::steady_clock::time_point>();
   auto timestamps_mutex = std::mutex();
 
-  auto loop = PausableLoopThread(std::chrono::milliseconds(50), [&](size_t) {
+  auto loop = PausableLoopThread(std::chrono::milliseconds(std::chrono::milliseconds(50)), [&](size_t) {
     auto lock = std::lock_guard(timestamps_mutex);
     timestamps.push_back(std::chrono::steady_clock::now());
   });
 
-  std::this_thread::sleep_for(std::chrono::milliseconds(125));
+  std::this_thread::sleep_for(std::chrono::milliseconds(std::chrono::milliseconds(125)));
 
   loop.pause();
 
-  auto initial_timestamp_count = 0;
+  auto initial_timestamp_count = uint32_t{};
   {
     auto lock = std::lock_guard(timestamps_mutex);
     initial_timestamp_count = timestamps.size();
@@ -79,7 +79,7 @@ TEST_F(PausableLoopThreadTest, SetLoopSleepTimeChangesInterval) {
 
   EXPECT_GE(initial_timestamp_count, 2);
 
-  loop.set_loop_sleep_time(std::chrono::milliseconds(10));
+  loop.set_loop_sleep_time(std::chrono::milliseconds(std::chrono::milliseconds(10)));
   {
     auto lock = std::lock_guard(timestamps_mutex);
     EXPECT_EQ(initial_timestamp_count, timestamps.size());
@@ -87,7 +87,7 @@ TEST_F(PausableLoopThreadTest, SetLoopSleepTimeChangesInterval) {
 
   loop.resume();
 
-  std::this_thread::sleep_for(std::chrono::milliseconds(55));
+  std::this_thread::sleep_for(std::chrono::milliseconds(std::chrono::milliseconds(55)));
   {
     auto lock = std::lock_guard(timestamps_mutex);
     // We expect at least 4-5 new timestamps in this period. Check for more than initial + 3 to be safe.
