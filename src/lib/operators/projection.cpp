f64a1d33f851e0ed27ee29b9450e28cf9f3385d9
Martin Boissier <martin.boissier@hpi.de>
2025-07-30 00:36:02 +0200

    NOLINT for false positiv, I guess.

--- src/lib/operators/projection.cpp
+++ src/lib/operators/projection.cpp
@@ -285,6 +285,7 @@ std::shared_ptr<const Table> Projection::_on_execute() {
       // originate from the same sorted input column.
       for (const auto& [output_column_id, input_column_id] : output_column_to_input_column) {
         const auto iter = std::ranges::find_if(sorted_by, [&](const auto& sort) {
+          // NOLINTNEXTLINE(clang-analyzer-core.CallAndMessage): False positive by clang tidy.
           return input_column_id == sort.column;
         });
         if (iter != sorted_by.end()) {
