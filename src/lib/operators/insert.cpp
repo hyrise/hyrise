398f8ac0b90c3bda19a56491467614c3b0dbccb7
dey4ss <27929897+dey4ss@users.noreply.github.com>
2025-08-13 09:32:47 +0200

    remove header

--- src/lib/operators/insert.cpp
+++ src/lib/operators/insert.cpp
@@ -13,7 +13,6 @@
 #include "operators/abstract_operator.hpp"
 #include "resolve_type.hpp"
 #include "storage/abstract_segment.hpp"
-#include "storage/mvcc_data.hpp"
 #include "storage/segment_encoding_utils.hpp"
 #include "storage/segment_iterate.hpp"
 #include "storage/value_segment.hpp"
