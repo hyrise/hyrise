840ced41dbdc2e6bbd1c11147fb6a926e07115d3
Martin Boissier <martin.boissier@hpi.de>
2025-09-15 16:52:33 +0200

    Forgot install script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -51,7 +51,7 @@ if echo $REPLY | grep -E '^[Yy]$' > /dev/null; then
             if sudo apt-get update >/dev/null; then
 
                 # Packages added here should also be added to the Dockerfile
-                if ! sudo apt-get install --no-install-recommends -y software-properties-common lsb-release git python3 python3-pip autoconf bash-completion bc clang-18 llvm-20 clang-20 clang-format-20 clang-tidy-20 libclang-rt-dev cmake curl dos2unix g++-15 gcc-15 graphviz libboost-all-dev libhwloc-dev libncurses5-dev libnuma-dev libnuma1 libpq-dev libreadline-dev libsqlite3-dev libtbb-dev lld-20 man parallel postgresql-server-dev-all valgrind; then
+                if ! sudo apt-get install --no-install-recommends -y software-properties-common lsb-release git python3 python3-pip autoconf bash-completion bc clang-19 llvm-20 clang-20 clang-format-20 clang-tidy-20 libclang-rt-dev cmake curl dos2unix g++-15 gcc-15 graphviz libboost-all-dev libhwloc-dev libncurses5-dev libnuma-dev libnuma1 libpq-dev libreadline-dev libsqlite3-dev libtbb-dev lld-20 man parallel postgresql-server-dev-all valgrind; then
                     echo "Error during apt-get installations."
                     exit 1
                 fi
