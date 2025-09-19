9d08d5ca7dc9bcc5f19b538e852cb97bb50ea2ce
stastez <steeger@uni-potsdam.de>
2025-09-19 15:48:39 +0200

    Test for alias operator issue

--- src/test/lib/operators/alias_operator_test.cpp
+++ src/test/lib/operators/alias_operator_test.cpp
@@ -66,6 +66,35 @@ TEST_F(AliasOperatorTest, ForwardSortedByFlag) {
   }
 }
 
+TEST_F(AliasOperatorTest, ForwardSortedByFlagWithFlippedColumns) {
+  alias_operator->execute();
+  const auto result_table_unsorted = alias_operator->get_output();
+
+  for (ChunkID chunk_id{0}; chunk_id < result_table_unsorted->chunk_count(); ++chunk_id) {
+    const auto& sorted_by = result_table_unsorted->get_chunk(chunk_id)->individually_sorted_by();
+    EXPECT_TRUE(sorted_by.empty());
+  }
+
+  // Verify that the sorted_by flag is set when it's present in input.
+  const auto sort_definition =
+      std::vector<SortColumnDefinition>{SortColumnDefinition{ColumnID{0}}, SortColumnDefinition{ColumnID{2}}};
+  auto sort = std::make_shared<Sort>(table_wrapper, sort_definition);
+  sort->execute();
+
+  auto alias_operator_sorted = std::make_shared<AliasOperator>(sort, column_ids, aliases);
+  alias_operator_sorted->execute();
+
+  const auto result_table_sorted = alias_operator_sorted->get_output();
+  for (ChunkID chunk_id{0}; chunk_id < result_table_sorted->chunk_count(); ++chunk_id) {
+    const auto& sorted_by = result_table_sorted->get_chunk(chunk_id)->individually_sorted_by();
+    ASSERT_EQ(sorted_by.size(), 2);
+    EXPECT_EQ(sorted_by[0].column, ColumnID{1});
+    EXPECT_EQ(sorted_by[0].sort_mode, SortMode::AscendingNullsFirst);
+    EXPECT_EQ(sorted_by[1].column, ColumnID{0});
+    EXPECT_EQ(sorted_by[1].sort_mode, SortMode::AscendingNullsFirst);
+  }
+}
+
 // This test checks for an edge case where a sorted column appears twice (due to a repeated projection) but only one of
 // the two columns is known to the alias operator (see #2321).
 TEST_F(AliasOperatorTest, ForwardSortedByFlagForRepeatedColumnReferences) {
