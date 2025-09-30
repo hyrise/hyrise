28f883f5e02ea8209d734aab5679a7655a1f02df
Bouncner <martin+github@boissier.de>
2025-09-30 15:24:44 +0200

    Best working config for JOB

--- src/lib/scheduler/node_queue_scheduler.cpp
+++ src/lib/scheduler/node_queue_scheduler.cpp
@@ -37,12 +37,12 @@ namespace {
  * We scale number of groups linearly between (NUM_GROUPS_MIN_FACTOR * _workers_per_node) and (NUM_GROUPS_MAX_FACTOR *
  * _workers_per_node).
  */
-constexpr auto NUM_GROUPS_MIN_FACTOR = 0.1f;
+constexpr auto NUM_GROUPS_MIN_FACTOR = 0.001f;
 constexpr auto NUM_GROUPS_MAX_FACTOR = 2.0f;
 constexpr auto NUM_GROUPS_RANGE = NUM_GROUPS_MAX_FACTOR - NUM_GROUPS_MIN_FACTOR;
 
-// This factor is used to determine at which queue load we use the maximum number of groups.
-constexpr auto UPPER_LIMIT_QUEUE_SIZE_FACTOR = size_t{10};
+// This factor is used to determine at which queue load we use the minimum number of groups.
+constexpr auto UPPER_LIMIT_QUEUE_SIZE_FACTOR = size_t{40};
 }  // namespace
 
 namespace hyrise {
@@ -300,7 +300,7 @@ std::optional<size_t> NodeQueueScheduler::determine_group_count(
 
   // Using max() for small machines where NUM_GROUPS_MIN_FACTOR * cores can yield group_counts smaller one.
   const auto group_count =
-      std::max(size_t{2}, static_cast<size_t>(static_cast<float>(_worker_count) * group_count_factor));
+      std::max(size_t{1}, static_cast<size_t>(static_cast<float>(_worker_count) * group_count_factor));
   // If the resulting groups are smaller than 4 tasks, skip grouping.
   if ((task_count / group_count) < 4) {
     return std::nullopt;
