d026bd4eab424a644fd59ddd2214169b64c6e08a
Tobias Jordan <git@tobiasjordan.io>
2025-10-10 09:12:42 +0000

    benchmark

--- src/benchmark/star_schema_data_micro_benchmark.cpp
+++ src/benchmark/star_schema_data_micro_benchmark.cpp
@@ -528,22 +528,23 @@ BENCHMARK_DEFINE_F(ReductionBenchmarks, BadCaseReduce)(benchmark::State& state) 
 //   }
 // }
 
-BENCHMARK_REGISTER_F(ReductionBenchmarks, WorstCaseReduce)
-    ->ArgsProduct({// {1, 2, 3, 4, 5, 7, 14, 28},
-                   {1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14,
-                    15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28},
-                   {1, 5, 10, 20, 100}});
+// BENCHMARK_REGISTER_F(ReductionBenchmarks, WorstCaseReduce)
+//     ->ArgsProduct({// {1, 2, 3, 4, 5, 7, 14, 28},
+//                    {1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14,
+//                     15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28},
+//                    {1, 5, 10, 20, 100}});
 
 BENCHMARK_REGISTER_F(ReductionBenchmarks, BestCaseReduce)
     ->ArgsProduct({// {1, 2, 3, 4, 5, 7, 14, 28},
                    {1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14,
                     15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28},
-                   {1, 5, 10, 20, 100}});
+                  //  {1, 5, 10, 20, 100}});
+                   {20}});
 
-BENCHMARK_REGISTER_F(ReductionBenchmarks, BadCaseReduce)
-    ->ArgsProduct({// {1, 2, 3, 4, 5, 7, 14, 28},
-                   {1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14,
-                    15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28},
-                   {1, 5, 10, 20, 100}});
+// BENCHMARK_REGISTER_F(ReductionBenchmarks, BadCaseReduce)
+//     ->ArgsProduct({// {1, 2, 3, 4, 5, 7, 14, 28},
+//                    {1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14,
+//                     15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28},
+//                    {1, 5, 10, 20, 100}});
 
 }  // namespace hyrise
