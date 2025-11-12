cfe9f78a30129f9660eed0c3d39662d92594284f
Tobias Jordan <git@tobiasjordan.io>
2025-11-12 15:58:43 +0100

    get_l1_cache_size.sh

---
+++ scripts/get_l1_cache_size.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Path to the L1 cache size file
+L1_CACHE_FILE="/sys/devices/system/cpu/cpu0/cache/index2/size"
+L1_CACHE_SIZE_KB=$(cat "$L1_CACHE_FILE" | sed 's/K//')
+L1_CACHE_SIZE_BYTES=$((L1_CACHE_SIZE_KB * 1024))
+   
+# Output the result
+echo "$L1_CACHE_SIZE_BYTES"
