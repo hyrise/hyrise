3856772eaeeabfd067d8f94b0ad90ef856160013
Tobias Jordan <git@tobiasjordan.io>
2025-11-12 14:01:38 +0100

    min_max_predicate.cpp

--- src/lib/utils/min_max_predicate.hpp
+++ src/lib/utils/min_max_predicate.hpp
@@ -17,38 +17,17 @@ class BaseMinMaxPredicate {
 template <typename DataType>
 class MinMaxPredicate : public BaseMinMaxPredicate {
  public:
-  MinMaxPredicate() : _min_value(INT32_MAX), _max_value(INT32_MIN) {
-    Assert((std::is_same<DataType, int32_t>::value), "MinMaxPredicate can only be instantiated with int32_t.");
-  }
+  MinMaxPredicate();
 
-  void merge_from(const DataType& minimum, const DataType& maximum) {
-    if constexpr (std::is_same_v<DataType, int32_t>) {
-      set_atomic_min(_min_value, minimum);
-      set_atomic_max(_max_value, maximum);
-    } else {
-      Fail("MinMaxPredicate only supports int32_t values.");
-    }
-  }
+  void merge_from(const DataType& minimum, const DataType& maximum);
 
-  DataType min_value() const {
-    if constexpr (std::is_same_v<DataType, int32_t>) {
-      return _min_value;
-    }
-    Fail("MinMaxPredicate only supports int32_t values.");
-  }
+  DataType min_value() const;
 
-  DataType max_value() const {
-    if constexpr (std::is_same_v<DataType, int32_t>) {
-      return _max_value;
-    }
-    Fail("MinMaxPredicate only supports int32_t values.");
-  }
+  DataType max_value() const;
 
  private:
   std::atomic<int32_t> _min_value;
   std::atomic<int32_t> _max_value;
 };
-
-// template class MinMaxPredicate<int32_t>;
 
 }  // namespace hyrise
