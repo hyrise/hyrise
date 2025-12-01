718bc30f51f541d904cfd37c494f1fd32dda5a0e
Lukas Hagen <43916057+Greenscreen23@users.noreply.github.com>
2025-12-01 14:44:34 +0100

    Fix another benchmark_all bug

--- scripts/benchmark_all.sh
+++ scripts/benchmark_all.sh
@@ -99,6 +99,7 @@ do
   # If there is a bolt profile, we should compile in such a way that bolt can be used afterwards
   if [ -f ../resources/bolt.fdata ]
   then
+    echo "Detected bolt.fdata file, compiling for BOLT"
     cmake -DCOMPILE_FOR_BOLT=On ..
   else
     cmake -DCOMPILE_FOR_BOLT=Off ..
@@ -107,6 +108,7 @@ do
   # If there is a PGO profile, use it while building
   if [ -f ../resources/libhyrise.profdata ]
   then
+    echo "Detected libhyrise.profdata file, compiling with PGO"
     cmake -DPGO_OPTIMIZE=../resources/libhyrise.profdata ..
   else
     cmake -UPGO_OPTIMIZE ..
@@ -120,7 +122,7 @@ do
   if [ -f ../resources/bolt.fdata ]
   then
     mv lib/libhyrise_impl.so lib/libhyrise_impl.so.old
-    llvm-bolt lib/libhyrise_impl.so.old -o lib/libhyrise_impl.so -data bolt.fdata -reorder-blocks=ext-tsp -reorder-functions=hfsort -split-functions -split-all-cold -split-eh -dyno-stats
+    llvm-bolt lib/libhyrise_impl.so.old -o lib/libhyrise_impl.so -data ../resources/bolt.fdata -reorder-blocks=ext-tsp -reorder-functions=hfsort -split-functions -split-all-cold -split-eh -dyno-stats
     strip -R .rela.text -R ".rela.text.*" -R .rela.data -R ".rela.data.*" lib/libhyrise_impl.so
   fi
 
