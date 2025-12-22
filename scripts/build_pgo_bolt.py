cf9b7a459e5bd873f49c6da71dd6469f5902de61
Lukas Hagen <43916057+Greenscreen23@users.noreply.github.com>
2025-12-22 14:14:12 +0100

    Remove icp

--- scripts/build_pgo_bolt.py
+++ scripts/build_pgo_bolt.py
@@ -106,7 +106,6 @@ def build(*targets, bolt_instrument=False, pgo_instrument=False, bolt_optimize=F
             "--split-eh",
             "--dyno-stats",
             "--plt=hot",
-            "--indirect-call-promotion=all"
         )
         run_build('strip -R .rela.text -R ".rela.text.*" -R .rela.data -R ".rela.data.*" lib/libhyrise_impl.so')
 
