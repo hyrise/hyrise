e034a6939945ad44befc989a2f83059452eebe04
Lukas Hagen <43916057+Greenscreen23@users.noreply.github.com>
2026-02-13 14:42:48 +0100

    Small things

--- src/benchmark/micro_benchmark_utils.cpp
+++ src/benchmark/micro_benchmark_utils.cpp
@@ -1,13 +1,14 @@
 #include "micro_benchmark_utils.hpp"
 
 #include <cstddef>
+#include <cstdint>
 #include <vector>
 
 namespace hyrise {
 
 void micro_benchmark_clear_cache() {
-  constexpr auto ITEM_COUNT = size_t{2} * 1024 * 1024 * 1024 / sizeof(int);
-  auto clear = std::vector<int>(ITEM_COUNT, 42);
+  constexpr auto ITEM_COUNT = size_t{2} * 1024 * 1024 * 1024 / sizeof(int32_t);
+  auto clear = std::vector<int32_t>(ITEM_COUNT, 42);
   for (auto index = size_t{0}; index < ITEM_COUNT; ++index) {
     clear[index] += 1;
   }
