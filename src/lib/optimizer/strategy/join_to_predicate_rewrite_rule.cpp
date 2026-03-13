44b7abf2e0729b34c47d7497e24f19b20bf606c9
Lukas Hagen <43916057+Greenscreen23@users.noreply.github.com>
2026-03-13 15:17:52 +0100

    Undo fiddling with optimizer rules

--- src/lib/optimizer/strategy/join_to_predicate_rewrite_rule.cpp
+++ src/lib/optimizer/strategy/join_to_predicate_rewrite_rule.cpp
@@ -95,8 +95,7 @@ void gather_rewrite_info(
     for (const auto& predicate_operand : candidate_expression->arguments) {
       if (predicate_operand->type == ExpressionType::LQPColumn) {
         candidate_column_expression = predicate_operand;
-      } else if (predicate_operand->type == ExpressionType::Value ||
-                 predicate_operand->type == ExpressionType::Placeholder) {
+      } else if (predicate_operand->type == ExpressionType::Value) {
         candidate_value_expression = predicate_operand;
       }
     }
