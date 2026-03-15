3e31245f27f98f4e37f5197c27d445111c4127c5
Tobias Jordan <git@tobiasjordan.io>
2026-03-15 18:20:59 +0000

    selector

--- src/lib/logical_query_plan/lqp_translator.cpp
+++ src/lib/logical_query_plan/lqp_translator.cpp
@@ -437,9 +437,9 @@ std::shared_ptr<AbstractOperator> LQPTranslator::_translate_join_node(
 
   if (join_node->is_semi_reduction()) {
     const auto build_reducer = std::make_shared<Reduce>(left_input_operator, right_input_operator,
-                                                        primary_join_predicate, ReduceMode::Build, UseMinMax::No, 23);
+                                                        primary_join_predicate, ReduceMode::Build, UseMinMax::No);
     return std::make_shared<Reduce>(left_input_operator, build_reducer, primary_join_predicate, ReduceMode::Probe,
-                                    UseMinMax::No, 23);
+                                    UseMinMax::No);
   }
 
   auto join_operator = std::shared_ptr<AbstractOperator>{};
