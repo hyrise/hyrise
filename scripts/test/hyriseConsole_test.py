9a78c134618c34b0944611d81fe56ef1c236856c
Lukas Hagen <43916057+Greenscreen23@users.noreply.github.com>
2026-03-31 10:32:48 +0200

    Fullci please....

--- scripts/test/hyriseConsole_test.py
+++ scripts/test/hyriseConsole_test.py
@@ -60,17 +60,17 @@ def main():
     # Test load command.
     console.sendline("load resources/test_data/tbl/10_ints.tbl test")
     console.expect("Loading '.*tbl/10_ints.tbl' into table 'test'")
-    console.expect('Encoding "test" using Unencoded')
+    console.expect("Encoding 'test' using 'Unencoded'")
 
     console.sendline("load resources/test_data/bin/float.bin test_bin")
     console.expect("Loading '.*bin/float.bin' into table 'test_bin'")
-    console.expect('Encoding "test_bin" using Unencoded')
+    console.expect("Encoding 'test_bin' using 'Unencoded'")
 
     # Reload table with a specified encoding and check meta tables for applied encoding.
     console.sendline("load resources/test_data/bin/float.bin test_bin RunLength")
     console.expect("Loading '.*bin/float.bin' into table 'test_bin'")
-    console.expect('Table "test_bin" already existed. Replacing it.')
-    console.expect('Encoding "test_bin" using RunLength')
+    console.expect("Table 'test_bin' already existed. Replacing it.")
+    console.expect("Encoding 'test_bin' using 'RunLength'")
     console.sendline(
         "select encoding_type from meta_segments where table_name='test_bin' and chunk_id=0 and column_id=0"
     )
