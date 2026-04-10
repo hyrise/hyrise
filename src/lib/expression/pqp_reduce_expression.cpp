05bc14712367bb9a1c90f41ddbbe6d5ea6205085
dey4ss <27929897+dey4ss@users.noreply.github.com>
2026-04-10 13:04:37 +0200

    [WIP] LQP representation

--- src/lib/expression/pqp_reduce_expression.cpp
+++ src/lib/expression/pqp_reduce_expression.cpp
@@ -1,4 +1,4 @@
-#include "pqp_build_expression.hpp"
+#include "pqp_reduce_expression.hpp"
 
 #include <cstddef>
 #include <memory>
@@ -20,49 +20,48 @@
 
 namespace hyrise {
 
-PQPBuildExpression::PQPBuildExpression(const std::shared_ptr<Build>& init_pqp, const ColumnID init_column_id,
+PQPReduceExpression::PQPReduceExpression(const std::shared_ptr<AbstractOperator>& init_reducer, const ColumnID init_column_id,
                                        const DataType data_type)
     : AbstractExpression(ExpressionType::PQPBuild, {}),
-      build(init_pqp),
-      column_id(init_column_id),
+      reducer{init_reducer},
+      column_id{init_column_id},
       _data_type{data_type} {}
 
-std::shared_ptr<AbstractExpression> PQPBuildExpression::_on_deep_copy(
+std::shared_ptr<AbstractExpression> PQPReduceExpression::_on_deep_copy(
     std::unordered_map<const AbstractOperator*, std::shared_ptr<AbstractOperator>>& copied_ops) const {
-  auto copied_in = std::static_pointer_cast<Build>(build->deep_copy(copied_ops));
-  return std::make_shared<PQPBuildExpression>(copied_in, column_id, _data_type);
+  return std::make_shared<PQPReduceExpression>(reducer->deep_copy(copied_ops), column_id, _data_type);
 }
 
-std::string PQPBuildExpression::description(const DescriptionMode /*mode*/) const {
+std::string PQPReduceExpression::description(const DescriptionMode /*mode*/) const {
   auto stream = std::stringstream{};
-  stream << "BUILD (PQP, " << build << ")";
+  stream << "Reduce (PQP, " << build << ")";
   return stream.str();
 }
 
-DataType PQPBuildExpression::data_type() const {
+DataType PQPReduceExpression::data_type() const {
   return _data_type;
 }
 
-bool PQPBuildExpression::_shallow_equals(const AbstractExpression& expression) const {
+bool PQPReduceExpression::_shallow_equals(const AbstractExpression& expression) const {
   // For deep copies of this expression, the PQP is recreated. It is not only difficult to correctly compare PQPs, but
   // it also has unclear semantics. Is an executed PQP equal to a non-executed one? How about two executed ones?
-  // As such, we only report equality if two PQPBuildExpression refer to the very same PQP, meaning that a deep copy
+  // As such, we only report equality if two PQPReduceExpression refer to the very same PQP, meaning that a deep copy
   // is not equal to its source. If this ever becomes an issue, it should be easy to spot.
-  DebugAssert(dynamic_cast<const PQPBuildExpression*>(&expression),
+  DebugAssert(dynamic_cast<const PQPReduceExpression*>(&expression),
               "Different expression type should have been caught by AbstractExpression::operator==");
-  const auto& other = static_cast<const PQPBuildExpression&>(expression);
-  return build == other.build && column_id == other.column_id;
+  const auto& other = static_cast<const PQPReduceExpression&>(expression);
+  return reducer == other.reducer && column_id == other.column_id;
 }
 
-size_t PQPBuildExpression::_shallow_hash() const {
+size_t PQPReduceExpression::_shallow_hash() const {
   size_t hash{0};
   boost::hash_combine(hash, column_id);
   boost::hash_combine(hash, _data_type);
-  boost::hash_combine(hash, build->type());  // TODO(anyone): Not a full hash. Implement and use a hash of/on PQPs?
+  boost::hash_combine(hash, reducer->type());  // TODO(anyone): Not a full hash. Implement and use a hash of/on PQPs?
   return hash;
 }
 
-bool PQPBuildExpression::_on_is_nullable_on_lqp(const AbstractLQPNode& /*lqp*/) const {
+bool PQPReduceExpression::_on_is_nullable_on_lqp(const AbstractLQPNode& /*lqp*/) const {
   Fail("Nullability 'on lqp' should never be queried from a PQPSelect");
 }
 
--- src/lib/expression/pqp_reduce_expression.hpp
+++ src/lib/expression/pqp_reduce_expression.hpp
@@ -14,15 +14,15 @@ namespace hyrise {
 class Build;
 
 /**
- * A PQPSubqueryExpression represents a subquery (think `a > (SELECT MIN(a) FROM ...`) used as part of an expression in
+ * A PQPReduceExpression represents a subquery (think `a > (SELECT MIN(a) FROM ...`) used as part of an expression in
  * a PQP.
  *
  * The parameters of a PQPSubqueryExpression are equivalent to the correlated parameters of a nested SELECT in SQL.
  */
-class PQPBuildExpression : public AbstractExpression {
+class PQPReduceExpression : public AbstractExpression {
  public:
   // Constructor for single-column PQPSubqueryExpressions as used in `a IN (SELECT ...)` or `SELECT (SELECT ...)`.
-  PQPBuildExpression(const std::shared_ptr<Build>& init_pqp, const ColumnID init_column_id, const DataType data_type);
+  PQPReduceExpression(const std::shared_ptr<AbstractOperator>& init_reducer, const ColumnID init_column_id, const DataType data_type);
 
   std::shared_ptr<AbstractExpression> _on_deep_copy(
       std::unordered_map<const AbstractOperator*, std::shared_ptr<AbstractOperator>>& copied_ops) const override;
@@ -30,8 +30,8 @@ class PQPBuildExpression : public AbstractExpression {
   std::string description(const DescriptionMode /*mode*/) const override;
   DataType data_type() const override;
 
-  const std::shared_ptr<Build> build;
-  const ColumnID column_id;
+  std::shared_ptr<AbstractOperator> reducer;
+  ColumnID column_id;
 
  protected:
   bool _shallow_equals(const AbstractExpression& expression) const override;
