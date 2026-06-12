d53e3d753387bb120857bc7caa56f8a2522e440a
Martin Boissier <martin.boissier@hpi.de>
2026-06-12 22:17:18 +0200

    Adjust format for main branch

--- scripts/format.sh
+++ scripts/format.sh
@@ -18,8 +18,8 @@ elif [ "$1" = "staged" ]; then
     # Run on all files that are staged to be committed.
     git diff --diff-filter=d --cached --name-only | grep -E "^src.*\.[chi]pp$" | xargs -I{} sh -c "${format_cmd}"
 else
-    # Run on all changed as well as untracked cpp/hpp files, as compared to the current master. Skip deleted files.
-    { git diff --diff-filter=d --name-only master & git ls-files --others --exclude-standard; } | grep -E "^src.*\.[chi]pp$" | xargs -I{} sh -c "${format_cmd}"
+    # Run on all changed as well as untracked cpp/hpp files, as compared to the current main. Skip deleted files.
+    { git diff --diff-filter=d --name-only main & git ls-files --others --exclude-standard; } | grep -E "^src.*\.[chi]pp$" | xargs -I{} sh -c "${format_cmd}"
 fi
 
 # Python formatting
