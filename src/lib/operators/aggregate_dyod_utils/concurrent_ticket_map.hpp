5f556e6e332c8cb5c6fe0d16d59620204c28ad58
Robert Weeke <Robert.Weeke@student.hpi.uni-potsdam.de>
2026-07-21 17:22:43 +0200

    make lint happy

--- src/lib/operators/aggregate_dyod_utils/concurrent_ticket_map.hpp
+++ src/lib/operators/aggregate_dyod_utils/concurrent_ticket_map.hpp
@@ -96,7 +96,8 @@ class ConcurrentTicketMap {
     _slots = SlotArray{static_cast<Slot*>(::operator new[](capacity * sizeof(Slot)))};
 
     const auto initialize_slots = [slots = _slots.get()](const size_t begin, const size_t end) {
-      // TODO(@forUnity): Test using std::memset here. This would also set key to 0 and hardcode that EMPTY == 0. However it might be faster.
+      // TODO(@forUnity): Test using std::memset here. This would also set key to 0 and hardcode that EMPTY == 0.
+      // However it might be faster.
       std::memset(slots + begin, 0, (end - begin) * sizeof(Slot));
     };
 
