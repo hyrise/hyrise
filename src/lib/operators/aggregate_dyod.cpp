7a336efacae171c5b751f4574bcdb9130c58f889
Johanna G. <jojo.gasse@gmail.com>
2026-08-08 15:23:17 +0200

    ups

--- src/lib/operators/aggregate_dyod.cpp
+++ src/lib/operators/aggregate_dyod.cpp
@@ -1000,12 +1000,10 @@ KeysPerChunk<AggregateKey> AggregateDYOD::_partition_by_groupby_keys(const std::
             The ID 0 is reserved for NULL values. The combined IDs build an AggregateKey for each row.
             */
 
-          (void)
-
-              // This time, we have no idea how much space we need, so we take some memory and then rely on the automatic
-              // resizing. The size is quite random, but since single memory allocations do not cost too much, we rather
-              // allocate a bit too much.
-              auto temp_buffer = std::pmr::monotonic_buffer_resource(1'000'000);
+          // This time, we have no idea how much space we need, so we take some memory and then rely on the automatic
+          // resizing. The size is quite random, but since single memory allocations do not cost too much, we rather
+          // allocate a bit too much.
+          auto temp_buffer = std::pmr::monotonic_buffer_resource(1'000'000);
           auto allocator = PolymorphicAllocator<std::pair<const ColumnDataType, DYODAggregateKeyEntry>>{&temp_buffer};
 
           auto id_map = boost::unordered_flat_map<ColumnDataType, DYODAggregateKeyEntry, std::hash<ColumnDataType>,
