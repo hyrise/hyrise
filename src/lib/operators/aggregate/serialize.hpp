f562e106626213a0b7d512851b9ae16b1feb32a0
Till Prochaska <1512805+tillprochaska@users.noreply.github.com>
2026-08-10 12:27:40 +0200

    Remove unused helpers

--- src/lib/operators/aggregate/serialize.hpp
+++ src/lib/operators/aggregate/serialize.hpp
@@ -45,38 +45,4 @@ void serialize_value(std::vector<std::byte>& buffer, const pmr_string& value, bo
   std::memcpy(buffer.data() + offset + 1, value.data(), value.size());
 }
 
-template <typename T, bool Nullable>
-  requires std::is_trivially_copyable_v<T> && (!Nullable)
-T deserialize_value(const std::span<const std::byte>& bytes) {
-  T value;
-  std::memcpy(&value, bytes.data(), sizeof(T));
-  return value;
-}
-
-template <typename T, bool Nullable>
-  requires std::is_trivially_copyable_v<T> && Nullable
-std::optional<T> deserialize_value(const std::span<const std::byte>& bytes) {
-  if (bytes[0] == std::byte{0x01}) {
-    return std::nullopt;
-  }
-  T value;
-  std::memcpy(&value, bytes.data() + 1, sizeof(T));
-  return value;
-}
-
-template <typename T, bool Nullable>
-  requires std::is_same_v<T, pmr_string> && (!Nullable)
-pmr_string deserialize_value(const std::span<const std::byte>& bytes) {
-  return pmr_string(reinterpret_cast<const char*>(bytes.data()), bytes.size());
-}
-
-template <typename T, bool Nullable>
-  requires std::is_same_v<T, pmr_string> && Nullable
-std::optional<pmr_string> deserialize_value(const std::span<const std::byte>& bytes) {
-  if (bytes[0] == std::byte{0x01}) {
-    return std::nullopt;
-  }
-  return pmr_string(reinterpret_cast<const char*>(bytes.data() + 1), bytes.size() - 1);
-}
-
 }  // namespace hyrise
