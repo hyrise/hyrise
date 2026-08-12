fae58d766eb467035ed925e02597b076f7c9831f
dey4ss <27929897+dey4ss@users.noreply.github.com>
2026-08-12 15:22:24 +0200

    silence lint warning

--- src/bin/playground.cpp
+++ src/bin/playground.cpp
@@ -2,9 +2,9 @@
 
 #include "types.hpp"
 
-int main() {
-  using namespace hyrise;
+using namespace hyrise;
 
+int main() {
   const auto world = pmr_string{"world"};
   std::cout << "Hello " << world << "!\n";
   return 0;
