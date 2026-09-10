8524c7536e4c9064cbaa16bc29b440b37a89c74c
FePrHPI <felix.preissner@student.hpi.uni-potsdam.de>
2026-09-10 11:03:42 +0200

    tidy not happy

--- src/lib/storage/fixed_string_dictionary_segment/fixed_string.cpp
+++ src/lib/storage/fixed_string_dictionary_segment/fixed_string.cpp
@@ -16,8 +16,7 @@
 
 namespace hyrise {
 
-FixedString::FixedString(char* mem, size_t string_length)
-    : _mem(mem), _maximum_length(string_length), _owns_memory(false) {}
+FixedString::FixedString(char* mem, size_t string_length) : _mem(mem), _maximum_length(string_length) {}
 
 FixedString::FixedString(const FixedString& other)
     : _mem(new char[other._maximum_length]{}), _maximum_length(other._maximum_length), _owns_memory(true) {
