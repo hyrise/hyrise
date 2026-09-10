34d9aff083c4c891fa2179ece258871ae40333f2
FePrHPI <felix.preissner@student.hpi.uni-potsdam.de>
2026-09-10 10:50:22 +0200

    tidy

--- src/lib/storage/fixed_string_dictionary_segment/fixed_string.hpp
+++ src/lib/storage/fixed_string_dictionary_segment/fixed_string.hpp
@@ -26,7 +26,7 @@ class FixedString {
   // Create a FixedString with an existing one
   FixedString(const FixedString& other);
   // NOLINTNEXTLINE(cppcoreguidelines-noexcept-move-operations,hicpp-noexcept-move,performance-noexcept-move-constructor)
-  FixedString(FixedString&&);
+  FixedString(FixedString&& /*other*/);
 
   ~FixedString();
 
@@ -84,7 +84,7 @@ class FixedString {
  private:
   char* _mem;
   size_t _maximum_length;
-  bool _owns_memory;
+  bool _owns_memory = false;
 };
 
 }  // namespace hyrise
