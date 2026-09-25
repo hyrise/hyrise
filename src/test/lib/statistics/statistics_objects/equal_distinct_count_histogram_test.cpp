dd25255c5ebd8d847a6bd5293ac9f394b5135142
Martin Boissier <martin.boissier@hpi.de>
2026-09-25 21:35:52 +0200

    Use orders table as 2s test runtime for lineitem was a bit heavy

--- src/test/lib/statistics/statistics_objects/equal_distinct_count_histogram_test.cpp
+++ src/test/lib/statistics/statistics_objects/equal_distinct_count_histogram_test.cpp
@@ -81,40 +81,54 @@ TEST_F(EqualDistinctCountHistogramTest, AllNullValues) {
   ASSERT_FALSE(hist);
 }
 
-TEST_F(EqualDistinctCountHistogramTest, FromTPCHlineitem) {
+TEST_F(EqualDistinctCountHistogramTest, FromTPCHOrders) {
   Hyrise::get().set_scheduler(std::make_shared<NodeQueueScheduler>());
 
-  const auto lineitem = load_table("resources/test_data/tbl/tpch/sf-0.02/lineitem.tbl", ChunkOffset{1'000});
-  ASSERT_EQ(lineitem->chunk_count(), 121);
+  const auto orders = load_table("resources/test_data/tbl/tpch/sf-0.02/orders.tbl", ChunkOffset{900});
+  ASSERT_EQ(orders->row_count(), 30'000);
+  ASSERT_EQ(orders->chunk_count(), 34);
 
   // For each column, we test the first, middle, and last bin.
-  const auto orderkey_histogram = EqualDistinctCountHistogram<int32_t>::from_column(*lineitem, ColumnID{0}, 16);
+  const auto orderkey_histogram = EqualDistinctCountHistogram<int32_t>::from_column(*orders, ColumnID{0}, 16);
   ASSERT_TRUE(orderkey_histogram);
   ASSERT_EQ(orderkey_histogram->bin_count(), 16);
-  EXPECT_EQ(orderkey_histogram->total_count(), lineitem->row_count());
-  EXPECT_EQ(orderkey_histogram->bin(BinID{0}), HistogramBin<int32_t>(1, 7491, 7501, 1875));
-  EXPECT_EQ(orderkey_histogram->bin(BinID{8}), HistogramBin<int32_t>(60'001, 67'491, 7467, 1875));
-  EXPECT_EQ(orderkey_histogram->bin(BinID{15}), HistogramBin<int32_t>(112'486, 120'000, 7499, 1875));
+  EXPECT_EQ(orderkey_histogram->total_count(), orders->row_count());
+  EXPECT_EQ(orderkey_histogram->bin(BinID{0}), HistogramBin<int32_t>(1, 7491, 1875, 1875));
+  EXPECT_EQ(orderkey_histogram->bin(BinID{8}), HistogramBin<int32_t>(60'001, 67'491, 1875, 1875));
+  EXPECT_EQ(orderkey_histogram->bin(BinID{15}), HistogramBin<int32_t>(112'486, 120'000, 1875, 1875));
 
-  const auto quantity_histogram = EqualDistinctCountHistogram<float>::from_column(*lineitem, ColumnID{4}, 64);
-  ASSERT_TRUE(quantity_histogram);
-  ASSERT_EQ(quantity_histogram->bin_count(), 50);
-  EXPECT_EQ(quantity_histogram->total_count(), lineitem->row_count());
-  EXPECT_EQ(quantity_histogram->bin(BinID{0}), HistogramBin<float>(1.0f, 1.0f, 2414, 1));
-  EXPECT_EQ(quantity_histogram->bin(BinID{25}), HistogramBin<float>(26.0f, 26.0f, 2518, 1));
-  EXPECT_EQ(quantity_histogram->bin(BinID{49}), HistogramBin<float>(50.0f, 50.0f, 2426, 1));
+  const auto totalprice_histogram = EqualDistinctCountHistogram<float>::from_column(*orders, ColumnID{3}, 64);
+  ASSERT_TRUE(totalprice_histogram);
+  ASSERT_EQ(totalprice_histogram->bin_count(), 64);
+  EXPECT_EQ(totalprice_histogram->total_count(), orders->row_count());
+  EXPECT_EQ(totalprice_histogram->bin(BinID{0}), HistogramBin<float>(913.01f, 8181.45f, 469, 469));
+  EXPECT_EQ(totalprice_histogram->bin(BinID{32}), HistogramBin<float>(135'630.52f, 139'741.90f, 470, 468));
+  EXPECT_EQ(totalprice_histogram->bin(BinID{63}), HistogramBin<float>(326'850.39f, 451'578.10f, 468, 468));
 
-  const auto comment_histogram = EqualDistinctCountHistogram<pmr_string>::from_column(*lineitem, ColumnID{15}, 256);
+  // More bins than values in o_orderpriority.
+  const auto orderpriority_histogram = EqualDistinctCountHistogram<pmr_string>::from_column(*orders, ColumnID{5}, 64);
+  ASSERT_TRUE(orderpriority_histogram);
+  ASSERT_EQ(orderpriority_histogram->bin_count(), 5);
+  EXPECT_EQ(orderpriority_histogram->total_count(), orders->row_count());
+  EXPECT_EQ(orderpriority_histogram->bin(BinID{0}), HistogramBin<pmr_string>("1-URGENT", "1-URGENT", 5985, 1));
+  EXPECT_EQ(orderpriority_histogram->bin(BinID{1}), HistogramBin<pmr_string>("2-HIGH", "2-HIGH", 6062, 1));
+  EXPECT_EQ(orderpriority_histogram->bin(BinID{2}), HistogramBin<pmr_string>("3-MEDIUM", "3-MEDIUM", 5902, 1));
+  EXPECT_EQ(orderpriority_histogram->bin(BinID{3}),
+            HistogramBin<pmr_string>("4-NOT SPECIFIED", "4-NOT SPECIFIED", 6010, 1));
+  EXPECT_EQ(orderpriority_histogram->bin(BinID{4}), HistogramBin<pmr_string>("5-LOW", "5-LOW", 6041, 1));
+
+  const auto comment_histogram = EqualDistinctCountHistogram<pmr_string>::from_column(*orders, ColumnID{8}, 256);
   ASSERT_TRUE(comment_histogram);
   ASSERT_EQ(comment_histogram->bin_count(), 256);
-  EXPECT_EQ(comment_histogram->total_count(), lineitem->row_count());
-  EXPECT_EQ(comment_histogram->bin(BinID{0}),
-            HistogramBin<pmr_string>(" Tiresias ", " accounts cajole furiously f", 512, 451));
+  EXPECT_EQ(comment_histogram->total_count(), orders->row_count());
+  EXPECT_EQ(comment_histogram->bin(BinID{0}), HistogramBin<pmr_string>(" about the accounts. slyly express accounts wa",
+                                                                       " accounts nag blithel", 118, 118));
   EXPECT_EQ(comment_histogram->bin(BinID{128}),
-            HistogramBin<pmr_string>("ironic requests. blithely ironic pl", "ithely after the furiously silent pack",
-                                     458, 451));
-  EXPECT_EQ(comment_histogram->bin(BinID{255}),
-            HistogramBin<pmr_string>("yly ironic instructions. regular foxes w", "zzle: pending i", 465, 450));
+            HistogramBin<pmr_string>("ironic requests. packages cajole according",
+                                     "ithely blithe deposits sleep beyond the", 117, 117));
+  EXPECT_EQ(
+      comment_histogram->bin(BinID{255}),
+      HistogramBin<pmr_string>("yly regular packages ar", "zzle. carefully enticing deposits nag furio", 117, 117));
 }
 
 }  // namespace hyrise
